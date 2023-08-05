303c78ccb117f3c0bc345d6c045e1005766a0e38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:28 +0000

    Add package pronto/0.1.3/pronto-0.1.3.tar.gz

---
+++ packages/pronto/pronto-0.1.3.tar.gz/pronto-0.1.3/pronto/__init__.py
@@ -0,0 +1,10 @@
+__version__='0.1.3'
+__author__='Martin Larralde'
+__author_email__ = 'martin.larralde@ens-cachan.fr'
+
+from pronto.ontology import Ontology
+#from pronto.obo import Obo
+#from pronto.owl import OwlXML
+from pronto.term import Term, TermList
+
+__all__ = ["Ontology", "Obo", "OwlXML", "Term", "TermList"]
