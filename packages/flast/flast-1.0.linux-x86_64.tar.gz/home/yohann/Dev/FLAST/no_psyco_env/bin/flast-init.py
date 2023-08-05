cd5948b1286f361562c64759879ea57268972bd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:03 +0000

    Add package flast/1.0/flast-1.0.linux-x86_64.tar.gz

---
+++ packages/flast/flast-1.0.linux-x86_64.tar.gz/home/yohann/Dev/FLAST/no_psyco_env/bin/flast-init.py
@@ -0,0 +1,42 @@
+#!/home/yohann/Dev/FLAST/no_psyco_env/bin/python
+
+import os
+import argparse
+from jinja2 import Template
+from flast import templates
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--pg", required=False, dest="pg", action='store_true',
+                    help="create a project with postgresql support")
+parser.add_argument("--redis", required=False, dest="rs", action='store_true',
+                    help="create a project with redis support")
+templates_path = templates.__path__[0]
+
+cur_dir = os.path.abspath('.')
+args = parser.parse_args()
+
+for elem in os.listdir(templates_path):
+    if not elem.endswith("py"):
+        continue
+    path = os.path.join(templates_path, elem)
+    target = os.path.join(cur_dir, elem)
+
+    if not os.path.exists(target):
+        try:
+            template = Template(open(path).read())
+        except:
+            import ipdb; ipdb.set_trace()
+        app_type= ["BaseApp"]
+        if args.pg:
+            app_type.append("PostgreSQLApp")
+        if args.rs:
+            app_type.append("RedisApp")
+        app_type.reverse()
+        render = template.render(app_type=app_type)
+        with open(target, "w") as fd:
+            fd.write(render)
+
+for elem in ["sql", "templates", "statics"]:
+    target = os.path.join(cur_dir, elem)
+    if not os.path.exists(target):
+        os.mkdir(target)
