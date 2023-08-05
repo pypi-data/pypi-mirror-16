a5a3825789f3c49b56fe2f3872a15a84e877dc29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:00 +0000

    Add package puzcw/0.0.6/puzcw-0.0.6.tar.gz

---
+++ packages/puzcw/puzcw-0.0.6.tar.gz/puzcw-0.0.6/setup.py
@@ -0,0 +1,16 @@
+import os
+from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name='puzcw',
+    version='0.0.6',
+    author='Brian Wu',
+    author_email='brian.george.wu@gmail.com',
+    description='Python API for converting puz files to python objects',
+    keywords=['crossword', 'puz'],
+    packages=['puzcw'],
+    package_data={'static':['*']}
+)
