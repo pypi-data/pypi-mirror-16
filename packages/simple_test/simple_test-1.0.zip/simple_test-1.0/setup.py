641d1894119c48261c3b86f9b4e7aa8dd9628619
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:11 +0000

    Add package simple_test/1.0/simple_test-1.0.zip

---
+++ packages/simple_test/simple_test-1.0.zip/simple_test-1.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(
+    name='simple_test',
+    version='1.0',
+    author='xldang',
+    author_email='467232490@qq.com',
+    #url='www.fallincloud.com',
+    description='A simple tester of Pypi upload procedures',
+    )
