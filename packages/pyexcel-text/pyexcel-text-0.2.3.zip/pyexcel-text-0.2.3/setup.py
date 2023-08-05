3e183e26375a24d9a6ffbe1eb047ea60edafeff5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:40 +0000

    Add package pyexcel-text/0.2.3/pyexcel-text-0.2.3.zip

---
+++ packages/pyexcel-text/pyexcel-text-0.2.3.zip/pyexcel-text-0.2.3/setup.py
@@ -0,0 +1,90 @@
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
+
+NAME = 'pyexcel-text'
+AUTHOR = 'C.W.'
+VERSION = '0.2.3'
+EMAIL = 'wangc_2011 (at) hotmail.com'
+LICENSE = 'New BSD'
+PACKAGES = find_packages(exclude=['ez_setup', 'examples', 'tests'])
+DESCRIPTION = (
+    'A plugin to pyexcel and provides the capability to present and write d' +
+    'ata in text formats' +
+    ''
+)
+KEYWORDS = [
+    'excel',
+    'python',
+    'pyexcel',
+    "plain",
+    "simple",
+    "grid",
+    "pipe",
+    "orgtbl",
+    "rst",
+    "mediawiki",
+    "latex",
+    "latex_booktabs",
+    "html",
+    "json"
+]
+
+INSTALL_REQUIRES = [
+    'pyexcel>=0.2.2',
+    'tabulate>=0.7.4',
+]
+
+EXTRAS_REQUIRE = {
+}
+
+CLASSIFIERS = [
+    'Topic :: Office/Business',
+    'Topic :: Utilities',
+    'Topic :: Software Development :: Libraries',
+    'Programming Language :: Python',
+    'License :: OSI Approved :: BSD License',
+    'Intended Audience :: Developers',
+    'Development Status :: 3 - Alpha',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3.3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: Implementation :: PyPy'
+]
+
+
+def read_files(*files):
+    """Read files into setup"""
+    text = ""
+    for single_file in files:
+        text = text + read(single_file) + "\n"
+    return text
+
+
+def read(afile):
+    """Read a file into setup"""
+    with open(afile, 'r') as opened_file:
+        return opened_file.read()
+
+
+if __name__ == '__main__':
+    setup(
+        name=NAME,
+        author=AUTHOR,
+        version=VERSION,
+        author_email=EMAIL,
+        description=DESCRIPTION,
+        install_requires=INSTALL_REQUIRES,
+        keywords=KEYWORDS,
+        extras_require=EXTRAS_REQUIRE,
+        packages=PACKAGES,
+        include_package_data=True,
+        long_description=read_files('README.rst', 'CHANGELOG.rst'),
+        zip_safe=False,
+        tests_require=['nose'],
+        license=LICENSE,
+        classifiers=CLASSIFIERS
+    )
