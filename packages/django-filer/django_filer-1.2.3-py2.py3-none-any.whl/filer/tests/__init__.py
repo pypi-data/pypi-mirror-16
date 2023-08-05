51d1da4e1ade2d6009d75b75f8a3c591b71ab761
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:28 +0000

    Add package django-filer/1.2.3/django_filer-1.2.3-py2.py3-none-any.whl

---
+++ packages/django-filer/django_filer-1.2.3-py2.py3-none-any.whl/filer/tests/__init__.py
@@ -0,0 +1,10 @@
+#-*- coding: utf-8 -*-
+from __future__ import absolute_import
+
+from .admin import *
+from .dump import *
+from .models import *
+from .permissions import *
+from .server_backends import *
+from .tools import *
+from .utils import *
