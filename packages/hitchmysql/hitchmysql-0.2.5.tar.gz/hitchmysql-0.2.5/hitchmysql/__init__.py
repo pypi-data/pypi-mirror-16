d996b792c8abf0f2ad8d937e9d23beefa78e4e62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:46 +0000

    Add package hitchmysql/0.2.5/hitchmysql-0.2.5.tar.gz

---
+++ packages/hitchmysql/hitchmysql-0.2.5.tar.gz/hitchmysql-0.2.5/hitchmysql/__init__.py
@@ -0,0 +1,7 @@
+from hitchmysql.mysql_service import MySQLDatabase
+from hitchmysql.mysql_service import MySQLUser
+from hitchmysql.mysql_service import MySQLService
+from hitchmysql.mysql_package import MySQLPackage
+
+
+UNIXPACKAGES = ["libaio1", "libmysqlclient-dev", "libncurses5-dev", ]
