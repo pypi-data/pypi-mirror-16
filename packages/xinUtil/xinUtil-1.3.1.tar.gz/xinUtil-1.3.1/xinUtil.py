25df02f677723482c0b96444fbe0103ad1af5894
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:34 +0000

    Add package xinUtil/1.3.1/xinUtil-1.3.1.tar.gz

---
+++ packages/xinUtil/xinUtil-1.3.1.tar.gz/xinUtil-1.3.1/xinUtil.py
@@ -0,0 +1,14 @@
+""" This is the "xinUtil.py" module, and it provides 
+print_list() which prints lists that may or may not
+include nested lists."""
+import sys
+
+def print_list(the_list, indent=False, level=0, fh=sys.stdout):
+    for each_item in the_list:
+        if isinstance(each_item, list):
+            print_list(each_item, indent, level+1, fh)
+        else:
+            if indent:
+                for tab_step in range(level):
+                    print("\t", end='', file=fh)
+            print(each_item, file=fh)
