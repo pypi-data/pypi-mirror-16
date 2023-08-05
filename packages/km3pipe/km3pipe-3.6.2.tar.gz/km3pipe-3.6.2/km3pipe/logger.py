9b281bdfb238dbd71ccaf751c979b62bf6e56372
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:32 +0000

    Add package km3pipe/3.6.2/km3pipe-3.6.2.tar.gz

---
+++ packages/km3pipe/km3pipe-3.6.2.tar.gz/km3pipe-3.6.2/km3pipe/logger.py
@@ -0,0 +1,42 @@
+# coding=utf-8
+# Filename: logger.py
+# pylint: disable=locally-disabled,C0103
+"""
+The logging facility.
+
+"""
+from __future__ import division, absolute_import, print_function
+
+import logging
+import logging.config
+
+__author__ = "Tamas Gal"
+__copyright__ = "Copyright 2016, Tamas Gal and the KM3NeT collaboration."
+__credits__ = []
+__license__ = "MIT"
+__maintainer__ = "Tamas Gal"
+__email__ = "tgal@km3net.de"
+__status__ = "Development"
+
+try:
+    logging.config.fileConfig('logging.conf')
+except Exception:
+    logging.basicConfig()
+
+logging.addLevelName(logging.INFO, "\033[1;32m%s\033[1;0m" %
+                     logging.getLevelName(logging.INFO))
+logging.addLevelName(logging.DEBUG, "\033[1;34m%s\033[1;0m" %
+                     logging.getLevelName(logging.DEBUG))
+logging.addLevelName(logging.WARNING, "\033[1;33m%s\033[1;0m" %
+                     logging.getLevelName(logging.WARNING))
+logging.addLevelName(logging.ERROR, "\033[1;31m%s\033[1;0m" %
+                     logging.getLevelName(logging.ERROR))
+logging.addLevelName(logging.CRITICAL, "\033[1;101m%s\033[1;0m" %
+                     logging.getLevelName(logging.CRITICAL))
+
+ch = logging.StreamHandler()
+
+# pylint: disable=C0103
+formatter = logging.Formatter('[%(levelname)s] %(name)s: %(message)s')
+# ch.setFormatter(formatter)
+# logger.addHandler(ch)
