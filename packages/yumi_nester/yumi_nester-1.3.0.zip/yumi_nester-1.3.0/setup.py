09ecb2a62fb480fd8f3457c41af2ba1e92146777
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:18 +0000

    Add package yumi_nester/1.3.0/yumi_nester-1.3.0.zip

---
+++ packages/yumi_nester/yumi_nester-1.3.0.zip/yumi_nester-1.3.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+       name         ='yumi_nester',
+       version      ='1.3.0',
+       py_modules   =['nester'],
+       author       ='hfpython',
+       author_email ='hfpython@headfirstlabs.com',
+       url          ='http://www.headfirestlabs.com',
+       description  ='A simple printer of nested lists',
+       )
