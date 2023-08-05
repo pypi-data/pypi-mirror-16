17d9a96da762676fe9be4f6b7518ac3e4f4029fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:33 +0000

    Add package PyPika/0.0.3/PyPika-0.0.3.tar.gz

---
+++ packages/PyPika/PyPika-0.0.3.tar.gz/PyPika-0.0.3/pypika/enums.py
@@ -0,0 +1,65 @@
+# coding: utf8
+from aenum import Enum
+
+__author__ = "Timothy Heys"
+__email__ = "theys@kayak.com"
+__version__ = "0.0.1"
+
+
+class Arithmetic(Enum):
+    add = '+'
+    sub = '-'
+    mul = '*'
+    div = '/'
+
+
+class Comparator(Enum):
+    pass
+
+
+class Equality(Comparator):
+    eq = '='
+    ne = '<>'
+    gt = '>'
+    gte = '>='
+    lt = '<'
+    lte = '<='
+
+
+class Matching(Comparator):
+    like = ' LIKE '
+    regex = ' REGEX '
+    bin_regex = ' REGEX BINARY '
+
+
+class Boolean(Comparator):
+    and_ = 'AND'
+    or_ = 'OR'
+    xor_ = 'XOR'
+
+
+class Order(Enum):
+    asc = 'ASC'
+    desc = 'DESC'
+
+
+class JoinType(Enum):
+    left = ''
+    right = 'RIGHT'
+    inner = 'INNER'
+    outer = 'OUTER'
+
+
+class UnionType(Enum):
+    distinct = ''
+    all = ' ALL'
+
+
+class DatePart(Enum):
+    year = 'YEAR'
+    month = 'MONTH'
+    day = 'DAY'
+    hour = 'HOUR'
+    minute = 'MINUTE'
+    second = 'SECOND'
+    microsecond = 'MICROSECOND'
