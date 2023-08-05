7e9a75ff9aa1a4d355c42c41c7330bbb5b17b7fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:32 +0000

    Add package grep-result/0.0.6/grep-result-0.0.6.tar.gz

---
+++ packages/grep-result/grep-result-0.0.6.tar.gz/grep-result-0.0.6/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name = "grep-result",
+    version = "0.0.6",
+    packages=find_packages(),
+    author="llby",
+    author_email="llby@shingor.net",
+    license="MIT",
+    url='https://github.com/llby/grep-result',
+    classifiers=["Topic :: Utilities"]
+)
