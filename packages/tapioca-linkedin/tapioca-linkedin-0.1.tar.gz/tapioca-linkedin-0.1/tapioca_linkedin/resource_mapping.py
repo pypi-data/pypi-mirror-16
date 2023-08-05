673bb4f39d8cedf43e16611b3278f5f76f40dfd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:26 +0000

    Add package tapioca-linkedin/0.1/tapioca-linkedin-0.1.tar.gz

---
+++ packages/tapioca-linkedin/tapioca-linkedin-0.1.tar.gz/tapioca-linkedin-0.1/tapioca_linkedin/resource_mapping.py
@@ -0,0 +1,8 @@
+# coding: utf-8
+
+RESOURCE_MAPPING = {
+    'profile': {
+        'resource': 'people/~:(id,firstName,lastName,headline,summary,positions,num-connections,num-connections-capped,api-standard-profile-request,picture-url,location,industry,public-profile-url)?format=json',
+        'docs': 'https://developer.linkedin.com/docs/fields/basic-profile'
+    },
+}
