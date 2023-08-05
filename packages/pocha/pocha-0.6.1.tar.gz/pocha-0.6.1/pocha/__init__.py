608e59892eedb5675bbdf721610ce39f1a331db8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:49 +0000

    Add package pocha/0.6.1/pocha-0.6.1.tar.gz

---
+++ packages/pocha/pocha-0.6.1.tar.gz/pocha-0.6.1/pocha/__init__.py
@@ -0,0 +1,11 @@
+"""
+main init module
+"""
+
+from pocha import cli
+from pocha import discover
+from pocha import reporters
+from pocha import runner
+from pocha import util
+
+from pocha.common import describe, it
