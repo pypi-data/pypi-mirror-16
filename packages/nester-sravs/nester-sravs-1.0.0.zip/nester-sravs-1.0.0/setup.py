5d4fe16d74b4bf8f43a30223810ac0f38ae4d28b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:31 +0000

    Add package nester-sravs/1.0.0/nester-sravs-1.0.0.zip

---
+++ packages/nester-sravs/nester-sravs-1.0.0.zip/nester-sravs-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'nester-sravs',
+    version = '1.0.0',
+    py_modules = ['nester-sravs'],
+    author = 'yogi',
+    author_email = 'yogendramummaneni@gmail.com',
+    url = 'http://www.yogi.com',
+    description = 'A simple printer of nested lists',
+)
