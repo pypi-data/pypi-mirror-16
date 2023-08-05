b49080123217562c103d40f17121cd0b48ddf46b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:18 +0000

    Add package hellodyy/1.0.0/hellodyy-1.0.0.tar.gz

---
+++ packages/hellodyy/hellodyy-1.0.0.tar.gz/hellodyy-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+setup(
+  name = 'hellodyy',
+  version = '1.0.0',
+  py_modules = ['hellodyy'],
+  author = 'dyy',
+  author_email = 'gtfx0209@gamil.com',
+  url = 'http://dyy.im',
+  description = 'hello python by dyy'
+
+)
