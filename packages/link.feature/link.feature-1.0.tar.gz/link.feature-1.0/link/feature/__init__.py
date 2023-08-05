0340c32e44d8e3cc24e696178a63876c03eac7c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:13 +0000

    Add package link.feature/1.0/link.feature-1.0.tar.gz

---
+++ packages/link.feature/link.feature-1.0.tar.gz/link.feature-1.0/link/feature/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from link.feature.core import Feature
+from link.feature.core import addfeatures, getfeatures
+from link.feature.core import hasfeature, getfeature
+
+
+__all__ = ['Feature', 'addfeatures', 'getfeatures', 'hasfeature', 'getfeature']
+
+__version__ = '1.0'
