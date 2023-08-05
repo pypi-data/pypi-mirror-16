763bf371ca6d1ed96ddbf3e4ac1544a34507655b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:03 +0000

    Add package pstack/0.2/pstack-0.2.tar.gz

---
+++ packages/pstack/pstack-0.2.tar.gz/pstack-0.2/setup.py
@@ -0,0 +1,22 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+entry_points = ["pstack = stack:stack"]
+
+
+setup(
+    name="pstack",
+    version='0.2',
+    description="Tool to print python thread and greenlet stacks",
+    author="Haochuan Guo",
+    author_email="guohaochuan@gmail.com",
+    packages=find_packages(),
+    url="https://github.com/wooparadog/pstack/",
+    entry_points={"console_scripts": entry_points},
+    install_requires=[
+        'click==5.1',
+    ],
+)
