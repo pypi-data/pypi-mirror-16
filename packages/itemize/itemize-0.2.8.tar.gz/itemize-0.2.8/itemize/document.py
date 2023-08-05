16fb427270aa7c439b9c79c26dfe7fad24408711
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:15 +0000

    Add package itemize/0.2.8/itemize-0.2.8.tar.gz

---
+++ packages/itemize/itemize-0.2.8.tar.gz/itemize-0.2.8/itemize/document.py
@@ -0,0 +1,116 @@
+import logging
+import os
+import tempfile
+import cups
+from jinja2 import Environment, PackageLoader, FileSystemLoader
+from z3c.rml import rml2pdf
+
+logger = logging.getLogger(__name__)
+
+class Document(object):
+    def __init__(self, template_name=None, data={}):
+        self._template_name = template_name
+        self._data = data
+        self._template = None
+        self._filename = None
+        self._pdf = None
+
+        self.load_template(template_name)
+        self.render()
+
+    def load_template(self, template_name):
+        # Load the RML template into the preprocessor.
+        logger.info('Loading RML/Jinja2 Template.')
+        environment = None
+        try:
+            logger.debug('Loading templates package with PackageLoader.')
+            environment = Environment(loader=PackageLoader('itemize', 'templates'))
+        except ImportError as e:
+            logger.debug('Loading templates package with FileSystemLoader.')
+            environment = Environment(loader=FileSystemLoader('templates'))
+
+        # Set up debug filter.
+        def debug(text):
+            logger.debug(text)
+            return ''
+        environment.filters['debug'] = debug
+
+        template = environment.get_template(
+            '%s/template.rml.jinja2' % template_name)
+        logger.info('Loaded RML/Jinja2 Template.')
+        self._template = template
+
+        environment.globals.update({
+            'TEMPLATE_DIR': os.path.relpath(
+                os.path.dirname(self.template.filename), os.getcwd())
+        })
+
+        return template
+
+    def render(self):
+        # Do preprocessing.
+        logger.info('Rendering template.')
+        rml_text = self._template.render(self._data)
+        rml_text = rml_text.strip()
+        logger.info('Rendered template.')
+
+        # Generate PDF output.
+        logger.info('Generating PDF document.')
+        pdf = rml2pdf.parseString(rml_text)
+        logger.info('Generated PDF document.')
+
+        self._pdf = pdf
+        return self._pdf
+
+    def save(self, path):
+        filename = os.path.basename(path)
+        logger.info('Saving PDF document.')
+
+        if not self._pdf:
+            self.render()
+
+        with open(path, 'wb') as pdf_file:
+            pdf_file.write(self._pdf.read())
+        logger.info('Saved PDF document.')
+
+        self._filename = filename
+        return self._filename
+
+    def print(self):
+
+        printers = None
+        try:
+            connection = cups.Connection()
+        except RuntimeError as e:
+            logger.error(
+                'Unable to connect to printer: CUPS server is not running. '
+                '(Hint: run `sudo /etc/init.d/cups start`).')
+        else:
+            printers = connection.getPrinters()
+
+        printed = False
+        if printers:
+            printer = [printer for printer in printers][0]
+
+            if 'paused' in printers[printer]['printer-state-reasons']:
+                print('Enabling printer...')
+                connection.enablePrinter(printer)
+
+            if not self.filename:
+                temporary_file = tempfile.NamedTemporaryFile()
+                self.save(temporary_file.name)
+
+            logger.info('Printing document.')
+            connection.printFile(printer, self.filename, 'Label', {})
+            printed = True
+            logger.info('Printed document.')
+
+        return printed
+
+    @property
+    def filename(self):
+        return self._filename
+
+    @property
+    def template(self):
+        return self._template
---
+++ packages/itemize/itemize-0.2.8.tar.gz/itemize-0.2.8/itemize/main.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import json
+import logging
+import os
+import signal
+import stat
+import sys
+from argparse import ArgumentParser
+from .document import Document
+
+logger = logging.getLogger(__name__)
+
+def parse_arguments():
+    # Parse arguments.
+    parser = ArgumentParser(description='Create data-driven receipts.')
+    parser.add_argument('filename', nargs='?', default=None,
+        help='Path to a JSON-formatted file that specifies document form data.')
+    parser.add_argument(
+        '-t', '--template', dest='template_name', action='store',
+        default='receipt', help='set template name (default: receipt)')
+    parser.add_argument(
+        '-o', '--output', dest='output_filename', action='store',
+        default='output.pdf', help='set template name (default: output.pdf)')
+    parser.add_argument(
+        '-d', '--log', dest='loglevel', action='store', default='ERROR',
+        help=(
+            'set log level [DEBUG, INFO, WARNING, ERROR, CRITICAL] '
+            '(default: ERROR)'))
+    parser.add_argument(
+        '--debug', dest='debug', action='store_true', default=False,
+        help='run in DEBUG mode')
+    args = parser.parse_args()
+
+    if args.debug:
+        args.loglevel = 'DEBUG'
+
+    # Configure logger.
+    numeric_level = getattr(logging, args.loglevel.upper(), None)
+    if not isinstance(numeric_level, int):
+        raise ValueError('Invalid log level: %s' % args.loglevel.upper())
+    logging.basicConfig(level=numeric_level)
+
+    return args
+
+
+def sigint_handler(signal, frame):
+    sys.exit()
+
+
+def main():
+    # Register SIGINT (Ctrl-C) handler.
+    signal.signal(signal.SIGINT, sigint_handler)
+
+    args = parse_arguments()
+    logger.debug('Parsed arguments: %s' % args)
+
+    # Support relative file loading using ~ shortcut.
+    filename, relative_path = 'stdin', None
+    if args.filename:
+        absolute_path = os.path.abspath(os.path.expanduser(args.filename))
+        relative_path = os.path.join(
+            '.', os.path.relpath(absolute_path, os.getcwd()))
+        filename = os.path.basename(relative_path)
+
+    # Load JSON input.
+    json_input = {}
+    try:
+        logger.debug('Opening input file (%s).' % relative_path)
+        json_file = open(relative_path)
+    except IOError as e:
+        sys.exit('Could not open input file (%s).\nExiting...' % relative_path)
+    except TypeError as e:
+        # If stdin is not piped or redirected, display a prompt.
+        logger.debug('Reading from stdin.')
+        mode = os.fstat(sys.stdin.fileno()).st_mode
+        if not (stat.S_ISFIFO(mode) or stat.S_ISREG(mode)):
+            print('Reading from stdin. Use Ctrl-D to mark end-of file:')
+        json_input = sys.stdin.read()
+    else:
+        logger.debug('Reading input file (%s).' % relative_path)
+        json_input = json_file.read()
+        logger.debug('Closing input file (%s).' % relative_path)
+        json_file.close()
+
+    # Parse loaded JSON.
+    try:
+        json_data = json.loads(json_input)
+    except ValueError as e:
+        sys.exit(
+            'Could not parse %s! Check that the input is in a valid JSON '
+            'format: %s\nExiting...' % (filename, str(e)))
+
+    # Render and print a PDF document.
+    document = Document(template_name=args.template_name, data=json_data)
+    document.save(args.output_filename)
+    document.print()
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
