a37f3e7d34f58dd9d8a48a9741f59d8e9bc4c9eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:19 +0000

    Add package gemnett.nester/1.1.0/gemnett.nester-1.1.0.zip

---
+++ packages/gemnett.nester/gemnett.nester-1.1.0.zip/gemnett.nester-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name            =   'gemnett.nester',
+    version         =   '1.1.0',
+    py_modules      =   ['nester'],
+    author          =   'gemnett',
+    author_email    =   'gemnett@yahoo.com',
+    url             =   'https://www.facebook.com/tinh.gin',
+    description     =   'A simple printer of nested lists',
+      )
