4e702fb11ce4ab55b5f4333847aba8a525345d1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:23 +0000

    Add package blockstack-client/0.0.13.6/blockstack-client-0.0.13.6.tar.gz

---
+++ packages/blockstack-client/blockstack-client-0.0.13.6.tar.gz/blockstack-client-0.0.13.6/setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+
+from setuptools import setup, find_packages
+
+# to set __version__
+exec(open('blockstack_client/version.py').read())
+
+setup(
+    name='blockstack-client',
+    version=__version__,
+    url='https://github.com/blockstack/blockstack-client',
+    license='GPLv3',
+    author='Blockstack.org',
+    author_email='support@blockstack.org',
+    description='Python client library for Blockstack',
+    keywords='blockchain bitcoin btc cryptocurrency name key value store data',
+    packages=find_packages(),
+    scripts=['bin/blockstack'],
+    download_url='https://github.com/blockstack/blockstack-client/archive/master.zip',
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=[
+        'virtualchain>=0.0.13.0',
+        'protocoin>=0.1',
+        'blockstack-profiles>=0.4.4',
+        'pybitcoin>=0.9.9',
+        'blockstack-zones>=0.1.6',
+        'blockstack-storage-drivers>=0.0.13.6',
+        'blockstack-utxo>=0.0.13.0',
+        'defusedxml>=0.4.1',
+        'keylib>=0.0.5',
+        'mixpanel>=4.3.1',
+        'pycrypto>=2.6.1'
+    ],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet',
+        'Topic :: Security :: Cryptography',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
