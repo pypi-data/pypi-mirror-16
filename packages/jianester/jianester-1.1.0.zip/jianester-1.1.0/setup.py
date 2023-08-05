f109a000b9c1da1aca604ddd3edf5b169ac22d56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:16 +0000

    Add package jianester/1.1.0/jianester-1.1.0.zip

---
+++ packages/jianester/jianester-1.1.0.zip/jianester-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+setup(
+        name = 'jianester',
+        version = '1.1.0',
+        PY_module = ['jianester'],
+        author = 'hfpython',
+        author_email = 'hfpython@headfirstlabs.com',
+        url = 'http://www.headfirstlabs.com',
+        discription = 'A simple printer of nested lists',
+     )
+
