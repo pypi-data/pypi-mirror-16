8440a5cb7e72945f6e1d7272c53c779180be4c0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:48 +0000

    Add package pyetsy/0.4.3/pyetsy-0.4.3.tar.gz

---
+++ packages/pyetsy/pyetsy-0.4.3.tar.gz/pyetsy-0.4.3/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+import os
+from setuptools import setup
+
+this_dir = os.path.realpath(os.path.dirname(__file__))
+long_description = open(os.path.join(this_dir, 'README.md'), 'r').read()
+
+requirements = [
+    'httplib2',
+    'oauth2==1.9.0.post1',
+    'simplejson',
+]
+
+test_requirements = [
+    'pytest',
+]
+
+setup(
+    name='pyetsy',
+    version='0.4.3',
+    author='Dan McKinley & Fulfil.IO Inc.',
+    author_email='dan@etsy.com,support@fulfil.io',
+    description='Python access to the Etsy API',
+    license='GPL v3',
+    keywords='etsy api handmade',
+    packages=['etsy'],
+    long_description=long_description,
+    test_suite='test',
+    install_requires=['simplejson >= 2.0'],
+    extras_require={
+        'OAuth': ["oauth2>=1.2.0"],
+    },
+    package_data={'etsy': ['README.md']},
+)
