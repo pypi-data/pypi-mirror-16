8f0f640c7093cd70e3c905a8b8ea80144d57c31b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:09 +0000

    Add package touchdown/0.14.1/touchdown-0.14.1-py2.py3-none-any.whl

---
+++ packages/touchdown/touchdown-0.14.1-py2.py3-none-any.whl/touchdown/goals/__init__.py
@@ -0,0 +1,46 @@
+# Copyright 2015 Isotoma Limited
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from .apply import Apply
+from .cost import Cost
+from .destroy import Destroy
+from .dot import Dot
+from .edit import Edit
+from .get import Get
+from .get_signin_url import GetSigninUrl
+from .refresh import Refresh
+from .rollback import Rollback
+from .scp import Scp
+from .set import Set
+from .snapshot import Snapshot
+from .ssh import Ssh
+from .tail import Tail
+
+
+__all__ = [
+    "Apply",
+    "Cost",
+    "Destroy",
+    "Dot",
+    "Edit",
+    "Get",
+    "GetSigninUrl",
+    "Refresh",
+    "Rollback",
+    "Set",
+    "Scp",
+    "Snapshot",
+    "Ssh",
+    "Tail",
+]
