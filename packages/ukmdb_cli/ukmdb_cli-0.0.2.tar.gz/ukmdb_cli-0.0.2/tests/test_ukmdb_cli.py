ed0499c9c8d9c108958bfbbeb869c210caded230
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:07 +0000

    Add package ukmdb_cli/0.0.2/ukmdb_cli-0.0.2.tar.gz

---
+++ packages/ukmdb_cli/ukmdb_cli-0.0.2.tar.gz/ukmdb_cli-0.0.2/tests/test_ukmdb_cli.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_ukmdb_cli
+----------------------------------
+
+Tests for `ukmdb_cli` module.
+"""
+
+# import pytest
+
+
+# from ukmdb_cli import ukmdb_cli
+
+
+class TestUkmdb_cli(object):
+
+    @classmethod
+    def setup_class(cls):
+        pass
+
+    def test_something(self):
+        pass
+
+    @classmethod
+    def teardown_class(cls):
+        pass
