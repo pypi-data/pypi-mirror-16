5166d32e532a15037f85b3efe512ec09125fa88d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:21 +0000

    Add package weberWinSV/0.0.1/weberWinSV-0.0.1.zip

---
+++ packages/weberWinSV/weberWinSV-0.0.1.zip/weberWinSV-0.0.1/weberWinSV/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/local/bin/python
+# -*- coding:utf-8 -*-
+"""    
+    2015/10/14  WeiYanfeng
+    WeberWinSV 包
+"""
+from WeberWinSV import CSerialJson
+
+if __name__ == '__main__':
+    pass
