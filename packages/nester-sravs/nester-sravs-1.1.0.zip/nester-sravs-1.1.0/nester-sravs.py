1f141c3f72b6743c556a964699b385d323d85262
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:08 +0000

    Add package nester-sravs/1.1.0/nester-sravs-1.1.0.zip

---
+++ packages/nester-sravs/nester-sravs-1.1.0.zip/nester-sravs-1.1.0/nester-sravs.py
@@ -0,0 +1,25 @@
+"""
+This is the "nester.py" module, and it provides one function called
+print_lol() which prints lists that may or may not include nested lists.
+"""
+
+def print_lol(the_list, level):
+
+    """This function takes a positional argument called "the_list", which is any
+    Python list (of, possibly, nested lists). Each data item in the provided list
+    is (recursively) printed on the screen on its own line.
+    A second argument called "level" is used to insert tab-stops when a nested list is encountered.
+    """
+
+    for each_item in the_list:
+        if isinstance(each_item, list):
+            print_lol(each_item,level+1)
+        else:
+            for tab_stop in range(level):
+                print '\t',
+            print(each_item)
+
+movies = ["The Holy Grail", 1975, "Terry Jones & Terry Gailliam", 91,
+          ["Graham Chapman", ["Michael Palin", "John Cleese", "Terry Gilliam", "Eric Idle", "Terry Jones"]]]
+
+print_lol(movies,0)
