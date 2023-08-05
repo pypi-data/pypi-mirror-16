cd1709414cf509cd4ef9c43f5c969a4f8d425865
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:54 +0000

    Add package simab/0.0.34/simab-0.0.34.tar.gz

---
+++ packages/simab/simab-0.0.34.tar.gz/simab-0.0.34/simab/algorithms/softmax.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+import math
+from .algorithm import *
+
+class Softmax(Algorithm):
+    """ Softmax(Boltzmann Exploration) Algorithm
+
+    :param tau: decides randomness of the exploration. larger tau means more randomness.
+    """
+    def __init__(self, arms, tau, label=None):
+        Algorithm.__init__(self, arms, label=label)
+        self.tau = tau
+        if self.label is None:
+            self.label = 'Softmax(t=%s)' % self.tau
+
+    def _select_arm(self):
+        unknown_arm = get_unknown_arm(self.history)
+        if unknown_arm is None:
+            means = get_means(self.history)
+            divider = sum([math.exp(mean/self.tau) for mean in means])
+            probabilities = [math.exp(mean/self.tau)/divider for mean in means]
+            selected_arm = pick_by_probability(probabilities)
+        else:
+            selected_arm = unknown_arm
+        return selected_arm
+
+    def summary(self):
+        summary = Algorithm.summary(self)
+        summary['tau'] = self.tau
+        return summary
+
