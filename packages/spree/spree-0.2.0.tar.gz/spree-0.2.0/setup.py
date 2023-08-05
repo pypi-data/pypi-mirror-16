64d16ec124fc3d5121e09dffa081bc2c4cf53b62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:32 +0000

    Add package spree/0.2.0/spree-0.2.0.tar.gz

---
+++ packages/spree/spree-0.2.0.tar.gz/spree-0.2.0/setup.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = [
+]
+
+test_requirements = [
+    'pytest',
+]
+
+setup(
+    name='spree',
+    version='0.2.0',
+    description="Spree python api client",
+    long_description=readme + '\n\n' + history,
+    author="Fulfil.IO Inc.",
+    author_email='info@fulfil.io',
+    url='https://github.com/fulfilio/python-spree',
+    packages=[
+        'spree',
+    ],
+    package_dir={'spree':
+                 'spree'},
+    include_package_data=True,
+    install_requires=requirements,
+    license="BSD license",
+    zip_safe=False,
+    keywords='spree',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    test_suite='tests',
+    tests_require=test_requirements
+)
