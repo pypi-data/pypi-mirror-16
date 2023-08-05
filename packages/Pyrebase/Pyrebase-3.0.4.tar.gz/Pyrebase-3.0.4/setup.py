899f768d8bfcae12fcc1499ac6f14e4213a18e8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:55 +0000

    Add package Pyrebase/3.0.4/Pyrebase-3.0.4.tar.gz

---
+++ packages/Pyrebase/Pyrebase-3.0.4.tar.gz/Pyrebase-3.0.4/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Pyrebase',
+    version='3.0.4',
+    url='https://github.com/thisbejim/Pyrebase',
+    description='A simple python wrapper for the Firebase API',
+    author='James Childs-Maidment',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.4',
+    ],
+    keywords='Firebase',
+    packages=find_packages(exclude=['tests']),
+    install_requires=['requests', 'sseclient', 'gcloud', 'oauth2client']
+)
