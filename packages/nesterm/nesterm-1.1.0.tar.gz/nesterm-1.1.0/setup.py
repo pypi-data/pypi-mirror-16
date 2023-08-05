de277c2be0468ee27049c6485a7c72520b326776
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:41 +0000

    Add package nesterm/1.1.0/nesterm-1.1.0.tar.gz

---
+++ packages/nesterm/nesterm-1.1.0.tar.gz/nesterm-1.1.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(
+  name = 'nesterm',
+  version = '1.1.0',
+  py_modules = ['nester'],
+  author = 'mhusaini',
+  author_email = 'mhusassini@headfirstlabs.com',
+  url = 'http://www.headfirstlabs.com',
+  description = 'A simple printer of nested lists')
