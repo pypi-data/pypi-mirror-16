bc4b6346cc32a6896571a3b7a2e712390bf65fd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:59 +0000

    Add package decider_bwa_pe/0.7/decider_bwa_pe-0.7.tar.gz

---
+++ packages/decider_bwa_pe/decider_bwa_pe-0.7.tar.gz/decider_bwa_pe-0.7/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name = 'decider_bwa_pe',
+      author = 'Jeremiah H. Savage',
+      author_email = 'jeremiahsavage@gmail.com',
+      version = 0.7,
+      description = 'match readgroup and fastq outputs',
+      url = 'https://github.com/jeremiahsavage/decider_bwa_pe',
+      license = 'Apache 2.0',
+      packages = find_packages(),
+      install_requires = [
+          'cython',
+          'pandas',
+          'biopython',
+          'sqlalchemy',
+          'cdis_pipe_utils'
+      ],
+      classifiers = [
+          'Development Status :: 3 - Alpha',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: Apache Software License',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 3',
+      ],
+)
