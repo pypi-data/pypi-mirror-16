730259fb4a4fd5d89f5399b6968e66399949ce25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:34 +0000

    Add package Edward/1.0.9/edward-1.0.9-py2-none-any.whl

---
+++ packages/Edward/edward-1.0.9-py2-none-any.whl/edward/criticisms.py
@@ -0,0 +1,429 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import numpy as np
+import tensorflow as tf
+
+from edward.util import logit, get_dims, get_session
+
+
+def evaluate(metrics, model, variational, data, y_true=None):
+    """Evaluate fitted model using a set of metrics.
+
+    Parameters
+    ----------
+    metrics : list or str
+        List of metrics or a single metric.
+    model : ed.Model
+        Probability model p(x, z)
+    variational : ed.Variational
+        Variational approximation to the posterior p(z | x)
+    data : dict
+        Data dictionary to evaluate model with. For TensorFlow,
+        Python, and Stan models, the key type is a string; for PyMC3,
+        the key type is a Theano shared variable. For TensorFlow,
+        Python, and PyMC3 models, the value type is a NumPy array or
+        TensorFlow placeholder; for Stan, the value type is the type
+        according to the Stan program's data block.
+    y_true : np.ndarray or tf.Tensor
+        True values to compare to in supervised learning tasks.
+
+    Returns
+    -------
+    list or float
+        A list of evaluations or a single evaluation.
+
+    Raises
+    ------
+    NotImplementedError
+        If an input metric does not match an implemented metric in Edward.
+    """
+    sess = get_session()
+    # Monte Carlo estimate the mean of the posterior predictive:
+    # 1. Sample a batch of latent variables from posterior
+    n_minibatch = 100
+    zs = variational.sample(size=n_minibatch)
+    # 2. Make predictions, averaging over each sample of latent variables
+    y_pred = model.predict(data, zs)
+
+    # Evaluate y_pred according to y_true for all metrics.
+    evaluations = []
+    if isinstance(metrics, str):
+        metrics = [metrics]
+
+    for metric in metrics:
+        if metric == 'accuracy' or metric == 'crossentropy':
+            # automate binary or sparse cat depending on max(y_true)
+            support = tf.reduce_max(y_true).eval()
+            if support <= 1:
+                metric = 'binary_' + metric
+            else:
+                metric = 'sparse_categorical_' + metric
+
+        if metric == 'binary_accuracy':
+            evaluations += [sess.run(binary_accuracy(y_true, y_pred))]
+        elif metric == 'categorical_accuracy':
+            evaluations += [sess.run(categorical_accuracy(y_true, y_pred))]
+        elif metric == 'sparse_categorical_accuracy':
+            evaluations += [sess.run(sparse_categorical_accuracy(y_true, y_pred))]
+        elif metric == 'log_loss' or metric == 'binary_crossentropy':
+            evaluations += [sess.run(binary_crossentropy(y_true, y_pred))]
+        elif metric == 'categorical_crossentropy':
+            evaluations += [sess.run(categorical_crossentropy(y_true, y_pred))]
+        elif metric == 'sparse_categorical_crossentropy':
+            evaluations += [sess.run(sparse_categorical_crossentropy(y_true, y_pred))]
+        elif metric == 'hinge':
+            evaluations += [sess.run(hinge(y_true, y_pred))]
+        elif metric == 'squared_hinge':
+            evaluations += [sess.run(squared_hinge(y_true, y_pred))]
+        elif metric == 'mse' or metric == 'MSE' or \
+             metric == 'mean_squared_error':
+            evaluations += [sess.run(mean_squared_error(y_true, y_pred))]
+        elif metric == 'mae' or metric == 'MAE' or \
+             metric == 'mean_absolute_error':
+            evaluations += [sess.run(mean_absolute_error(y_true, y_pred))]
+        elif metric == 'mape' or metric == 'MAPE' or \
+             metric == 'mean_absolute_percentage_error':
+            evaluations += [sess.run(mean_absolute_percentage_error(y_true, y_pred))]
+        elif metric == 'msle' or metric == 'MSLE' or \
+             metric == 'mean_squared_logarithmic_error':
+            evaluations += [sess.run(mean_squared_logarithmic_error(y_true, y_pred))]
+        elif metric == 'poisson':
+            evaluations += [sess.run(poisson(y_true, y_pred))]
+        elif metric == 'cosine' or metric == 'cosine_proximity':
+            evaluations += [sess.run(cosine_proximity(y_true, y_pred))]
+        elif metric == 'log_lik' or metric == 'log_likelihood':
+            evaluations += [sess.run(y_pred)]
+        else:
+            raise NotImplementedError()
+
+    if len(evaluations) == 1:
+        return evaluations[0]
+    else:
+        return evaluations
+
+
+def ppc(model, variational=None, data=None, T=None, size=100):
+    """Posterior predictive check.
+    (Rubin, 1984; Meng, 1994; Gelman, Meng, and Stern, 1996)
+    If no posterior approximation is provided through ``variational``,
+    then we default to a prior predictive check (Box, 1980).
+
+    PPC's form an empirical distribution for the predictive discrepancy,
+
+    .. math::
+        p(T) = \int p(T(yrep) | z) p(z | y) dz
+
+    by drawing replicated data sets yrep and calculating
+    :math:`T(yrep)` for each data set. Then it compares it to
+    :math:`T(y)`.
+
+    Parameters
+    ----------
+    model : ed.Model
+        class object that implements the ``sample_likelihood`` method
+    variational : ed.Variational, optional
+        latent variable distribution q(z) to sample from. It is an
+        approximation to the posterior, e.g., a variational
+        approximation or an empirical distribution from MCMC samples.
+        If not specified, samples will be obtained from the model
+        through the ``sample_prior`` method.
+    data : dict, optional
+        Observed data to compare to. If not specified, will return
+        only the reference distribution with an assumed replicated
+        data set size of 1. For TensorFlow, Python, and Stan models,
+        the key type is a string; for PyMC3, the key type is a Theano
+        shared variable. For TensorFlow, Python, and PyMC3 models, the
+        value type is a NumPy array or TensorFlow placeholder; for
+        Stan, the value type is the type according to the Stan
+        program's data block.
+    T : function, optional
+        Discrepancy function, which takes a data dictionary and list
+        of latent variables as input and outputs a tf.Tensor. Default
+        is the identity function.
+    size : int, optional
+        number of replicated data sets
+
+    Returns
+    -------
+    list
+        List containing the reference distribution, which is a Numpy
+        vector of size elements,
+
+        .. math::
+            (T(yrep^{1}, z^{1}), ..., T(yrep^{size}, z^{size}))
+
+        and the realized discrepancy, which is a NumPy vector of size
+        elements,
+
+        .. math::
+            (T(y, z^{1}), ..., T(y, z^{size})).
+
+        If the discrepancy function is not specified, then the list
+        contains the full data distribution where each element is a
+        data set (dictionary).
+    """
+    sess = get_session()
+    if data is None:
+        N = 1
+        y = {}
+    else:
+        # Assume all values have the same data set size.
+        N = get_dims(data.values()[0])[0]
+        y = data
+
+    # 1. Sample from posterior (or prior).
+    # We must fetch zs out of the session because sample_likelihood()
+    # may require a SciPy-based sampler.
+    if variational is not None:
+        zs = variational.sample(size=size)
+        # This is to avoid fetching, e.g., a placeholder x with the
+        # dictionary {x: np.array()}. TensorFlow will raise an error.
+        if isinstance(zs, list):
+            zs = [tf.identity(zs_elem) for zs_elem in zs]
+        else:
+            zs = tf.identity(zs)
+
+        zs = sess.run(zs)
+    else:
+        zs = model.sample_prior(size=size)
+        zs = zs.eval()
+
+    # 2. Sample from likelihood.
+    yreps = model.sample_likelihood(zs, size=N)
+
+    # 3. Calculate discrepancy.
+    if T is None:
+        if y is None:
+            return yreps
+        else:
+            return [yreps, y]
+
+    Tyreps = []
+    Tys = []
+    for yrep, z in zip(yreps, tf.unpack(zs)):
+        Tyreps += [T(yrep, z)]
+        if y is not None:
+            Tys += [T(y, z)]
+
+    if y is None:
+        return sess.run(tf.pack(Tyreps))
+    else:
+        return sess.run([tf.pack(Tyreps), tf.pack(Tys)])
+
+
+# Classification metrics
+
+
+def binary_accuracy(y_true, y_pred):
+    """Binary prediction accuracy, also known as 0/1-loss.
+
+    Parameters
+    ----------
+    y_true : tf.Tensor
+        Tensor of 0s and 1s.
+    y_pred : tf.Tensor
+        Tensor of probabilities.
+    """
+    y_true = tf.cast(y_true, tf.float32)
+    y_pred = tf.cast(tf.round(y_pred), tf.float32)
+    return tf.reduce_mean(tf.cast(tf.equal(y_true, y_pred), tf.float32))
+
+
+def categorical_accuracy(y_true, y_pred):
+    """Multi-class prediction accuracy. One-hot representation for ``y_true``.
+
+    Parameters
+    ----------
+    y_true : tf.Tensor
+        Tensor of 0s and 1s, where the outermost dimension of size ``K``
+        has only one 1 per row.
+    y_pred : tf.Tensor
+        Tensor of probabilities, with same shape as ``y_true``.
+        The outermost dimension denote the categorical probabilities for
+        that data point per row.
+    """
+    y_true = tf.cast(tf.argmax(y_true, len(y_true.get_shape()) - 1), tf.float32)
+    y_pred = tf.cast(tf.argmax(y_pred, len(y_pred.get_shape()) - 1), tf.float32)
+    return tf.reduce_mean(tf.cast(tf.equal(y_true, y_pred), tf.float32))
+
+
+def sparse_categorical_accuracy(y_true, y_pred):
+    """Multi-class prediction accuracy. Label {0, 1, .., K-1}
+    representation for ``y_true``.
+
+    Parameters
+    ----------
+    y_true : tf.Tensor
+        Tensor of integers {0, 1, ..., K-1}.
+    y_pred : tf.Tensor
+        Tensor of probabilities, with shape ``(y_true.get_shape(), K)``.
+        The outermost dimension are the categorical probabilities for
+        that data point.
+    """
+    y_true = tf.cast(y_true, tf.float32)
+    y_pred = tf.cast(tf.argmax(y_pred, len(y_pred.get_shape()) - 1), tf.float32)
+    return tf.reduce_mean(tf.cast(tf.equal(y_true, y_pred), tf.float32))
+
+
+def binary_crossentropy(y_true, y_pred):
+    """Binary cross-entropy.
+
+    Parameters
+    ----------
+    y_true : tf.Tensor
+        Tensor of 0s and 1s.
+    y_pred : tf.Tensor
+        Tensor of probabilities.
+    """
+    y_true = tf.cast(y_true, tf.float32)
+    y_pred = logit(tf.cast(y_pred, tf.float32))
+    return tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(y_pred, y_true))
+
+
+def categorical_crossentropy(y_true, y_pred):
+    """Multi-class cross entropy. One-hot representation for ``y_true``.
+
+    Parameters
+    ----------
+    y_true : tf.Tensor
+        Tensor of 0s and 1s, where the outermost dimension of size K
+        has only one 1 per row.
+    y_pred : tf.Tensor
+        Tensor of probabilities, with same shape as y_true.
+        The outermost dimension denote the categorical probabilities for
+        that data point per row.
+    """
+    y_true = tf.cast(y_true, tf.float32)
+    y_pred = logit(tf.cast(y_pred, tf.float32))
+    return tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(y_pred, y_true))
+
+
+def sparse_categorical_crossentropy(y_true, y_pred):
+    """Multi-class cross entropy. Label {0, 1, .., K-1} representation
+    for ``y_true.``
+
+    Parameters
+    ----------
+    y_true : tf.Tensor
+        Tensor of integers {0, 1, ..., K-1}.
+    y_pred : tf.Tensor
+        Tensor of probabilities, with shape ``(y_true.get_shape(), K)``.
+        The outermost dimension are the categorical probabilities for
+        that data point.
+    """
+    y_true = tf.cast(y_true, tf.int64)
+    y_pred = logit(tf.cast(y_pred, tf.float32))
+    return tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(y_pred, y_true))
+
+
+def hinge(y_true, y_pred):
+    """Hinge loss.
+
+    Parameters
+    ----------
+    y_true : tf.Tensor
+        Tensor of 0s and 1s.
+    y_pred : tf.Tensor
+        Tensor of real value.
+    """
+    y_true = tf.cast(y_true, tf.float32)
+    y_pred = tf.cast(y_pred, tf.float32)
+    return tf.reduce_mean(tf.maximum(1.0 - y_true * y_pred, 0.0))
+
+
+def squared_hinge(y_true, y_pred):
+    """Squared hinge loss.
+
+    Parameters
+    ----------
+    y_true : tf.Tensor
+        Tensor of 0s and 1s.
+    y_pred : tf.Tensor
+        Tensor of real value.
+    """
+    y_true = tf.cast(y_true, tf.float32)
+    y_pred = tf.cast(y_pred, tf.float32)
+    return tf.reduce_mean(tf.square(tf.maximum(1.0 - y_true * y_pred, 0.0)))
+
+
+# Regression metrics
+
+
+def mean_squared_error(y_true, y_pred):
+    """Mean squared error loss.
+
+    Parameters
+    ----------
+    y_true : tf.Tensor
+    y_pred : tf.Tensor
+        Tensors of same shape and type.
+    """
+    return tf.reduce_mean(tf.square(y_pred - y_true))
+
+
+def mean_absolute_error(y_true, y_pred):
+    """Mean absolute error loss.
+
+    Parameters
+    ----------
+    y_true : tf.Tensor
+    y_pred : tf.Tensor
+        Tensors of same shape and type.
+    """
+    return tf.reduce_mean(tf.abs(y_pred - y_true))
+
+
+def mean_absolute_percentage_error(y_true, y_pred):
+    """Mean absolute percentage error loss.
+
+    Parameters
+    ----------
+    y_true : tf.Tensor
+    y_pred : tf.Tensor
+        Tensors of same shape and type.
+    """
+    diff = tf.abs((y_true - y_pred) / tf.clip_by_value(tf.abs(y_true), 1e-8, np.inf))
+    return 100.0 * tf.reduce_mean(diff)
+
+
+def mean_squared_logarithmic_error(y_true, y_pred):
+    """Mean squared logarithmic error loss.
+
+    Parameters
+    ----------
+    y_true : tf.Tensor
+    y_pred : tf.Tensor
+        Tensors of same shape and type.
+    """
+    first_log = tf.log(tf.clip_by_value(y_pred, 1e-8, np.inf) + 1.0)
+    second_log = tf.log(tf.clip_by_value(y_true, 1e-8, np.inf) + 1.0)
+    return tf.reduce_mean(tf.square(first_log - second_log))
+
+
+def poisson(y_true, y_pred):
+    """Negative Poisson log-likelihood of data ``y_true`` given predictions
+    ``y_pred`` (up to proportion).
+
+    Parameters
+    ----------
+    y_true : tf.Tensor
+    y_pred : tf.Tensor
+        Tensors of same shape and type.
+    """
+    return tf.reduce_sum(y_pred - y_true * tf.log(y_pred + 1e-8))
+
+
+def cosine_proximity(y_true, y_pred):
+    """Cosine similarity of two vectors.
+
+    Parameters
+    ----------
+    y_true : tf.Tensor
+    y_pred : tf.Tensor
+        Tensors of same shape and type.
+    """
+    y_true = tf.nn.l2_normalize(y_true, len(y_true.get_shape()) - 1)
+    y_pred = tf.nn.l2_normalize(y_pred, len(y_pred.get_shape()) - 1)
+    return tf.reduce_sum(y_true * y_pred)
---
+++ packages/Edward/edward-1.0.9-py2-none-any.whl/edward/util.py
@@ -0,0 +1,374 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import numpy as np
+import tensorflow as tf
+
+
+def cumprod(xs):
+    """Cumulative product of a tensor along first dimension.
+
+    https://github.com/tensorflow/tensorflow/issues/813
+
+    Parameters
+    ----------
+    x : tf.Tensor
+        vector, matrix, or n-Tensor
+
+    Returns
+    -------
+    tf.Tensor
+        A Tensor with `cumprod` applied along its first dimension.
+    """
+    values = tf.unpack(xs)
+    out = []
+    prev = tf.ones_like(values[0])
+    for val in values:
+        s = prev * val
+        out.append(s)
+        prev = s
+
+    result = tf.pack(out)
+    return result
+
+
+def dot(x, y):
+    """Compute dot product between a Tensor matrix and a Tensor vector.
+
+    If x is a ``[M x N]`` matrix, then y is a ``M``-vector.
+
+    If x is a ``M``-vector, then y is a ``[M x N]`` matrix.
+
+    Parameters
+    ----------
+    x : tf.Tensor
+        ``M x N`` matrix or ``M`` vector (see above)
+    y : tf.Tensor
+        ``M`` vector or ``M x N`` matrix (see above)
+
+    Returns
+    -------
+    tf.Tensor
+        ``N``-vector
+    """
+    if len(x.get_shape()) == 1:
+        vec = x
+        mat = y
+        return tf.matmul(tf.expand_dims(vec, 0), mat)
+    else:
+        mat = x
+        vec = y
+        return tf.matmul(mat, tf.expand_dims(vec, 1))
+
+
+def get_dims(x):
+    """Get values of each dimension.
+
+    Parameters
+    ----------
+    x : tf.Tensor or np.ndarray
+        scalar, vector, matrix, or n-tensor
+
+    Returns
+    -------
+    list
+        Python list containing dimensions of `x`
+    """
+    if isinstance(x, tf.Tensor):
+        dims = x.get_shape()
+        if len(dims) == 0: # scalar
+            return []
+        else: # array
+            return [dim.value for dim in dims]
+
+    elif isinstance(x, np.ndarray):
+        return list(x.shape)
+    else:
+        raise NotImplementedError()
+
+
+def get_session():
+    """Get the globally defined TensorFlow session.
+
+    If the session is not already defined, then the function will create
+    a global session.
+
+    Returns
+    -------
+    _ED_SESSION : tf.InteractiveSession
+    """
+    global _ED_SESSION
+    if tf.get_default_session() is None:
+        _ED_SESSION = tf.InteractiveSession()
+
+    return _ED_SESSION
+
+
+def hessian(y, xs):
+    """Calculate Hessian of y with respect to each x in xs.
+
+    Parameters
+    ----------
+    y : tf.Tensor
+        Tensor to calculate Hessian of.
+    xs : list
+        List of TensorFlow variables to calculate with respect to.
+        The variables can have different shapes.
+
+    Returns
+    -------
+    tf.Tensor
+        A matrix where each row is
+        .. math:: \partial_{xs} ( [ \partial_{xs} y ]_j ).
+    """
+    # Calculate flattened vector grad_{xs} y.
+    grads = tf.gradients(y, xs)
+    grads = [tf.reshape(grad, [-1]) for grad in grads]
+    grads = tf.concat(0, grads)
+    # Loop over each element in the vector.
+    mat = []
+    d = grads.get_shape()[0]
+    for j in range(d):
+        # Calculate grad_{xs} ( [ grad_{xs} y ]_j ).
+        gradjgrads = tf.gradients(grads[j], xs)
+        # Flatten into vector.
+        hi = []
+        for l in range(len(xs)):
+            hij = gradjgrads[l]
+            # return 0 if gradient doesn't exist; TensorFlow returns None
+            if hij is None:
+                hij = tf.zeros(xs[l].get_shape(), dtype=tf.float32)
+
+            hij = tf.reshape(hij, [-1])
+            hi.append(hij)
+
+        hi = tf.concat(0, hi)
+        mat.append(hi)
+
+    # Form matrix where each row is grad_{xs} ( [ grad_{xs} y ]_j ).
+    return tf.pack(mat)
+
+
+def kl_multivariate_normal(loc_one, scale_one, loc_two=0, scale_two=1):
+    """Calculate the KL of multivariate normal distributions with
+    diagonal covariances.
+
+    Parameters
+    ----------
+    loc_one : tf.Tensor
+        n-dimensional vector, or M x n-dimensional matrix where each
+        row represents the mean of a n-dimensional Gaussian
+    scale_one : tf.Tensor
+        n-dimensional vector, or M x n-dimensional matrix where each
+        row represents the standard deviation of a n-dimensional Gaussian
+    loc_two : tf.Tensor, optional
+        n-dimensional vector, or M x n-dimensional matrix where each
+        row represents the mean of a n-dimensional Gaussian
+    scale_two : tf.Tensor, optional
+        n-dimensional vector, or M x n-dimensional matrix where each
+        row represents the standard deviation of a n-dimensional Gaussian
+
+    Returns
+    -------
+    tf.Tensor
+        for scalar or vector inputs, outputs the scalar
+        ``KL( N(z; loc_one, scale_one) || N(z; loc_two, scale_two) )``
+        for matrix inputs, outputs the vector
+        ``[KL( N(z; loc_one[m,:], scale_one[m,:]) || N(z; loc_two[m,:], scale_two[m,:]) )]_{m=1}^M``
+    """
+    if loc_two == 0 and scale_two == 1:
+        return 0.5 * tf.reduce_sum(
+            tf.square(scale_one) + tf.square(loc_one) - \
+            1.0 - 2.0 * tf.log(scale_one))
+    else:
+        return 0.5 * tf.reduce_sum(
+            tf.square(scale_one/scale_two) + \
+            tf.square((loc_two - loc_one)/scale_two) - \
+            1.0 + 2.0 * tf.log(scale_two) - 2.0 * tf.log(scale_one), 1)
+
+
+def log_sum_exp(x):
+    """Compute the ``log_sum_exp`` of the elements in x.
+
+    Parameters
+    ----------
+    x : tf.Tensor
+        vector or matrix with second dimension 1
+        shape=TensorShape([Dimension(N)])
+        shape=TensorShape([Dimension(N), Dimension(1)])
+
+    Returns
+    -------
+    tf.Tensor
+        scalar if vector input, vector if matrix tensor input
+    """
+    x_max = tf.reduce_max(x)
+    return tf.add(x_max, tf.log(tf.reduce_sum(tf.exp(tf.sub(x, x_max)))))
+
+
+def logit(x):
+    """Evaluate :math:`\log(x / (1 - x))` elementwise.
+
+    Clips all elements to be between :math:`(0,1)`.
+
+    Parameters
+    ----------
+    x : tf.Tensor
+        scalar, vector, matrix, or n-Tensor
+
+    Returns
+    -------
+    tf.Tensor
+        size corresponding to size of input
+    """
+    x = tf.clip_by_value(x, 1e-8, 1.0 - 1e-8)
+    return tf.log(x) - tf.log(1.0 - x)
+
+
+def multivariate_rbf(x, y=0.0, sigma=1.0, l=1.0):
+    """Squared-exponential kernel
+
+    .. math:: k(x, y) = \sigma^2 \exp{ -1/(2l^2) \sum_i (x_i - y_i)^2 }
+
+    Parameters
+    ----------
+    x : tf.Tensor
+        scalar, vector, matrix, or n-Tensor
+    y : Optional[tf.Tensor], default 0.0
+        scalar, vector, matrix, or n-Tensor
+    sigma : Optional[double], default 1.0
+        standard deviation of radial basis function
+    l : Optional[double], default 1.0
+        lengthscale of radial basis function
+
+    Returns
+    -------
+    tf.Tensor
+        scalar if vector input, rank-(n-1) if n-Tensor input
+    """
+    return tf.pow(sigma, 2.0) * \
+           tf.exp(-1.0/(2.0*tf.pow(l, 2.0)) * \
+                  tf.reduce_sum(tf.pow(x - y , 2.0)))
+
+
+def rbf(x, y=0.0, sigma=1.0, l=1.0):
+    """Squared-exponential kernel element-wise
+
+    .. math:: k(x, y) = \sigma^2 \exp{ -1/(2l^2) (x_i - y_i)^2 }
+
+    Parameters
+    ----------
+    x : tf.Tensor
+        scalar, vector, matrix, or n-Tensor
+    y : Optional[tf.Tensor], default 0.0
+        scalar, vector, matrix, or n-Tensor
+    sigma : Optional[double], default 1.0
+        standard deviation of radial basis function
+    l : Optional[double], default 1.0
+        lengthscale of radial basis function
+
+    Returns
+    -------
+    tf.Tensor
+        size corresponding to size of input
+    """
+    return tf.pow(sigma, 2.0) * \
+           tf.exp(-1.0/(2.0*tf.pow(l, 2.0)) * tf.pow(x - y , 2.0))
+
+
+def set_seed(x):
+    """Set seed for both NumPy and TensorFlow.
+
+    Parameters
+    ----------
+    x : int, float
+        seed
+    """
+    np.random.seed(x)
+    tf.set_random_seed(x)
+
+
+def softplus(x):
+    """Elementwise Softplus function
+
+    .. math:: \log(1 + \exp(x))
+
+    TensorFlow can't currently autodiff through ``tf.nn.softplus()``.
+
+    Parameters
+    ----------
+    x : tf.Tensor
+        scalar, vector, matrix, or n-Tensor
+
+    Returns
+    -------
+    tf.Tensor
+        size corresponding to size of input
+    """
+    return tf.log(1.0 + tf.exp(x))
+
+
+def stop_gradient(x):
+    """Apply ``tf.stop_gradient()`` element-wise.
+
+    Parameters
+    ----------
+    x : tf.Tensor or list
+        scalar, vector, matrix, or n-Tensor or list thereof
+
+    Returns
+    -------
+    tf.Tensor or list
+        size corresponding to size of input
+    """
+    if isinstance(x, tf.Tensor):
+        return tf.stop_gradient(x)
+    else: # list
+        return [tf.stop_gradient(i) for i in x]
+
+
+def to_simplex(x):
+    """Transform real vector of length ``(K-1)`` to a simplex of dimension ``K``
+    using a backward stick breaking construction.
+
+    Parameters
+    ----------
+    x : tf.tensor or np.array
+        scalar, vector, or matrix
+
+    Returns
+    -------
+    tf.Tensor
+        Same shape as input but with last dimension of size ``K``.
+
+    Notes
+    -----
+    x as a 3d or higher tensor is not guaranteed to be supported.
+    """
+    if isinstance(x, tf.Tensor):
+        shape = get_dims(x)
+    else:
+        shape = x.shape
+
+    if len(shape) == 1:
+        n_rows = ()
+        K_minus_one = shape[0]
+        eq = -tf.log(tf.cast(K_minus_one - tf.range(K_minus_one),
+                             dtype=tf.float32))
+        z = tf.sigmoid(eq + x)
+        pil = tf.concat(0, [z, tf.constant([1.0])])
+        piu = tf.concat(0, [tf.constant([1.0]), 1.0 - z])
+        S = cumprod(piu)
+        return S * pil
+    else:
+        n_rows = shape[0]
+        K_minus_one = shape[1]
+        eq = -tf.log(tf.cast(K_minus_one - tf.range(K_minus_one),
+                             dtype=tf.float32))
+        z = tf.sigmoid(eq + x)
+        pil = tf.concat(1, [z, tf.ones([n_rows, 1])])
+        piu = tf.concat(1, [tf.ones([n_rows, 1]), 1.0 - z])
+        # cumulative product along 1st axis
+        S = tf.pack([cumprod(piu_x) for piu_x in tf.unpack(piu)])
+        return S * pil
