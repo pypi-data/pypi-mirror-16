1431ff8f0883fa496b78841ba9f877a860aef907
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:38 +0000

    Add package Products.OFSP/3.0/Products.OFSP-3.0.zip

---
+++ packages/Products.OFSP/Products.OFSP-3.0.zip/Products.OFSP-3.0/setup.py
@@ -0,0 +1,48 @@
+##############################################################################
+#
+# Copyright (c) 2010 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+
+from setuptools import setup, find_packages
+
+setup(name='Products.OFSP',
+      version='3.0',
+      url='http://pypi.python.org/pypi/Products.OFSP',
+      license='ZPL 2.1',
+      description="General Zope 2 help screens.",
+      author='Zope Foundation and Contributors',
+      author_email='zope-dev@zope.org',
+      long_description=(open('README.rst').read() + '\n' +
+                        open('CHANGES.rst').read()),
+      packages=find_packages('src'),
+      namespace_packages=['Products'],
+      package_dir={'': 'src'},
+      classifiers=[
+          "Development Status :: 6 - Mature",
+          "Environment :: Web Environment",
+          "Framework :: Zope2",
+          "License :: OSI Approved :: Zope Public License",
+          "Operating System :: OS Independent",
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 2 :: Only",
+          "Programming Language :: Python :: 2.7",
+          "Programming Language :: Python :: Implementation :: CPython",
+      ],
+      install_requires=[
+          'setuptools',
+          'AccessControl',
+          'Persistence',
+          'Zope2 >= 2.13.0a1',
+      ],
+      include_package_data=True,
+      zip_safe=False,
+      )
