0bd6099d2d85d18bfbde56ad1385ea99c8eaf933
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:24 +0000

    Add package helputils/0.5.8/helputils-0.5.8.tar.gz

---
+++ packages/helputils/helputils-0.5.8.tar.gz/helputils-0.5.8/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name="helputils",
+    version="0.5.8",
+    author="eayin2",
+    author_email="eayin2 at gmail dot com",
+    packages=find_packages(),
+    url="https://github.com/eayin2/helputils",
+    description="Bunch of random functions and classes that are useful.",
+    install_requires=['gymail'],
+)
+
