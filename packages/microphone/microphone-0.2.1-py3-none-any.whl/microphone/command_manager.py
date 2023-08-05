da88f9826eec118f56efa4a12e9634afe4c87ddc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:33 +0000

    Add package microphone/0.2.1/microphone-0.2.1-py3-none-any.whl

---
+++ packages/microphone/microphone-0.2.1-py3-none-any.whl/microphone/command_manager.py
@@ -0,0 +1,28 @@
+from vexmessage import create_vex_message
+
+
+class CommandManager:
+    def __init__(self, audio_driver, messaging):
+        self.audio_driver = audio_driver
+        self.messaging = messaging
+
+    def handle_command(self, msg):
+        command = msg.contents.get('command')
+
+        if command == 'list devices':
+            devices = self.audio_driver.get_devices()
+            frame = create_vex_message(msg.source,
+                                       'microphone',
+                                       'RSP',
+                                       response=devices,
+                                       original=command)
+
+            self.messaging.communication_socket.send_multipart(frame)
+        elif command == 'record':
+            device = self.audio_driver.get_default_device()
+
+            # FIXME: pass in in settings instead
+            bits = 16
+            channels  = 2
+            chunksize = 1024
+            device.record(chunksize, bits, channels)
