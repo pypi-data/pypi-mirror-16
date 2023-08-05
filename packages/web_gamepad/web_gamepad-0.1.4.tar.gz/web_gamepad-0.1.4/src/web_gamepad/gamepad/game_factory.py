361173c9b126ab223509be9319f77bf4dd43d547
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:35 +0000

    Add package web_gamepad/0.1.4/web_gamepad-0.1.4.tar.gz

---
+++ packages/web_gamepad/web_gamepad-0.1.4.tar.gz/web_gamepad-0.1.4/src/web_gamepad/gamepad/game_factory.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+from web_gamepad.gamepad.gamepad_controller import GamepadController
+
+
+__game = None
+
+def get_game(height=None, width=None, invaders_count=None, notify_callback=print):
+    global __game
+
+    if not __game:
+        __game = GamepadController(notify_callback=notify_callback)
+    return __game
