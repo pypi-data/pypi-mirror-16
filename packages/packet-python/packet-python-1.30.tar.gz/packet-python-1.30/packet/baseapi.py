6272b8980b617d4c38a8e040a8c1ad337ed3afcb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:09 +0000

    Add package packet-python/1.30/packet-python-1.30.tar.gz

---
+++ packages/packet-python/packet-python-1.30.tar.gz/packet-python-1.30/packet/baseapi.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+import json
+import logging
+import requests
+
+
+class Error(Exception):
+    """Base exception class for this module"""
+    pass
+
+
+class JSONReadError(Error):
+    pass
+
+
+class BaseAPI(object):
+    """
+        Basic api class for
+    """
+
+    def __init__(self, auth_token, consumer_token):
+        self.auth_token = auth_token
+        self.consumer_token = consumer_token
+        self.end_point = 'api.packet.net'
+        self._log = logging.getLogger(__name__)
+
+    def call_api(self, method, type='GET', params=None):  # pragma: no cover
+        if params is None:
+            params = {}
+
+        url = 'https://' + self.end_point + '/' + method
+
+        headers = {'X-Auth-Token': self.auth_token,
+                   'X-Consumer-Token': self.consumer_token,
+                   'Content-Type': 'application/json'}
+
+        # remove token from log
+        headers_str = str(headers).replace(self.auth_token.strip(), 'TOKEN')
+        self._log.debug('%s %s %s %s' %
+                        (type, url, params, headers_str))
+
+        if type == 'GET':
+            url = url + '%s' % self._parse_params(params)
+            resp = requests.get(url, headers=headers)
+        elif type == 'POST':
+            resp = requests.post(url, headers=headers, data=json.dumps(params))
+        elif type == 'DELETE':
+            resp = requests.delete(url, headers=headers)
+        elif type == 'PATCH':
+            resp = requests.patch(url, headers=headers, data=json.dumps(params))
+        else:
+            raise Error(
+                'method type not recognizes as one of GET, POST, DELETE or PATCH: %s' % type
+            )
+
+        if not resp.content:
+            data = None
+        elif resp.headers['content-type'].startswith("application/json"):
+            try:
+                data = resp.json()
+            except ValueError as e:
+                raise JSONReadError(
+                    'Read failed: %s' % e.message
+                )
+        else:
+            data = resp.content
+
+        if not resp.ok:
+            msg = data
+            if not data:
+                msg = "(empty response)"
+            elif 'errors' in data:
+                msg = ', '.join(data['errors'])
+            raise Error(
+                'Error {0}: {1}'.format(resp.status_code, msg)
+            )
+        self.meta = None
+        if data:
+            self.meta = data.get('meta')
+        return data
+
+    def _parse_params(self, params):  # pragma: no cover
+        vals = list()
+        for k, v in params.items():
+            vals.append(str("%s=%s" % (k, v)))
+        return "?" + "&".join(vals)
