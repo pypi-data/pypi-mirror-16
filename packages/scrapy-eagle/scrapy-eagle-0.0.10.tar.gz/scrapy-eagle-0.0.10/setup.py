be588b175f5511e543934e2a6292b32063fb7430
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:06 +0000

    Add package scrapy-eagle/0.0.10/scrapy-eagle-0.0.10.tar.gz

---
+++ packages/scrapy-eagle/scrapy-eagle-0.0.10.tar.gz/scrapy-eagle-0.0.10/setup.py
@@ -0,0 +1,36 @@
+import os
+from setuptools import setup, find_packages
+
+
+LONG_DESC = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
+
+
+setup(name='scrapy-eagle',
+    version='0.0.10',
+    description='Run Scrapy Distributed',
+    long_description=LONG_DESC,
+    author='Rafael Alfredo Capucho',
+    author_email='rafael.capucho@gmail.com',
+    url='http://github.com/rafaelcapucho/scrapy-eagle',
+    packages=find_packages(),
+    license='BSD',
+    install_requires=[
+        'Scrapy>=1.1.0',
+        'redis>=2.10.0',
+        'flask',
+        'redis',
+        'pymongo',
+        'requests'
+    ],
+    entry_points = {
+        'console_scripts': ['eagle_server=scrapy_eagle.dashboard.server:main'],
+    },
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Framework :: Scrapy',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Intended Audience :: Developers',
+    ],
+)
