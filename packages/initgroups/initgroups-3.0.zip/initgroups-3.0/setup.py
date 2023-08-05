ca97b9a23e9055f608cfe3490acf49481bcb4fde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:39 +0000

    Add package initgroups/3.0/initgroups-3.0.zip

---
+++ packages/initgroups/initgroups-3.0.zip/initgroups-3.0/setup.py
@@ -0,0 +1,47 @@
+##############################################################################
+#
+# Copyright (c) 2010 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+
+from setuptools import setup, find_packages
+
+setup(name='initgroups',
+      version='3.0',
+      url='http://pypi.python.org/pypi/initgroups',
+      license='ZPL 2.1',
+      description="Convenience uid/gid helper function used in Zope2.",
+      author='Zope Corporation and Contributors',
+      author_email='zope-dev@zope.org',
+      long_description=(open('README.rst').read() + '\n' +
+                        open('CHANGES.rst').read()),
+
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
+      classifiers=[
+          "Development Status :: 6 - Mature",
+          "Environment :: Web Environment",
+          "Framework :: Zope2",
+          "License :: OSI Approved :: Zope Public License",
+          "Operating System :: OS Independent",
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 2",
+          "Programming Language :: Python :: 2.7",
+          "Programming Language :: Python :: 3",
+          "Programming Language :: Python :: 3.3",
+          "Programming Language :: Python :: 3.4",
+          "Programming Language :: Python :: 3.5",
+          "Programming Language :: Python :: Implementation :: CPython",
+          "Programming Language :: Python :: Implementation :: PyPy",
+      ],
+      include_package_data=True,
+      zip_safe=False,
+      )
