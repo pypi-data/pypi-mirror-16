753dafcd0b08d56ae16b1b63dc955b376c1e1f05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:28 +0000

    Add package gphoto2-cffi/0.3/gphoto2-cffi-0.3.tar.gz

---
+++ packages/gphoto2-cffi/gphoto2-cffi-0.3.tar.gz/gphoto2-cffi-0.3/gphoto2cffi/__init__.py
@@ -0,0 +1,4 @@
+from .gphoto2 import (Camera, list_cameras, supported_cameras,
+                      get_library_version)
+
+__all__ = [Camera, list_cameras, supported_cameras, get_library_version]
