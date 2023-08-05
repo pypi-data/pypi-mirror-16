3ec6894198151dbf5bfdba4ddb6acc46d99bba65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:33 +0000

    Add package link.middleware/0.19/link.middleware-0.19.tar.gz

---
+++ packages/link.middleware/link.middleware-0.19.tar.gz/link.middleware-0.19/link/middleware/file.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+
+from b3j0f.conf import Configurable, category
+from link.middleware.core import Middleware
+from link.middleware import CONF_BASE_PATH
+
+import os
+
+
+@Configurable(
+    paths='{0}/file.conf'.format(CONF_BASE_PATH),
+    conf=category('FILE')
+)
+class FileMiddleware(Middleware):
+    """
+    Middleware with the same API as the
+    :class:`link.middleware.http.HTTPMiddleware` but with ``file://` URI.
+    """
+
+    __protocols__ = ['file']
+
+    def get(self):
+        """
+        Get content of file pointed by middleware.
+
+        :returns: file's content
+        :rtype: str
+        """
+
+        path = '{0}{1}'.format(os.sep, os.path.join(*self.path))
+
+        with open(path) as f:
+            result = f.read()
+
+        return result
+
+    def post(self, data):
+        """
+        Write data to file pointed by middleware.
+
+        :param data: data to write
+        :type data: str
+        """
+
+        self.put(data)
+
+    def put(self, data):
+        """
+        Write data to file pointed by middleware.
+
+        :param data: data to write
+        :type data: str
+        """
+
+        path = '{0}{1}'.format(os.sep, os.path.join(*self.path))
+
+        with open(path, 'w') as f:
+            f.write(data)
+
+    def delete(self, data):
+        """
+        Delete file pointed by middleware.
+
+        :param data: unused (only for API compatibility)
+        """
+
+        path = '{0}{1}'.format(os.sep, os.path.join(*self.path))
+
+        if os.path.exists(path):
+            os.remove(path)
+
+    def options(self):
+        raise NotImplementedError('Unsupported by protocol')
+
+    def head(self):
+        raise NotImplementedError('Unsupported by protocol')
+
+    def patch(self, data):
+        raise NotImplementedError('Unsupported by protocol')
---
+++ packages/link.middleware/link.middleware-0.19.tar.gz/link.middleware-0.19/link/middleware/socket.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+
+from b3j0f.conf import Configurable, category
+from link.middleware.connectable import ConnectableMiddleware
+from link.middleware import CONF_BASE_PATH
+
+
+@Configurable(
+    paths='{0}/socket.conf'.format(CONF_BASE_PATH),
+    conf=category('SOCKET')
+)
+class SocketMiddleware(ConnectableMiddleware):
+    """
+    Socket middleware.
+    """
+
+    def new_socket(self, host, port):
+        """
+        Create a new socket (must be overriden).
+
+        :param host: host to connect to
+        :type host: str
+        :param port: port to connect to
+        :type port: int
+        :returns: socket object
+        """
+
+        raise NotImplementedError()
+
+    def _connect(self):
+        socks = [
+            self.new_socket(host, port)
+            for host, port in self.hosts
+        ]
+
+        return socks
+
+    def _disconnect(self, socks):
+        for sock in socks:
+            sock.close()
+
+    def _isconnected(self, socks):
+        return any([sock is not None for sock in socks])
+
+    def _send(self, sock, data):
+        """
+        Send data into socket (must be overriden).
+
+        :param sock: socket as returned by ``new_socket()``
+        :param data: data to send
+        """
+
+        raise NotImplementedError()
+
+    def _receive(self, sock, bufsize):
+        """
+        Fetch data from socket (must be overriden).
+
+        :param sock: socket as returned by ``new_socket()``
+        :param bufsize: Size of data to fetch
+        :returns: data read from socket
+        """
+
+        raise NotImplementedError()
+
+    def send(self, data):
+        """
+        Send data to the middleware.
+
+        :param data: data to send
+        """
+
+        for sock in self.conn:
+            self._send(sock, data)
+
+    def receive(self, bufsize):
+        """
+        Fetch data from middleware.
+
+        :param bufsize: Size of data to fetch
+        :returns: data read from middleware
+        """
+
+        data = []
+
+        for sock in self.conn:
+            data.append(self._receive(sock, bufsize))
+
+        if len(data) == 1:
+            return data[0]
+
+        else:
+            return data
