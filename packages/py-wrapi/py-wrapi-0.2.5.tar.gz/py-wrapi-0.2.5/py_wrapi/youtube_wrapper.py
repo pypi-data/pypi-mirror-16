b5c1a8eb5bd1fa26128e7aa1abca8d0b5de76b74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:39 +0000

    Add package py-wrapi/0.2.5/py-wrapi-0.2.5.tar.gz

---
+++ packages/py-wrapi/py-wrapi-0.2.5.tar.gz/py-wrapi-0.2.5/py_wrapi/youtube_wrapper.py
@@ -0,0 +1,37 @@
+from tapioca import (TapiocaAdapter, generate_wrapper_from_adapter, JSONAdapterMixin)
+from requests_oauthlib import OAuth2
+from .youtube_resource_mapping import RESOURCE_MAPPING
+from helpers import youtubeinfo, google_auth
+
+access_token = None
+
+class YoutubeClientAdapter(JSONAdapterMixin, TapiocaAdapter):
+    api_root = 'https://www.googleapis.com/youtube/v3/'
+    resource_mapping = RESOURCE_MAPPING
+
+    def get_request_kwargs(self, api_params, *args, **kwargs):
+        params = super(YoutubeClientAdapter, self).get_request_kwargs(
+            api_params, *args, **kwargs)
+
+        params['auth'] = OAuth2(
+            api_params.get('client_id', ''), token={
+                'access_token': self.auth(api_params.get('client_id'), api_params.get('client_secret')),
+                'token_type': 'Bearer'})
+        return params
+
+    def get_iterator_list(self, response_data):
+        return response_data
+
+    def get_iterator_next_request_kwargs(self, iterator_request_kwargs,
+                                         response_data, response):
+        pass
+
+    def auth(self, client_id, client_secret):
+        global access_token
+        if not access_token:
+            scope = 'https://www.googleapis.com/auth/youtube'
+            access_token = google_auth(client_id, client_secret, scope)
+        return access_token
+
+Youtube = generate_wrapper_from_adapter(YoutubeClientAdapter)
+Youtube.info = youtubeinfo
