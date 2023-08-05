4401e918b18db1078cc0a50b62653c8438090144
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:03 +0000

    Add package pytsite/0.79/PytSite-0.79.tar.gz

---
+++ packages/pytsite/PytSite-0.79.tar.gz/PytSite-0.79/pytsite/comments/_model.py
@@ -0,0 +1,30 @@
+"""Comments Models.
+"""
+from typing import Tuple as _Tuple
+from pytsite import image as _image, auth as _auth
+
+__author__ = 'Alexander Shepetko'
+__email__ = 'a@shepetko.com'
+__license__ = 'MIT'
+
+
+class CommentInterface:
+    @property
+    def thread_id(self) -> str:
+        raise NotImplementedError("Not implemented yet")
+
+    @property
+    def body(self) -> str:
+        raise NotImplementedError("Not implemented yet")
+
+    @property
+    def images(self) -> _Tuple[_image.model.Image]:
+        raise NotImplementedError("Not implemented yet")
+
+    @property
+    def status(self) -> str:
+        raise NotImplementedError("Not implemented yet")
+
+    @property
+    def author(self) -> _auth.model.AbstractUser:
+        raise NotImplementedError("Not implemented yet")
---
+++ packages/pytsite/PytSite-0.79.tar.gz/PytSite-0.79/pytsite/disqus/_widget.py
@@ -0,0 +1,31 @@
+"""Disqus Widgets.
+"""
+from pytsite import widget as _widget, html as _html, reg as _reg, tpl as _tpl
+
+__author__ = 'Alexander Shepetko'
+__email__ = 'a@shepetko.com'
+__license__ = 'MIT'
+
+
+class Comments(_widget.Abstract):
+    """Disqus Comments Widget.
+    """
+    def __init__(self, uid: str, **kwargs):
+        """Init.
+        """
+        super().__init__(uid, **kwargs)
+
+        self._short_name = _reg.get('disqus.short_name')
+
+    @property
+    def short_name(self) -> str:
+        """Get Disqus short name.
+        """
+        return self._short_name
+
+    def get_html_em(self, **kwargs) -> _html.Element:
+        """Render the widget.
+        :param **kwargs:
+        """
+        return _html.Div(_tpl.render('pytsite.disqus@widget', {'widget': self}),
+                         uid=self._uid, cls='widget widget-disqus')
