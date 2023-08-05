1d4c5d41ce5c2df3ef25c173cec01603093db8aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:28 +0000

    Add package pronto/0.1.9/pronto-0.1.9-py3-none-any.whl

---
+++ packages/pronto/pronto-0.1.9-py3-none-any.whl/pronto/__init__.py
@@ -0,0 +1,19 @@
+# coding: utf-8
+"""
+**pronto**: a Python frontend to ontologies
+===========================================
+
+"""
+
+
+
+__version__='0.1.9'
+__author__='Martin Larralde'
+__author_email__ = 'martin.larralde@ens-cachan.fr'
+
+from pronto.ontology import Ontology
+from pronto.term import Term, TermList
+from pronto.relationship import Relationship
+from pronto.parser import Parser
+
+__all__ = ["Ontology", "Term", "TermList", "Relationship", "Parser"]
