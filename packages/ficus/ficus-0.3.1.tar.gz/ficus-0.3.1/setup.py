46e59da15ca3d19fcec0afc15e8ad8cd63e93d1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:00 +0000

    Add package ficus/0.3.1/ficus-0.3.1.tar.gz

---
+++ packages/ficus/ficus-0.3.1.tar.gz/ficus-0.3.1/setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+import sys, platform, os
+
+try:
+    from setuptools import *
+except ImportError:
+    from distribute_setup import use_setuptools
+    use_setuptools()
+finally:
+    from setuptools import *
+
+from glob import glob
+
+if sys.version_info < (2, 6):
+    print >> sys.stderr, "ERROR: ficus requires python 2.6 or greater"
+    sys.exit()
+
+__version__ = open(os.path.join('ficus', 'VERSION')).read().strip()
+
+cmdclass = {}
+
+def main():
+    setup(  name = 'ficus',
+            version = __version__,
+            description = 'context managers for matplotlib',
+            url = 'https://github.com/camillescott/ficus',
+            author = 'Camille Scott',
+            author_email = 'camille.scott.w@gmail.com',
+            license = 'BSD',
+            test_suite = 'nose.collector',
+            tests_require = ['nose'],
+            packages = ['ficus'],
+            install_requires = ['nose',
+                                #'sphinxcontrib-napoleon',
+                                #'Sphinx',
+                                'matplotlib',
+                                'nose-capturestderr'],
+            include_package_data = True,
+            zip_safe = False,
+            cmdclass = cmdclass  )
+        
+if __name__ == "__main__":
+    main()
