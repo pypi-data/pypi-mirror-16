d415e8dba1f2b9e61d35aabe7f8f1677431ab088
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:47 +0000

    Add package dropbox-offline-backup/0.0.1/dropbox-offline-backup-0.0.1.tar.gz

---
+++ packages/dropbox-offline-backup/dropbox-offline-backup-0.0.1.tar.gz/dropbox-offline-backup-0.0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='dropbox-offline-backup',
+    version='0.0.1',
+    description='Backup your Dropbox offline',
+    url='https://github.com/764664/dropbox-offline-backup',
+    author='Jie Lu',
+    author_email='764664@gmail.com',
+    license='MIT',
+    install_requires=['dropbox'],
+    entry_points={
+        'console_scripts': [
+            'dropboxbackup=src:main',
+        ],
+    }
+)
