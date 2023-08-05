2de6811baf474c7bbe2c3906e1dc5f703447762a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:41 +0000

    Add package vacuum/0.0.5/vacuum-0.0.5.tar.gz

---
+++ packages/vacuum/vacuum-0.0.5.tar.gz/vacuum-0.0.5/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+#coding:utf-8
+# Author        : tuxpy
+# Email         : q8886888@qq.com.com
+# Last modified : 2016-07-14 14:55:30
+# Filename      : setup.py
+# Description   : 
+
+from setuptools import setup
+print('in setup')
+
+setup(
+        name = 'vacuum',
+        version = '0.0.5',
+        author = 'duoshoubang',
+        packages = [
+            'vacuum',
+            'vacuum.third_party',
+            'vacuum.third_party.submail',
+            ],
+        install_requires = ['cattle'],
+        description = 'JJ Bob',
+        scripts = ['bin/vacuum'],
+        )
+
