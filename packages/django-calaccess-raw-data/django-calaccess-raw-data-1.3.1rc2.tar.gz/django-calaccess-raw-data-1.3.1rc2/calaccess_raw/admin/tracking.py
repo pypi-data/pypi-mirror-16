31c05ac0715c5a1304d054a5585a33dc90e2a3bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:42 +0000

    Add package django-calaccess-raw-data/1.3.1rc2/django-calaccess-raw-data-1.3.1rc2.tar.gz

---
+++ packages/django-calaccess-raw-data/django-calaccess-raw-data-1.3.1rc2.tar.gz/django-calaccess-raw-data-1.3.1rc2/calaccess_raw/admin/tracking.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Custom administration panels for tracking models.
+"""
+from __future__ import unicode_literals
+from django.contrib import admin
+from calaccess_raw import models
+from .base import BaseAdmin
+
+
+@admin.register(models.RawDataVersion)
+class RawDataVersionAdmin(BaseAdmin):
+    """
+    Custom admin for the RawDataVersion model.
+    """
+    list_display = (
+        "id",
+        "release_datetime",
+        "pretty_size",
+    )
+    list_display_links = ('release_datetime',)
+    list_filter = ("release_datetime",)
+
+
+@admin.register(models.RawDataFile)
+class RawDataFileAdmin(BaseAdmin):
+    """
+    Custom admin for the RawDataFile model.
+    """
+    list_display = (
+        "id",
+        "version",
+        "file_name",
+        "download_records_count",
+        "clean_records_count",
+        "load_records_count",
+        "error_count",
+        "download_columns_count",
+        "clean_columns_count",
+        "load_columns_count",
+        "pretty_download_file_size",
+        "pretty_clean_file_size"
+    )
+    list_display_links = ('id', 'file_name',)
+    list_filter = ("version__release_datetime",)
+
+
+@admin.register(models.RawDataCommand)
+class RawDataCommandAdmin(BaseAdmin):
+    """
+    Custom admin for the RawDataCommand model.
+    """
+    list_display = (
+        "id",
+        "version",
+        "command",
+        "file_name",
+        "start_datetime",
+        "finish_datetime",
+    )
+    list_display_links = ("id", "command",)
+    list_filter = (
+        "start_datetime",
+        "finish_datetime",
+        "version__release_datetime",
+        "command",
+    )
