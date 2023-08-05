e42dfc9b539f063014df52b32cb343b92db7e4d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:30 +0000

    Add package pocomp/0.3/pocomp-0.3.tar.gz

---
+++ packages/pocomp/pocomp-0.3.tar.gz/pocomp-0.3/pocomp/pocomp.py
@@ -0,0 +1,15 @@
+import os
+import sys
+
+import polib
+
+root_dir = sys.argv[1]
+
+
+def main():
+    for file in os.listdir(root_dir):
+        if file.split(".")[-1] == "po":
+            file_name = file.split(".")[0]
+            po = polib.pofile("{}{}".format(root_dir, file))
+            po.save_as_mofile("{}{}.{}".format(root_dir, file_name, "mo"))
+            sys.stdout.write("UPDATED {}{}.mo\n".format(root_dir, file_name))
