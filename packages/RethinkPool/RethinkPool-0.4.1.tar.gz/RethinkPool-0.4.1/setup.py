209f4dc6924e76721f7bfeb43b72ba7e6b81b88f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:33 +0000

    Add package RethinkPool/0.4.1/RethinkPool-0.4.1.tar.gz

---
+++ packages/RethinkPool/RethinkPool-0.4.1.tar.gz/RethinkPool-0.4.1/setup.py
@@ -0,0 +1,16 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='RethinkPool',
+    version='0.4.1',
+    packages=['rethinkpool'],
+    url='https://github.com/lucidfrontier45/RethinkPool',
+    license='Apache License v2',
+    author='Shiqiao Du',
+    author_email='lucidfrontier.45@gmail.com',
+    description='RethinkDB Connection Pool for Python',
+    install_requires=["future", "rethinkdb"]
+)
