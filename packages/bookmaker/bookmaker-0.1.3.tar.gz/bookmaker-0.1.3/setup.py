d57e28094be532eba9b791524d4a0033dfaa7fee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:12 +0000

    Add package bookmaker/0.1.3/bookmaker-0.1.3.tar.gz

---
+++ packages/bookmaker/bookmaker-0.1.3.tar.gz/bookmaker-0.1.3/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+version = '0.1.3'
+
+setup(name='bookmaker',
+      version=version,
+      author="paul brian",
+      author_email="paul@mikadosoftware.com",
+      classifiers=[
+    # How mature is this project? Common values are
+    'Development Status :: 3 - Alpha',
+
+    # Indicate who your project is intended for
+    'Intended Audience :: Developers',
+    # Pick your license as you wish (should match "license" above)
+     'License :: OSI Approved :: MIT License',
+
+    # Specify the Python versions you support here. In particular, ensure
+    # that you indicate whether you support Python 2, Python 3 or both.
+    'Programming Language :: Python :: 2',
+     ],
+ )
