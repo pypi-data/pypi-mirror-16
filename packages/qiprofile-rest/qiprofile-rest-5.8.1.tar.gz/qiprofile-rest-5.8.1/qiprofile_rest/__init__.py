e2ba5c5ed0d3cf1ceb3105ddce73cfafa7e066de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:04 +0000

    Add package qiprofile-rest/5.8.1/qiprofile-rest-5.8.1.tar.gz

---
+++ packages/qiprofile-rest/qiprofile-rest-5.8.1.tar.gz/qiprofile-rest-5.8.1/qiprofile_rest/__init__.py
@@ -0,0 +1,7 @@
+"""The Quantitative Imaging Profile (*QiPr*) REST server."""
+
+__version__ = '5.8.1'
+"""
+The one-based major.minor.patch version. Minor and patch
+version numbers begin at 1.
+"""
