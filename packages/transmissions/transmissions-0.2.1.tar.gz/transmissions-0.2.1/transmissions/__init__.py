053d5f13f94f4e00826837266390e265f94f988d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:12 +0000

    Add package transmissions/0.2.1/transmissions-0.2.1.tar.gz

---
+++ packages/transmissions/transmissions-0.2.1.tar.gz/transmissions-0.2.1/transmissions/__init__.py
@@ -0,0 +1,7 @@
+from transmissions import trigger
+from transmissions.trigger import message
+from transmissions.models import Notification, Channel, TriggerBehavior
+
+__all__ = ('trigger', 'Notification', 'Channel', 'TriggerBehavior', 'message')
+
+__version__ = (0, 2, 1)
