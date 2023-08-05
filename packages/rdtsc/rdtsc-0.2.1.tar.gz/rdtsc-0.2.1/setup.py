6187b83b73f661be63bc208331c23ffe6665f739
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:28 +0000

    Add package rdtsc/0.2.1/rdtsc-0.2.1.tar.gz

---
+++ packages/rdtsc/rdtsc-0.2.1.tar.gz/rdtsc-0.2.1/setup.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+
+import sys
+import os
+import subprocess
+
+from setuptools import setup
+
+from distutils.command.build import build
+
+if sys.platform == 'darwin':
+    sofile = 'rdtsc.dylib'
+elif sys.platform == 'win32':
+    sofile = 'rdtsc.dll'
+else:
+    sofile = 'rdtsc.so.1'
+
+
+class RTDSCBuild(build):
+    def initialize_options(self):
+        build.initialize_options(self)
+        self.build_base = os.path.join(self.build_base, 'python')
+
+    def run(self):
+        subprocess.call(['cc', '-shared', '-o',
+                         os.path.join('src', 'rdtsc', sofile), '-fPIC',
+                         'src/rdtsc.c'])
+        build.run(self)
+
+
+setup(
+    name="rdtsc",
+    version="0.2.1",
+    author="James Brown",
+    author_email="Roguelazer@gmail.com",
+    url="https://github.com/Roguelazer/rdtsc",
+    license="ISC",
+    packages=['rdtsc'],
+    package_dir={'rdtsc': 'src/rdtsc'},
+    cmdclass={'build': RTDSCBuild},
+    package_data={'rdtsc': [sofile]},
+    keywords=["performance"],
+    description="Cycle timer wrapping the Intel x86 RTDSC instruction",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: C",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: ISC License (ISCL)",
+    ],
+    zip_safe=False,
+)
