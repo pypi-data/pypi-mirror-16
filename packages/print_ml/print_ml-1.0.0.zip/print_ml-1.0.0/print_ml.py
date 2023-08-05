74e17e499ff4e4bfbd34f9f4a7f307509e636bb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:46 +0000

    Add package print_ml/1.0.0/print_ml-1.0.0.zip

---
+++ packages/print_ml/print_ml-1.0.0.zip/print_ml-1.0.0/print_ml.py
@@ -0,0 +1,9 @@
+'''这是测试函数，可以打印'''
+def print_lol(the_list):
+    '''参数1是列表值，'''
+    for each_item in the_list:
+        if isinstance(each_item,list):
+            print_lol(each_item)
+        else:
+            print(each_item)
+            
