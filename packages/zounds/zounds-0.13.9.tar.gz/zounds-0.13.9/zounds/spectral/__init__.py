3d3c6d73719b88ba54a5ae4239731073452fb415
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:20 +0000

    Add package zounds/0.13.9/zounds-0.13.9.tar.gz

---
+++ packages/zounds/zounds-0.13.9.tar.gz/zounds-0.13.9/zounds/spectral/__init__.py
@@ -0,0 +1,5 @@
+from sliding_window import \
+    SlidingWindow, OggVorbisWindowingFunc, NDSlidingWindow
+
+from spectral import \
+    FFT, DCT, DCTIV, BarkBands, Chroma, BFCC, SpectralCentroid, SpectralFlatness
