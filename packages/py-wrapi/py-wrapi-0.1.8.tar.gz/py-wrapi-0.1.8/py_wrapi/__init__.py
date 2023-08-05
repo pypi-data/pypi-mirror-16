e8560273a0b179ea86a9cb97d79d80faf15cdf5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:07 +0000

    Add package py-wrapi/0.1.8/py-wrapi-0.1.8.tar.gz

---
+++ packages/py-wrapi/py-wrapi-0.1.8.tar.gz/py-wrapi-0.1.8/py_wrapi/__init__.py
@@ -0,0 +1,9 @@
+# coding: utf-8
+
+__author__ = 'Andre Tan'
+__email__ = 'tanandre93@gmail.com'
+__version__ = '0.1.8'
+
+from .sently_wrapper import Sently
+from .gsheets_wrapper import GSheets
+from .gmail_wrapper import Gmail
---
+++ packages/py-wrapi/py-wrapi-0.1.8.tar.gz/py-wrapi-0.1.8/py_wrapi/gmail_resource_mapping.py
@@ -0,0 +1,59 @@
+# coding: utf-8
+
+RESOURCE_MAPPING = {
+    'send_message': {
+        'resource': '{email}/messages/send',
+        'docs': 'https://developers.google.com/gmail/api/v1/reference/users/messages/send',
+        'methods': ['POST'],
+        'remarks': 'To include entire email message in an RFC 2822 formatted and base64url encoded string in request body'
+    },
+    'message': {
+        'resource': '{email}/messages/{message_id}',
+        'docs': 'https://developers.google.com/gmail/api/v1/reference/users/messages/get',
+        'methods': ['GET'],
+        'remarks': 'No request parameters required'
+    },
+    'thread': {
+        'resource': '{email}/threads/{thread_id}',
+        'docs': 'https://developers.google.com/gmail/api/v1/reference/users/threads/get',
+        'methods': ['GET'],
+        'remarks': 'No request parameters required'
+    },
+    'all_messages': {
+        'resource': '{email}/messages/',
+        'docs': 'https://developers.google.com/gmail/api/v1/reference/users/messages/list',
+        'methods': ['GET'],
+        'remarks': 'No request parameters required'
+    },
+    'all_threads': {
+        'resource': '{email}/threads',
+        'docs': 'https://developers.google.com/gmail/api/v1/reference/users/threads/list',
+        'methods': ['GET'],
+        'remarks': 'No request parameters required'
+    },
+    'trash_message': {
+        'resource': '{email}/messages/{message_id}/trash',
+        'docs': 'https://developers.google.com/gmail/api/v1/reference/users/messages/trash',
+        'methods': ['POST'],
+        'remarks': 'No request parameters required'
+    },
+    'trash_thread': {
+        'resource': '{email}/threads/{thread_id}/trash',
+        'docs': 'https://developers.google.com/gmail/api/v1/reference/users/threads/trash',
+        'methods': ['POST'],
+        'remarks': 'No request parameters required'
+    },
+    'get_self': {
+        'resource': '{email}/profile',
+        'docs': 'https://developers.google.com/gmail/api/v1/reference/users/getProfile',
+        'methods': ['GET'],
+        'remarks': 'No request parameters required'
+    },
+    'get_self': {
+        'resource': '{email}/labels',
+        'docs': 'https://developers.google.com/gmail/api/v1/reference/users/labels/list',
+        'methods': ['GET'],
+        'remarks': 'No request parameters required'
+    }
+
+}
