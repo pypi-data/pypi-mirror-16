940916380661e6fd52cb09030de145a6caaf4d7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:49 +0000

    Add package Islykill2/0.9rc3/Islykill2-0.9rc3.tar.gz

---
+++ packages/Islykill2/Islykill2-0.9rc3.tar.gz/Islykill2-0.9rc3/setup.py
@@ -0,0 +1,21 @@
+try:
+    from setuptools import setup
+except ImportError:
+    import distribute_setup
+    distribute_setup.use_setuptools()
+    from setuptools import setup
+
+
+setup(
+    name='Islykill2',
+    version='0.9rc3',
+    description='Utility to parse and verify Islykill authentication, '
+                'using SAML 2.0',
+    author='Sindri Gudmundsson',
+    author_email='sindrigudmundsson@gmail.com',
+    url='http://www.irdn.is/',
+    packages=['islykill2'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=['signxml']
+)
