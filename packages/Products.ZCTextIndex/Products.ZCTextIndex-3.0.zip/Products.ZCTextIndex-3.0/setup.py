7d5e2efe1b0ac32ceb956f1f62236a81044c4994
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:49 +0000

    Add package Products.ZCTextIndex/3.0/Products.ZCTextIndex-3.0.zip

---
+++ packages/Products.ZCTextIndex/Products.ZCTextIndex-3.0.zip/Products.ZCTextIndex-3.0/setup.py
@@ -0,0 +1,55 @@
+##############################################################################
+#
+# Copyright (c) 2010 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+
+from setuptools import setup, find_packages
+
+setup(name='Products.ZCTextIndex',
+      version='3.0',
+      url='http://pypi.python.org/pypi/Products.ZCTextIndex',
+      license='ZPL 2.1',
+      description="Full text indexing for ZCatalog / Zope 2.",
+      author='Zope Foundation and Contributors',
+      author_email='zope-dev@zope.org',
+      long_description=(open('README.rst').read() + '\n' +
+                        open('CHANGES.rst').read()),
+      packages=find_packages('src'),
+      namespace_packages=['Products'],
+      package_dir={'': 'src'},
+      classifiers=[
+          "Development Status :: 6 - Mature",
+          "Environment :: Web Environment",
+          "Framework :: Zope2",
+          "License :: OSI Approved :: Zope Public License",
+          "Operating System :: OS Independent",
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 2 :: Only",
+          "Programming Language :: Python :: 2.7",
+          "Programming Language :: Python :: Implementation :: CPython",
+      ],
+      install_requires=[
+          'setuptools',
+          'AccessControl',
+          'Acquisition',
+          'BTrees',
+          'transaction',
+          'Persistence',
+          'Products.ZCatalog',
+          'zExceptions',
+          'Zope2 >= 2.13.0dev',
+          'ZODB',
+          'zope.interface',
+      ],
+      include_package_data=True,
+      zip_safe=False,
+      )
