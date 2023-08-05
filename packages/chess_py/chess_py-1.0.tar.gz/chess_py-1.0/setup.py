207f07a71246c3b779583c33c7def08524a3c6cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:10 +0000

    Add package chess_py/1.0/chess_py-1.0.tar.gz

---
+++ packages/chess_py/chess_py-1.0.tar.gz/chess_py-1.0/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='chess_py',
+      version='1.0',
+      description='Python chess client',
+      author='Aubhro Sengupta',
+      author_email='aubhrosengupta@gmail.com',
+      url='https://github.com/LordDarkula/chess_py',
+      packages=['chess_py', 'chess_py.core', 'chess_py.core.algebraic', 'chess_py.game', 'chess_py.pieces',
+                'chess_py.players'],
+      )
