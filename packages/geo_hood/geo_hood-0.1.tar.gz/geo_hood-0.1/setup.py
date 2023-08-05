494fb9d545de493a4ffd90ea6afcbb626b12c83b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:16 +0000

    Add package geo_hood/0.1/geo_hood-0.1.tar.gz

---
+++ packages/geo_hood/geo_hood-0.1.tar.gz/geo_hood-0.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(name='geo_hood',
+      version='0.1',
+      description='Preprocessing geographic consumer data for P&G\s hadoop cluster',
+      url='http://github.com/robenalt/geo_hood',
+      author='Cody Hanson',
+      author_email='cjohnhanson@uchicago.edu',
+      packages=['geo_hood'])
+
+      
+      
