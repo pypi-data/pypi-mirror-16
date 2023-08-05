72f61cf6610997ea97021e232cdf23a53ece65a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:58 +0000

    Add package nesterlalala/1.0.1/nesterlalala-1.0.1.tar.gz

---
+++ packages/nesterlalala/nesterlalala-1.0.1.tar.gz/nesterlalala-1.0.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+setup(
+	name	=	'nesterlalala',
+	version	=	'1.0.1',
+	py_modules=	['nesterlalala'],
+	author=		'zcy',
+	author_email=	'zju_zcy@qq.com',
+	url=		'',
+	description=	'MyFirstTestFunction',
+)
+
