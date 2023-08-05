9c204554d3f1f8033a8c85465a66f1b369ecb830
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:39 +0000

    Add package py-wrapi/0.2.8/py-wrapi-0.2.8.tar.gz

---
+++ packages/py-wrapi/py-wrapi-0.2.8.tar.gz/py-wrapi-0.2.8/py_wrapi/gsheets_wrapper.py
@@ -0,0 +1,38 @@
+from tapioca import (TapiocaAdapter, generate_wrapper_from_adapter, JSONAdapterMixin)
+from requests_oauthlib import OAuth2
+from .gsheets_resource_mapping import RESOURCE_MAPPING
+from .printers import gsheetsinfo
+from .authenticate import google_auth
+
+access_token = None
+
+class GSheetsClientAdapter(JSONAdapterMixin, TapiocaAdapter):
+    api_root = 'https://sheets.googleapis.com/v4/'
+    resource_mapping = RESOURCE_MAPPING
+
+    def get_request_kwargs(self, api_params, *args, **kwargs):
+        params = super(GSheetsClientAdapter, self).get_request_kwargs(
+            api_params, *args, **kwargs)
+
+        params['auth'] = OAuth2(
+            api_params.get('client_id', ''), token={
+                'access_token': self.auth(api_params.get('client_id'), api_params.get('client_secret')),
+                'token_type': 'Bearer'})
+        return params
+
+    def get_iterator_list(self, response_data):
+        return response_data
+
+    def get_iterator_next_request_kwargs(self, iterator_request_kwargs,
+                                         response_data, response):
+        pass
+
+    def auth(self, client_id, client_secret):
+        global access_token
+        if not access_token:
+            scope = 'https://www.googleapis.com/auth/spreadsheets'
+            access_token = google_auth(client_id, client_secret, scope)
+        return access_token
+
+GSheets = generate_wrapper_from_adapter(GSheetsClientAdapter)
+GSheets.info = gsheetsinfo
---
+++ packages/py-wrapi/py-wrapi-0.2.8.tar.gz/py-wrapi-0.2.8/py_wrapi/youtube_resource_mapping.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+
+RESOURCE_MAPPING = {
+    'search': {
+        'resource': 'search',
+        'docs': 'https://developers.google.com/youtube/v3/docs/search/list',
+        'methods': ['GET'],
+        'remarks': "Required query parameter: 'part'"
+    }
+}
