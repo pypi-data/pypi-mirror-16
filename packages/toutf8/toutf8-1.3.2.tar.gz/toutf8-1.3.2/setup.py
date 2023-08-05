69c7b98c29aa51550daf8154eefb8b2fdf58597a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:42 +0000

    Add package toutf8/1.3.2/toutf8-1.3.2.tar.gz

---
+++ packages/toutf8/toutf8-1.3.2.tar.gz/toutf8-1.3.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name="toutf8",
+    version="1.3.2",
+    license='http://www.apache.org/licenses/LICENSE-2.0',
+    description="A tool to change all files in specified folder to utf-8 encoding",
+    author='1e0n',
+    author_email='i@leons.im',
+    keywords='utf8 encoding gbk gb2312 shiftjis ascii gb18030',
+    url='https://github.com/liangsun/toutf8',
+    packages=['toutf8'],
+    install_requires=['chardet'],
+    entry_points={
+        'console_scripts': [
+            'toutf8=toutf8:main',
+        ],
+    },
+)
