d7789aa68c91d64830fa47417618a17f9c0c7a3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:56 +0000

    Add package django-debug-toolbar-django-info/0.3.0/django-debug-toolbar-django-info-0.3.0.tar.gz

---
+++ packages/django-debug-toolbar-django-info/django-debug-toolbar-django-info-0.3.0.tar.gz/django-debug-toolbar-django-info-0.3.0/django_info_panel/__init__.py
@@ -0,0 +1,11 @@
+# coding: utf-8
+"""
+    django-info-panel
+    ~~~~~~~~~~~~~~~~~
+
+    :copyleft: 2015-2016 by the django-debug-toolbar-django-info team, see AUTHORS for more details.
+    :created: 2015 by JensDiemer.de
+    :license: GNU GPL v3 or above, see LICENSE for more details.
+"""
+
+__version__ = "0.3.0"
