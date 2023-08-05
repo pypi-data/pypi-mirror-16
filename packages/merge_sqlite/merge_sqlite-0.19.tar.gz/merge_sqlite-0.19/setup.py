07beb35b4ed22f13eba6f8131c3275e753bd95db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:15 +0000

    Add package merge_sqlite/0.19/merge_sqlite-0.19.tar.gz

---
+++ packages/merge_sqlite/merge_sqlite-0.19.tar.gz/merge_sqlite-0.19/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name = 'merge_sqlite',
+      author = 'Jeremiah H. Savage',
+      author_email = 'jeremiahsavage@gmail.com',
+      version = 0.19,
+      description = 'merge arbitrary number of sqlite files',
+      url = 'https://github.com/jeremiahsavage/merge_sqlite',
+      license = 'Apache 2.0',
+      packages = find_packages(),
+      install_requires = [
+          'pandas',
+          'sqlalchemy',
+          'cdis_pipe_utils'
+      ],
+      classifiers = [
+          'Development Status :: 3 - Alpha',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: Apache Software License',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 3',
+      ],
+)
