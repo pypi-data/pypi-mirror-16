f330ff18e46736c34786e7ce41b875d9b48ada66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:59 +0000

    Add package py-stringmatching/0.2.0/py_stringmatching-0.2.0.tar.gz

---
+++ packages/py-stringmatching/py_stringmatching-0.2.0.tar.gz/py_stringmatching-0.2.0/py_stringmatching/__init__.py
@@ -0,0 +1,25 @@
+__version__ = "0.2.0"
+
+# Import tokenizers
+from py_stringmatching.tokenizer.alphabetic_tokenizer import AlphabeticTokenizer
+from py_stringmatching.tokenizer.alphanumeric_tokenizer import AlphanumericTokenizer
+from py_stringmatching.tokenizer.delimiter_tokenizer import DelimiterTokenizer
+from py_stringmatching.tokenizer.qgram_tokenizer import QgramTokenizer
+from py_stringmatching.tokenizer.whitespace_tokenizer import WhitespaceTokenizer
+
+# Import similarity measures
+from py_stringmatching.similarity_measure.affine import Affine
+from py_stringmatching.similarity_measure.cosine import Cosine
+from py_stringmatching.similarity_measure.dice import Dice
+from py_stringmatching.similarity_measure.hamming_distance import HammingDistance
+from py_stringmatching.similarity_measure.jaccard import Jaccard
+from py_stringmatching.similarity_measure.jaro import Jaro
+from py_stringmatching.similarity_measure.jaro_winkler import JaroWinkler
+from py_stringmatching.similarity_measure.levenshtein import Levenshtein
+from py_stringmatching.similarity_measure.monge_elkan import MongeElkan
+from py_stringmatching.similarity_measure.needleman_wunsch import NeedlemanWunsch
+from py_stringmatching.similarity_measure.overlap_coefficient import OverlapCoefficient
+from py_stringmatching.similarity_measure.smith_waterman import SmithWaterman
+from py_stringmatching.similarity_measure.soft_tfidf import SoftTfIdf
+from py_stringmatching.similarity_measure.tfidf import TfIdf
+
