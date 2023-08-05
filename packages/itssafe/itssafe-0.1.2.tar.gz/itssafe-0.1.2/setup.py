5358db67eddb60d1c85a33d33af183e31ac3ac37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:56 +0000

    Add package itssafe/0.1.2/itssafe-0.1.2.tar.gz

---
+++ packages/itssafe/itssafe-0.1.2.tar.gz/itssafe-0.1.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="itssafe",
+    version="0.1.2",
+    description="A signer and unsigner that can be explained to those who would need to copy the implemenation in non-Python environments",
+    author="Mirus Research",
+    author_email="frank@mirusresearch.com",
+    packages=find_packages(),
+    url='https://bitbucket.org/mirusresearch/itssafe',
+    license='MIT license, see LICENSE',
+    py_modules=["itssafe"],
+    # install_requires=[
+    #     ],
+)
