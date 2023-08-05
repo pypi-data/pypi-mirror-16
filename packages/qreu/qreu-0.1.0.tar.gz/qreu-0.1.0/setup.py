e2d52d478b8730c5582c9ec0d9b8ca2ce7061a1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:09 +0000

    Add package qreu/0.1.0/qreu-0.1.0.tar.gz

---
+++ packages/qreu/qreu-0.1.0.tar.gz/qreu-0.1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='qreu',
+    version='0.1.0',
+    packages=find_packages(),
+    url='https://github.com/gisce/qreu',
+    install_requires=[
+        'flanker'
+    ],
+    license='MIT',
+    author='GISCE-TI, S.L.',
+    author_email='devel@gisce.net',
+    description='EMail Wrapper'
+)
