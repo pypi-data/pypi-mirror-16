0937aec91a25e3ef5b85aefbe7e5df6073ac2d18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:51 +0000

    Add package rbpy/0.1.0/rbpy-0.1.0-py3.5.egg

---
+++ packages/rbpy/rbpy-0.1.0-py3.5.egg/rbpy/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+__title__ = 'rbpy'
+__version__ = '0.1.0'
+__author__ = 'Matt Maybeno'
+__license__ = 'BSD'
+__copyright__ = 'Copyright 2016 Matt Maybeno'
+
+from .relay import Box
