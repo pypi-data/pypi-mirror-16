11acc2ed102559e3a56a810c856b1b6099f4e647
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:41 +0000

    Add package simplesat/0.5.0.post1/simplesat-0.5.0.post1.tar.gz

---
+++ packages/simplesat/simplesat-0.5.0.post1.tar.gz/simplesat-0.5.0.post1/simplesat/_version.py
@@ -0,0 +1,9 @@
+# THIS FILE IS GENERATED FROM SETUP_EXT
+version = '0.5.0.post1'
+full_version = '0.5.0.post1'
+git_revision = 'd363adea53cbbcd60c625648277041307980b6e7'
+is_released = True
+
+msi_version = '0.5.0.811'
+
+version_info = (0, 5, 0, 'final', 0)
