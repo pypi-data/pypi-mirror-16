9a5eb86e0011823e673c2e040042f11eff25d265
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:50 +0000

    Add package webpowerpy/0.3/webpowerpy-0.3.tar.gz

---
+++ packages/webpowerpy/webpowerpy-0.3.tar.gz/webpowerpy-0.3/setup.py
@@ -0,0 +1,22 @@
+from __future__ import print_function
+from setuptools import setup, find_packages
+
+setup(
+    name='webpowerpy',
+    version='0.3',
+    url='http://github.com/dooplan/webpower/',
+    license='GNU General Public License v3.0',
+    author='Pasqual Guerrero',
+    install_requires=['suds'],
+    author_email='pasqual.guerrero@gmail.com',
+    description='Python wrapper around WebPower SOAP-v4.2 API',
+    packages=find_packages(),
+    classifiers = [
+        'Programming Language :: Python',
+        'Natural Language :: English',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: Unix',
+    ],
+)
