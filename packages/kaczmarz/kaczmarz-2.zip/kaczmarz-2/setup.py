6b7470fd1f5ad7de8c973ab4f22d2f009f637296
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:07 +0000

    Add package kaczmarz/2/kaczmarz-2.zip

---
+++ packages/kaczmarz/kaczmarz-2.zip/kaczmarz-2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(name='kaczmarz',
+	version='2',
+	description='',
+	url='https://github.com/lightningleaf/kaczmarz',
+	author='lightningleaf',
+	author_email='lightningleaf0@gmail.com',
+	license='None',
+	packages=['kaczmarz'],
+	install_requires=[
+	'recordclass',
+	],
+	zip_safe=False,
+	classifiers=[
+	'Programming Language :: Python :: 3.5',
+	]
+	)
