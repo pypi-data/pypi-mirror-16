3aca2b3809272b5e83c012bfbba3d35208baeb01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:10:04 +0000

    Add package gisgroup-statbank/0.0.2/gisgroup-statbank-0.0.2.tar.gz

---
+++ packages/gisgroup-statbank/gisgroup-statbank-0.0.2.tar.gz/gisgroup-statbank-0.0.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='gisgroup-statbank',
+    version='0.0.2',
+    description='Statbank API client library',
+    url='http://github.com/gisgroup/statbank-python',
+    author='Gis Group ApS',
+    author_email='valentin@gisgroup.dk, zacharias@gisgroup.dk',
+    license='MIT',
+    packages=['statbank'],
+    install_requires=[
+        'python-dateutil',
+    ],
+    test_suite='tests',
+)
