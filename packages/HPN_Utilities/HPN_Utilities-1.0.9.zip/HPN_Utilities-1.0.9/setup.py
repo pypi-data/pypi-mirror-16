f6fbe947aaa3b669b673539573b35db38fa92d69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:17 +0000

    Add package HPN_Utilities/1.0.9/HPN_Utilities-1.0.9.zip

---
+++ packages/HPN_Utilities/HPN_Utilities-1.0.9.zip/HPN_Utilities-1.0.9/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+setup(name='HPN_Utilities', packages=['HPN_Utilities'], version='1.0.9',
+      description='Utilities in support of Quali CloudShell HPN Shell drivers', author='Gregory Sloan',
+      author_email='gregory.s@quali.com',
+      url='https://github.com/QualiSystemsLab/Customer-HPE/HPE_Shells/HPN_Utilities',
+      download_url='https://github.com/QualiSystemsLab/Customer-HPE/HPE_Shells/HPN_Utilities/tarball/1.0.9',
+      keywords=['telnet', 'console', 'handler', 'hpe', 'hpn', 'quali', 'cloudshell'], classifiers=[],
+      )
