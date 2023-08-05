3742f9367e7096f78cc65afd31faadbf2843c288
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:18 +0000

    Add package norduniclient/0.3.1/norduniclient-0.3.1.tar.gz

---
+++ packages/norduniclient/norduniclient-0.3.1.tar.gz/norduniclient-0.3.1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+version = '0.3.1'
+
+requires = [
+    'neo4jdb==0.0.9',
+]
+
+testing_requires = [
+    'nose',
+    'coverage',
+    'nosexcover',
+]
+
+setup(
+    name='norduniclient',
+    version=version,
+    url='https://github.com/NORDUnet/python-norduniclient',
+    license='',
+    author='Johan Lundberg',
+    author_email='lundberg@nordu.net',
+    description='Neo4j (>=2.2.8) database client for NORDUnet network inventory',
+    packages=find_packages(),
+    zip_safe=True,
+    install_requires=requires,
+    tests_require=testing_requires,
+    test_suite="norduniclient",
+    extras_require={
+        'testing': testing_requires
+    }
+)
