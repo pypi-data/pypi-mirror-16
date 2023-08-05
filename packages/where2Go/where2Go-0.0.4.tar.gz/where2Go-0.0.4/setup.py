df650ba822548dd850652924c2f1a1aa42c33698
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:04 +0000

    Add package where2Go/0.0.4/where2Go-0.0.4.tar.gz

---
+++ packages/where2Go/where2Go-0.0.4.tar.gz/where2Go-0.0.4/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+with open('README.md') as description:
+    long_description = description.read()
+
+with open('requirements.txt') as requirements:
+    reqs = requirements.read()
+
+reqs_list = reqs.split('\n')
+
+setup(
+    name='where2Go',
+    version='0.0.4',
+    author='Georgi Ivanov',
+    author_email='georgi.g.ivanov.95@gmail.com',
+    description='Your weekend-adventure assistant',
+    license='GNU GPL v2',
+    keywords=['weekend', 'walk', 'adventure', 'mountain', 'nature'],
+    packages=['where2Go'],
+    package_data={'where2Go': ['landmarks.db']},
+    long_description=long_description,
+    install_requires=reqs_list,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Natural Language :: English',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Games/Entertainment'
+    ]
+)
