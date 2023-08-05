92682bf34ebaded09111e4aa44141335a562e40c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:16 +0000

    Add package polycircles/0.3.5/polycircles-0.3.5.tar.gz

---
+++ packages/polycircles/polycircles-0.3.5.tar.gz/polycircles-0.3.5/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+setup(name='polycircles',
+      version='0.3.5',
+      description='Polycircles: WGS84 Circle approximations using polygons',
+      long_description=open('README').read(),
+      classifiers=[
+          "Development Status :: 3 - Alpha",
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 2.6",
+          "Programming Language :: Python :: 2.7",
+          "Programming Language :: Python :: 3.3",
+          "Topic :: Scientific/Engineering :: GIS",
+          "Topic :: Software Development :: Libraries",
+          "Environment :: Console",
+          "Intended Audience :: Developers",
+          "Intended Audience :: Information Technology",
+          "Intended Audience :: Science/Research",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: OS Independent"
+      ],
+      url='http://polycircles.rtfd.org',
+      author='Adam Matan',
+      author_email='adam@matan.name',
+      license='MIT',
+      packages=['polycircles'],
+      include_package_data=True,
+      install_requires=['geographiclib'],
+      tests_require=['geopy >= 0.99', 'nose >= 1.3.0', 'simplekml >= 1.2.3'],
+      test_suite='polycircles.test',
+      zip_safe=False)
+from setuptools import setup
+
