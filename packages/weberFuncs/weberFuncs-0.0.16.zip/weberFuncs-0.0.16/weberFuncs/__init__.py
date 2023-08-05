65a1d0a771a84cb2a503fa594fac8ab67319de90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:20 +0000

    Add package weberFuncs/0.0.16/weberFuncs-0.0.16.zip

---
+++ packages/weberFuncs/weberFuncs-0.0.16.zip/weberFuncs-0.0.16/weberFuncs/__init__.py
@@ -0,0 +1,24 @@
+#!/usr/local/bin/python
+# -*- coding:utf-8 -*-
+"""    
+    2015/10/14  WeiYanfeng
+    公共函数 包
+"""
+from WyfPublicFuncs import GetCurrentTime,GetUnixTimeStr,GetUnixTimeLocal,GetLocalTime,GetYYYYMMDDhhnnss,\
+    GetCurrentTimeMS,GetCurrentTimeMSs,GetTimeInteger,GetTimeIntMS
+
+from WyfPublicFuncs import PrintInline,PrintNewline,PrintTimeMsg,PrintMsTimeMsg,PrintAndSleep,\
+    PrettyPrintObj,PrettyPrintStr,printCmdString,printHexString,IsUtf8String
+
+from WyfPublicFuncs import GetRandomInteger,ConvertStringToInt32,GetCodeFmString,crc32,md5,md5file
+
+from WyfPublicFuncs import GetSrcParentPath,GetCriticalMsgLog,CAppendLogBase
+
+from WyfPublicFuncs import RequestsHttpGet,RequestsHttpPost
+
+from WyfPublicFuncs import ClassForAttachAttr
+
+from CSendSMTPMail import CSendSMTPMail
+
+if __name__ == '__main__':
+    pass
