4300c72e7db0add6110b8a8a85f9a19e7f10244e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:09 +0000

    Add package pyprel/2016.7.12.1553/pyprel-2016.7.12.1553.tar.gz

---
+++ packages/pyprel/pyprel-2016.7.12.1553.tar.gz/pyprel-2016.7.12.1553/setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+import setuptools
+
+def main():
+
+    setuptools.setup(
+        name             = "pyprel",
+        version          = "2016.07.12.1553",
+        description      = "Python print elegant",
+        long_description = Markdown_to_reStructuredText("README.md"),
+        url              = "https://github.com/wdbm/pyprel",
+        author           = "Will Breaden Madden",
+        author_email     = "w.bm@cern.ch",
+        license          = "GPLv3",
+        py_modules       = [
+                           "pyprel"
+                           ],
+        install_requires = [
+                           "pyfiglet",
+                           "shijian"
+                           ],
+        entry_points     = """
+            [console_scripts]
+            pyprel = pyprel:pyprel
+        """
+    )
+
+def read(*paths):
+    with open(os.path.join(*paths), "r") as filename:
+        return filename.read()
+
+def Markdown_to_reStructuredText(filename):
+    try:
+        import pypandoc
+        return pypandoc.convert(filename, "rst")
+    except:
+        print("pypandoc not found; long description could be corrupted")
+        return read(filename)
+
+if __name__ == "__main__":
+    main()
