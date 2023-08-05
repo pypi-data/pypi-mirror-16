932dd1c58fccb2328dccf79c6b29c63cbab74479
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:47 +0000

    Add package pocha/0.5.1/pocha-0.5.1.tar.gz

---
+++ packages/pocha/pocha-0.5.1.tar.gz/pocha-0.5.1/setup.py
@@ -0,0 +1,40 @@
+"""
+setup.py
+"""
+from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+
+class PochaTestCommand(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        from pocha.cli import cli
+        cli()
+
+setup(
+    name='pocha',
+    version='0.5.1',
+    author='Rodney Gomes',
+    author_email='rodneygomes@gmail.com',
+    url='',
+    install_requires=open('requirements.txt').read().split('\n'),
+    tests_require=open('test-requirements.txt').read().split('\n'),
+    cmdclass={'test': PochaTestCommand},
+    test_suite='test',
+    keywords=[''],
+    py_modules=['pocha'],
+    packages=find_packages(exclude=['tests']),
+
+    entry_points={
+        'console_scripts': [
+            'pocha=pocha.cli:cli'
+        ]
+    },
+
+    license='Apache 2.0 License',
+    description='',
+    long_description='',
+)
