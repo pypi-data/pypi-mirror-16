8595efebd8a881a230d7671019d2a4c49916926d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:36 +0000

    Add package esgprep/2.4.2/esgprep-2.4.2.tar.gz

---
+++ packages/esgprep/esgprep-2.4.2.tar.gz/esgprep-2.4.2/esgprep/mapfile/exceptions.py
@@ -0,0 +1,18 @@
+"""
+.. module:: esgprep.mapfile.exceptions.py
+    :platform: Unix
+    :synopsis: Custom exceptions used in this module for better readability of code.
+
+.. moduleauthor:: Guillaume Levavasseur <glipsl@ipsl.jussieu.fr>
+
+"""
+
+
+# class NoFilesFound(Exception):
+#     """
+#     To be raised if no files found into directories to scan.
+#     """
+#     def __init__(self, **args):
+#         # Default message
+#         self.msg = 'NO FILES FOUND'
+#         super(self.__class__, self).__init__(self.msg)
