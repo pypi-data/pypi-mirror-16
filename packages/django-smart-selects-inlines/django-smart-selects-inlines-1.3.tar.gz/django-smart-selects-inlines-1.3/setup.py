a91d24705acf845a3ea5c65a8e401a785d901df6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:43 +0000

    Add package django-smart-selects-inlines/1.3/django-smart-selects-inlines-1.3.tar.gz

---
+++ packages/django-smart-selects-inlines/django-smart-selects-inlines-1.3.tar.gz/django-smart-selects-inlines-1.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name="django-smart-selects-inlines",
+      version="1.3",
+      description="Django fork of django-smart-selects that fixes inlines.",
+      author="Ahmad Alzoughbi",
+      email="ahmadzoughbi@gmail.com",
+      url="https://github.com/azoughbi/smart_selects",
+      packages=find_packages(),
+      include_package_data=True,
+      )
