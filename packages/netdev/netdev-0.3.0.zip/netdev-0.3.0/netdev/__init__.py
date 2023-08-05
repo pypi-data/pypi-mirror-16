43003daad8f275074dd04446dc11ed7ad91719fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:42 +0000

    Add package netdev/0.3.0/netdev-0.3.0.zip

---
+++ packages/netdev/netdev-0.3.0.zip/netdev-0.3.0/netdev/__init__.py
@@ -0,0 +1,6 @@
+from netdev.dispatcher import connect
+from netdev.dispatcher import platforms
+from netdev.exceptions import DisconnectError
+
+__version__ = '0.3.0'
+__all__ = ('connect', 'platforms', 'DisconnectError')
