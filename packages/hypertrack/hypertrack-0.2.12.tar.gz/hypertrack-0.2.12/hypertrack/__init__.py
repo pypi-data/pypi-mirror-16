d4b42b82a38350d493eaf93039f910633f4ea8ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:22 +0000

    Add package hypertrack/0.2.12/hypertrack-0.2.12.tar.gz

---
+++ packages/hypertrack/hypertrack-0.2.12.tar.gz/hypertrack-0.2.12/hypertrack/__init__.py
@@ -0,0 +1,17 @@
+secret_key = None
+base_url = 'https://app.hypertrack.io/api/v1/'
+
+
+from hypertrack.resource import (
+    Customer,
+    Destination,
+    Fleet,
+    Driver,
+    Hub,
+    Task,
+    Trip,
+    GPSLog,
+    Event
+)
+
+from hypertrack.version import VERSION
