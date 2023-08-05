d33ec60eaeeddd93d8620e770ccde5e2868f1be7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:43 +0000

    Add package nester_poscom/1.1.0/nester_poscom-1.1.0.zip

---
+++ packages/nester_poscom/nester_poscom-1.1.0.zip/nester_poscom-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name            = 'nester_poscom',
+        version         = '1.1.0',
+        py_modules      = ['nester_poscom'],
+        author          = 'anuj s pandit',
+        author_email    = 'anuj.pandit@gmail.com',
+        url             = 'http://www.positronscombine.com',
+        description     = 'A simple printer of nested lists', 
+     )
