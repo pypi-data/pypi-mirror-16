5da8b9c98c4b05c8cb0c418d4a4dc24edaf39f09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:22 +0000

    Add package khan-mongo/0.1.8/khan-mongo-0.1.8.tar.gz

---
+++ packages/khan-mongo/khan-mongo-0.1.8.tar.gz/khan-mongo-0.1.8/setup.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup
+from khan import __version__, __author__, __email__
+
+requirements = [req[:-1] for req in open('requirements.txt', 'r').readlines()]
+
+test_requirements = requirements
+
+setup(
+    name='khan-mongo',
+    version=__version__,
+    description="'Khan'",
+    long_description='# Khan is a unix "top" clone for MongoDB, inspired by Innotop and Motop.',
+    author=__author__,
+    author_email=__email__,
+    url='https://github.com/otherpirate/khan',
+    packages=[
+        'khan',
+    ],
+    package_dir={'khan':
+                 'khan'},
+    include_package_data=True,
+    install_requires=requirements,
+    entry_points={
+        'console_scripts': [
+            'khan=khan.main:main',
+        ],
+    },
+    license="MIT license",
+    zip_safe=False,
+    keywords='khan',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    test_suite='tests',
+    tests_require=test_requirements
+)
