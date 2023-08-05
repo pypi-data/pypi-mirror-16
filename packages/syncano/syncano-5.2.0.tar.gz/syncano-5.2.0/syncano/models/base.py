5e953a0fe522c408adb57a717b6acb8fae18d958
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:06 +0000

    Add package syncano/5.2.0/syncano-5.2.0.tar.gz

---
+++ packages/syncano/syncano-5.2.0.tar.gz/syncano-5.2.0/syncano/models/base.py
@@ -0,0 +1,14 @@
+from .archetypes import *  # NOQA
+from .fields import *  # NOQA
+from .instances import *  # NOQA
+from .accounts import *  # NOQA
+from .billing import *  # NOQA
+from .channels import *  # NOQA
+from .classes import *  # NOQA
+from .data_views import *  # NOQA
+from .incentives import *  # NOQA
+from .traces import *  # NOQA
+from .push_notification import *  # NOQA
+from .geo import *  # NOQA
+from .backups import *  # NOQA
+from .data_views import DataEndpoint as EndpointData  # NOQA
