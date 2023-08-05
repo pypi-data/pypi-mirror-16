bd0b2ffad60643c593e69c49e500db9ef9fb4ffb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:43 +0000

    Add package nester160712/1.1.4/nester160712-1.1.4.zip

---
+++ packages/nester160712/nester160712-1.1.4.zip/nester160712-1.1.4/nester160712.py
@@ -0,0 +1,9 @@
+def nester(the_list, level=0, indent=True):
+        for each_item in the_list:
+                if isinstance(each_item, list):
+                        nester(each_item, level+2, indent)
+                else:
+                        if indent:
+                                for num in range(level):
+                                        print(' ', end='')
+                        print(each_item)
