e380bb67fed6b87a16bac58fac7825e459dbd47c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:36 +0000

    Add package drparse/0.0.3/drparse-0.0.3.tar.gz

---
+++ packages/drparse/drparse-0.0.3.tar.gz/drparse-0.0.3/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+import os
+import codecs
+import uuid
+
+from setuptools import setup
+from pip.req import parse_requirements
+
+def read(*parts):
+    filename = os.path.join(os.path.dirname(__file__), *parts)
+    with codecs.open(filename, encoding='utf-8') as fp:
+        return fp.read()
+
+setup(
+    name='drparse',
+    author='Tim van der Hulst',
+    author_email='tim.vdh@gmail.com',
+    version='0.0.3',
+    url='https://github.com/hampsterx/drparse',
+    install_requires=[str(ir.req) for ir in parse_requirements('requirements.txt', session=uuid.uuid4())],
+    py_modules=['drparse'],
+    license=read('LICENSE'),
+    description='Parse dates out of a textual string',
+    long_description=read('README.md'),
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+    ]
+)
