6f603bfc0f1945b18cd77450173a97915105413b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:14 +0000

    Add package moebot/0.1.0/moebot-0.1.0.tar.gz

---
+++ packages/moebot/moebot-0.1.0.tar.gz/moebot-0.1.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+from moebot import VERSION, __author__, __email__
+
+setup(
+    name='moebot',
+    version=VERSION,
+    author_email=__email__,
+    author=__author__,
+    packages=find_packages(),
+    description='A simple mediawiki API wrapper',
+    url='https://github.com/grzhan/moebot',
+    download_url='https://github.com/grzhan/moebot/tarball/' + VERSION,
+    keywords="mediawiki api wrapper"
+)
