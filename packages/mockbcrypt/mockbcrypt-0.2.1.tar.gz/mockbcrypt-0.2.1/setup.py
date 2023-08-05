8627b75f261f53618d2ab7e306288f3a2980fe5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:36 +0000

    Add package mockbcrypt/0.2.1/mockbcrypt-0.2.1.tar.gz

---
+++ packages/mockbcrypt/mockbcrypt-0.2.1.tar.gz/mockbcrypt-0.2.1/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+
+setup(
+    name='mockbcrypt',
+    version='0.2.1',
+    description='A bcrypt mock plugin for nosetests',
+    author=', '.join([
+        'Bone Yao',
+    ]),
+    url='https://github.com/boneyao/mockbcrypt',
+    packages=['mockbcrypt', ],
+    install_requires=[
+        'nose',
+        'mock'
+    ],
+    license='MIT',
+    entry_points={
+        'nose.plugins.0.10': [
+            'mockbcrypt = mockbcrypt.plugin:MockBcryptPlugin',
+        ]
+    },
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Testing',
+        'Environment :: Console',
+    ],
+)
