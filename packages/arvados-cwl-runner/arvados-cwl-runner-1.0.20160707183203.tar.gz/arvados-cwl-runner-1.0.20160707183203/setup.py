06ebef6623053bd13b98b305580cbed3ba60e4b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:09 +0000

    Add package arvados-cwl-runner/1.0.20160707183203/arvados-cwl-runner-1.0.20160707183203.tar.gz

---
+++ packages/arvados-cwl-runner/arvados-cwl-runner-1.0.20160707183203.tar.gz/arvados-cwl-runner-1.0.20160707183203/setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import setuptools.command.egg_info as egg_info_cmd
+
+from setuptools import setup, find_packages
+
+SETUP_DIR = os.path.dirname(__file__) or '.'
+README = os.path.join(SETUP_DIR, 'README.rst')
+
+try:
+    import gittaggers
+    tagger = gittaggers.EggInfoFromGit
+except ImportError:
+    tagger = egg_info_cmd.egg_info
+
+setup(name='arvados-cwl-runner',
+      version='1.0',
+      description='Arvados Common Workflow Language runner',
+      long_description=open(README).read(),
+      author='Arvados',
+      author_email='info@arvados.org',
+      url="https://arvados.org",
+      download_url="https://github.com/curoverse/arvados.git",
+      license='Apache 2.0',
+      packages=find_packages(),
+      scripts=[
+          'bin/cwl-runner',
+          'bin/arvados-cwl-runner'
+      ],
+      install_requires=[
+          'cwltool==1.0.20160630171631',
+          'arvados-python-client>=0.1.20160322001610',
+          'ruamel.yaml==0.11.11', # this should be declared by schema_salad instead, but see #9567
+      ],
+      data_files=[
+          ('share/doc/arvados-cwl-runner', ['LICENSE-2.0.txt', 'README.rst']),
+      ],
+      test_suite='tests',
+      tests_require=['mock>=1.0'],
+      zip_safe=True,
+      cmdclass={'egg_info': tagger},
+      )
