7545205c71d8e820d82724aeb614b602e509d59e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:36 +0000

    Add package deeperj123b/1.0.1/deeperj123b-1.0.1.zip

---
+++ packages/deeperj123b/deeperj123b-1.0.1.zip/deeperj123b-1.0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name="deeperj123b",
+    version="1.0.1",
+    py_modules=["deeperj123b"],
+    author="hfpython",
+    author_mail="hfpython@headfirstlabs.com",
+    url="http://www.headfirstlabs.com",
+    description="a simple print tool",
+    )
+
+    
