8432042de9b608e9150aa049377a8a413eee57e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:55 +0000

    Add package SQLiteHelper/0.7/SQLiteHelper-0.7.tar.gz

---
+++ packages/SQLiteHelper/SQLiteHelper-0.7.tar.gz/SQLiteHelper-0.7/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+  name = 'SQLiteHelper',
+  packages = ['SQLiteHelper'],
+  version = '0.7',
+  description = 'Wrapper of the sqlite3 module for easy of use',
+  author = 'Tom Orth',
+  author_email = 'torth212@gmail.com',
+  url = 'https://github.com/atf1999/SQLiteHelper',
+  download_url = 'https://github.com/atf1999/SQLiteHelper/tarball/0.7',
+  keywords = ['sqlite', 'database'],
+  classifiers = [],
+)
