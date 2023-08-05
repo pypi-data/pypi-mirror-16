0ec4c9c65f70dd51b13a0ad753de8d25bd6b01b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:41 +0000

    Add package nester_pj/1.0.0/nester_pj-1.0.0.tar.gz

---
+++ packages/nester_pj/nester_pj-1.0.0.tar.gz/nester_pj-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+name = 'nester_pj',
+version = '1.0.0',
+py_modules = ['nester_pj'],
+author = 'hfpython',
+author_email = 'abc@gmail.com',
+url = 'http://www.headfirstlabs.com',
+description = 'A simple list printer',
+)
