ed959b6c2f2e2bd1e562f2a6a6648372fa30a233
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:50 +0000

    Add package axsemantics/0.1.4.post1/axsemantics-0.1.4.post1-py3-none-any.whl

---
+++ packages/axsemantics/axsemantics-0.1.4.post1-py3-none-any.whl/axsemantics/constants.py
@@ -0,0 +1,8 @@
+API_BASE = 'https://api.ax-semantics.com'
+#API_BASE_RINCEWIND = 'http://localhost:8001'
+API_BASE_RINCEWIND = 'https://training-api.ax-semantics.com'
+API_TOKEN = None
+API_TOKEN_RINCEWIND = None
+API_VERSION = 'v1'
+
+DEBUG = False
