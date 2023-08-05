9c273d5da1e423c5c628b8330352c6748237fcf4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:44 +0000

    Add package distribution_py_710/1.1.0/distribution_py_710-1.1.0.tar.gz

---
+++ packages/distribution_py_710/distribution_py_710-1.1.0.tar.gz/distribution_py_710-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name         = 'distribution_py_710',
+    version      = '1.1.0',
+    py_modules   = ['distribution_py_710'],
+    author       = 'py710839160',
+    author_email = '710839160@qq.com',
+    url          = 'http://www.python.org',
+    description  = 'A start',
+    )
