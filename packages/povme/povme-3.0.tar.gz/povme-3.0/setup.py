0293012c54fafa62a7c60cf4a1345a85c8ede225
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:34 +0000

    Add package povme/3.0/povme-3.0.tar.gz

---
+++ packages/povme/povme-3.0.tar.gz/povme-3.0/setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+
+setup(
+  name = 'povme',
+  packages = ['povme'],
+  version = '3.0',
+  description = 'POVME (Pocket VOlume MEasurer) is a Python package for extracting actionable information from ensembles of protein structures for use in drug design.',
+  author = 'Jeff Wagner',
+  author_email = 'jwagnerjpl@gmail.com',
+  url = 'https://github.com/POVME/POVME',
+  download_url = 'https://github.com/POVME/POVME/tarball/2.0',
+  keywords = ['protein folding', 'protein', 'pocket volume measurer', 'computational protein folding', 'protein structures', 'drug design'],
+  classifiers = [
+    "Development Status :: 4 - Beta",
+    "Environment :: Console",
+    "Intended Audience :: Science/Research",
+    "License :: OSI Approved :: MIT License",
+    "Natural Language :: English",
+    "Programming Language :: Python :: 2.7",
+    "Topic :: Scientific/Engineering :: Bio-Informatics",
+    "Topic :: Scientific/Engineering :: Chemistry"
+  ]
+)
