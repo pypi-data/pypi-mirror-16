ee6643db99206d2b2f4992e60b3a5923342c1934
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:44 +0000

    Add package mezzanine-shortcodes/0.1.3/mezzanine_shortcodes-0.1.3-py3-none-any.whl

---
+++ packages/mezzanine-shortcodes/mezzanine_shortcodes-0.1.3-py3-none-any.whl/shortcodes/__init__.py
@@ -0,0 +1,6 @@
+__version__ = '0.1.3'
+
+from .render import richtext_filters
+from .register_api import button, Menu, menubutton, GenericButton, GenericMenubutton
+
+default_app_config = 'shortcodes.apps.ShortcodesConfig'
