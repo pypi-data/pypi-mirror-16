9ce5afccebf617adf56a2204f96aa79bcb9cbfa6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:30 +0000

    Add package github-email-explorer/0.1.0/github-email-explorer-0.1.0.tar.gz

---
+++ packages/github-email-explorer/github-email-explorer-0.1.0.tar.gz/github-email-explorer-0.1.0/github_email_explorer/cli_explore.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+
+from datetime import datetime
+from tabulate import tabulate
+import argparse
+import re
+
+import github_email
+
+
+class ExploreCliArgs(object):
+    def __init__(self):
+        p = argparse.ArgumentParser(prog='ge-explore')
+        p.add_argument('--repo', help='Repo on Github, type "<account>/<repo>"')
+        p.add_argument('--action_type', default='starred', help='"starred" and "fork" are the only two options now')
+        p.add_argument('--client_id', help='Github OAuth client ID')
+        p.add_argument('--client_secret', help='Github OAuth client secret')
+        p.add_argument('--status', action='store_true', help='Github API status')
+
+        args = p.parse_args()
+
+        self.repo = args.repo
+        self.repo_user = None
+        self.repo_name = None
+        if self.repo:
+            tmp = re.split('/', self.repo)
+            assert len(tmp) == 2, "repo format is not correct"
+
+            self.repo_user, self.repo_name = tmp[0], tmp[1]
+
+        self.action_type = args.action_type
+        self.client_id = args.client_id if args.client_id else ''
+        self.client_secret = args.client_secret if args.client_secret else ''
+        self.status = args.status
+
+
+def get_github_email_by_repo():
+    """ Get user email by repos
+    """
+    explore_cli_args = ExploreCliArgs()
+
+    github_api_auth = (explore_cli_args.client_id, explore_cli_args.client_secret)
+
+    if explore_cli_args.status:
+        # call api status
+        status = github_email.api_status(github_api_auth)
+        table = [["Core", status.core_limit, status.core_remaining, datetime.utcfromtimestamp(status.core_reset_time).strftime('%Y-%m-%dT%H:%M:%SZ')],
+                 ["Search", status.search_limit, status.search_remaining, datetime.utcfromtimestamp(status.search_reset_time).strftime('%Y-%m-%dT%H:%M:%SZ')]]
+        print "== GitHub API Status =="
+        print tabulate(table, headers=['Resource Type', 'Limit', 'Remaining', 'Reset Time'])
+        return
+
+    # get repo from explore_cli_args
+    for act_type in explore_cli_args.action_type:
+        ges = select_builder(act_type)(explore_cli_args.repo_user, explore_cli_args.repo_name, github_api_auth)
+    print 'Total: {}/{}'.format(len([ge for ge in ges if ge.email]), len(ges))
+    print github_email.format_email(ges)
+
+
+def select_builder(act_type):
+    return {
+        'starred': github_email.stargazers_email_info,
+        'fork': github_email.forks_email_info,
+    }[act_type]
+
+
+if __name__ == '__main__':
+    get_github_email_by_repo()
---
+++ packages/github-email-explorer/github-email-explorer-0.1.0.tar.gz/github-email-explorer-0.1.0/github_email_explorer/github_email.py
@@ -0,0 +1,162 @@
+# -*- coding: utf-8 -*-
+
+import requests
+from api_url import GitHubEndPoint as EndPoint
+
+
+class GithubUserEmail(object):
+    def __init__(self, *args, **kwargs):
+        self.g_id = None
+        self.name = kwargs.get('name', None)
+        self.email = kwargs.get('email', None)
+        if len(args) > 0 and (type(args[0]) is tuple):
+            self.name = args[0][0]
+            self.email = args[0][1]
+
+
+class GithubAPIStatus(object):
+    def __init__(self):
+        self.core_limit = None
+        self.core_remaining = None
+        self.core_reset_time = None
+        self.search_limit = None
+        self.search_remaining = None
+        self.search_reset_time = None
+
+
+class GithubRepository(object):
+    def __init__(self):
+        self.repo_id = None
+        self.name = None
+        self.description = None
+        self.stargazers_count = 0
+        self.watchers_count = 0
+        self.forks_count = 0
+
+
+def stargazers_user_ids(user_id, repo, github_api_auth):
+    # pagination
+    github_repo = repository(user_id, repo, github_api_auth)
+    per_page = 100
+    total_pages = github_repo.stargazers_count / per_page
+
+    # create url
+    url = EndPoint.add_auth_info(EndPoint.stargazers(user_id, repo), github_api_auth)
+
+    return request_user_ids_by_roll_pages(url, total_pages, per_page)
+
+
+def forks_user_ids(user_id, repo, github_api_auth):
+    # pagination
+    github_repo = repository(user_id, repo, github_api_auth)
+    per_page = 100
+    total_pages = github_repo.forks_count / per_page
+
+    # create url
+    url = EndPoint.add_auth_info(EndPoint.forks(user_id, repo), github_api_auth)
+
+    return request_user_ids_by_roll_pages(url, total_pages, per_page)
+
+
+def request_user_ids_by_roll_pages(url, total_pages, per_page):
+    # loop page with url
+    user_ids = []
+    for i in range(0, total_pages + 1):
+        url = EndPoint.pagination(url, page=(i + 1), per_page=per_page)
+        r = requests.get(url)
+
+        # raise error when found nothing
+        r.raise_for_status()
+
+        # handling result
+        user_ids = user_ids + [info['login'] if 'login' in info else info['owner']['login'] for info in r.json()]
+
+    return user_ids
+
+
+def stargazers_email_info(repo_user_id, repo_name, github_api_auth=None):
+    stargazers_ids = stargazers_user_ids(repo_user_id, repo_name, github_api_auth)
+    return users_email_info(stargazers_ids, github_api_auth)
+
+
+def forks_email_info(repo_user_id, repo_name, github_api_auth=None):
+    forks_ids = forks_user_ids(repo_user_id, repo_name, github_api_auth)
+    return users_email_info(forks_ids, github_api_auth)
+
+
+def users_email_info(action_user_ids, github_api_auth):
+    ges = []
+    for user_id in action_user_ids:
+        try:
+            ges.append(request_user_email(user_id, github_api_auth))
+        except requests.exceptions.HTTPError as e:
+            print e
+            # Return email addresses that have received after exception happened
+            return ges
+
+    return ges
+
+
+def request_user_email(user_id, github_api_auth):
+    """
+    Get email from the profile
+    """
+
+    rsp = requests.get(EndPoint.add_auth_info(EndPoint.user_profile(user_id), github_api_auth))
+    # raise error when found nothing
+    rsp.raise_for_status()
+
+    rsp = rsp.json()
+    ge = GithubUserEmail()
+    ge.g_id = rsp['login']
+    ge.name = rsp['name'] if rsp['name'] else rsp['login']
+    ge.name = ge.name.encode('utf-8')
+    ge.email = rsp['email']
+
+    # TODO user email from events
+
+    return ge
+
+
+def format_email(ges):
+    """
+    John <John@example.org>; Peter James <James@example.org>
+    """
+
+    formatted_email = ['{} <{}>'.format(ge.name, ge.email) for ge in ges if ge.email]
+    formatted_email = '; '.join(formatted_email)
+    return formatted_email
+
+
+def api_status(github_api_auth):
+    rsp = requests.get(EndPoint.add_auth_info(EndPoint.rate_limit(), github_api_auth))
+    rsp = rsp.json()
+    status = GithubAPIStatus()
+    status.core_reset_time = rsp['resources']['core']['reset']
+    status.core_limit = rsp['resources']['core']['limit']
+    status.core_remaining = rsp['resources']['core']['remaining']
+    status.search_reset_time = rsp['resources']['search']['reset']
+    status.search_limit = rsp['resources']['search']['limit']
+    status.search_remaining = rsp['resources']['search']['remaining']
+    return status
+
+
+def repository(user_id, repo, github_api_auth):
+    rsp = requests.get(EndPoint.add_auth_info(EndPoint.repository(user_id, repo), github_api_auth))
+    rsp = rsp.json()
+    repo = GithubRepository()
+    repo.repo_id = rsp['id']
+    repo.name = rsp['name']
+    repo.description = rsp['description']
+    repo.stargazers_count = rsp['stargazers_count']
+    repo.watchers_count = rsp['watchers_count']
+    repo.forks_count = rsp['forks_count']
+    return repo
+
+
+if __name__ == '__main__':
+    # print request_user_email('yuecen')
+    ges = stargazers_email_info('yuecen', 'github-email-explorer')
+    print 'Total: {}/{}'.format(len([ge for ge in ges if ge.email]), len(ges))
+    print format_email(ges)
+
