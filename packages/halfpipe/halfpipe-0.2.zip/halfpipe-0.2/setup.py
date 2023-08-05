d0dedc0230a215ead41eefcf1e7f8b9efa61ed5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:52 +0000

    Add package halfpipe/0.2/halfpipe-0.2.zip

---
+++ packages/halfpipe/halfpipe-0.2.zip/halfpipe-0.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='halfpipe',
+      version='0.2',
+      description='Functional Programming Tools',
+      url='https://github.com/migloo/halfpipe',
+      author='migloo',
+      author_email='migloo@gmail.com',
+      license='MIT',
+      packages=['halfpipe'],
+      install_requires=['re', 'functools'],
+      zip_safe=False)
+
