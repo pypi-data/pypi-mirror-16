939852bfeae137f05b1031792c0106603c66f22f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:19 +0000

    Add package nester_ken/1.3.0/nester_ken-1.3.0.tar.gz

---
+++ packages/nester_ken/nester_ken-1.3.0.tar.gz/nester_ken-1.3.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'nester_ken',
+    version = '1.3.0',
+    py_modules = ['nester_ken'],
+    author = 'ken',
+    author_email = 'kenneth.jh.han@gmail.com',
+    url = 'http://www.bioinformaticist.com',
+    description = 'A simple printer of nested lists',
+    )
