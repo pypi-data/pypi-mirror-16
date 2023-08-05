2b534b3f2db32cdfa9f81ad941379769523dce5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:49 +0000

    Add package Sphinx/1.4.5/Sphinx-1.4.5.tar.gz

---
+++ packages/Sphinx/Sphinx-1.4.5.tar.gz/Sphinx-1.4.5/tests/test_ext_ifconfig.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+    test_ext_ifconfig
+    ~~~~~~~~~~~~~~~~~
+
+    Test sphinx.ext.ifconfig extension.
+
+    :copyright: Copyright 2007-2016 by the Sphinx team, see AUTHORS.
+    :license: BSD, see LICENSE for details.
+"""
+
+from util import with_app
+
+
+@with_app(testroot='ext-ifconfig')
+def test_ifconfig(app, status, warning):
+    app.builder.build_all()
