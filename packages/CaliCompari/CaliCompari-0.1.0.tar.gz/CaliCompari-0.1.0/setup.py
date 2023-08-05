cef3cca3de2ed7ac94548352d24ee148f2beae82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:41 +0000

    Add package CaliCompari/0.1.0/CaliCompari-0.1.0.tar.gz

---
+++ packages/CaliCompari/CaliCompari-0.1.0.tar.gz/CaliCompari-0.1.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+# version_file = open('VERSION')
+# version = version_file.read().strip()
+
+setup(name="CaliCompari",
+      version='0.1.0',
+      description="Compare spectra with reference spectrum.",
+      py_modules=['calicompari'],
+      author='Jonathan Whitmore',
+      author_email='jbwhit@gmail.com',
+      url='https://github.com/jbwhit/CaliCompari',
+      )
