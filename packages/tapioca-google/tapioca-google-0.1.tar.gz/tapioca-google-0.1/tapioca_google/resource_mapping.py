d976d76f97085bd82ab4d8bfefe45f40886b6018
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:25 +0000

    Add package tapioca-google/0.1/tapioca-google-0.1.tar.gz

---
+++ packages/tapioca-google/tapioca-google-0.1.tar.gz/tapioca-google-0.1/tapioca_google/resource_mapping.py
@@ -0,0 +1,8 @@
+# coding: utf-8
+
+RESOURCE_MAPPING = {
+    'people': {
+        'resource': 'people/{id}',
+        'docs': 'https://developers.google.com/+/web/api/rest/?hl=en_US#api-calls'
+    },
+}
