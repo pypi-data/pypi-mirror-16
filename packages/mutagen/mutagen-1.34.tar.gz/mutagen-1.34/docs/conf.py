9bea3cc238c5b95c2f7a96569df75811a3a52429
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:02 +0000

    Add package mutagen/1.34/mutagen-1.34.tar.gz

---
+++ packages/mutagen/mutagen-1.34.tar.gz/mutagen-1.34/docs/conf.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+dir_ = os.path.dirname(os.path.realpath(__file__))
+sys.path.insert(0, dir_)
+sys.path.insert(0, os.path.abspath(os.path.join(dir_, "..")))
+import mutagen
+
+needs_sphinx = "1.3"
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.intersphinx',
+    'ext',
+]
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/2.7', None),
+    'python3': ('https://docs.python.org/3.5', None),
+}
+source_suffix = '.rst'
+master_doc = 'index'
+project = 'mutagen'
+copyright = u'2016, Joe Wreschnig, Michael Urman, Lukáš Lalinský, ' \
+            u'Christoph Reiter, Ben Ockmore & others'
+version = mutagen.version_string
+release = mutagen.version_string
+exclude_patterns = ['_build']
+bug_url_template = "https://github.com/quodlibet/mutagen/issues/%s"
+pr_url_template = "https://github.com/quodlibet/mutagen/pull/%s"
+bbpr_url_template = "https://bitbucket.org/lazka/mutagen/pull-requests/%s"
+
+autodoc_member_order = "bysource"
+default_role = "obj"
+
+html_theme = "sphinx_rtd_theme"
+html_favicon = "images/favicon.ico"
+html_theme_options = {
+    "display_version": False,
+}
