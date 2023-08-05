8906e0e6e24b47cd1e60da173c7aa4b15c870184
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:17 +0000

    Add package singularity/0.4/singularity-0.4.tar.gz

---
+++ packages/singularity/singularity-0.4.tar.gz/singularity-0.4/singularity/scripts.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+
+'''
+script.py: part of singularity command line tool
+Runtime executable, "shub"
+
+'''
+
+from singularity.runscript import get_runscript_template
+from singularity.utils import check_install
+from singularity.package import package
+from glob import glob
+import argparse
+import sys
+import os
+
+def main():
+    parser = argparse.ArgumentParser(
+    description="package Singularity containers for singularity hub.")
+    parser.add_argument("--image", dest='image', help="full path to singularity image (for use with --package)", type=str, default=None)
+    parser.add_argument("--outfolder", dest='outfolder', help="full path to folder for output, if not specified, will go to pwd", type=str, default=None)
+    parser.add_argument("--runscript", dest='runscript', help="specify extension to generate a runscript template in the PWD, or include --outfolder to change output directory. Currently supported types are py (python).", type=str, default=None)
+    parser.add_argument('--package', help="package a singularity container for singularity hub", dest='package', default=False, action='store_true')
+    try:
+        args = parser.parse_args()
+    except:
+        parser.print_help()
+        sys.exit(0)
+
+    # Output folder will be pwd if not specified
+    if args.outfolder == None:
+        output_folder = os.getcwd()
+    else:
+        output_folder = args.outfolder
+
+    # If the user wants a runscript template.
+    if args.runscript != None:
+        get_runscript_template(output_folder=output_folder,
+                               script_name="singularity",
+                               language=args.runscript)
+
+    # We can only continue if singularity is installed
+    if check_install() == True:
+
+       # We also must have an image to work with
+       if args.image !=None:
+           image = os.path.abspath(args.image)
+
+           # Make sure the image exists!
+           if os.path.exists(image):
+
+               # The user wants to package the image
+               if args.package == True:
+                   package(image_path=image,
+                           output_folder=output_folder,
+                           runscript=True,
+                           software=True)
+
+       else:
+          print("Please specify a singularity image with --image.")
+
+
+if __name__ == '__main__':
+    main()
