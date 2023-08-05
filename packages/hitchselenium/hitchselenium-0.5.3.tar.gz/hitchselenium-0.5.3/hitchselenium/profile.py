7bf647b9ac6c2d84d28a764ad6b881fe827b4341
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:25 +0000

    Add package hitchselenium/0.5.3/hitchselenium-0.5.3.tar.gz

---
+++ packages/hitchselenium/hitchselenium-0.5.3.tar.gz/hitchselenium-0.5.3/hitchselenium/profile.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+import os
+
+
+class Profile(webdriver.FirefoxProfile):
+    def __init__(self, *args, **kwargs):
+        super(Profile, self).__init__(*args, **kwargs)
+        self.set_preference("startup.homepage_welcome_url.additional", "")
