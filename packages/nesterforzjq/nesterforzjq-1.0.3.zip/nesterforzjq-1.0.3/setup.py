ff8681b84fd29b06df3edb645de8f18e024ca622
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:28 +0000

    Add package nesterforzjq/1.0.3/nesterforzjq-1.0.3.zip

---
+++ packages/nesterforzjq/nesterforzjq-1.0.3.zip/nesterforzjq-1.0.3/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+		name         = 'nesterforzjq',
+		version      = '1.0.3',
+		py_modules   = ['nester'],
+		author       = 'Nker',
+		author_email = 'zhaojiaqi@gmail.com',
+		url          = 'http://www.baidu.com',
+		description  = 'A simple printer of nested lists'
+	  )
