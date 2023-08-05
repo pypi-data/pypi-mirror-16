7e4f358f7a4c92fb6412d91e8ef4cbb451aa30ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:01 +0000

    Add package py-wrapi/0.1/py-wrapi-0.1.tar.gz

---
+++ packages/py-wrapi/py-wrapi-0.1.tar.gz/py-wrapi-0.1/py_wrapi/sently_resource_mapping.py
@@ -0,0 +1,14 @@
+# coding: utf-8
+
+RESOURCE_MAPPING = {
+    'resource_name': {
+        'resource': 'resource_url',
+        'docs': 'http://www.documentation.com/link/to/resource/documentation'
+    },
+    'send_sms': {
+        'resource': 'api/outboundmessage',
+        'docs': 'http://www.google.com.sg'
+    }
+}
+
+# resp = api.send_sms().post(data=body)()
