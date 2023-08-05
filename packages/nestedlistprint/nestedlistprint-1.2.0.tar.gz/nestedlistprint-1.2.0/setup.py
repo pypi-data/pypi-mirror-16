74ccc3bb24c2f032e846d461eaf027de01bf531d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:29 +0000

    Add package nestedlistprint/1.2.0/nestedlistprint-1.2.0.tar.gz

---
+++ packages/nestedlistprint/nestedlistprint-1.2.0.tar.gz/nestedlistprint-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name = 'nestedlistprint',
+	version = '1.2.0',
+	py_module = ['nestedlistprint'],
+	author = 'wubo',
+	author_email = "496983824@qq.com",
+	url = 'http://weibo.com/fastbird7/home?wvr=5&lf=reg',
+	description = 'A simple print of nested lists'
+	)
