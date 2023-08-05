a5e98445f89fc138b6ad8fac0c13a91ea9344d44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:22 +0000

    Add package django-tastypie-sorl-thumbnail/0.1.3/django-tastypie-sorl-thumbnail-0.1.3.tar.gz

---
+++ packages/django-tastypie-sorl-thumbnail/django-tastypie-sorl-thumbnail-0.1.3.tar.gz/django-tastypie-sorl-thumbnail-0.1.3/setup.py
@@ -0,0 +1,37 @@
+from codecs import open
+
+from setuptools import setup, find_packages
+
+from tastypie_sorl_thumbnail import __version__, __author__, __email__, __license__
+
+setup(
+    name="django-tastypie-sorl-thumbnail",
+    version=__version__,
+    author=__author__,
+    author_email=__email__,
+    url='https://github.com/tomi77/tastypie-sorl-thumbnail',
+    description='sorl-thumbnail support for a Django Tastypie',
+    long_description=open("README.rst").read(),
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities',
+    ],
+    license=__license__,
+    packages=find_packages(exclude=['testapp']),
+    install_requires=[
+        'django-tastypie',
+        'sorl-thumbnail',
+    ]
+)
