6b8c71044cb5766c56ecf870de7e0a46ed998165
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:39 +0000

    Add package ntfy/2.0.4/ntfy-2.0.4-py2.py3-none-any.whl

---
+++ packages/ntfy/ntfy-2.0.4-py2.py3-none-any.whl/ntfy/__init__.py
@@ -0,0 +1,39 @@
+import logging
+from importlib import import_module
+
+__version__ = '2.0.4'
+
+
+def notify(message, title, config=None, **kwargs):
+    from .config import load_config
+
+    if config is None:
+        config = load_config()
+
+    ret = 0
+
+    for backend in config.get('backends', ['default']):
+        backend_config = config.get(backend, {})
+        backend_config.update(kwargs)
+        if 'backend' in backend_config:
+            backend = backend_config['backend']
+        try:
+            module = import_module('ntfy.backends.{}'.format(backend))
+        except ImportError:
+            ret = 1
+            logging.getLogger(__name__).error(
+                'failed to load backend {}'.format(backend),
+                exc_info=True)
+            continue
+
+        try:
+            module.notify(message=message, title=title, **backend_config)
+        except (SystemExit, KeyboardInterrupt):
+            raise
+        except Exception:
+            logging.getLogger(__name__).error(
+                'Failed to send notification using {}'.format(backend),
+                exc_info=True)
+            ret = 1
+
+    return ret
---
+++ packages/ntfy/ntfy-2.0.4-py2.py3-none-any.whl/ntfy/backends/default.py
@@ -0,0 +1,14 @@
+from sys import platform
+from importlib import import_module
+
+
+def notify(title, message, **kwargs):
+    """
+    This backend automatically selects the correct desktop notification backend
+    for your operating system.
+    """
+    for os in ['linux', 'win32', 'darwin']:
+        if platform.startswith(os):
+            module = import_module('ntfy.backends.{}'.format(os))
+            module.notify(title=title, message=message, **kwargs)
+            break
