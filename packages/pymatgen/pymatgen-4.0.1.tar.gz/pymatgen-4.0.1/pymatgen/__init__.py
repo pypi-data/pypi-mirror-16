37624c52cc4ed60361a3858f9543d5b59b4b1a9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:23 +0000

    Add package pymatgen/4.0.1/pymatgen-4.0.1.tar.gz

---
+++ packages/pymatgen/pymatgen-4.0.1.tar.gz/pymatgen-4.0.1/pymatgen/__init__.py
@@ -0,0 +1,17 @@
+from __future__ import unicode_literals
+
+__author__ = "Pymatgen Development Team"
+__email__ ="pymatgen@googlegroups.com"
+__maintainer__ = "Shyue Ping Ong"
+__maintainer_email__ ="shyuep@gmail.com"
+__date__ = "Jul 8 2016"
+__version__ = "4.0.1"
+
+
+# Useful aliases for commonly used objects and modules.
+# Allows from pymatgen import <class> for quick usage.
+
+from .core import *
+from .electronic_structure.core import Spin, Orbital
+from .matproj.rest import MPRester
+from monty.json import MontyEncoder, MontyDecoder, MSONable
