5525d368170cde4a011a838134342897689ab62e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:17 +0000

    Add package emilesu_nester/1.3.0/emilesu_nester-1.3.0.zip

---
+++ packages/emilesu_nester/emilesu_nester-1.3.0.zip/emilesu_nester-1.3.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'emilesu_nester',
+    version = '1.3.0',
+    py_modules = ['emilesu_nester'],
+    author = 'emilesu',
+    author_email = 'emilesu@qq.com',
+    url = 'http://www.bigdasu.com',
+    description = 'A simple printer of nested lists',
+    )
