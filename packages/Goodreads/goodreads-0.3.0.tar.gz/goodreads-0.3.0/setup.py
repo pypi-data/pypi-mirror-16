28b3cc9ea8cc5fcd13a4ef80d36c09f7a98fba15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:45 +0000

    Add package Goodreads/0.3.0/goodreads-0.3.0.tar.gz

---
+++ packages/Goodreads/goodreads-0.3.0.tar.gz/goodreads-0.3.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='goodreads',
+    description="Python wrapper for Goodreads API",
+    long_description=open("README.rst").read(),
+    author='Sefa Kilic',
+    author_email='sefakilic@gmail.com',
+    url='https://github.com/sefakilic/goodreads/',
+    version='0.3.0',
+    install_requires=['nose', 'xmltodict', 'requests', 'rauth'],
+    packages=['goodreads'],
+    scripts=[],
+    license='MIT',
+)
