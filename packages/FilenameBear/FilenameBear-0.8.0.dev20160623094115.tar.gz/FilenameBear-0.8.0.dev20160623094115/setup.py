58747535f7dfd49c73ea24d72260cfcd1a1d6269
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:32 +0000

    Add package FilenameBear/0.8.0.dev20160623094115/FilenameBear-0.8.0.dev20160623094115.tar.gz

---
+++ packages/FilenameBear/FilenameBear-0.8.0.dev20160623094115.tar.gz/FilenameBear-0.8.0.dev20160623094115/setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+import locale
+
+from setuptools import setup
+
+
+try:
+    locale.getlocale()
+except (ValueError, UnicodeError):
+    locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
+
+
+if __name__ == "__main__":
+    try:
+        setup(name='FilenameBear',
+              version='0.8.0.dev20160623094115',
+              description="'FilenameBear' bear for coala (http://coala.rtfd.org/)",
+              authors={'The coala developers'},
+              authors_emails={'coala-devel@googlegroups.com'},
+              maintainers={'The coala developers'},
+              maintainers_emails={'coala-devel@googlegroups.com'},
+              platforms={'any'},
+              license='AGPL-3.0',
+              long_description="""None""",
+              classifiers=[
+                   "Development Status :: 4 - Beta",
+                   "Environment :: Console",
+                   "Environment :: Win32 (MS Windows)",
+                   "Intended Audience :: Science/Research",
+                   "Intended Audience :: Developers",
+                   "Programming Language :: "
+                        "Python :: Implementation :: CPython",
+                   "Programming Language :: Python :: 3.3",
+                   "Programming Language :: Python :: 3.4",
+                   "Programming Language :: Python :: 3.5",
+                   "Programming Language :: Python :: 3 :: Only",
+                   "Topic :: Scientific/Engineering :: Information Analysis",
+                   "Topic :: Software Development :: Quality Assurance",
+                   "Topic :: Text Processing :: Linguistic"])
+
+    finally:
+        print('[WARN] If you do not install the bears using the coala'
+              'installation tool, there may be problems with the dependencies'
+              'and they may not work.')
