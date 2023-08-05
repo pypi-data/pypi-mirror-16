2ba1db3df771d18600e90a1dc3e276ec3b297dec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:00 +0000

    Add package fischy/1.7.0/fischy-1.7.0.zip

---
+++ packages/fischy/fischy-1.7.0.zip/fischy-1.7.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name         = 'fischy',
+    version      = '1.7.0',
+    py_modules   = ['fischy'],
+    author       = 'fischy',
+    author_email = '563310255@qq.com',
+    url          = ' ',
+    description  = 'A sample printer of fischy lists',
+    )
