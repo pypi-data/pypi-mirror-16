588c6fe9e9c8c9125b9c91f4b46ea367d9089bdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:28 +0000

    Add package flash-services/0.3.3/flash_services-0.3.3-py3-none-any.whl

---
+++ packages/flash-services/flash_services-0.3.3-py3-none-any.whl/flash_services/core.py
@@ -0,0 +1,143 @@
+"""Core service description."""
+
+from abc import ABCMeta, abstractmethod
+from datetime import datetime
+from datetime import timezone
+from html import escape, unescape
+import logging
+from urllib.parse import urlencode
+
+from dateutil.parser import parse
+
+from .utils import remove_tags
+
+logger = logging.getLogger(__name__)
+
+
+class Service(metaclass=ABCMeta):
+    """Abstract base class for services."""
+
+    FRIENDLY_NAME = escape('<unnamed>')
+    """:py:class:`str`: The friendly name of the service."""
+
+    REQUIRED = set()
+    """:py:class:`set`: The service's required configuration keys."""
+
+    ROOT = ''
+    """:py:class:`str`: The root URL for the service API."""
+
+    TEMPLATE = 'undefined-section'
+    """:py:class:`str`: The name of the template to render."""
+
+    @abstractmethod
+    def __init__(self, *_, **kwargs):
+        self.service_name = kwargs.get('name')
+
+    @abstractmethod
+    def update(self):
+        """Get the current state to display on the dashboard."""
+        raise NotImplementedError
+
+    @property
+    def headers(self):
+        """Get the headers for the service requests."""
+        return {}
+
+    def url_builder(self, endpoint, *, root=None, params=None, url_params=None):
+        """Create a URL for the specified endpoint.
+
+        Arguments:
+          endpoint (:py:class:`str`): The API endpoint to access.
+          root: (:py:class:`str`, optional): The root URL for the
+            service API.
+          params: (:py:class:`dict`, optional): The values for format
+            into the created URL (defaults to ``None``).
+          url_params: (:py:class:`dict`, optional): Parameters to add
+            to the end of the URL (defaults to ``None``).
+
+        Returns:
+          :py:class:`str`: The resulting URL.
+
+        """
+        if root is None:
+            root = self.ROOT
+        return ''.join([
+            root,
+            endpoint,
+            '?' + urlencode(url_params) if url_params else '',
+        ]).format(**params or {})
+
+    @classmethod
+    def from_config(cls, **config):
+        """Manipulate the configuration settings."""
+        missing = cls.REQUIRED.difference(config)
+        if missing:
+            message = 'missing required config keys ({}) from {}'
+            raise TypeError(message.format(
+                ', '.join(missing),
+                unescape(cls.FRIENDLY_NAME),
+            ))
+        instance = cls(**config)
+        return instance
+
+    @staticmethod
+    def calculate_timeout(http_date):
+        """Extract request timeout from e.g. ``Retry-After`` header.
+
+        Notes:
+          Per :rfc:`2616#section-14.37`, the ``Retry-After`` header can
+          be either an integer number of seconds or an HTTP date. This
+          function can handle either.
+
+        Arguments:
+          http_date (:py:class:`str`): The date to parse.
+
+        Returns:
+          :py:class:`int`: The timeout, in seconds.
+
+        """
+        try:
+            return int(http_date)
+        except ValueError:
+            date_after = parse(http_date)
+        utc_now = datetime.now(tz=timezone.utc)
+        return int((date_after - utc_now).total_seconds())
+
+
+class ContinuousIntegrationService(Service):
+    """Service subclass for common CI behaviour."""
+
+    OUTCOMES = dict()
+    """:py:class:`dict`: Mapping from service to Flash outcomes."""
+
+    TEMPLATE = 'ci-section'
+
+    @classmethod
+    @abstractmethod
+    def format_build(cls, build):
+        outcome = build.get('outcome')
+        if outcome not in cls.OUTCOMES:
+            logger.warning('unknown outcome: %s', outcome)
+        return dict(
+            author=build.get('author', escape('<no author>')),
+            duration=build.get('duration'),
+            elapsed=build.get('elapsed'),
+            message=build.get('message', escape('<no message>')),
+            outcome=cls.OUTCOMES.get(outcome),
+            started_at=build.get('started_at'),
+        )
+
+
+class VersionControlService(Service):
+    """Service subclass for common (D)VCS behaviour."""
+
+    TEMPLATE = 'vcs-section'
+
+    @classmethod
+    @abstractmethod
+    def format_commit(cls, commit):
+        return dict(
+            author=commit.get('author', escape('<no author>')),
+            committed=commit.get('committed'),
+            message=remove_tags(commit.get('message', '')),
+        )
---
+++ packages/flash-services/flash_services-0.3.3-py3-none-any.whl/flash_services/github.py
@@ -0,0 +1,150 @@
+"""Defines the GitHub service integration."""
+
+import logging
+from collections import defaultdict, OrderedDict
+
+import requests
+
+from .auth import UrlParamMixin
+from .core import VersionControlService
+from .utils import occurred
+
+
+logger = logging.getLogger(__name__)
+
+
+class GitHub(UrlParamMixin, VersionControlService):
+    """Show the current status of a GitHub repository.
+
+    Arguments:
+      api_token (:py:class:`str`): A valid token for the GitHub API.
+      account (:py:class:`str`): The name of the account.
+      repo (:py:class:`str`): The name of the repository.
+      branch (:py:class:`str`, optional): The branch to get commit data
+        from.
+
+    Attributes:
+      repo_name (:py:class:`str`): The repository name, in the format
+        ``account/repo``.
+
+    """
+
+    AUTH_PARAM = 'access_token'
+    FRIENDLY_NAME = 'GitHub'
+    REQUIRED = {'api_token', 'account', 'repo'}
+    ROOT = 'https://api.github.com'
+
+    def __init__(self, *, api_token, account, repo, branch=None, **kwargs):
+        super().__init__(api_token=api_token, **kwargs)
+        self.account = account
+        self.repo = repo
+        self.branch = branch
+        self.repo_name = '{}/{}'.format(account, repo)
+
+    @property
+    def name(self):
+        """The full name of the repo, including branch if provided."""
+        if self.branch:
+            return '{} [{}]'.format(self.repo_name, self.branch)
+        return self.repo_name
+
+    @property
+    def headers(self):
+        headers = super().headers
+        headers['User-Agent'] = self.repo
+        return headers
+
+    def update(self):
+        logger.debug('fetching GitHub project data')
+        url_prm = OrderedDict(sha=self.branch) if self.branch else OrderedDict()
+        response = requests.get(
+            self.url_builder(
+                '/repos/{repo}/commits',
+                params={'repo': self.repo_name},
+                url_params=url_prm,
+            ),
+            headers=self.headers,
+        )
+        if response.status_code == 200:
+            return self.format_data(self.name, response.json())
+        logger.error('failed to update GitHub project data')
+        return {}
+
+    @classmethod
+    def format_data(cls, name, data):
+        """Re-format the response data for the front-end.
+
+        Arguments:
+          data (:py:class:`list`): The JSON data from the response.
+          name (:py:class:`str`): The name of the repository.
+
+        Returns:
+          :py:class:`dict`: The re-formatted data.
+
+        """
+        return dict(
+            commits=[cls.format_commit(commit.get('commit', {}))
+                     for commit in data[:5] or []],
+            name=name,
+        )
+
+    @classmethod
+    def format_commit(cls, commit):
+        """Re-format the commit data for the front-end.
+
+        Arguments:
+          commit (:py:class:`dict`): The JSON data from the response.
+
+        Returns:
+          :py:class:`dict`: The re-formatted data.
+
+        """
+        author = commit.get('author', {}).get('name')
+        committer = commit.get('committer', {}).get('name')
+        if author is None:
+            author_name = committer
+        elif committer is None or author == committer:
+            author_name = author
+        else:
+            author_name = '{} [{}]'.format(author, committer)
+        return super().format_commit(dict(
+            author=author_name,
+            committed=occurred(commit.get('committer', {}).get('date')),
+            message=commit.get('message', ''),
+        ))
+
+
+class GitHubIssues(GitHub):
+    """Show the current status of GitHub issues and pull requests."""
+
+    FRIENDLY_NAME = 'GitHub Issues'
+    TEMPLATE = 'gh-issues-section'
+
+    def update(self):
+        logger.debug('fetching GitHub issue data')
+        url_params = OrderedDict(state='all')
+        response = requests.get(
+            self.url_builder(
+                '/repos/{repo}/issues',
+                params={'repo': self.repo_name},
+                url_params=url_params,
+            ),
+            headers=self.headers,
+        )
+        if response.status_code == 200:
+            return self.format_data(self.name, response.json())
+        logger.error('failed to update GitHub issue data')
+        return {}
+
+    @classmethod
+    def format_data(cls, name, data):
+        counts = defaultdict(int)
+        for issue in data:
+            if issue.get('pull_request') is not None:
+                counts['{}-pull-requests'.format(issue['state'])] += 1
+            else:
+                counts['{}-issues'.format(issue['state'])] += 1
+        return dict(
+            issues=counts,
+            name=name,
+        )
