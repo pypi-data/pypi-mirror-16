2b8089f589a84832def8da0151c377b976a41438
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:11 +0000

    Add package coala-bears/0.8.0.dev20160713195741/coala_bears-0.8.0.dev20160713195741-py3-none-any.whl

---
+++ packages/coala-bears/coala_bears-0.8.0.dev20160713195741-py3-none-any.whl/bears/c_languages/CSharpLintBear.py
@@ -0,0 +1,23 @@
+from coalib.bearlib.abstractions.Linter import linter
+
+
+@linter(executable='mcs',
+        use_stdout=False,
+        use_stderr=True,
+        output_format='regex',
+        output_regex=r'.+\((?P<line>\d+),(?P<column>\d+)\): '
+                     r'(?P<severity>error|warning) \w+: (?P<message>.+)')
+class CSharpLintBear:
+    """
+    Checks C# code for syntactical correctness using the ``mcs`` compiler.
+    """
+
+    LANGUAGES = {"C#"}
+    AUTHORS = {'The coala developers'}
+    AUTHORS_EMAILS = {'coala-devel@googlegroups.com'}
+    LICENSE = 'AGPL-3.0'
+    CAN_DETECT = {'Syntax'}
+
+    @staticmethod
+    def create_arguments(filename, file, config_file):
+        return filename,
