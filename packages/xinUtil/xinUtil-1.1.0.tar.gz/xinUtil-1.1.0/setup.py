29ab545e137f72909d2d362610444bd49d09ab17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:27 +0000

    Add package xinUtil/1.1.0/xinUtil-1.1.0.tar.gz

---
+++ packages/xinUtil/xinUtil-1.1.0.tar.gz/xinUtil-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name        =  'xinUtil',
+    version     =  '1.1.0',
+    py_modules  = ['xinUtil'],
+    auther      = 'hxin',
+    auther_email = 'yuepeng.xin@gmail.com',
+    url         = 'http://null.com',
+    description = 'A simple printer of nested lists'
+)
