1b8b5b2ea95c16d66ebbb8f93230f608ecaca7a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:42 +0000

    Add package scalr-ctl/1.0.49/scalr-ctl-1.0.49.tar.gz

---
+++ packages/scalr-ctl/scalr-ctl-1.0.49.tar.gz/scalr-ctl-1.0.49/scalrctl/defaults.py
@@ -0,0 +1,8 @@
+__author__ = 'Dmitriy Korsakov'
+
+import os
+
+CONFIG_DIRECTORY = os.path.expanduser(os.environ.get("SCALRCLI_HOME", os.path.join(os.path.expanduser("~"), ".scalr")))
+CONFIG_PATH = os.path.join(CONFIG_DIRECTORY, "%s.yaml" % os.environ.get("SCALRCLI_PROFILE", "default"))
+VERSION = open(os.path.join(os.path.dirname(__file__), "VERSION")).read().strip()
+ROUTES_PATH = os.path.join(CONFIG_DIRECTORY, "available_api_routes.json")
