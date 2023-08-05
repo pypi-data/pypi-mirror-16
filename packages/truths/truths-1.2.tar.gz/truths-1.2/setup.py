e7204f1e9220b3f59dd93d6fe7da722bafdb5a59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:14 +0000

    Add package truths/1.2/truths-1.2.tar.gz

---
+++ packages/truths/truths-1.2.tar.gz/truths-1.2/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+#
+# Copyright 2013 Trey Morris
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+from setuptools import setup, find_packages
+
+
+setup(
+    name='truths',
+    version='1.2',
+    author='Trey Morris',
+    author_email='trey@treymorris.com',
+    description='auto generate truth tables',
+    long_description=open('README.rst').read(),
+    install_requires=['prettytable'],
+    classifiers=['Development Status :: 5 - Production/Stable',
+                 'License :: OSI Approved :: Apache Software License'],
+    keywords=['truth', 'table', 'truth table', 'truthtable', 'logic'],
+    packages=find_packages(),
+    license='Apache Software License',
+    url='https://github.com/tr3buchet/truths')
