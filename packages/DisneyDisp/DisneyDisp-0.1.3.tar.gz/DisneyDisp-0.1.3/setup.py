5a08b0ec93333a27a98c613d9aee51a76b30f7be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:58 +0000

    Add package DisneyDisp/0.1.3/DisneyDisp-0.1.3.tar.gz

---
+++ packages/DisneyDisp/DisneyDisp-0.1.3.tar.gz/DisneyDisp-0.1.3/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='DisneyDisp',
+      version='0.1.3',
+      description='A python implementation of the \"Scene Reconstruction from High Spatio-Angular Resolution Light Fields\". For more information see https://www.disneyresearch.com/project/lightfields/.',
+      url='https://github.com/manuSrep/DisneyDispPy.git',
+      author='Marcel Gutsche, Manuel Tuschen',
+      author_email='Manuel_Tuschen@web.de',
+      license='GPL3 License',
+      packages=['DisneyDisp'],
+      install_requires=["scipy","numpy","scikit-image","matplotlib","numba","h5py","progressbar2","easyScripting"],
+      zip_safe=False,
+      scripts=['DisneyDisp/disney.py', 'DisneyDisp/clif2lf.py', 'DisneyDisp/dmap2hdf5.py', 'DisneyDisp/imgs2lf.py', 'DisneyDisp/lf2epi.py']
+      )
