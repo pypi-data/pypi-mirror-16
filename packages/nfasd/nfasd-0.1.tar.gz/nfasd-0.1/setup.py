5335bb320ae77ec51b2beceb31caa3bc9765a346
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:20 +0000

    Add package nfasd/0.1/nfasd-0.1.tar.gz

---
+++ packages/nfasd/nfasd-0.1.tar.gz/nfasd-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='nfasd',
+      version='0.1',
+      description='fasd for neovim',
+      url='http://github.com/haifengkao/nfasd',
+      author='Hai Feng Kao',
+      author_email='haifeng@cocoaspice.in',
+      license='MIT',
+      packages=['nfasd'],
+      zip_safe=False)
