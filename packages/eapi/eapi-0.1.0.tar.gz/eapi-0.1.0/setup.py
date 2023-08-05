8f708419a08d64720dd1b469c12e6a3c48303bb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:47 +0000

    Add package eapi/0.1.0/eapi-0.1.0.tar.gz

---
+++ packages/eapi/eapi-0.1.0.tar.gz/eapi-0.1.0/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+
+# gemäß der Beschreibung von
+# https://packaging.python.org/distributing/
+setuptools.setup(
+    name="eapi",
+    version="0.1.0",
+    description="Modul zur Ansteuerung eines EA-Moduls fuer den Raspberry Pi.",
+    author="Marco Bakera")
