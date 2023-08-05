4794acee6b158291c870f1e33c97a93b51f5a825
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:53 +0000

    Add package nesjer/1.2.0/nesjer-1.2.0.tar.gz

---
+++ packages/nesjer/nesjer-1.2.0.tar.gz/nesjer-1.2.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name='nesjer',
+    version='1.2.0',
+    py_modules=['nesjer'],
+    author='hj',
+    author_email='johnhan324@gmail.com',
+    url='',
+    description='nest loop',
+    )
