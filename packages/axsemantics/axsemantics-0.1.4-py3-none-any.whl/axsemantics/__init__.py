ec8b8d7570ec11688634f2ef5eccc826fe692f21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:18 +0000

    Add package axsemantics/0.1.4/axsemantics-0.1.4-py3-none-any.whl

---
+++ packages/axsemantics/axsemantics-0.1.4-py3-none-any.whl/axsemantics/__init__.py
@@ -0,0 +1,16 @@
+# AX-Semantics Python bindings
+# API docs at https://apidocs.ax-semantics.com
+import axsemantics.constants
+from axsemantics.errors import (
+    APIConnectionError,
+    APIError,
+    AuthenticationError,
+)
+from axsemantics.resources import (
+    ContentProject,
+    ContentProjectList,
+    Thing,
+    Training,
+    TrainingList,
+)
+from axsemantics.utils import login
