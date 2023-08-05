3b789bc339eaa41bfce889d1e629bcad37f9ac78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:53 +0000

    Add package pypusu/1.0.2/pypusu-1.0.2.zip

---
+++ packages/pypusu/pypusu-1.0.2.zip/pypusu-1.0.2/setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+
+setup(
+    name='pypusu',
+    packages=['pypusu'],  # this must be the same as the name above
+    version='1.0.2',
+    description='Python client for PuSuEngine',
+    author='Janne Enberg',
+    author_email='janne.enberg@lietu.net',
+    url='https://github.com/PuSuEngine/pypusu',
+    download_url='https://github.com/PuSuEngine/pypusu/tarball/v1.0.2',
+    keywords=['pubsub', 'publisher', 'subscriber', 'messaging'],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved",
+        "Programming Language :: Python",
+        "Topic :: Communications :: Chat",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
