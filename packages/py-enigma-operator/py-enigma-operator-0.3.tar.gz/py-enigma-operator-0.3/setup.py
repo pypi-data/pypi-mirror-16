ac7642618e0d4a1d9290e1ac2eb0fce519cce7ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:11 +0000

    Add package py-enigma-operator/0.3/py-enigma-operator-0.3.tar.gz

---
+++ packages/py-enigma-operator/py-enigma-operator-0.3.tar.gz/py-enigma-operator-0.3/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+from distutils.core import setup
+
+
+setup(
+    name='py-enigma-operator',
+    packages=['py-enigma-operator'],
+    install_requires=['py-enigma'],
+    version='0.3',
+    description='Wrapper for py-enigma.',
+    author='Sean Gilleran',
+    author_email='sgilleran@gmail.com',
+    url='https://github.com/seangilleran/py_enigma_operator',
+    download_url='https://github.com/seangilleran/py_enigma_operator/tarball/0.3',
+    keywords=['enigma', 'encryption', 'historical'],
+    classifiers=[]
+)
