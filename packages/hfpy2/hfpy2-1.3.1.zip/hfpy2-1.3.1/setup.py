9f768531fd4455803a4bd5fbb61f23fcfe52d2c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:47 +0000

    Add package hfpy2/1.3.1/hfpy2-1.3.1.zip

---
+++ packages/hfpy2/hfpy2-1.3.1.zip/hfpy2-1.3.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+       name = 'hfpy2',
+       version = '1.3.1',
+       py_modules =  ['hfpy2'],
+       author = 'kaishen',
+       author_email = 'kaishen_ou1226@163.com',
+       url = 'http://www.163.com',
+       description = 'A simple printer',
+     )
