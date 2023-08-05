ad3192a2f692fe7aa652bbf8880e0f04f3279031
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:21 +0000

    Add package wagtailembedder/1.1/wagtailembedder-1.1.tar.gz

---
+++ packages/wagtailembedder/wagtailembedder-1.1.tar.gz/wagtailembedder-1.1/setup.py
@@ -0,0 +1,38 @@
+import os
+from setuptools import setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='wagtailembedder',
+    version='1.1',
+    packages=['wagtailembedder'],
+    include_package_data=True,
+    license='BSD License',
+    description='Snippets embedder for Wagtail RichTextField.',
+    long_description=README,
+    url='https://github.com/springload/wagtailembedder/',
+    author='Jordi J. Tablada',
+    author_email='jordi@springload.co.nz',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
