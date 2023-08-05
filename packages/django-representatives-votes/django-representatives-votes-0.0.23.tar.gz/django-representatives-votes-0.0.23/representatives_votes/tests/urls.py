a46a8c889707aa7ae8cb5301b7ca614b498fc816
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:32 +0000

    Add package django-representatives-votes/0.0.23/django-representatives-votes-0.0.23.tar.gz

---
+++ packages/django-representatives-votes/django-representatives-votes-0.0.23.tar.gz/django-representatives-votes-0.0.23/representatives_votes/tests/urls.py
@@ -0,0 +1,31 @@
+from django.conf.urls import include, url
+
+from rest_framework import routers
+
+from representatives_votes.api import (
+    DossierViewSet,
+    ProposalViewSet,
+    VoteViewSet,
+)
+
+from representatives.api import (
+    ChamberViewSet,
+    ConstituencyViewSet,
+    GroupViewSet,
+    MandateViewSet,
+    RepresentativeViewSet,
+)
+
+router = routers.DefaultRouter()
+router.register(r'chambers', ChamberViewSet)
+router.register(r'constituencies', ConstituencyViewSet)
+router.register(r'groups', GroupViewSet)
+router.register(r'mandates', MandateViewSet)
+router.register(r'representatives', RepresentativeViewSet)
+router.register(r'dossiers', DossierViewSet)
+router.register(r'proposals', ProposalViewSet)
+router.register(r'votes', VoteViewSet)
+
+urlpatterns = [
+    url(r'api/', include(router.urls)),
+]
