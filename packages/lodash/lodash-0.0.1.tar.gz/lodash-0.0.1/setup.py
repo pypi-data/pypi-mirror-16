6f268a01e07c49b39e3d9b79f968b3e03fbce0e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:16 +0000

    Add package lodash/0.0.1/lodash-0.0.1.tar.gz

---
+++ packages/lodash/lodash-0.0.1.tar.gz/lodash-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+	name='lodash',
+	packages=['lodash'],
+	version='0.0.1',
+	description='python implementation for lodash',
+	author='ironmaniiith',
+	author_email='aalekhj2507@gmail.com',
+	url='https://github.com/ironmaniiith',
+)
