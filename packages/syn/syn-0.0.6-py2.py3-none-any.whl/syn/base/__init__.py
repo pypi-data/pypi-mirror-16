d6cac7ea406cfe0bc4b6558d7bc93f5ad3342273
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:43 +0000

    Add package syn/0.0.6/syn-0.0.6-py2.py3-none-any.whl

---
+++ packages/syn/syn-0.0.6-py2.py3-none-any.whl/syn/base/__init__.py
@@ -0,0 +1,5 @@
+from .b import Base, Attr, Attrs, This
+from .b import ListWrapper
+from .b import create_hook, init_hook, coerce_hook, setstate_hook
+from .b import Counter
+from .b import check_idempotence
---
+++ packages/syn/syn-0.0.6-py2.py3-none-any.whl/syn/base_utils/__init__.py
@@ -0,0 +1,7 @@
+from .hash import *
+from .dict import *
+from .py import *
+from .filters import *
+from .context import *
+from .str import *
+from .tree import *
