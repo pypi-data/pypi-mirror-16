d56bbf6fa6dc5331a0080a85741eaf0878b2cf7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:40 +0000

    Add package redpanda/0.2.0/redpanda-0.2.0.tar.gz

---
+++ packages/redpanda/redpanda-0.2.0.tar.gz/redpanda-0.2.0/redpanda/__init__.py
@@ -0,0 +1,11 @@
+""" Pandas-ORM Integration. """
+
+
+import pandas
+import sqlalchemy
+from sqlalchemy import create_engine
+from .orm import Session
+
+
+__author__ = "amancevice"
+__version__ = "0.2.0"
