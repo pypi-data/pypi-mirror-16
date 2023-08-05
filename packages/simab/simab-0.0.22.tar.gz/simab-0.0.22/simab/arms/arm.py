8e272860ddd3deaa2e0cc0458ef335ca85f35b95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:44 +0000

    Add package simab/0.0.22/simab-0.0.22.tar.gz

---
+++ packages/simab/simab-0.0.22.tar.gz/simab-0.0.22/simab/arms/arm.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+import uuid
+
+class Arm(object):
+    """ Abstract Arm class.
+
+    :param label: labels of the arm. Defaulted to an UUID.
+    """
+    def __init__(self, label=None):
+        self.label = label or str(uuid.uuid4())
+        self.prediction = None
+        self.is_predicted = False
+        self.count = 0
+
+    def pick(self):
+        """ Pick a reward from prediction only if prediction exists.
+        """
+        if self.is_predicted:
+            reward = self.prediction[self.count]
+        else:
+            reward = None
+        self.count += 1
+        return reward
+
+    def predict(self, max_rounds=1000, prediction=[]):
+        """ Set designated number of values as a prediction.
+        This method is used if you want to compare the performance of each algorithm to oracle.
+
+        :param max_rounds: maximum number of rounds to predict
+        :param prediction: float list of prediction
+        """
+        if prediction and len(prediction)>0:
+            self.prediction = prediction
+        else:
+            self.prediction = [self.pick() for _ in range(max_rounds)]
+        self.is_predicted = True
+
+
