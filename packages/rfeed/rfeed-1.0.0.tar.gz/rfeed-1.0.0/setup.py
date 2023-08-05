76604c984286c7a1b290fccf04bc403fe662b9fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:04 +0000

    Add package rfeed/1.0.0/rfeed-1.0.0.tar.gz

---
+++ packages/rfeed/rfeed-1.0.0.tar.gz/rfeed-1.0.0/setup.py
@@ -0,0 +1,29 @@
+from distutils.core import setup
+
+setup(
+    name="rfeed",
+    packages=['rfeed'],
+    version="1.0.0",
+    description="Python RSS 2.0 Generator",
+    author="Santiago L. Valdarrama",
+    author_email="svpino@gmail.com",
+    maintainer="Egor Smolyakov",
+    maintainer_email="egorsmkv@gmail.com",
+    url="https://github.com/egorsmkv/rfeed",
+    keywords=['feed', 'rss'],
+    license="MIT",
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Communications',
+        'Topic :: Internet',
+        'Topic :: Text Processing',
+        'Topic :: Text Processing :: Markup',
+        'Topic :: Text Processing :: Markup :: XML'
+    ]
+)
