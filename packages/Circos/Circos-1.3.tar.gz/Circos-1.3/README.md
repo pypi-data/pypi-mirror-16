5f40489a823fc63e23bd4f0e183211922367ed44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:25 +0000

    Add package Circos/1.3/Circos-1.3.tar.gz

---
+++ packages/Circos/Circos-1.3.tar.gz/Circos-1.3/README.md
@@ -0,0 +1,11 @@
+# Circos
+
+A utility for making circos plots in matplotlib.
+
+URL: circos.ca
+
+## Changelog
+
+Version 1.2:
+
+1. Fixed `setup.py` script.
---
+++ packages/Circos/Circos-1.3.tar.gz/Circos-1.3/setup.py
@@ -0,0 +1,30 @@
+import os
+from setuptools import setup, find_packages
+from pip.req import parse_requirements
+
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+install_reqs = parse_requirements('requirements.txt', session=False)
+reqs = [str(ir.req) for ir in install_reqs]
+
+setup(name="Circos",
+      version="1.3",
+      author="Eric J. Ma, Justin Zabilansky, Jon Charest",
+      author_email="ericmajinglong@gmail.com",
+      description=("Circos plots in Python!"),
+      license="MIT",
+      keywords="network visualization, matplotlib, circos",
+      url="http://github.com/ericmjl/Circos",
+      # packages=find_packages(),
+      py_modules=['circos'],
+      # package_data={'': ['README.md', 'requirements.txt']},
+      install_requires=reqs,
+      long_description=read('README.md'),
+      classifiers=["Topic :: Scientific/Engineering :: Visualization"],
+      )
