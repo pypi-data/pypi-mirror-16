672ef75946e29349a4ce8809d4701770dcc23c43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:43 +0000

    Add package thorn/1.3.0/thorn-1.3.0-py2.py3-none-any.whl

---
+++ packages/thorn/thorn-1.3.0-py2.py3-none-any.whl/thorn/tasks.py
@@ -0,0 +1,61 @@
+"""
+
+    thorn.tasks
+    ===========
+
+    Tasks used by the Celery dispatcher.
+
+"""
+from __future__ import absolute_import, unicode_literals
+
+from celery import shared_task
+from celery.utils.functional import memoize
+
+from ._state import app_or_default
+
+__all__ = ['send_event', 'dispatch_requests', 'dispatch_request']
+
+
+@memoize()
+def _worker_dispatcher():
+    from .dispatch.celery import WorkerDispatcher
+    return WorkerDispatcher()
+
+
+@shared_task(ignore_result=True)
+def send_event(event, payload, sender, timeout, context={}):
+    """Task called by process dispatching the event.
+
+    This will use the WorkerDispatcher to dispatch the individual
+    HTTP requests in batches (``dispatch_requests -> dispatch_request``).
+
+    """
+    _worker_dispatcher().send(
+        event, payload, sender, timeout=timeout, context=context)
+
+
+@shared_task(ignore_result=True)
+def dispatch_requests(reqs, app=None):
+    """Process a batch of HTTP requests."""
+    app = app_or_default(app)
+    session = app.Request.Session()
+    [dispatch_request(session=session, app=app, **req) for req in reqs]
+
+
+@shared_task(bind=True, ignore_result=True)
+def dispatch_request(self, event, data, sender, subscriber,
+                     session=None, app=None, **kwargs):
+    """Process a single HTTP request."""
+    app = app_or_default(app)
+    # the user is serialized as the pk, so we cannot pass it
+    # directly to Subscriber, but we also don't need it at this point.
+    subscriber.pop('user', None)
+    subscriber = app.Subscriber(**subscriber)
+    request = app.Request(event, data, sender, subscriber, **kwargs)
+    try:
+        request.dispatch(session=session, propagate=request.retry)
+    except request.connection_errors + request.timeout_errors as exc:
+        if request.retry:
+            raise self.retry(exc=exc, max_retries=request.retry_max,
+                             countdown=request.retry_delay)
+        raise
---
+++ packages/thorn/thorn-1.3.0-py2.py3-none-any.whl/thorn/utils/hmac.py
@@ -0,0 +1,56 @@
+"""
+
+    thorn.utils.hmac
+    ================
+
+    HMAC Message signing utilities.
+
+"""
+from __future__ import absolute_import, unicode_literals
+
+import base64
+import hashlib
+import hmac
+import random
+import string
+
+from .compat import bytes_if_py3, to_bytes
+
+try:
+    import itsdangerous
+except ImportError:  # pragma: no cover
+    itsdangerous = None  # noqa
+
+# Some version of PyPy does not have hashlib.algorithms_available
+allowed_algorithms = {
+    'sha1', 'sha224', 'sha256', 'sha384', 'sha512',
+}
+
+punctuation = string.punctuation.replace('"', '').replace("'", '')
+
+
+def get_digest(d):
+    assert d.lower() in allowed_algorithms
+    return getattr(hashlib, d.lower())
+
+
+def sign(digest_method, key, message):
+    return base64.b64encode(bytes_if_py3(hmac.new(
+        to_bytes(key),
+        to_bytes(message),
+        digestmod=get_digest(digest_method)).digest()))
+
+
+def verify(digest, digest_method, key, message):
+    return hmac.compare_digest(sign(digest_method, key, message), digest)
+
+
+def random_secret(
+        length, chars=string.ascii_letters + string.digits + punctuation):
+    return ''.join(random.choice(chars) for _ in range(length))
+
+
+def compat_sign(digest_method, key, message):
+    return itsdangerous.Signer(
+        key, digest_method=get_digest(digest_method),
+    ).get_signature(message)
