a597826718d4b32f54d4b8ce3012e2e59b263ace
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:22 +0000

    Add package otree-core/0.7.6/otree-core-0.7.6.zip

---
+++ packages/otree-core/otree-core-0.7.6.zip/otree-core-0.7.6/otree/asgi.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import  # for channels module
+import os
+import channels.asgi
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+
+from otree.common_internal import release_any_stale_locks  # noqa
+release_any_stale_locks()
+
+# clear any tasks in Huey DB, so they don't pile up over time,
+# especially if you run the server without the timeoutworker to consume the
+# tasks.
+# ideally we would only schedule a task in Huey if timeoutworker is running,
+# so that we don't pile up messages that never get consumed, but I don't know
+# how and when to check if Huey is running, in a performant way.
+# this code is also in timeoutworker.
+from huey.contrib.djhuey import HUEY  # noqa
+HUEY.flush()
+
+channel_layer = channels.asgi.get_channel_layer()
