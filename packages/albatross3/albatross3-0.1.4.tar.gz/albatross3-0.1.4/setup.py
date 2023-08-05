0bc1f558fc3486188fafa2ca56812b21759b0c01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:11 +0000

    Add package albatross3/0.1.4/albatross3-0.1.4.tar.gz

---
+++ packages/albatross3/albatross3-0.1.4.tar.gz/albatross3-0.1.4/setup.py
@@ -0,0 +1,42 @@
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='albatross3',
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # https://packaging.python.org/en/latest/single_source_version.html
+    version='0.1.4',
+    description='A modern async python3 web framework',
+    long_description=long_description,
+    url='https://github.com/kespindler/albatross',
+    author='Kurt Spindler',
+    author_email='kespindler@gmail.com',
+    license='MIT',
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.5',
+    ],
+    keywords='web http server async',
+    packages=['albatross'],
+    install_requires=[],
+    extras_require={
+        'dev': [],
+        'test': [],
+    },
+)
