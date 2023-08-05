d4efde66e5309ee4d6fd07b12e8fe9d4cb9b567c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:56 +0000

    Add package simpletex/0.1.2/simpletex-0.1.2.tar.gz

---
+++ packages/simpletex/simpletex-0.1.2.tar.gz/simpletex-0.1.2/simpletex/formatting/core.py
@@ -0,0 +1,16 @@
+from simpletex.core import Formatter
+
+
+class Indent(Formatter):
+    def __init__(self):
+        super().__init__()
+
+    def __call__(self, text):
+        lines = str(text).split('\n')
+        return '\n'.join(map(self._tab_line, lines))
+
+    @staticmethod
+    def _tab_line(text):
+        if text != '':
+            text = '\t' + text
+        return text
