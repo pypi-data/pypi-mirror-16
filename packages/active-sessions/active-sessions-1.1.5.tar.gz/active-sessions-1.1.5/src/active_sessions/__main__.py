23b348683d66c7e9dec6c0ba7540a6f59df7a1de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:38 +0000

    Add package active-sessions/1.1.5/active-sessions-1.1.5.tar.gz

---
+++ packages/active-sessions/active-sessions-1.1.5.tar.gz/active-sessions-1.1.5/src/active_sessions/__main__.py
@@ -0,0 +1,14 @@
+import argparse
+import active_sessions
+
+# Create an argument parser.
+parser = argparse.ArgumentParser('active-sessions')
+
+
+def main(args=None):
+    """Main entry point for your project.
+    """
+
+
+if __name__ == '__main__':
+    main(['-h'])
