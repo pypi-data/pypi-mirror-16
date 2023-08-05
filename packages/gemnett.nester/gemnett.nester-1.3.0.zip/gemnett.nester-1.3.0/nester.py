78bb4aa95ce3cf2702f9e4e12aaba368e24682b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:59 +0000

    Add package gemnett.nester/1.3.0/gemnett.nester-1.3.0.zip

---
+++ packages/gemnett.nester/gemnett.nester-1.3.0.zip/gemnett.nester-1.3.0/nester.py
@@ -0,0 +1,19 @@
+"""This is the "nester.py" module, and it provides one function called
+print_lol() which prints lists that may or not include nested lists."""
+
+def print_lol(the_list, indent=False, level=0):
+    """This function takes a positional argument called "the_listed", which is any
+    Python list (of, possibly, nested lists). Each data item in the provided list
+    is (recursively) printed to the screen on its own line."""
+    for each_item in the_list:
+        if isinstance(each_item, list):
+            print_lol(each_item, indent, level + 1)
+        else:
+            if indent:
+                for tab_stop in range(level):
+                    print("\t", end='')
+            print(each_item)
+"""movies = [ "The Holy Grail", 1975, "Terry Jones & Terry Gilliam",
+91,["Graham Chapman", ["Michael Palin",
+"John Cleese", "Terry Gilliam", "Eric Idle",
+"Terry Jones"]]]"""
