0b157407da9b288dd9b81c755f00226181295b27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:10:09 +0000

    Add package enhydris-synoptic/0.1.0/enhydris-synoptic-0.1.0.tar.gz

---
+++ packages/enhydris-synoptic/enhydris-synoptic-0.1.0.tar.gz/enhydris-synoptic-0.1.0/enhydris_synoptic/celery.py
@@ -0,0 +1,12 @@
+import os
+
+from celery import Celery
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+
+from django.conf import settings
+
+app = Celery('enhydris_synoptic')
+
+app.config_from_object('django.conf:settings')
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
