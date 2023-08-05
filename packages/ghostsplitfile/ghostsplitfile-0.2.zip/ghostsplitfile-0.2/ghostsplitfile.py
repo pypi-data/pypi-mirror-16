c5e6bc964b3bf5c8307a9803609b9979b510fa48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:10:02 +0000

    Add package ghostsplitfile/0.2/ghostsplitfile-0.2.zip

---
+++ packages/ghostsplitfile/ghostsplitfile-0.2.zip/ghostsplitfile-0.2/ghostsplitfile.py
@@ -0,0 +1,27 @@
+"""This is the "ghostsplitfile.py" module and it provide one function called split_file() which splits document by separator and prints."""
+
+import os
+def split_file( file_name, split_char=':' ):
+    """
+    This function takes one positional argument called "file_name", which is the local document to print.
+    It takes another positional argument called "split_char", which is the separator to splits the document with default value ':'
+    Each data item in the provided local document is (recursively) printed to the screen on it's own line.
+    """
+    #file_name = 'xxx\python\chapter3\sketch.txt'
+    #split_char = ':'
+    try:
+        #os.chdir( 'xxx\python\chapter3' )
+        data = open( file_name )
+        data.seek( 0 ) 
+        for each_line in data:
+            print( each_line )
+        try:
+            ( role, line_spoken ) = each_line.split( split_char )
+            print( role, end='' )
+            print( ' said: ', end='' )
+            print( line_spoken )
+        except:
+            pass
+        data.close()    
+    except:
+        print( 'The data file is missing!' )
---
+++ packages/ghostsplitfile/ghostsplitfile-0.2.zip/ghostsplitfile-0.2/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup  
+setup(  
+        name = 'ghostsplitfile',  
+        version = '0.2',  
+        py_modules = ['ghostsplitfile'],  
+        author = 'ghostbehemoth',  
+        author_email = 'ghostbehemoth@gmail.com',  
+        url = 'http://www.pythonphp.com',  
+        description = 'A printer of local document by separator',  
+    )  
