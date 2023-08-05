64d9a122893f46427916c7448e71ee9d3b6e62f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:32 +0000

    Add package jupynbimp/0.1/jupynbimp-0.1.tar.gz

---
+++ packages/jupynbimp/jupynbimp-0.1.tar.gz/jupynbimp-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='jupynbimp',
+      version='0.1',
+      description='import jupyter notebooks as modules',
+      url='http://github.com/dcal12/jupynbimp',
+      author='Doug Callaway',
+      author_email='DCAL12@users.noreply.github.com',
+      license='MIT',
+      packages=['jupynbimp'],
+      zip_safe=False)
