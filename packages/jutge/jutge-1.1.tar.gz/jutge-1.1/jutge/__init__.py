06c03d14868c7317cb7b6b3f3ff11e28ac7d2381
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:03 +0000

    Add package jutge/1.1/jutge-1.1.tar.gz

---
+++ packages/jutge/jutge-1.1.tar.gz/jutge-1.1/jutge/__init__.py
@@ -0,0 +1,25 @@
+
+import sys
+
+def tokenizer():
+    for line in sys.stdin.readlines():
+        for word in line.split():
+            yield word
+    while True:
+        yield None
+
+tokens = tokenizer()
+
+def read (*types):
+    if len(types) == 0:
+        token = next(tokens)
+        if token is None: return None
+        return token
+    else:
+        result = []
+        for typ in types:
+            token = next(tokens)
+            if token is None: result.append(None)
+            else: result.append(typ(token))
+        return result
+
