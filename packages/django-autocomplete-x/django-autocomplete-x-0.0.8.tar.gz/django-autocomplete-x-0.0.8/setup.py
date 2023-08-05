e9892b27e6fffa170af8b49c859823bc9d3a2f74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:11 +0000

    Add package django-autocomplete-x/0.0.8/django-autocomplete-x-0.0.8.tar.gz

---
+++ packages/django-autocomplete-x/django-autocomplete-x-0.0.8.tar.gz/django-autocomplete-x-0.0.8/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+
+# version (shape.function.path)
+# python setup.py sdist upload
+setup(
+	name='django-autocomplete-x',
+	version='0.0.8',
+	packages=['django_autocomplete', ],
+	url='https://github.com/exildev/django-autocomplete',
+	author="Luis Miguel Morales Pajaro",
+	author_email="luismiguel.mopa@gmail.com",
+	licence="Creative Common",
+	description="django autocomplete plugin",
+	platform="Linux",
+	zip_safe=False,
+	include_package_data=True,
+	package_data={'django_autocomplete': [
+		'static/django_autocomplete/css/*.css',
+		'static/django_autocomplete/js/*.js'
+	]},
+)
