d35d931b07786b585763a03478d247f674024308
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:24 +0000

    Add package flupan/0.0.4/flupan-0.0.4.tar.gz

---
+++ packages/flupan/flupan-0.0.4.tar.gz/flupan-0.0.4/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='flupan',
+      version='0.0.4',
+      description='Python library to parse influenza passaging annotations',
+      url='http://github.com/clauswilke/flupan',
+      author='Claire D. McWhite',
+      author_email='claire.mcwhite@utexas.edu',
+      license='MIT',
+      scripts=['src/translate_passage'],
+      test_suite='nose.collector',
+      tests_require=['nose']
+     )
