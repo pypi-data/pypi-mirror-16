e380c6a69b948c9983c906939a347b31979f3958
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:16 +0000

    Add package aaaarg-python/0.12/aaaarg-python-0.12.tar.gz

---
+++ packages/aaaarg-python/aaaarg-python-0.12.tar.gz/aaaarg-python-0.12/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(name='aaaarg-python',
+      version='0.12',
+      description='Python interface for aaaarg',
+      url='http://github.com/metadada/aaaarg-python',
+      author='metadada',
+      author_email='metadadametadada@gmail.com',
+      license='GPL3',
+      packages=['aaaarg'],
+      install_requires=[
+          'requests',
+          'lxml'
+      ],
+      classifiers=[
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+      ],
+      zip_safe=False)
