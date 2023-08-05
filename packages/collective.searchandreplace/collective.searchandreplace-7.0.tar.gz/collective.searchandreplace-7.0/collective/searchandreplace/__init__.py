d5d4d128e5dc604c3b0c72d013c221dce8d1a14f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:10 +0000

    Add package collective.searchandreplace/7.0/collective.searchandreplace-7.0.tar.gz

---
+++ packages/collective.searchandreplace/collective.searchandreplace-7.0.tar.gz/collective.searchandreplace-7.0/collective/searchandreplace/__init__.py
@@ -0,0 +1,7 @@
+from zope.i18nmessageid import MessageFactory
+
+# Define permission string, so we can import it if needed.  See also
+# the permission in configure.zcml.
+UseSearchAndReplace = "collective.searchandreplace: Use Search And Replace"
+# Message factory:
+SearchAndReplaceMessageFactory = MessageFactory('SearchAndReplace')
