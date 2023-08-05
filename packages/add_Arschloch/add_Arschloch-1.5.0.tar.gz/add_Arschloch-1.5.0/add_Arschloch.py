0f5b2629a763995b682753edc4d4324767db00ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:02 +0000

    Add package add_Arschloch/1.5.0/add_Arschloch-1.5.0.tar.gz

---
+++ packages/add_Arschloch/add_Arschloch-1.5.0.tar.gz/add_Arschloch-1.5.0/add_Arschloch.py
@@ -0,0 +1,21 @@
+""" Dies ist das Modul "add_Arschloch". Es gibt Listen aus, welche sich in
+Listen verbergen. Jeweils ein Objekt pro Zeile. Dabei wird für jedes Listen-Objekt
+das Wort "Arschloch" hinzugefügt. Mit dem zweiten Argument "ebene" kann bestimmt
+werden, wie weit die nächste Liste jeweils engerückt werden soll."""
+
+tl = ["Andi","Ivi",[666,"Burger","Hamlet",[777,"Schawul","Beni"]]]
+tl.append("handtuch")
+tl.insert(1,"tu es")
+
+def print_auspack(liste,einzug = False, ebene = 0):
+    for teil_liste in liste:
+        if isinstance(teil_liste, list):
+            print_auspack(teil_liste, einzug, ebene + 2)
+            
+        else:
+            if einzug == True:
+                for anzahl in range(ebene):
+                    print("\t",end='')
+            print(teil_liste,"Arschloch") 
+           
+        
---
+++ packages/add_Arschloch/add_Arschloch-1.5.0.tar.gz/add_Arschloch-1.5.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name          = 'add_Arschloch',
+    version       = '1.5.0',
+    py_modules    = ['add_Arschloch'],
+    author        = 'Zensurist',
+    author_email  = 'zensurist@gmx.ch',
+    url           = 'http://www.zensurist.ch',
+    description   = 'Einfache Ausgabe geschachtelter Listen mit Arschloch',
+    )
