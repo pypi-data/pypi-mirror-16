08fe2764a725a08d5dd405047219595d9206bca1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:20 +0000

    Add package tolly/1.0.0/tolly-1.0.0.zip

---
+++ packages/tolly/tolly-1.0.0.zip/tolly-1.0.0/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+
+
+setup(
+        name         = 'tolly',
+        
+        version      = '1.0.0',
+        
+        py_modules   = ['tolly'],
+        
+        author       = 'takinboro',
+        
+        author_email = 'tolaakinboro@gmail.com',
+        
+        url          = 'http://www.tolaakinboro.com',
+        
+        description  = 'A simple printer of nested lists',
+
+
+      )
