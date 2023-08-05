aa05ac1258bd1df3a00c7755ac156f64c56b0e9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:36 +0000

    Add package nesterSad/1.2.0/nesterSad-1.2.0.zip

---
+++ packages/nesterSad/nesterSad-1.2.0.zip/nesterSad-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name            = 'nesterSad',
+    version         = '1.2.0',
+    py_modules      = ['nester'],
+    author          = 'Etienne Desjardins',
+    author_email    = 'etienne.desjardins@hotmail.com',
+    url             = '',
+    description     = 'A simple printer of nested lists',
+    )
