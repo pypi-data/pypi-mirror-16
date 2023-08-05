f5adcf14785013bdc1e2fd898c563bc73ac20ccf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:04 +0000

    Add package lists_prints12/1.1.0/lists_prints12-1.1.0.zip

---
+++ packages/lists_prints12/lists_prints12-1.1.0.zip/lists_prints12-1.1.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup       #从发布工具导入setup函数
+
+setup(
+    name        ='lists_prints12',
+    version     ='1.1.0',
+    py_modules  =['lists_prints12'],
+    author      ='Steve Cater',
+    author_email='1765175915@qq.com',
+    url         ='http://www.headfirstlabs.com',
+    description ='A simple printer of nested lists',
+    )
+
