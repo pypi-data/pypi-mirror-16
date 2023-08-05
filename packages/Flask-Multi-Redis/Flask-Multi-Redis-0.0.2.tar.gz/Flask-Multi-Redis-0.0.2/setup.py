5a4b54f55b1c50c8a4a6b35ea54d420e5be7c65a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:57 +0000

    Add package Flask-Multi-Redis/0.0.2/Flask-Multi-Redis-0.0.2.tar.gz

---
+++ packages/Flask-Multi-Redis/Flask-Multi-Redis-0.0.2.tar.gz/Flask-Multi-Redis-0.0.2/setup.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+import io
+
+from setuptools import setup
+
+with io.open('README.rst', encoding='utf-8') as f:
+    README = f.read()
+with io.open('HISTORY.rst', encoding='utf-8') as f:
+    HISTORY = f.read()
+
+DESC = "MultiThreaded and MultiServers Redis Extension for Flask Applications"
+LICENSE = "GNU Affero General Public License v3 or later (AGPLv3+)"
+
+setup(
+    name='Flask-Multi-Redis',
+    version='0.0.2',
+    url='https://github.com/max-k/flask-multi-redis',
+    author='Thomas Sarboni',
+    author_email='max-k@post.com',
+    maintainer='Thomas Sarboni',
+    maintainer_email='max-k@post.com',
+    download_url='https://github.com/max-k/flask-multi-redis/releases',
+    description=DESC,
+    long_description=README + '\n\n' + HISTORY,
+    packages=['flask_multi_redis'],
+    package_data={'': ['LICENSE']},
+    zip_safe=False,
+    install_requires=[
+        'Flask>=0.9',
+        'redis>=2.6.8',
+        'more-itertools>=2.1'
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Flask',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: {}'.format(LICENSE),
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
