29497975f3bac12ebb4084d20dc19a4c918232dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:16 +0000

    Add package coala-bears/0.8.0.dev20160720120217/coala-bears-0.8.0.dev20160720120217.tar.gz

---
+++ packages/coala-bears/coala-bears-0.8.0.dev20160720120217.tar.gz/coala-bears-0.8.0.dev20160720120217/bears/latex/LatexLintBear.py
@@ -0,0 +1,20 @@
+from coalib.bearlib.abstractions.Linter import linter
+
+
+@linter(executable='chktex',
+        output_format='regex',
+        output_regex=r'(?P<severity>Error|Warning) \d+ in .+ line '
+                     r'(?P<line>\d+): (?P<message>.*)')
+class LatexLintBear:
+    """
+    Checks the code with ``chktex``.
+    """
+    LANGUAGES = {"Tex"}
+    AUTHORS = {'The coala developers'}
+    AUTHORS_EMAILS = {'coala-devel@googlegroups.com'}
+    LICENSE = 'AGPL-3.0'
+    CAN_DETECT = {'Syntax', 'Formatting'}
+
+    @staticmethod
+    def create_arguments(filename, file, config_file):
+        return filename,
