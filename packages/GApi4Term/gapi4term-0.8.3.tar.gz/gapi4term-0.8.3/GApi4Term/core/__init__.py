c6a4e39004ef9beede762fd06ed0c188df735aa1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:35 +0000

    Add package GApi4Term/0.8.3/gapi4term-0.8.3.tar.gz

---
+++ packages/GApi4Term/gapi4term-0.8.3.tar.gz/gapi4term-0.8.3/GApi4Term/core/__init__.py
@@ -0,0 +1,16 @@
+from configstore import Config
+from gmail import GMail
+from drive import Drive
+from oauth import GoogleAccounts
+from urlshortener import URLShortener
+from tasks import Tasks
+
+__all__ = [
+        Config,
+        GMail,
+        Drive,
+        GoogleAccounts,
+        URLShortener,
+        Tasks
+]
+
