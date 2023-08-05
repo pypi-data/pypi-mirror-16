2ce6ba1dd120f19e95f6fc950232376ef38e66e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:06 +0000

    Add package OpenREM/0.7.2b1/OpenREM-0.7.2b1.tar.gz

---
+++ packages/OpenREM/OpenREM-0.7.2b1.tar.gz/OpenREM-0.7.2b1/openrem/remapp/static/js/chartFullScreen.js
@@ -0,0 +1,29 @@
+var chartStartHeight = 0;
+var chartStartFooterHeight = 0;
+var chartFullScreen = false;
+
+function enterFullScreen(div_id, chart_div) {
+    if (chartFullScreen == false) {
+        chartStartHeight = document.getElementById(chart_div).clientHeight;
+        chartStartFooterHeight = document.getElementById(div_id).clientHeight - chartStartHeight;
+        document.getElementById(chart_div).style.height = $(window).height() - chartStartFooterHeight + 'px';
+        chartFullScreen = true;
+    }
+    else {
+        document.getElementById(chart_div).style.height = chartStartHeight + 'px';
+        chartFullScreen = false;
+    }
+
+    $('#'+div_id).toggleClass('fullscreen');
+
+    var chart = $('#'+chart_div).highcharts();
+    chart.setSize($('#'+chart_div).width(), $('#'+chart_div).height());
+}
+
+
+function fitChartToDiv(chart_div) {
+    if ($('#' + chart_div).width() && $('#' + chart_div).height()) {
+        var chart = $('#' + chart_div).highcharts();
+        chart.setSize($('#' + chart_div).width(), $('#' + chart_div).height());
+    }
+}
