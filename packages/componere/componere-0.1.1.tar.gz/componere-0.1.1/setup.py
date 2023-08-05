1646429ea37822df55021fa78d13e8eaa79b616d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:34 +0000

    Add package componere/0.1.1/componere-0.1.1.tar.gz

---
+++ packages/componere/componere-0.1.1.tar.gz/componere-0.1.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(name='componere',
+      version='0.1.1',
+      url='https://github.com/premisedata/componere',
+      author='Moustafa Maher and Mostafa Gabriel',
+      author_email='mmaher@premise.com, mgabriel@premise.com',
+      packages=find_packages(),
+      install_requires=['graphviz', 'pyyaml'])
