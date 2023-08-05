570ba9cadc3746bcd58a3880d84157523e52365f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:50 +0000

    Add package pystl/0.1.0/pystl-0.1.0.zip

---
+++ packages/pystl/pystl-0.1.0.zip/pystl-0.1.0/setup.py
@@ -0,0 +1,19 @@
+
+from distutils.core import setup
+
+setup(
+    name = 'pystl',
+    packages = ['pystl'],
+    version = '0.1.0',
+    description = 'Simple Python package to write STL files for 3D printing.',
+    author='Len Wanger',
+    author_email='len_wanger@hotmail.com',
+    url='https://github.com/lwanger/pystl',
+    license= 'MIT',
+    keywords = 'STL stereolithography, 3D printing',
+    classifiers = [ 'License :: OSI Approved :: MIT License', 'Development Status :: 5 - Production/Stable',
+                    'Operating System :: OS Independent', 'Programming Language :: Python',
+                    'Programming Language :: Python :: 3',
+                    'Intended Audience :: Developers',
+                    'Topic :: Multimedia :: Graphics :: 3D Modeling'],
+)
