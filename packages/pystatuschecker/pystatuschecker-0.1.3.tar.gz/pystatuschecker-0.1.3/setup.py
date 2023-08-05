e74b131a0ec4d5baebc104af505fd93bd2483e2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:21 +0000

    Add package pystatuschecker/0.1.3/pystatuschecker-0.1.3.tar.gz

---
+++ packages/pystatuschecker/pystatuschecker-0.1.3.tar.gz/pystatuschecker-0.1.3/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+requirements = []
+test_requirements = ['unittest2']
+
+setup(
+    name='pystatuschecker',
+    version='0.1.3',
+    description="A very simple package for checking the status of a service and its components",
+    author="Tim Martin",
+    author_email='tim@timmartin.me',
+    url='https://github.com/timmartin19/pystatuschecker',
+    py_modules=['status_checker'],
+    install_requires=requirements,
+    license="MIT license",
+    zip_safe=False,
+    keywords='status_checker',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    test_suite='tests',
+    tests_require=test_requirements
+)
