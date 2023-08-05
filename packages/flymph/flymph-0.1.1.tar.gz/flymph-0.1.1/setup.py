fc94ad9af6bfe3b515359e54d0a1c341755f3db9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:29 +0000

    Add package flymph/0.1.1/flymph-0.1.1.tar.gz

---
+++ packages/flymph/flymph-0.1.1.tar.gz/flymph-0.1.1/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+import sys, os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+version = '0.1.1'
+
+install_requires = [
+    # List your project dependencies here.
+    # For more details, see:
+    # http://packages.python.org/distribute/setuptools.html#declaring-dependencies
+    'flask',
+    'lymph',
+]
+
+
+setup(name='flymph',
+    version=version,
+    description="Flask as Lymph Web API",
+    # long_description=README + '\n\n' + NEWS,
+    classifiers=[
+      # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    ],
+    keywords='flask lymph web-api',
+    author='Srinivas Devaki',
+    author_email='mr.eightnoteight@gmail.com',
+    url='https://github.com/eightnoteight/flymph',
+    license='MIT License',
+    packages=find_packages('src'),
+    package_dir={'': 'src'}, include_package_data=True,
+    zip_safe=False,
+    install_requires=install_requires,
+    entry_points={
+        'console_scripts':
+            ['flymph=flymph:main']
+    }
+)
+
