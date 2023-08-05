8d96d39ddaf21c390b2a7ec42560a1fd30954247
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:57 +0000

    Add package toktokkie/0.9.9.1/toktokkie-0.9.9.1-py3-none-any.whl

---
+++ packages/toktokkie/toktokkie-0.9.9.1-py3-none-any.whl/toktokkie/metadata.py
@@ -0,0 +1,115 @@
+"""
+LICENSE:
+Copyright 2015,2016 Hermann Krumrey
+
+This file is part of toktokkie.
+
+    toktokkie is a program that allows convenient managing of various
+    local media collections, mostly focused on video.
+
+    toktokkie is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    toktokkie is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with toktokkie.  If not, see <http://www.gnu.org/licenses/>.
+LICENSE
+"""
+
+"""
+The metadata is stored here. It can be used by any other module in this project this way, most
+notably by the setup.py file
+"""
+
+project_name = "toktokkie"
+"""
+The name of the project
+"""
+
+project_description = "A personal media manager program"
+"""
+A short description of the project
+"""
+
+version_number = "0.9.9.1"
+"""
+The current version of the program.
+"""
+
+development_status = "Development Status :: 4 - Beta"
+"""
+The current development status of the program
+"""
+
+project_url = "http://namibsun.net/namboy94/tok-tokkie"
+"""
+A URL linking to the home page of the project, in this case a
+self-hosted Gitlab page
+"""
+
+download_url = "http://gitlab.namibsun.net/namboy94/tok-tokkie/repository/archive.zip?ref=master"
+"""
+A URL linking to the current source zip file.
+"""
+
+author_name = "Hermann Krumrey"
+"""
+The name(s) of the project author(s)
+"""
+
+author_email = "hermann@krumreyh.com"
+"""
+The email address(es) of the project author(s)
+"""
+
+license_type = "GNU GPL3"
+"""
+The project's license type
+"""
+
+dependencies = ['tvdb_api', 'beautifulsoup4', 'gfworks', 'typing', 'irc', 'puffotter']
+"""
+Python Packaging Index requirements
+"""
+
+audience = "Intended Audience :: End Users/Desktop"
+"""
+The intended audience of this software
+"""
+
+environment = "Environment :: Other Environment"
+"""
+The intended environment in which the program will be used
+"""
+
+programming_languages = ['Programming Language :: Python :: 3',
+                         'Programming Language :: Python :: 2']
+"""
+The programming language used in this project
+"""
+
+topic = "Topic :: Utilities"
+"""
+The broad subject/topic of the project
+"""
+
+language = "Natural Language :: English"
+"""
+The (default) language of this project
+"""
+
+compatible_os = "Operating System :: OS Independent"
+"""
+The Operating Systems on which the program can run
+"""
+
+license_identifier = "License :: OSI Approved :: GNU General Public License v3 (GPLv3)"
+"""
+The license used for this project
+"""
