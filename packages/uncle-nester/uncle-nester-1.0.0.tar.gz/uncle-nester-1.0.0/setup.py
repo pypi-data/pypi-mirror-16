9a5c17a0b92c136e2071a06532ee09cdfd4a56e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:45 +0000

    Add package uncle-nester/1.0.0/uncle-nester-1.0.0.tar.gz

---
+++ packages/uncle-nester/uncle-nester-1.0.0.tar.gz/uncle-nester-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'uncle-nester',
+    version = '1.0.0',
+    py_modules = ['uncle-nester'],
+    author = 'jk',
+    author_email = 'bioankeyang@gmail.com',
+    url = '',
+    description = 'A simple printer of nested lists'
+)
