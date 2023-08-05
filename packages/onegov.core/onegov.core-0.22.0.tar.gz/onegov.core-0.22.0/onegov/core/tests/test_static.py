ceb4e1b6befcfab57e5c12f0dfd73b99b7ae3e77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:46 +0000

    Add package onegov.core/0.22.0/onegov.core-0.22.0.tar.gz

---
+++ packages/onegov.core/onegov.core-0.22.0.tar.gz/onegov.core-0.22.0/onegov/core/tests/test_static.py
@@ -0,0 +1,86 @@
+import os.path
+import time
+
+from onegov.core import Framework
+from onegov.core import utils
+from onegov.core.static import StaticFile
+from webtest import TestApp as Client
+
+
+def test_static_file(temporary_directory):
+
+    class App(Framework):
+        serve_static_files = True
+        static_files = temporary_directory
+
+    app = App()
+    app.configure_application()
+    app.namespace = 'test'
+    app.set_application_id('test/test')
+
+    with open(os.path.join(temporary_directory, 'robots.txt'), 'w') as f:
+        f.write('foobar')
+
+    app.serve_static_files = False
+    assert StaticFile.from_application(app, 'robots.txt') is None
+
+    app.serve_static_files = True
+    assert StaticFile.from_application(app, 'robots.txt').path == 'robots.txt'
+    assert StaticFile.from_application(app, '../robots.txt') is None
+
+
+def test_static_file_app(temporary_directory):
+
+    class App(Framework):
+        serve_static_files = True
+        static_files = temporary_directory
+
+    utils.scan_morepath_modules(App)
+
+    with open(os.path.join(temporary_directory, 'robots.txt'), 'w') as f:
+        f.write('foobar')
+
+    app = App()
+    app.configure_application()
+    app.namespace = 'test'
+    app.set_application_id('test/test')
+
+    c = Client(app)
+    assert c.get('/static/robots.txt').text == 'foobar'
+
+    # make sure the if-modified-since header is honored
+    timestamp = time.gmtime(time.time() + 1)
+    modified = time.strftime('%a, %d %b %Y %H:%M:%S GMT', timestamp)
+    headers = {'If-Modified-Since': modified}
+
+    response = c.get('/static/robots.txt', headers=headers, expect_errors=True)
+    assert response.status_code == 304
+
+    # make sure inexistant files return a 404
+    assert c.get('/static/humans.txt', expect_errors=True).status_code == 404
+
+
+def test_root_file_app(temporary_directory):
+    class App(Framework):
+        serve_static_files = True
+        static_files = temporary_directory
+
+    class RobotsTxt(StaticFile):
+        pass
+
+    @App.path(model=RobotsTxt, path='robots.txt')
+    def get_favicon(app, absorb):
+        return StaticFile.from_application(app, 'robots.txt')
+
+    utils.scan_morepath_modules(App)
+
+    with open(os.path.join(temporary_directory, 'robots.txt'), 'w') as f:
+        f.write('foobar')
+
+    app = App()
+    app.configure_application()
+    app.namespace = 'test'
+    app.set_application_id('test/test')
+
+    c = Client(app)
+    assert c.get('/robots.txt').text == 'foobar'
