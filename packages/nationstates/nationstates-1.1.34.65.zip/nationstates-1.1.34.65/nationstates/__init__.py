cc5a405860f62aacaa8afb65b4f24639fa83b430
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:06 +0000

    Add package nationstates/1.1.34.65/nationstates-1.1.34.65.zip

---
+++ packages/nationstates/nationstates-1.1.34.65.zip/nationstates-1.1.34.65/nationstates/__init__.py
@@ -0,0 +1,166 @@
+from time import time as timestamp
+from time import sleep
+
+import copy
+
+if __name__ != "__main__":
+    from .objects import (
+        Nationstates,
+        Shard,
+        get_ratelimit,
+        clear_ratelimit,
+    )
+    from .NScore import __apiversion__
+    from .NScore import exceptions
+else:
+    from objects import (
+        Nationstates,
+        Shard,
+        get_ratelimit,
+        clear_ratelimit,
+    )
+    from NScore import __apiversion__
+    from NScore import exceptions
+
+
+# this is used in nationstates get_??? methods
+
+
+"""PyNationstates
+
+This module wraps around the Nationstates API to create a simple uniform way to access the API.
+"""
+
+
+class Api(object):
+
+    def __init__(self, user_agent=None, v=__apiversion__):
+        """Creates Api Instance"""
+        self.instance_version = (v, (v != __apiversion__))
+        self.nsobj = Nationstates("world", shard=None, auto_load=False, api_mother=self)
+        self.user_agent = user_agent if user_agent else None
+        self.__xrls__ = 0
+
+    @property
+    def user_agent(self):
+        """Returns current instance's user_agent"""
+        return self._user_agent_store
+
+    @user_agent.setter
+    def user_agent(self, val):
+        """Sets the user_agent"""
+        self._user_agent_store = val
+        self.nsobj.set_useragent(val)
+
+    def _call(self, api, value, shard, user_agent, auto_load, version):
+        """Used internally to request the api"""
+        self.nsobj(api, value=value, shard=shard,
+                   user_agent=user_agent, auto_load=auto_load, version=version)
+        return self.nsobj
+
+    @property
+    def xrls(self):
+        return self.__xrls__
+    
+
+    def request(self, api, value=None, shard=None,
+                user_agent=None, auto_load=False,
+                version=__apiversion__, use_error_xrls=True, user_error_rl=False):
+        """Requests the api with the specific parameters
+
+        :param api: The api being requested
+        :param value: The value of the api
+        :param shard: Shards to be requested
+        :param user_agent: user_agent to be used for this request
+        :param auto_load: If true the Nationstates instance will request the api on creation
+        :param version: version to use.
+
+        """
+        version = version if (version and version != __apiversion__) else (
+            self.instance_version[0]
+            if self.instance_version[1] else __apiversion__)
+        useragent = self.user_agent if not user_agent else user_agent
+        req = copy.copy(
+            self._call(api, value, shard, useragent, False, version))
+        req.api_instance.session = self.nsobj.api_instance.session
+        req.__use_error_xrls__ = use_error_xrls
+        req.__use_error_xrls__ = user_error_rl
+        if auto_load:
+            req.auto_load_bool = auto_load
+            req.load()
+        return req
+
+    def get_nation(self, value=None, shard=None,
+                   user_agent=None, auto_load=True,
+                   version=__apiversion__):
+        """
+        Handle Nation requests
+
+        :param api: The api being requested
+        :param value: The value of the api
+        :param shard: Shards to be requested
+        :param user_agent: user_agent to be used for this request
+        :param auto_load: If true the Nationstates instance will request the api on creation
+        :param version: version to use.
+        """
+        return self.request("nation", value, shard, user_agent,
+                            auto_load, version)
+
+    def get_region(self, value=None, shard=None,
+                   user_agent=None, auto_load=True,
+                   version=__apiversion__):
+        """
+        Handles Region requests
+
+        :param api: The api being requested
+        :param value: The value of the api
+        :param shard: Shards to be requested
+        :param user_agent: user_agent to be used for this request
+        :param auto_load: If true the Nationstates instance will request the api on creation
+        :param version: version to use.
+        """
+        return self.request("region", value, shard, user_agent,
+                            auto_load, version)
+
+    def get_world(self, shard=None, user_agent=None, auto_load=True,
+                  version=__apiversion__):
+        """
+        Handles world requests
+
+        :param api: The api being requested
+        :param shard: Shards to be requested
+        :param user_agent: user_agent to be used for this request
+        :param auto_load: If true the Nationstates instance will request the api on creation
+        :param version: version to use.
+        """
+        return self.request("world", None, shard, user_agent,
+                            auto_load, version)
+
+    def get_wa(self, council=None, shard=None,
+               user_agent=None, auto_load=True,
+               version=__apiversion__):
+        """
+        Handles WA requests
+
+        :param api: The api being requested
+        :param council: The council being requested
+        :param shard: Shards to be requested
+        :param user_agent: user_agent to be used for this request
+        :param auto_load: If true the Nationstates instance will request the api on creation
+        :param version: version to use.
+        """
+        return self.request("wa", council, shard, user_agent,
+                            auto_load, version)
+
+
+def gen_url(api, value=None, shard=None, version=None):
+    """Generates a url based on the parameters"""
+
+    if value is None and not api == "world":
+        raise exceptions.NSError(
+            "{} requires parameters to generate url.".format(api))
+
+    instance = Nationstates(api, value=value, shard=shard,
+                            version=version, user_agent="", auto_load=False)
+    instance.api_instance.session.close()
+    return instance.url
---
+++ packages/nationstates/nationstates-1.1.34.65.zip/nationstates-1.1.34.65/setup.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+"""
+
+   ___       __     __                 
+  / _ \___  / /__  / /  __ _  ___ ____ 
+ / // / _ \/ / _ \/ _ \/  ' \/ _ `/ _ \
+/____/\___/_/ .__/_//_/_/_/_/\_,_/_//_/
+           /_/                         
+
+
+The MIT License (MIT)
+
+Copyright (c) 2015 Dolphman
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+
+version = '1.1.34.65'
+
+from setuptools import setup
+setup(name='nationstates',
+      install_requires=["requests==2.10.0", "Beautifulsoup4==4.4.1", "xmltodict==0.10.2", "ezurl==0.1.3.24"],
+      version=version,
+      description='Nationstates API wrapper for python',
+      author='Joshua Walters',
+      author_email='therealdolphman@gmail.com',
+      url='https://github.com/Dolphman/pynationstates',
+      packages=['nationstates', 'nationstates.NScore'],
+      package_data={'': ['LICENSE.txt', "readme.md"]},
+      download_url='https://github.com/Dolphman/pynationstates/releases/tag/'+version,
+      keywords=['nationstates', 'api wrapper', 'api',
+                "Nationstates API", "Nationstates API Wrapper", "Wrapper",
+                "nationstates python bindings", "web api", "web wrapper"],
+      classifiers=["License :: OSI Approved :: MIT License",
+                   "Operating System :: OS Independent",
+                   "Topic :: Utilities",
+                   "Programming Language :: Python :: 2.7",
+                   "Programming Language :: Python :: 3",
+                   "Programming Language :: Python :: 3.0",
+                   "Programming Language :: Python :: 3.1",
+                   "Programming Language :: Python :: 3.2",
+                   "Programming Language :: Python :: 3.3",
+                   "Programming Language :: Python :: 3.4",
+                   "Programming Language :: Python :: 3.5"],
+      license="MIT"
+      )
