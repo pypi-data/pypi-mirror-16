6ce0b87acb58773ac5ba8538844923296bd10f42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:02 +0000

    Add package pypershelf/0.1.1/pypershelf-0.1.1.tar.gz

---
+++ packages/pypershelf/pypershelf-0.1.1.tar.gz/pypershelf-0.1.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pypershelf',
+      version='0.1.1',
+      description='ORM With Focus on RM. Inspired by Bookshelf.js',
+      url='https://github.com/insipidish/pypershelf',
+      author='insipidish',
+      author_email='insipidish@gmail.com',
+      license='MIT',
+      packages=['pypershelf'],
+      zip_safe=False)
