c7c56bd2a712687a246d37f5d0a53f8b1f9e6a1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:04 +0000

    Add package catprep/0.0.2/catprep-0.0.2-py3-none-any.whl

---
+++ packages/catprep/catprep-0.0.2-py3-none-any.whl/catprep/__init__.py
@@ -0,0 +1,22 @@
+# coding:utf-8
+import pandas as pd
+import numpy as np
+
+
+class PointwiseFiller(object):
+    def __init__(self):
+        self.means = {}
+
+    def fit(self, df, columns, target):
+        for column in columns:
+            rows = []
+            for key, value in df.groupby(column):
+                row = [key, value[target].mean()]
+                rows.append(row)
+            self.means[column] = pd.DataFrame(rows, columns=[column, '{}_mean'.format(column)])
+
+    def transform(self, df):
+        transformed_df = df
+        for column, mean_df in self.means.items():
+            transformed_df = pd.merge(transformed_df, mean_df, on=column)
+        return transformed_df
