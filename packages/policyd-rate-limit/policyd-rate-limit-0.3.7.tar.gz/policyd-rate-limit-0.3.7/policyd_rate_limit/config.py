b69b12be4a6d699fb810ee58f360f691bfa0f49b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:02 +0000

    Add package policyd-rate-limit/0.3.7/policyd-rate-limit-0.3.7.tar.gz

---
+++ packages/policyd-rate-limit/policyd-rate-limit-0.3.7.tar.gz/policyd-rate-limit-0.3.7/policyd_rate_limit/config.py
@@ -0,0 +1,58 @@
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License version 3 for
+# more details.
+#
+# You should have received a copy of the GNU General Public License version 3
+# along with this program; if not, write to the Free Software Foundation, Inc., 51
+# Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+# (c) 2015 Valentin Samir
+from policyd_rate_limit.const import SQLITE_DB, MYSQL_DB, PGSQL_DB
+
+debug = True
+
+user = "policyd-rate-limit"
+group = "policyd-rate-limit"
+pidfile = "/var/run/policyd-rate-limit/policyd-rate-limit.pid"
+
+mysql_config = {
+    "user": "username",
+    "passwd": "*****",
+    "db": "database",
+    "host": "localhost",
+    "charset": 'utf8',
+}
+
+sqlite_config = {
+    "database": "/var/lib/policyd-rate-limit/db.sqlite3",
+}
+
+pgsql_config = {
+    "database": "database",
+    "user": "username",
+    "password": "*****",
+    "host": "localhost",
+}
+
+backend = SQLITE_DB
+
+# SOCKET=("127.0.0.1", 8552)
+SOCKET = "/var/spool/postfix/ratelimit/policy"
+socket_permission = 0o666
+
+# list of (number of mails, number of seconds)
+limits = [
+    (10, 60),  # limit to 10 mails by minutes
+    (150, 86400),  # limits to 150 mails by days
+]
+
+limit_by_sasl = True
+limit_by_ip = False
+
+limited_netword = [
+]
+
+# actions return to postfix, see http://www.postfix.org/access.5.html for a list of actions.
+success_action = "dunno"
+fail_action = "defer_if_permit"
