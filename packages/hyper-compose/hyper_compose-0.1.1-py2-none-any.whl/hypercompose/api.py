f4ca10a50749f8d18fe15dfc507ac3eb2e9bfec8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:03 +0000

    Add package hyper-compose/0.1.1/hyper_compose-0.1.1-py2-none-any.whl

---
+++ packages/hyper-compose/hyper_compose-0.1.1-py2-none-any.whl/hypercompose/api.py
@@ -0,0 +1,26 @@
+import json
+import os
+import urlparse
+
+from docker import Client
+
+from .requests_aws4auth import AWS4Auth
+
+
+class Hyper(Client):
+    def __init__(self, config):
+        self.config = json.load(open(os.path.expanduser(config)))
+        clouds = self.config['clouds'].items()
+        if len(clouds) != 1:
+            raise RuntimeError("supports only one cloud in config")
+        url, self.creds = clouds[0]
+        url = urlparse.urlparse(url)
+        base_url = "https://" + url.netloc
+        Client.__init__(self, base_url, tls=True)
+        self.auth = AWS4Auth(self.creds['accesskey'], self.creds['secretkey'],
+                             url.netloc.split(".")[0], 'hyper')
+        self._version = "1.23"
+
+    def create_network(*arg, **kw):
+        #ignore network creation
+        pass
