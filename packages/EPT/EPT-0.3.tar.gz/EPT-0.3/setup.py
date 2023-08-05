293639faba4c26fa96facf9a6b5ea60dbb172467
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:22 +0000

    Add package EPT/0.3/EPT-0.3.tar.gz

---
+++ packages/EPT/EPT-0.3.tar.gz/EPT-0.3/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'EPT',
+  packages = ['EPT'], 
+  version = '0.3',
+  description = 'Use tor as proxy to bypass IP limit querys',
+  author = 'Matheus Bernardes',
+  author_email = 'mthbernardes@gmail.com',
+  url = 'https://github.com/mthbernardes/EternalProxyTor',
+  download_url = 'https://github.com/mthbernardes/EternalProxyTor/tarball/0.3',
+  keywords = ['requests', 'proxy', 'tor'],
+  classifiers = [],
+)
