b8ff08b37b20792db600436cbc39822e239059f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:08 +0000

    Add package nesterwyz/1.2.0/nesterwyz-1.2.0.tar.gz

---
+++ packages/nesterwyz/nesterwyz-1.2.0.tar.gz/nesterwyz-1.2.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+	name 		= 'nesterwyz',
+	version 	= '1.2.0',
+	py_modules	= ['nesterwyz'],
+	author		= 'wyz',
+	author_email= 'wyzgogo@hotmail.com',
+	url			= 'http://www.google.com',
+	description	= 'A simple printer of nested lists',
+	)
