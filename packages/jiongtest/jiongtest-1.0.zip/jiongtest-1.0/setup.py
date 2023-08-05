d3cc00631faed68fa8bf8fb0a8cc83e82eea593a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:10:13 +0000

    Add package jiongtest/1.0/jiongtest-1.0.zip

---
+++ packages/jiongtest/jiongtest-1.0.zip/jiongtest-1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name = 'jiongtest',
+        version = '1.0',
+        py_modules = ['liprintlist'],
+        author = 'li',
+        author_email = '1184285688@qq.com',
+        url = 'http://pypi.python.org/pypi',
+        description = 'a simple printer of nested lists',
+    )
