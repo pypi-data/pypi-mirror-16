99c63fbf9372374ace2d92bc0effc607933d10cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:52 +0000

    Add package coala-bears/0.8.0.dev20160705151130/coala_bears-0.8.0.dev20160705151130-py3-none-any.whl

---
+++ packages/coala-bears/coala_bears-0.8.0.dev20160705151130-py3-none-any.whl/bears/java/InferBear.py
@@ -0,0 +1,29 @@
+import re
+
+from coalib.bearlib.abstractions.Lint import Lint
+from coalib.bears.LocalBear import LocalBear
+from coalib.results.RESULT_SEVERITY import RESULT_SEVERITY
+
+
+class InferBear(LocalBear, Lint):
+    executable = 'infer'
+    arguments = '-npb -- javac {filename}'
+    output_regex = re.compile(
+        r'(?P<file_name>.+):'
+        r'(?P<line>.+): '
+        r'(?P<severity>error|warning): '
+        r'(?P<message>.*)')
+    severity_map = {
+        "error": RESULT_SEVERITY.MAJOR,
+        "warning": RESULT_SEVERITY.NORMAL}
+    LANGUAGES = {"Java"}
+    AUTHORS = {'The coala developers'}
+    AUTHORS_EMAILS = {'coala-devel@googlegroups.com'}
+    LICENSE = 'AGPL-3.0'
+    CAN_DETECT = {'Security'}
+
+    def run(self, filename, file):
+        '''
+        Checks the code with ``infer``.
+        '''
+        return self.lint(filename)
