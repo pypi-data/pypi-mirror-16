8023e699526d427dd3822eab00a0b4c4bb2748e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:30 +0000

    Add package flash-services/0.3.4/flash_services-0.3.4.tar.gz

---
+++ packages/flash-services/flash_services-0.3.4.tar.gz/flash_services-0.3.4/flash_services/static/scripts/services.js
@@ -0,0 +1,55 @@
+/* globals updateCommit,updateItems,updateOutcome */
+
+var SERVICES = {
+  codeship: function (pane, data) {
+    if (data.builds) {
+      updateItems(pane, data.builds, '.build-outcome', updateOutcome);
+    }
+  },
+  coveralls: function (pane, data) {
+    if (data.builds) {
+      updateItems(pane, data.builds, '.coverage', function (element, data) {
+        ['author', 'committed', 'coverage', 'message_text'].forEach(function (attr) {
+          element.find('.' + attr).text(data[attr]);
+        });
+      });
+    }
+  },
+  gh_issues: function (pane, data) {
+    if (data.issues) {
+      var states = ['open-issues', 'closed-issues', 'open-pull-requests',
+                    'closed-pull-requests'];
+      states.forEach(function (state) {
+        pane.find('.' + state).text(data.issues[state] || 0);
+      });
+    }
+  },
+  github: function (pane, data) {
+    if (data.commits) {
+      updateItems(pane, data.commits, '.commit', updateCommit);
+    }
+  },
+  tracker: function (pane, data) {
+    if (data.velocity) { pane.find('.velocity').text(data.velocity); }
+    if (data.stories) {
+      pane.find('.ready').text(
+        (data.stories.planned || 0) + (data.stories.unstarted || 0)
+      );
+      pane.find('.accepted').text(data.stories.accepted || 0);
+      pane.find('.in-flight').text(data.stories.started || 0);
+      pane.find('.completed').text(
+        (data.stories.finished || 0) + (data.stories.delivered || 0)
+      );
+    }
+  },
+  travis: function (pane, data) {
+    if (data.builds) {
+      updateItems(pane, data.builds, '.build-outcome', updateOutcome);
+    }
+  },
+  travis_pro: function (pane, data) {
+    if (data.builds) {
+      updateItems(pane, data.builds, '.build-outcome', updateOutcome);
+    }
+  }
+};
