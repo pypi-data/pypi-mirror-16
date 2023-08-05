76c19d9673e6fdf09d90fe6545d50196a2368497
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:28 +0000

    Add package pytronlinks/0.2.1/pytronlinks-0.2.1-py2.py3-none-any.whl

---
+++ packages/pytronlinks/pytronlinks-0.2.1-py2.py3-none-any.whl/pytronlinks/__init__.py
@@ -0,0 +1,28 @@
+# -*- coding: UTF-8 -*-
+
+"""
+    Pytron-links, MVC Links Interface
+    ~~~~~~
+
+    Links for Python
+
+          http://mega-voice-command.com/
+
+    :copyright: (c) 2016 by traBpUkciP.
+    :license: BSD, see LICENSE for more details.
+"""
+
+
+from .client import *
+
+
+__copyright__ = 'Copyright 2016 by traBpUkciP'
+__version__ = '0.2.1'
+__license__ = 'BSD'
+__author__ = 'traBpUkciP'
+__email__ = 'duroktar@gmail.com'
+__source__ = 'https://github.com/Duroktar/Links-AI-Interface/'
+
+__all__ = [
+    'Client',
+]
