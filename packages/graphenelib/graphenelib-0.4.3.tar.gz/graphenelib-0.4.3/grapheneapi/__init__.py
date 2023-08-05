afa17cae2916ae1ed050c9f079b8a0b7388f5312
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:12 +0000

    Add package graphenelib/0.4.3/graphenelib-0.4.3.tar.gz

---
+++ packages/graphenelib/graphenelib-0.4.3.tar.gz/graphenelib-0.4.3/grapheneapi/__init__.py
@@ -0,0 +1,12 @@
+from grapheneapi.grapheneapi import GrapheneAPI
+from grapheneapi.graphenewsprotocol import GrapheneWebsocketProtocol
+from grapheneapi.graphenews import GrapheneWebsocket
+from grapheneapi.graphenewsrpc import GrapheneWebsocketRPC
+from grapheneapi.grapheneclient import GrapheneClient
+
+__all__ = ['graphenewsprotocol',
+           'graphenews',
+           'grapheneapi',
+           'grapheneclient',
+           'graphenewsrpc'
+           ]
---
+++ packages/graphenelib/graphenelib-0.4.3.tar.gz/graphenelib-0.4.3/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+from pip.req import parse_requirements
+
+# Work around mbcs bug in distutils.
+# http://bugs.python.org/issue10945
+import codecs
+try:
+    codecs.lookup('mbcs')
+except LookupError:
+    ascii = codecs.lookup('ascii')
+    codecs.register(lambda name, enc=ascii: {True: enc}.get(name == 'mbcs'))
+
+VERSION = '0.4.3'
+
+setup(name='graphenelib',
+      version=VERSION,
+      description='Python library for graphene-based blockchains',
+      long_description=open('README.md').read(),
+      download_url='https://github.com/xeroc/python-graphenelib/tarball/' + VERSION,
+      author='Fabian Schuh',
+      author_email='<Fabian@BitShares.eu>',
+      maintainer='Fabian Schuh',
+      maintainer_email='<Fabian@BitShares.eu>',
+      url='http://www.github.com/xeroc/python-graphenelib',
+      keywords=['bitshares', 'muse', 'library', 'api',
+                'rpc', 'coin', 'tradingbot', 'exchange'],
+      packages=["grapheneapi",
+                "graphenebase",
+                "grapheneextra",
+                "grapheneexchange",
+                ],
+      install_requires=["autobahn>=0.14",
+                        "ecdsa==0.13",
+                        "pycrypto==2.6.1",
+                        "requests==2.10.0",
+                        "scrypt==0.7.1",
+                        "six==1.10.0",
+                        "txaio==2.5.1",
+                        "Unidecode==0.4.19",
+                        "websocket-client==0.37.0",
+                        ],
+      classifiers=['License :: OSI Approved :: MIT License',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python :: 3',
+                   'Development Status :: 3 - Alpha',
+                   'Intended Audience :: Developers',
+                   'Intended Audience :: Financial and Insurance Industry',
+                   'Topic :: Office/Business :: Financial',
+                   ],
+      setup_requires=['pytest-runner'],
+      tests_require=['pytest'],
+      include_package_data=True,
+      )
