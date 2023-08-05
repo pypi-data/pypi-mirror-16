2dea4d2708e8e551128711a3a7943c999a447141
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:21 +0000

    Add package django-admin-bootstrapped-compatible/3.1.2/django-admin-bootstrapped-compatible-3.1.2.tar.gz

---
+++ packages/django-admin-bootstrapped-compatible/django-admin-bootstrapped-compatible-3.1.2.tar.gz/django-admin-bootstrapped-compatible-3.1.2/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+import os
+
+CLASSIFIERS = [
+    'Development Status :: 5 - Production/Stable',
+    'Environment :: Web Environment',
+    'Framework :: Django',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: BSD License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 3',
+    'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    'Topic :: Software Development',
+    'Topic :: Software Development :: Libraries :: Application Frameworks',
+]
+
+setup(
+    author="Riccardo Forina",
+    author_email="riccardo@forina.me",
+    maintainer="Riccardo Magliocchetti",
+    maintainer_email="riccardo.magliocchetti@gmail.com",
+    name='django-admin-bootstrapped-compatible',
+    version='3.1.2',
+    description='A Bootstrap theme for Django Admin',
+    long_description=open(os.path.join(os.path.dirname(__file__), 'README.md')).read(),
+    url='https://github.com/cesar5792/django-admin-bootstrapped-1.9-compatible',
+    license='BSD License',
+    platforms=['OS Independent'],
+    classifiers=CLASSIFIERS,
+    install_requires=[
+        'setuptools',
+        'Django>=1.9',
+    ],
+    dependencies = [
+    'https://github.com/cesar57927/django-admin-bootstrapped-1.9-compatible.git@master#egg=django_admin_bootstrapped'
+    ],
+    test_suite='django_admin_bootstrapped.runtests.runtests',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False
+)
