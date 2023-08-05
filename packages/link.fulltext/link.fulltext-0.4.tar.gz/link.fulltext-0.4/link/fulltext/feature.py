b89e4bca59cd16f243fceff797c7d5cecca9f797
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:53 +0000

    Add package link.fulltext/0.4/link.fulltext-0.4.tar.gz

---
+++ packages/link.fulltext/link.fulltext-0.4.tar.gz/link.fulltext-0.4/link/fulltext/feature.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+from link.middleware.core import Feature
+
+
+class FulltextIndex(Feature):
+    """
+    API for middlewares supporting the Full-Text Index feature.
+    """
+
+    name = 'fulltext'
+    DATA_ID = 'id'
+
+    def search(self, query):
+        raise NotImplementedError()
+
+    def add(self, doc):
+        raise NotImplementedError()
+
+    def delete(self, query):
+        raise NotImplementedError()
