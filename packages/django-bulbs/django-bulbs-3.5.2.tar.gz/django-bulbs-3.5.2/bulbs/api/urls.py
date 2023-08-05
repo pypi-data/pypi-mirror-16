2e73a77e257ee49395089df4be51d876100a3bc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:48 +0000

    Add package django-bulbs/3.5.2/django-bulbs-3.5.2.tar.gz

---
+++ packages/django-bulbs/django-bulbs-3.5.2.tar.gz/django-bulbs-3.5.2/bulbs/api/urls.py
@@ -0,0 +1,43 @@
+from django.conf import settings
+from django.conf.urls import url, include
+
+from bulbs.cms_notifications.api import notifications_view
+from .views import api_v1_router, MeViewSet, ReportBugEmail
+
+
+urlpatterns = (
+    url(r"^report-bug/?$", ReportBugEmail.as_view(), name="report-bug"),
+    url(r"^me/logout/?$", "django.contrib.auth.views.logout", name="logout"),
+    url(r"^me/?$", MeViewSet.as_view({"get": "retrieve"}), name="me"),
+    url(r"^", include(api_v1_router.urls))  # noqa
+)
+
+if "bulbs.promotion" in settings.INSTALLED_APPS:
+    urlpatterns += (
+        url(r"^", include("bulbs.promotion.urls")),
+    )
+
+if "bulbs.special_coverage" in settings.INSTALLED_APPS:
+    urlpatterns += (
+        url(r"^", include("bulbs.special_coverage.urls")),
+    )
+
+if "bulbs.cms_notifications" in settings.INSTALLED_APPS:
+    urlpatterns += (
+        url(r"^notifications/(?P<pk>\d+)?", notifications_view, name="notifications"),
+    )
+
+if "bulbs.contributions" in settings.INSTALLED_APPS:
+    urlpatterns += (
+        url(r"^contributions/", include("bulbs.contributions.urls")),
+    )
+
+if "bulbs.sections" in settings.INSTALLED_APPS:
+    urlpatterns += (
+        url(r"^", include("bulbs.sections.urls")),
+    )
+
+if "bulbs.poll" in settings.INSTALLED_APPS:
+    urlpatterns += (
+        url(r"^", include("bulbs.poll.api")),
+    )
