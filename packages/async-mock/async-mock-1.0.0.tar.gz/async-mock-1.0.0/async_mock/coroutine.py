c4f0bc285cdee0afbe90fb5b187cef7461cc7e8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:21 +0000

    Add package async-mock/1.0.0/async-mock-1.0.0.tar.gz

---
+++ packages/async-mock/async-mock-1.0.0.tar.gz/async-mock-1.0.0/async_mock/coroutine.py
@@ -0,0 +1,83 @@
+import asyncio
+from unittest.mock import Mock
+
+def SimpleCoroutineMock(f=lambda *args, **kwargs: None, loop=None):
+    builder = CoroutineMockBuilder(loop=loop)
+    return builder.addDelegate(f).build().mock()
+
+
+class CoroutineMock(object):
+    def __init__(self, loop, returnSequence, block:asyncio.Event):
+        self.__start = asyncio.Event(loop=loop)
+        self.__end = asyncio.Event(loop=loop)
+        self.__returnSequence = tuple(returnSequence)
+        if (len(self.__returnSequence) < 1):
+            self.__returnSequence = (lambda *args, **kwargs: None, )
+        self.__returnSequenceLen = len(self.__returnSequence)
+        self.__block = block
+        self.__mock = self.__createMock()
+
+    def __createMock(self):
+        returnIndex = 0
+
+        async def cr(*args, **kwargs):
+            nonlocal returnIndex
+            try:
+                self.__end.clear()
+                self.__start.set()
+                if (self.__block is not None):
+                    await self.__block.wait()
+                    self.__block.clear()
+                returnFunc = self.__returnSequence[returnIndex % self.__returnSequenceLen]
+                returnIndex += 1
+                return returnFunc(*args, **kwargs)
+            finally:
+                self.__start.clear()
+                self.__end.set()
+
+        return Mock(wraps=cr)
+
+    def start(self):
+        return self.__start
+
+    def end(self):
+        return self.__end
+
+    def unblock(self):
+        self.__block.set()
+
+    def mock(self):
+        return self.__mock
+
+
+class CoroutineMockBuilder(object):
+    def __init__(self, loop):
+        self.__loop = loop
+        self.__block = None
+        self.__returnSequence = []
+
+    def blocks(self):
+        return self.blocksOn(asyncio.Event(loop=self.__loop))
+
+    def blocksOn(self, event:asyncio.Event):
+        self.__block = event
+        return self
+
+    def exception(self, e, repeats=1):
+        def r(*args, **kwargs):
+            raise e
+        self.__returnSequence.extend([r] * repeats)
+        return self
+
+    def returns(self, v, repeats=1):
+        def r(*args, **kwargs):
+            return v
+        self.__returnSequence.extend([r] * repeats)
+        return self
+
+    def addDelegate(self, f, repeats=1):
+        self.__returnSequence.extend([f] * repeats)
+        return self
+
+    def build(self):
+        return CoroutineMock(self.__loop, self.__returnSequence, self.__block)
---
+++ packages/async-mock/async-mock-1.0.0.tar.gz/async-mock-1.0.0/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+setup(
+  name = 'async-mock',
+  version = '1.0.0',
+  description = 'asyncio mocker builder library',
+  author = 'Christopher Brichford',
+  author_email = 'chrisb@farmersbusinessnetwork.com',
+  license='Other/Proprietary',
+  keywords = ['testing'], # arbitrary keywords
+  classifiers = ['License :: OSI Approved :: BSD License'],
+  install_requires=[],
+  packages=[
+        'async_mock'
+    ],
+)
