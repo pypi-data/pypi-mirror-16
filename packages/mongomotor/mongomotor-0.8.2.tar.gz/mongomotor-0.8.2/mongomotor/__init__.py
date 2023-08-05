a5981f01b9bf7a42b04f81fc623b7b58cb17f6d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:21 +0000

    Add package mongomotor/0.8.2/mongomotor-0.8.2.tar.gz

---
+++ packages/mongomotor/mongomotor-0.8.2.tar.gz/mongomotor-0.8.2/mongomotor/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from mongomotor.monkey import patch_all
+patch_all()
+
+from mongoengine.connection import connect, disconnect
+from mongomotor.document import(
+    Document, EmbeddedDocument, DynamicDocument, MapReduceDocument)
+
+VERSION = '0.8.2'
