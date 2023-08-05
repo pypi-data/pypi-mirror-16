4652d6b07f9b403a10f32e45d56d737d22cb5a14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:28 +0000

    Add package colorhash/1.0.2/colorhash-1.0.2.tar.bz2

---
+++ packages/colorhash/colorhash-1.0.2.tar.bz2/colorhash-1.0.2/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from io import open
+import os
+from setuptools import setup
+
+dist_dir = os.path.dirname(os.path.abspath(__file__))
+readme = os.path.join(dist_dir, 'README.rst')
+with open(readme, 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='colorhash',
+    version='1.0.2',
+    description='Generate a color based on a value',
+    long_description=long_description,
+    url='https://bitbucket.org/fk/python-color-hash',
+    author='Felix Krull',
+    author_email='f_krull@gmx.de',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+    ],
+
+    py_modules=['colorhash'],
+    package_dir={'': 'src'},
+
+    test_suite='test',
+)
