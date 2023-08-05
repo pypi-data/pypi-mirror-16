7598be4a905eca3e35c6f99bf3033f146604e9bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:37 +0000

    Add package dbaas_networkapi/0.1.5/dbaas_networkapi-0.1.5.tar.gz

---
+++ packages/dbaas_networkapi/dbaas_networkapi-0.1.5.tar.gz/dbaas_networkapi-0.1.5/dbaas_networkapi/equipment.py
@@ -0,0 +1,9 @@
+class Equipment(object):
+
+    def __init__(self, name, ip, port):
+        self.name = name
+        self.ip = ip
+        self.port = port
+
+    def __str__(self):
+        return '{}({}:{})'.format(self.name, self.ip, self.port)
