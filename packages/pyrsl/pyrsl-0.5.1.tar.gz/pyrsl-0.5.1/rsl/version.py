a779fc24f088514f400120bc6412f2d8e64bc3dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:09 +0000

    Add package pyrsl/0.5.1/pyrsl-0.5.1.tar.gz

---
+++ packages/pyrsl/pyrsl-0.5.1.tar.gz/pyrsl-0.5.1/rsl/version.py
@@ -0,0 +1,12 @@
+# encoding: utf-8
+# Copyright (C) 2015 John Törnblom
+'''
+Keep version info in one single place
+'''
+
+
+name = 'pyrsl'
+date = '2016-06-20'
+release = '0.5.1'
+
+complete_string = '%s v%s (%s)' % (name, release, date)
