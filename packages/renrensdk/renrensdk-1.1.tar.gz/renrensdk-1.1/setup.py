b8940fd911ef50b49cc50f343d95e5e42ce0f5b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:17 +0000

    Add package renrensdk/1.1/renrensdk-1.1.tar.gz

---
+++ packages/renrensdk/renrensdk-1.1.tar.gz/renrensdk-1.1/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+
+NAME = "renrensdk"
+DESCRIPTION = "Renren Python3 SDK for social network research."
+AUTHOR = "Luping Yu"
+AUTHOR_EMAIL = "lazydingding@gmail.com"
+URL = "https://github.com/lazydingding/renrensdk"
+VERSION = "1.1"
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    # long_description=read("README.md"),
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    license="Apache License, Version 2.0",
+    url=URL,
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+    ],
+    zip_safe=False,
+)
