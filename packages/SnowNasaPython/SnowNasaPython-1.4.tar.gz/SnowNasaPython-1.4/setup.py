6164292f733bb3200297475a8d013241ab5641a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:09 +0000

    Add package SnowNasaPython/1.4/SnowNasaPython-1.4.tar.gz

---
+++ packages/SnowNasaPython/SnowNasaPython-1.4.tar.gz/SnowNasaPython-1.4/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+  name = 'SnowNasaPython',
+  packages = ['SnowNasaPython'], 
+  version = '1.4',
+  description = 'A python lib to interact with Nasas API',
+  author = 'Justin Snow',
+  author_email = 'jrsnow8921@gmail.com',
+  url = 'https://github.com/Jrsnow8921/SnowNasaPython.git',
+  download_url = 'https://github.com/Jrsnow8921/SnowNasaPython/archive/1.4.tar.gz', 
+  keywords = ['Nasa', 'API'], 
+  classifiers = [],
+  py_modules = ['snownasapython'],
+)
