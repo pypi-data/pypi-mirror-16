33db27256dfb6bc6d9a08b0e91af207addf5e7df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:22 +0000

    Add package mypackagetsrif/0.3/mypackagetsrif-0.3.zip

---
+++ packages/mypackagetsrif/mypackagetsrif-0.3.zip/mypackagetsrif-0.3/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'mypackagetsrif',
+  packages = ['mypackagetsrif'], # this must be the same as the name above
+  version = '0.3',
+  description = 'A random test lib',
+  author = 'Ujvala',
+  author_email = 'ujvala.bhumi@gmail.com',
+  url = 'https://github.com/ujvala-bhumi/Pypi-test-project', # use the URL to the github repo
+  download_url = 'https://github.com/ujvala-bhumi/Pypi-test-project/tarball/0.2', # I'll explain this in a second
+  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+  classifiers = [],
+)
