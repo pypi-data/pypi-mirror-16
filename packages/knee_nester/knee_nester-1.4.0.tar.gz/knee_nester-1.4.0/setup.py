07229aed6bc7e51454d44cd72325f6b40c84638b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:57 +0000

    Add package knee_nester/1.4.0/knee_nester-1.4.0.tar.gz

---
+++ packages/knee_nester/knee_nester-1.4.0.tar.gz/knee_nester-1.4.0/setup.py
@@ -0,0 +1,18 @@
+'''
+created		:	02-JUL-2016
+author		:	kneerunjun@gmail.com
+description	:	sets up the moudule for distribution
+'''
+
+from distutils.core import setup
+
+setup(
+	name="knee_nester",
+	py_modules=["knee_nester"],
+	author="kneerunjun",
+	author_email="kneerunjun@gmail.com",
+	version="1.4.0",
+	description="adding testing function and data",
+	url="http://kneerunjun.wordpress.com"
+)
+
