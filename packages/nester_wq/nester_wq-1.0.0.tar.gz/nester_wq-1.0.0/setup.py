35a770d94198968b773505bcecbe69332da9ae04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:40 +0000

    Add package nester_wq/1.0.0/nester_wq-1.0.0.tar.gz

---
+++ packages/nester_wq/nester_wq-1.0.0.tar.gz/nester_wq-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup (
+    name = 'nester_wq',
+    version = '1.0.0',
+    py_modules = ['nester_wq'],
+    author = 'qi',
+    author_email = 'wq19890727@gmail.com',
+    url = 'http://www.headfirstlabs.com',
+    description = 'simple script',
+)
