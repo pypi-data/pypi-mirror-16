3d194c6e20d921a04b563f612d2b0f84041c2f24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:52 +0000

    Add package my_funky_package/0.2/my_funky_package-0.2.tar.gz

---
+++ packages/my_funky_package/my_funky_package-0.2.tar.gz/my_funky_package-0.2/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+  name = 'my_funky_package',
+  packages = ['my_funky_package'], # this must be the same as the name above #???
+  version = '0.2',
+  description = 'A random test lib',
+  author = 'Umut Orman',
+  author_email = 'umutormans@gmail.com',
+  url = 'https://github.com/UmutOrman/mypackage', # use the URL to the github repo
+  download_url = 'https://github.com/UmutOrman/mypackage/tarball/0.1', # I'll explain this in a second
+  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+  classifiers = [],
+)
