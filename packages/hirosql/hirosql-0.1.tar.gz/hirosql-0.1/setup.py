908c35cdd5b7c0a6386f5909470324aae6fadef9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:22 +0000

    Add package hirosql/0.1/hirosql-0.1.tar.gz

---
+++ packages/hirosql/hirosql-0.1.tar.gz/hirosql-0.1/setup.py
@@ -0,0 +1,24 @@
+# coding: utf-8
+from setuptools import setup, find_packages
+from pip.req import parse_requirements
+
+install_requires = [str(ir.req) for ir in parse_requirements("requirements.txt", session=False)]
+
+
+setup(
+    name='hirosql',
+    version='0.1',
+    author='Hiroyuki Ishii',
+    author_email="hiroyuki.ishii.42@gmail.com",
+    # url="http://",
+    py_modules=['hirosql'],
+    packages=find_packages(),
+    description='A small development tool for mysql or other databases',
+    dependency_links=[],
+    keywords=['sql'],
+    install_requires=install_requires,
+    entry_points='''
+        [console_scripts]
+        hirosql=hirosql.main:cli
+    ''',
+)
