c40df6812c17289b8c659e01d8a393c6a7d0c707
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:56 +0000

    Add package liliyu/1.0.0/liliyu-1.0.0.zip

---
+++ packages/liliyu/liliyu-1.0.0.zip/liliyu-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name          = 'liliyu',
+    version       = '1.0.0',
+    py_modules    = ['liliyu'],
+    author        = 'muyu7',
+    author_email  = '13419676764@163.com',
+    url           = 'http://www.headfirstlabs.com',
+    description   = 'A simple printer of nested lists',
+    )
