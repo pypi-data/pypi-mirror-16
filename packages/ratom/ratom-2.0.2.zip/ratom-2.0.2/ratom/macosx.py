40f520eb4a4f10fe1d5d1cfc880c8e3a03e81da4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:15 +0000

    Add package ratom/2.0.2/ratom-2.0.2.zip

---
+++ packages/ratom/ratom-2.0.2.zip/ratom-2.0.2/ratom/macosx.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+"""update Mac OSX"""
+
+# File: ratom/macosx.py
+# Version: 2.0.2
+# Date: 2016-07-19
+# Author: qtfkwk <qtfkwk+ratom@gmail.com>
+# Copyright: (C) 2016 by qtfkwk
+# License: BSD 2-Clause License (https://opensource.org/licenses/BSD-2-Clause)
+
+from common import *
+
+def check():
+    """check if can update Mac OSX"""
+    return has('softwareupdate')
+
+def main(argv=None, cfg=None):
+    """update Mac OSX"""
+    cfg = init(argv, cfg)
+    info('macosx: started')
+    if not check():
+        info('macosx: failed check')
+        return
+    section('Mac OSX', 'sudo softwareupdate -iav', dryrun=cfg['dryrun'])
+    info('macosx: finished')
+
+if __name__ == '__main__':
+    main()
+
---
+++ packages/ratom/ratom-2.0.2.zip/ratom-2.0.2/ratom/macosx_microsoft.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+"""update Microsoft software on Mac OSX"""
+
+# File: ratom/macosx_microsoft.py
+# Version: 2.0.2
+# Date: 2016-07-19
+# Author: qtfkwk <qtfkwk+ratom@gmail.com>
+# Copyright: (C) 2016 by qtfkwk
+# License: BSD 2-Clause License (https://opensource.org/licenses/BSD-2-Clause)
+
+from common import *
+
+_f = '/Library/Application Support/Microsoft/MAU2.0/Microsoft AutoUpdate.app'
+
+def check():
+    """check if can update Microsoft software on Mac OSX"""
+    return os.path.isdir(_f)
+
+def main(argv=None, cfg=None):
+    """update Microsoft software on Mac OSX"""
+    cfg = init(argv, cfg)
+    info('macosx_microsoft: started')
+    if not check():
+        info('macosx_microsoft: failed check')
+        return
+    c = 'open -W %s' % _f.replace(' ', r'\ ')
+    section('Microsoft AutoUpdate (Mac OSX)', c, dryrun=cfg['dryrun'])
+    info('macosx_microsoft: finished')
+
+if __name__ == '__main__':
+    main()
+
