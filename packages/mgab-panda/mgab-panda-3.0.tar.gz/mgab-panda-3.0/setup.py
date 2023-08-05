34b8d0940b9ae711fab6c3c4f5eab1ba2872e0d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:39 +0000

    Add package mgab-panda/3.0/mgab-panda-3.0.tar.gz

---
+++ packages/mgab-panda/mgab-panda-3.0.tar.gz/mgab-panda-3.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(name='mgab-panda',
+      version='3.0',
+      description='The panda song',
+      author='mgab',
+      author_email='mgab@panda.com',
+      entry_points={
+        'console_scripts': [
+            'mgabpanda = panda.__main__:_main',
+        ]
+      },
+      packages=find_packages())
