4fab50a3bcecb84cb59e050b89634713c0711263
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:03 +0000

    Add package nbgen/0.1.1/nbgen-0.1.1.tar.gz

---
+++ packages/nbgen/nbgen-0.1.1.tar.gz/nbgen-0.1.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="nbgen",
+    description="Generates a Jupyter Notebook and slides from your data",
+    url="https://github.com/ewjoachim/nbgen",
+    version="0.1.1",
+    packages=find_packages(exclude=["sample.py"]),
+    entry_points={
+        'console_scripts': [
+            'nbgen = nbgen:main',
+        ],
+    },
+    author="Joachim Jablon",
+    author_email="ewjoachim@gmail.com",
+    install_requires=['jupyter', 'docopt'],
+    keywords=["jupyter", "notebook", "generation", "slides"],
+)
