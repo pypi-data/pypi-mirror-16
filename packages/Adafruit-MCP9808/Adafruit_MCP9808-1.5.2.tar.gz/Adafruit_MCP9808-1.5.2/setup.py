4227bd8d9c422143b3cfba7de77e54d9a60923a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:33 +0000

    Add package Adafruit-MCP9808/1.5.2/Adafruit_MCP9808-1.5.2.tar.gz

---
+++ packages/Adafruit-MCP9808/Adafruit_MCP9808-1.5.2.tar.gz/Adafruit_MCP9808-1.5.2/setup.py
@@ -0,0 +1,28 @@
+try:
+    from ez_setup import use_setuptools
+    use_setuptools()
+except ImportError:
+    pass  # Ignore failure to import ez_setup during pip install scenarios.
+
+from setuptools import setup, find_packages
+
+classifiers = ['Development Status :: 4 - Beta',
+               'Operating System :: POSIX :: Linux',
+               'License :: OSI Approved :: MIT License',
+               'Intended Audience :: Developers',
+               'Programming Language :: Python :: 2.7',
+               'Programming Language :: Python :: 3',
+               'Topic :: Software Development',
+               'Topic :: System :: Hardware']
+
+setup(name              = 'Adafruit_MCP9808',
+      version           = '1.5.2',
+      author            = 'Tony DiCola',
+      author_email      = 'tdicola@adafruit.com',
+      description       = 'Library for accessing the MCP9808 precision temperature sensor on a Raspberry Pi or Beaglebone Black.',
+      license           = 'MIT',
+      classifiers       = classifiers,
+      url               = 'https://github.com/adafruit/Adafruit_Python_MCP9808/',
+      dependency_links  = ['https://github.com/adafruit/Adafruit_Python_GPIO/tarball/master#egg=Adafruit-GPIO-0.6.5'],
+      install_requires  = ['Adafruit-GPIO>=0.6.5'],
+      packages          = find_packages())
