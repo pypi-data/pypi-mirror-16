cd27e7826ee5875908534a3a9fe364068f74183c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:25 +0000

    Add package encrypter_pictures/0.1.1/encrypter_pictures-0.1.1.tar.gz

---
+++ packages/encrypter_pictures/encrypter_pictures-0.1.1.tar.gz/encrypter_pictures-0.1.1/src/encrypter_pictures/gui.py
@@ -0,0 +1,191 @@
+#!/usr/bin/python3
+from PyQt5.QtCore import QDir, Qt
+from PyQt5.QtGui import QImage, QPainter, QPalette, QPixmap
+from PyQt5.QtWidgets import (QAction, QApplication, QFileDialog, QLabel,
+        QMainWindow, QMenu, QMessageBox, QScrollArea, QSizePolicy, QPushButton,
+        QHBoxLayout, QVBoxLayout)
+from PyQt5.QtPrintSupport import QPrintDialog, QPrinter
+from cript import cript
+import sys
+
+
+class ImageViewer(QMainWindow):
+    def __init__(self):
+        super(ImageViewer, self).__init__()
+
+        self.printer = QPrinter()
+        self.scaleFactor = 0.0
+
+        self.imageLabel = QLabel()
+        self.imageLabel.setBackgroundRole(QPalette.Base)
+        self.imageLabel.setSizePolicy(QSizePolicy.Ignored, QSizePolicy.Ignored)
+        self.imageLabel.setScaledContents(True)
+
+        self.scrollArea = QScrollArea()
+        self.scrollArea.setBackgroundRole(QPalette.Dark)
+        self.scrollArea.setWidget(self.imageLabel)
+        self.setCentralWidget(self.scrollArea)
+
+        self.createActions()
+        self.createMenus()
+        self.initUI()
+
+        self.setWindowTitle("Image Viewer")
+        self.resize(500, 400)
+
+    def initUI(self):
+
+        self.okButton = QPushButton("Criptografar", self)
+        self.okButton.move(45, 50)
+        self.okButton.clicked.connect(self.buttonClicked)
+
+        self.statusBar()
+
+    def buttonClicked(self):
+        try:
+            self.statusBar().showMessage("Encrypting: " + self.fileName)
+            self.output_path = cript(self.fileName)
+            self.show_image()
+            self.statusBar().showMessage("Salve on: " + self.output_path)
+        except AttributeError:
+            self.statusBar().showMessage("Select an image")
+
+    def show_image(self):
+        """docstring for show_image"""
+        if self.output_path:
+            image = QImage(self.output_path)
+            if image.isNull():
+                QMessageBox.information(self, "Image Viewer",
+                        "Cannot load %s." % self.output_path)
+                return
+
+            self.imageLabel.setPixmap(QPixmap.fromImage(image))
+            self.scaleFactor = 1.0
+
+            self.printAct.setEnabled(True)
+            self.fitToWindowAct.setEnabled(True)
+            self.updateActions()
+
+            if not self.fitToWindowAct.isChecked():
+                self.imageLabel.adjustSize()
+    def open(self):
+        self.fileName, _ = QFileDialog.getOpenFileName(self, "Open File",
+                QDir.currentPath())
+        if self.fileName:
+            image = QImage(self.fileName)
+            if image.isNull():
+                QMessageBox.information(self, "Image Viewer",
+                        "Cannot load %s." % self.fileName)
+                return
+
+            self.imageLabel.setPixmap(QPixmap.fromImage(image))
+            self.scaleFactor = 1.0
+
+            self.printAct.setEnabled(True)
+            self.fitToWindowAct.setEnabled(True)
+            self.updateActions()
+
+            if not self.fitToWindowAct.isChecked():
+                self.imageLabel.adjustSize()
+
+    def print_(self):
+        dialog = QPrintDialog(self.printer, self)
+        if dialog.exec_():
+            painter = QPainter(self.printer)
+            rect = painter.viewport()
+            size = self.imageLabel.pixmap().size()
+            size.scale(rect.size(), Qt.KeepAspectRatio)
+            painter.setViewport(rect.x(), rect.y(), size.width(), size.height())
+            painter.setWindow(self.imageLabel.pixmap().rect())
+            painter.drawPixmap(0, 0, self.imageLabel.pixmap())
+
+    def zoomIn(self):
+        self.scaleImage(1.25)
+
+    def zoomOut(self):
+        self.scaleImage(0.8)
+
+    def normalSize(self):
+        self.imageLabel.adjustSize()
+        self.scaleFactor = 1.0
+
+    def fitToWindow(self):
+        fitToWindow = self.fitToWindowAct.isChecked()
+        self.scrollArea.setWidgetResizable(fitToWindow)
+        if not fitToWindow:
+            self.normalSize()
+
+        self.updateActions()
+
+    def about(self):
+        QMessageBox.about(self, "About Encrypter Pictures",
+                "<p>The <b>Encrypter Pictures</b> is a software to encrypt and decrypting any given image")
+
+    def createActions(self):
+        self.openAct = QAction("&Open...", self, shortcut="Ctrl+O",
+                triggered=self.open)
+
+        self.printAct = QAction("&Print...", self, shortcut="Ctrl+P",
+                enabled=False, triggered=self.print_)
+
+        self.exitAct = QAction("E&xit", self, shortcut="Ctrl+Q",
+                triggered=self.close)
+
+        self.zoomInAct = QAction("Zoom &In (25%)", self, shortcut="Ctrl++",
+                enabled=False, triggered=self.zoomIn)
+
+        self.zoomOutAct = QAction("Zoom &Out (25%)", self, shortcut="Ctrl+-",
+                enabled=False, triggered=self.zoomOut)
+
+        self.normalSizeAct = QAction("&Normal Size", self, shortcut="Ctrl+S",
+                enabled=False, triggered=self.normalSize)
+
+        self.fitToWindowAct = QAction("&Fit to Window", self, enabled=False,
+                checkable=True, shortcut="Ctrl+F", triggered=self.fitToWindow)
+
+        self.aboutAct = QAction("&About", self, triggered=self.about)
+
+        self.aboutQtAct = QAction("About &Qt", self,
+                triggered=QApplication.instance().aboutQt)
+
+    def createMenus(self):
+        self.fileMenu = QMenu("&File", self)
+        self.fileMenu.addAction(self.openAct)
+        self.fileMenu.addAction(self.printAct)
+        self.fileMenu.addSeparator()
+        self.fileMenu.addAction(self.exitAct)
+
+        self.viewMenu = QMenu("&View", self)
+        self.viewMenu.addAction(self.zoomInAct)
+        self.viewMenu.addAction(self.zoomOutAct)
+        self.viewMenu.addAction(self.normalSizeAct)
+        self.viewMenu.addSeparator()
+        self.viewMenu.addAction(self.fitToWindowAct)
+
+        self.helpMenu = QMenu("&Help", self)
+        self.helpMenu.addAction(self.aboutAct)
+        self.helpMenu.addAction(self.aboutQtAct)
+
+        self.menuBar().addMenu(self.fileMenu)
+        self.menuBar().addMenu(self.viewMenu)
+        self.menuBar().addMenu(self.helpMenu)
+
+    def updateActions(self):
+        self.zoomInAct.setEnabled(not self.fitToWindowAct.isChecked())
+        self.zoomOutAct.setEnabled(not self.fitToWindowAct.isChecked())
+        self.normalSizeAct.setEnabled(not self.fitToWindowAct.isChecked())
+
+    def scaleImage(self, factor):
+        self.scaleFactor *= factor
+        self.imageLabel.resize(self.scaleFactor * self.imageLabel.pixmap().size())
+
+        self.adjustScrollBar(self.scrollArea.horizontalScrollBar(), factor)
+        self.adjustScrollBar(self.scrollArea.verticalScrollBar(), factor)
+
+        self.zoomInAct.setEnabled(self.scaleFactor < 3.0)
+        self.zoomOutAct.setEnabled(self.scaleFactor > 0.333)
+
+    def adjustScrollBar(self, scrollBar, factor):
+        scrollBar.setValue(int(factor * scrollBar.value()
+                                + ((factor - 1) * scrollBar.pageStep()/2)))
+
