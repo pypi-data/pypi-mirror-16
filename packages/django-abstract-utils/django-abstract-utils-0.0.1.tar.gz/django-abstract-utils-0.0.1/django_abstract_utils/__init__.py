8ab7d31f087ae9722622066003ecd2da473e4066
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:53 +0000

    Add package django-abstract-utils/0.0.1/django-abstract-utils-0.0.1.tar.gz

---
+++ packages/django-abstract-utils/django-abstract-utils-0.0.1.tar.gz/django-abstract-utils-0.0.1/django_abstract_utils/__init__.py
@@ -0,0 +1,23 @@
+def AbstractClassWithoutFieldsNamed(cls, *excl):
+	"""
+	Removes unwanted fields from abstract base classes.
+
+	Ripped off from some Gist. Feel free to submit a PR with authorship!
+
+	Usage::
+	>>> from oscar.apps.address.abstract_models import AbstractBillingAddress
+
+	>>> from koe.meta import AbstractClassWithoutFieldsNamed as without
+	>>> class BillingAddress(without(AbstractBillingAddress, 'phone_number')):
+	...     pass
+	"""
+	if cls._meta.abstract:
+		remove_fields = [f for f in cls._meta.local_fields if f.name in excl]
+		for f in remove_fields:
+			cls._meta.local_fields.remove(f)
+		return cls
+	else:
+		raise Exception("Not an abstract model")
+
+
+without = AbstractClassWithoutFieldsNamed
