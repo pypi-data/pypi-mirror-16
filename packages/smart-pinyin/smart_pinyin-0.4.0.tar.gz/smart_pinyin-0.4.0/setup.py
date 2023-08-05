243df726a685c2843a8348ce7b56093854c02b33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:54 +0000

    Add package smart-pinyin/0.4.0/smart_pinyin-0.4.0.tar.gz

---
+++ packages/smart-pinyin/smart_pinyin-0.4.0.tar.gz/smart_pinyin-0.4.0/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+from setuptools import setup, find_packages
+
+extra = {}
+
+with open('README.md', 'rt') as f:
+    extra['long_description'] = f.read()
+
+setup(
+    name='smart_pinyin',
+    version='0.4.0',
+    description='Smart Chinese-to-Pinyin converter.',
+    author='mapix',
+    author_email='mapix.me@gmail.com',
+    url='https://github.com/mapix/pinyin',
+    license='MIT',
+    packages=find_packages(),
+    package_data={
+        'pinyin': [
+            'data/*',
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+    ],
+    install_requires=['distribute', 'jieba'],
+    **extra
+)
