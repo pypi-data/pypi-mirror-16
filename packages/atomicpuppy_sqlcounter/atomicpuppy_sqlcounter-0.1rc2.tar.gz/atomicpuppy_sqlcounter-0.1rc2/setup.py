18bbf97a854d793ffcd15e57ec46f26923e669d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:14 +0000

    Add package atomicpuppy_sqlcounter/0.1rc2/atomicpuppy_sqlcounter-0.1rc2.tar.gz

---
+++ packages/atomicpuppy_sqlcounter/atomicpuppy_sqlcounter-0.1rc2.tar.gz/atomicpuppy_sqlcounter-0.1rc2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+install_requires = [
+    "atomicpuppy==0.3rc0",
+    "retrying==1.3.3",
+]
+
+tests_require = [
+    "Contexts==0.10.2",
+]
+
+setup(
+    name="atomicpuppy_sqlcounter",
+    version="0.1rc2",
+    install_requires=install_requires,
+    tests_require=tests_require,
+    py_modules=['atomicpuppy_sqlcounter'],
+    url='https://github.com/madedotcom/atomicpuppy-sqlcounter',
+    description='A sqlalchemy based counter for AtomicPuppy',
+    author='Francesco Pighi',
+    keywords=['AtomicPuppy'],
+    download_url='https://github.com/madedotcom/atomicpuppy-sqlcounter/tarball/0.1rc2',
+    license='MIT',
+)
