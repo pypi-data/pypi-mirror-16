3b022a2292c4fa61177db08566cdc93f1701fd7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:32 +0000

    Add package kaczmarz/17/kaczmarz-17.zip

---
+++ packages/kaczmarz/kaczmarz-17.zip/kaczmarz-17/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(name='kaczmarz',
+	version='17',
+	description='Simulations and visualizations of the Kaczmarz algorithm on various classifications of linear systems',
+	url='https://github.com/lightningleaf/kaczmarz',
+	author='lightningleaf',
+	author_email='lightningleaf0@gmail.com',
+	license='None',
+	packages=['kaczmarz'],
+	install_requires=[
+	'recordclass',
+	'numpy'
+	],
+	zip_safe=True,
+	classifiers=[
+	'Programming Language :: Python :: 3.5',
+	'Intended Audience :: Science/Research'
+	]
+	)
