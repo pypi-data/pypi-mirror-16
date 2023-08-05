7dfcadf119190e5ffedbd9e1c8d1024566bc4a5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:15 +0000

    Add package datatools/0.1.1/datatools-0.1.1.tar.gz

---
+++ packages/datatools/datatools-0.1.1.tar.gz/datatools-0.1.1/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+"""A package that makes data-wringling at commandline a matter of pipes."""
+from setuptools import find_packages, setup
+
+setup(name = 'datatools',
+    version = '0.1.1',
+    description = "Data Tools",
+    long_description=open('README.md').read(),
+    author="Antonio Lima",
+    author_email="anto87@gmail.com",
+    url="http://github.com/themiurgo/datatools",
+    license = "MIT",
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'dbyrow = datatools.cli:byrow',
+            'dcompute = datatools.cli:compute',
+            'ddescribe = datatools.cli:describe',
+            'dgrep = datatools.cli:grep',
+            'djoin = datatools.cli:join',
+            'dj2gj = datatools:json2geojson',
+            'djsonexplorer = datatools.cli:jsonexplorer',
+            'dunique = datatools.cli:dunique',
+            'drandom = datatools.cli:drandom',
+        ],
+    },
+    scripts=[
+        'datatools/external/cols',
+        'datatools/external/body',
+        'datatools/external/header',
+    ]
+)
