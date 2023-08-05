6f592ad4a002d503276b61eff08c49c602a07de1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:35 +0000

    Add package zendeskhc/0.4.1/zendeskhc-0.4.1.tar.gz

---
+++ packages/zendeskhc/zendeskhc-0.4.1.tar.gz/zendeskhc-0.4.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+setuptools.setup(
+        install_requires=['requests'],
+        author = 'Caleb Boylan',
+        name = 'zendeskhc',
+        description = 'Python module for interacting with the Zendesk Help Center API',
+        author_email = 'calebboylan@gmail.com',
+        url = 'https://github.com/squidboylan/zendeskpy',
+        version = '0.4.1',
+        classifiers = [
+            'Development Status :: 3 - Alpha',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: Apache Software License',
+            'Operating System :: POSIX :: Linux',
+            'Programming Language :: Python',
+            'Programming Language :: Python :: 2',
+            'Programming Language :: Python :: 2.7',
+        ],
+        packages=setuptools.find_packages(exclude=["tests"]),
+)
