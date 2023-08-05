1e384bd10461c82d7fbd927e56cad26e040ded55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:07 +0000

    Add package whirls/1.1/whirls-1.1.zip

---
+++ packages/whirls/whirls-1.1.zip/whirls-1.1/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='whirls',
+    version='1.1',
+    description='A fullscreen program that displays an animated, twisting tessellation of whirls.',
+    long_description=long_description,
+    url='https://bitbucket.org/David_Nickerson/whirls',
+    author='David Nickerson',
+    author_email='UNKNOWN',
+    license='GPLv3+',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Games/Entertainment :: Simulation',
+    ],
+    keywords='curve derived polygon pursuit spiral whirl',
+    package_dir={'': 'src'},
+    py_modules=['whirls'],
+    install_requires=['matplotlib', 'numpy', 'pgu', 'pygame', 'scipy'],
+    entry_points={
+        'console_scripts': [
+            'whirls=whirls:main',
+        ],
+    },
+)
