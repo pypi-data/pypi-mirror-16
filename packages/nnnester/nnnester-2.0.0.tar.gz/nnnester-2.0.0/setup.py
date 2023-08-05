2b42c5c985dcca385043814bd80f596a46b45384
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:46 +0000

    Add package nnnester/2.0.0/nnnester-2.0.0.tar.gz

---
+++ packages/nnnester/nnnester-2.0.0.tar.gz/nnnester-2.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name        ='nnnester',
+    version     ='2.0.0',
+    py_modules  =['nnnester'],
+    author      ='owencx',
+    author_email='cx1410431042@gmail.com',
+    description ='A simple printer of nester lists',
+    )
