0e16560d5bbf46eb1049ff4f73b4ff1b841bc6c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:18 +0000

    Add package txttk/0.8.1/txttk-0.8.1-py2.py3-none-any.whl

---
+++ packages/txttk/txttk-0.8.1-py2.py3-none-any.whl/txttk/__init__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import (absolute_import, division,
+                        print_function, unicode_literals)
+from builtins import *
+
+import os
+from txttk import nlptools, retools, feature
+
+__author__ = 'Chia-Jung, Yang'
+__email__ = 'jeroyang@gmail.com'
+
+version_path = os.path.join(os.path.dirname(__file__), '../VERSION')
+with open(version_path) as f:
+    __version__ = f.read().strip()
+
+__all__ = ['feature', 'nlptools', 'retools']
