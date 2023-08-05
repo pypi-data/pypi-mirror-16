d79cb2ef707199fbfb1fc1b45769255477c36c14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:22 +0000

    Add package vexmessage/0.1.0/vexmessage-0.1.0.tar.gz

---
+++ packages/vexmessage/vexmessage-0.1.0.tar.gz/vexmessage-0.1.0/vexmessage/__init__.py
@@ -0,0 +1,60 @@
+import pickle as _pickle
+import textwrap as _textwrap
+
+
+VERSION = '0.2.0'
+
+
+def decode(frame):
+    target = frame[0].decode('ascii')
+    deserial = _pickle.loads(frame[1])
+    source = deserial[0]
+    version = deserial[1]
+    # probably have a interp lib here /shame
+    type = deserial[2]
+    content = deserial[3]
+
+    return Message(target, source, version, type, **content)
+
+
+def encode(target, source, type, version=VERSION, **message):
+    target = target.encode('ascii')
+    serialization = _pickle.dumps((source, version, type, message))
+    return (target, serialization)
+
+
+def create_vex_message(target, source, type, version=VERSION, **msg):
+    return encode(target, source, type, version, **msg)
+
+
+def decode_vex_message(frame):
+    return decode(frame)
+
+
+class Message:
+    def __init__(self, target, source, version, type, **content):
+        self.target = target
+        self.source = source
+        self.VERSION = VERSION
+        self.type = type
+        self.contents = content
+
+    def __repr__(self):
+        s = "type: {}  target: {}  source: {}  version: {}  contents: {}"
+        target = self.target
+        if target == '':
+            target = 'all'
+        s = s.format(self.type,
+                     target,
+                     self.source,
+                     self.VERSION,
+                     self.contents)
+
+        return _textwrap.fill(s)
+
+
+class VexTypes:
+    command = 'CMD'
+    response = 'RSP'
+    message = 'MSG'
+    # STAUS ?
