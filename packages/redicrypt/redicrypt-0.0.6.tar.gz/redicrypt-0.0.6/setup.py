a27a3518c16aaf8c50cde3bc4a098e4eee52bb8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:37 +0000

    Add package redicrypt/0.0.6/redicrypt-0.0.6.tar.gz

---
+++ packages/redicrypt/redicrypt-0.0.6.tar.gz/redicrypt-0.0.6/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+setup(
+    name="redicrypt",
+    version="0.0.6",
+    author="Chris Dutra",
+    author_email="cdutra@apprenda.com",
+    description="Python-based cryptography package for redis.",
+    license="MIT",
+    keywords="redis data cryptography",
+    url="http://github.com/dutronlabs/redicrypt",
+    packages=['redicrypt'],
+    install_requires=['pycrypto'],
+    long_description="This client package provides a means to encrypt/decrypt data being stored into redis.",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: BSD License",
+    ],
+)
