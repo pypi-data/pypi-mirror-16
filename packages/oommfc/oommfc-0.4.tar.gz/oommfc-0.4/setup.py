b5ac4421ca68ec69b40149caa7320ee63312e838
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:20 +0000

    Add package oommfc/0.4/oommfc-0.4.tar.gz

---
+++ packages/oommfc/oommfc-0.4.tar.gz/oommfc-0.4/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+with open('README.rst') as f:
+    readme = f.read()
+
+setup(
+    name='oommfc',
+    version='0.4',
+    description='A Python-based OOMMF calculator.',
+    long_description=readme,
+    author='Computational Modelling Group',
+    author_email='fangohr@soton.ac.uk',
+    url = 'https://github.com/joommf/oommfc',
+    download_url = 'https://github.com/joommf/oommfc/tarball/0.2',
+    packages=['oommfc', 'oommfc.energies', 'oommfc.tests'],
+    install_requires=[
+        'finitedifferencefield',
+        'oommffield',
+        'oommfodt'
+    ],
+    classifiers=[
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+    ]
+)
