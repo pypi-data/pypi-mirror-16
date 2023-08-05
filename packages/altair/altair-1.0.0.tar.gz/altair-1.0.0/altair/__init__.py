b4d651fce52abaab170932d19ab2deca6aca932b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:10 +0000

    Add package altair/1.0.0/altair-1.0.0.tar.gz

---
+++ packages/altair/altair-1.0.0.tar.gz/altair-1.0.0/altair/__init__.py
@@ -0,0 +1,58 @@
+__version__ = '1.0.0'
+
+from .api import (
+    load_vegalite_spec,
+    Label,
+    Formula,
+    StackOffset,
+    Data,
+    FacetScaleConfig,
+    AxisConfig,
+    Shape,
+    Axis,
+    AggregateOp,
+    ScaleConfig,
+    NiceTime,
+    Transform,
+    VerticalAlign,
+    SortOrder,
+    Legend,
+    LegendConfig,
+    Column,
+    FontWeight,
+    SortField,
+    Text,
+    MarkConfig,
+    TimeUnit,
+    FacetConfig,
+    FontStyle,
+    Config,
+    Order,
+    HorizontalAlign,
+    Path,
+    Scale,
+    Encoding,
+    Facet,
+    Size,
+    FacetGridConfig,
+    Row,
+    Bin,
+    AxisOrient,
+    X, Y,
+    Color,
+    Opacity,
+    DataFormat,
+    Chart,
+    LayeredChart,
+    FacetedChart,
+    CellConfig,
+    Detail,
+)
+
+from .datasets import (
+    list_datasets,
+    load_dataset
+)
+
+from .tutorial import tutorial
+
