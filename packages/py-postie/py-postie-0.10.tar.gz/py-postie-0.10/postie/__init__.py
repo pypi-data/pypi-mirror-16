d5062262e78f29935606182a47a34a77aadfc5a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:31 +0000

    Add package py-postie/0.10/py-postie-0.10.tar.gz

---
+++ packages/py-postie/py-postie-0.10.tar.gz/py-postie-0.10/postie/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+
+from .main import Postie
+from .cli import create_parser, main
---
+++ packages/py-postie/py-postie-0.10.tar.gz/py-postie-0.10/setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name='py-postie',
+    version='0.10',
+    py_modules=['py-postie'],
+    description='Utility to batch send emails and text messages',
+    url='http://github.com/itsnauman/postie',
+    author='Nauman Ahmad',
+    author_email='nauman-ahmad@outlook.com',
+    license='MIT',
+    include_package_data=True,
+    packages=['postie'],
+    install_requires=[
+        'mailthon',
+        'twilio',
+        'jinja2',
+    ],
+    entry_points='''
+        [console_scripts]
+        postie=postie.cli:main
+    ''',
+)
