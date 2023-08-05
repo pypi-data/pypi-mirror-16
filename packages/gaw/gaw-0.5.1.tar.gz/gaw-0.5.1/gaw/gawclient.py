35a0e733e8b6f458e67f3636b5be3bf525a3a6ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:49 +0000

    Add package gaw/0.5.1/gaw-0.5.1.tar.gz

---
+++ packages/gaw/gaw-0.5.1.tar.gz/gaw-0.5.1/gaw/gawclient.py
@@ -0,0 +1,74 @@
+from __future__ import print_function, absolute_import
+from gaw.jsonsocketserver import JsonSocketClient
+
+class GawClient:
+
+    def __init__(self, ip, port, secret=None, is_encrypt=False,
+                 connection_lifetime = 30, verbose=False,
+                 request_maker=None,
+                 _state=0, _service_name=None, _method_name=None):
+        self.ip = ip
+        self.port = port
+        self.secret = secret
+        self.is_encrypt = is_encrypt
+        self.connection_lifetime = connection_lifetime
+        self.verbose = verbose
+
+        if request_maker is None:
+            self.request_maker = JsonSocketClient(client_lifetime=connection_lifetime, verbose=verbose)
+        else:
+            self.request_maker = request_maker
+
+        self._state = _state
+        self._service_name = _service_name
+        self._method_name = _method_name
+
+    def __getattr__(self, item):
+        # dynamic method calling handler
+        if self._state == 0:
+            state = 1
+            service_name = item
+
+            return GawClient(
+                ip=self.ip, port=self.port,
+                secret=self.secret, is_encrypt=self.is_encrypt,
+                connection_lifetime=self.connection_lifetime,
+                verbose=self.verbose,
+                request_maker=self.request_maker,
+                _state=state,
+                _service_name=service_name,
+                _method_name=self._method_name
+            )
+        elif self._state == 1:
+            method_name = item
+
+            return self.get_procedure_caller(
+                service_name=self._service_name,
+                method_name=method_name,
+                ip=self.ip,
+                port=self.port,
+                secret=self.secret,
+                is_encrypt=self.is_encrypt,
+                request_maker=self.request_maker,
+                verbose=self.verbose
+            )
+        else:
+            raise ValueError('state not recognized')
+
+    def get_procedure_caller(self, service_name, method_name,
+                             ip, port, secret, is_encrypt,
+                             request_maker, verbose):
+        assert isinstance(request_maker, JsonSocketClient), 'request maker should be a jsonsocketclient'
+        path = '{}/{}'.format(service_name, method_name)
+
+        def rpc(*args, **kwargs):
+            if verbose:
+                print('gawclient: procedure call path', path, 'args:', args, 'kwargs:', kwargs)
+
+            return request_maker.request(ip=ip, port=port, path=path,
+                                         secret=secret, is_encrypt=is_encrypt,
+                                         payload=dict(
+                                             args=args,
+                                             kwargs=kwargs
+                                         ))
+        return rpc
---
+++ packages/gaw/gaw-0.5.1.tar.gz/gaw-0.5.1/gaw/gawserver.py
@@ -0,0 +1,67 @@
+from __future__ import print_function, absolute_import
+from gaw.entrypoint import Entrypoint
+from gaw.jsonsocketserver import JsonSocketServer
+
+def pluck(d, *args):
+    assert isinstance(d, dict)
+    return (d[arg] for arg in args)
+
+class GawServer:
+
+    def __init__(self, ip, port, secret=None, is_encrypt=False, verbose=False):
+        self.ip = ip
+        self.port = port
+        self.secret = secret
+        self.is_encrypt = is_encrypt
+        self.verbose = verbose
+        self.socketserver = JsonSocketServer(ip, port, secret=secret, is_encrypt=is_encrypt, verbose=verbose)
+
+        self.path = dict()
+
+    def add(self, service_class, *args, **kwargs):
+        service_name = service_class.name
+
+        print('gawserver: running service', service_name)
+
+        entry_methods = Entrypoint.get_entrypoints_from_class(service_class)
+
+        for method_name in entry_methods:
+            path = '{}/{}'.format(service_name, method_name)
+
+            if self.verbose: print('gawserver: add path {}'.format(path))
+
+            # add path
+            self.socketserver.register_route(path, self.router)
+
+            # save misc
+            self.path[path] = dict(service_name=service_name,
+                                   service_class=service_class,
+                                   method_name=method_name,
+                                   args=args,
+                                   kwargs=kwargs)
+
+        return self
+
+    def run(self):
+        self.socketserver.start()
+
+    def router(self, path, **payload):
+        if self.verbose:
+            print('gawserver: router path:', path)
+
+        cls, method_name, args, kwargs = pluck(self.path[path], 'service_class', 'method_name', 'args', 'kwargs')
+
+        # init a new instance of the class
+        cls_instance = cls(*args, **kwargs)
+
+        # call the designated method
+        args = payload['args']
+        kwargs = payload['kwargs']
+        result = getattr(cls_instance, method_name)(*args, **kwargs)
+
+        if self.verbose:
+            print('gawserver: result ', result)
+
+        return result
+
+
