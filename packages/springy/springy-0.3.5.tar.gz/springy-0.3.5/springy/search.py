35a3b69ab0b9ac222949744788dc38348f53ad42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:51 +0000

    Add package springy/0.3.5/springy-0.3.5.tar.gz

---
+++ packages/springy/springy-0.3.5.tar.gz/springy-0.3.5/springy/search.py
@@ -0,0 +1,31 @@
+from elasticsearch_dsl import Search as BaseSearch, Q
+
+
+class IterableSearch(BaseSearch):
+    """
+    This class is used to make search class iterable
+    (also fixes https://github.com/elastic/elasticsearch-dsl-py/issues/279)
+    """
+
+    def __iter__(self):
+        return iter(self.execute())
+
+    def __len__(self):
+        return self.count()
+
+    def execute(self):
+        try:
+            return self._cached_result
+        except AttributeError:
+            self._cached_result = super(IterableSearch, self).execute()
+            return self._cached_result
+
+    def parse(self, query):
+        return self.query('query_string', query=query, use_dis_max=True)
+
+    def raw(self, raw_dict):
+        return self.update_from_dict(raw_dict)
+
+
+class Search(IterableSearch):
+    pass
