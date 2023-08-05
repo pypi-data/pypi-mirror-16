5d2313e30da2a34d26512b27ea4ab0c49db13c65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:53 +0000

    Add package django-buckets/0.1.7/django-buckets-0.1.7.tar.gz

---
+++ packages/django-buckets/django-buckets-0.1.7.tar.gz/django-buckets-0.1.7/buckets/test/views.py
@@ -0,0 +1,24 @@
+import os
+from django.core.files.storage import default_storage
+from django.views.decorators.http import require_POST
+from django.http import HttpResponse
+from django.conf import settings
+
+from buckets.test.utils import ensure_dirs
+
+
+@require_POST
+def fake_s3_upload(request):
+    ensure_dirs()
+    key = request.POST.get('key')
+
+    if '/' in key:
+        path = os.path.join(settings.MEDIA_ROOT,
+                            's3/uploads', key[:key.rfind('/')])
+        if not os.path.exists(path):
+            os.makedirs(path)
+
+    file = request.FILES.get('file')
+    default_storage.save(key, file.read())
+
+    return HttpResponse('', status=204)
