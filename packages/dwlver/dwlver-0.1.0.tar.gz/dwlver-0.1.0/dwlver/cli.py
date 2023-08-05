bee3f63daf3cfca523b6ca81f7a19cfdadbd0e95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:04 +0000

    Add package dwlver/0.1.0/dwlver-0.1.0.tar.gz

---
+++ packages/dwlver/dwlver-0.1.0.tar.gz/dwlver-0.1.0/dwlver/cli.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+import click
+
+@click.command()
+def main(args=None):
+    """Console script for dwlver"""
+    click.echo("Replace this message by putting your code into "
+                "dwlver.cli.main")
+    click.echo("See click documentation at http://http://click.pocoo.org/")
+
+
+if __name__ == "__main__":
+    main()
