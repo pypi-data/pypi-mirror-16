f7c7e0496003a10ad72b63ae8e5c51833534e008
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:03:58 +0000

    Add package bluecanary/0.0.7/bluecanary-0.0.7.tar.gz

---
+++ packages/bluecanary/bluecanary-0.0.7.tar.gz/bluecanary-0.0.7/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+import os
+import sys
+
+__version__ = '0.0.7'
+__requirements__ = [
+    "boto3==1.2.3",
+    "click==6.2",
+    "pluginbase==0.3",
+    "PyYAML==3.11",
+]
+
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    os.system('python setup.py bdist_wheel upload')
+    sys.exit()
+
+
+setup(
+    name='bluecanary',
+    version=__version__,
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=__requirements__,
+    entry_points='''
+    [console_scripts]
+    bluecanary=bluecanary.scripts.bluecanary:cli
+    ''',
+)
