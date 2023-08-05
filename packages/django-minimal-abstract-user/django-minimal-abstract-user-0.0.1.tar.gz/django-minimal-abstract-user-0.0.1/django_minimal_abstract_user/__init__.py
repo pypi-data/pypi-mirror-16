46037ea16d34376bbe941ec082f1109290ce042f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:54 +0000

    Add package django-minimal-abstract-user/0.0.1/django-minimal-abstract-user-0.0.1.tar.gz

---
+++ packages/django-minimal-abstract-user/django-minimal-abstract-user-0.0.1.tar.gz/django-minimal-abstract-user-0.0.1/django_minimal_abstract_user/__init__.py
@@ -0,0 +1,5 @@
+from django.contrib.auth.models import AbstractUser as AbstractUserWithJunk
+from django_abstract_utils import without
+
+
+AbstractUser = without(AbstractUserWithJunk, 'username', 'first_name', 'last_name', 'email')
