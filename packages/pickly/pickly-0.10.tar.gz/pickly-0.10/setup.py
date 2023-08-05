3d03c5c0db4ced8534ee84ffb7e0fe5574861340
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:13 +0000

    Add package pickly/0.10/pickly-0.10.tar.gz

---
+++ packages/pickly/pickly-0.10.tar.gz/pickly-0.10/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='pickly',
+    version='0.10',
+    py_modules=['pickly'],
+    description='JSON object mapping in Python',
+    url='http://github.com/itsnauman/pickly',
+    author='Nauman Ahmad',
+    author_email='nauman-ahmad@outlook.com',
+    license='MIT',
+    include_package_data=True,
+    packages=['pickly']
+)
