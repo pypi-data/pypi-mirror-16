6a1dc22d5876545ceb0ff2d3ab82c12ca9823938
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:57 +0000

    Add package knee_nester/1.3.0/knee_nester-1.3.0.tar.gz

---
+++ packages/knee_nester/knee_nester-1.3.0.tar.gz/knee_nester-1.3.0/setup.py
@@ -0,0 +1,18 @@
+'''
+created		:	02-JUL-2016
+author		:	kneerunjun@gmail.com
+description	:	sets up the moudule for distribution
+'''
+
+from distutils.core import setup
+
+setup(
+	name="knee_nester",
+	py_modules=["knee_nester"],
+	author="kneerunjun",
+	author_email="kneerunjun@gmail.com",
+	version="1.3.0",
+	description="improved indentation and compatibility to use the old api as well",
+	url="http://kneerunjun.wordpress.com"
+)
+
