2d3cc4c147325127eac08bfac973bc202211203d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:06 +0000

    Add package lianxiw/1.2.0/lianxiw-1.2.0.zip

---
+++ packages/lianxiw/lianxiw-1.2.0.zip/lianxiw-1.2.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+
+setup(
+		name               = 'lianxiw',
+		version            = '1.2.0',
+		py_modules         = ['lianxiw'],
+		author             = 'cuiyu88',
+		author_email       = '582928308@qq.com',
+		url                = 'http://www.headfirstlabs.com',
+		description        = 'A simple printer of nested lists',
+	)
