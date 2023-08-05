149f25fad9a774aaad44efdb9ecbcf22b82020f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:13 +0000

    Add package picom/0.5.5/picom-0.5.5.zip

---
+++ packages/picom/picom-0.5.5.zip/picom-0.5.5/picom/__init__.py
@@ -0,0 +1,16 @@
+
+__version__ = '0.5.5'
+
+from .main import store_API_id
+from .main import store_all
+from .main import store_credentials
+from .main import store_id
+from .main import store_ip
+from .main import storeAPI_key
+from .main import get_api_free
+from .main import get_credentials
+from .main import renew_credentials
+from .main import send_alarm_signal
+from .main import test_connection
+from .main import update_garage_state
+from .main import get_garages
