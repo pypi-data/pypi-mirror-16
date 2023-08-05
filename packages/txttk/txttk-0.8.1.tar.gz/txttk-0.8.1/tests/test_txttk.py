d0403514cd3517cf05bdafe708347f31000a372d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:18 +0000

    Add package txttk/0.8.1/txttk-0.8.1.tar.gz

---
+++ packages/txttk/txttk-0.8.1.tar.gz/txttk-0.8.1/tests/test_txttk.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import (absolute_import, division,
+                        print_function, unicode_literals)
+from builtins import *
+
+"""
+test_txttk
+----------------------------------
+
+Tests for `txttk` module.
+"""
+
+import unittest
+
+from txttk import txttk
+
+
+class TestTxttk(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_something(self):
+        pass
+
+    def tearDown(self):
+        pass
