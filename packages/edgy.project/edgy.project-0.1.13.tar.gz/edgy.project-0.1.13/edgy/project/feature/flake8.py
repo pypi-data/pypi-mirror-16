8a460d6236bb9f08ae57a0b6a6833c4925958a74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:37 +0000

    Add package edgy.project/0.1.13/edgy.project-0.1.13.tar.gz

---
+++ packages/edgy.project/edgy.project-0.1.13.tar.gz/edgy.project-0.1.13/edgy/project/feature/flake8.py
@@ -0,0 +1,22 @@
+# coding: utf-8
+
+from __future__ import absolute_import, print_function, unicode_literals
+
+from . import Feature, SUPPORT_PRIORITY
+
+
+class Flake8Feature(Feature):
+    requires = {'python'}
+
+    def configure(self):
+        self.dispatcher.add_listener('edgy.project.feature.make.on_generate', self.on_make_generate,
+                                     priority=SUPPORT_PRIORITY)
+
+    def on_make_generate(self, event):
+        makefile = event.makefile
+        makefile.add_target('lint', '''
+            $(VIRTUAL_ENV)/bin/flake8 {name}
+        '''.format(name=event.package_name), deps=('install',), phony=True)
+
+
+__feature__ = Flake8Feature
