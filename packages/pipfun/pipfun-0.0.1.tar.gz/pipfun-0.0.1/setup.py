b8eeede79b73f0d2d8f249b764677ba33f38a828
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:10:16 +0000

    Add package pipfun/0.0.1/pipfun-0.0.1.tar.gz

---
+++ packages/pipfun/pipfun-0.0.1.tar.gz/pipfun-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+ 
+setup(
+    name = 'pipfun',
+    packages = ['pip_project'],
+    version = '0.0.1',
+    description = 'Does nothing much',
+    author='Umut Orman',
+    author_email='umutormans@gmail.com',
+    url='https://github.com/youraccount/foolib',
+)
