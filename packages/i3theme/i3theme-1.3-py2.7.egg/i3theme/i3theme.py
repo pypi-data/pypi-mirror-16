4b6d8f3c4908cbf86dc1deee10d53ac58617681f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:26 +0000

    Add package i3theme/1.3/i3theme-1.3-py2.7.egg

---
+++ packages/i3theme/i3theme-1.3-py2.7.egg/i3theme/i3theme.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+
+from utils.themeparser import ThemeParser
+from utils.filebridge import FileBridge
+from utils.argparser import argParser
+
+import os
+from ecdsa.ecdsa import __main__
+
+
+def main():
+    args=argParser.parse_args()
+
+
+    if args.clean:
+        
+        print "Cleaning configuration file..."
+        FileBridge.cleanConfigFile(os.path.join(os.environ["HOME"],".i3/config"))
+        print "Done !"
+    elif args.list:
+        themesFolderPath=os.path.join(os.path.dirname(os.path.realpath(__file__)),"themes")
+        for root, dirs, files in os.walk(themesFolderPath):
+            for file in files:
+                print file
+
+    elif args.file != None:
+        print "Apply theme"
+        themeParser=ThemeParser(args.file)
+        fileBridge=FileBridge(themeParser)
+        fileBridge.apply()
+        print "Done !"
+    elif args.theme != None:
+        print "Apply theme"
+        themesFilePath=os.path.join(os.path.dirname(os.path.realpath(__file__)), 'themes',args.theme)
+        themeParser=ThemeParser(themesFilePath)
+        fileBridge=FileBridge(themeParser)
+        fileBridge.apply()
+        print "Done !"
+
+
+
+if __name__ == "__main__":
+    main()
