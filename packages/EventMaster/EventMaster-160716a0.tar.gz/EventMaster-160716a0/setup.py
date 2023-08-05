eac863cd3d1901a77e3f314fcb9105a6463c16da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:03:56 +0000

    Add package EventMaster/160716a0/EventMaster-160716a0.tar.gz

---
+++ packages/EventMaster/EventMaster-160716a0.tar.gz/EventMaster-160716a0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+  name = 'EventMaster',
+  packages = ['EventMaster'],
+  version = '160716.a',
+  description = 'Unofficial Python Library for connecting to, reading from and controlling a Barco E2, S3 or other EventMaster Switcher',
+  classifiers = [
+    "Development Status :: 3 - Alpha",
+    "Intended Audience :: Developers",
+    "Programming Language :: Python :: 2.7",
+    "Programming Language :: Python :: 3.3"
+  ],
+  author = 'Kye Lewis',
+  author_email = 'klewis@stagingconnections.com',
+  url = 'http://github.com/kyelewisstgc/EventMaster-Python',
+  download_url = 'https://github.com/kyelewisstgc/EventMaster-Python/tarball/0.1',
+  keywords = 'control barco E2 S3 eventmaster'
+  )
