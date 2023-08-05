559c17eeffcd00c548949e4115e28cf8eb41fcc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:54 +0000

    Add package pypolyback/0.9.1/pypolyback-0.9.1-py2-none-any.whl

---
+++ packages/pypolyback/pypolyback-0.9.1-py2-none-any.whl/pypolyback/config.py
@@ -0,0 +1,42 @@
+import os
+import json
+
+def load(scope='default'):
+    """
+    loads choosen config file
+    if scope is not define loads default (first try `local`, then `dev` and last `prod`)
+    
+    Args:
+        scope: config file, without `.json`
+    """
+    
+    if scope is 'default':
+        for scope in ['local', 'dev', 'prod']:
+            path = 'config/{}.json'.format(scope)
+            if os.path.exists(path):
+                config = load(scope)
+                if config:
+                    return config
+                    
+    else:
+        path = 'config/{}.json'.format(scope)
+        if os.path.exists(path):
+            with open(path, 'r') as config:
+                obj = json.loads(config.read())
+                obj['scope'] = scope
+
+                if 'server' not in obj:
+                    obj['server'] = {}
+                else:
+                    fill_default_value(obj['server'], 'port', 8888)
+                    fill_default_value(obj['server'], 'disable_cors', False)
+
+                return obj
+    raise EnvironmentError('No config file found')
+
+def fill_default_value(obj, key, default):
+    if key not in obj:
+        obj[key] = default
+
+#shortcut to current config
+value = load()
