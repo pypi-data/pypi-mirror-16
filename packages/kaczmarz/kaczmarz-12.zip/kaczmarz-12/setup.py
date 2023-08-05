52ec721245fd564ba8d79d98f8f176876712979c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:24 +0000

    Add package kaczmarz/12/kaczmarz-12.zip

---
+++ packages/kaczmarz/kaczmarz-12.zip/kaczmarz-12/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+try:
+	with open("README.md") as rdmf:
+		for i, line in enumerate(rdmf):
+			if i == 1:
+				desc = line
+			elif i > 1:
+				break
+except FileNotFoundError:
+	desc = 'Simulations and visualizations of the Kaczmarz algorithm on various classifications of linear systems'
+	pass
+
+setup(name='kaczmarz',
+	version='12',
+	description=desc,
+	url='https://github.com/lightningleaf/kaczmarz',
+	author='lightningleaf',
+	author_email='lightningleaf0@gmail.com',
+	license='None',
+	packages=['kaczmarz'],
+	install_requires=[
+	'recordclass',
+	'numpy'
+	],
+	zip_safe=False,
+	classifiers=[
+	'Programming Language :: Python :: 3.5',
+	]
+	)
