825c97d29e2c82cf1b058bad5b120c9ea18b7a2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:10 +0000

    Add package pytsite/0.79.3/PytSite-0.79.3.tar.gz

---
+++ packages/pytsite/PytSite-0.79.3.tar.gz/PytSite-0.79.3/pytsite/content_import/_eh.py
@@ -0,0 +1,90 @@
+"""PytSite Content Import Event Handlers.
+"""
+from datetime import datetime as _datetime, timedelta as _timedelta
+from frozendict import frozendict as _frozendict
+from pytsite import odm as _odm, logger as _logger, content as _content, reg as _reg
+from . import _api, _model, _error
+
+__author__ = 'Alexander Shepetko'
+__email__ = 'a@shepetko.com'
+__license__ = 'MIT'
+
+
+def odm_model_setup_indexes(entity: _odm.model.Entity):
+    if isinstance(entity, _content.model.Content):
+        entity.define_field(_odm.field.Dict('content_import'))
+        entity.define_index([('content_import.source_domain', _odm.I_ASC)])
+
+
+def cron_1min():
+    """'pytsite.cron.1min' event handler.
+    """
+    max_errors = _reg.get('content_import.max_errors', 13)
+    delay_errors = _reg.get('content_export.delay_errors', 120)
+
+    importer_finder = _odm.find('content_import') \
+        .where('enabled', '=', True) \
+        .where('paused_till', '<', _datetime.now()) \
+        .sort([('errors', _odm.I_ASC)])
+
+    for importer in importer_finder.get():  # type: _model.ContentImport
+        options = dict(importer.driver_opts)
+        options.update({
+            'content_author': importer.content_author,
+            'content_model': importer.content_model,
+            'content_language': importer.content_language,
+            'content_status': importer.content_status,
+            'content_section': importer.content_section,
+        })
+
+        driver = _api.get_driver(importer.driver)
+        max_items = 20
+        items_imported = 0
+        try:
+            _logger.info('Content import started. Driver: {}. Options: {}'.format(driver.get_name(), options), __name__)
+
+            for entity in driver.get_entities(_frozendict(options)):
+                if items_imported == max_items:
+                    break
+
+                try:
+                    # Append additional tags
+                    if entity.has_field('tags'):
+                        for tag_title in importer.add_tags:
+                            entity.f_add('tags', _content.dispense_tag(tag_title).save())
+
+                    # Save entity
+                    entity.save()
+
+                    # Remove first image from body
+                    if entity.has_field('body') and entity.has_field('images'):
+                        entity.f_set('body', entity.f_get('body', process_tags=False).replace('[img:1]', ''))
+
+                    _logger.info("Content entity imported: '{}'".format(entity.f_get('title')), __name__)
+                    items_imported += 1
+
+                except Exception as e:
+                    # Delete attached images
+                    if entity.has_field('images'):
+                        for img in entity.f_get('images'):
+                            img.delete()
+
+                    _logger.warn("Error while saving entity '{}'. {}".format(entity.title, str(e)), __name__)
+
+            _logger.info('Content import finished. Entities imported: {}.'.format(items_imported), __name__)
+
+        except _error.ContentImportError as entity:
+            # Increment errors counter
+            importer.f_inc('errors')
+
+            # Store info about error
+            importer.f_set('last_error', str(entity))
+
+            if importer.errors >= max_errors:
+                # Disable if maximum errors count reached
+                importer.f_set('enabled', False)
+            else:
+                # Pausing importer
+                importer.f_set('paused_till', _datetime.now() + _timedelta(minutes=delay_errors))
+
+            _logger.error('Import error: {}.'.format(str(entity)), __name__)
---
+++ packages/pytsite/PytSite-0.79.3.tar.gz/PytSite-0.79.3/pytsite/flag/_api.py
@@ -0,0 +1,117 @@
+"""PytSite Flag API.
+"""
+from pytsite import odm as _odm, auth as _auth, cache as _cache, events as _events
+
+__author__ = 'Alexander Shepetko'
+__email__ = 'a@shepetko.com'
+__license__ = 'MIT'
+
+_CACHE_TTL = 300  # 5 min
+_cache_p = _cache.create_pool('pytsite.flag')
+
+
+def count(entity: _odm.model.Entity, flag_type: str = 'default') -> int:
+    """Get flags count for the entity.
+    """
+    return _odm.find('flag').where('entity', '=', entity).where('type', '=', flag_type).count()
+
+
+def sum(entity: _odm.model.Entity, flag_type: str = 'default') -> float:
+    """Get sum of scores for the entity.
+    """
+    c_key = 'sum.{}.{}'.format(entity.id, flag_type)
+    if _cache_p.has(c_key):
+        return _cache_p.get(c_key)
+
+    ag = _odm.aggregate('flag').match('entity', '=', entity).match('type', '=', flag_type)
+
+    ag.group({
+        '_id': None,
+        'sum': {'$sum': '$score'}
+    })
+
+    r = list(ag.get())
+    v = r[0]['sum'] if r else 0.0
+
+    return _cache_p.put(c_key, v, _CACHE_TTL)
+
+
+def average(entity: _odm.model.Entity, flag_type: str = 'default') -> float:
+    """Get average score for the entity.
+    """
+    c_key = 'average.{}.{}'.format(entity.id, flag_type)
+    if _cache_p.has(c_key):
+        return _cache_p.get(c_key)
+
+    ag = _odm.aggregate('flag').match('entity', '=', entity).match('type', '=', flag_type)
+
+    ag.group({
+        '_id': None,
+        'avg': {'$avg': '$score'}
+    })
+
+    r = list(ag.get())
+    v = r[0]['avg'] if r else 0.0
+
+    return _cache_p.put(c_key, v, _CACHE_TTL)
+
+
+def is_flagged(entity: _odm.model.Entity, user: _auth.model.AbstractUser, flag_type: str = 'default') -> bool:
+    """Check if an entity is flagged by a user.
+    """
+    if user.is_anonymous:
+        return False
+
+    f = _odm.find('flag').where('entity', '=', entity).where('author', '=', user).where('type', '=', flag_type)
+
+    return bool(f.count())
+
+
+def flag(entity: _odm.model.Entity, user: _auth.model.AbstractUser, flag_type: str = 'default', score: float = 1.0):
+    """Flag the entity.
+    """
+    if user.is_anonymous or is_flagged(entity, user):
+        return
+
+    e = _odm.dispense('flag')
+    e.f_set('entity', entity).f_set('author', user).f_set('type', flag_type).f_set('score', score)
+    e.save()
+
+    _events.fire('pytsite.flag.flag', entity=entity, user=user, flag_type=flag_type, score=score)
+
+
+def unflag(entity: _odm.model.Entity, user: _auth.model.AbstractUser, flag_type: str = 'default'):
+    """Remove flag.
+    """
+    if user.is_anonymous or not is_flagged(entity, user):
+        return
+
+    f = _odm.find('flag').where('entity', '=', entity).where('author', '=', user).where('type', '=', flag_type)
+    f.first().delete()
+
+    _events.fire('pytsite.flag.unflag', entity=entity, user=user, flag_type=flag_type)
+
+
+def toggle(entity: _odm.model.Entity, author: _auth.model.AbstractUser, flag_type: str = 'default', score: float = 1.0) -> bool:
+    """Toggle flag.
+    """
+    if author.is_anonymous:
+        return False
+
+    if is_flagged(entity, author, flag_type):
+        unflag(entity, author, flag_type)
+        return False
+    else:
+        flag(entity, author, flag_type, score)
+        return True
+
+
+def delete(entity: _odm.model.Entity) -> int:
+    """Delete all flags for particular entity.
+    """
+    r = 0
+    for flag_entity in _odm.find('flag').where('entity', '=', entity).get():
+        flag_entity.delete()
+        r += 1
+
+    return r
