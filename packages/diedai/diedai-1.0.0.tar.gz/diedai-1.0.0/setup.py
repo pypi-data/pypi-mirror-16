b136420d8cd233e45c7097feb08c11027b942c33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:28 +0000

    Add package diedai/1.0.0/diedai-1.0.0.tar.gz

---
+++ packages/diedai/diedai-1.0.0.tar.gz/diedai-1.0.0/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+from distutils.core import setup
+
+setup (
+	name			= 'diedai',
+	version			= '1.0.0',
+	py_modules		= ['diedai'],
+	author			= 'hfpython',
+	author_email	= 'hfpython@headfirstlabs.com',
+	url				= 'http://www.headfirstlabs.com',
+	description		= 'A simple printer of diedai lists',
+)
