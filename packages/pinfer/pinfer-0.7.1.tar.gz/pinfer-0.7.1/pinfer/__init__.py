1cb5221f751c44a9a1d77b1ae15ee677d7469a78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:55 +0000

    Add package pinfer/0.7.1/pinfer-0.7.1.tar.gz

---
+++ packages/pinfer/pinfer-0.7.1.tar.gz/pinfer-0.7.1/pinfer/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'Nick Fyson'
+__email__ = 'mail@nickfyson.co.uk'
+__version__ = '0.1.0'
+
+import sys
+import os
+
+from .io import load_notung_nhx  # NOQA
+from .itree import build_itree  # NOQA
+from .visualise import vis_tree  # NOQA
---
+++ packages/pinfer/pinfer-0.7.1.tar.gz/pinfer-0.7.1/setup.py
@@ -0,0 +1,34 @@
+import os
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup, find_packages
+
+
+def read(fname):
+    # Utility function to read the README file.
+    # Used for the long_description.  It's nice, because now 1) we have a top level
+    # README file and 2) it's easier to type in the README file than to put a raw
+    # string in below ...
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name="pinfer",
+    version="v0.7.1",
+    author="Nick Fyson",
+    author_email="nick@fyson.net",
+    description="Inference of ancestral Protein Interaction Networks (PINs).",
+    long_description=read('README.md'),
+    install_requires=read('requirements.txt'),
+    license="BSD",
+    keywords='PPI protein modelling interaction',
+    url='https://github.com/nickfyson/pinfer',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+    ],
+)
