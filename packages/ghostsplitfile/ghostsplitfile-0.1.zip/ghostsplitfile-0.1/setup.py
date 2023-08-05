b6d569a6a1bbe427112d39ccbca730a949126f19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:10:01 +0000

    Add package ghostsplitfile/0.1/ghostsplitfile-0.1.zip

---
+++ packages/ghostsplitfile/ghostsplitfile-0.1.zip/ghostsplitfile-0.1/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup  
+setup(  
+        name = 'ghostsplitfile',  
+        version = '0.1',  
+        py_modules = ['ghostsplitfile'],  
+        author = 'ghostbehemoth',  
+        author_email = 'ghostbehemoth@gmail.com',  
+        url = 'http://www.pythonphp.com',  
+        description = 'A printer of local document by separator',  
+    )  
