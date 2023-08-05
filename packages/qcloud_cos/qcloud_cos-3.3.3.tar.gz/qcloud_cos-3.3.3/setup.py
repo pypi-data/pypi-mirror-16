96936629d7cd2ded60c5cb7728bda3cbdf248601
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:41 +0000

    Add package qcloud_cos/3.3.3/qcloud_cos-3.3.3.tar.gz

---
+++ packages/qcloud_cos/qcloud_cos-3.3.3.tar.gz/qcloud_cos-3.3.3/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+import sys
+if sys.version_info[0] != 2 or sys.version_info[1] < 7:
+    sys.exit('Sorry, only python 2.7 is supported')
+
+from setuptools import setup, find_packages
+setup(
+        name = 'qcloud_cos',
+        version = '3.3.3',
+        description = 'python sdk for tencent qcloud cos',
+        license = 'MIT License',
+        install_requires=['requests'],
+
+        author = 'chengwu',
+        author_email = 'chengwu@tencent.com',
+
+        packages = find_packages(),
+        )
