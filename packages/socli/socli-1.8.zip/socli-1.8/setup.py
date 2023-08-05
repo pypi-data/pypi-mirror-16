b6eacda37c802a17c300386ba6254456a369aa11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:32 +0000

    Add package socli/1.8/socli-1.8.zip

---
+++ packages/socli/socli-1.8.zip/socli-1.8/setup.py
@@ -0,0 +1,26 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+from sys import exit,version
+import sys
+if version < '1.0.0':
+    print("Python 1 is not supported...")
+    sys.exit(1)
+
+setup(
+    name='socli',
+    packages=["socli"],
+    entry_points = {"console_scripts": ['socli = socli.socli:main']},
+    install_requires=['BeautifulSoup4','requests'],
+    requires=['BeautifulSoup4','requests'],
+    version='1.8',
+    url='http://www.github.com/gautamkrishnar/socli',
+    keywords="stack overflow cli",
+    license='BSD',
+    author='Gautam krishna R',
+    author_email='r.gautamkrishna@gmail.com',
+    description='Stack overflow commnand line interface. SoCLI allows you to search and browse stack overfow from the terninal.',
+    long_description="Go to www.github.com/gautamkrishnar/socli"
+    )
