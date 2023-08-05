30f30689b3215b044d283909efa5b24f79d28f61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:00 +0000

    Add package opensm/0.1.0/opensm-0.1.0.zip

---
+++ packages/opensm/opensm-0.1.0.zip/opensm-0.1.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+setup(
+    name='opensm',
+    description='Sound mixer',
+    author='John Iannandrea',
+    author_email='jiannandrea@gmail.com',
+    url='http://github.com/isivisi/opensoundmixer',
+    install_requires=[
+        'requests',
+        'numpy'
+    ],
+    include_package_data=True,
+    version='0.1.0',
+    packages=['opensm'],
+    zip_safe=False,
+    license='GNU',
+    keywords='sound mixer',
+    classifiers=[
+        'Programming Language :: Python :: 3.5',
+        'Operating System :: OS Independent',
+    ],
+    entry_points={
+        'console_scripts': [
+            'opensm = opensm.osm:main'
+        ]
+    }
+)
