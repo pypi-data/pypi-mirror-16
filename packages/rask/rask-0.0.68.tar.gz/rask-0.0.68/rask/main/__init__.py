a53d0279b2265079cf39137a13f1d0e4589c7975
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:05 +0000

    Add package rask/0.0.68/rask-0.0.68.tar.gz

---
+++ packages/rask/rask-0.0.68.tar.gz/rask-0.0.68/rask/main/__init__.py
@@ -0,0 +1,52 @@
+from rask.base import Base
+from rask.options import define,options,parse_command_line
+from tornado.web import Application
+
+__all__ = ['Main']
+
+class Main(Base):
+    __http__ = None
+    __http_routes__ = []
+    __options__ = {
+        'http':{
+            'port':8088
+        },
+        'rmq':{
+            'channel':{
+                'prefetch':10
+            }
+        }
+    }
+    
+    def __init__(self):
+        self.setup()
+        self.before()
+        self.ioengine.loop.add_callback(self.after)
+        self.ioengine.loop.add_callback(self.http)
+        self.ioengine.start()
+
+    def after(self):
+        pass
+        
+    def before(self):
+        pass
+
+    def http(self):
+        try:
+            assert self.__http_routes__
+        except AssertionError:
+            return False
+        except:
+            raise
+        else:
+            self.__http_app__ = Application(self.__http_routes__,autoreload=options.autoreload)
+            self.__http__ = self.__http_app__.listen(options.rask['http']['port'])
+            self.__http__.xheaders = True
+            self.log.info('HTTP Server - %s' % options.rask['http']['port'])
+        return True
+    
+    def setup(self):
+        define('autoreload',default=False)
+        define('rask',default=self.__options__)
+        parse_command_line()
+        return True
