c20e77df26b7abab5709ac1908930f594201bf1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:31 +0000

    Add package jupyterthemes/0.4.6/jupyterthemes-0.4.6.tar.gz

---
+++ packages/jupyterthemes/jupyterthemes-0.4.6.tar.gz/jupyterthemes-0.4.6/setup.py
@@ -0,0 +1,40 @@
+import os
+from setuptools import setup
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='jupyterthemes',
+    version='0.4.6',
+    packages=['jupyterthemes'],
+    include_package_data=True,
+    package_data={'jupyterthemes': ['sandbox/*.js', 'styles/*.css']},
+    description='Select and install a Jupyter notebook theme',
+    long_description=README,
+    license='MIT',
+    url='https://github.com/dunovank/jupyter-themes/',
+    download_url='https://github.com/dunovank/jupyter-themes/tarball/0.2',
+    author='dunovank, miraculixx',
+    author_email='dunovank@gmail.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+    keywords=['jupyter', 'ipython', 'notebook', 'themes', 'css'],
+    entry_points={
+        'console_scripts': [
+            'jupyter-theme = jupyterthemes:main',
+            'jt = jupyterthemes:main',
+        ],
+    }
+)
