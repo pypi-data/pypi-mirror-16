d0adeb2eb30c411aac80eecbf9f04dfbcb47c656
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:03 +0000

    Add package simplesat/0.6.0/simplesat-0.6.0-py2.py3-none-any.whl

---
+++ packages/simplesat/simplesat-0.6.0-py2.py3-none-any.whl/simplesat/_version.py
@@ -0,0 +1,9 @@
+# THIS FILE IS GENERATED FROM SETUP_EXT
+version = '0.6.0'
+full_version = '0.6.0'
+git_revision = '267f9b9377a0094ad4f819559f0f8e2b164bf765'
+is_released = True
+
+msi_version = '0.6.0.814'
+
+version_info = (0, 6, 0, 'final', 0)
---
+++ packages/simplesat/simplesat-0.6.0-py2.py3-none-any.whl/simplesat/errors.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+class SolverException(Exception):
+    pass
+
+
+class UnexpectedlySatisfiable(SolverException):
+    pass
+
+
+class InvalidConstraint(SolverException):
+    pass
+
+
+class InvalidDependencyString(InvalidConstraint):
+    pass
+
+
+class NoPackageFound(SolverException):
+    def __init__(self, requirement, *a, **kw):
+        # NOTE: Working around a circular import
+        from simplesat.constraints.requirement import Requirement
+
+        if not isinstance(requirement, Requirement):
+            msg = "NoPackageFound expects a Requirement as first arg. Got {!r}"
+            raise TypeError(msg.format(requirement))
+        super(NoPackageFound, self).__init__(*a, **kw)
+        self.requirement = requirement
+        self.args = self.args or (str(requirement),)
+
+
+class MissingInstallRequires(NoPackageFound):
+    pass
+
+
+class MissingConflicts(NoPackageFound):
+    pass
+
+
+class SatisfiabilityError(SolverException):
+    def __init__(self, unsat):
+        self.unsat = unsat
+        super(SatisfiabilityError, self).__init__(self.reason)
+
+    @property
+    def reason(self):
+        return self.unsat.to_string()
