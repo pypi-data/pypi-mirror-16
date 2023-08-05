5177427103bcad6d042ff73c9a8bec31bc2889d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:03:53 +0000

    Add package Text.1/1.2.5/Text.1-1.2.5.zip

---
+++ packages/Text.1/Text.1-1.2.5.zip/Text.1-1.2.5/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name = 'Text.1',
+	version = '1.2.5',
+	py_modules = ['text'],
+	description = 'text package',
+	author = "DUO",
+	author_email = 'DUO.1080@gmail.com',
+	url = "http://www.DUO.org",
+	)
