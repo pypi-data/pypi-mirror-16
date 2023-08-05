87a746eeacf831b7e267d034e592915266b80785
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:36 +0000

    Add package nesterrr01/1.2.0/nesterrr01-1.2.0.tar.gz

---
+++ packages/nesterrr01/nesterrr01-1.2.0.tar.gz/nesterrr01-1.2.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+	name			= 'nesterrr01',
+	version			= '1.2.0',
+	py_modules		= ['nesterrr01'],
+	author			= 'adamzzz',
+	author_email	= 'amunguia86@icloud.com',
+	url				= 'http://www.icloud.com',
+	description		= 'A simple printer of nested lists',
+)
