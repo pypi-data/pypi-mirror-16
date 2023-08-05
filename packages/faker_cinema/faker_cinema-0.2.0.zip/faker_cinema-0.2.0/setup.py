94a61c176d8fb29c83e16df3312467a474014a65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:10 +0000

    Add package faker_cinema/0.2.0/faker_cinema-0.2.0.zip

---
+++ packages/faker_cinema/faker_cinema-0.2.0.zip/faker_cinema-0.2.0/setup.py
@@ -0,0 +1,35 @@
+import json
+from os import path
+
+from setuptools import setup, find_packages
+
+
+def _get_version():
+    version_file = path.join(path.abspath(path.dirname(__file__)), 'version.json')
+
+    with open(version_file) as ver:
+        version = json.load(ver)
+        return '{0}.{1}.{2}'.format(version['major'], version['minor'], version['patch'])
+
+
+def _get_requirements():
+    requirements_txt = path.join(path.abspath(path.dirname(__file__)), 'requirements.txt')
+
+    with open(requirements_txt) as req:
+        return [line.strip() for line in req]
+
+
+setup(
+    name='faker_cinema',
+    description='A cinema industry-specific provider for faker',
+    keywords=['testing', 'faker-provider'],
+    version=_get_version(),
+    install_requires=_get_requirements(),
+    package_dir={'faker_cinema': 'faker_cinema'},
+    packages=find_packages(exclude=['*tests*']),
+    author='Michael Hinstridge',
+    author_email='mhinstridge@gmail.com',
+    license='MIT License',
+    url='https://github.com/RangerOfFire/faker-cinema',
+    download_url='https://github.com/RangerOfFire/faker-cinema/tarball/release/{0}'.format(_get_version())
+)
