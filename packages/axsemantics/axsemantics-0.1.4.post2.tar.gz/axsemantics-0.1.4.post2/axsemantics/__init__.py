b600eb5bd63fd21977b3b4532c691f47c62e81e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:15 +0000

    Add package axsemantics/0.1.4.post2/axsemantics-0.1.4.post2.tar.gz

---
+++ packages/axsemantics/axsemantics-0.1.4.post2.tar.gz/axsemantics-0.1.4.post2/axsemantics/__init__.py
@@ -0,0 +1,20 @@
+# AX-Semantics Python bindings
+# API docs at https://apidocs.ax-semantics.com
+import axsemantics.constants
+from axsemantics.errors import (
+    APIConnectionError,
+    APIError,
+    AuthenticationError,
+)
+from axsemantics.resources import (
+    ContentProject,
+    ContentProjectList,
+    Thing,
+    Training,
+    TrainingList,
+)
+from axsemantics.utils import (
+    login,
+    login_myax,
+    login_rincewind,
+)
