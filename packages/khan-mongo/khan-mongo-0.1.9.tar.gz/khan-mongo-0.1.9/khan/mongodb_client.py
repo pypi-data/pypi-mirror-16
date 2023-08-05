39b7a82b34943403222bfdb79e6f57c03e1f69e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:10 +0000

    Add package khan-mongo/0.1.9/khan-mongo-0.1.9.tar.gz

---
+++ packages/khan-mongo/khan-mongo-0.1.9.tar.gz/khan-mongo-0.1.9/khan/mongodb_client.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+import logging
+import pymongo
+
+LOG = logging.getLogger(__name__)
+
+MONGO_CONNECTION_DEFAULT_TIMEOUT = 5000
+MONGO_AUTHENTICATION_ERROR_CODE = 18
+
+
+class AuthenticationError(Exception):
+    pass
+
+
+class ConnectionError(Exception):
+    pass
+
+
+class MongoDB(object):
+    def __init__(self, host, port, database,
+                 user, password, timeout=MONGO_CONNECTION_DEFAULT_TIMEOUT,
+                 mongodb_client=pymongo.MongoClient):
+        self._host = host
+        self._port = port
+        self._database = database
+        self._user = user
+        self._password = password
+        self._timeout = timeout
+        self._mongodb_client = mongodb_client
+        self._authenticate()
+
+    @property
+    def _connection_string(self):
+        return 'mongodb://{user}:{password}@{host}:{port}'.format(
+            user=self._user, password=self._password, host=self._host, port=self._port)
+     
+    def _init_database_client(self):
+        client = getattr(self._mongodb_client(
+            self._connection_string,
+            connectTimeoutMS=self._timeout
+
+        ), self._database)
+
+        return client
+
+    def _authenticate(self):
+        self._client = self._init_database_client()
+        try:
+            return self._client.authenticate(self._user, self._password)
+        except pymongo.errors.OperationFailure as e:
+            if e.code == MONGO_AUTHENTICATION_ERROR_CODE:
+                error_message = 'Invalid credentials to database {}: {}'.format(
+                    self._database, self._connection_string)
+                raise AuthenticationError(error_message)
+
+    def _execute(self, func, attempts=10, command=None):
+        try:
+            return func(command)
+        except pymongo.errors.PyMongoError as e:
+            if attempts > 0 and getattr(e, 'code', 0) != MONGO_AUTHENTICATION_ERROR_CODE:
+                print('Reconnecting... Attemps: {}'.format(attempts))
+                self._authenticate()
+                return self._execute(func, attempts - 1, command)
+            else:
+                error_message = 'Error connecting to database {}: {}\n{}'.format(
+                    self._database, self._connection_string, e)
+                raise ConnectionError(error_message)
+
+    def current_op(self):
+        return self._execute(self._client.current_op)
+
+    def execute_command(self, command):
+        return self._execute(self._client.command, command=command)
