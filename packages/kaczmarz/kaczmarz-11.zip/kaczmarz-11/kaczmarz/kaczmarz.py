0b3af511b7c0937c4bad49dec9d6473e92a45b88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:24 +0000

    Add package kaczmarz/11/kaczmarz-11.zip

---
+++ packages/kaczmarz/kaczmarz-11.zip/kaczmarz-11/kaczmarz/kaczmarz.py
@@ -0,0 +1,9 @@
+import numpy as np
+from recordclass import recordclass
+
+def on(sys, itrs):
+	for i in itrs:
+		row = sys.A[i, :]
+		rej = (row @ sys.x - sys.b[i]) / (row @ row) * row
+		sys.x -= rej
+	return np.copy(x)
