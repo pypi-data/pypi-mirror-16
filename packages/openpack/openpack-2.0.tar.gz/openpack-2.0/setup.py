3d7d0909680cce2c6d35242cb43a2cef26d50675
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:32 +0000

    Add package openpack/2.0/openpack-2.0.tar.gz

---
+++ packages/openpack/openpack-2.0.tar.gz/openpack-2.0/setup.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+
+# Project skeleton maintained at https://github.com/jaraco/skeleton
+
+import io
+import sys
+
+import setuptools
+
+with io.open('README.rst', encoding='utf-8') as readme:
+	long_description = readme.read()
+
+needs_pytest = {'pytest', 'test'}.intersection(sys.argv)
+pytest_runner = ['pytest_runner'] if needs_pytest else []
+needs_sphinx = {'release', 'build_sphinx', 'upload_docs'}.intersection(sys.argv)
+sphinx = ['sphinx', 'rst.linker'] if needs_sphinx else []
+needs_wheel = {'release', 'bdist_wheel'}.intersection(sys.argv)
+wheel = ['wheel'] if needs_wheel else []
+
+name = 'openpack'
+description = ''
+
+setup_params = dict(
+	name=name,
+	use_scm_version=True,
+	author="Christian Wyglendowski (YouGov), Jason R. Coombs (YouGov)",
+	author_email="open.source@yougov.com",
+	description=description or name,
+	long_description=long_description,
+	url="https://github.com/jaraco/" + name,
+	packages=setuptools.find_packages(),
+	include_package_data=True,
+	namespace_packages=name.split('.')[:-1],
+	install_requires=[
+		'lxml',
+		'six',
+		'jaraco.collections>=1.3.2',
+	],
+	extras_require={
+	},
+	setup_requires=[
+		'setuptools_scm>=1.9',
+	] + pytest_runner + sphinx + wheel,
+	tests_require=[
+		'pytest>=2.8',
+	],
+	classifiers=[
+		"Development Status :: 5 - Production/Stable",
+		"Intended Audience :: Developers",
+		"License :: OSI Approved :: MIT License",
+		"Programming Language :: Python :: 2.7",
+		"Programming Language :: Python :: 3",
+	],
+	entry_points={
+		'console_scripts': [
+			'part-edit = openpack.editor:part_edit_cmd',
+			'zip-listdir = openpack.editor:pack_dir_cmd',
+		],
+	},
+)
+if __name__ == '__main__':
+	setuptools.setup(**setup_params)
