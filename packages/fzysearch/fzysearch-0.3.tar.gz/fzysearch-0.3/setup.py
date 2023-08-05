2f190993606002d872c2a220fd3fe28238950906
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:03 +0000

    Add package fzysearch/0.3/fzysearch-0.3.tar.gz

---
+++ packages/fzysearch/fzysearch-0.3.tar.gz/fzysearch-0.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='fzysearch',
+      version='0.3',
+      description='fuzzy searches a string input to a list and returns results from the list in order of relevance',
+      url='http://github.com/ridwanmsharif/fzysearch',
+      author='Ridwan M. Sharif',
+      author_email='ridwanmsharif@hotmail.com',
+      license='MIT',
+      packages=['fzysearch'],
+      zip_safe=False)
