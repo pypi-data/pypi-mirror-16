d1cc6bccf26464f9c393b0061b87d92167143848
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:51 +0000

    Add package nester5/1.1/nester5-1.1.zip

---
+++ packages/nester5/nester5-1.1.zip/nester5-1.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'nester5',
+    version = '1.1',
+    py_modules = ['nester5'],
+    author = 'harry',
+    author_email = 'harrychangdev@gmail.com',
+    url = 'http://www.ntut.edu.tw',
+    description = 'A simple printer of nested lists',
+    )
