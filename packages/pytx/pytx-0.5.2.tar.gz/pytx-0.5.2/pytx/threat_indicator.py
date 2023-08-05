ebb61ebc4f6c339ab03c4571b69fdefb4a0cfbff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:00 +0000

    Add package pytx/0.5.2/pytx-0.5.2.tar.gz

---
+++ packages/pytx/pytx-0.5.2.tar.gz/pytx-0.5.2/pytx/threat_indicator.py
@@ -0,0 +1,27 @@
+from .common import Common
+from .vocabulary import ThreatIndicator as ti
+from .vocabulary import ThreatExchange as t
+
+
+class ThreatIndicator(Common):
+
+    _URL = t.URL + t.VERSION + t.THREAT_INDICATORS
+    _DETAILS = t.URL + t.VERSION
+    _RELATED = t.URL + t.VERSION
+
+    _fields = [
+        ti.ID,
+        ti.INDICATOR,
+        ti.METADATA,
+        ti.TYPE,
+    ]
+
+    _default_fields = [
+        ti.ID,
+        ti.INDICATOR,
+        ti.METADATA,
+        ti.TYPE,
+    ]
+
+    _unique = [
+    ]
