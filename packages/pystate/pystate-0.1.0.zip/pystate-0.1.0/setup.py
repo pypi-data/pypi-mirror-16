799637798117fbfceb16bb773f8e02a8e6feb5e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:50 +0000

    Add package pystate/0.1.0/pystate-0.1.0.zip

---
+++ packages/pystate/pystate-0.1.0.zip/pystate-0.1.0/setup.py
@@ -0,0 +1,24 @@
+
+from distutils.core import setup
+
+setup(
+    name = 'pystate',
+    packages = ['pystate'],
+    version = '0.1.0',
+    description = 'Python package for co-routine base state machines.',
+    author='Len Wanger',
+    author_email='len_wanger@hotmail.com',
+    url='https://github.com/lwanger/pystate',
+    license= 'License :: OSI Approved :: MIT License',
+    keywords = 'state machine, coroutine, co-routine',
+    classifiers = [ 'License :: Freely Distributable', 'Development Status :: 5 - Production/Stable',
+                    'Operating System :: OS Independent', 'Programming Language :: Python',
+                    'Programming Language :: Python :: 2.5',
+                    'Programming Language :: Python :: 2.6',
+                    'Programming Language :: Python :: 2.7',
+                    'Programming Language :: Python :: 3.3',
+                    'Programming Language :: Python :: 3.4',
+                    'Programming Language :: Python :: 3.5',
+                    'Programming Language :: Python :: 3.6',
+                    'Intended Audience :: Developers' ]
+)
