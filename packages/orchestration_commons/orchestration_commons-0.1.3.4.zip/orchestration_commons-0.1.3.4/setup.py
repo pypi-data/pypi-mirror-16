5063fa7bfd15b562ab4db3665b8b065849bb7396
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:22 +0000

    Add package orchestration_commons/0.1.3.4/orchestration_commons-0.1.3.4.zip

---
+++ packages/orchestration_commons/orchestration_commons-0.1.3.4.zip/orchestration_commons-0.1.3.4/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name="orchestration_commons",
+    packages=['orchestration_commons'],
+    version='0.1.3.4',
+    description='Common functions used in orchestrators.',
+    author='Jesus Martinez',
+    author_email='jesus.a.martinez.v@gmail.com',
+    classifiers=[],
+    download_url='https://github.com/adminMesfix/orchestration-commons/tarball/0.1.3.4',
+    url='https://github.com/adminMesfix/orchestration-commons',
+    keywords=['orchestrator', 'orchestration', 'commons'],
+    install_requires=['requests']
+)
+
