aed4c61f6206ce71d111ce5efed626e5f6db93e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:18 +0000

    Add package GraphDash/0.9/GraphDash-0.9.tar.gz

---
+++ packages/GraphDash/GraphDash-0.9.tar.gz/GraphDash-0.9/setup.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Packaging
+"""
+
+from __future__ import with_statement
+import sys
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    LONG_DESCRIPTION = f.read()
+
+INSTALL_REQUIRES = [
+    'argparse',
+    'PyYAML==3.11',
+    'Flask==0.11.1',
+    'Markdown==2.4',
+    'Pygments==2.1.3',
+    'stop-words',
+]
+
+if sys.version_info[0] < 3:
+    INSTALL_REQUIRES.append('ushlex==0.99')
+
+setup(
+    name='GraphDash',
+    version='0.9',
+    author='Alex Prengère',
+    author_email='alexprengere@amadeus.com',
+    url='https://github.com/AmadeusITGroup/graphdash',
+    description='A web-based dashboard built on graphs and their metadata.',
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    package_data={
+        'graphdash': [
+            'templates/*.html',
+            'assets/css/*',
+            'assets/js/*',
+            'assets/img/*',
+        ],
+    },
+    zip_safe=False,
+    install_requires=INSTALL_REQUIRES,
+    entry_points={
+        'console_scripts': [
+            'GraphDash=graphdash.__main__:main',
+        ],
+    },
+    scripts=[
+        'GraphDashManage',
+    ],
+)
