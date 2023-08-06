66cc8308554f48f9a9535243dd9a9f706d2e5121
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:18 +0000

    Add package torsession/0.2.6/torsession-0.2.6.tar.gz

---
+++ packages/torsession/torsession-0.2.6.tar.gz/torsession-0.2.6/torsession/__init__.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+'''An async && sync session backend with mongodb for tornado'''
+
+import uuid
+from datetime import datetime, timedelta
+
+VERSION = (0, 2, 6)
+
+
+def get_version():
+    return '.'.join(map(str, VERSION))
+
+__version__ = get_version()
+
+
+def gen_session_id():
+    return str(uuid.uuid1())
+
+
+class SessionBase(object):
+    def __init__(self, collection, id_, expired_time, **data):
+        self.collection = collection
+        self.id = id_
+        self.expired_time = expired_time
+        self.data = data
+
+    def expired(self):
+        return self.expired_time < datetime.now()
+
+
+class SessionManagerBase(object):
+    EXPIRED_AFTER = timedelta(days=3)
+
+    def __init__(self, collection, expired_after=None):
+        self.collection = collection
+        self.expired_after = expired_after or self.EXPIRED_AFTER
