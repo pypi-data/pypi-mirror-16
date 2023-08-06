cbddfed777d7aed597af65476c802e09f4238d79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:06:52 +0000

    Add package django-versatileimagefield/1.6/django_versatileimagefield-1.6-py2.py3-none-any.whl

---
+++ packages/django-versatileimagefield/django_versatileimagefield-1.6-py2.py3-none-any.whl/tests/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework.serializers import ModelSerializer
+
+from versatileimagefield.serializers import VersatileImageFieldSerializer
+
+from .models import VersatileImageTestModel
+
+
+class VersatileImageTestModelSerializer(ModelSerializer):
+    """Serializes VersatileImageTestModel instances"""
+    image = VersatileImageFieldSerializer(
+        sizes='test_set'
+    )
+    optional_image = VersatileImageFieldSerializer(
+        sizes='test_set'
+    )
+
+    class Meta:
+        model = VersatileImageTestModel
+        fields = (
+            'image',
+            'optional_image',
+        )
