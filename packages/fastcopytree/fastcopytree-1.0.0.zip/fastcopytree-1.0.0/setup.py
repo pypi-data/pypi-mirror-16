02d3f95718f4f6bb7d07c9f8e18d0cd371730c12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:22:21 +0000

    Add package fastcopytree/1.0.0/fastcopytree-1.0.0.zip

---
+++ packages/fastcopytree/fastcopytree-1.0.0.zip/fastcopytree-1.0.0/setup.py
@@ -0,0 +1,4 @@
+from distutils.core import setup
+
+setup(name='fastcopytree', version='1.0.0', py_modules=['fastcopytree'],
+      author='dong fuguo', author_email='dongfuguo2005@126.com', url='http://user.qzone.qq.com/306467355/2')
