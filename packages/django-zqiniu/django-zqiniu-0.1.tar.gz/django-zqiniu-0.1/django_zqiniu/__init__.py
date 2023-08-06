b229091427ca2b0d3a34b00f7047bf1deedecfff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:18:06 +0000

    Add package django-zqiniu/0.1/django-zqiniu-0.1.tar.gz

---
+++ packages/django-zqiniu/django-zqiniu-0.1.tar.gz/django-zqiniu-0.1/django_zqiniu/__init__.py
@@ -0,0 +1,15 @@
+QINIU_SETTINGS = {
+    'ACCESS_KEY': 'wwfwe',
+    'SECRET_KEY': 'sdwecfwefef',
+    'DEFAULT_BUCKET': 'test',
+    'BUCKET_CONFIGS': {
+        'test': {
+            'BUCKET_NAME': 'test',
+            'BUCKET_URL': 'http://oce67vkmg.bkt.clouddn.com',
+            'BIND_URL': 'http://img.chayoulun.com',
+            'PREFIX': '',
+            'IS_PRIVATE': False,
+        }
+    }
+
+}
