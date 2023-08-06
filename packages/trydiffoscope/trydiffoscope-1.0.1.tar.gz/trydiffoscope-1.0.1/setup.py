fd5f47c6b2ed2dfe64470f8a86df0e264e539f5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:40 +0000

    Add package trydiffoscope/1.0.1/trydiffoscope-1.0.1.tar.gz

---
+++ packages/trydiffoscope/trydiffoscope-1.0.1.tar.gz/trydiffoscope-1.0.1/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup(
+    name='trydiffoscope',
+
+    url="https://chris-lamb.co.uk/projects/trydiffoscope",
+    version='1.0.1',
+    description="in-depth comparison of files, archives, and directories (try.diffoscope.org client)",
+
+    author="Chris Lamb",
+    author_email="lamby@debian.org",
+    license="GPL-3+",
+
+    packages=(),
+    scripts=(
+        'trydiffoscope',
+    ),
+    install_requires=(
+        'requests',
+    ),
+)
