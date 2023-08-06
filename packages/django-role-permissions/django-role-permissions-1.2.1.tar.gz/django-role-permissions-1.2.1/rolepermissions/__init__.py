be633308f42a0ee12e6a6689e9d39cc270303e83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:44 +0000

    Add package django-role-permissions/1.2.1/django-role-permissions-1.2.1.tar.gz

---
+++ packages/django-role-permissions/django-role-permissions-1.2.1.tar.gz/django-role-permissions-1.2.1/rolepermissions/__init__.py
@@ -0,0 +1,15 @@
+import django
+from distutils.version import StrictVersion
+
+
+__version__ = '1.2.1'
+
+
+dj_version = django.get_version()
+
+
+if StrictVersion(dj_version) < StrictVersion('1.7'):
+    from rolepermissions.loader import load_roles_and_permissions
+    load_roles_and_permissions()
+else:
+    default_app_config = 'rolepermissions.apps.RolePermissions'
