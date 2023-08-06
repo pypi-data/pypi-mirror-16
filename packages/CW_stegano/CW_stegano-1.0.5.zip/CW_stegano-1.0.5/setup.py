f24714ae7a80601920a9fbd24d2811afda8fbc8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:34 +0000

    Add package CW_stegano/1.0.5/CW_stegano-1.0.5.zip

---
+++ packages/CW_stegano/CW_stegano-1.0.5.zip/CW_stegano-1.0.5/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+import pip, sys
+files = ["steganography"]
+setup(
+  name = 'CW_stegano',
+  packages = files,
+  version = '1.0.5',
+  description = 'A fun little python script that encodes and decodes strings and images',
+  author = 'Calder White',
+  author_email = 'calderwhite1@gmail.com',
+  url = 'https://github.com/CalderWhite/steganography',
+  keywords = [],
+  classifiers = []
+)
+# test dependancies now
+depend = {
+    "PIL" : "Pillow"
+    }
+for i in depend:
+    try:
+        __import__(i)
+    except:
+        print("Missing module [%s]. Attempting to install %s" % i,depend[i])
+        try:
+            pip.main(["install",depend[i]])
+        except:
+            print("an error occured:")
+            print(sys.exc_info())
