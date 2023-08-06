bb1b149ae03dcf334a79718a372e4afdedec63dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:05 +0000

    Add package Facebook-Bot-Library/0.1/Facebook-Bot-Library-0.1.tar.gz

---
+++ packages/Facebook-Bot-Library/Facebook-Bot-Library-0.1.tar.gz/Facebook-Bot-Library-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+      name = 'Facebook-Bot-Library',
+      packages = ['FBBlib'],
+      version = '0.1',
+      description = 'A facebook messaging SDK for Flask',
+      author = 'boostedLab',
+      author_email = 'issues@boostedlab.ru',
+      url = 'https://github.com/boosted-lab/Facebook-Bot-Library',
+      download_url = 'https://github.com/boosted-lab/Facebook-Bot-Libraryt/tarball/0.1',
+      keywords = ['Facebook', 'Messenger', 'SDK', 'Bot', 'Flask'],
+      classifiers = [],
+)
