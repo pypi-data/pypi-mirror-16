5bc385b155dd38f260b29ff3a016d503fce677de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:05 +0000

    Add package ulid/1.0/ulid-1.0.tar.gz

---
+++ packages/ulid/ulid-1.0.tar.gz/ulid-1.0/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# -*- coding:Utf-8 -*-
+
+from setuptools import setup
+
+setup(name='ulid',
+      version='1.0',
+      description='Pyhton version of this: https://github.com/alizain/ulid',
+      author='Massimo DiPierro',
+      author_email='massimo.dipierro@gmail.com',
+      long_description=open("README.md").read(),
+      url='https://github.com/mdipierro/ulid',
+      py_modules=["ulid"],
+      license= 'BSD',
+     )
+
