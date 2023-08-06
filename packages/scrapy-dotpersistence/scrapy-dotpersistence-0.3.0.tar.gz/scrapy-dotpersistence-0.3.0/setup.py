4cc3506b3249a66424a015ad857ab0e082a13f04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:42 +0000

    Add package scrapy-dotpersistence/0.3.0/scrapy-dotpersistence-0.3.0.tar.gz

---
+++ packages/scrapy-dotpersistence/scrapy-dotpersistence-0.3.0.tar.gz/scrapy-dotpersistence-0.3.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='scrapy-dotpersistence',
+    version='0.3.0',
+    url='https://github.com/scrapy-plugins/scrapy-dotpersistence',
+    description='Scrapy extension to sync `.scrapy` folder to an S3 bucket',
+    long_description=open('README.rst').read(),
+    author='Scrapy developers',
+    license='BSD',
+    py_modules=['scrapy_dotpersistence'],
+    zip_safe=False,
+    author_email='opensource@scrapinghub.com',
+    platforms=['Any'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+    ],
+    install_requires=[
+	    'Scrapy>=1.0.3',
+	    'awscli>=1.10.51',
+    ],
+)
