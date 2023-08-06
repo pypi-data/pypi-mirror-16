ff8c4418cf1edcf548a46b96d3af0b01b48f6861
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:10 +0000

    Add package wp_diveintolist/1.0.0/wp_diveintolist-1.0.0.tar.gz

---
+++ packages/wp_diveintolist/wp_diveintolist-1.0.0.tar.gz/wp_diveintolist-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+
+setup(
+    name='wp_diveintolist',
+    version='1.0.0',
+    py_modules='wp_diveintolist',
+    author='wp',
+    author_email='weisubao@126.com',
+    url='http://www.34miao.com',
+    description='A simple print function for nested list.',
+)
