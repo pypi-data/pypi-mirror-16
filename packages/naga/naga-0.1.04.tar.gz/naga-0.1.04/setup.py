aa637f10a3054488e083b273f5627ec85c7bca18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:41 +0000

    Add package naga/0.1.04/naga-0.1.04.tar.gz

---
+++ packages/naga/naga-0.1.04.tar.gz/naga-0.1.04/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+if __name__ == '__main__':
+    setup(
+        name='naga',
+        packages=['naga'],  # this must be the same as the name above
+        version='0.1.04',
+        description='A powerful Python/Lisp-hybrid-monster!',
+        author='James J. Tolton',
+        author_email='jjtolton@gmail.com',
+        url='https://github.com/hiimjayhireme/naga',
+        keywords=['testing', 'logging', 'example'],
+        classifiers=[],
+    )
