eaee9ce7e2832d3c500d29c3eb22b066791b3574
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:48 +0000

    Add package amalgamate/0.1.2/amalgamate-0.1.2.tar.gz

---
+++ packages/amalgamate/amalgamate-0.1.2.tar.gz/amalgamate-0.1.2/setup.py
@@ -0,0 +1,39 @@
+#! /usr/bin/env python
+import sys
+try:
+    from setuptools import setup
+    HAVE_SETUPTOOLS = True
+except ImportError:
+    from distutils.core import setup
+    HAVE_SETUPTOOLS = False
+
+
+VERSION = '0.1.2'
+
+setup_kwargs = {
+    "version": VERSION,
+    "description": 'Collapses Python packages into a single module.',
+    "license": 'BSD 3-clause',
+    "author": 'The xonsh developers',
+    "author_email": 'xonsh@googlegroups.com',
+    "url": 'https://github.com/xonsh/amalgamate',
+    "download_url": "https://github.com/xonsh/amalgamate/zipball/" + VERSION,
+    "classifiers": [
+        "License :: OSI Approved",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "Topic :: Utilities",
+        ],
+    "zip_safe": False,
+    "scripts": ['amalgamate.py'],
+    "data_files": [("", ['LICENSE', 'README.rst']),],
+    }
+
+
+if __name__ == '__main__':
+    setup(
+        name='amalgamate',
+        py_modules=['amalgamate'],
+        long_description=open('README.rst').read(),
+        **setup_kwargs
+        )
