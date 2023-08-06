4fced98d183733e7cd64e55e46ef7cc41e7d538b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:20:02 +0000

    Add package naster_2016825/1.1.1/naster_2016825-1.1.1.zip

---
+++ packages/naster_2016825/naster_2016825-1.1.1.zip/naster_2016825-1.1.1/nester.py
@@ -0,0 +1,8 @@
+def print_lol(L,level):
+    for i in L:
+        if isinstance(i,list):
+            print_lol(i,level+1)
+        else:
+            for tab_stop in range(level):
+                print("\t",end='')
+            print(i)
---
+++ packages/naster_2016825/naster_2016825-1.1.1.zip/naster_2016825-1.1.1/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+        name		= 'naster_2016825',
+	version		= '1.1.1',
+	py_modules	= ['nester'],
+	author		= 'lisiyao',
+	author_email    = 'shenghuojidubo@163.com',
+	url		= 'http://www.baidu.com',
+	description     = 'A simple printer of nested lists',
+    )
