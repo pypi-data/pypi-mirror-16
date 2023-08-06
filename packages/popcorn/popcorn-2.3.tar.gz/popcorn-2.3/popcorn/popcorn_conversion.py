bf2286f3c6e662de6be7ddb30339fbeecc60d9dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:21 +0000

    Add package popcorn/2.3/popcorn-2.3.tar.gz

---
+++ packages/popcorn/popcorn-2.3.tar.gz/popcorn-2.3/popcorn/popcorn_conversion.py
@@ -0,0 +1,24 @@
+
+# Popcorn 2.3
+# https://github.com/InitializeSahib/Popcorn
+
+
+class PopConv:
+
+    def kilo_to_pound(self, kilo):
+        return kilo * 2.3046
+
+    def pound_to_kilo(self, pound):
+        return pound / 2.3046
+
+    def yard_to_foot(self, yard):
+        return yard * 3
+
+    def foot_to_yard(self, foot):
+        return foot / 3
+
+    def pound_to_stone(self, pound):
+        return pound / 14
+
+    def stone_to_pound(self, stone):
+        return stone * 14
