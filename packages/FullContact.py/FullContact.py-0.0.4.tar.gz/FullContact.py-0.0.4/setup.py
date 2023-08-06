d831285f8c8d12a6b07a199e9cee58605d9683cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:05:42 +0000

    Add package FullContact.py/0.0.4/FullContact.py-0.0.4.tar.gz

---
+++ packages/FullContact.py/FullContact.py-0.0.4.tar.gz/FullContact.py-0.0.4/setup.py
@@ -0,0 +1,42 @@
+"""
+FullContact.py
+--------------
+
+Simple Python interface for FullContact, using Requests.
+
+"""
+from setuptools import setup
+
+
+setup(
+    name='FullContact.py',
+    version='0.0.4',
+    url='https://github.com/fullcontact/fullcontact.py',
+    license='MIT',
+    author=['FullContact'],
+    author_email=['support@fullcontact.com'],
+    description='Simple Python interface for FullContact, using Requests',
+    long_description=open('README.md', 'r').read(),
+    packages=['fullcontact'],
+    zip_safe=False,
+    platforms='any',
+    install_requires=[
+        'requests',
+    ],
+    include_package_data=True,
+    tests_require=[
+        'nose>=1.0',
+        'flake8'
+    ],
+    test_suite='nose.collector',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
