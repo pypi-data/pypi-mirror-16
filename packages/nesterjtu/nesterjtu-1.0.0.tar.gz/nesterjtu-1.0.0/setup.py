e5a0e40ae05f013c914b3d18f12e70d8cbd5e1ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:05:11 +0000

    Add package nesterjtu/1.0.0/nesterjtu-1.0.0.tar.gz

---
+++ packages/nesterjtu/nesterjtu-1.0.0.tar.gz/nesterjtu-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+	name		= 'nesterjtu',
+	version		= '1.0.0',
+	py_modules	= ['nester'],
+	author		= 'hfpython',
+	author_email	= 'hfpython@headfirstlabs.com',
+	url		= 'http://www.headfirstlabs.com',
+	description	= 'a simple printer of nested lists',
+     )
+	
