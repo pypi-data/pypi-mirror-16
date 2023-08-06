3bb5baac6feb90af8d2e7709031aa5774b5bf049
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:56:52 +0000

    Add package laminate_default/0.2/laminate_default-0.2.tar.gz

---
+++ packages/laminate_default/laminate_default-0.2.tar.gz/laminate_default-0.2/setup.py
@@ -0,0 +1,19 @@
+"""Laminate Default Theme"""
+from setuptools import setup
+
+setup(name='laminate_default',
+      version='0.2',
+      url='https://github.com/ohenrik/laminate_default',
+      description='Default theme for laminate',
+      long_description=__doc__,
+      author='Ole Henrik Skogstrøm',
+      author_email='laminate@amplify.no',
+      packages=['laminate_default', 'laminate_default.templates'],
+      include_package_data=True,
+      install_requires=[],
+      classifiers=[
+          'Development Status :: 2 - Pre-Alpha'
+      ]
+     )
+
+__author__ = 'Ole Henrik Skogstrøm'
