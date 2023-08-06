50d4eb80a7cf53912ad0f14889e9adcbdd9a8950
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:18:13 +0000

    Add package kubefuse/0.7.2/kubefuse-0.7.2.tar.gz

---
+++ packages/kubefuse/kubefuse-0.7.2.tar.gz/kubefuse-0.7.2/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "kubefuse",
+    version = "0.7.2",
+    packages = find_packages(),
+    author = "Bart Spaans",
+    author_email = "bart.spaans@gmail.com",
+    url = "https://github.com/bspaans/kubefuse",
+    description = "A FUSE file system for Kubernetes",
+    classifiers = [
+        'Programming Language :: Python',
+        'License :: OSI Approved :: Apache Software License',
+        'Topic :: System',
+        'Topic :: System :: Clustering',
+        'Topic :: System :: Filesystems',
+    ],
+    license="Apache License v2",
+    entry_points = {
+        'console_scripts': [
+            'kubefuse = kubefuse.kubefuse:main'
+        ]
+    },
+    install_requires = [
+        'fusepy>=2.0.4',
+        'python-myna==1.2.0',
+        'PyYAML>=3',
+        'six>=1.10.0'
+    ]
+ )
