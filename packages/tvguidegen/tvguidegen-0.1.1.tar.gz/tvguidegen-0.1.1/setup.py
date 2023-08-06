6d3402fc39ee8eaaa9948e90c53a0ad5e5d659c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:52 +0000

    Add package tvguidegen/0.1.1/tvguidegen-0.1.1.tar.gz

---
+++ packages/tvguidegen/tvguidegen-0.1.1.tar.gz/tvguidegen-0.1.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup,find_packages
+
+setup(name='tvguidegen',
+      version='0.1.1',
+      description='TV Guide Generator',
+      url='https://github.com/7jtv/tvguidegen',
+      author='Kas IPTV',
+      author_email='kas.iptv@gmail.com',
+      license='MIT',
+      packages=find_packages(exclude=('data', 'tests*')),
+      scripts=['bin/tvguidegen'],
+      install_requires=[
+          'pymongo',
+      ],
+      long_description = open("README.md").read(),
+      zip_safe=False
+
+)
