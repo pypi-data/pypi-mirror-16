a2f18f65b55d0f3e9c5b68545f4cbc671db0af07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:06:56 +0000

    Add package abp/0.4.6/abp-0.4.6.tar.gz

---
+++ packages/abp/abp-0.4.6.tar.gz/abp-0.4.6/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+from glob import glob
+from os import path
+
+STATIC = glob("abp/static/*.*")+glob("abp/static/img/*.*")+glob("abp/static/scripts/*.*")
+print STATIC
+
+setup(
+    name = "abp",
+    version = "0.4.6",
+    packages = ["abp", "abp.static"],
+    test_suite = "tests",
+    author = "Pete Shadbolt",
+    author_email = "hello@peteshadbolt.co.uk",
+    url = "https://github.com/peteshadbolt/abp",
+    description = "Port of C++ due to Simon Anders and Hans J Briegel",
+    keywords = "quantum",
+    setup_requires = ["numpy"],
+    scripts = ["bin/abpserver"],
+    install_requires = ["numpy", "networkx", "tqdm", "websocket-client", "websocket-server"],
+    package_data = {"abp.static": STATIC}
+)
