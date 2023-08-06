eb8221d2e25dac612ab1bdcef75fdca30a851255
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:45 +0000

    Add package ncpol2sdpa/1.11.1/ncpol2sdpa-1.11.1.tar.gz

---
+++ packages/ncpol2sdpa/ncpol2sdpa-1.11.1.tar.gz/ncpol2sdpa-1.11.1/setup.py
@@ -0,0 +1,42 @@
+"""
+Ncpol2SDPA: A converter from commutative and noncommutative polynomial
+optimization problems to sparse SDP input formats.
+"""
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+setup(
+    name='ncpol2sdpa',
+    version='1.11.1',
+    author='Peter Wittek',
+    author_email='peterwittek@users.noreply.github.com',
+    packages=['ncpol2sdpa'],
+    url='http://ncpol2sdpa.readthedocs.io/',
+    keywords=[
+        'sdp',
+        'semidefinite programming',
+        'relaxation',
+        'polynomial optimization problem',
+        'noncommuting variable',
+        'sdpa'],
+    license='LICENSE',
+    description='A converter from polynomial optimization problems of\
+                 noncommutative variables to sparse SDPA input format.',
+    long_description=open('README.rst').read(),
+    classifiers=[
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+        'Development Status :: 4 - Beta',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Topic :: Scientific/Engineering :: Physics',
+        'Intended Audience :: Science/Research',
+        'Programming Language :: Python'
+    ],
+    install_requires=[
+        "sympy >= 0.7.2",
+        "numpy"
+    ],
+    test_suite="tests"
+)
