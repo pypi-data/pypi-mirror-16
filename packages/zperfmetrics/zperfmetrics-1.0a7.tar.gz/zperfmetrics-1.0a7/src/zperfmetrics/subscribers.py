2c308e7b5ac41ab92cb64a6a5cd0002327cb5339
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:35 +0000

    Add package zperfmetrics/1.0a7/zperfmetrics-1.0a7.tar.gz

---
+++ packages/zperfmetrics/zperfmetrics-1.0a7.tar.gz/zperfmetrics-1.0a7/src/zperfmetrics/subscribers.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from time import time
+from zperfmetrics import ZMetric
+
+
+def annotate_start_publish(event):
+    event.request._zperfmetrics_pubstart = time()
+
+
+def measurement_after_traversal(event):
+    with ZMetric(stat='publish.traversal') as metric:
+        metric.start = event.request._zperfmetrics_pubstart
+    event.request._zperfmetrics_start = time()
+
+
+def measurement_before_commit(event):
+    with ZMetric(stat='publish.beforecommit') as metric:
+        metric.start = event.request._zperfmetrics_start
+    event.request._zperfmetrics_start = time()
+
+
+def measurement_request_success(event):
+    with ZMetric(stat='publish.commit') as metric:
+        metric.start = event.request._zperfmetrics_start
+        metric.request = event.request
+    with ZMetric(stat='publish.sum') as metric:
+        metric.start = event.request._zperfmetrics_pubstart
+        metric.request = event.request
