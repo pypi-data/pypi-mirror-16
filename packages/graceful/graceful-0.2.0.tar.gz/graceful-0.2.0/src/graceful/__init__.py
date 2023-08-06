7b453899e2030c67d9d437d6656618254e877ecd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:23 +0000

    Add package graceful/0.2.0/graceful-0.2.0.tar.gz

---
+++ packages/graceful/graceful-0.2.0.tar.gz/graceful-0.2.0/src/graceful/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""Minimalist framework for self-descriptive REST APIs build on top of falcon.
+
+It is inspired by Django REST Framework package. Mostly by how object
+serialization is done but more emphasis is put on API to be self-descriptive.
+"""
+VERSION = (0, 2, 0)  # PEP 386  # noqa
+__version__ = ".".join([str(x) for x in VERSION])  # noqa
