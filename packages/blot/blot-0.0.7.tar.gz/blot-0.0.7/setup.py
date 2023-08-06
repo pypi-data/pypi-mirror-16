8270582c21452644b006495a0033cda9d3b4b1f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:10 +0000

    Add package blot/0.0.7/blot-0.0.7.tar.gz

---
+++ packages/blot/blot-0.0.7.tar.gz/blot-0.0.7/setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name="blot",
+      version="0.0.7",
+      packages=['blot', 'blot.readers', 'blot.assets', 'blot.writers'],
+      package_dir={
+          "blot": "blot",
+          "blot.readers": "blot/readers",
+          "blot.assets": "blot/assets",
+          "blot.writers": "blot/writers"
+      },
+      install_requires=[
+          "nltk==3.2.1",
+          "dateparser==0.4.0",
+          "awesome-slugify==1.6.5",
+          "bs4==0.0.1",
+          "Fabric==1.11.1",
+          "humanize==0.5.1",
+          "Jinja2==2.8",
+          "mock==2.0.0",
+          "nose==1.3.7",
+          "Pygments==2.1.3",
+          "titlecase==0.8.1",
+          "markdown2==2.3.1",
+          "summarize_github"
+      ],
+      dependency_links=[
+          'http://github.com/vgel/summarize.py/tarball/0911f23a500dcb0de7b2722587f2dff3598a443a#egg=summarize_github',
+      ],
+      # metadata for upload to PyPI
+      author="Dustin Lacewell",
+      author_email="dlacewell@gmail.com",
+      description="An unassuming static site-generator for Python",
+      keywords="site-generator",
+      url="http://github.com/dustinlacewell/blot", )
