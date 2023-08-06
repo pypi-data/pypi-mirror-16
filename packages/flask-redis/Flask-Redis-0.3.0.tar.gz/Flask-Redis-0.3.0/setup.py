29e194fdfcbb0ef7b285ce1e51eb867d1e436eb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:17:16 +0000

    Add package flask-redis/0.3.0/Flask-Redis-0.3.0.tar.gz

---
+++ packages/flask-redis/Flask-Redis-0.3.0.tar.gz/Flask-Redis-0.3.0/setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+import io
+
+from setuptools import setup
+
+with io.open('README.rst', encoding='utf-8') as f:
+    README = f.read()
+with io.open('HISTORY.rst', encoding='utf-8') as f:
+    HISTORY = f.read()
+
+
+setup(
+    name='Flask-Redis',
+    version='0.3.0',
+    url='https://github.com/underyx/flask-redis',
+    author='Rhys Elsmore',
+    author_email='me@rhys.io',
+    maintainer='Bence Nagy',
+    maintainer_email='bence@underyx.me',
+    download_url='https://github.com/underyx/flask-redis/releases',
+    description='Redis Extension for Flask Applications',
+    long_description=README + '\n\n' + HISTORY,
+    packages=['flask_redis'],
+    package_data={'': ['LICENSE']},
+    zip_safe=False,
+    install_requires=[
+        'Flask>=0.8',
+        'redis>=2.7.6',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Flask',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
