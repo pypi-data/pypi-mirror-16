33d6542e0da2b495977fcbc98cf9787fd51fd439
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:33:30 +0000

    Add package blowdrycss/0.2.9/blowdrycss-0.2.9.win-amd64.zip

---
+++ packages/blowdrycss/blowdrycss-0.2.9.win-amd64.zip/Users/nick/test_blowdrycss_pypi/Scripts/blowdrycss-script.py
@@ -0,0 +1,10 @@
+#!C:\Users\nick\test_blowdrycss_pypi\Scripts\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'blowdrycss==0.2.9','console_scripts','blowdrycss'
+__requires__ = 'blowdrycss==0.2.9'
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.exit(
+        load_entry_point('blowdrycss==0.2.9', 'console_scripts', 'blowdrycss')()
+    )
