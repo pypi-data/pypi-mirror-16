0009be4ea066416957589ec4883544884cd4a02f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:14 +0000

    Add package qspin/0.1.1/qspin-0.1.1.tar.gz

---
+++ packages/qspin/qspin-0.1.1.tar.gz/qspin-0.1.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'qspin',
+  packages = ['qspin'], # this must be the same as the name above
+  version = '0.1.1',
+  description = 'Learn quantum spin and entanglement',
+  author = 'Donald Gavel',
+  author_email = 'donald.gavel@gmail.com',
+  url = 'https://bitbucket.org/donald_gavel/qspin', # use the URL to the github repo
+  download_url = 'https://bitbucket.org/donald_gavel/qspin/downloads/qspin-0.1.tar',
+  keywords = ['quantum', 'spin', 'electron'], # arbitrary keywords
+  classifiers = [],
+)
