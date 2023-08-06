1056f57fc5f3d792870d967d79d9e62b783baad0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:05:47 +0000

    Add package seed-services-client/0.6.0/seed_services_client-0.6.0-py2-none-any.whl

---
+++ packages/seed-services-client/seed_services_client-0.6.0-py2-none-any.whl/seed_services_client/__init__.py
@@ -0,0 +1,15 @@
+"""Seed Services client library."""
+
+from .identity_store import IdentityStoreApiClient
+from .stage_based_messaging import StageBasedMessagingApiClient
+from .auth import AuthApiClient
+from .control_interface import ControlInterfaceApiClient
+from .hub import HubApiClient
+from .message_sender import MessageSenderApiClient
+
+__version__ = "0.6.0"
+
+__all__ = [
+    'IdentityStoreApiClient', 'StageBasedMessagingApiClient', 'AuthApiClient',
+    'ControlInterfaceApiClient', 'HubApiClient', 'MessageSenderApiClient'
+]
---
+++ packages/seed-services-client/seed_services_client-0.6.0-py2-none-any.whl/seed_services_client/auth.py
@@ -0,0 +1,51 @@
+from demands import JSONServiceClient, HTTPServiceClient
+
+
+class AuthApiClient(object):
+    """
+    Client for Auth Service.
+
+    :param str email:
+
+        An email address.
+
+    :param str password:
+
+        A password.
+
+    :param str api_url:
+        The full URL of the API.
+
+    """
+
+    def __init__(self, email, password, api_url, session=None,
+                 session_http=None):
+        if session is None:
+            session = JSONServiceClient(url=api_url)
+        # login
+        data = {"email": email, "password": password}
+        login = session.post('/user/tokens/', data=data)
+        self.token = login["token"]
+        headers = {'Authorization': 'Token %s' % self.token}
+        session = JSONServiceClient(url=api_url, headers=headers)
+        self.session = session
+        if session_http is None:
+            session_http = HTTPServiceClient(url=api_url, headers=headers)
+        self.session_http = session_http
+
+    def get_permissions(self):
+        return self.session.get('/user/')
+
+    def get_teams(self):
+        return self.session.get('/teams/')
+
+    def create_user(self, user):
+        return self.session.post('/users/', data=user)
+
+    def add_user_to_team(self, user, team):
+        # Returns a 204 with empty content so lets return True if it worked
+        response = self.session_http.put('/teams/%s/users/%s/' % (team, user,))
+        if response.status_code == 204:
+            return True
+        else:
+            return False
