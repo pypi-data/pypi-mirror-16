28525845da0762575ace7079684579d0d82ddfda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:05:34 +0000

    Add package Khayyam/2.11.0/Khayyam-2.11.0.tar.gz

---
+++ packages/Khayyam/Khayyam-2.11.0.tar.gz/Khayyam-2.11.0/khayyam/algorithms.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+import warnings
+
+__author__ = 'vahid'
+
+try:
+    from .algorithms_c import \
+        is_jalali_leap_year, \
+        get_julian_day_from_gregorian_date, \
+        get_days_in_jalali_year, \
+        get_days_in_jalali_month, \
+        get_julian_day_from_jalali_date, \
+        get_jalali_date_from_julian_day, \
+        get_gregorian_date_from_julian_day, \
+        get_jalali_date_from_gregorian_date
+
+except ImportError:  # pragma: no cover
+    warnings.warn(
+        "The Cython extension is not available. Switching to fallback python pure algorithms, "
+        "so it's may be slower than C implementation of the algorithms.")
+    from .algorithms_pure import \
+        is_jalali_leap_year, \
+        get_julian_day_from_gregorian_date, \
+        get_days_in_jalali_year, \
+        get_days_in_jalali_month, \
+        get_julian_day_from_jalali_date, \
+        get_jalali_date_from_julian_day, \
+        get_gregorian_date_from_julian_day, \
+        get_jalali_date_from_gregorian_date
+
+
+
