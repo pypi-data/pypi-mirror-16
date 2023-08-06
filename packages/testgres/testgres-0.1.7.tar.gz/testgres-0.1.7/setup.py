1745ae46a7972f1153c6b7ab0a2b47760e64415b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:03:29 +0000

    Add package testgres/0.1.7/testgres-0.1.7.tar.gz

---
+++ packages/testgres/testgres-0.1.7.tar.gz/testgres-0.1.7/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+  name = 'testgres',
+  packages = ['testgres'],
+  version = '0.1.7',
+  description = 'Testing utility for postgresql and it''s extensions',
+  author = 'Ildar Musin',
+  author_email = 'zildermann@gmail.com',
+  url = 'https://github.com/postgrespro/testgres',
+  #download_url = 'https://github.com/postgrespro/testgres/tarball/0.1.1',
+  keywords = ['testing', 'postgresql'],
+  classifiers = [],
+  install_requires = ["pg8000"]
+)
