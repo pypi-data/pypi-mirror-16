72ba30f6a485804dc2ee99ff3e9b78ee39a3f1f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:01 +0000

    Add package hamster-lib/0.12.0/hamster-lib-0.12.0.tar.gz

---
+++ packages/hamster-lib/hamster-lib-0.12.0.tar.gz/hamster-lib-0.12.0/tests/helpers/test_helpers.py
@@ -0,0 +1,27 @@
+# -*- encoding: utf-8 -*-
+
+from __future__ import absolute_import, unicode_literals
+
+import pickle
+
+import pytest
+from hamster_lib.helpers import helpers
+
+
+class TestLoadTmpFact(object):
+    """Test related to the loading of the 'ongoing fact'."""
+    def test_no_file_present(self):
+        """Make sure that we return ``False`` if there is no 'tmpfile' present."""
+        assert helpers._load_tmp_fact('non_existing_file') is False
+
+    def test_file_instance_invalid(self, base_config):
+        """Make sure we throw an error if the instance picked in the file is no ``Fact``."""
+        with open(base_config['tmpfile_path'], 'wb') as fobj:
+            pickle.dump('foobar', fobj)
+        with pytest.raises(TypeError):
+            helpers._load_tmp_fact(base_config['tmpfile_path'])
+
+    def test_valid(self, base_config, tmp_fact):
+        """Make sure that we return the stored 'ongoing fact' as expected."""
+        result = helpers._load_tmp_fact(base_config['tmpfile_path'])
+        assert result == tmp_fact
