f5e737cce4f6d47c2482f0d8d37ec377747ca3a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:58:40 +0000

    Add package nester_volnet/1.0.3/nester_volnet-1.0.3.tar.gz

---
+++ packages/nester_volnet/nester_volnet-1.0.3.tar.gz/nester_volnet-1.0.3/nester.py
@@ -0,0 +1,16 @@
+"""This is the "standard" way to include a multiple-line comment in your code."""
+
+# This is a single line comment
+
+def print_list(the_list, level):
+	for item in the_list:
+		if isinstance(item, list):
+			print_list(item, level + 1)
+		else:
+			i = 0
+			result = ""
+			for i in range(level):
+				result += "\t"
+			result = result + item
+			print(result)
+
---
+++ packages/nester_volnet/nester_volnet-1.0.3.tar.gz/nester_volnet-1.0.3/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'nester_volnet',
+    version = '1.0.3',
+    py_modules = ['nester'],
+    author = 'volnet',
+    author_email = 'volnet@tom.com',
+    url = 'http://volnet.github.io',
+    description='A simple printer of nested lists, this version change the while to range()',
+)
