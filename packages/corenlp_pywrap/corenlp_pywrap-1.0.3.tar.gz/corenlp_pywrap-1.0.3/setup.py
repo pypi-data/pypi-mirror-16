d062c114d3036b98d04dd6bd4be9c1989f2b3d94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:18 +0000

    Add package corenlp_pywrap/1.0.3/corenlp_pywrap-1.0.3.tar.gz

---
+++ packages/corenlp_pywrap/corenlp_pywrap-1.0.3.tar.gz/corenlp_pywrap-1.0.3/setup.py
@@ -0,0 +1,25 @@
+import os
+from setuptools import setup
+setup(
+    name = "corenlp_pywrap",
+    version = "1.0.3",
+    author = "hhsecond",
+    author_email = "sherinct@live.com",
+    description = ("A powerful python wraper for Stanford CoreNLP"),
+    license = "MIT",
+    keywords = "stanford corenlp wrapper",
+    install_requires=['requests'],
+    url = "https://www.github.com/hhsecond/corenlp_pywrap",
+    download_url = "https://www.github.com/hhsecond/corenlp_pywrap/tarball/1.0.3",
+    packages=['corenlp_pywrap'],
+    long_description='Production Ready version equiped with basic'\
+        'output fetch of stanfornlp and custom arrange function, '\
+        'for more Info - '\
+        'CheckItOut: https://github.com/hhsecond/corenlp_pywrap',
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
