350f1f0b05c4a4419b2095962a8b1c14b4e43481
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:56:49 +0000

    Add package autolux/0.0.23/autolux-0.0.23.tar.gz

---
+++ packages/autolux/autolux-0.0.23.tar.gz/autolux-0.0.23/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+desc = ""
+try: desc=open('README.rst').read()
+except: pass
+setup(
+    name='autolux',
+    version='0.0.23',
+    author='okay',
+    author_email='okay.zed+al@gmail.com',
+    packages=['autolux' ],
+    scripts=['bin/autolux'],
+    url='http://github.com/autolume/autolux',
+    license='MIT',
+    description='an auto luxer',
+    long_description=desc
+    )
+
