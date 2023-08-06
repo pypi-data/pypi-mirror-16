9499f4b29cf87547ccdeb7df6e2dcbecf89b4d0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:58:13 +0000

    Add package gputools/0.1.3/gputools-0.1.3.tar.gz

---
+++ packages/gputools/gputools-0.1.3.tar.gz/gputools-0.1.3/setup.py
@@ -0,0 +1,28 @@
+
+from setuptools import setup, find_packages
+
+setup(name='gputools',
+    version='0.1.3',
+    description='OpenCL accelerated volume processing',
+    url='',
+    author='Martin Weigert',
+    author_email='mweigert@mpi-cbg.de',
+    license='MIT',
+    packages=find_packages(),
+
+    install_requires=["numpy>=1.10.0",
+                      "pyopencl>=2016.1",
+                      "pyfft"],
+
+    package_data={"gputools":
+                  ['core/kernels/*.cl',
+                   'convolve/kernels/*.cl',
+                   'denoise/kernels/*.cl',
+                   'deconv/kernels/*.cl',
+                   'noise/kernels/*.cl',
+                   'transforms/kernels/*.cl',
+                  ],
+    },
+
+    entry_points = {}
+)
