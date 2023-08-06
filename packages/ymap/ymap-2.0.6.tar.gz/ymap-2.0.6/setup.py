95df5a75987680767da3fa136cfee59bf7e395fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:19:41 +0000

    Add package ymap/2.0.6/ymap-2.0.6.tar.gz

---
+++ packages/ymap/ymap-2.0.6.tar.gz/ymap-2.0.6/setup.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+
+import sys
+import os
+import ez_setup
+ez_setup.use_setuptools()
+from setuptools import setup, find_packages
+from distutils.core import setup
+
+if sys.version_info >= (3,1):
+	install_require = ["Orange3"]
+
+elif sys.version_info < (3,0):
+	install_require = ["Orange"]
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+	name = "ymap",
+	
+	version = "2.0.6",
+	
+	description = "An automated method to map yeast variants to protein modifications and functional regions",
+	
+	url = "https://github.com/CSB-KUL/yMap",
+	
+	author = "Ahmed Arslan and Vera van Noort, KU Leuven, Belgium",
+	
+	author_email = "vera.vannoort@kuleuven.be",
+	
+	maintainer = "Ahmed Arslan",
+	
+	maintainer_email = "ahmed.arslan@kuleuven.be",
+	
+	license = "MIT",
+	
+	keywords = (
+
+		'Biofinformatics',
+		
+		'Proteins variants',
+		
+		'PostTransational-Modifications (PTMs)',
+		
+		'Proteins domains',
+		
+		'Secondary Structures',
+		
+		'Protein-Protein Interactions',
+		
+		'Genomics',
+		
+		'Proteomics',
+		
+		'Gene Ontology',
+		
+		'yMap',
+		),
+
+	long_description=read('README.md'),
+
+
+	classifiers=[ 
+	'Intended Audience :: Science/Research',
+	'Topic :: Education',
+	'License :: OSI Approved :: MIT License',
+	'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    ],
+
+
+    zip_safe = False,
+
+    packages = ['ymap', 'tests'],
+
+	install_requires = [install_require, "Orange-Bioinformatics"],
+
+	include_package_data = True,
+	
+	entry_points={
+        'console_scripts': [
+            'ydata = ymap.ymap:data',
+            'ygenes = ymap.ymap:ymap_genes',
+            'yproteins = ymap.ymap:ymap_proteins',
+            'yweb = ymap.ymap:web',
+            'test = ymap.tests:main',
+            
+        ],
+    }
+)
+
+
