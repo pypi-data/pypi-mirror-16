c7d2ec607fee098e840cd915499965f20c002af5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:03:10 +0000

    Add package pelican-next-meetup/0.2.4/pelican-next-meetup-0.2.4.tar.gz

---
+++ packages/pelican-next-meetup/pelican-next-meetup-0.2.4.tar.gz/pelican-next-meetup-0.2.4/README.rst
@@ -0,0 +1,47 @@
+Next Meetup plugin for Pelican
+==============================
+
+`Pelican <https://github.com/getpelican/pelican>`__ plugin to retrieve
+information about your group's next meetup by calling
+`Meetup.com <http://www.meetup.com/>`__ API.
+
+Install
+-------
+
+Run ``pip install pelican-next-meetup``, then add ``next_meetup`` to
+``pelicanconf.py`` configuration's\ ``PLUGINS`` list:
+
+.. code:: python
+
+    PLUGINS = [
+        # all other plugins
+        'next_meetup',
+    ]
+
+Set it up
+---------
+
+For plugin to call the Meetup.com API, a valid meetup group's URL must
+be specified in ``pelicanconf.py``, such as:
+
+.. code:: python
+
+    MEETUP_URL = 'python-lv'
+
+Plugin will fetch the data of the first upcoming meetup and store data
+to ``next_meetup`` variable for use in templates;
+if no next meetup is scheduled, variable will be falsy.
+If code other than HTTP ``200 OK`` is returned,
+an exception will be raised by
+`requests <https://github.com/kennethreitz/requests>`__ library.
+
+Example of data returned:
+
+.. code:: javascript
+
+    {
+      'time': 1452704400000,
+      'url': 'http://www.meetup.com/python-lv/events/227548959/',
+      'utc_offset': 7200000,
+      'name': 'Months 1st #PyNight'
+    }
---
+++ packages/pelican-next-meetup/pelican-next-meetup-0.2.4.tar.gz/pelican-next-meetup-0.2.4/next_meetup/next_meetup.py
@@ -0,0 +1,35 @@
+"""Pelican plugin to get info on your next meetup."""
+import requests
+from pelican import signals
+
+
+def get_next_meetup(generator, metadata):
+    """Fetch information about next meetup as per MEETUP_URL variable."""
+    if 'MEETUP_URL' in generator.settings.keys():
+        URL = "https://api.meetup.com/2/events"
+        payload = {
+            "sign": True,
+            "photo-host": "public",
+            "group_urlname": generator.settings['MEETUP_URL'],
+            "page": 1  # retrieve only the next event
+        }
+        response = requests.get(URL, params=payload)
+        # throw requests error if no 200 OK
+        if response.raise_for_status() is None:
+            results = response.json()["results"]
+            # populate the exposed variable with data
+            if not results:
+                generator.context["next_meetup"] = False
+            else:
+                data = results[0]
+                generator.context["next_meetup"] = {
+                    "name": data["name"],
+                    "url": data["event_url"],
+                    "time": data["time"],
+                    "utc_offset": data["utc_offset"]
+                }
+
+
+def register():
+    """Register with Pelican."""
+    signals.page_generator_context.connect(get_next_meetup)
