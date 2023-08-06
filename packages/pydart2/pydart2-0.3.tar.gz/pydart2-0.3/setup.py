2820a550d49dc4ae1c4f3ae58d31a32837f7f6d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:22 +0000

    Add package pydart2/0.3/pydart2-0.3.tar.gz

---
+++ packages/pydart2/pydart2-0.3.tar.gz/pydart2-0.3/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup
+from setuptools.command.install import install
+from setuptools.command.build_py import build_py
+from setuptools.command.build_ext import build_ext
+
+
+class Pydart2BuildPy(build_py):
+    def run(self):
+        print("[pydart2] pre-build_py")
+        build_py.run(self)
+        print("[pydart2] post-build_py")
+
+
+class Pydart2BuildExt(build_ext):
+    def run(self):
+        print("[pydart2] pre-build_ext")
+        build_ext.run(self)
+        print("[pydart2] post-build_ext")
+
+
+class Pydart2Install(install):
+    def run(self):
+        print("[pydart2] pre-install")
+        install.run(self)
+        print("[pydart2] post-install")
+
+setup(name='pydart2',
+      version='0.3',
+      description='Python Interface for DART Simulator',
+      url='https://github.com/sehoonha/pydart2',
+      author='Sehoon Ha',
+      author_email='sehoon.ha@gmail.com',
+      license='BSD',
+      cmdclass={'build_py': Pydart2BuildPy,
+                'build_ext': Pydart2BuildExt,
+                'install': Pydart2Install},
+      packages=['pydart2'],
+      zip_safe=False)
