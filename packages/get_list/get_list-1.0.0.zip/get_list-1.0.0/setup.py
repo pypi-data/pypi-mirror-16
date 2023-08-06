0693c4b59135c13794e9f8c411ef4213d7d4b425
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:15 +0000

    Add package get_list/1.0.0/get_list-1.0.0.zip

---
+++ packages/get_list/get_list-1.0.0.zip/get_list-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name         = 'get_list',
+    version      = '1.0.0',
+    py_modules   = ['getlist'],
+    author       = 'liu',
+    author_email = '1945522306@qq.com',
+    url          = '',
+    description  = 'A Test File',
+    )
