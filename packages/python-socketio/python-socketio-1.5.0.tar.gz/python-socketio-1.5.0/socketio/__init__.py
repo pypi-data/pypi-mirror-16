621816e5fa96cce333f2ea70d6afbd8f64a94af8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:16:42 +0000

    Add package python-socketio/1.5.0/python-socketio-1.5.0.tar.gz

---
+++ packages/python-socketio/python-socketio-1.5.0.tar.gz/python-socketio-1.5.0/socketio/__init__.py
@@ -0,0 +1,10 @@
+from .middleware import Middleware
+from .base_manager import BaseManager
+from .pubsub_manager import PubSubManager
+from .kombu_manager import KombuManager
+from .redis_manager import RedisManager
+from .server import Server
+from .namespace import Namespace
+
+__all__ = [Middleware, Server, BaseManager, PubSubManager, KombuManager,
+           RedisManager, Namespace]
