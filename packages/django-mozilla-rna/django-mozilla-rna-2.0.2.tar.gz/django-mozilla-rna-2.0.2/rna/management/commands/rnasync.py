5b1cbd96a40103c1660483b5192f1d44cb69beab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:23:24 +0000

    Add package django-mozilla-rna/2.0.2/django-mozilla-rna-2.0.2.tar.gz

---
+++ packages/django-mozilla-rna/django-mozilla-rna-2.0.2.tar.gz/django-mozilla-rna-2.0.2/rna/management/commands/rnasync.py
@@ -0,0 +1,25 @@
+from django.conf import settings
+from django.core.management import BaseCommand
+
+from synctool.functions import sync_data
+
+from rna.utils import get_last_modified_date
+
+
+DEFAULT_RNA_SYNC_URL = 'https://nucleus.mozilla.org/rna/sync/'
+
+
+class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument('-u', '--url',
+                            default=getattr(settings, 'RNA_SYNC_URL', DEFAULT_RNA_SYNC_URL),
+                            help='Full URL to RNA Sync endpoint')
+        parser.add_argument('-c', '--clean', action='store_true',
+                            help='Delete all RNA data before sync.')
+
+    def handle(self, *args, **options):
+        clean = options['clean']
+        sync_data(url=options['url'],
+                  clean=clean,
+                  last_modified=None if clean else get_last_modified_date(),
+                  api_token=None)
