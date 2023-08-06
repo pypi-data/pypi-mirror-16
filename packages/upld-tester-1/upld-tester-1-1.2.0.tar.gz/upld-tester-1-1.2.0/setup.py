dcd2e73c69397255e23686eb237d119bebdc1141
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:23:15 +0000

    Add package upld-tester-1/1.2.0/upld-tester-1-1.2.0.tar.gz

---
+++ packages/upld-tester-1/upld-tester-1-1.2.0.tar.gz/upld-tester-1-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name            = 'upld-tester-1',
+	version         = '1.2.0',
+	py_modules      = ['upld-tester-1'],
+	author          = 'MU, Xin',
+	author_email    = 'muxin@live.co.uk',
+	url             = 'http://www.icbc.com.cn',
+	description     = 'A simple printer of nested lists',
+	)
