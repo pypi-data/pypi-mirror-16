2e74d9bf06cb2e2080eb3e2abba99250a035a0bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:46 +0000

    Add package MrWang_nester/1.0.0/MrWang_nester-1.0.0.tar.gz

---
+++ packages/MrWang_nester/MrWang_nester-1.0.0.tar.gz/MrWang_nester-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name		= 'MrWang_nester',
+	version		= '1.0.0',
+	py_modules	= ['MrWang_nester'],
+	author		= 'MrWang1996',
+	author_email	= 'wyn1996@hotmail.com',
+	url		= 'http://www.headfirstlabs.com',
+	description	= 'A simple printer of nested lists',
+	)
