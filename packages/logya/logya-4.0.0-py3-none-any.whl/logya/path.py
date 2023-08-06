103eec47e345d0e0e7f7e694cc4ade594864debf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:58:25 +0000

    Add package logya/4.0.0/logya-4.0.0-py3-none-any.whl

---
+++ packages/logya/logya-4.0.0-py3-none-any.whl/logya/path.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+# Wrappers for functions from os.path.
+import os
+import re
+
+from logya import allowed_exts
+
+re_url_replace = re.compile(r'[\/\s_]+')
+
+
+class PathResourceError(Exception):
+    pass
+
+
+def canonical_filename(name):
+    """Get file name from given path or file.
+
+    If name is not recognized as a file name a /index.html is added. To be
+    recognized as a file name it must end with an allowed extension.
+    Leading slashes are stripped off.
+    """
+
+    # Paths for generated indexes do not start with a slash.
+    if not name.startswith('/'):
+        name = '/{}'.format(name)
+
+    # Only allowed extension will be written to a file, otherwise a
+    # directory with the name is created and content written to index.html.
+    ext = os.path.splitext(name)[1]
+    if not ext or ext.lstrip('.') not in allowed_exts:
+        name = os.path.join(name, 'index.html')
+
+    return name.lstrip('/')
+
+
+def join(basedir, *args, **kwargs):
+    """Get joined name relative to basedir for file or path name.
+
+    Raises an exception if resource is required and doesn't exist.
+    """
+
+    path = os.path.join(basedir, *args)
+    if kwargs.get('required') and not os.path.exists(path):
+        raise PathResourceError(
+            'Resource at path {} does not exist.'.format(path))
+    return path
+
+
+def list_dirs_from_url(url):
+    """Returns a list of directories from given url.
+
+    The last directory is omitted as it contains an index.html file
+    containing the content of the corresponding document."""
+
+    return [d for d in url.strip('/').split('/') if d][:-1]
+
+
+def slugify(path):
+    return re.sub(re_url_replace, '-', path).lower()
+
+
+def target_file(basedir, url):
+    """Determine the absolute filename to create and return it.
+
+    If a URL points to a directory 'index.html' will be appended.
+    """
+
+    filename = canonical_filename(url)
+    return os.path.join(basedir, filename)
+
+
+def url_from_filename(filename, basedir=None):
+    """Creates a URL to be used in docs from basedir and filename."""
+
+    ext = os.path.splitext(filename)[1]
+    if ext:
+        filename = filename.replace(ext, '/')
+
+    if basedir:
+        filename = filename.replace(basedir, '')
+
+    return filename
