cf87df542c5b8280e26108527524279787f8cb6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:11 +0000

    Add package base64_util/0.1/base64_util-0.1.tar.gz

---
+++ packages/base64_util/base64_util-0.1.tar.gz/base64_util-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='base64_util',
+      version='0.1',
+      description='Some utility functions for handling base64 strings',
+      url='http://github.com/jlents/base64_util',
+      author='Joshua Lents',
+      author_email='joshua.lents@gmail.com',
+      license='MIT',
+      packages=['base64_util'],
+      zip_safe=False)
