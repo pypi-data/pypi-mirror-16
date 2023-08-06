57dd6fe68e076f4096fa1d588cfa8a9a4f487238
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:22:07 +0000

    Add package beryl/1.0/beryl-1.0.tar.gz

---
+++ packages/beryl/beryl-1.0.tar.gz/beryl-1.0/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+  name = 'beryl',
+  packages = ['beryl'],
+  package_dir = {'beryl': 'beryl'},
+  package_data = {'beryl': ['__init__.py','recorder.py','cache/text.txt']},
+  version = '1.0',
+  description = 'Makes Writing Tests Super Easy and Quick',
+  author = 'Daniel J. Dufour',
+  author_email = 'daniel.j.dufour@gmail.com',
+  url = 'https://github.com/DanielJDufour/beryl',
+  download_url = 'https://github.com/DanielJDufour/beryl/tarball/download',
+  keywords = ['python', 'testing'],
+  classifiers = [],
+)
