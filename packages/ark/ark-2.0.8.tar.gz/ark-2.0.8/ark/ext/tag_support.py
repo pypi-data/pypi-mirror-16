1ce604ad36870744e632621d788602421ebfd82a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:31 +0000

    Add package ark/2.0.8/ark-2.0.8.tar.gz

---
+++ packages/ark/ark-2.0.8.tar.gz/ark-2.0.8/ark/ext/tag_support.py
@@ -0,0 +1,113 @@
+# --------------------------------------------------------------------------
+# This extension implements Ark's record-tagging functionality.
+#
+# Author: Darren Mulholland <darren@mulholland.xyz>
+# License: Public Domain
+# --------------------------------------------------------------------------
+
+import ark
+from ark.utils import slugify
+
+
+# Maps tag-slugs to lists of record-filepaths indexed by type.
+rmap = {}
+
+
+# Maps tag-slugs to tag-names indexed by type.
+nmap = {}
+
+
+# A Tag instance pairs a tag-name with its corresponding tag-index url.
+class Tag:
+
+    def __init__(self, name, url):
+        self.name = name
+        self.url = url
+
+    def __repr__(self):
+        return 'Tag(name=%s, url=%s)' % (repr(self.name), repr(self.url))
+
+    def __str__(self):
+        return '<a href="%s">%s</a>' % (self.url, self.name)
+
+
+# Register a callback on the 'init_record' event hook to process and register
+# the record's tags.
+@ark.hooks.register('init_record')
+def register_tags(record):
+    tags, record['tags'] = record.get('tags', ''), []
+    for tag in (t.strip() for t in tags.split(',')):
+        if tag:
+            register(record['type'], tag, record['src'])
+            record['tags'].append(Tag(tag, url(record['type'], tag)))
+
+
+# Register a callback on the 'exit_build' event hook to build the tag index
+# pages.
+@ark.hooks.register('exit_build')
+def build_tag_indexes():
+
+    # Iterate over the site's record types.
+    for rectype, recmap in rmap.items():
+
+        # Fetch the type's configuration data.
+        typedata = ark.site.types(rectype)
+
+        # Iterate over the registered tags for the type.
+        for slug, filelist in recmap.items():
+
+            reclist = []
+            for filepath in filelist:
+                record = ark.records.record(filepath)
+                if typedata['order_by'] in record:
+                    reclist.append(record)
+
+            index = ark.pages.Index(
+                rectype,
+                slugs(rectype, slug),
+                reclist,
+                typedata['per_tag_index']
+            )
+
+            index['tag'] = nmap[rectype][slug]
+            index['is_tag_index'] = True
+            index['trail'] = [typedata['title'], nmap[rectype][slug]]
+
+            index.render()
+
+
+# Register a callback on the 'page_classes' filter to add tag classes.
+@ark.hooks.register('page_classes')
+def add_tag_classes(classes, page):
+    if page.get('is_tag_index'):
+        classes.append('tag-index')
+        classes.append('tag-index-%s' % slugify(page['tag']))
+    return classes
+
+
+# Register a callback on the 'page_templates' filter to add tag templates.
+@ark.hooks.register('page_templates')
+def add_tag_templates(templates, page):
+    if page.get('is_tag_index'):
+        templates = ['%s-tag-index' % page['type']['name'], 'tag-index', 'index']
+    return templates
+
+
+# Register a new tag mapping.
+def register(rectype, tag, filepath):
+    rmap.setdefault(rectype, {}).setdefault(slugify(tag), []).append(filepath)
+    nmap.setdefault(rectype, {}).setdefault(slugify(tag), tag)
+
+
+# Returns the tag-index url for the specified tag.
+def url(rectype, tag):
+    return ark.site.url(slugs(rectype, tag, 'index'))
+
+
+# Returns the output-slug list for the specified tag. Appends arguments.
+def slugs(rectype, tag, *append):
+    slugs = ark.site.slugs(rectype)
+    slugs.append(ark.site.types(rectype, 'tag_slug'))
+    slugs.append(slugify(tag))
+    slugs.extend(append)
+    return slugs
---
+++ packages/ark/ark-2.0.8.tar.gz/ark-2.0.8/ark/ini/config.py
@@ -0,0 +1,45 @@
+# --------------------------------------------------------------------------
+# Site Configuration File
+# --------------------------------------------------------------------------
+
+# This is a sample site configuration file. You can use this file to
+# customise how Ark builds your site. Variables you set here are also
+# available to themes and plugins.
+
+
+# --------------------------------------------------------------------------
+# Theme Settings
+# --------------------------------------------------------------------------
+
+# The theme to use when building the site. This attribute should specify
+# the name of a theme directory in the site's 'lib' folder.
+theme = "phoenix"
+
+# The default theme displays the 'title' string as the site's title.
+title = "Site Title"
+
+# The default theme displays the 'tagline' string below the site's title.
+tagline = "Just another Ark site"
+
+# The default theme displays the 'copyright' string in the site's footer.
+copyright = "Copyright &copy; 2016 Foobar Industries"
+
+
+# --------------------------------------------------------------------------
+# Root URL
+# --------------------------------------------------------------------------
+
+# Your root url can be an explicit domain ("http://example.com/") for
+# absolute urls, a single forward slash ("/") for site-relative urls, or an
+# empty string (the default) for page-relative urls.
+root = ""
+
+
+# --------------------------------------------------------------------------
+# File Extension
+# --------------------------------------------------------------------------
+
+# You can choose an arbitrary file extension for generated files (".html")
+# or pass an empty string ("") to use no extension at all. Specify a single
+# forward slash ("/") to generate directory-style urls.
+extension = ".html"
