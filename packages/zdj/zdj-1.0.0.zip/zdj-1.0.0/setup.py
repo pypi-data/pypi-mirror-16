dfdba0483d6d50bcfa25300035f8516502a2d2c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:19 +0000

    Add package zdj/1.0.0/zdj-1.0.0.zip

---
+++ packages/zdj/zdj-1.0.0.zip/zdj-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+	name = 'zdj',
+	version = '1.0.0',
+	py_modules = ['nester'],
+	author = 'zdj',
+	author_email = 'dj35@mail.ustc.edu.cn',
+	url = 'https://github.com/DingjZhang',
+	description = 'A simple module',
+
+) 
