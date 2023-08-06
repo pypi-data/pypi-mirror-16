84f41c5aeea035a0fa0e4f007dc5b168e2c9b79b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:19 +0000

    Add package msg/0.7.4/msg-0.7.4.tar.gz

---
+++ packages/msg/msg-0.7.4.tar.gz/msg-0.7.4/msg/models.py
@@ -0,0 +1,78 @@
+import datetime
+import sqlalchemy
+
+from sqlalchemy.ext.declarative import declarative_base
+from werkzeug.security import (generate_password_hash,
+                               check_password_hash)
+
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'users'
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
+    created = sqlalchemy.Column(sqlalchemy.DateTime,
+                                default=datetime.datetime.utcnow)
+    username = sqlalchemy.Column(sqlalchemy.String(30), unique=True)
+    password_hash = sqlalchemy.Column(sqlalchemy.String())
+    bio = sqlalchemy.Column(sqlalchemy.String())
+
+    def __init__(self, username, password, bio=None):
+        self.username = username
+        self.password_hash = self.hash_password(password)
+        self.bio = bio
+
+    @staticmethod
+    def hash_password(password):
+        return generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def to_dict(self):
+        """Return a dictionary representation of this
+        user, which keys are in jsonStyle.
+
+        """
+
+        return {'username': self.username,
+                'bio': self.bio,
+                'id': self.id,
+                'created': self.created.isoformat("T") + 'Z'}
+
+
+class Message(Base):
+    """A submission to the site, just some text,
+    a timestamp, an associated user, and an ID.
+
+    """
+
+    __tablename__ = 'posts'
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
+    user_id = sqlalchemy.Column(sqlalchemy.Integer,
+                                sqlalchemy.ForeignKey(User.id))
+    created = sqlalchemy.Column(sqlalchemy.DateTime,
+                                default=datetime.datetime.utcnow)
+    text = sqlalchemy.Column(sqlalchemy.String())
+
+    user = sqlalchemy.orm.relationship('User', foreign_keys='Message.user_id',
+                                       lazy='subquery')
+
+    def __init__(self, user_id, text):
+        self.user_id = user_id
+        self.text = text
+
+    def __repr__(self):
+        return '<Message #%s>' % self.id
+
+    def to_dict(self):
+        """Return a dictionary representation of this
+        user, which keys are in jsonStyle.
+
+        """
+
+        return {'id': self.id,
+                'text': self.text,
+                'user': self.user.to_dict(),
+                'created': self.created.isoformat("T") + 'Z'}
---
+++ packages/msg/msg-0.7.4.tar.gz/msg-0.7.4/setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+"""msg package installer.
+
+"""
+
+import sys
+from setuptools import setup
+from distutils.version import StrictVersion
+
+from pip.req import parse_requirements
+
+
+# Here I am lazy; I really didn't want to maintain the depends
+# in both requirements files AND in this setup, thus we are
+# going to read them all into a list.
+#
+# CREDIT: http://stackoverflow.com/questions/14399534/how-can-i-reference-requirements-txt-for-the-install-requires-kwarg-in-setuptool
+#
+# parse_requirements() returns generator of pip.req.InstallRequirement objects
+install_reqs = parse_requirements('requirements/develop.txt', session=False)
+# reqs is a list of requiremens
+# e.g. ['django==1.5.1', 'mezzanine==1.4.6']
+reqs = [str(ir.req) for ir in install_reqs]
+
+# demon magic to get version without activating
+# imports
+exec(open('msg/__init__.py').read())
+setup(name='msg',
+      packages=['msg'],
+      version=__version__,
+      description='msg server backend',
+      setup_requires=['setuptools-markdown'],
+      long_description_markdown_filename='README.md',
+      author='Lily Seabreeze',
+      author_email='lillian.gardenia.seabreeze@gmail.com',
+      license='MIT',
+      classifiers=['Development Status :: 3 - Alpha',
+                   'Intended Audience :: Developers',
+                   'Natural Language :: English',
+                   'Programming Language :: Python :: 2.7',
+                   'Programming Language :: Python :: 3.4',
+                  ],
+    )
+
