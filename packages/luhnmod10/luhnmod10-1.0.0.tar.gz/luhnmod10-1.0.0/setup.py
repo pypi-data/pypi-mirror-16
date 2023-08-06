463d34e1314f0eec4e9d0c26215154d1d07c6343
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:03 +0000

    Add package luhnmod10/1.0.0/luhnmod10-1.0.0.tar.gz

---
+++ packages/luhnmod10/luhnmod10-1.0.0.tar.gz/luhnmod10-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='luhnmod10',
+      version='1.0.0',
+      description='A fast and simple in-place implementation of the luhn check algorithm.',
+      url='https://github.com/luhnmod10/python',
+      author='Leigh McCulloch',
+      license='MIT',
+      packages=['luhnmod10'],
+      zip_safe=True,
+      )
