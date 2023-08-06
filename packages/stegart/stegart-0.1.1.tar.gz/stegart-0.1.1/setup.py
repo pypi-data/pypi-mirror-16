e12c846c6169deecfa57b705092bb71419e08cc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:16:59 +0000

    Add package stegart/0.1.1/stegart-0.1.1.tar.gz

---
+++ packages/stegart/stegart-0.1.1.tar.gz/stegart-0.1.1/setup.py
@@ -0,0 +1,40 @@
+'''Renders a graphical representation of the data provided'''
+
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='stegart',
+    version='0.1.1',
+    description='Renders a graphical representation of the data provided',
+    long_description=long_description,
+    url='https://github.com/kulinacs/stegart',
+    author='kulinacs',
+    author_email='nicklaus@kulinacs.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Multimedia :: Graphics',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ],
+    keywords='image representation steganography',
+    packages=find_packages(),
+    install_requires=['numpy'],
+    entry_points={
+        'console_scripts': [
+            'stegart=stegart.stegart:main',
+        ],
+    },
+)
