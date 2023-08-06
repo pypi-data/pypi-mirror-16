224c64be854b57e0b08197927eee95509270d4ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:23:29 +0000

    Add package plone.i18n/3.0.5/plone.i18n-3.0.5.tar.gz

---
+++ packages/plone.i18n/plone.i18n-3.0.5.tar.gz/plone.i18n-3.0.5/plone/i18n/normalizer/fr.py
@@ -0,0 +1,31 @@
+# -*- coding: UTF-8 -*-
+
+from plone.i18n.normalizer.interfaces import INormalizer
+from zope.interface import implementer
+from plone.i18n.normalizer.base import mapUnicode
+
+# French character mapping
+mapping = {
+    339: 'oe'
+}
+
+@implementer(INormalizer)
+class Normalizer(object):
+    """
+    This normalizer can normalize any unicode string and returns a version
+    that only contains of ASCII characters.
+
+    Let's make sure that this implementation actually fulfills the API.
+
+      >>> from zope.interface.verify import verifyClass
+      >>> verifyClass(INormalizer, Normalizer)
+      True
+    """
+
+    def normalize(self, text, locale=None, max_length=None):
+        """
+        Returns a normalized text. text has to be a unicode string.
+        """
+        return mapUnicode(text, mapping=mapping)
+
+normalizer = Normalizer()
