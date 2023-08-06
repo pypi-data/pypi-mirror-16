887badd5de5b1ba92d3df767782a6aa090242d3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:03 +0000

    Add package kibana/0.7/kibana-0.7.tar.gz

---
+++ packages/kibana/kibana-0.7.tar.gz/kibana-0.7/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name='kibana',
+    packages=['kibana'],
+    version='0.7',
+    description='Kibana configuration index (.kibana) command line interface and python API (visualization import/export and mappings refresh)',
+    author='Ryan Farley',
+    author_email='rfarley@mitre.org',
+    url='https://github.com/rfarley3/Kibana',
+    download_url='https://github.com/rfarley3/Kibana/tarball/0.7',
+    keywords=['kibana', 'config', 'import', 'export', 'mappings'],
+    classifiers=[],
+    install_requires=(
+        'elasticsearch',
+        'argparse',
+        'requests',
+    ),
+    entry_points={
+        'console_scripts': [
+            'dotkibana = kibana.__main__:main',
+        ]
+    },
+)
