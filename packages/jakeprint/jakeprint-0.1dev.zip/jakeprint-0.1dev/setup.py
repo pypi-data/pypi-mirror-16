f82a9164452ab934179250e259b1f3393bfe51f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:53 +0000

    Add package jakeprint/0.1dev/jakeprint-0.1dev.zip

---
+++ packages/jakeprint/jakeprint-0.1dev.zip/jakeprint-0.1dev/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+	name = "jakeprint",
+	version = "0.1dev",
+	packages = ["jakeprint",],
+	license = "MIT",
+	long_description = open("README.txt").read(),
+)
