69cacbd632626a2fda512abf13360dffaa02522c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:16 +0000

    Add package django-admin-rangefilter/0.1.2/django-admin-rangefilter-0.1.2.tar.gz

---
+++ packages/django-admin-rangefilter/django-admin-rangefilter-0.1.2.tar.gz/django-admin-rangefilter-0.1.2/rangefilter/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+
+__author__ = 'Dmitriy Sokolov'
+__version__ = '0.1.2'
+
+
+default_app_config = 'rangefilter.apps.RangeFilterConfig'
