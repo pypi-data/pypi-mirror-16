25625b124e7af65d2acc200899a6d4913d63cebe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:04 +0000

    Add package openiot/0.3/openiot-0.3.tar.gz

---
+++ packages/openiot/openiot-0.3.tar.gz/openiot-0.3/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='openiot',
+      version='0.3',
+      description='OpenIoT Gateway',
+      url='http://www.openiot.org.ng',
+      author='Ahmad Sadiq',
+      author_email='sadiq.a.ahmad@gmail.com',
+      license='BSD',
+      packages=['openiot'],
+      scripts=['bin/openiotgw.py'],
+      zip_safe=False)
