986323c2043f3ae845d9fd1546121459bbcfbafa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:10 +0000

    Add package gmailcount/0.1.3/gmailcount-0.1.3.tar.gz

---
+++ packages/gmailcount/gmailcount-0.1.3.tar.gz/gmailcount-0.1.3/setup.py
@@ -0,0 +1,32 @@
+import os
+from setuptools import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name="gmailcount",
+    version='0.1.3',
+    author="Julian Andrews",
+    author_email="jandrews271@gmail.com",
+    description=("Script to count the number of emails in your gmail inbox"),
+    license="MIT",
+    keywords="email gmail script",
+    url="http://packages.python.org/gmailcount",
+    long_description=read('README.md'),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Communications :: Email",
+        "Topic :: Utilities",
+    ],
+    scripts=['gmailcount'],
+    install_requires=['requests>=2.9', 'keyring>=8.4'],
+    extras_require={
+        'secretservice': ['secretstorage'],
+        'altkeyrings': ['keyrings.alt'],
+    }
+)
