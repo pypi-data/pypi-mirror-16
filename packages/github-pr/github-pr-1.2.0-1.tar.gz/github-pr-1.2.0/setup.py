4e805696987128cb321bd572ee17833b7bf70864
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:30 +0000

    Add package github-pr/1.2.0/github-pr-1.2.0-1.tar.gz

---
+++ packages/github-pr/github-pr-1.2.0-1.tar.gz/github-pr-1.2.0/setup.py
@@ -0,0 +1,36 @@
+"""
+(c) Copyright 2015. DataXu, Inc. All Rights Reserved.
+
+github-pr setup
+"""
+
+from setuptools import setup
+
+setup(
+    name='github-pr',
+    author='DataXu',
+    author_email='mferrante@dataxu.com',
+    description='Pull Request utility script',
+    license='(c) Copyright 2015. DataXu, Inc. All Rights Reserved.',
+    entry_points={
+        'console_scripts': [
+            'github-pr = github_pr:main',
+        ]
+    },
+    install_requires=[
+        'argparse==1.3.0',
+        'PyGithub>=1.25.2',
+        'wsgiref==0.1.2',
+        'tabulate==0.7.5'
+    ],
+    setup_requires=[
+        'argparse',
+        'PyGithub',
+        'wsgiref',
+        'tabulate'
+    ],
+    url='https://github.com/dataxu/github-pr',
+    version='1.2.0',
+    scripts=['github_pr.py'],
+    keywords=['github'],
+)
