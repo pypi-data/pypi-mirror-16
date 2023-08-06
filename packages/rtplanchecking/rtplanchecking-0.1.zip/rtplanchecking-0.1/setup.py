163856798c9a5a77710b6c2db422a71b337908e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:16 +0000

    Add package rtplanchecking/0.1/rtplanchecking-0.1.zip

---
+++ packages/rtplanchecking/rtplanchecking-0.1.zip/rtplanchecking-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name = "rtplanchecking",
+    version = "0.1",
+    author = "Simon Biggs",
+    author_email = "mail@simonbiggs.net",
+    description = "Checks patient plans",
+    keywords = [],
+    packages = ["rtplanchecking"],
+    license='AGPL3+',
+    classifiers = [],
+)
