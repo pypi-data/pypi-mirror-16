202e919a9319afa2f9ad21086d2b82e294697e2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:26 +0000

    Add package nylas/1.2.3/nylas-1.2.3.tar.gz

---
+++ packages/nylas/nylas-1.2.3.tar.gz/nylas-1.2.3/tests/credentials.py
@@ -0,0 +1,3 @@
+APP_ID = '2ajt0hf90hrrfoi41j6fl95vr'
+APP_SECRET = 'aztw0hkr2yqp5xkxqztjlxms6'
+AUTH_TOKEN = 'IbohROubhH81plXg6Lu4Ycm2Q1GMsC' # The access token to a local account (i.e: running on your machine).
---
+++ packages/nylas/nylas-1.2.3.tar.gz/nylas-1.2.3/tests/system.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+import json
+import re
+import pytest
+import time
+import datetime
+import sys
+from nylas import APIClient
+from nylas.client.restful_models import Label, Folder
+from nylas.client.errors import *
+from credentials import APP_ID, APP_SECRET, AUTH_TOKEN
+
+client = APIClient(APP_ID, APP_SECRET, AUTH_TOKEN)
+
+count = 0
+
+print "Listing accounts"
+for account in client.accounts:
+    print (account.email_address, account.provider)
+
+print 'Marking the first thread as unread'
+th = client.threads.where({'in': 'inbox'}).first()
+print th.subject
+th.mark_as_unread()
+
+print "Displaying 10 thread subjects"
+for thread in client.threads.items():
+    print thread.subject
+    count += 1
+    if count == 10:
+        break
+
+print "Sending an email"
+draft = client.drafts.create()
+draft.to = [{'name': 'Python SDK test', 'email': 'karim@nylas.com'}]
+draft.subject = "Python SDK test"
+draft.body = "Stay polish, stay hungary"
+draft.send()
+
+print 'Creating an event'
+calendar = filter(lambda cal: not cal.read_only, client.calendars)[0]
+ev = client.events.create()
+ev.title = "Party at the Ritz"
+
+d1 = datetime.datetime.now() + datetime.timedelta(days=5,hours=4)
+d2 = datetime.datetime.now() + datetime.timedelta(days=5,hours=5)
+
+ev.when = {"start_time": time.mktime(d1.timetuple()), "end_time": time.mktime(d2.timetuple())}
+ev.location = "The Old Ritz"
+ev.participants = [{'name': 'Karim Hamidou', 'email': 'karim@nylas.com'}]
+ev.calendar_id = calendar.id
+ev.save(notify_participants='true')
+
+print 'Listing folders'
+for label in client.labels:
+    print label.display_name
