50ac51bd1b6f2d5b103fce2492597aebbd591e53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:20 +0000

    Add package dirlog/0.2.3/dirlog-0.2.3.tar.gz

---
+++ packages/dirlog/dirlog-0.2.3.tar.gz/dirlog-0.2.3/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+import fastentrypoints
+
+setup(
+    name='dirlog',
+    version='0.2.3',
+    description='keep a log of directories you visit to get back fast',
+    long_description=open('README.rst').read(),
+    url='https://github.com/ninjaaron/dirlog',
+    author='Aaron Christianson',
+    author_email='ninjaaron@gmail.com',
+    py_modules = ['dirlog'],
+    classifiers=['Programming Language :: Python :: 3.5'],
+    entry_points={'console_scripts': [
+        'dirlog=dirlog:install',
+        'dlog=dirlog:wrap',
+        'dirlog-cd=dirlog:main']},
+)
