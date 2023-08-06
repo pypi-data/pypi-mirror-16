afe5abac31125b2dcb826275b302547ac8f2a6ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:58:55 +0000

    Add package os-gobble/0.1.5/os-gobble-0.1.5.tar.gz

---
+++ packages/os-gobble/os-gobble-0.1.5.tar.gz/os-gobble-0.1.5/gobble/cli.py
@@ -0,0 +1,19 @@
+"""Command line interface for Gobble"""
+
+
+from click import command, argument, Path
+from click import group
+
+from gobble.fiscal import FiscalDataPackage
+
+
+@group(name='gobble')
+def cli():
+    pass
+
+
+@command(name='check')
+@argument('filepath', type=Path(exists=True))
+def validate(filepath):
+    package = FiscalDataPackage(filepath)
+    package.validate(raise_error=False)
