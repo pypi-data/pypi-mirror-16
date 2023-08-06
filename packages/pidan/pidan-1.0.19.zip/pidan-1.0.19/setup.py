65647bcc2c395c89d18380575c93e8377d2fa173
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:56 +0000

    Add package pidan/1.0.19/pidan-1.0.19.zip

---
+++ packages/pidan/pidan-1.0.19.zip/pidan-1.0.19/setup.py
@@ -0,0 +1,16 @@
+# install:python setup.py sdist upload
+# -*- coding:utf-8 -*-
+from distutils.core import setup
+ 
+setup (
+    name = 'pidan',
+    version = '1.0.19',
+    packages=["pidan"],
+    platforms=['any'],
+    author = 'chengang',
+    author_email = 'chengang.net@gmail.com',
+    description = u'修改http相关函数，对头尾空格进行过滤',
+    package_data = {
+        '': ['*.data'],
+    },
+)
