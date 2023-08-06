b93295655ea536118b7c1e010f44dee0cbd59ac8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:20 +0000

    Add package netfilter/0.6.4/netfilter-0.6.4.tar.gz

---
+++ packages/netfilter/netfilter-0.6.4.tar.gz/netfilter-0.6.4/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='netfilter',
+    version='0.6.4',
+    description='Python modules for manipulating netfilter rules',
+    url='https://github.com/jlaine/python-netfilter',
+    author='Jeremy Laine',
+    author_email='jeremy.laine@m4x.org',
+    license='GPLv3+',
+    packages=['netfilter'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: System :: Networking :: Firewalls',
+        'Topic :: System :: Systems Administration',
+    ],
+    test_suite='tests',
+)
