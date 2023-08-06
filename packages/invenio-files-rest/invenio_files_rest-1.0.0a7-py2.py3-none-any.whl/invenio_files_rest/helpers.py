35ba222ddad82c3e3955a06177d06728f94268ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:06 +0000

    Add package invenio-files-rest/1.0.0a7/invenio_files_rest-1.0.0a7-py2.py3-none-any.whl

---
+++ packages/invenio-files-rest/invenio_files_rest-1.0.0a7-py2.py3-none-any.whl/invenio_files_rest/helpers.py
@@ -0,0 +1,155 @@
+# This file is part of Invenio.
+# Copyright (C) 2015, 2016 CERN.
+#
+# Invenio is free software; you can redistribute it
+# and/or modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of the
+# License, or (at your option) any later version.
+#
+# Invenio is distributed in the hope that it will be
+# useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Invenio; if not, write to the
+# Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+# MA 02111-1307, USA.
+#
+# In applying this license, CERN does not
+# waive the privileges and immunities granted to it by virtue of its status
+# as an Intergovernmental Organization or submit itself to any jurisdiction.
+
+"""File serving helpers for Files REST API."""
+
+from __future__ import absolute_import, print_function
+
+import hashlib
+import mimetypes
+import os
+from time import time
+
+from flask import current_app, request
+from werkzeug.datastructures import Headers
+from werkzeug.wsgi import FileWrapper
+
+
+def send_stream(stream, filename, size, mtime, mimetype=None, restricted=True,
+                as_attachment=False, etag=None, content_md5=None,
+                chunk_size=8192, conditional=True):
+    """Send the contents of a file to the client."""
+    # Guess mimetype from filename if not provided.
+    if mimetype is None and filename:
+        mimetype = mimetypes.guess_type(filename)[0]
+    if mimetype is None:
+        mimetype = 'application/octet-stream'
+
+    # Construct headers
+    headers = Headers()
+    if as_attachment:
+        headers.add('Content-Disposition', 'attachment', filename=filename)
+    else:
+        headers.add('Content-Disposition', 'inline')
+    headers['Content-Length'] = size
+    if content_md5:
+        headers['Content-MD5'] = content_md5
+
+    # Construct response object.
+    rv = current_app.response_class(
+        FileWrapper(stream, buffer_size=chunk_size),
+        mimetype=mimetype,
+        headers=headers,
+        direct_passthrough=True,
+    )
+
+    # Set etag if defined
+    if etag:
+        rv.set_etag(etag)
+
+    # Set last modified time
+    if mtime is not None:
+        rv.last_modified = int(mtime)
+
+    # Set cache-control
+    if not restricted:
+        rv.cache_control.public = True
+        cache_timeout = current_app.get_send_file_max_age(filename)
+        if cache_timeout is not None:
+            rv.cache_control.max_age = cache_timeout
+            rv.expires = int(time() + cache_timeout)
+
+    if conditional:
+        rv = rv.make_conditional(request)
+
+    return rv
+
+
+def make_path(base_uri, path, filename, path_dimensions, split_length):
+    """Generate a path as base location for file instance."""
+    assert len(path) > path_dimensions * split_length
+
+    uri_parts = []
+    for i in range(path_dimensions):
+        uri_parts.append(path[0:split_length])
+        path = path[split_length:]
+    uri_parts.append(path)
+    uri_parts.append(filename)
+
+    return os.path.join(base_uri, *uri_parts)
+
+
+def compute_md5_checksum(stream, chunk_size=None, progress_callback=None):
+    """Helper method to compute checksum from a stream.
+
+    :param src: File-like object.
+    :param chunk_size: Read at most size bytes from the file.
+    :param progress_callback: Function accepting one argument with number
+        of bytes read.
+    """
+    bytes_read = 0
+    m = hashlib.md5()
+    while 1:
+        chunk = stream.read(chunk_size)
+        if not chunk:
+            if progress_callback:
+                progress_callback(bytes_read)
+            break
+        m.update(chunk)
+        bytes_read += len(chunk)
+        if progress_callback:
+            progress_callback(bytes_read)
+    return "md5:{0}".format(m.hexdigest())
+
+
+def populate_from_path(bucket, source, checksum=True, key_prefix=''):
+    """Populate a ``bucket`` from all files in path."""
+    from .models import FileInstance, ObjectVersion
+
+    def create_file(key, path):
+        """Create new ``ObjectVersion`` from path or existing ``FileInstance``.
+
+        It checks MD5 checksum and size of existing ``FileInstance``s.
+        """
+        key = key_prefix + key
+
+        if checksum:
+            file_checksum = compute_md5_checksum(open(path, 'rb'),
+                                                 chunk_size=80960)
+            file_instance = FileInstance.query.filter_by(
+                checksum=file_checksum, size=os.path.getsize(path)
+            ).first()
+            if file_instance:
+                return ObjectVersion.create(
+                    bucket, key, _file_id=file_instance.id
+                )
+        return ObjectVersion.create(bucket, key, stream=open(path, 'rb'))
+
+    if os.path.isfile(source):
+        yield create_file(os.path.basename(source), source)
+    else:
+        for root, dirs, files in os.walk(source, topdown=False):
+            for name in files:
+                filename = os.path.join(root, name)
+                assert filename.startswith(source)
+                parts = [p for p in filename[len(source):].split(os.sep) if p]
+                yield create_file('/'.join(parts), os.path.join(root, name))
---
+++ packages/invenio-files-rest/invenio_files_rest-1.0.0a7-py2.py3-none-any.whl/invenio_files_rest/tasks.py
@@ -0,0 +1,142 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of Invenio.
+# Copyright (C) 2016 CERN.
+#
+# Invenio is free software; you can redistribute it
+# and/or modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of the
+# License, or (at your option) any later version.
+#
+# Invenio is distributed in the hope that it will be
+# useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Invenio; if not, write to the
+# Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+# MA 02111-1307, USA.
+#
+# In applying this license, CERN does not
+# waive the privileges and immunities granted to it by virtue of its status
+# as an Intergovernmental Organization or submit itself to any jurisdiction.
+
+"""Celery tasks for Invenio-Files-REST."""
+
+from __future__ import absolute_import, print_function
+
+import uuid
+from datetime import datetime
+
+from celery import current_task, shared_task
+from celery.states import state
+from celery.utils.log import get_task_logger
+from flask import current_app
+from invenio_db import db
+from sqlalchemy.exc import IntegrityError
+
+from .models import FileInstance, Location, MultipartObject, ObjectVersion
+
+logger = get_task_logger(__name__)
+
+
+def progress_updater(size, total):
+    """Progress reporter for checksum verification."""
+    current_task.update_state(
+        state=state('PROGRESS'),
+        meta=dict(size=size, total=total)
+    )
+
+
+@shared_task(ignore_result=True)
+def verify_checksum(file_id):
+    """Verify checksum of a file instance."""
+    f = FileInstance.query.get(uuid.UUID(file_id))
+    f.verify_checksum(progress_callback=progress_updater)
+    db.session.commit()
+
+
+@shared_task(ignore_result=True, max_retries=3, default_retry_delay=20 * 60)
+def migrate_file(src_id, location_name, post_fixity_check=False):
+    """Task to migrate a file instance to a new location."""
+    location = Location.get_by_name(location_name)
+    f_src = FileInstance.get(src_id)
+
+    # Create destination
+    f_dst = FileInstance.create()
+    db.session.commit()
+
+    try:
+        # Copy contents
+        f_dst.copy_contents(
+            f_src,
+            progress_callback=progress_updater,
+            default_location=location.uri,
+        )
+        db.session.commit()
+    except Exception:
+        # Remove destination file instance if an error occurred.
+        db.session.delete(f_dst)
+        db.session.commit()
+        raise
+
+    # Update all objects pointing to file.
+    ObjectVersion.relink_all(f_src, f_dst)
+    db.session.commit()
+
+    # Start a fixity check
+    if post_fixity_check:
+        verify_checksum.delay(str(f_dst.id))
+
+
+@shared_task(ignore_result=True)
+def remove_file_data(file_id, silent=True):
+    """Remove file instance and associated data."""
+    try:
+        # First remove FileInstance from database and commit transaction to
+        # ensure integrity constraints are checked and enforced.
+        f = FileInstance.get(file_id)
+        if not f.writable:
+            return
+        f.delete()
+        db.session.commit()
+        # Next, remove the file on disk. This leaves the possibility of having
+        # a file on disk dangling in case the database removal works, and the
+        # disk file removal doesn't work.
+        f.storage().delete()
+    except IntegrityError:
+        if not silent:
+            raise
+
+
+@shared_task(ignore_result=True)
+def merge_multipartobject(upload_id):
+    """Merge multipart object."""
+    mp = MultipartObject.query.filter_by(upload_id=upload_id).one_or_none()
+    if not mp:
+        raise RuntimeError('Upload ID does not exists.')
+    if not mp.completed:
+        raise RuntimeError('MultipartObject is not completed.')
+
+    try:
+        mp.merge_parts()
+        db.session.commit()
+    except Exception:
+        db.session.rollback()
+        raise
+
+
+@shared_task(ignore_result=True)
+def remove_expired_multipartobjects():
+    """Remove expired multipart objects."""
+    delta = current_app.config['FILES_REST_MULTIPART_EXPIRES']
+    expired_dt = datetime.utcnow() - delta
+
+    file_ids = []
+    for mp in MultipartObject.query_expired(expired_dt):
+        file_ids.append(str(mp.file_id))
+        mp.delete()
+
+    for fid in file_ids:
+        remove_file_data.delay(fid)
