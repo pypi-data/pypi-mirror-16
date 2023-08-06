742729716ec85ec15f0f4a64135bc161749c6b0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:36:23 +0000

    Add package lsanomaly/0.1.2/lsanomaly-0.1.2.tar.gz

---
+++ packages/lsanomaly/lsanomaly-0.1.2.tar.gz/lsanomaly-0.1.2/setup.py
@@ -0,0 +1,40 @@
+import re
+import codecs
+from setuptools import setup, find_packages
+
+with codecs.open('README.rst', 'r', 'utf-8') as f:
+    split = re.split('(Table of Contents)', f.read())
+    readme = split[1] + split[2]
+
+install_requires = ['scikit-learn']
+
+setup(
+    name='lsanomaly',
+    version='0.1.2',
+    url='https://github.com/lsanomaly/lsanomaly',
+    description='Least squares anomaly detection.',
+    long_description=readme,
+    author='John Quinn, David Westerhoff',
+    author_email='jquinn@cit.ac.ug, dmwesterhoff@gmail.com',
+    license='MIT',
+    keywords='anomaly outlier novelty detection ' +
+             'machine learning scikit-learn sklearn',
+    packages=find_packages(exclude=('tests', 'tests.*')),
+    include_package_data=True,
+    entry_points={
+    },
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    install_requires=install_requires,
+)
