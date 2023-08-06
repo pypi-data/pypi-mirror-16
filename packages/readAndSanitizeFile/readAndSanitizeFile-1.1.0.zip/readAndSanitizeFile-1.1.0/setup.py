1485a8cee1b2736f48117b3d169f3a6ca6dac66e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:51 +0000

    Add package readAndSanitizeFile/1.1.0/readAndSanitizeFile-1.1.0.zip

---
+++ packages/readAndSanitizeFile/readAndSanitizeFile-1.1.0.zip/readAndSanitizeFile-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup (
+    name = 'readAndSanitizeFile',
+    version = '1.1.0',
+    py_modules = ['sanitizeTwo'],
+    author = 'Harkit',
+    author_email = 'harkit.shrestha@gmail.com',
+    url = 'http://www.thebukhara.com',
+    description = 'Testing some data reading and sanitizing',
+)
