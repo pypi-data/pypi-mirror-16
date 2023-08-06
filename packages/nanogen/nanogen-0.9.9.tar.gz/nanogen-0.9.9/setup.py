03e3327f4dcd336cb2551f6820be9f403d51e4f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:14:45 +0000

    Add package nanogen/0.9.9/nanogen-0.9.9.tar.gz

---
+++ packages/nanogen/nanogen-0.9.9.tar.gz/nanogen-0.9.9/setup.py
@@ -0,0 +1,46 @@
+import os
+from setuptools import setup
+
+from nanogen import version
+
+entry_points = {
+    'console_scripts': ['nanogen = cli:cli']
+}
+
+long_description = open(
+    os.path.join(
+        os.path.dirname(__file__),
+        'README.rst'
+    )
+).read()
+
+setup(name='nanogen',
+      version=version,
+      description='A very small static blog generator',
+      long_description=long_description,
+      author='Bill Israel',
+      author_email='bill.israel@gmail.com',
+      license='MIT',
+      url='https://github.com/epochblue/nanogen',
+      py_modules=['nanogen'],
+      install_requires=[
+          'click==6.6',
+          'mistune==0.7.3',
+          'Jinja2==2.8',
+          'Pygments==2.1.3'
+      ],
+      entry_points=entry_points,
+      keywords=['command line', 'static generator', 'blog'],
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Environment :: Console',
+          'Intended Audience :: Developers',
+          'Intended Audience :: Information Technology',
+          'License :: OSI Approved :: MIT License',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3.5',
+          'Topic :: Internet :: WWW/HTTP',
+          'Topic :: Utilities',
+          'Topic :: Software Development :: Libraries :: Python Modules'
+      ])
