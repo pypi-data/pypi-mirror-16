993b50881c8d94bb03b63dceea93eb5aeba7113a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:33 +0000

    Add package Firenado/0.1.4.1/Firenado-0.1.4.1.tar.gz

---
+++ packages/Firenado/Firenado-0.1.4.1.tar.gz/Firenado-0.1.4.1/setup.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+# vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4:
+#
+# Copyright 2013-2014 Flavio Garcia
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from setuptools import setup
+import firenado
+
+install_requires = [
+    "pyyaml>=3.11",
+    "tornado>=4.2.1",
+    "six==1.10.0",
+]
+
+setup(
+    name='Firenado',
+    version='.'.join(map(str,firenado.__version__)),
+    description='Firenado is a python web framework based on '
+            'Tornado web framework/server.',
+    license='Apache License V2.0',
+    author='Flavio Garcia',
+    author_email='piraz@candango.org',
+    install_requires=install_requires,
+    url='https://github.com/candango/firenado',
+    packages=[
+        'firenado',
+        'firenado.components',
+        'firenado.components.assets',
+        'firenado.components.firenado',
+        'firenado.conf',
+        'firenado.management',
+        'firenado.util',
+    ],
+    package_dir={'firenado': 'firenado'},
+    package_data={'firenado': [
+        'conf/*.yml', 'management/templates/*/*.txt',
+        'components/*/conf/*.yaml.example',
+        'components/*/templates/*.html',
+        'components/*/static/css/*.css',
+        'components/*/static/js/*.js',
+        'components/*/static/js/locales/*',
+        'components/*/static/js/views/*.ejs',
+
+    ]},
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Internet :: WWW/HTTP :: WSGI',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    scripts=['firenado/bin/firenado-cli.py'],
+    entry_points={'console_scripts': [
+        'firenado = firenado.management:run_from_command_line',
+    ]},
+)
+
