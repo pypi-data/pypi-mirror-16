b1d56871bf93979d67ac0f734c82e96ed7bea2be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:56:39 +0000

    Add package eva/0.1.1/eva-0.1.1.tar.gz

---
+++ packages/eva/eva-0.1.1.tar.gz/eva-0.1.1/setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='eva',
+    version='0.1.1',
+    description='Plugin for virtualenvwrapper to automatically '
+                'export config vars found in your project level '
+                '.env file.',
+    author='Brian Jinwright',
+    author_email='opensource@ipoots.com',
+    url='https://github.com/bjinwright/eva',
+    namespace_packages=['virtualenvwrapper'],
+    packages=['virtualenvwrapper'],
+    py_modules=['eva.cli'],
+
+    install_requires=[
+        'virtualenv',
+        'virtualenvwrapper>=2.11',
+        'click>=6.6',
+        'terminaltables>=3.0.0',
+    ],
+    entry_points={
+        'virtualenvwrapper.pre_activate': [
+            'configvars = virtualenvwrapper.configvars:pre_activate',
+        ],
+        'virtualenvwrapper.pre_activate_source': [
+            'configvars = virtualenvwrapper.configvars:pre_activate_source',
+        ],
+        'virtualenvwrapper.post_deactivate': [
+            'configvars = virtualenvwrapper.configvars:post_deactivate',
+        ],
+        'virtualenvwrapper.post_deactivate_source': [
+            'configvars = virtualenvwrapper.configvars:post_deactivate_source',
+        ],
+        'console_scripts':[
+            'eva=eva.cli:eva'
+        ]
+    },
+
+)
