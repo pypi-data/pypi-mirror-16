a275a8b13bfd54241134b864c3d2694b9fcc9a76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:18:25 +0000

    Add package xonsh-autoxsh/0.2/xonsh-autoxsh-0.2.tar.gz

---
+++ packages/xonsh-autoxsh/xonsh-autoxsh-0.2.tar.gz/xonsh-autoxsh-0.2/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+
+setup(
+    name='xonsh-autoxsh',
+    version='0.2',
+    url='https://github.com/Granitas/xonsh-autoxsh',
+    license='GPLv3',
+    author='Bernardas Ališauskas',
+    author_email='bernardas.alisauskas@gmail.com',
+    description="Auto launcher of `.autoxsh` scripts for Xonsh shell's `cd` function",
+    packages=['xontrib'],
+    package_dir={'xontrib': 'xontrib'},
+    package_data={'xontrib': ['*.xsh']},
+    platforms='any',
+    classifiers=[
+        'Environment :: Console',
+        'Intended Audience :: End Users/Desktop',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Desktop Environment',
+        'Topic :: System :: Shells',
+        'Topic :: System :: System Shells',
+]
+)
