ae2c3cceb27c97629549859d0b4580fcf0196929
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:17:40 +0000

    Add package rasdapy/0.0.4/rasdapy-0.0.4.tar.gz

---
+++ packages/rasdapy/rasdapy-0.0.4.tar.gz/rasdapy-0.0.4/tests/test_crud.py
@@ -0,0 +1,98 @@
+import unittest
+import lib.ras as ras
+
+
+class TestDemoCollectionGreySet(unittest.TestCase):
+    def setUp(self):
+        self.hostname = "localhost"
+        self.username = "rasadmin"
+        self.password = "rasadmin"
+        self.collName = "RasdaPyGrey"
+        self.con = ras.Connection(hostname=self.hostname, username=self.username, password=self.password)
+        self.db = self.con.database("RASBASE")
+
+    def test_a_create_collection(self):
+        txn = self.db.transaction(rw=True)
+        q = txn.query("create collection " + self.collName + " GreySet")
+        res = q.eval()
+        txn.commit()
+        self.assertEqual(res, None)
+
+    def test_b_insert_in_collection(self):
+        txn = self.db.transaction(rw=True)
+        q = txn.query("insert into " + self.collName + " values marray x in [0:2, 0:2] values 1c")
+        res = q.eval()
+        txn.commit()
+        self.assertEqual(res, None)
+
+    def test_c_read_from_collection(self):
+        txn = self.db.transaction(rw=False)
+        q = txn.query("select " + self.collName + "[0:2,0:2] from " + self.collName)
+        data = q.eval()
+        txn.abort()
+        self.assertIsInstance(data, ras.Array)
+
+    def test_d_read_scalar_from_collection(self):
+        txn = self.db.transaction(rw=False)
+        q = txn.query("select avg_cells(" + self.collName + ") from " + self.collName)
+        data = q.eval()
+        txn.abort()
+        self.assertIsInstance(data, list)
+        self.assertEqual(data, [1.0])
+
+    def test_e_drop_collection(self):
+        txn = self.db.transaction(rw=True)
+        q = txn.query("drop collection " + self.collName)
+        res = q.eval()
+        txn.commit()
+        self.assertEqual(res, None)
+
+
+class TestDemoCollectionRGBSet(unittest.TestCase):
+    def setUp(self):
+        self.hostname = "localhost"
+        self.username = "rasadmin"
+        self.password = "rasadmin"
+        self.collection_name = "RasdaPyRGBTest"
+        self.con = ras.Connection(hostname=self.hostname, username=self.username, password=self.password)
+        self.db = self.con.database("RASBASE")
+
+    def test_a_create_collection(self):
+        txn = self.db.transaction(rw=True)
+        q = txn.query("create collection " + self.collection_name + " RGBSet")
+        res = q.eval()
+        txn.commit()
+        self.assertEqual(res, None)
+
+    def test_b_insert_in_collection(self):
+        txn = self.db.transaction(rw=True)
+        q = txn.query("insert into " + self.collection_name + " values marray x in [0:2, 0:2] values {1c, 2c, 3c}")
+        res = q.eval()
+        txn.commit()
+        self.assertEqual(res, None)
+
+    def test_c_read_from_collection(self):
+        txn = self.db.transaction(rw=False)
+        q = txn.query("select " + self.collection_name + "[0:2,0:2] from " + self.collection_name)
+        data = q.eval()
+        txn.abort()
+        self.assertIsInstance(data, ras.Array)
+
+    def test_d_read_scalar_from_collection(self):
+        txn = self.db.transaction(rw=False)
+        q = txn.query("select avg_cells(" + self.collection_name + ") from " + self.collection_name)
+        data = q.eval()
+        txn.abort()
+        self.assertIsInstance(data, list)
+        self.assertEqual(data, [[1.0, 2.0, 3.0]])
+
+    def test_e_drop_collection(self):
+        txn = self.db.transaction(rw=True)
+        q = txn.query("drop collection " + self.collection_name)
+        res = q.eval()
+        txn.commit()
+        self.assertEqual(res, None)
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ packages/rasdapy/rasdapy-0.0.4.tar.gz/rasdapy-0.0.4/tests/test_lib.py
@@ -0,0 +1,122 @@
+"""
+ *
+ * This file is part of rasdaman community.
+ *
+ * Rasdaman community is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Rasdaman community is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU  General Public License for more details.
+ *
+ * You should have received a copy of the GNU  General Public License
+ * along with rasdaman community.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Copyright 2003 - 2016 Peter Baumann / rasdaman GmbH.
+ *
+ * For more information please see <http://www.rasdaman.org>
+ * or contact Peter Baumann via <baumann@rasdaman.com>.
+ *
+"""
+from lib import utils
+from lib.rasda import ExpNode, RasCollection
+import unittest
+
+
+class TestUtils(unittest.TestCase):
+    def test_md5_hash(self):
+        pass_array = ["rasguest", "dJ4ng0Pi5", "m0n4L154", "514cK8oT"]
+        hash_array = ["8e70a429be359b6dace8b5b2500dedb0", "a4187b53523c7a13a5dcd3354418f7c7",
+                      "a69d024049be4503e206d4f39b54ea2e", "1cecd6d9eebf70df02411d50e45c178f"]
+        for index, password in enumerate(pass_array):
+            password_hash = utils.get_md5_string(password)
+            self.assertEquals(password_hash, hash_array[index])
+
+    def test_type_structure_char(self):
+        inp_str = "set <marray <char, [100:140, 40:80]>>"
+        self.assertEqual(utils.get_type_structure_from_string(inp_str), {"base_type": "marray", "type": "char"})
+
+    def test_type_structure_struct(self):
+        inp_str = "set <marray <struct { short foo, char bar }, [100:150, 50:80]>>"
+        self.assertEqual(utils.get_type_structure_from_string(inp_str),
+                         {"base_type": "marray", "type": "struct",
+                          "sub_type": {"names": ["foo", "bar"], "types": ["short", "char"]}})
+
+    def test_convert_data_from_bin(self):
+        test_inp = [{"dtype": "char", "data": "\x16"}]
+        test_out = [21]
+        if len(test_inp) == len(test_out):
+            for i in xrange(0, len(test_inp) - 1):
+                self.assertEqual(utils.convert_data_from_bin(test_inp[i]["dtype"], test_inp[i]["data"]), test_out[i])
+
+
+class TestExpNode(unittest.TestCase):
+    def setUp(self):
+        self.value = "+"
+        self.lchild = "mr"
+        self.rchild = "5"
+
+    def test_exp_node_creation(self):
+        node = ExpNode(value=self.value, lchild=self.lchild, rchild=self.rchild)
+        self.assertEqual(node.value, self.value)
+        self.assertEqual(node.lchild, self.lchild)
+        self.assertEqual(node.rchild, self.rchild)
+
+
+class TestQueryConstruction(unittest.TestCase):
+    def setUp(self):
+        self.query_simple = "select mr from mr"
+        self.query_avg_with_subsetting = "select avg_cells(mr[1:10]) from mr"
+        self.query_subsetting_with_condition = "select mr[1:10] from mr where oid=2"
+        self.query_arithmetic_operations = "select pow(((((mr+9)/2)-3)*5),2) from mr"
+        self.query_trigonometric_operations = "select tan(cos(sin(mr))) from mr"
+
+    def test_query_simple(self):
+        col = RasCollection("mr")
+        self.assertEqual(self.query_simple, str(col.query))
+
+    def test_query_avg_with_subsetting(self):
+        col = RasCollection("mr")
+        col = col[1:10]
+        col = col.avg_cells()
+        self.assertEqual(self.query_avg_with_subsetting, str(col.query))
+
+    def test_query_subsetting_with_condition(self):
+        col = RasCollection("mr")
+        col = col[1:10]
+        col.filter(oid=2)
+        self.assertEqual(self.query_subsetting_with_condition, str(col.query))
+
+    def test_query_arithmetic_operations(self):
+        col = RasCollection("mr")
+        col += 9
+        col /= 2
+        col -= 3
+        col *= 5
+        col **= 2
+        self.assertEqual(self.query_arithmetic_operations, str(col.query))
+
+    def test_query_trigonometric_operations(self):
+        col = RasCollection("mr")
+        col = col.sin()
+        col = col.cos()
+        col = col.tan()
+        self.assertEqual(self.query_trigonometric_operations, str(col.query))
+
+    def test_query_equal(self):
+        col_1 = RasCollection("mr")
+        col_1 += 9
+        col_1 = col_1[1:10]
+        col_1 **= 2
+        col_2 = RasCollection("mr")
+        col_2 += 9
+        col_2 = col_2[1:10]
+        col_2 **= 2
+        self.assertTrue(col_1 == col_2)
+
+
+if __name__ == "__main__":
+    unittest.main()
