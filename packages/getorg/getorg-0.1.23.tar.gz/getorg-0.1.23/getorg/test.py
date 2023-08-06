c76d3f80ab7656221ea55433651ee4ca75a39f73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:08 +0000

    Add package getorg/0.1.23/getorg-0.1.23.tar.gz

---
+++ packages/getorg/getorg-0.1.23.tar.gz/getorg-0.1.23/getorg/test.py
@@ -0,0 +1,26 @@
+def test_map_org():
+    import getorg
+    from github import Github
+    import ghlogin
+    gh = Github(login_or_token=ghlogin.gh_key)
+
+    
+    map_obj, loc_dict, metadata_dict = getorg.orgmap.map_org(gh,"getorg-test")
+
+    assert loc_dict['https://api.github.com/users/getorg-testacct'].longitude == 0.0
+    assert round(loc_dict['https://api.github.com/users/staeiou'].longitude,2) == -122.27
+    assert metadata_dict == {'duplicate_count': 0,  'error_count': 0,  'no_loc_count': 0,  'user_loc_count': 2}
+
+
+def test_map_orgs():
+    import getorg
+    from github import Github
+    import ghlogin
+    gh = Github(login_or_token=ghlogin.gh_key)
+
+    map_obj, loc_dict, metadata_dict = getorg.orgmap.map_orgs(gh,['trace-ethnography', 'getorg-test', 'getorg'])
+    
+    assert loc_dict['https://api.github.com/users/getorg-testacct'].longitude == 0.0
+    assert round(loc_dict['https://api.github.com/users/staeiou'].longitude,2) == -122.27
+    assert len(loc_dict) == 2
+    assert metadata_dict == {'error_count': 0, 'user_loc_count': 5, 'no_loc_count': 1, 'duplicate_count': 1}
