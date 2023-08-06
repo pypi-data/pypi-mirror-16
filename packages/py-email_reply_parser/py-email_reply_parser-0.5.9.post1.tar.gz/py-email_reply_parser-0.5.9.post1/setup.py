d8ab0f1b25257d5d69708a82fc79888876f46e92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:36:24 +0000

    Add package py-email_reply_parser/0.5.9.post1/py-email_reply_parser-0.5.9.post1.tar.gz

---
+++ packages/py-email_reply_parser/py-email_reply_parser-0.5.9.post1.tar.gz/py-email_reply_parser-0.5.9.post1/setup.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+version = {}
+with open('email_reply_parser/version.py', 'r') as f:
+    exec(f.read(), {}, version)
+
+setup(
+    name='py-email_reply_parser',
+    version=version['VERSION'],
+    description='Email reply parser',
+    packages=['email_reply_parser'],
+    package_data={'email_reply_parser': ['../VERSION']},
+    author='Franklyn Tackitt',
+    author_email='frank@comanage.com',
+    url='https://github.com/DisruptiveLabs/email-reply-parser',
+    license='MIT',
+    test_suite='test',
+    classifiers=[
+        'Topic :: Software Development',
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+    ]
+)
