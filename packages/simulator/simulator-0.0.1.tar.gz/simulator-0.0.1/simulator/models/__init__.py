0713a1e1ede26de017d84abf665b150d3138d483
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:46 +0000

    Add package simulator/0.0.1/simulator-0.0.1.tar.gz

---
+++ packages/simulator/simulator-0.0.1.tar.gz/simulator-0.0.1/simulator/models/__init__.py
@@ -0,0 +1,16 @@
+# coding: utf-8
+# !/usr/bin/python
+
+"""
+Project: simulator
+Thu Jul 14 00:01:33 2016
+"""
+
+import TResistor as TRsistor
+import Model as Model
+import Resistor as Resistor
+import ResistorInSeries as ResistorInSeries
+
+# Author
+__author__ = 'Jason Xing Zhang'
+__email__ = 'xingz@uvic.ca'
