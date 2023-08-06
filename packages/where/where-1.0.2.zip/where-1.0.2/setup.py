f2d875ad0d2f60782efb28dc49c1dc7a88be25a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:32 +0000

    Add package where/1.0.2/where-1.0.2.zip

---
+++ packages/where/where-1.0.2.zip/where-1.0.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+
+description = r"""Locates absolute file paths like the Windows 'where' or the Linux 'which' utility.
+Makes use of the PATH variable and the current directory."""
+
+setuptools.setup(
+    name="where",
+    packages = ["where"],
+    version="1.0.2",
+    description=description,
+    author = 'Henry Weickert',
+    author_email = 'henryweickert@gmail.com',
+    url = 'https://github.com/hweickert/where',
+    keywords = ['where', 'which'],
+    install_requires=["itermate==1.0.2"],
+    entry_points={
+        'console_scripts': [
+            'where = where.__main__:main',
+        ]
+    }
+)
