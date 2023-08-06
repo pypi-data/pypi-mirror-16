e6c015c9c5514a36a6b1c0533b7f0c6219ff1f56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:14:24 +0000

    Add package mh_nester/1.3.0/mh_nester-1.3.0.zip

---
+++ packages/mh_nester/mh_nester-1.3.0.zip/mh_nester-1.3.0/mh_nester.py
@@ -0,0 +1,20 @@
+"""这是一个"nester.py"模块，提供了一个命名为print_lol()的函数，函数的作用是打印列表内容，
+列表中可能包含嵌套列表"""
+    
+def print_lol(the_list,indent=False,level=0): 
+    
+    """这个函数取一个位置参数，命名为the_list，可以为任意一个python列表或嵌套列表，指定列表中的每一个数据项会递归出现在屏幕上，每个数据各占一行。"""
+    """num 参数是用来遇到嵌套列表时输出制表符"""
+    """indent用于表示缩进的判断，默认为false，若需要缩进，则设定为true"""
+    
+    for each_item in the_list:
+        if isinstance(each_item,list):
+            print_lol(each_item,indent,level+1)
+        else:
+            if indent:
+                for tab_stop in range(level):
+                    print('\t',end='')
+                """表示输出的字符为制表符，python默认为简单换行"""
+            print(each_item)
+
+            
---
+++ packages/mh_nester/mh_nester-1.3.0.zip/mh_nester-1.3.0/mh_setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='mh_nester',
+    version='1.3.0',
+    py_modules=['mh_nester'],
+    author='MonicaHuang',
+    author_email='xmii37@163.com',
+    url='',
+    description='a simple printer of nested lists',
+    )
