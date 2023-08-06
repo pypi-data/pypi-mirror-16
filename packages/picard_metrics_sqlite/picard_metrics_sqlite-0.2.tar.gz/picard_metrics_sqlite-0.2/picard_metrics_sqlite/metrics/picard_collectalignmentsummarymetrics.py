96fffcdf9b32c57cde27deeffa2ed7474b0ebf4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:29 +0000

    Add package picard_metrics_sqlite/0.2/picard_metrics_sqlite-0.2.tar.gz

---
+++ packages/picard_metrics_sqlite/picard_metrics_sqlite-0.2.tar.gz/picard_metrics_sqlite-0.2/picard_metrics_sqlite/metrics/picard_collectalignmentsummarymetrics.py
@@ -0,0 +1,23 @@
+import os
+
+import metrics_util
+
+def picard_CollectAlignmentSummaryMetrics_to_df(stats_path, logger):
+    select = 'CATEGORY'
+    df = metrics_util.picard_select_tsv_to_df(stats_path, select, logger)
+    return df
+
+
+def run(uuid, stats_path, bam, fasta, input_state, engine, logger, metric_name):
+    table_name = 'picard_' + metric_name
+    df = picard_CollectAlignmentSummaryMetrics_to_df(stats_path, logger)
+    if df is None:
+        sys.exit('No metrics collected from: %s' % stats_path)
+    else:
+        df['uuid'] = uuid
+        df['bam'] = bam
+        df['input_state'] = input_state
+        df['fasta'] = fasta
+        table_name = table_name_list[i]
+        df.to_sql(table_name, engine, if_exists='append')
+    return
