48db4ead2df41a723c4abca8f707bd2266369514
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:19:01 +0000

    Add package incuna-surveys/0.0.3/incuna_surveys-0.0.3-py2.py3-none-any.whl

---
+++ packages/incuna-surveys/incuna_surveys-0.0.3-py2.py3-none-any.whl/surveys/views_api.py
@@ -0,0 +1,40 @@
+from rest_framework import generics
+from rest_framework.generics import get_object_or_404
+from rest_framework.response import Response
+
+from .models import Survey, UserResponse
+from .serializers import SurveyResponseSerializer, SurveySerializer
+
+
+class SurveyView(generics.RetrieveAPIView):
+    """Display a survey to the user."""
+    queryset = Survey.objects.all()
+    serializer_class = SurveySerializer
+
+
+class SurveyPostView(generics.CreateAPIView):
+    serializer_class = SurveyResponseSerializer
+
+    def dispatch(self, request, *args, **kwargs):
+        survey_pk = kwargs.pop('pk')
+        self.survey = get_object_or_404(Survey.objects.all(), pk=survey_pk)
+        return super().dispatch(request, *args, **kwargs)
+
+    def get_serializer(self, *args, **kwargs):
+        kwargs['data']['survey'] = self.survey.pk
+        serializer = super().get_serializer(*args, **kwargs)
+        return serializer
+
+
+class SurveyLatestView(generics.ListAPIView):
+    queryset = UserResponse.objects.all()
+
+    def dispatch(self, request, *args, **kwargs):
+        survey_pk = kwargs.pop('pk')
+        self.user_id = kwargs.pop('user_id')
+        self.survey = get_object_or_404(Survey.objects.all(), pk=survey_pk)
+        return super().dispatch(request, *args, **kwargs)
+
+    def list(self, request, *args, **kwargs):
+        data = self.get_queryset().latest_for_user(self.survey, self.user_id)
+        return Response(data)
