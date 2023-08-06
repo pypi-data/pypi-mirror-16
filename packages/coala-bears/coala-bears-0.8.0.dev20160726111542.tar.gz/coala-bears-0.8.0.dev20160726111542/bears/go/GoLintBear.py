8a704606494efc62b570b80a25467b0b20b4eda1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:56 +0000

    Add package coala-bears/0.8.0.dev20160726111542/coala-bears-0.8.0.dev20160726111542.tar.gz

---
+++ packages/coala-bears/coala-bears-0.8.0.dev20160726111542.tar.gz/coala-bears-0.8.0.dev20160726111542/bears/go/GoLintBear.py
@@ -0,0 +1,29 @@
+import shlex
+
+from coalib.bearlib.abstractions.Linter import linter
+
+
+@linter(executable='golint',
+        output_format='regex',
+        output_regex=r'.+:(?P<line>\d+):(?P<column>\d+): (?P<message>.*)')
+class GoLintBear:
+    """
+    Checks the code using ``golint``. This will run golint over each file
+    separately.
+    """
+    LANGUAGES = {"Go"}
+    AUTHORS = {'The coala developers'}
+    AUTHORS_EMAILS = {'coala-devel@googlegroups.com'}
+    LICENSE = 'AGPL-3.0'
+    CAN_DETECT = {'Formatting'}
+
+    @staticmethod
+    def create_arguments(filename, file, config_file,
+                         golint_cli_options: str=''):
+        """
+        :param golint_cli_options: Any other flags you wish to pass to golint.
+        """
+        args = ()
+        if golint_cli_options:
+            args += tuple(shlex.split(golint_cli_options))
+        return args + (filename,)
