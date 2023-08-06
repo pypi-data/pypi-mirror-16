5cf81e6e5a73358f5009f157a0168f21d82a8ee8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:09 +0000

    Add package fbmq/1.0.5/fbmq-1.0.5.tar.gz

---
+++ packages/fbmq/fbmq-1.0.5.tar.gz/fbmq-1.0.5/setup.py
@@ -0,0 +1,39 @@
+from distutils.core import setup
+
+
+long_description = ''
+
+try:
+    import subprocess
+    import pandoc
+
+    process = subprocess.Popen(
+        ['which pandoc'],
+        shell=True,
+        stdout=subprocess.PIPE,
+        universal_newlines=True
+    )
+
+    pandoc_path = process.communicate()[0]
+    pandoc_path = pandoc_path.strip('\n')
+
+    pandoc.core.PANDOC_PATH = pandoc_path
+
+    doc = pandoc.Document()
+    doc.markdown = open('README.md').read()
+
+    long_description = doc.rst
+except:
+    pass
+
+setup(name='fbmq',
+      version='1.0.5',
+      description='A Python Library For Using The Facebook Messenger Platform API',
+      long_description=long_description,
+      url='https://github.com/conbus/fbmq',
+      author='kimwz',
+      author_email='kimwz.kr@gmail.com',
+      packages=['fbmq'],
+      py_modules=['fbmq'],
+      install_requires=['requests>=2.0'],
+)
