f19547fbdfc4c5cf27aab8739e4ca087e4525eca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:06 +0000

    Add package PyPIGO/0.0.1/PyPIGO-0.0.1.tar.gz

---
+++ packages/PyPIGO/PyPIGO-0.0.1.tar.gz/PyPIGO-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='PyPIGO',
+      version='0.0.1',
+      description='pypi test.',
+      author='wak',
+      author_email='j@',
+      url='http://webapp-jj-01.azurewebsites.net',
+      packages=['PyPIGO'],
+     )
