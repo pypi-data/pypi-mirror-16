63e2d02ec8652893b14de147ba471df34325dcdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:29 +0000

    Add package web3/1.5.0/web3-1.5.0.tar.gz

---
+++ packages/web3/web3-1.5.0.tar.gz/web3-1.5.0/web3/utils/config.py
@@ -0,0 +1,33 @@
+ETH_PADDING = 32
+ETH_SIGNATURE_LENGTH = 4
+ETH_POLLING_TIMEOUT = 500
+
+ETH_UNITS = [
+    'wei',
+    'kwei',
+    'Mwei',
+    'Gwei',
+    'szabo',
+    'finney',
+    'femtoether',
+    'picoether',
+    'nanoether',
+    'microether',
+    'milliether',
+    'nano',
+    'micro',
+    'milli',
+    'ether',
+    'grand',
+    'Mether',
+    'Gether',
+    'Tether',
+    'Pether',
+    'Eether',
+    'Zether',
+    'Yether',
+    'Nether',
+    'Dether',
+    'Vether',
+    'Uether'
+]
