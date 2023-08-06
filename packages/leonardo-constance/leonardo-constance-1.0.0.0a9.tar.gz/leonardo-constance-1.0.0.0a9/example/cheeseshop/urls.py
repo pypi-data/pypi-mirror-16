e564a309c5c4a86fbd2c89e4420137dfbbe79b60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:34 +0000

    Add package leonardo-constance/1.0.0.0a9/leonardo-constance-1.0.0.0a9.tar.gz

---
+++ packages/leonardo-constance/leonardo-constance-1.0.0.0a9.tar.gz/leonardo-constance-1.0.0.0a9/example/cheeseshop/urls.py
@@ -0,0 +1,22 @@
+from django.conf.urls import *
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Example:
+    # (r'^cheeseshop/', include('cheeseshop.foo.urls')),
+
+    # Uncomment the admin/doc line below and add 'django.contrib.admindocs'
+    # to INSTALLED_APPS to enable admin documentation:
+    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    (r'^admin/', include(admin.site.urls)),
+)
+
+if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns()
