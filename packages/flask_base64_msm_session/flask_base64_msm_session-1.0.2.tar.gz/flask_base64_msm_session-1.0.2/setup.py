7021750c3c8e0ce8bf47465d9c510e53c83d40a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:50 +0000

    Add package flask_base64_msm_session/1.0.2/flask_base64_msm_session-1.0.2.tar.gz

---
+++ packages/flask_base64_msm_session/flask_base64_msm_session-1.0.2.tar.gz/flask_base64_msm_session-1.0.2/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+try:    
+    from setuptools import setup, find_packages
+except:    
+    from distutils.core import setup, find_packages
+ 
+setup(name='flask_base64_msm_session',
+      version='1.0.2',
+      description='Use base64 encoder on memcached server. And it will use memcached on session',
+      author='Sparrow Jang',
+      author_email='sparrow.jang@gmail.com',
+      url='https://github.com/eHanlin/flask-base64-msm-session',
+      packages=find_packages(),
+      install_requires=['pylibmc', 'msm-transcoder', 'msm_api']
+     )   
+
