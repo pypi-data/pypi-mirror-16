17e94ee240f563a95fc40b26552145e79af1f436
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:16:33 +0000

    Add package ha-ffmpeg/0.6/ha-ffmpeg-0.6.zip

---
+++ packages/ha-ffmpeg/ha-ffmpeg-0.6.zip/ha-ffmpeg-0.6/haffmpeg/core.py
@@ -0,0 +1,215 @@
+"""Base functionality of ffmpeg HA wrapper."""
+import logging
+import queue
+import re
+import shlex
+import subprocess
+import threading
+
+_LOGGER = logging.getLogger(__name__)
+
+HAFFMPEG_QUEUE_END = '!HAFFMPEG_QUEUE_END!'
+
+
+class HAFFmpeg(object):
+    """Base HA FFmpeg process.
+
+    Object is iterable but only for data streams! For other things use the
+    process property to call from Popen object.
+    """
+
+    def __init__(self, ffmpeg_bin, chunk_size=1024):
+        """Base initialize."""
+        self._ffmpeg = ffmpeg_bin
+        self._argv = [ffmpeg_bin]
+        self._chunk_size = chunk_size
+        self._bin_mode = None
+        self._proc = None
+
+    # pylint: disable=too-many-arguments
+    def open(self, cmd, output="-", extra_cmd=None, text=False,
+             stdout_pipe=True, stderr_pipe=False):
+        """Start a ffmpeg instance and pipe output."""
+        stdout = subprocess.PIPE if stdout_pipe else subprocess.DEVNULL
+        stderr = subprocess.PIPE if stderr_pipe else subprocess.DEVNULL
+
+        if self.is_running:
+            _LOGGER.critical("FFmpeg is allready running!")
+            return
+
+        # add cmds
+        self._argv.extend(cmd)
+
+        # exists a extra cmd from customer
+        if extra_cmd is not None:
+            self._argv.extend(shlex.split(extra_cmd))
+
+        # cleanup filters
+        for opts in (['-filter:a', '-af'], ['-filter:v', '-vf']):
+            filter_list = []
+            new_argv = []
+            cmd_iter = iter(self._argv)
+            for element in cmd_iter:
+                if element in opts:
+                    filter_list.insert(0, next(cmd_iter))
+                else:
+                    new_argv.append(element)
+
+            # update argv if changes
+            if filter_list:
+                new_argv.extend([opts[0], ",".join(filter_list)])
+                self._argv = new_argv.copy()
+
+        # add output
+        if output is None:
+            self._argv.extend(['-f', 'null', '-'])
+        else:
+            self._argv.append(output)
+
+        # start ffmpeg
+        _LOGGER.debug("Start FFmpeg with %s.", str(self._argv))
+        self._proc = subprocess.Popen(
+            self._argv,
+            stderr=stderr,
+            stdout=stdout,
+            stdin=subprocess.PIPE,
+            universal_newlines=text
+        )
+
+        # save bin/text mode of process
+        self._bin_mode = False if text else True
+
+    def close(self, timeout=5):
+        """Stop a ffmpeg instance."""
+        if not self.is_running:
+            _LOGGER.error("FFmpeg isn't running!")
+            return
+
+        # set stop command for ffmpeg
+        stop = b'q' if self._bin_mode else 'q'
+
+        try:
+            # send stop to ffmpeg
+            self._proc.communicate(input=stop, timeout=timeout)
+            _LOGGER.debug("Close FFmpeg process.")
+        except subprocess.TimeoutExpired:
+            _LOGGER.warning("Timeout while waiting of FFmpeg.")
+            self._proc.kill()
+            self._proc.wait()
+
+        # clean ffmpeg cmd
+        self._argv = [self._ffmpeg]
+        self._proc = None
+        self._bin_mode = None
+
+    @property
+    def process(self):
+        """Return a Popen object or None of not running."""
+        return self._proc
+
+    @property
+    def is_running(self):
+        """Return True if ffmpeg is running."""
+        if self._proc is None or self._proc.poll() is not None:
+            return False
+        return True
+
+    def __iter__(self):
+        """Read data from ffmpeg PIPE/STDERR as iter."""
+        return self
+
+    def __next__(self):
+        """Get next buffer data."""
+        if not self.is_running:
+            _LOGGER.debug("don't exists data from a process.")
+            raise StopIteration
+
+        # check if reading from pipe
+        if self._proc.stdout is None:
+            _LOGGER.critical("Iterator havn't data to  read from!")
+            raise StopIteration
+
+        return self._proc.stdout.read(self._chunk_size)
+
+
+class HAFFmpegQue(HAFFmpeg):
+    """Read FFmpeg STDERR output to QUE."""
+
+    def __init__(self, ffmpeg_bin, chunk_size=1024):
+        """Base initialize."""
+        super().__init__(ffmpeg_bin, chunk_size=chunk_size)
+
+        self._que = queue.Queue()
+        self._que_thread = None
+
+    def _read_lines_to_que(self, pattern=None):
+        """Read line from STDERR to Que they match with pattern / thread."""
+        if pattern is not None:
+            cmp = re.compile(pattern)
+
+        # read lines
+        while self.is_running:
+            try:
+                line = self._proc.stderr.readline()
+            except Exception:
+                break
+
+            match = True if pattern is None else cmp.search(line)
+            if match:
+                try:
+                    _LOGGER.debug("Put do que: %s", line)
+                    self._que.put(line, block=False)
+                except queue.Full:
+                    _LOGGER.warning("Queue is full...")
+
+        # send end to reader of queue
+        self._que.put(HAFFMPEG_QUEUE_END)
+
+    def start_reading_que(self, pattern=None):
+        """Read line from STDERR to Que they match with pattern."""
+        if self._que_thread is not None:
+            _LOGGER.critical("Thread is allready running now!")
+            return False
+        elif self._bin_mode:
+            _LOGGER.critical("ReadingQue not support ob Binmode!")
+            return False
+
+        self._que_thread = threading.Thread(
+            target=self._read_lines_to_que,
+            kwargs={'pattern': pattern}
+        )
+
+        self._que_thread.start()
+        _LOGGER.debug("Start thread. Pattern: %s", pattern)
+        return True
+
+
+class HAFFmpegWorker(HAFFmpegQue):
+    """Process Que data into a thread."""
+
+    def __init__(self, ffmpeg_bin, chunk_size=1024):
+        """Base initialize."""
+        super().__init__(ffmpeg_bin, chunk_size)
+
+        self._worker_thread = None
+
+    def start_worker(self, cmd, output=None, extra_cmd=None, pattern=None):
+        """Start ffmpeg process with que and process data."""
+        if self._worker_thread is not None and self._worker_thread.is_alive():
+            _LOGGER.warning("Can't start worker. It is allready running!")
+            return
+
+        # start ffmpeg and reading to queue
+        self.open(cmd=cmd, output=output, extra_cmd=extra_cmd,
+                  stdout_pipe=False, stderr_pipe=True, text=True)
+        if not self.start_reading_que(pattern=pattern):
+            _LOGGER.warning("Can't start worker if queue is not running!")
+            return
+
+        self._worker_thread = threading.Thread(target=self._worker_process)
+        self._worker_thread.start()
+        _LOGGER.debug("Start working thread.")
+
+    def _worker_process(self):
+        """Function run in thread for process que data."""
+        raise NotImplementedError
---
+++ packages/ha-ffmpeg/ha-ffmpeg-0.6.zip/ha-ffmpeg-0.6/haffmpeg/sensor.py
@@ -0,0 +1,100 @@
+"""For HA camera components."""
+import logging
+import queue
+import re
+
+from .core import HAFFmpegWorker, HAFFMPEG_QUEUE_END
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class SensorNoise(HAFFmpegWorker):
+    """Implement a noise detection on a autio stream."""
+
+    STATE_NONE = 0
+    STATE_NOISE = 1
+    STATE_END = 2
+    STATE_DETECT = 3
+
+    def __init__(self, ffmpeg_bin, callback):
+        """Init noise sensor."""
+        super().__init__(ffmpeg_bin)
+
+        self._callback = callback
+        self._peak = -30
+        self._time_duration = 1
+        self._time_reset = 2
+
+    def set_options(self, time_duration=1, time_reset=2, peak=-30):
+        """Set option parameter for noise sensor."""
+        self._time_duration = time_duration
+        self._time_reset = time_reset
+        self._peak = peak
+
+    def open_sensor(self, input_source, output_dest=None, extra_cmd=None):
+        """Open FFmpeg process as mjpeg video stream."""
+        command = [
+            "-i",
+            input_source,
+            "-vn",
+            "-filter:a",
+            "silencedetect=n={}dB:d=1".format(self._peak)
+        ]
+
+        # run ffmpeg, read output
+        self.start_worker(cmd=command, output=output_dest, extra_cmd=extra_cmd,
+                          pattern="silence")
+
+    def _worker_process(self):
+        """This function run in thread for process que data."""
+        state = self.STATE_NONE
+        timeout = None
+
+        re_start = re.compile("silence_start")
+        re_end = re.compile("silence_end")
+
+        # process queue data
+        while True:
+            try:
+                _LOGGER.debug("Reading State: %d, timeout: %s", state, timeout)
+                data = self._que.get(block=True, timeout=timeout)
+                timeout = None
+                if data == HAFFMPEG_QUEUE_END:
+                    return
+            except queue.Empty:
+                _LOGGER.debug("Blocking timeout")
+                # noise
+                if state == self.STATE_DETECT:
+                    # noise detected
+                    self._callback(True)
+                    state = self.STATE_NOISE
+
+                elif state == self.STATE_END:
+                    # no noise
+                    self._callback(False)
+                    state = self.STATE_NONE
+
+                timeout = None
+                continue
+
+            if re_start.search(data):
+                if state == self.STATE_NOISE:
+                    # stop noise detection
+                    state = self.STATE_END
+                    timeout = self._time_reset
+                elif state == self.STATE_DETECT:
+                    # reset if only a peak
+                    state = self.STATE_NONE
+                continue
+
+            if re_end.search(data):
+                if state == self.STATE_NONE:
+                    # detect noise begin
+                    state = self.STATE_DETECT
+                    timeout = self._time_duration
+                elif state == self.STATE_END:
+                    # back to noise status
+                    state = self.STATE_NOISE
+                continue
+
+        _LOGGER.warning("Unknown data from queue!")
