71509d7c36ff402d6800df38ceec73c3d9a1c742
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:18 +0000

    Add package ggplot/0.10.8/ggplot-0.10.8.tar.gz

---
+++ packages/ggplot/ggplot-0.10.8.tar.gz/ggplot-0.10.8/ggplot/geoms/geom_now_its_art.py
@@ -0,0 +1,29 @@
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+
+import matplotlib.image as mpimg
+import os
+import sys
+
+from .geom import geom
+
+_ROOT = os.path.abspath(os.path.dirname(__file__))
+
+
+class geom_now_its_art(geom):
+    """
+    Put a bird on it!
+
+    alpha:
+        transparency of bird
+
+    Examples
+    --------
+    """
+    DEFAULT_AES = {'alpha': 0.5}
+
+    def plot(self, ax, data, _aes):
+        params = self._get_plot_args(data, _aes)
+        img = mpimg.imread(os.path.join(_ROOT, 'bird.png'))
+        ax.imshow(img, **params)
+        sys.stderr.write("Put a bird on it!\n")
---
+++ packages/ggplot/ggplot-0.10.8.tar.gz/ggplot-0.10.8/ggplot/geoms/geom_path.py
@@ -0,0 +1,24 @@
+from .geom_line import geom_line
+
+
+class geom_path(geom_line):
+    """
+    Sequence of connected (x, y) coordinates
+
+    x:
+        x values for (x, y) coordinates
+    y:
+        y values for (x, y) coordinates
+    color:
+        color of line
+    alpha:
+        transparency of color
+    linetype:
+        type of the line ('solid', 'dashed', 'dashdot', 'dotted')
+    size:
+        thickness of line
+
+    Examples
+    --------
+    """
+    is_path = True
