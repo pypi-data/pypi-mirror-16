45334f0e77cd7e8ef219038b9216f134f918027c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:33 +0000

    Add package vivo-rdflib-sparqlstore/0.1/vivo-rdflib-sparqlstore-0.1.tar.gz

---
+++ packages/vivo-rdflib-sparqlstore/vivo-rdflib-sparqlstore-0.1.tar.gz/vivo-rdflib-sparqlstore-0.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+description = 'RDFLib SPARQLStore and SPARQLUpdateStore implementation for VIVO.'
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(
+    name = 'vivo-rdflib-sparqlstore',
+    version = '0.1',
+    url = 'https://github.com/lawlesst/rdflib-vivo-sparqlstore',
+    author = 'Ted Lawless',
+    author_email = 'lawlesst@gmail.com',
+    packages=find_packages(),
+    install_requires=required,
+    include_package_data=True,
+    description = description,
+    entry_points='''
+        [console_scripts]
+        vivoUpdate=scripts.command:process
+    '''
+)
+
