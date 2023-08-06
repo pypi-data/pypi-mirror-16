4c710ca2e8df0dc1f6f9688837a981594f4dda42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:03:16 +0000

    Add package comiccrawler/2016.8.19/comiccrawler-2016.8.19.zip

---
+++ packages/comiccrawler/comiccrawler-2016.8.19.zip/comiccrawler-2016.8.19/comiccrawler/mods/tumblr.py
@@ -0,0 +1,43 @@
+#! python3
+
+"""this is konachan module for tumblr
+
+Ex:
+	http://otn-pct.tumblr.com/
+
+"""
+
+import re, json
+from urllib.parse import urljoin
+
+from ..core import Episode
+
+domain = ["tumblr.com"]
+name = "tumblr"
+noepfolder = True
+
+def get_title(html, url):
+	title = re.search(r"<title>([^<]+)", html).group(1)
+	id = url[url.index("://") + 3: url.index(".tumblr.com")]
+	return "[tumblr] {} ({})".format(title, id)
+	
+def get_episodes(html, url):
+	s = []
+	# base = re.search("(https?://[^/]+)", url).group(1)
+	pattern = 'href="(' + re.escape(urljoin(url, "/post/")) + '(\d+))'
+	for m in re.finditer(pattern, html):
+		url, uid = m.groups()
+		s.append(Episode(uid, url))
+	return s[::-1]
+			
+def get_images(html, url):
+	s = re.search('<script type="application/ld\+json">([^<]*)</script>', html).group(1)
+	o = json.loads(s)
+	if isinstance(o["image"], str):
+		return [o["image"]]
+	return o["image"]["@list"]
+
+def get_next_page(html, url):
+	match = re.search('<a href="([^"]+)" id="next">', html)
+	if match:
+		return urljoin(url, match.group(1))
