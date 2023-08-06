163fe0fb7a4dc74e04ea4f555e57b35b2a80905e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:15 +0000

    Add package Nester_revised/1.2.0/Nester_revised-1.2.0.tar.gz

---
+++ packages/Nester_revised/Nester_revised-1.2.0.tar.gz/Nester_revised-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name		= 'Nester_revised',
+	version		= '1.2.0',
+	py_modules	= ['Nester_revised'],
+	author		= 'MrWang1996',
+	author_email	= 'wyn1996@hotmail.com',
+	url		= 'http://www.headfirstlabs.com',
+	description	= 'A simple printer of nested lists',
+	)
