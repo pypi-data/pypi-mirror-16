952aff22f5d364fbdc1bc554ad6ff4d5dd9818f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:22 +0000

    Add package ci-jira-releaser/0.3.7/ci-jira-releaser-0.3.7.tar.gz

---
+++ packages/ci-jira-releaser/ci-jira-releaser-0.3.7.tar.gz/ci-jira-releaser-0.3.7/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(name='ci-jira-releaser',
+      version='0.3.7',
+      long_description=readme(),
+      description='Simple way to release past(or current) commits within Jira',
+      url='https://github.com/bookmd/jira-releaser',
+      author='Ben Waters',
+      author_email='ben@book-md.com',
+      license='MIT',
+      packages=['ci_jira_releaser'],
+      install_requires=['jira', 'gitpython', 'requests'],
+      scripts=['bin/ci-jira-releaser'],
+      zip_safe=False)
