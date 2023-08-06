e08a0ac5ca5456ae61ce3cdb3d79ae4a855b7665
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:05:45 +0000

    Add package module_name/0.5.1/module_name-0.5.1.tar.gz

---
+++ packages/module_name/module_name-0.5.1.tar.gz/module_name-0.5.1/setup.py
@@ -0,0 +1,38 @@
+from distutils.core import setup
+
+try:
+    import pypandoc
+    long_description = pypandoc.convert_file('README.md', 'rst')
+except ImportError:
+    long_description = open('README.md').read()
+
+DISTNAME='module_name'
+FULLVERSION='0.5.1'
+
+setup(
+    name = DISTNAME,
+    packages = [DISTNAME],
+    version = FULLVERSION,
+    description = 'Simple Module to resolve module namespace',
+    long_description = long_description,
+    author = 'Dale Jung',
+    author_email = 'dale@dalejung.com',
+    url = 'https://github.com/dalejung/module_name',
+    license = 'MIT',
+    download_url = 'https://github.com/dalejung/module_name/tarball/'+FULLVERSION,
+    entry_points={
+        'console_scripts': [
+            'py-module-name=module_name.cli:main',
+        ]
+    },
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
