d3f6cc2fba838d6b21afbf11b494858530101211
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:00 +0000

    Add package DeadPyDaylight/0.1/DeadPyDaylight-0.1.tar.gz

---
+++ packages/DeadPyDaylight/DeadPyDaylight-0.1.tar.gz/DeadPyDaylight-0.1/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+	name = 'DeadPyDaylight',
+	version = '0.1',
+	description = 'Script tool to auto ready-up in Dead By Daylight',
+	url = 'https://github.com/Aidid51/DeadPyDaylight',
+	author = 'Aidid51',
+	author_email = 'rooibosboss@gmail.com',
+	license = 'MIT',
+	py_modules = ['DeadPyDaylight'],
+	install_requires = [
+		'pyautogui',
+	] )
