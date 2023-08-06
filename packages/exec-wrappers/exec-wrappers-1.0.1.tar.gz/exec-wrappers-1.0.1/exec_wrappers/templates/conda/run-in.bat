b97b2bfbcc170366fb5bb690d1cea605091795f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:19 +0000

    Add package exec-wrappers/1.0.1/exec-wrappers-1.0.1.tar.gz

---
+++ packages/exec-wrappers/exec-wrappers-1.0.1.tar.gz/exec-wrappers-1.0.1/exec_wrappers/templates/conda/run-in.bat
@@ -0,0 +1,20 @@
+@echo off
+
+@REM This script was based on the original conda activate.bat
+
+@setlocal
+
+@set "CONDA_DEFAULT_ENV=__CONDA_ENV_DIR__"
+@set "PATH=%CONDA_DEFAULT_ENV%;%CONDA_DEFAULT_ENV%\Scripts;%CONDA_DEFAULT_ENV%\Library\bin;%PATH%"
+
+@REM Run any activate scripts
+@if exist "%CONDA_DEFAULT_ENV%\etc\conda\activate.d" (
+    @pushd "%CONDA_DEFAULT_ENV%\etc\conda\activate.d"
+    @for %%g in (*.bat) do @call "%%g"
+    @popd
+)
+
+@REM Execute the given command
+__COMMAND__%*
+
+@endlocal
