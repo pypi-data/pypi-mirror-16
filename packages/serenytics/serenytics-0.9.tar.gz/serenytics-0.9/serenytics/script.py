ec9c0571f7578f776ac43c2ce443b91f044e9eec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:49 +0000

    Add package serenytics/0.9/serenytics-0.9.tar.gz

---
+++ packages/serenytics/serenytics-0.9.tar.gz/serenytics-0.9/serenytics/script.py
@@ -0,0 +1,77 @@
+import json
+import logging
+from copy import deepcopy
+
+from .helpers import SerenyticsException, make_request
+from . import settings
+
+logger = logging.getLogger(__name__)
+
+
+class UnknownScript(SerenyticsException):
+    def __init__(self, uuid_or_name):
+        super(UnknownScript, self).__init__(u'Script with uuid or name "{}" does not exist'.format(uuid_or_name))
+
+
+class Script(object):
+    """
+    Serenytics script
+    """
+
+    def __init__(self, config, headers):
+        self._config = config
+        self._headers = headers
+
+    @property
+    def name(self):
+        return self._config['name']
+
+    @property
+    def uuid(self):
+        return self._config['uuid']
+
+    def run(self, params=None, async=True):
+        """
+        Run script
+
+        :param params: Parameters to pass to the script execution
+        :type params: dict
+
+        :param async: Whether this call waits for the script execution to be finished.
+
+        :returns None if async=True. If async=False, returns a dict containing information on the script execution
+            - execution_status (either "SUCCESS" or "FAILURE")
+            - stdout
+            - stderr
+            - return_code
+        """
+        run_url = settings.SERENYTICS_API_DOMAIN + '/api/script/' + self.uuid + '/run'
+        script_params = deepcopy(params or {})
+        script_params['async'] = async
+        response = make_request('post', run_url, data=json.dumps(script_params), headers=self._headers,
+                                expected_json_status='ok')
+        if not async:
+            result = response.json()
+            return {
+                'execution_status': result['execution_status'],
+                'stdout': result['stdout'],
+                'stderr': result['stderr'],
+                'return_code': result['return_code']
+            }
+
+    def is_running(self):
+        """
+        :return: Whether the current script is running
+        """
+        state_url = settings.SERENYTICS_API_DOMAIN + '/api/script/' + self.uuid + '/state'
+        response = make_request('get', state_url, headers=self._headers)
+        return response.json()['running']
+
+    def get_last_logs(self):
+        """
+        Fetch last 10 logs
+        :return:
+        """
+        logs_url = settings.SERENYTICS_API_DOMAIN + '/api/script/' + self.uuid + '/logs'
+        response = make_request('get', logs_url, headers=self._headers)
+        return response.json()
