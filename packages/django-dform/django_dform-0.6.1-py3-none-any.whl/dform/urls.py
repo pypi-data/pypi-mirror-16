5fc7f5e2e3fa66172fe2e27bfa9f7559843a195f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:03:33 +0000

    Add package django-dform/0.6.1/django_dform-0.6.1-py3-none-any.whl

---
+++ packages/django-dform/django_dform-0.6.1-py3-none-any.whl/dform/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import url
+from dform import views
+
+urlpatterns = [
+    url(r'^sample_survey/(\d+)/$', views.sample_survey, 
+        name='dform-sample-survey'),
+
+    url(r'^survey/(\d+)/(\w+)/$', views.survey, name='dform-survey'),
+    url(r'^embedded_survey/(\d+)/(\w+)/$', views.embedded_survey, 
+      name='dform-embedded-survey'),
+
+    url(r'^survey_with_answers/(\d+)/(\w+)/(\d+)/(\w+)/$', 
+        views.survey_with_answers, name='dform-survey-with-answers'),
+    url(r'^embedded_survey_with_answers/(\d+)/(\w+)/(\d+)/(\w+)/$', 
+        views.embedded_survey_with_answers, 
+        name='dform-embedded-survey-with-answers'),
+]
