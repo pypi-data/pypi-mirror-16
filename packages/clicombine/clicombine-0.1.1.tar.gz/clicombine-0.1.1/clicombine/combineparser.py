da54fb0fd697bfab7a1e874b8b6aa0185da0a517
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:37 +0000

    Add package clicombine/0.1.1/clicombine-0.1.1.tar.gz

---
+++ packages/clicombine/clicombine-0.1.1.tar.gz/clicombine-0.1.1/clicombine/combineparser.py
@@ -0,0 +1,180 @@
+# combineparser.py
+# author: Elias Rubin
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+# CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+import glob
+import utils
+import sys
+import os
+import json
+
+ALLOWED_KEYS = {
+    'twelve_meter_filename',
+    'seven_meter_filename',
+    'output_basename',
+    'weightings',
+    'mode',
+    'imagermode',
+    'seven_meter_spw',
+    'twelve_meter_spw',
+    'field',
+    'outframe',
+    'seven_meter_imsize',
+    'twelve_meter_imsize',
+    'seven_meter_cell',
+    'twelve_meter_cell',
+    'phasecenter',
+    'robust',
+    'restfreq',
+    'start',
+    'width',
+    'nchan',
+    'thresh',
+    'produce_feather',
+    'produce_uv',
+    'moments'
+}
+
+
+def parse(config_fname, clargs):
+    error_occured = False
+    if not os.path.exists(config_fname):
+        utils.eprint("{} no such file '{}'".format(
+            utils.color_string("Error:", 'red', bold=True),
+            utils.color_string(config_fname, 'red', bold=True)))
+        sys.exit(0)
+
+    with open(config_fname, mode='r') as f:
+        param_dict = json.load(f)
+
+    if not clargs.raw:
+        for k, v in param_dict.iteritems():
+
+            if k not in ALLOWED_KEYS:
+                parse_error(utils.color_string("{} : {}".format(k, v), 'p'), k,
+                            config_fname, expected=closest_legal(k, ALLOWED_KEYS))
+                error_occured = True
+                continue
+
+            if not legal_value_for_key(k, v, param_dict):
+                error_occured = True
+                continue
+
+            # TODO
+            # units = parse_units(k, v)
+
+            # if units not in ALLOWED_UNITS[k]:
+            #     parse_error("{} : {}".format(k, v), units, config_fname,
+            #                 expected=closest_legal(units, ALLOWED_UNITS[k]))
+            #     error_occured = True
+
+    if error_occured:
+        sys.exit(0)
+
+    return param_dict
+
+
+def legal_value_for_key(key, value, param_dict):
+    """Determine if the value is a legal value for the key, given the conditions
+    of param_dict."""
+
+    if param_dict['mode'] == 'channel':
+        if key == 'start' and not utils.castable(value, int):
+            utils.eprint("{} Mode channel requires integer value for 'start'".format(
+                utils.color_string("Error:", 'red', bold=True)))
+            return False
+
+        if key == 'width' and not utils.castable(value, int):
+            utils.eprint("{} Mode channel requires integer value for 'width'".format(
+                utils.color_string("Error:", 'red', bold=True)))
+            return False
+
+    if key == 'nchan' and not utils.castable(value, int):
+        return False
+
+    if 'imsize' in key and not utils.castable(value, list):
+        if not utils.castable(value, int):
+            return False
+
+    if key == 'thresh' and not utils.castable(value, float) and 'jy' not in value.lower():
+        utils.eprint("{} key 'thresh' must be numeric or in [m]jy.".format(
+            utils.color_string("Error:", 'red', bold=True)))
+        return False
+
+    if key == 'robust' and not utils.castable(value, float):
+        utils.eprint("{} key 'robust' must be of type float".format(
+            utils.color_string("Error:", 'red', bold=True)))
+        return False
+
+    if key == 'moments' and not utils.castable(value, list):
+        utils.eprint('{} key "moments" must be of type list<int>'.format(
+            utils.color_string("Error:", 'red', bold=True)))
+        return False
+
+    if key == 'seven_meter_filename' and not glob.glob(value):
+        utils.eprint('{} no file of name {} present'.format(
+            utils.color_string("Error:", 'red', bold=True), value))
+        return False
+
+    if key == 'twelve_meter_filename' and not glob.glob(value):
+        utils.eprint('{} no file of name {} present'.format(
+            utils.color_string("Error:", 'red', bold=True), value))
+        return False
+
+    return True
+
+
+def closest_legal(illegal_string, legal_collection, metric=utils.levenshtein):
+    """ Find the closest string (as determined by metric) to
+        illegal_string in legal_collection """
+
+    return sorted([(metric(illegal_string, legal_string), legal_string)
+                   for legal_string in legal_collection],
+                  key=lambda x: x[0])[0][1]
+
+
+def parse_error(line, problem, fname, linenum=None, expected=None):
+
+    if linenum and expected:
+        error_string = """{} you wrote '{}' at {}:{}.
+        We had trouble parsing '{}'.  Did you mean '{}'?""".format(
+            utils.color_string("Error:", 'red', bold=True),
+            line, fname, linenum,
+            utils.color_string(problem, 'red'),
+            utils.color_string(expected, 'blue'))
+
+    elif linenum:
+        error_string = """{} you wrote '{}' at {}:{}.
+            We had trouble parsing '{}'.""".format(
+            utils.color_string("Error:", 'red', bold=True),
+            line, fname, linenum,
+            utils.color_string(problem, 'red'))
+
+    elif expected:
+        error_string = """{} you wrote '{}' in {}.  We had trouble parsing
+        '{}'.  Did you mean '{}'?""".format(
+            utils.color_string("Error:", 'red', bold=True),
+            line, fname,
+            utils.color_string(problem, 'red'),
+            utils.color_string(expected, 'blue'))
+    else:
+        error_string = """{} you wrote '{}' in {}.  We had trouble parsing
+        '{}'.""".format(
+            utils.color_string("Error:", 'red', bold=True),
+            line, fname, utils.color_string(problem, 'red'))
+
+    utils.eprint(error_string)
---
+++ packages/clicombine/clicombine-0.1.1.tar.gz/clicombine-0.1.1/clicombine/utils.py
@@ -0,0 +1,138 @@
+from __future__ import print_function
+import sys
+
+
+def param_dict_to_clean_input(param_dict, seven_meter):
+    """Convert a param dict into input appropriately formatted for casa's
+       'clean' task."""
+
+    thresh = to_jansky(param_dict['thresh'])
+
+    clean_input = {}
+
+    # using str() because casa breaks when given unicode string literals
+    # in the generated code, i.e. an input like **{'cell': u'0.0145arcsec'}
+    # does not work, but **{'cell' : '0.0145arcsec'} does.
+    clean_input.update({
+        'mode': str(param_dict['mode']),
+        'imagermode': str(param_dict['imagermode']),
+        'spw': str(param_dict['seven_meter_spw']) if
+        seven_meter else str(param_dict['twelve_meter_spw']),
+        'field': str(param_dict['field']),
+        'outframe': str(param_dict['outframe']),
+        'imsize': param_dict['seven_meter_imsize'] if
+        seven_meter else param_dict['twelve_meter_imsize'],
+        'cell': str(param_dict['seven_meter_cell']) if
+        seven_meter else str(param_dict['twelve_meter_cell']),
+        'phasecenter': str(param_dict['phasecenter']),
+        'robust': numberify(param_dict['robust']),
+        'restfreq': str(param_dict['restfreq']),
+        'start': str(param_dict['start']),
+        'width': str(param_dict['width']),
+        'nchan': (param_dict['nchan'])
+    })
+
+    return thresh, clean_input
+
+
+def castable(value, typename):
+    """Can value be cast as type typename without throwing an exception?"""
+    try:
+        typename(value)
+        return True
+    except Exception:
+        return False
+
+
+def numberify(non_numeric_string):
+    if non_numeric_string == '':
+        return ''
+    if isinstance(non_numeric_string, (int, float)):
+        return non_numeric_string
+    if isinstance(non_numeric_string, str):
+        return float(''.join((c for c in non_numeric_string if c in '0123456789.')))
+
+
+def to_jansky(thresh):
+    if (isinstance(thresh, float)):
+        return thresh
+
+    if (isinstance(thresh, int)):
+        return thresh
+
+    if 'mjy' in thresh.lower():
+        return float(thresh.lower().strip('mjy')) / 1000.
+
+    if 'jy' in thresh.lower():
+        return float(thresh.lower().strip('jy'))
+
+    else:
+        return 1.0
+
+
+def config_name_from_input_name(input_fname):
+    if '.json' in input_fname:
+        return input_fname
+    return input_fname + '.json'
+
+
+def levenshtein(s1, s2):
+    """
+    source:
+    https: // en.wikibooks.org / wiki / Algorithm_Implementation
+    / Strings / Levenshtein_distance
+    """
+    if len(s1) < len(s2):
+        return levenshtein(s2, s1)
+
+    # len(s1) >= len(s2)
+    if len(s2) == 0:
+        return len(s1)
+
+    previous_row = range(len(s2) + 1)
+    for i, c1 in enumerate(s1):
+        current_row = [i + 1]
+        for j, c2 in enumerate(s2):
+            # j+1 instead of j since previous_row and current_row are one
+            # character longer
+            insertions = previous_row[j + 1] + 1
+            deletions = current_row[j] + 1       # than s2
+            substitutions = previous_row[j] + (c1 != c2)
+            current_row.append(min(insertions, deletions, substitutions))
+        previous_row = current_row
+
+    return previous_row[-1]
+
+
+def color_string(string, color_arg, bold=False, underline=False):
+    PURPLE = '\033[95m'
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    c = color_arg[0].lower()
+    if c == 'p':
+        color = PURPLE
+    elif c == 'b':
+        color = BLUE
+    elif c == 'y':
+        color = YELLOW
+    elif c == 'g':
+        color = GREEN
+    elif c == 'r':
+        color = RED
+    else:
+        color = ''
+
+    b = BOLD if bold else ''
+    u = UNDERLINE if underline else ''
+
+    return b + u + color + string + ENDC
+
+
+def eprint(*args, **kwargs):
+    """Print to stderr. """
+    print(*args, file=sys.stderr, **kwargs)
