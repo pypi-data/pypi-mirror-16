7fe933710b441b687db418ec70f605b03fc613b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:17:40 +0000

    Add package nester35/1.0.0/nester35-1.0.0.zip

---
+++ packages/nester35/nester35-1.0.0.zip/nester35-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup (
+        name         = 'nester35' ,
+        version      = '1.0.0' ,
+        py_modules   = ['nester35'] ,
+        author       = 'hfpython' ,
+        author_email = 'hfpython@headfirstlabs.com' ,
+        url          = 'http://www.headfirstlabs.com',
+        description  = 'A simple printer of nested lists' ,
+      )
