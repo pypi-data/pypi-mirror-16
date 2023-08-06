0a7d261a9386cac3857956e1d97a695b286ba8e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:52 +0000

    Add package jschema/0.3.2/jschema-0.3.2.tar.gz

---
+++ packages/jschema/jschema-0.3.2.tar.gz/jschema-0.3.2/jschema/make/make_golang.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from jschema.make._options import Options
+from jschema.make._golang_maker import GolangMaker
+
+
+def make_golang(schema, options):
+    maker = GolangMaker(schema, Options(options))
+    maker.find_structs()
+    maker.prepare_structs()
+    maker.save_structs()
---
+++ packages/jschema/jschema-0.3.2.tar.gz/jschema-0.3.2/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup
+from jschema import __version__
+
+setup(
+    name="jschema",
+    version=__version__,
+    description="json schema tookit",
+    license="MIT",
+    author="Stepan Perlov",
+    author_email="stepanperlov@gmail.com",
+    install_requires=["PyYAML", "jinja2"],
+    packages=["jschema", "jschema.make", "jschema.node"],
+    package_data={"jschema.make": ["templates/*.j2"]},
+    url="https://github.com/stepan-perlov/jschema",
+    platforms=["linux"]
+)
