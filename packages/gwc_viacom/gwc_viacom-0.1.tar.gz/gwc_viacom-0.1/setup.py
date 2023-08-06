4636229cdf731fb3e3e6cfee5efea68960af60b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:16:35 +0000

    Add package gwc_viacom/0.1/gwc_viacom-0.1.tar.gz

---
+++ packages/gwc_viacom/gwc_viacom-0.1.tar.gz/gwc_viacom-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='gwc_viacom',
+      version='0.1',
+      description='GWC Viacom 2016 Principles of Coding',
+      url='http://github.com/kathrynthegreat/gwc_viacom',
+      author='GWC Viacom 2016',
+      author_email='kvasilaky@gmail.com',
+      license='GWC',
+      packages=['gwc_viacom'],
+      zip_safe=False)
