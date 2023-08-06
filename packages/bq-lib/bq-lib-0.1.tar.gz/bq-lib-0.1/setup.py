780beea735accdf94f98a76c5b793e0d48d10a83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:44 +0000

    Add package bq-lib/0.1/bq-lib-0.1.tar.gz

---
+++ packages/bq-lib/bq-lib-0.1.tar.gz/bq-lib-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name = 'bq-lib',
+      version = '0.1',
+      description = 'A simple python wrapper for Google BigQuery',
+      url = 'https://github.com/jpvelez/bqlib',
+      download_url = 'https://github.com/jpvelez/bqlib/tarball/0.1',
+      author = 'Manish Nag',
+      author_email = 'manish@spotify.com',
+      keywords = ['BigQuery', 'Google Cloud Platform'],
+      license = 'MIT')
