79c71fea3c060d4521173b90e5ba05a0ee360112
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:03:12 +0000

    Add package gimmik/2.1/gimmik-2.1-py3-none-any.whl

---
+++ packages/gimmik/gimmik-2.1-py3-none-any.whl/gimmik-2.1.dist-info/DESCRIPTION.rst
@@ -0,0 +1,9 @@
+GiMMiK is a Python based kernel generator for
+matrix multiplication kernels for various accelerator platforms.  For
+small operator matrices the generated kernels are capable of
+outperfoming the state-of-the-art general matrix multiplication
+routines such as cuBLAS GEMM or clBLAS GEMM.  GiMMiK was originally
+developed as part of Bartosz Wozniak's master's thesis in the
+Department of Computing at Imperial College London and is currently
+maintained by Freddie Witherden.
+
---
+++ packages/gimmik/gimmik-2.1-py3-none-any.whl/gimmik/__init__.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+import pkgutil
+import re
+
+from mako.template import Template
+import numpy as np
+
+from gimmik._version import __version__
+
+
+def generate_mm(mat, dtype, platform, alpha=1.0, beta=0.0, funcn='gimmik_mm'):
+    # Data type
+    dtype = np.dtype(dtype).type
+    if dtype == np.float32:
+        dtype = 'float'
+    elif dtype == np.float64:
+        dtype = 'double'
+    else:
+        raise ValueError('Invalid floating point data type')
+
+    # Multiply the matrix through by alpha
+    mat = alpha*mat
+
+    # Template arguments
+    tplargs = {'dtype': dtype, 'mat': mat, 'beta': beta, 'funcn': funcn}
+
+    # Load and render the template
+    tpl = pkgutil.get_data(__name__, 'kernels/{0}.mako'.format(platform))
+    src = Template(tpl).render(**tplargs)
+
+    # At single precision suffix all floating point constants by 'f'
+    if dtype == 'float':
+        src = re.sub(r'(?=\d*[.eE])(?=\.?\d)\d*\.?\d*(?:[eE][+-]?\d+)?',
+                     r'\g<0>f', src)
+
+    # Return the source
+    return src
