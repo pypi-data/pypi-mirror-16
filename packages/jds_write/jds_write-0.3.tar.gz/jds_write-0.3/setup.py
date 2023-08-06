6879524c3f087ff16b97637b5ee9b175199481c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:59 +0000

    Add package jds_write/0.3/jds_write-0.3.tar.gz

---
+++ packages/jds_write/jds_write-0.3.tar.gz/jds_write-0.3/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='jds_write',
+      version='0.3',
+      description='The funniest joke in the world',
+      url='http://github.com/despiegj/jds_write',
+      author='Jan De Spiegeleer',
+      author_email='jds@riskconcile.com',
+      license='MIT',
+      packages=['jds_write'],
+      install_requires=[
+          'nbconvert','nbformat'],
+      zip_safe=False)
