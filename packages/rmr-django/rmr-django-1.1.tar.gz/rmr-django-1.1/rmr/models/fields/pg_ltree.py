deb595dc2033967f4e98aee3da8312893e5c5ad5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:57 +0000

    Add package rmr-django/1.1/rmr-django-1.1.tar.gz

---
+++ packages/rmr-django/rmr-django-1.1.tar.gz/rmr-django-1.1/rmr/models/fields/pg_ltree.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+
+class PgLtreeField(models.TextField):
+
+    description = 'Ltree'
+
+    def db_type(self, connection):
+        return 'ltree'
+
+    def get_internal_type(self):
+        return "CharField"
+
+    def formfield(self, **kwargs):
+        return super(models.TextField, self).formfield()
+
+
+@PgLtreeField.register_lookup
+class AncestorOf(models.Transform):
+    lookup_name = 'ancestor_of'
+    operator = '@>'
+
+
+@PgLtreeField.register_lookup
+class DescendantOf(models.Transform):
+    lookup_name = 'descendant_of'
+    operator = '<@'
+
+
+@PgLtreeField.register_lookup
+class Match(models.Transform):
+    lookup_name = 'match'
+    operator = '~'
