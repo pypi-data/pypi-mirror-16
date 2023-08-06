f0ba7dc5196019501386208622d1468130668b6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:58:48 +0000

    Add package jupyter-react/0.1.2/jupyter-react-0.1.2.zip

---
+++ packages/jupyter-react/jupyter-react-0.1.2.zip/jupyter-react-0.1.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(name='jupyter-react',
+      version='0.1.2',
+      description='React component extension for Jupyter Notebooks',
+      url='https://github.com/timbr-io/jupyter-react',
+      author='Chris Helm',
+      author_email='chelm@timbr.io',
+      license='MIT',
+      packages=['jupyter_react'],
+      zip_safe=False,
+      install_requires=[
+          "ipython",
+          "traitlets"
+        ]
+      )
