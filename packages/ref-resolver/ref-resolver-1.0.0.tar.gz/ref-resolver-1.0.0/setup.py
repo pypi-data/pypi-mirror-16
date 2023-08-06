3f991bcdc5a309b97c17d662ae1c4cf7adb2f88c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:13 +0000

    Add package ref-resolver/1.0.0/ref-resolver-1.0.0.tar.gz

---
+++ packages/ref-resolver/ref-resolver-1.0.0.tar.gz/ref-resolver-1.0.0/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='ref-resolver',
+      version='1.0.0',
+      description='A lightweight python json schema ref resolver and inliner.',
+      author='Purush Swaminathan',
+      author_email='purukaushik@asu.edu',
+      license='MPL 2.0',
+      url='https://github.com/purukaushik/ref-resolver',
+      download_url='https://github.com/purukaushik/ref-resolver.git',
+      packages=find_packages(),
+     )
