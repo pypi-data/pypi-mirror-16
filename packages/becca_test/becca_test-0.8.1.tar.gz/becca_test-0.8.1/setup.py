5069e87f49378b531d90049f95e8fd071eccab12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:00 +0000

    Add package becca_test/0.8.1/becca_test-0.8.1.tar.gz

---
+++ packages/becca_test/becca_test-0.8.1.tar.gz/becca_test-0.8.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='becca_test',
+version='0.8.1',
+description='A test suite for Becca',
+url='http://github.com/brohrer/becca_test',
+download_url='https://github.com/brohrer/becca_test/archive/master.zip',
+author='Brandon Rohrer',
+author_email='brohrer@gmail.com',
+license='MIT',
+packages=['becca_test'],
+install_requires=['becca'],
+zip_safe=False)
