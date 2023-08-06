eb10d8ccd3e0e8ad3ae2bd1d1a6098ab364f871f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:12 +0000

    Add package os-gobble/0.1.9/os-gobble-0.1.9.tar.gz

---
+++ packages/os-gobble/os-gobble-0.1.9.tar.gz/os-gobble-0.1.9/gobble/cli.py
@@ -0,0 +1,92 @@
+"""Command line interface for Gobble"""
+
+import io
+
+from json import loads
+from click import command, argument, Path, group, option, echo
+from os.path import join
+
+from gobble.user import create_user
+from gobble.fiscal import FiscalDataPackage
+from gobble.config import ROOT_DIR
+from gobble.search import search
+
+
+@group()
+def gobble():
+    pass
+
+
+@command()
+@argument('filepath', type=Path(exists=True))
+def version():
+    filename = join(ROOT_DIR, 'package.json')
+    with io.open(filename) as json:
+        package = loads(json.read())
+        echo(package['version'])
+
+
+@command()
+def start():
+    create_user()
+
+
+@command()
+@argument('filepath',
+          type=Path(exists=True),
+          metavar='full path to JSON datapackage descriptor')
+def check(filepath):
+    package = FiscalDataPackage(filepath)
+    package.validate(raise_error=False)
+
+
+@command()
+@argument('query_all')
+@option('--private', default=True, help='include private packages')
+@option('--limit', default=50, help='limit the search results')
+@option('--title', default=None, help='search package titles')
+@option('--author', default=None, help='search packages by authors')
+@option('--description', default=None, help='search packages by descriptions')
+@option('--region', default=None, help='search packages by regions')
+@option('--country', default=None, help='search packages by 2-digit country code')
+@option('--city', default=None, help='search packages by city')
+@option('--any', default=None, help='search packages by city')
+def pull(private,
+         limit,
+         query_all,
+         title,
+         author,
+         description,
+         region,
+         country,
+         city):
+
+    search_options = (
+        ('title', title),
+        ('author', author),
+        ('description', description),
+        ('regionCode', region),
+        ('countryCode', country),
+        ('cityCode', city)
+    )
+
+    if query_all:
+        query = {'q': all}
+    else:
+        query = {key: value for key, value in search_options if value}
+
+    search(query, private=private, limit=limit)
+
+
+@command()
+@argument('filepath', type=Path(exists=True))
+def push(filepath):
+    package = FiscalDataPackage(filepath)
+    package.upload()
+
+
+gobble.add_command(version)
+gobble.add_command(check)
+gobble.add_command(pull)
+gobble.add_command(push)
+gobble.add_command(start)
---
+++ packages/os-gobble/os-gobble-0.1.9.tar.gz/os-gobble-0.1.9/gobble/search.py
@@ -0,0 +1,59 @@
+"""Search for users and package inside the ElasticSearch database"""
+
+
+from gobble.api import search_packages, handle
+from gobble.user import User
+
+SEARCH_KEYS = ['q', 'size', 'title', 'author', 'description',
+               'regionCode', 'countryCode', 'cityCode']
+
+
+def search(query, private=True, limit=None):
+    """Query the packages on Open-Spending.
+
+    You can search a package by `title`, `author`, `description`, `regionCode`,
+    `countryCode` or`cityCode`. You can match all these fields at once with the
+    magic `q` key.
+
+    If authentication-token was provided, then private packages from the
+    authenticated user will also be included. Otherwise, only public packages
+    will be returned. You can limit the size of your results with the `size`
+    parameter.
+
+    :param query: a `dict` of key value pairs
+    :param private: show private datapackages
+    :param limit: the number of results returned
+
+    :type query: `dict`
+    :rtype private: `bool'
+    :rtype size: `int'
+
+    :return: a dictionary with the results
+    :rtype: :class: `list` of `dict`
+    """
+    _validate(query)
+
+    if limit:
+        query.update(size=limit)
+
+    quoted_query = _sanitize(query)
+
+    if private:
+        user = User()
+        quoted_query.update(jwt=user.token)
+
+    response = search_packages(params=quoted_query)
+    return handle(response)
+
+
+def _sanitize(query):
+    keys = ['package.' + str(k) for k in query.keys()]
+    values = ['"' + str(v) + '"' for v in query.values()]
+    return dict(zip(keys, values))
+
+
+def _validate(query):
+    for key in query.keys():
+        if key not in SEARCH_KEYS:
+            msg = 'Invalid search key "{key}" for package'
+            raise ValueError(msg.format(key=key))
