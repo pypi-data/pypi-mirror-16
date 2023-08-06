8113561bd642ce56600c16498667081969f7a73b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:43 +0000

    Add package skyb/0.1.3/skyb-0.1.3.tar.gz

---
+++ packages/skyb/skyb-0.1.3.tar.gz/skyb-0.1.3/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='skyb',
+    packages=['skyb', 'skyb.conf', 'skyb.model', 'skyb.utils', 'skyb.quick', 'skyb.quick.tornado'],
+    version='0.1.3',
+    description='skypiea base',
+    author='woshifyz',
+    author_email='monkey.d.pandora@gmail.com',
+    url='https://github.com/woshifyz/skyb',
+    download_url='https://github.com/woshifyz/skyb/tarball/0.1.3',
+    keywords=['skypiea', 'base'],
+    classifiers=[],
+)
