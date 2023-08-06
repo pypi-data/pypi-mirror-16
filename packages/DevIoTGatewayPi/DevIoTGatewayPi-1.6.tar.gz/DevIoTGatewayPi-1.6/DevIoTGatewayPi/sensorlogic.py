e5b7928a8862a3da966bc8c94b1354c2e498af31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:24 +0000

    Add package DevIoTGatewayPi/1.6/DevIoTGatewayPi-1.6.tar.gz

---
+++ packages/DevIoTGatewayPi/DevIoTGatewayPi-1.6.tar.gz/DevIoTGatewayPi-1.6/DevIoTGatewayPi/sensorlogic.py
@@ -0,0 +1,16 @@
+
+__author__ = 'tingxxu'
+
+class SensorLogic:
+
+    @staticmethod
+    def update(sensor, data):
+        pass
+
+    @staticmethod
+    def action(sensor, data):
+        pass
+
+    @staticmethod
+    def modify(sensor, data):
+        return False
