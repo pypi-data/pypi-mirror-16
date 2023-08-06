e9854b3f5f5ba75cf304b55b2922370451abdf09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:41 +0000

    Add package Swoop/0.4.1/Swoop-0.4.1.tar.gz

---
+++ packages/Swoop/Swoop-0.4.1.tar.gz/Swoop-0.4.1/Swoop/tools/CheckEagle.py
@@ -0,0 +1,141 @@
+#!/usr/bin/env python
+
+import Swoop as HE
+import argparse
+import shutil
+from lxml import etree as ET
+import sys
+import logging as log
+import sys, traceback
+
+def countParts(et, tags, attrs):
+    for et in et.findall(".//*"):
+        if et.tag in tags:
+            tags[et.tag] = tags[et.tag] + 1
+        else:
+            tags[et.tag] =  1
+        for i in et.attrib:
+            if i in attrs:
+                attrs[i] = attrs[i] + 1
+            else:
+                attrs[i] =  1
+
+def compareEagleElementTrees(orig, new):
+    origAttrs = {}
+    origTags = {}
+    countParts(orig,origTags, origAttrs)
+    newAttrs = {}
+    newTags = {}
+    countParts(new,newTags, newAttrs)
+
+    mismatches = 0
+    for i in origAttrs:
+        if i not in newAttrs:
+            newAttrs[i] = 0
+
+    for i in origTags:
+        if i not in newTags:
+            newTags[i] = 0
+                
+    for i in origAttrs:
+        if newAttrs[i] != origAttrs[i]:
+            log.warning("Attribute count mismatch for '" + i + "': orig=" + str(origAttrs[i]) + "; new=" + str(newAttrs[i]))
+            mismatches = mismatches + 1
+    for i in origTags:
+        if newTags[i] != origTags[i]:
+            log.warning("Tag count mismatch for '" + i + "': orig=" + str(origTags[i]) + "; new=" + str(newTags[i]))
+            mismatches = mismatches + 1
+
+    if mismatches != 0:
+        for i in origTags:
+            if int(newTags[i]) - int(origTags[i]) != 0:
+                print i + ": " + str(newTags[i]) + " " + str(origTags[i]) + " " + str(int(newTags[i]) - int(origTags[i]))
+        
+        for i in origAttrs:
+            if int(newAttrs[i]) - int(origAttrs[i]) != 0:
+                print i + ": " + str(newAttrs[i]) + " " + str(origAttrs[i]) + " " + str(int(newAttrs[i]) - int(origAttrs[i]))
+        
+                
+    return mismatches
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Check whether eagle files are dtd conforming")
+    parser.add_argument("--file", required=True,  type=str, nargs='+', dest='file', help="files to process")
+    parser.add_argument("--scrubbed-suffix", required=False,  type=str, nargs=1, dest='scrubSuffix', help="Suffix for scrubbed output files.  The empty string to overwrite input.")
+    parser.add_argument("-v", required=False, action='store_true', dest='verbose', help="Be verbose")
+    parser.add_argument("-q", required=False, action='store_true', dest='quiet', help="Be silent")
+    parser.add_argument("--internal-check", required=False, action='store_true', dest='internalCheck', help="Do checks on Swoop internals")
+    parser.add_argument("--stop-on-error", required=False, action='store_true', dest='stopOnError', help="Stop on first error.")
+    parser.add_argument("--crash-on-error", required=False, action='store_true', dest='crashOnError', help="Don't catch exceptions.")
+
+    args = parser.parse_args()
+    
+    if args.verbose:
+        log.basicConfig(format="%(levelname)s: %(message)s", level=log.DEBUG)
+        log.info("Verbose output.")
+    else:
+        log.basicConfig(format="%(levelname)s: %(message)s")
+
+    success = 0
+    failed = 0
+    internalError = 0
+    corruptInput = 0
+    for i in args.file:
+
+        if not args.quiet:
+            print "Validating " + i + "...",
+
+        try:
+            # for the internal check, we compare the input and output.  With
+            # bestEffort == True, we can successfully parse illegal input, but
+            # since we always produce legal output, they won't match.
+            f = HE.EagleFile.from_file(i, bestEffort=(not args.internalCheck))
+            #f.write(i+".xml")
+            if f.validate():
+                goodSoFar = True
+                if not args.quiet:
+                    print("valid.")
+                if args.internalCheck:
+                    if compareEagleElementTrees(f.root, f.get_et()) > 0:
+                        goodSoFar = False
+
+                if goodSoFar:
+                    success += 1
+                    if args.scrubSuffix is not None:
+                        parts = i.split(".")
+                        n = ".".join(parts[0:-1]+args.scrubSuffix + [parts[-1]])
+                        f.write(n)
+                else:
+                    failed += 1
+                
+            else:
+                failed += 1
+                if not args.quiet:
+                    print("invalid.")
+        except HE.EagleFormatError as e:
+            corruptInput += 1
+            print("corrupt.")
+            print e
+        except Exception as e:
+            internalError += 1
+            if args.crashOnError:
+                ex_type, ex, tb = sys.exc_info()
+                traceback.print_tb(tb)
+                raise e
+            if not args.quiet:
+                print "Swoop error: " + str(e)
+                
+        if args.stopOnError and failed + internalError != 0:
+            break
+        
+    if not args.quiet:
+        print "Valid: " + str(success)
+        print "Corrupt Input: " + str(corruptInput)
+        print "Invalid: " + str(failed) 
+        print "Error: " + str(internalError)
+            
+    if failed + internalError == 0:
+        sys.exit(0)
+    else:
+        sys.exit(1)
---
+++ packages/Swoop/Swoop-0.4.1.tar.gz/Swoop-0.4.1/setup.py
@@ -0,0 +1,85 @@
+from setuptools import setup
+from setuptools import Extension
+from setuptools.command.build_py import build_py
+import os
+from codecs import open
+import sys
+
+#from Cython.Build import *
+
+#import argparse
+#parser = argparse.ArgumentParser(description="Fix eagle files to make them dtd conforming")
+#parser.add_argument("--dtd", required=True,  type=str, nargs=1, dest='dtd', help="Eagle dtd to use.")
+
+class BuildSwoop(build_py):
+
+    def run(self):
+        import Swoop.GenerateSwoop
+        dtd = open("Swoop/eagleDTD.py", "w")
+        os.system("patch Swoop/eagle-7.2.0.dtd Swoop/eagle.dtd.diff -o Swoop/eagle-swoop.dtd")
+        dtd.write('DTD="""')
+        dtd.write(open("Swoop/eagle-swoop.dtd").read())
+        dtd.write('"""')
+        dtd.close()
+        Swoop.GenerateSwoop.main("Swoop/Swoop.py")
+        build_py.run(self)
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(here, 'DESCRIPTION.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+with open(os.path.join(here, 'VERSION.txt'), encoding='utf-8') as f:
+    version = f.read()
+
+setup(name='Swoop',
+      version=version,
+      description="Swoop is a Python library for working with CadSoft Eagle files.",
+      long_description=long_description,
+      classifiers=[
+          "Development Status :: 4 - Beta",
+          "Intended Audience :: Science/Research",
+          "Operating System :: MacOS",
+          "Operating System :: POSIX",
+          "Operating System :: POSIX :: Linux",
+          "Operating System :: Unix",
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 2",
+          "Programming Language :: Python :: 2.7",
+          "Topic :: Scientific/Engineering",
+          "Topic :: Scientific/Engineering :: Electronic Design Automation (EDA)",
+          "Topic :: System",
+          "Topic :: System :: Hardware",
+      ],
+      author="NVSL, University of California San Diego",
+      author_email="swanson@cs.ucsd.edu",
+      url="http://nvsl.ucsd.edu/Swoop/",
+      test_suite="test",
+      packages = ["Swoop", "Swoop.ext", "Swoop.tools"],
+      package_dir={
+          'Swoop' : 'Swoop',
+      },
+      package_data={
+          "" : ["*.rst"],
+          "Swoop" : ["Swoop/Swoop.py.jinja", "Swoop/eagle.dtd.diff", "Swoop/eagle.dtd"]
+      },
+      
+      #ext_modules = cythonize([Extension("*", ["Swoop/Swoop.pyx"], extra_compile_args=["-O4"])]),
+      install_requires=["lxml>=3.4.2",  "Sphinx>=1.3.1", "numpy","Jinja2>=2.7.3", "shapely>=1.5.13"],
+      setup_requires=["Jinja2>=2.7.3"],
+      include_package_data=True,
+      entry_points={
+        'console_scripts': [
+            'cleanupEagle = Swoop.tools.CleanupEagle:main',
+            'checkEagle = Swoop.tools.CheckEagle:main',
+            'mergeLibrary = Swoop.tools.MergeLibrary:main',
+            'fixEagle = Swoop.tools.FixEagle:main',
+            'snapSchematic = Swoop.tools.SnapToGrid:main',
+            'relayerEagle =  Swoop.tools.Relayer:main'
+            ]
+        },
+      keywords = "PCB Eagle CAD printed circuit boards schematic electronics CadSoft",
+      cmdclass={'build_py': BuildSwoop}
+      )
+
+
