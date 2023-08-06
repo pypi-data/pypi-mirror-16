654cf5e625303b04a8885c1ebead312ea132b0d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:36 +0000

    Add package banditvis/0.3/banditvis-0.3.tar.gz

---
+++ packages/banditvis/banditvis-0.3.tar.gz/banditvis-0.3/banditvis/__main__.py
@@ -0,0 +1,63 @@
+"""
+Entry point for the banditvis script installed along with this package.
+
+
+Usage:
+    banditvis [options] input
+    banditvis -h | --help
+    banditvis -V | --version
+
+Positional:
+  input                 The path to your .txt input file.
+
+Optional:
+  -D, --delete          Delete intermediate data files.
+  --data=<directory>    The path to the data directory, defaults to the
+                          current directory.
+  --out=<directory>     The path to the output location, defaults to the
+                          current directory.
+"""
+from . import __version__
+from .manager import run
+import time
+import sys
+import argparse
+
+def get_usage():
+    return '\n\n\n'.join(__doc__.split('\n\n\n')[1:])
+
+def get_args():
+    """
+    Command line argument parser. Parses the arguments
+    """
+    parser = argparse.ArgumentParser(prog='banditvis')
+    parser.add_argument("input",
+        help="The path to your .txt input file.")
+    parser.add_argument("--data", nargs='?',
+        help="The path to the data directory, defaults to the current directory.")
+    parser.add_argument("--out", nargs='?',
+        help="The path to the output location, defaults to the current directory.")
+    parser.add_argument("-D", "--delete", action='store_true',
+        help="Delete intermediate data files.")
+
+    # check for certain arguments
+    if ("-h" or "--help") in sys.argv[1:]:
+        print(get_usage())
+        sys.exit(0)
+    elif ("-V" or "--version") in sys.argv[1:]:
+        print("banditvis ({}) -- installed at {}".format(__version__, "/".join(__file__.split("/")[:-1])))
+        sys.exit(0)
+
+    # parse the arguments
+    else:
+        args = vars(parser.parse_args())
+        # remove any None values
+        arguments = {key:args[key] for key in args.keys() if args[key] is not None}
+        return arguments
+
+def main():
+    run(**get_args())
+
+if __name__ == "__main__":
+    main()
+
---
+++ packages/banditvis/banditvis-0.3.tar.gz/banditvis-0.3/banditvis/defaults.py
@@ -0,0 +1,20 @@
+
+CORE_DEFAULTS = {
+    'out': "hi",
+    'Animate': False,
+    'FPS': 20,
+    'HelpLines': True,
+    'InputData': False,
+    'LevelCurves': True,
+    'Multiprocess': 1,
+    'NoAxesTick': False,
+    'Normalized': False,
+    'PlotSave': "temp.pdf",
+    'PlotTitle': False,
+    'DeleteData': False
+}
+
+USER_DEFAULTS = {
+    'out': 'Output',
+    'data': 'Data'
+}
