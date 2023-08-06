e8dc46a8f6abb1d3cf0dd3d71ea91bbe089aa7a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:18:59 +0000

    Add package xrayutilities/1.3.3/xrayutilities-1.3.3.tar.gz

---
+++ packages/xrayutilities/xrayutilities-1.3.3.tar.gz/xrayutilities-1.3.3/doc/source/xrayutilities.simpack.rst
@@ -0,0 +1,46 @@
+xrayutilities.simpack package
+=============================
+
+Submodules
+----------
+
+xrayutilities.simpack.fit module
+--------------------------------
+
+.. automodule:: xrayutilities.simpack.fit
+    :members:
+    :undoc-members:
+    :show-inheritance:
+
+xrayutilities.simpack.helpers module
+------------------------------------
+
+.. automodule:: xrayutilities.simpack.helpers
+    :members:
+    :undoc-members:
+    :show-inheritance:
+
+xrayutilities.simpack.models module
+-----------------------------------
+
+.. automodule:: xrayutilities.simpack.models
+    :members:
+    :undoc-members:
+    :show-inheritance:
+
+xrayutilities.simpack.smaterials module
+---------------------------------------
+
+.. automodule:: xrayutilities.simpack.smaterials
+    :members:
+    :undoc-members:
+    :show-inheritance:
+
+
+Module contents
+---------------
+
+.. automodule:: xrayutilities.simpack
+    :members:
+    :undoc-members:
+    :show-inheritance:
---
+++ packages/xrayutilities/xrayutilities-1.3.3.tar.gz/xrayutilities-1.3.3/xrayutilities/simpack/__init__.py
@@ -0,0 +1,44 @@
+# This file is part of xrayutilities.
+#
+# xrayutilities is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses/>.
+#
+# Copyright (C) 2016 Dominik Kriegner <dominik.kriegner@gmail.com>
+"""
+simulation subpackage of xrayutilities.
+
+This package provides possibilities to simulate X-ray diffraction and
+reflectivity curves of thin film samples. It could be extended for more
+general use in future if there is demand for that.
+
+In addition it provides a fitting routine for reflectivity data which is based
+on lmfit.
+"""
+
+from .smaterials import SMaterial, MaterialList
+from .smaterials import Layer, LayerStack
+from .smaterials import CrystalStack, GradedLayerStack
+from .smaterials import PseudomorphicStack001
+from .smaterials import PseudomorphicStack111
+
+from .models import Model, LayerModel
+from .models import KinematicalModel
+from .models import KinematicalMultiBeamModel
+from .models import SpecularReflectivityModel
+from .models import SimpleDynamicalCoplanarModel
+from .models import DynamicalModel
+
+from .fit import fit_xrr
+
+from .helpers import coplanar_alphai
+from .helpers import get_qz
