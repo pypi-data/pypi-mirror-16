6f6aa01f6918bbfed89e6ed97f0c8b0b074b0fe4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:22:31 +0000

    Add package pydmm/0.4.6/pydmm-0.4.6.zip

---
+++ packages/pydmm/pydmm-0.4.6.zip/pydmm-0.4.6/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name='pydmm',
+  packages=['pydmm'], 
+  version='0.4.6',
+  description='Read data from a digital multimeter',
+  long_description=open('README.txt').read(),
+  author='Michael Weigend',
+  author_email='mw@creative-informatics.de',
+  license='MIT-License',
+  url='http://pypi.python.org/pypi/pydmm',
+  keywords=['dmm', 'digital multimeter']
+)
