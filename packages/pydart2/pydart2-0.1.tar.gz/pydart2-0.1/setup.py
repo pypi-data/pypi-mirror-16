22251c2ea5aec1712183fe73cbb586e2c4ad802f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:20 +0000

    Add package pydart2/0.1/pydart2-0.1.tar.gz

---
+++ packages/pydart2/pydart2-0.1.tar.gz/pydart2-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pydart2',
+      version='0.1',
+      description='Python Interface for DART Simulator',
+      url='https://github.com/sehoonha/pydart2',
+      author='Sehoon Ha',
+      author_email='sehoon.ha@gmail.com',
+      license='BSD',
+      packages=['pydart2'],
+      zip_safe=False)
