a29da7ebd5ed1c804762e9c534caf984ab478deb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:54 +0000

    Add package dojo-toolkit/0.1.2/dojo-toolkit-0.1.2.tar.gz

---
+++ packages/dojo-toolkit/dojo-toolkit-0.1.2.tar.gz/dojo-toolkit-0.1.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+
+with open("requirements.txt") as reqs:
+    install_requires = reqs.readlines()
+
+
+setup(
+    name="dojo-toolkit",
+    version="0.1.2",
+    url="https://github.com/grupy-sanca/dojo-toolkit",
+
+    author="Grupy-Sanca",
+
+    description="A Toolkit for Python Coding Dojos",
+    long_description=open('README.rst').read(),
+
+    packages=find_packages(),
+
+    install_requires=install_requires,
+
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
