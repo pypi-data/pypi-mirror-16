deca0c7607d06b989fdb80d3a005395b6699bd12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:22 +0000

    Add package datavision/2016.8.4.2253/datavision-2016.8.4.2253.tar.gz

---
+++ packages/datavision/datavision-2016.8.4.2253.tar.gz/datavision-2016.8.4.2253/setup.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+import setuptools
+
+def main():
+
+    setuptools.setup(
+        name             = "datavision",
+        version          = "2016.08.04.2253",
+        description      = "Python data visualisation",
+        long_description = Markdown_to_reStructuredText("README.md"),
+        url              = "https://github.com/wdbm/datavision",
+        author           = "Will Breaden Madden",
+        author_email     = "w.bm@cern.ch",
+        license          = "GPLv3",
+        py_modules       = [
+                           "datavision"
+                           ],
+        install_requires = [
+                           "matplotlib",
+                           "numpy",
+                           "Pillow",
+                           "pygame",
+                           "pylab",
+                           "pyprel",
+                           "scipy",
+                           "shijian"
+                           ],
+        entry_points     = """
+            [console_scripts]
+            datavision = datavision:datavision
+        """
+    )
+
+def read(*paths):
+    with open(os.path.join(*paths), "r") as filename:
+        return filename.read()
+
+def Markdown_to_reStructuredText(filename):
+    try:
+        import pypandoc
+        return pypandoc.convert(filename, "rst")
+    except:
+        print("pypandoc not found; long description could be corrupted")
+        return read(filename)
+
+if __name__ == "__main__":
+    main()
