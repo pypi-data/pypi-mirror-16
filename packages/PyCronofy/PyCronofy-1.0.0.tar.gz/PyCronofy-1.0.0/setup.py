0b819a56d60bef1f84341f5b702fb676f4501d83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:29 +0000

    Add package PyCronofy/1.0.0/PyCronofy-1.0.0.tar.gz

---
+++ packages/PyCronofy/PyCronofy-1.0.0.tar.gz/PyCronofy-1.0.0/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+from distutils.core import setup
+setup(
+    name='PyCronofy',
+    version='1.0.0',
+    description='Python library for Cronofy',
+    author='Venuebook Dev Team (Dan Pozmanter et al)',
+    author_email='dev@venuebook.com',
+    url='https://github.com/venuebook/pycronofy',
+    packages=['pycronofy']
+    )
