d5c4b2e384d8b45c817a10c0f4858185b9022404
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:03:43 +0000

    Add package dateutilsmit/0.0.1/dateutilsmit-0.0.1.zip

---
+++ packages/dateutilsmit/dateutilsmit-0.0.1.zip/dateutilsmit-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+  name='dateutilsmit',
+  py_modules=['dateutilsmit'],
+  version='0.0.1',
+  description="MIT Wrapper for dateutils",
+  author='Zach Swift',
+  author_email='cras.zswift@gmail.com',
+  url='https://github.com/2achary/dateutilsmit',
+  install_requires=['dateutils==0.6.6'],
+  download_url = 'https://github.com/2achary/dateutilsmit/tarball/0.1'
+)
