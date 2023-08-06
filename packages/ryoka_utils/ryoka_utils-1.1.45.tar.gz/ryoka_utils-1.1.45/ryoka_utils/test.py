157775f323487dac544ddc4629d59e5c3f9eb123
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:11 +0000

    Add package ryoka_utils/1.1.45/ryoka_utils-1.1.45.tar.gz

---
+++ packages/ryoka_utils/ryoka_utils-1.1.45.tar.gz/ryoka_utils-1.1.45/ryoka_utils/test.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from utils import Utils
+from debugger import Debugger
+from mail import SendMail
+from slack import Slack
+
+if __name__ == '__main__':
+    u = Utils()
+
+    u.zip_tar_bz2("./icon.png")
+#    u.unzip_tar_bz2("./icon.tar.bz2")
