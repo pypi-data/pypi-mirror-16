a99ac567f8881ed932a181409d2be9ddd4175ab3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:22 +0000

    Add package abilian-sbe/0.2.16/abilian-sbe-0.2.16.tar.gz

---
+++ packages/abilian-sbe/abilian-sbe-0.2.16.tar.gz/abilian-sbe-0.2.16/demo/test.py
@@ -0,0 +1,24 @@
+#!./bin/python
+
+import subprocess
+import time
+import urllib
+
+# Some random number
+BIND = '0.0.0.0'
+PORT = 4034
+HOME = "http://{}:{}/".format(BIND, PORT)
+
+subprocess.call(['./bin/pip', 'install', 'gunicorn'])
+
+p = subprocess.Popen(["./bin/gunicorn", 'wsgi:app', '-b', '{}:{}'.format(BIND,
+                                                                         PORT)])
+
+try:
+    # Just in case
+    time.sleep(5)
+    page = urllib.urlopen(HOME).read()
+    assert "Welcome to Abilian" in page
+finally:
+    p.terminate()
+    p.wait()
