b2835a00e9e9264aaca8ca4021dabb7f4c087c26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:58 +0000

    Add package wav2image/1.0.2/wav2image-1.0.2.tar.gz

---
+++ packages/wav2image/wav2image-1.0.2.tar.gz/wav2image-1.0.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name='wav2image',
+    version='1.0.2',
+    description='Waveform generator that can generate any image type',
+    url='https://github.com/niland/wav2image',
+    author='Niland',
+    author_email='support@niland.io',
+    license='MIT',
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Topic :: Multimedia :: Sound/Audio :: Analysis',
+        'License :: OSI Approved :: MIT License'
+    ],
+    keywords='waveform svg png wav',
+    install_requires=['matplotlib', 'numpy', 'scipy'],
+    packages=['wav2image'],
+    entry_points={
+        'console_scripts': [
+            'wav2image=wav2image:main',
+        ]
+    }
+)
