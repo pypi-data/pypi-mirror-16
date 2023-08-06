c1ad5b037d7651c328fef04b414496964bc83ab9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:30 +0000

    Add package picard_metrics_sqlite/0.5/picard_metrics_sqlite-0.5.tar.gz

---
+++ packages/picard_metrics_sqlite/picard_metrics_sqlite-0.5.tar.gz/picard_metrics_sqlite-0.5/picard_metrics_sqlite/metrics/picard_collectoxogmetrics.py
@@ -0,0 +1,32 @@
+import os
+
+import .metrics_util
+
+def picard_CollectOxoGMetrics_to_df(stats_path, logger):
+    select = 'SAMPLE_ALIAS'
+    df = metrics_util.picard_select_tsv_to_df(stats_path, select, logger)
+    return df
+
+
+def run(uuid, stats_path, bam, fasta, vcf, input_state, engine, logger, metric_name):
+    stats_dir = os.path.dirname(stats_path)
+    stats_name = os.path.basename(stats_path)
+    stats_base, stats_ext = os.path.splitext(stats_name)
+
+    df_list = list()
+    table_name_list = list()
+
+    table_name = 'picard_' + metric_name
+    df = picard_CollectOxoGMetrics_to_df(stats_path, logger)
+    if df is not None:
+        df_list.append(df)
+        table_name_list.append(table_name)
+    for i, df in enumerate(df_list):
+        df['uuid'] = uuid
+        df['bam'] = bam
+        df['input_state'] = input_state
+        df['fasta'] = fasta
+        df['vcf'] = vcf
+        table_name = table_name_list[i]
+        df.to_sql(table_name, engine, if_exists='append')
+    return
