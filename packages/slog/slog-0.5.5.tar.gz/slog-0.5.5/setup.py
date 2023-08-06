1f39646eaffeb5b3b773401a4bb0f1a6fc4231c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:03:56 +0000

    Add package slog/0.5.5/slog-0.5.5.tar.gz

---
+++ packages/slog/slog-0.5.5.tar.gz/slog-0.5.5/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+        name = 'slog',
+        packages = ['slog'],
+        version = '0.5.5',
+        description = 'A simple logging library',
+        install_requires = ['termcolor'],
+        license = 'MIT',
+        author = 'Jules Mazur',
+        author_email = 'julesmazur [ at ] gmail [ dot ] com',
+        url = 'https://github.com/verandaguy/slog',
+        download_url = 'https://raw.githubusercontent.com/verandaguy/slog/master/slog.py',
+        keywords = ['logging', 'log', 'simple'],
+        classifiers = ['Development Status :: 4 - Beta', 'Environment :: Console', 'Intended Audience :: Developers', 'Natural Language :: English', 'Operating System :: POSIX', 'Programming Language :: Python :: 2','Topic :: Software Development :: Bug Tracking', 'Topic :: Software Development :: Quality Assurance', 'Topic :: Software Development :: Testing', 'Topic :: System :: Logging', 'Topic :: Terminals', 'Topic :: Utilities'])
+
