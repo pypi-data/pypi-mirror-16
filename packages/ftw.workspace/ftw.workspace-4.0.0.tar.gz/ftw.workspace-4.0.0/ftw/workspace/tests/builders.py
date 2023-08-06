6440dc68d0f1edce335aabdd952e1cfc24b27bbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:34 +0000

    Add package ftw.workspace/4.0.0/ftw.workspace-4.0.0.tar.gz

---
+++ packages/ftw.workspace/ftw.workspace-4.0.0.tar.gz/ftw.workspace-4.0.0/ftw/workspace/tests/builders.py
@@ -0,0 +1,15 @@
+from ftw.builder import builder_registry
+from ftw.builder.archetypes import ArchetypesBuilder
+import egov.contactdirectory.tests.builders
+
+
+class WorkspaceBuilder(ArchetypesBuilder):
+    portal_type = 'Workspace'
+
+
+class TabbedViewFolderBuilder(ArchetypesBuilder):
+    portal_type = 'TabbedViewFolder'
+
+
+builder_registry.register('workspace', WorkspaceBuilder)
+builder_registry.register('TabbedViewFolder', TabbedViewFolderBuilder)
