a851dc645bce01600d8083eb0cd397c5fb2d6823
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:45 +0000

    Add package consistent-hash-git/0.3/consistent_hash_git-0.3.tar.gz

---
+++ packages/consistent-hash-git/consistent_hash_git-0.3.tar.gz/consistent_hash_git-0.3/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# Copyright (c) 2012 Yummy Bian <yummy.bian#gmail.com>.
+
+import os
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+def list_files(path):
+    for fn in os.listdir(path):
+        if fn.startswith('.'):
+            continue
+        fn = os.path.join(path, fn)
+        if os.path.isfile(fn):
+            yield fn
+
+setup(
+    name='consistent_hash_git',
+    version='0.3',
+    author="Yummy Bian",
+    author_email="yummy.bian@gmail.com",
+    url="https://github.com/yummybian",
+    packages=['consistent_hash'],
+    platforms=["Any"],
+    license="BSD",
+    keywords='consistent hash hashing',
+    description="Implements consistent hashing with Python.",
+)
