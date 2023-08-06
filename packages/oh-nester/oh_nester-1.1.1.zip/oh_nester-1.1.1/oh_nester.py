e7f9145640bc91a50d804a0039a188272c865d25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:10 +0000

    Add package oh-nester/1.1.1/oh_nester-1.1.1.zip

---
+++ packages/oh-nester/oh_nester-1.1.1.zip/oh_nester-1.1.1/oh_nester.py
@@ -0,0 +1,11 @@
+def print_lol(one_list,indent=False,level=0):
+	for i in one_list:
+		if(indent == true):
+			if isinstance(i,list):
+				print_lol(i,True,level+1)
+			else:
+					for _ in range(level):
+						print("\t",end=" ")
+					print(i)
+		else:
+			print(i);
