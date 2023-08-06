7e138b194980f2869b12759050a8011db6394289
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:57:49 +0000

    Add package srktester/1.1.0/srktester-1.1.0.zip

---
+++ packages/srktester/srktester-1.1.0.zip/srktester-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name            = 'srktester',
+        version         = '1.1.0',
+        py_modules      = ['srktester'],
+        author          = 'TEST',
+        author_email    = 'sdfdsf@sdf.com',
+        url             = 'http://www.google.com',
+        description     = 'A simple printer of nested lists',
+    )
