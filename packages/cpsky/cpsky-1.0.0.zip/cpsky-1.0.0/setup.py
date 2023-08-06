1bdfe93aabce59d3fbcaf54e5c03f11a6394ca87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:45 +0000

    Add package cpsky/1.0.0/cpsky-1.0.0.zip

---
+++ packages/cpsky/cpsky-1.0.0.zip/cpsky-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name          ='cpsky',
+        version       ='1.0.0',
+        py_modules    =['cpsky'],
+        author        ='Insist',
+        author_email  ='1013837236@qq.com',
+        url           ='http://www.headfirstlabs.com',
+        description   ='A simple printer of nested lists',
+    )
