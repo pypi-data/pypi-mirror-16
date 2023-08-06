e785db2b0ffab266fe0358d26399780a7e2f77ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:19:13 +0000

    Add package pilas/1.4.7/pilas-1.4.7.tar.gz

---
+++ packages/pilas/pilas-1.4.7.tar.gz/pilas-1.4.7/data/asistente/js/version.js
@@ -0,0 +1,7 @@
+
+window.DESCRIPCION_VERSION = {
+  version: '1.4.7',
+  fecha: '28 de Jul del 2016',
+  commit: 'bf6719e',
+  changelog: ['Agregando un indicador de cantidad de actores closes #289', 'release 1.4.6', 'Merge pull request #288 from Robots-Linti/feature-distancia-entre-dos-puntos', 'distancia_entre_dos_puntos() y distancia_entre_dos_actores() restaurado desde la versión legacy para que funcione Actor.distancia_con(), obtener_angulo_entre() agregado a la clase Utils para que quede consistente con el uso de distancia_*', 'Eliminando el archivo temporal del editor al salir closes #287', ]
+}
