cc8618295c20a6cb4eafc06e567c4a7005206bc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:33 +0000

    Add package typecast/0.3.0/typecast-0.3.0.tar.gz

---
+++ packages/typecast/typecast-0.3.0.tar.gz/typecast-0.3.0/typecast/name.py
@@ -0,0 +1,26 @@
+import six
+from datetime import datetime, date
+from decimal import Decimal
+
+
+TESTS = ((six.text_type, 'string'),
+         (bool, 'bool'),
+         (int, 'integer'),
+         (float, 'float'),
+         (date, 'date'),
+         (datetime, 'datetime'),
+         (Decimal, 'decimal'))
+
+
+def name(value):
+    """Get the string title for a particular type.
+
+    Given a value, get an appropriate string title for the type that can
+    be used to re-cast the value later.
+    """
+    if value is None:
+        return 'any'
+    for (test, name) in TESTS:
+        if isinstance(value, test):
+            return name
+    return 'string'
