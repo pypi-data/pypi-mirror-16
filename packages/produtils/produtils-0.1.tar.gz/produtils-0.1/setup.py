bc6a249e10a6959c569f3a55a39121eef2609eea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:36 +0000

    Add package produtils/0.1/produtils-0.1.tar.gz

---
+++ packages/produtils/produtils-0.1.tar.gz/produtils-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='produtils',
+      version='0.1',
+      description='A nifty library of productivity tools',
+      url='https://bitbucket.org/mukundraj/produtils',
+      author='Mukund Raj',
+      author_email='mukundraj2@gmail.com',
+      license='MIT',
+      packages=['produtils'],
+      zip_safe=False)
