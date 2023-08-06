ad78b74bddab4474c4559c6bc721e556582cd508
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:19 +0000

    Add package vacuum/0.3/vacuum-0.3.tar.gz

---
+++ packages/vacuum/vacuum-0.3.tar.gz/vacuum-0.3/vacuum/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+#coding:utf-8
+# Author        : tuxpy
+# Email         : q8886888@qq.com.com
+# Last modified : 2016-07-12 15:39:15
+# Filename      : __init__.py
+# Description   : 
+from __future__ import print_function, unicode_literals
