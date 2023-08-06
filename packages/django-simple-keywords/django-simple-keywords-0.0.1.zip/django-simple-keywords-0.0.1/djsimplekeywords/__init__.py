3b69ff46b1c43391e6d23e1a2e2813d7079de724
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:51 +0000

    Add package django-simple-keywords/0.0.1/django-simple-keywords-0.0.1.zip

---
+++ packages/django-simple-keywords/django-simple-keywords-0.0.1.zip/django-simple-keywords-0.0.1/djsimplekeywords/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: UTF-8 -*-
+# lvjiyong on 2016/7/24.
+
---
+++ packages/django-simple-keywords/django-simple-keywords-0.0.1.zip/django-simple-keywords-0.0.1/setup.py
@@ -0,0 +1,29 @@
+# -*- coding: UTF-8 -*-
+
+from os.path import dirname, join
+
+from setuptools import setup, find_packages
+
+with open(join(dirname(__file__), 'VERSION'), 'rb') as f:
+    version = f.read().decode('ascii').strip()
+
+APP_NAME = 'django-simple-keywords'
+
+setup(
+    name=APP_NAME,
+    version=version,
+    description=APP_NAME,
+    author="lvjiyong",
+    author_email='lvjiyong',
+    url="https://github.com/lvjiyong/%s" % APP_NAME,
+    license="Apache2.0",
+    long_description=open('README.md').read(),
+    maintainer='lvjiyong',
+    platforms=["any"],
+    maintainer_email='lvjiyong@gmail.com',
+    include_package_data=True,
+    packages=find_packages(exclude=('tests', 'docs')),
+    install_requires=[
+       
+    ],
+)
