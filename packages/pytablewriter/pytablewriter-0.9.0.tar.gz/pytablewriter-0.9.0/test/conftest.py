c428d1d215986d87214ccfafd531c015307cc3f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:38 +0000

    Add package pytablewriter/0.9.0/pytablewriter-0.9.0.tar.gz

---
+++ packages/pytablewriter/pytablewriter-0.9.0.tar.gz/pytablewriter-0.9.0/test/conftest.py
@@ -0,0 +1,13 @@
+# encoding: utf-8
+
+"""
+.. codeauthor:: Tsuyoshi Hombashi <gogogo.vm@gmail.com>
+"""
+
+
+float_header_list = ["a", "b", "c"]
+float_value_matrix = [
+    0.1, 9.9,   0.0,
+    1.0, 99.9,  0.01,
+    1.2, 999.9, 0.001,
+]
