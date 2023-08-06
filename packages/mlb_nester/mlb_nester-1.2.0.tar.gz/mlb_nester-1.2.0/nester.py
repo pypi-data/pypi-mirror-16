86fe55f2d7f78d8bd7b6f7547dd86e788c57354a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:32 +0000

    Add package mlb_nester/1.2.0/mlb_nester-1.2.0.tar.gz

---
+++ packages/mlb_nester/mlb_nester-1.2.0.tar.gz/mlb_nester-1.2.0/nester.py
@@ -0,0 +1,15 @@
+"""A module with utilities to help with nested item iteration"""
+def print_lol(the_list, level=0):
+    """print_lol is used to iterate through a list to be printed, and ensure that
+        any items involved, if lists themselves, are also iterated through for proper 
+        text formatting of list items within that list.  This function uses recursions.
+        it additionally has a second argument called level, which is used to insert
+        tab stops when a nested list is encountered"""
+    for list_item in the_list:
+        if isinstance(list_item,list):
+            print_lol(list_item, level+1)
+        else:
+            for tab_stop in range(level):
+                print("\t", end="")
+            print(list_item)
+            
