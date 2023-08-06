ea45d3ce79c3a3dd58c73c4abef2dfc46712eb3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:18 +0000

    Add package ascii/1.1/ascii-1.1.zip

---
+++ packages/ascii/ascii-1.1.zip/ascii-1.1/ascii/asciify.py
@@ -0,0 +1,15 @@
+import math
+pixels = ".,:;i1tfLCG08@"
+
+import colorize
+
+def asciify(r,g,b,a):
+	pix = Pixel(r,g,b,a)
+
+	#
+	value = pix.intensity()
+	precision = 255 * 3 / (len(pixels) -1)
+	rawChar = pixels[int(round(value / precision))]
+	#
+
+	return colorize.colorize(rawChar, (r,g,b))
