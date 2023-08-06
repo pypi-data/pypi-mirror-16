bfd58ffe4821787d6793b2873334e6d15cb88b81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:19 +0000

    Add package microcosm-logging/0.8.0/microcosm-logging-0.8.0.tar.gz

---
+++ packages/microcosm-logging/microcosm-logging-0.8.0.tar.gz/microcosm-logging-0.8.0/microcosm_logging/decorators.py
@@ -0,0 +1,17 @@
+"""Decorator library for common logging functionality."""
+from logging import getLogger
+
+
+def logger(obj):
+    """
+    logging decorator, assigning an object the `logger` property.
+    Can be used on a Python class, e.g:
+
+        @logger
+        class MyClass(object):
+            ...
+
+
+    """
+    obj.logger = getLogger(obj.__name__)
+    return obj
