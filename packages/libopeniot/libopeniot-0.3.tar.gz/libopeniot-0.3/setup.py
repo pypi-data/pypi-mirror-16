077acdeae2ae271b5eb6493e48ca3d3a4020bafd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:04 +0000

    Add package libopeniot/0.3/libopeniot-0.3.tar.gz

---
+++ packages/libopeniot/libopeniot-0.3.tar.gz/libopeniot-0.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='libopeniot',
+      version='0.3',
+      description='OpenIoT Library',
+      url='http://www.openiot.org.ng',
+      author='Ahmad Sadiq',
+      author_email='sadiq.a.ahmad@gmail.com',
+      license='BSD',
+      packages=['libopeniot'],
+      zip_safe=False)
