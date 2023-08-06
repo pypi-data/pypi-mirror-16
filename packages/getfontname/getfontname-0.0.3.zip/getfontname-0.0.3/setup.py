4f267fbbbebac9afa8c7909ebf9a9a6efe918c93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:09 +0000

    Add package getfontname/0.0.3/getfontname-0.0.3.zip

---
+++ packages/getfontname/getfontname-0.0.3.zip/getfontname-0.0.3/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup,find_packages
+
+setup(name = 'getfontname',
+      version = '0.0.3',
+      description = 'get font name from the ttf/ttc font file.',
+      author = 'Chen MuSheng',
+      author_email = 'sheng.2179@gmail.com',
+      url = 'https://github.com/mu2019/getfontname/',
+      license='MIT',
+      platforms = 'any',
+      packages=['getfontname']
+      )
+      
