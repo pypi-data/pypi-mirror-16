d93b0f7a1d450655b67ace72f9d094ffbfd4c005
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:04 +0000

    Add package pescador/0.1.3/pescador-0.1.3.tar.gz

---
+++ packages/pescador/pescador-0.1.3.tar.gz/pescador-0.1.3/pescador/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+'''Pescador streaming learning'''
+
+from .util import *
+from .zmq_stream import *
+from .pescador import *
+
+from .version import version as __version__
---
+++ packages/pescador/pescador-0.1.3.tar.gz/pescador-0.1.3/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+
+import imp
+
+version = imp.load_source('pescador.version', 'pescador/version.py')
+
+setup(
+    name='pescador',
+    version=version.version,
+    description='Multiplex generators for incremental learning',
+    author='Pescador developers',
+    author_email='brian.mcfee@nyu.edu',
+    url='http://github.com/bmcfee/pescador',
+    download_url='http://github.com/bmcfee/pescador/releases',
+    packages=find_packages(),
+    long_description='Multiplex generators for incremental learning',
+    classifiers=[
+        "License :: OSI Approved :: ISC License (ISCL)",
+        "Programming Language :: Python",
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Multimedia :: Sound/Audio :: Analysis",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+    ],
+    keywords='machine learning',
+    license='ISC',
+    install_requires=[
+        'joblib>=0.9',
+        'six',
+        'pyzmq',
+        'numpy',
+        'scipy',
+        'scikit-learn',
+    ],
+    extras_require={
+        'docs': ['numpydoc']
+    }
+)
