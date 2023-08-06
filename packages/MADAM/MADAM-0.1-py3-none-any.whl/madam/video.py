6c9431c6f8f0e8222c6ea5b459f11b558ba0f72e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:16:22 +0000

    Add package MADAM/0.1/MADAM-0.1-py3-none-any.whl

---
+++ packages/MADAM/MADAM-0.1-py3-none-any.whl/madam/video.py
@@ -0,0 +1,30 @@
+import io
+import json
+import subprocess
+
+from madam.core import Asset, Processor
+
+
+class FFmpegProcessor(Processor):
+    """
+    Represents a processor that uses FFmpeg to read audio and video data.
+    """
+    def can_read(self, file):
+        ffprobe_cmd = 'ffprobe -print_format json -show_format -loglevel quiet -'
+        with subprocess.Popen(ffprobe_cmd.split(), stdin=subprocess.PIPE,
+                              stdout=subprocess.PIPE) as process:
+            try:
+                stdout, stderr = process.communicate(input=file.read())
+            except:
+                process.kill()
+                process.wait()
+                raise ValueError('Error when reading file-like object: %r' % file)
+            retcode = process.wait()
+            if retcode:
+                return False
+        string_result = stdout.decode('utf-8')
+        json_obj = json.loads(string_result)
+        return bool(json_obj)
+
+    def read(self, file):
+        return Asset(io.BytesIO(b''))
