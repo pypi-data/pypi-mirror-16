b5ce9257d4c71ab0fa680b6128b8cad4f0fa33ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:06:42 +0000

    Add package json-schema-reducer/0.1.4/json-schema-reducer-0.1.4.tar.gz

---
+++ packages/json-schema-reducer/json-schema-reducer-0.1.4.tar.gz/json-schema-reducer-0.1.4/setup.py
@@ -0,0 +1,29 @@
+import os
+import setuptools
+
+
+_here = os.path.dirname(__file__)
+
+
+setuptools.setup(
+    name='json-schema-reducer',
+    version='0.1.4',
+    description='Extract from a JSON/dict only whats in the JSON Schema',
+    long_description=open(os.path.join(_here, 'README.rst')).read(),
+    author='Peter Bengtsson',
+    author_email='mail@peterbe.com',
+    license='MIT',
+    py_modules=['json_schema_reducer'],
+    url='https://github.com/peterbe/json-schema-reducer',
+    include_package_data=False,
+    install_requires=[],
+    zip_safe=False,
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest', 'pytest-mock'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+    ],
+)
