f0ab4ecedac7d156d3f260e9a22c64e5b059cd07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:55 +0000

    Add package hls2dash/0.1.0/hls2dash-0.1.0.tar.gz

---
+++ packages/hls2dash/hls2dash-0.1.0.tar.gz/hls2dash-0.1.0/setup.py
@@ -0,0 +1,33 @@
+from os.path import dirname, abspath, join, exists
+from setuptools import setup
+
+try:
+    from pypandoc import convert
+    read_md = lambda f: convert(f, 'rst')
+except ImportError:
+    print("warning: pypandoc module not found, could not convert Markdown to RST")
+    read_md = lambda f: open(f, 'r').read()
+
+long_description = None
+if exists("README.md"):
+    long_description = read_md("README.md")
+
+install_reqs = [req for req in open(abspath(join(dirname(__file__), 'requirements.txt')))]
+
+setup(
+    name = "hls2dash",
+    version = "0.1.0",
+    author = "Jonas Birme",
+    author_email = "jonas.birme@eyevinn.se",
+    description = "Command line tools for HLS to MPEG DASH repackaging",
+    long_description=long_description,
+    license = "MIT",
+    install_requires=install_reqs,
+    url = "https://github.com/Eyevinn/hls-to-dash",
+    packages = ['hls2dash', 'hls2dash/lib'],
+    entry_points = {
+        'console_scripts': [
+            'hls-to-dash=hls2dash:main'
+        ]
+    }
+)
