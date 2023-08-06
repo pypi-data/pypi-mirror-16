965e4f359db857ee4e8a396fbea67863ffb2e04b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:40:02 +0000

    Add package testerv24/1.1.0/testerv24-1.1.0.zip

---
+++ packages/testerv24/testerv24-1.1.0.zip/testerv24-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name = 'testerv24',
+        version = '1.1.0',
+        py_modules = ['testerv24'], #Associate your module's metadata with the setup function arguments.
+        author = 'varun24143py',
+        author_email='varun24143@gmail.com',
+        description='A simple printer of nested lists',
+    )
+
