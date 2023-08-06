c57dda151af3056e4d6520481b4b44cb91f16ae4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:00 +0000

    Add package maester/0.1.9/maester-0.1.9.tar.gz

---
+++ packages/maester/maester-0.1.9.tar.gz/maester-0.1.9/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+  name = 'maester',
+  packages = ['maester'], 
+  version = '0.1.9',
+  description = 'A platform for running tests on multiple Android devices in series',
+  author = 'John James Gallagher & Joseph Thomas Campbell',
+  author_email = 'jcamp1095@gmail.com, johny.gallagher@gmail.com',
+  url = 'https://github.com/JohnyGall/Maester', 
+  download_url = 'https://github.com/JohnyGall/Maester/tarball/0.1.9', 
+  keywords = ['testing', 'android', 'automation', 'appium', 'multiple', 'series', 'unittest', 'test', 
+  'debug', 'pytest', 'qa', 'app', 'maester', 'debuging'], 
+  classifiers = [],
+)
