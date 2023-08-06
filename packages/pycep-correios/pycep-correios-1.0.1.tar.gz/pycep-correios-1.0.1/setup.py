0bee4d24a561b4d9abc63a703f4635f686191ec8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:02 +0000

    Add package pycep-correios/1.0.1/pycep-correios-1.0.1.tar.gz

---
+++ packages/pycep-correios/pycep-correios-1.0.1.tar.gz/pycep-correios-1.0.1/setup.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+# #############################################################################
+# The MIT License (MIT)
+#
+# Copyright (c) 2016 Michell Stuttgart
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# #############################################################################
+
+from setuptools import setup, find_packages
+
+setup(
+    name='pycep-correios',
+    version='1.0.1',
+    keywords='correios setuptools development cep',
+    packages=find_packages(),
+    url='https://github.com/mstuttgart/pycep-correios',
+    license='MIT',
+    author='Michell Stuttgart',
+    author_email='michellstut@gmail.com',
+    description='Método para busca de dados de CEP no webservice dos '
+                'Correios',
+    install_requires=[
+        'requests',
+    ],
+    test_suite='test',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.4',
+    ],
+)
