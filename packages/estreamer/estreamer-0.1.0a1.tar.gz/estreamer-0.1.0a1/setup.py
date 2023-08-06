6e262c7767db49f86711e61f69a3329ccd6bf45f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:15 +0000

    Add package estreamer/0.1.0a1/estreamer-0.1.0a1.tar.gz

---
+++ packages/estreamer/estreamer-0.1.0a1.tar.gz/estreamer-0.1.0a1/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+import re
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+def read(*parts):
+    return codecs.open(os.path.join(here, *parts), 'r').read()
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+long_description = read('README.rst')
+
+setup(
+    name='estreamer',
+    version=find_version('estreamer', '__init__.py'),
+    description='SourceFire eStream client Python library',
+    long_description=long_description,
+    url='https://github.com/spohara79/estreamer/',
+    author="Sean O'Hara",
+    author_email='spohara@gmail.com',
+    license='Apache Software License',
+    zip_safe=False,
+    packages=['estreamer'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+    ],
+
+    keywords='sample setuptools development',
+    install_requires=['pyOpenSSL>=0.15.1'],
+)
