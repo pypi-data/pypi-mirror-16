1d27d0aae98cbf022c82dbae9e29637128b8e2f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:57:50 +0000

    Add package srktester/1.3.0/srktester-1.3.0.zip

---
+++ packages/srktester/srktester-1.3.0.zip/srktester-1.3.0/srktester.py
@@ -0,0 +1,14 @@
+"""Sample test function"""
+def print_lol(the_list, indent = False, level = 0):
+    """Prints all the elments in the list including that of inner lists.
+    To indent inner list set indent = true
+    and set starting level of indent by setting the level.
+    """
+    for each_item in the_list:
+        if isinstance(each_item, list):
+            print_lol(each_item,level + 1)
+        else:
+            if indent == True:
+                for tab_stop in range(level):                
+                    print("\t",end='')
+            print(each_item)
