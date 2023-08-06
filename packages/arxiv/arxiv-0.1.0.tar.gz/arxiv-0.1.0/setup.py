26174f6af2836616ead923801c738562114f6f3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:57 +0000

    Add package arxiv/0.1.0/arxiv-0.1.0.tar.gz

---
+++ packages/arxiv/arxiv-0.1.0.tar.gz/arxiv-0.1.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+	name="arxiv",
+	version="0.1.0",
+	packages=["arxiv"],
+
+	# dependencies
+	install_requires=['feedparser'],
+
+	# metadata for upload to PyPI
+	author="Lukas Schwab",
+	author_email="lukas.schwab@gmail.com",
+	description="Python wrapper for the arXiv API: http://arxiv.org/help/api/",
+	license="MIT",
+	keywords="arxiv api wrapper academic journals papers",
+	url="https://github.com/lukasschwab/arxiv.py",
+	download_url="https://github.com/lukasschwab/arxiv.py/tarball/0.1.0",
+)
