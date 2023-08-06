5184d52ba98cbd7b679a33cb7a55c7381382f0ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:19 +0000

    Add package ggplot/0.10.8/ggplot-0.10.8-py2.py3-none-any.whl

---
+++ packages/ggplot/ggplot-0.10.8-py2.py3-none-any.whl/ggplot/geoms/geom_hline.py
@@ -0,0 +1,33 @@
+from .geom import geom
+from ..utils import is_iterable
+
+class geom_hline(geom):
+    """
+    Horizontal line(s)
+
+    color:
+        color of the line
+    linetype:
+        type of the line ('solid', 'dashed', 'dashdot', 'dotted')
+    size:
+        thickness of the line
+
+    Examples
+    --------
+    """
+    DEFAULT_AES = {'color': 'black', 'linetype': 'solid',
+                   'size': 1.0,}
+    REQUIRED_AES = {}
+    DEFAULT_PARAMS = {}
+
+    _aes_renames = {'size': 'linewidth', 'linetype': 'linestyle'}
+
+    def plot(self, ax, data, _aes):
+        params = self._get_plot_args(data, _aes)
+        variables = _aes.data
+        y = self.params.get('y')
+        if is_iterable(y):
+            for yi in y:
+                ax.axhline(yi, **params)
+        else:
+            ax.axhline(y, **params)
