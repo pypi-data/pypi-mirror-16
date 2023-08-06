dff1bd681e6c22baf8c4b210ddeddce1f5c1b4e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:14:59 +0000

    Add package rabird.core/0.3.9/rabird.core-0.3.9.zip

---
+++ packages/rabird.core/rabird.core-0.3.9.zip/rabird.core-0.3.9/src/rabird/core/__init__.py
@@ -0,0 +1,24 @@
+
+import sys
+
+version_info = (0, 3, 9)
+__version__ = '.'.join(map(str, version_info))
+__monkey_patched = False
+
+# Known Issues : Can't work with eventlet, Why?
+
+
+def monkey_patch():
+    global __monkey_patched
+
+    if __monkey_patched:
+        return
+
+    if sys.platform == 'win32':
+        from . import windows_api
+        from . import windows_fix
+
+        if sys.version_info[0] <= 2:
+            windows_fix.monkey_patch()
+
+        __monkey_patched = True
