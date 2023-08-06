84ab49849cad41789b4bc5799174653f9e7e3f24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:36 +0000

    Add package nester1990xing/1.2.5/nester1990xing-1.2.5.zip

---
+++ packages/nester1990xing/nester1990xing-1.2.5.zip/nester1990xing-1.2.5/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+
+setup( 
+	name		='nester1990xing',
+	version		='1.2.5',
+	py_modules	=['nester1990xing'],
+	author		='hfpython',
+	author_email	='649963254@qq.com',
+	url		='http://www.hradfirstlabs.com',
+	description	='A simple printer of nested lists',
+)
