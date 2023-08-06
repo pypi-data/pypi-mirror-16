299957bf038b1b4f27341bbd9b15079bb029b321
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:05:57 +0000

    Add package zhihu-oauth/0.0.15/zhihu_oauth-0.0.15.tar.gz

---
+++ packages/zhihu-oauth/zhihu_oauth-0.0.15.tar.gz/zhihu_oauth-0.0.15/zhihu_oauth/__init__.py
@@ -0,0 +1,14 @@
+# coding=utf-8
+
+from .client import ZhihuClient
+from .zhcls import (
+    Activity, ActType, Answer, Article, Comment, Column, Collection, People,
+    Question, Topic, ANONYMOUS
+)
+from .exception import NeedCaptchaException, UnexpectedResponseException
+
+__all__ = ['ZhihuClient', 'ANONYMOUS', 'Activity', 'ActType', 'Article',
+           'Answer', 'Collection', 'Column', 'Comment', 'People', 'Question',
+           'Topic', 'NeedCaptchaException', 'UnexpectedResponseException']
+
+__version__ = '0.0.15'
