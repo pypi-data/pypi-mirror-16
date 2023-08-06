5d5a30d430f14d2d11a411f27acea815fe952535
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:05:04 +0000

    Add package tinyblend/1.0.0/tinyblend-1.0.0.zip

---
+++ packages/tinyblend/tinyblend-1.0.0.zip/tinyblend-1.0.0/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+from sys import argv
+
+setup(name='tinyblend',
+      version='1.0.0',
+      description='A tiny blender file loader',
+      author='Gabriel Dubé',
+      author_email='gdube.475@gmail.com',
+      license='MIT',
+      url='https://github.com/gabdube/tinyblend',
+      download_url='https://github.com/gabdube/tinyblend',
+      keywords = ['game', '3d', 'blender', 'import'],
+      py_modules=['tinyblend'],
+     )
