b811c06763a0c6ec16b01028e24ff4d26a048510
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:32 +0000

    Add package tabler/1.0.02/tabler-1.0.02.zip

---
+++ packages/tabler/tabler-1.0.02.zip/tabler-1.0.02/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='tabler',
+    version='1.0.02',
+    description='Simple interface for tabulated data and .csv files',
+    author='Luke Shiner',
+    author_email='luke@lukeshiner.com',
+    url='http://tabler.lukeshiner.com',
+    keywords=['table', 'csv', 'simple'],
+    install_requires=['requests'],
+    packages=['tabler'])
