c1d482b4e3baa1c3c63e2d55496bb897d7740980
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:24 +0000

    Add package k_index_calculator/0.1/k_index_calculator-0.1.tar.gz

---
+++ packages/k_index_calculator/k_index_calculator-0.1.tar.gz/k_index_calculator-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='k_index_calculator',
+      version='0.1',
+      description='Python module which calculates the K-Index of a geomagnetic time series.',
+      url='https://github.com/TerminusEst/k_index_calculator',
+      author='Sean Blake',
+      author_email='blakese@tcd.ie',
+      license='MIT',
+      packages=['k_index_calculator'],
+      zip_safe=False)
