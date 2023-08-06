73cedbf3804edf10fe26e7a5e6467e8e6e9cea21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:54 +0000

    Add package cv_utils/0.2.0/cv_utils-0.2.0.tar.gz

---
+++ packages/cv_utils/cv_utils-0.2.0.tar.gz/cv_utils-0.2.0/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+
+setup(
+    name='cv_utils',
+    packages=['cv_utils'],
+    version='0.2.0',
+    description='Computer Vision or OpenCV python utility functions. '
+                'It also includes basic utilities for image processing and '
+                'computer vision related tasks.',
+    author='Michael Jaison Gnanasekar, Shreyas Joshi',
+    author_email='gmichaeljaison@gmail.com, shreyasvj25@gmail.com',
+    url='https://github.com/gmichaeljaison/cv-utils',
+    download_url='https://github.com/gmichaeljaison/cv-utils/tarball/0.1',
+    keywords=['computer vision', 'image processing', 'utility', 'template matching', 'deep networks'],
+    classifiers=[
+        "Topic :: Utilities"
+    ],
+    requires=['numpy', 'cv2', 'matplotlib', 'scipy', 'skimage'],
+    license='LICENSE.txt'
+)
