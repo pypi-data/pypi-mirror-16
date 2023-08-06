d093537fa005ac31a31116de6f1fbfe75276cf81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:24 +0000

    Add package dnszonetest/0.3.7/dnszonetest-0.3.7.tar.gz

---
+++ packages/dnszonetest/dnszonetest-0.3.7.tar.gz/dnszonetest-0.3.7/README.rst
@@ -0,0 +1,31 @@
+=============
+DNS Zone Test
+=============
+
+.. image:: https://img.shields.io/pypi/v/dnszonetest.svg
+        :target: https://pypi.python.org/pypi/dnszonetest
+
+.. image:: https://img.shields.io/travis/maartenq/dnszonetest.svg
+        :target: https://travis-ci.org/maartenq/dnszonetest
+
+.. image:: https://readthedocs.org/projects/dnszonetest/badge/?version=latest
+        :target: https://dnszonetest.readthedocs.io/en/latest/?badge=latest
+        :alt: Documentation Status
+
+.. image:: https://pyup.io/repos/github/maartenq/dnszonetest/shield.svg
+     :target: https://pyup.io/repos/github/maartenq/dnszonetest/
+     :alt: Updates
+
+.. image:: https://codecov.io/gh/maartenq/dnszonetest/branch/master/graph/badge.svg
+    :target: https://codecov.io/gh/maartenq/dnszonetest
+
+
+Dns Zone Test tests a DNS zone file agaist a given name server.
+
+* Free software: https://opensource.org/licenses/ISC
+* Documentation: https://dnszonetest.readthedocs.org
+* PyPi: https://pypi.python.org/pypi/dnszonetest
+* GitHub: https://github.com/maartenq/dnszonetest
+* Travis CI: https://travis-ci.org/maartenq/dnszonetest
+* Codecov: https://codecov.io/github/maartenq/dnszonetest
+
---
+++ packages/dnszonetest/dnszonetest-0.3.7.tar.gz/dnszonetest-0.3.7/setup.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+# setup.py
+# -*- coding: utf-8 -*-
+# vim: ai et ts=4 sw=4 sts=4 fenc=UTF-8 ft=python
+
+import sys
+from setuptools import setup
+from setuptools.command.test import test as TestCommand
+
+
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def run_tests(self):
+        # Import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
+
+
+version = '0.3.7'
+
+if sys.version_info < (3,):
+    dnspython = 'dnspython'
+else:
+    dnspython = 'dnspython3'
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = [
+    'dnspython==1.14.0',
+]
+
+# Add Python 2.6-specific dependencies
+if sys.version_info[:2] < (2, 7):
+        requirements.append('argparse')
+
+test_requirements = [
+    'pytest==3.0.0',
+    'pytest-cov==2.3.1',
+    'tox==2.3.1',
+]
+
+
+setup(
+    name='dnszonetest',
+    use_scm_version=False,
+    version=version,
+    description='DNS Zone Test tests a DNS zone file agaist a given name '
+    'server.',
+    long_description=readme + '\n\n' + history,
+    author='Maarten Diemel',
+    author_email='maarten@maartendiemel.nl',
+    url='https://github.com/maartenq/dnszonetest',
+    packages=[
+        'dnszonetest',
+    ],
+    package_dir={'dnszonetest':
+                 'dnszonetest'},
+    entry_points={
+        'console_scripts': [
+            'dnszonetest = dnszonetest.cli:main',
+        ]
+    },
+    include_package_data=True,
+    install_requires=requirements,
+    license='ISCL',
+    zip_safe=False,
+    keywords='dnszonetest',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: ISC License (ISCL)',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    test_suite='tests',
+    tests_require=test_requirements,
+    cmdclass={'test': PyTest},
+)
