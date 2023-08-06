ad52cc6dd8470a1a88fad7de70cd36c73e4e6f9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:07 +0000

    Add package stackdriver/0.1.1/stackdriver-0.1.1.tar.gz

---
+++ packages/stackdriver/stackdriver-0.1.1.tar.gz/stackdriver-0.1.1/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+setup(
+    name='stackdriver',
+    version='0.1.1',
+    author='Mihir Singh (@citruspi)',
+    author_email='hello@mihirsingh.com',
+    description='A Python client for the Stackdriver service',
+    url='https://github.com/citruspi/py-stackdriver',
+    classifiers=[
+        'License :: Public Domain',
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: System :: Monitoring',
+        'Operating System :: Unix',
+        'Operating System :: MacOS :: MacOS X'
+    ],
+    scripts=[
+        'scripts/stackdriver'
+    ],
+    packages=['stackdriver'],
+    zip_safe=False,
+    include_package_date = True,
+    platforms = 'any',
+    install_requires = [
+        'requests',
+        'click'
+    ]
+)
