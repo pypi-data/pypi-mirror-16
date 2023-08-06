9af36de82ff7bb7b2b28619f3cecfa9b36b12e73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:18 +0000

    Add package vy/1.3.1/vy-1.3.1.tar.gz

---
+++ packages/vy/vy-1.3.1.tar.gz/vy-1.3.1/vyapp/plugins/quick_search.py
@@ -0,0 +1,102 @@
+"""
+
+"""
+
+from vyapp.ask import Get
+from re import escape, split
+from vyapp.tools import set_status_msg
+
+class QuickSearch(object):
+    def __init__(self, area):
+        """
+
+        """
+        self.area = area
+        area.install(('NORMAL', '<Key-backslash>', lambda event: self.start_search()))
+
+    def start_search(self):
+        ask = Get(self.area, events = {'<Alt-p>':self.search_down, '<Alt-o>': self.search_up, '<Control-j>': self.search_down, 
+                                       '<Control-k>': self.search_up, '<<Data>>':self.update_search, '<BackSpace>': self.update_search,
+                                       '<Return>': lambda wid: self.stop_search(), '<Escape>': lambda wid: self.stop_search()})
+        set_status_msg('')
+
+    def stop_search(self):
+        self.area.tag_remove('sel', *self.start_range())
+        return True
+
+    def start_range(self):
+        return ('1.0', 'end')
+        
+    def range_down(self):
+        """
+        This method return the range to be searched that is down to the cursor position.
+        """
+
+        ranges = self.area.tag_ranges('sel')
+        if ranges:
+            return (ranges[-1], 'end')
+        else:
+            return ('insert', 'end')
+
+    def range_up(self):
+        """
+        The range to be searched up to the cursor position.
+        """
+
+        ranges = self.area.tag_ranges('sel')
+        if ranges:
+            return (ranges[0], '1.0')
+        else:
+            return ('insert', '1.0')
+
+    def update_search(self, wid):
+        """
+
+        """
+        data = wid.get()
+        pattern = self.make_pattern(data)
+        set_status_msg('Pattern:%s' % pattern)
+        range = self.start_range()
+        self.area.pick_next_down('sel', pattern, *range)
+
+    def make_pattern(self, data):
+        """
+
+        """
+
+        data    = split(' +', data)
+        pattern = ''
+        for ind in xrange(0, len(data)-1):
+            pattern = pattern + escape(data[ind]) + '.+?'
+        pattern = pattern + escape(data[-1])
+        return pattern
+
+    def search_up(self, wid):
+        """
+
+        """
+        data = wid.get()
+        pattern = self.make_pattern(data)
+        range = self.range_up()
+        self.area.pick_next_up('sel', pattern, *range)
+
+        
+    def search_down(self, wid):
+        """
+
+        """
+        data = wid.get()
+        pattern = self.make_pattern(data)
+        range = self.range_down()
+        self.area.pick_next_down('sel', pattern, *range)
+
+install = QuickSearch
+
+
+
+
+
+
+
+
+
