3284d18dd01769694284ac5d7af4e14784b6d20c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:39 +0000

    Add package mailchimp3/1.0.25/mailchimp3-1.0.25-py2.py3-none-any.whl

---
+++ packages/mailchimp3/mailchimp3-1.0.25-py2.py3-none-any.whl/mailchimp3/entities/listactivity.py
@@ -0,0 +1,15 @@
+from __future__ import unicode_literals
+from ..baseapi import BaseApi
+
+
+class ListActivity(BaseApi):
+
+    def __init__(self, *args, **kwargs):
+        super(ListActivity, self).__init__(*args, **kwargs)
+        self.endpoint = 'lists'
+
+    def all(self, list_id, **queryparams):
+        """
+        returns up to 180 days of list activity.
+        """
+        return self._mc_client._get(url=self._build_path(list_id, 'activity'), **queryparams)
