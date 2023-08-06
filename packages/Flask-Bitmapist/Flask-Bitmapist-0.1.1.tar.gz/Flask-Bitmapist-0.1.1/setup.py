11519ee80ded2c16afdef4cb93b8f406ffb09fd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:19:46 +0000

    Add package Flask-Bitmapist/0.1.1/Flask-Bitmapist-0.1.1.tar.gz

---
+++ packages/Flask-Bitmapist/Flask-Bitmapist-0.1.1.tar.gz/Flask-Bitmapist-0.1.1/setup.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+"""
+    Flask-Bitmapist Setup
+    ~~~~~~~~~~~~~~~~~~~~~
+    setup.py for Flask-Bitmapist
+
+    :copyright: (c) 2016 by Cuttlesoft, LLC.
+    :license: MIT, see LICENSE for more details.
+"""
+
+import sys
+
+from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+
+
+def get_requirements(suffix=''):
+    with open('requirements%s.txt' % suffix) as f:
+        rv = f.read().splitlines()
+    return rv
+
+
+class PyTest(TestCommand):
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = [
+            '-xrs',
+            '--cov', 'flask_bitmapist',
+            '--cov-report', 'term-missing',
+            '--pep8',
+            '--flakes',
+            '--cache-clear'
+        ]
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+        errno = pytest.main(self.test_args)
+        sys.exit(errno)
+
+
+setup(
+    name='Flask-Bitmapist',
+    version='0.1.1',
+    url='http://github.com/cuttlesoft/flask-bitmapist',
+    download_url='https://github.com/cuttlesoft/flask-bitmapist/tarball/0.1.1',
+    license='MIT',
+    author='Cuttlesoft, LLC',
+    author_email='engineering@cuttlesoft.com',
+    description='Flask extension that creates a simple interface to Bitmapist analytics library',
+    long_description=open('README.rst').read() + '\n\n' + open('HISTORY.rst').read(),
+    packages=find_packages(),
+    keywords=['Flask', 'Bitmapist'],
+    py_modules=['flask_bitmapist'],
+    zip_safe=False,
+    platforms='any',
+    install_requires=[
+        'Flask>=0.9',
+        'bitmapist>=3.97'
+    ],
+    tests_require=get_requirements('-test'),
+    cmdclass={'test': PyTest},
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules']
+)
