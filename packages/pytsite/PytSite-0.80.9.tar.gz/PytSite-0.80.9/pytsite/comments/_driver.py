0ea5d5c9343480acd11f7bd5ac9e3c436a30b65c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:27 +0000

    Add package pytsite/0.80.9/PytSite-0.80.9.tar.gz

---
+++ packages/pytsite/PytSite-0.80.9.tar.gz/PytSite-0.80.9/pytsite/comments/_driver.py
@@ -0,0 +1,44 @@
+"""PytSite Comments Abstract Driver.
+"""
+from typing import Iterable as _Iterable
+from abc import ABC as _ABC, abstractmethod as _abstractmethod
+from pytsite import widget as _widget, auth as _auth
+from . import _model
+
+__author__ = 'Alexander Shepetko'
+__email__ = 'a@shepetko.com'
+__license__ = 'MIT'
+
+
+class Abstract(_ABC):
+    """Abstract Comments Driver.
+    """
+
+    @_abstractmethod
+    def get_name(self) -> str:
+        """Get driver name.
+        """
+        pass
+
+    @_abstractmethod
+    def create_comment(self, thread_id: str, body: str, author: _auth.model.AbstractUser,
+                       status: str = 'published') -> _model.CommentInterface:
+        """Create new comment.
+        """
+        pass
+
+    @_abstractmethod
+    def get_widget(self, widget_uid: str, thread_id: str) -> _widget.Abstract:
+        """Get comments widget for particular thread.
+        """
+        pass
+
+    @_abstractmethod
+    def get_comments(self, thread_id: str) -> _Iterable[_model.CommentInterface]:
+        pass
+
+    @_abstractmethod
+    def get_comments_count(self, thread_id: str) -> int:
+        """Get comments count for particular thread.
+        """
+        pass
