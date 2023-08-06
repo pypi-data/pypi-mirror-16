eed53d06146a8b89229ef00e590cdf4fc4af6a90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:38 +0000

    Add package mawu/1.4.0/mawu-1.4.0.zip

---
+++ packages/mawu/mawu-1.4.0.zip/mawu-1.4.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name = 'mawu',
+      version = '1.4.0',
+      py_modules = ['mawu'],
+      author = 'John Tovor',
+      author_email = 'johnwealth18@gmail.com',
+      url = 'http://www.headfirstlabs.com',
+      description = 'A simple printer of nested lists',
+      )
