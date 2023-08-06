1d48b3da7d00d882b48a5bb088e2326e36017474
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:22:58 +0000

    Add package 24to25/1.0/24to25-1.0.zip

---
+++ packages/24to25/24to25-1.0.zip/24to25-1.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='24to25',
+      version='1.0',
+      author="Kieran O'Leary",
+      author_email="kieran.o.leary@gmail.com",
+      url='https://github.com/kieranjol/24to25/',
+      download_url='https://github.com/kieranjol/24to25/archive/0.1.zip',
+      py_modules=['24to25'],
+      )
