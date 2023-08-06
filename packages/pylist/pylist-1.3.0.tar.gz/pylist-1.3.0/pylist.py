c50325dd7f3cc7b4363aff4570338272bb6fe965
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:14 +0000

    Add package pylist/1.3.0/pylist-1.3.0.tar.gz

---
+++ packages/pylist/pylist-1.3.0.tar.gz/pylist-1.3.0/pylist.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+# _*_ coding: UTF-8 _*_
+
+
+#pylist = ["I AM body","I AM Girl","I AM treen "]
+#for  data  in pylist:
+#    print data 
+#count = 0
+#while count < len(pylist):
+#    print(pylist[count])
+#    count = count+1
+movies = ["The Holy Grail",1975, "Terry Jones & Terry Gilliam",91,
+             ["Graham Chapman",["Michael Palin","John Cleese",
+                 "Terry Gilliam","Eric Idle","Terry Jones"]]]
+#print movies
+#for each_item in movies:
+#    print(each_item)     
+
+def print_lol(the_list,indent=False,level=0):
+    for each_item in the_list:
+        if isinstance(each_item,list):
+            print_lol(each_item,indent,level+1)
+    else:
+        if indent:
+             for tab_stop in range(level):
+                print ("\t\n")
+        print(each_item)
+
+print_lol(movies) 
