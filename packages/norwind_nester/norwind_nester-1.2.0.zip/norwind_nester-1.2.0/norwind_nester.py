f929c76206e4f51b008c38fd79ab046b072635fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:03 +0000

    Add package norwind_nester/1.2.0/norwind_nester-1.2.0.zip

---
+++ packages/norwind_nester/norwind_nester-1.2.0.zip/norwind_nester-1.2.0/norwind_nester.py
@@ -0,0 +1,9 @@
+def print_lol(the_list,indent=False, tabnum=0):
+	for each_item in the_list:
+		if isinstance(each_item,list):
+			print_lol(each_item,indent,tabnum+1);
+		else:
+			if indent:
+				for num in range(tabnum):
+					print("\t",end='')
+			print(each_item)
---
+++ packages/norwind_nester/norwind_nester-1.2.0.zip/norwind_nester-1.2.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name    = 'norwind_nester',
+    version = '1.2.0',
+    py_modules = ['norwind_nester'],
+    author = 'norwind',
+	author_email = 'norwind@163.com',
+    description = 'A simple printer of nested lists',
+    )
