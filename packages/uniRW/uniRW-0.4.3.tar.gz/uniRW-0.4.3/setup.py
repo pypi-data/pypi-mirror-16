baf9e2611ff8d27241b6e75a995b5e64edd02718
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:54 +0000

    Add package uniRW/0.4.3/uniRW-0.4.3.tar.gz

---
+++ packages/uniRW/uniRW-0.4.3.tar.gz/uniRW-0.4.3/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+
+setup(
+  name = 'uniRW',
+  packages = ['uniRW'],
+  version = '0.4.3',
+  description = 'A universal reader and writer for stateful data file processing',
+  author = 'Langxuan Su',
+  author_email = 'lawliet@orion.codes',
+  url = 'https://github.com/law-liet/uniRW',
+  download_url = 'https://github.com/law-liet/uniRW/tarball/v0.4.3',
+  keywords = ['universal', 'reader', 'writer',  'map', 'reduce'],
+  classifiers = [
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 2.7",
+    "Programming Language :: Python :: 3",
+    "Development Status :: 4 - Beta",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+    "Topic :: Text Processing",
+    "Topic :: Utilities",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
