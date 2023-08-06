57051b933ddf9862fd713651205630504e368bc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:14 +0000

    Add package config-utils/0.3/config-utils-0.3.tar.gz

---
+++ packages/config-utils/config-utils-0.3.tar.gz/config-utils-0.3/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='config-utils',
+    packages = ['config-utils'],
+    version='0.3',
+    description='A python script that parses a simple configuration file',
+    author='Duan Uys',
+    author_email='duan_uys@icloud.com',
+    url='https://github.com/duanuys/config-file-read/tarball/0.1',
+    keywords=['configuration', 'file', 'parser'],
+    classifiers=[],
+)
