0668d05fef68af573c4b0e9cd76d5ab973364233
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:57:21 +0000

    Add package CVXcanon/0.1.1/CVXcanon-0.1.1.tar.gz

---
+++ packages/CVXcanon/CVXcanon-0.1.1.tar.gz/CVXcanon-0.1.1/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, Extension, find_packages
+from setuptools.command.install import install
+from distutils.command.build import build
+import numpy
+import os
+
+canon = Extension(
+    '_CVXcanon',
+    sources=['src/CVXcanon.cpp', 'src/LinOpOperations.cpp', 'src/python/CVXcanon_wrap.cpp'],
+    include_dirs=['src/', 'src/python/', 'include/Eigen', numpy.get_include()]
+)
+
+base_dir = os.path.dirname(__file__)
+about = {}
+with open(os.path.join(base_dir, "src", "python", "_version__.py")) as f:
+    exec(f.read(), about)
+
+setup(
+    name='CVXcanon',
+    version="0.1.1",
+    setup_requires=['numpy'],
+    author='Jack Zhu, John Miller, Paul Quigley',
+    author_email='jackzhu@stanford.edu, millerjp@stanford.edu, piq93@stanford.edu',
+    ext_modules=[canon],
+    package_dir={'': 'src/python'},
+    py_modules=['canonInterface', 'CVXcanon', '_version__'],
+    description='A low-level library to perform the matrix building step in cvxpy, a convex optimization modeling software.',
+    license='GPLv3',
+    url='https://github.com/jacklzhu/CVXcanon',
+    install_requires=[
+        'numpy',
+        'scipy',
+    ]
+)
---
+++ packages/CVXcanon/CVXcanon-0.1.1.tar.gz/CVXcanon-0.1.1/src/Utils.hpp
@@ -0,0 +1,26 @@
+//    This file is part of CVXcanon.
+//
+//    CVXcanon is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    CVXcanon is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with CVXcanon.  If not, see <http://www.gnu.org/licenses/>.
+
+// Some useful defines for Matricies/etc.
+
+#include "../include/Eigen/Sparse"
+#include "../include/Eigen/Core"
+
+#define NULL_MATRIX Eigen::SparseMatrix<double>(0,0)
+
+typedef Eigen::Matrix<int, Eigen::Dynamic, 1> Vector;
+typedef Eigen::SparseMatrix<double> Matrix;
+typedef std::map<int, Matrix> CoeffMap;
+typedef Eigen::Triplet<double> Triplet;
