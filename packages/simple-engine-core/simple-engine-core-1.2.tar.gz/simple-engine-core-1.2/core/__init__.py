d218c93a8626f4537f36f77f1a4752655b4a2706
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:15 +0000

    Add package simple-engine-core/1.2/simple-engine-core-1.2.tar.gz

---
+++ packages/simple-engine-core/simple-engine-core-1.2.tar.gz/simple-engine-core-1.2/core/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+from .interpreter import interpreter
+from .tools import cvd
+from .tools import wtf
+
+__VERSION__ = "1.2"
---
+++ packages/simple-engine-core/simple-engine-core-1.2.tar.gz/simple-engine-core-1.2/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+from core import __VERSION__
+
+setup(
+    name="simple-engine-core",
+    description="Reads in HTML, reads out JSON",
+    long_description=open("README.md").read(),
+
+    license="MIT",
+    version=__VERSION__,
+
+    url="https://github.com/apizzimenti/simple-engine-core.git",
+    author="Anthony Pizzimenti",
+    author_email="pizzimentianthony@gmail.com",
+
+    packages=["core"],
+
+    entry_points={
+        "console_scripts": ["engine=core.index:main"],
+    },
+
+    install_requires=[
+        "beautifulsoup4"
+    ]
+)
