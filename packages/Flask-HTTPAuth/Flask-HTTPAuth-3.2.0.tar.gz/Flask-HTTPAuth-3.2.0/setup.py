d9e2728a2d1f524d69114e1cfcfb7d3f7a39f867
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:11 +0000

    Add package Flask-HTTPAuth/3.2.0/Flask-HTTPAuth-3.2.0.tar.gz

---
+++ packages/Flask-HTTPAuth/Flask-HTTPAuth-3.2.0.tar.gz/Flask-HTTPAuth-3.2.0/setup.py
@@ -0,0 +1,38 @@
+"""
+Flask-HTTPAuth
+--------------
+
+Basic and Digest HTTP authentication for Flask routes.
+"""
+from setuptools import setup
+
+
+setup(
+    name='Flask-HTTPAuth',
+    version='3.2.0',
+    url='http://github.com/miguelgrinberg/flask-httpauth/',
+    license='MIT',
+    author='Miguel Grinberg',
+    author_email='miguelgrinberg50@gmail.com',
+    description='Basic and Digest HTTP authentication for Flask routes',
+    long_description=__doc__,
+    py_modules=['flask_httpauth'],
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=[
+        'Flask'
+    ],
+    test_suite="tests",
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
