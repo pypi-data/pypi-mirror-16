648b7ec14bdcdec024b204380e4862be72245e37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:31 +0000

    Add package aldryn-common/1.0.4/aldryn-common-1.0.4.tar.gz

---
+++ packages/aldryn-common/aldryn-common-1.0.4.tar.gz/aldryn-common-1.0.4/setup.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+from aldryn_common import __version__
+
+REQUIREMENTS = [
+    'aldryn-boilerplates',
+    'django-sortedm2m>=1.2.2,!=1.3.0,!=1.3.1',
+    'six',
+]
+
+CLASSIFIERS = [
+    'Development Status :: 5 - Production/Stable',
+    'Environment :: Web Environment',
+    'Framework :: Django',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: BSD License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    'Topic :: Software Development',
+    'Topic :: Software Development :: Libraries :: Application Frameworks',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3.3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+]
+
+setup(
+    name='aldryn-common',
+    version=__version__,
+    description='Common utitilities',
+    author='Divio AG',
+    author_email='info@divio.ch',
+    url='https://github.com/aldryn/aldryn-common',
+    packages=find_packages(),
+    license='LICENSE.txt',
+    platforms=['OS Independent'],
+    install_requires=REQUIREMENTS,
+    classifiers=CLASSIFIERS,
+    include_package_data=True,
+    zip_safe=False,
+    test_suite='tests',
+)
