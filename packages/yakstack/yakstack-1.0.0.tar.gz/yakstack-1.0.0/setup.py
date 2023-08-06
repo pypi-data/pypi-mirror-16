0e9924edff3a12d2f6be5cb4e34b902bf9b2392a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:14:45 +0000

    Add package yakstack/1.0.0/yakstack-1.0.0.tar.gz

---
+++ packages/yakstack/yakstack-1.0.0.tar.gz/yakstack-1.0.0/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup
+from codecs import open
+import yakstack
+
+with open('README.rst', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='yakstack',
+    version=yakstack.VERSION,
+
+    description='A command-line utility to help you stack your yaks.',
+    long_description=long_description,
+    keywords=['yak stack', 'yak frame', 'cli', 'tasks'],
+
+    url='https://github.com/gilmoreorless/yakstack',
+
+    author='Gilmore Davidson',
+    author_email='gilmoreorless@gmail.com',
+
+    license='MIT',
+
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Topic :: Utilities',
+
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+    ],
+
+    py_modules=['yakstack'],
+    entry_points={
+        'console_scripts': [
+            'yakstack=yakstack:main'
+        ]
+    }
+
+)
