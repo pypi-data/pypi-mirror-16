65ba9a60f721733ad36a06bc6ee6e4dbdf8f30a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:23 +0000

    Add package CPPCleanBear/0.8.0.1470601983/CPPCleanBear-0.8.0.1470601983.tar.gz

---
+++ packages/CPPCleanBear/CPPCleanBear-0.8.0.1470601983.tar.gz/CPPCleanBear-0.8.0.1470601983/setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+from setuptools import find_packages, setup
+
+
+if __name__ == "__main__":
+    try:
+        setup(name='CPPCleanBear',
+              version='0.8.0.1470601983',
+              description="'CPPCleanBear' bear for coala (http://coala.rtfd.org/)",
+              authors={'The coala developers'},
+              authors_emails={'coala-devel@googlegroups.com'},
+              maintainers={'The coala developers'},
+              maintainers_emails={'coala-devel@googlegroups.com'},
+              platforms={'any'},
+              license='AGPL-3.0',
+              packages=find_packages(exclude=["build.*"]),
+              long_description="""
+    Find problems in C++ source code that slow down development in large code
+    bases. This includes finding unused code, among other features.
+
+    Read more about available routines at
+    <https://github.com/myint/cppclean#features>.
+    """,
+              classifiers=[
+                   "Development Status :: 4 - Beta",
+                   "Environment :: Console",
+                   "Environment :: Win32 (MS Windows)",
+                   "Intended Audience :: Science/Research",
+                   "Intended Audience :: Developers",
+                   "Programming Language :: "
+                        "Python :: Implementation :: CPython",
+                   "Programming Language :: Python :: 3.4",
+                   "Programming Language :: Python :: 3.5",
+                   "Programming Language :: Python :: 3 :: Only",
+                   "Topic :: Scientific/Engineering :: Information Analysis",
+                   "Topic :: Software Development :: Quality Assurance",
+                   "Topic :: Text Processing :: Linguistic"])
+
+    finally:
+        print('[WARN] If you do not install the bears using the coala '
+              'installation tool, there may be problems with the dependencies '
+              'and they may not work.')
