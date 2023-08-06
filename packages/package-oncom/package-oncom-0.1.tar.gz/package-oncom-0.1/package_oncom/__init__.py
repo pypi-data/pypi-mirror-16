61c1c2adc33c9ffba0ef172cfb01bc017312718f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:19:54 +0000

    Add package package-oncom/0.1/package-oncom-0.1.tar.gz

---
+++ packages/package-oncom/package-oncom-0.1.tar.gz/package-oncom-0.1/package_oncom/__init__.py
@@ -0,0 +1,15 @@
+def hello1():
+	data = {
+		"package": "package_oncom",
+		"module": "__init__.py",
+		"function": "hello1",
+	}
+	return data
+
+def hello2():
+	data = {
+		"package": "package_oncom hihihihih",
+		"module": "__init__.py",
+		"function": "hello2",
+	}
+	return data
