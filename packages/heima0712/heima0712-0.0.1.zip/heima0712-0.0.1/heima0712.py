678f8e0e5e7e5f764b4dd9b678fc155417c7ddd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:25 +0000

    Add package heima0712/0.0.1/heima0712-0.0.1.zip

---
+++ packages/heima0712/heima0712-0.0.1.zip/heima0712-0.0.1/heima0712.py
@@ -0,0 +1,6 @@
+#coding:utf-8
+
+def function(num):
+	for each in range(0, num, 2):
+		print each
+
---
+++ packages/heima0712/heima0712-0.0.1.zip/heima0712-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+	name = "heima0712",
+	version = "0.0.1",
+	py_modules = ["heima0712"],
+	author = "0712",
+	url = "http://www.treenewbee.top",
+	description = "0712 QuanTi",
+	)
