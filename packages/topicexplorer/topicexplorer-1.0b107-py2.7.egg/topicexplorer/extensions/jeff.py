143d5efc4929e221937821cd3cfa764655c03a97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:40 +0000

    Add package topicexplorer/1.0b107/topicexplorer-1.0b107-py2.7.egg

---
+++ packages/topicexplorer/topicexplorer-1.0b107-py2.7.egg/topicexplorer/extensions/jeff.py
@@ -0,0 +1,31 @@
+from ConfigParser import RawConfigParser as ConfigParser, NoOptionError
+
+from bottle import static_file
+
+raw_corpus_path = None
+def init(app, config_file):
+    global raw_corpus_path
+    config = ConfigParser({ 'raw_corpus' : 'ap/'  })
+    config.read(config_file)
+
+    raw_corpus_path = config.get('main', 'raw_corpus')
+
+    @app.route('/fulltext/<doc_id>')
+    def get_doc(doc_id):
+        return static_file(doc_id, root=raw_corpus_path)
+
+import string
+def label(doc):
+    newdoc = doc.replace('_', ' ')
+    newdoc = newdoc.replace('.txt','')
+    try:
+        id, details = newdoc.split('--',1)
+        details = details.lower()
+        id = id.replace("LETTER ",'')
+        #newdoc = id + " -- " + string.capwords(details)
+        newdoc = string.capwords(details)
+    except:
+        pass
+    return newdoc
+
+
