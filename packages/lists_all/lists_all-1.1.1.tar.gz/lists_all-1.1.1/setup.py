18d4215ba26ebdab2c2811e58fbbb218bd0c94c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:41 +0000

    Add package lists_all/1.1.1/lists_all-1.1.1.tar.gz

---
+++ packages/lists_all/lists_all-1.1.1.tar.gz/lists_all-1.1.1/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup ( 
+        name        = 'lists_all',
+        version     = '1.1.1',
+        py_modules  = ['lists_all'],
+        author      = 'Arthur Ngondo',
+        author_email= 'ngondoarthur@gmail.com',
+        url         = 'http://www.artoriouscreative.co.ke',
+        description = 'A simple printer of nested lists'
+        )
