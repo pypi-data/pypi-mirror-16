398b76abd973b8b4a131535b0e8cc08014ff4ff0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:17:33 +0000

    Add package lixcode/1.0.1/lixcode-1.0.1.tar.gz

---
+++ packages/lixcode/lixcode-1.0.1.tar.gz/lixcode-1.0.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='lixcode',
+    version='1.0.1',
+    py_modules=['nester', 'test'],
+    author='lixleon',
+    author_email='lixleon@foxmail.com',
+    url=None,
+    description='This is test project'
+)
---
+++ packages/lixcode/lixcode-1.0.1.tar.gz/lixcode-1.0.1/test.py
@@ -0,0 +1,8 @@
+"""
+This is a test module
+"""
+def test_func(arg):
+    """
+    This is a function of test module.
+    """
+    print(arg)
