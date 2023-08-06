a1970e11b2fc389ab9b2576a4e8575ad612c5f19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:29 +0000

    Add package kclist/1.3.0/kclist-1.3.0.zip

---
+++ packages/kclist/kclist-1.3.0.zip/kclist-1.3.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+
+setup(
+    name = 'kclist',
+    version = '1.3.0',
+    py_modules = ['kclist'],
+    author = 'kcpython',
+    author_email = 'kevinchang823@gmail.com',
+    description = 'A simple printer of nested lists',
+    )
