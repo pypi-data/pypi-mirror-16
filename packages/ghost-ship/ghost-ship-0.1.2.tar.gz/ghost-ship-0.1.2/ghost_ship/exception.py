61fa3e790f416dc854cd2bbcd95944d5cb8db4ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:20:02 +0000

    Add package ghost-ship/0.1.2/ghost-ship-0.1.2.tar.gz

---
+++ packages/ghost-ship/ghost-ship-0.1.2.tar.gz/ghost-ship-0.1.2/ghost_ship/exception.py
@@ -0,0 +1,13 @@
+class JobNotFound(Exception):
+  pass
+
+class GroupNotFound(Exception):
+  pass
+
+class TykServiceNotFound(Exception):
+  pass
+
+class TykReloadFailed(Exception):
+  pass
+class JobTemplateIsNotFound(Exception):
+  pass
