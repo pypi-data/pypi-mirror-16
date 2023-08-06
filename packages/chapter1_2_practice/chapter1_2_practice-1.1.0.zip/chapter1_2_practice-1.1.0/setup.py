7bf151658a828fd42f755ca110e87f9f358b4217
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:57 +0000

    Add package chapter1_2_practice/1.1.0/chapter1_2_practice-1.1.0.zip

---
+++ packages/chapter1_2_practice/chapter1_2_practice-1.1.0.zip/chapter1_2_practice-1.1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+	name = 'chapter1_2_practice',
+	version = '1.1.0',
+	py_modules = ['chapter1_2_practice'],
+	author = 'TTT',
+	author_email = 'TzuTeng.Tzeng@ctci.com.tw',
+	url = '',
+	description = 'A simple printer of nested lists'
+	)
