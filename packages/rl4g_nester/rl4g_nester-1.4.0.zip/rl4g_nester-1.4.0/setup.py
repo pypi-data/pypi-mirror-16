aaaa515bc245905cd93093465406c2b380ca98e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:29 +0000

    Add package rl4g_nester/1.4.0/rl4g_nester-1.4.0.zip

---
+++ packages/rl4g_nester/rl4g_nester-1.4.0.zip/rl4g_nester-1.4.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup( 
+        name         = 'rl4g_nester', 
+        version      = '1.4.0',
+        py_modules   = ['nester'],
+        author       = 'RamiLego4Game',
+        author_email = 'ramilego4game@gmail.com',
+        url          = 'http://ramilego4game.byethost7.com',
+        description  = 'A simple printer of nested lists from Head First Python',
+     )
