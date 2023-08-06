fdbcf123ff57b66ff888f271399d57e51bd272c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:22:01 +0000

    Add package winontop/0.1/winontop-0.1.zip

---
+++ packages/winontop/winontop-0.1.zip/winontop-0.1/setup.py
@@ -0,0 +1,30 @@
+import os
+from setuptools import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "winontop",
+    version = "0.1",
+    author = "Jithu R Jacob",
+    author_email = "jithurjacob@gmail.com",
+    description = ("An easy tool to keep windows on top of others."),
+    license = "BSD",
+    keywords = "python windows top winontop windowsontop",
+    url = "https://github.com/jithurjacob/winontop",
+    download_url = 'https://github.com/jithurjacob/winontop/tarball/0.1',
+    packages=['winontop'],
+    entry_points = {
+        "console_scripts": ['winontop = winontop.winontop:main']
+        },
+    long_description=read('README'),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        'Operating System :: Microsoft',
+        'Environment :: Win32 (MS Windows)',
+        "License :: OSI Approved :: BSD License",
+    ],
+)
