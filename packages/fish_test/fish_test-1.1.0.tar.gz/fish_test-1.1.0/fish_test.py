1212c1723f896d3c70efb6b8a77efa6f38b77627
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:15:47 +0000

    Add package fish_test/1.1.0/fish_test-1.1.0.tar.gz

---
+++ packages/fish_test/fish_test-1.1.0.tar.gz/fish_test-1.1.0/fish_test.py
@@ -0,0 +1,10 @@
+'''A module to print list'''
+def print_lol(the_list, indent=False, level=0):
+	for item in the_list:
+		if isinstance(item, list):
+			print_lol(item, level+1)
+		else:
+			if indent:
+				for tab_stop in range(level):
+					print('\t', end='')
+			print(item)
---
+++ packages/fish_test/fish_test-1.1.0.tar.gz/fish_test-1.1.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+	name		= 'fish_test',
+	version		= '1.1.0',
+	py_modules	= ['fish_test'],
+	author		= 'gong',
+	author_email	= 'fishgolang@gmail.com'
+     )
