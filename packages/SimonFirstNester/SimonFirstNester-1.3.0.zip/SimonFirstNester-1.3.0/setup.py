8458234487504d01bd1dbd7336d7c477880d7315
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:31 +0000

    Add package SimonFirstNester/1.3.0/SimonFirstNester-1.3.0.zip

---
+++ packages/SimonFirstNester/SimonFirstNester-1.3.0.zip/SimonFirstNester-1.3.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+
+setup(
+    name='SimonFirstNester',
+    version='1.3.0',
+    py_modules=['SimonFirstNester'],
+    author='Simon',
+    author_email='ccqctljx@gmail.com',
+    description='A simple printer of nested lists',
+)
