ee8c4039a3f28f555b6c083e655b6c5e9a598f6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:07 +0000

    Add package pytsite/0.81.3/PytSite-0.81.3.tar.gz

---
+++ packages/pytsite/PytSite-0.81.3.tar.gz/PytSite-0.81.3/pytsite/image/__init__.py
@@ -0,0 +1,27 @@
+"""PytSite Image Package.
+"""
+# Public API
+from . import _model as model, _widget as widget
+from ._api import create, get, get_by_ref, get_resize_limit_height, get_resize_limit_width, get_resize_step
+
+__author__ = 'Alexander Shepetko'
+__email__ = 'a@shepetko.com'
+__license__ = 'MIT'
+
+
+def _init():
+    from pytsite import odm, router, assetman, events
+    from . import _eh
+
+    odm.register_model('image', model.Image)
+
+    router.add_rule(
+        '/image/resize/<int:width>/<int:height>/<string(length=2):p1>/<string(length=2):p2>/<string:filename>',
+        'pytsite.image@resize'
+    )
+
+    assetman.register_package(__name__)
+    events.listen('pytsite.setup', _eh.setup)
+
+
+_init()
