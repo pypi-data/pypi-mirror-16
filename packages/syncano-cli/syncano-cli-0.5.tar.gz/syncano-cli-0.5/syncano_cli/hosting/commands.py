f4a57eea30d38f8788664b960511945e8c3fd55a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:15:41 +0000

    Add package syncano-cli/0.5/syncano-cli-0.5.tar.gz

---
+++ packages/syncano-cli/syncano-cli-0.5.tar.gz/syncano-cli-0.5/syncano_cli/hosting/commands.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+import os
+import sys
+
+import click
+from syncano_cli.base.connection import create_connection, get_instance_name
+from syncano_cli.config import ACCOUNT_CONFIG_PATH
+from syncano_cli.hosting.utils import HostingCommands
+
+
+@click.group()
+def top_hosting():
+    pass
+
+
+@top_hosting.command()
+@click.option('--config', help=u'Account configuration file.')
+@click.option('--instance-name', help=u'Instance name.')
+@click.option('--list-files', is_flag=True, help='List files within the hosting.')
+@click.option('--publish', type=str, help='Publish files from the local directory to the Syncano Hosting.')
+def hosting(config, instance_name, list_files, publish):
+    """
+    Handle hosting and hosting files. Allow to publish static pages to the Syncano Hosting.
+    """
+
+    def validate_domain(provided_domain=None):
+        return 'default' if not provided_domain else provided_domain
+
+    def validate_publish(base_dir):
+        if not os.path.isdir(base_dir):
+            click.echo(u'ERROR: You should provide a project root directory here.')
+            sys.exit(1)
+
+    config = config or ACCOUNT_CONFIG_PATH
+    instance_name = get_instance_name(config, instance_name)
+
+    try:
+        connection = create_connection(config)
+        instance = connection.Instance.please.get(name=instance_name)
+
+        hosting_commands = HostingCommands(instance)
+
+        if list_files:
+            domain = validate_domain()
+            click.echo(u'INFO: List the hosting files: {} in instance: {}'.format(domain, instance_name))
+            hosting_files = hosting_commands.list_hosting_files(domain=domain)
+            hosting_commands.print_hosting_files(hosting_files)
+
+        if publish:
+            domain = validate_domain()
+            click.echo(u'INFO: Publish the hosting files: {} in instance: {}'.format(domain, instance_name))
+            validate_publish(base_dir=publish)
+            uploaded_files = hosting_commands.publish(domain=domain, base_dir=publish)
+            hosting_commands.print_hosting_files(uploaded_files)
+
+    except Exception as e:
+        click.echo(u'ERROR: {}'.format(e))
+        sys.exit(1)
---
+++ packages/syncano-cli/syncano-cli-0.5.tar.gz/syncano-cli-0.5/syncano_cli/hosting/utils.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+import os
+import sys
+
+import click
+
+
+class HostingCommands(object):
+
+    def __init__(self, instance):
+        self.instance = instance
+
+    def list_hosting(self):
+        return [
+            (hosting.label, hosting.domains) for hosting in self.instance.hostings.all()
+        ]
+
+    def list_hosting_files(self, domain):
+        hosting = self._get_hosting(domain=domain)
+        if not hosting:
+            click.echo(u'WARN: No default hosting found. Exit.')
+            sys.exit(1)
+
+        files_list = hosting.list_files()
+        return files_list
+
+    def publish(self, domain, base_dir):
+        uploaded_files = []
+        hosting = self._get_hosting(domain=domain)
+        if not hosting:
+            # create a new hosting if no default is present;
+            hosting = self.create_hosting(label='Default hosting', domain=domain)
+
+        for folder, subs, files in os.walk(base_dir):
+            path = folder.split(base_dir)[1][1:]  # skip the /
+            for single_file in files:
+                if path:
+                    file_path = '{}/{}'.format(path, single_file)
+                else:
+                    file_path = single_file
+
+                self._validate_path(file_path)
+
+                sys_path = os.path.join(folder, single_file)
+                with open(sys_path, 'rb') as upload_file:
+                    click.echo(u'INFO: Uploading file: {}'.format(file_path))
+                    hosting.upload_file(path=file_path, file=upload_file)
+
+                uploaded_files.append(file_path)
+        return uploaded_files
+
+    def create_hosting(self, label, domain):
+        hosting = self.instance.hostings.create(
+            label=label,
+            domains=[domain]
+        )
+        return hosting
+
+    def _get_hosting(self, domain):
+        hostings = self.instance.hostings.all()
+        for hosting in hostings:
+            if domain in hosting.domains:
+                return hosting
+
+    def _validate_path(self, file_path):
+        try:
+            file_path.decode('ascii')
+        except UnicodeEncodeError:
+            click.echo(u'ERROR: Unicode characters in path are not supported. Check the files names.')
+            sys.exit(1)
+
+    def print_hosting_files(self, hosting_files):
+        print('Hosting files:')
+        self._print_separator()
+        for file_path in hosting_files:
+            print(file_path)
+
+    @staticmethod
+    def _print_separator():
+        print(79 * '-')
