551b651714839c19c8fce1cc6e9e5930513eea38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:15:00 +0000

    Add package robotframework-mongodblibrary/0.3.3/robotframework-mongodblibrary-0.3.3.zip

---
+++ packages/robotframework-mongodblibrary/robotframework-mongodblibrary-0.3.3.zip/robotframework-mongodblibrary-0.3.3/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+
+"""Setup script for Robot's MongoDB Library distributions"""
+
+from distutils.core import setup
+
+import sys, os
+sys.path.insert(0, os.path.join('src','MongoDBLibrary'))
+
+from version import VERSION
+
+def main():
+    setup(name         = 'robotframework-mongodblibrary',
+          version      = VERSION,
+          description  = 'Mongo Database utility library for Robot Framework',
+          author       = 'Jerry Schneider',
+          author_email = 'jerry@iplantcollaborative.org',
+          url          = 'https://github.com/iPlantCollaborativeOpenSource',
+          package_dir  = { '' : 'src'},
+          install_requires = ["pymongo >= 3.2"],
+          packages     = ['MongoDBLibrary']
+          )
+        
+
+if __name__ == "__main__":
+    main()
