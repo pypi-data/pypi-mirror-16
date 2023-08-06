b6054db31ddf56d0d4fe2742a3b33f9704f83b17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:13 +0000

    Add package izug.seantis.reservation/0.8.1/izug.seantis.reservation-0.8.1.tar.gz

---
+++ packages/izug.seantis.reservation/izug.seantis.reservation-0.8.1.tar.gz/izug.seantis.reservation-0.8.1/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.8.1'
+
+setup(name='izug.seantis.reservation',
+      version=version,
+      description="iZug theme adjustments for seantis.reservation",
+      long_description='\n'.join((
+          open("README.md").read(),
+          open(os.path.join("docs", "HISTORY.txt")).read()
+      )),
+      # Get more strings from
+      # http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=[
+          'Programming Language :: Python',
+      ],
+      keywords='',
+      author='',
+      author_email='',
+      url='http://svn.plone.org/svn/collective/',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['izug', 'izug.seantis'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'Plone>=4.1'
+          'setuptools',
+          'seantis.dir.base>=1.10',
+          'seantis.dir.facility',
+          'seantis.reservation',
+          'izug.basetheme',
+          'collective.geo.zugmap'
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
