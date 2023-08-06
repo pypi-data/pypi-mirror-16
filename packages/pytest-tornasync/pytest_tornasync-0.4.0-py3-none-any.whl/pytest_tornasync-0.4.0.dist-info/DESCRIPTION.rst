6c812b5f4c68b3b1ef7adbcb41d0336bf5140ba0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:16:08 +0000

    Add package pytest-tornasync/0.4.0/pytest_tornasync-0.4.0-py3-none-any.whl

---
+++ packages/pytest-tornasync/pytest_tornasync-0.4.0-py3-none-any.whl/pytest_tornasync-0.4.0.dist-info/DESCRIPTION.rst
@@ -0,0 +1,151 @@
+================
+pytest-tornasync
+================
+
+.. image:: https://travis-ci.org/eukaryote/pytest-tornasync.svg?branch=master
+    :target: https://travis-ci.org/eukaryote/pytest-tornasync
+
+
+A simple pytest plugin that provides some helpful fixtures for testing
+Tornado apps and easy handling of plain (undecoratored) native coroutine tests
+(Python 3.5+).
+
+Why another Tornado pytest plugin when the excellent ``pytest-tornado`` already
+exists? The main reason is that I didn't want to have to decorate every test
+coroutine with ``@pytest.mark.gen_test``. This plugin doesn't have anything
+like ``gen_test``. Defining a test with ``async def`` is all that is required.
+
+
+Installation
+------------
+
+Install using pip, which must be run with Python 3.5+:
+
+.. code-block:: sh
+
+    pip install pytest-tornasync
+
+
+Usage
+-----
+
+Define an ``app`` fixture:
+
+.. code-block:: python
+
+    import pytest
+
+
+    @pytest.fixture
+    def app():
+        import yourapp
+        return yourapp.make_app()  # a tornado.web.Application
+
+
+Create tests as native coroutines using Python 3.5+ ``async def``:
+
+.. code-block:: python
+
+    async def test_app(http_server_client):
+        resp = await http_server_client.fetch('/')
+        assert resp.code == 200
+        # ...
+
+
+If you want to use the Tornado 3.2+ ``asyncio`` event loop, redefine the
+``io_loop`` fixture in your ``conftext.py`` as follows:
+
+.. code-block:: python
+
+    import pytest
+
+
+    @pytest.fixture
+    def io_loop(io_loop_asyncio)
+        return io_loop_asyncio
+
+
+
+Fixtures
+--------
+
+When the plugin is installed, then ``py.test --fixtures`` will show
+the fixtures that are available::
+
+http_server_port
+    Port used by `http_server`.
+http_server
+    Start a tornado HTTP server that listens on all available interfaces.
+
+    You must create an `app` fixture, which returns
+    the `tornado.web.Application` to be tested.
+
+    Raises:
+    FixtureLookupError: tornado application fixture not found
+http_server_client
+    Create an asynchronous HTTP client that can fetch from `http_server`.
+http_client
+    Create an asynchronous HTTP client that can fetch from anywhere.
+io_loop_tornado
+    Create a new `tornado.ioloop.IOLoop` for each test case.
+io_loop_asyncio
+    Create a new `tornado.platform.asyncio.AsyncIOLoop` for each test case.
+io_loop
+    Alias for `io_loop_tornado`, by default.
+
+    You may define an `io_loop` that uses the `io_loop_asyncio` fixture to
+    use an asyncio-backed Tornado event loop.
+
+
+
+Examples
+--------
+
+.. code-block:: python
+
+    import time
+
+    import tornado.web
+    import tornado.gen
+
+    import pytest
+
+
+    class MainHandler(tornado.web.RequestHandler):
+        def get(self):
+            self.write("Hello, world!")
+
+
+    @pytest.fixture
+    def app():
+        return tornado.web.Application([(r"/", MainHandler)])
+
+
+    async def test_http_server_client(http_server_client):
+        # http_server_client fetches from the `app` fixture and takes path
+        resp = await http_server_client.fetch('/')
+        assert resp.code == 200
+        assert resp.body == b"Hello, world!"
+
+
+    async def test_http_client(http_client):
+        # http_client fetches from anywhere and takes full URL
+        resp = await http_client.fetch('http://httpbin.org/status/204')
+        assert resp.code == 204
+
+
+    async def example_coroutine(period):
+        await tornado.gen.sleep(period)
+
+
+    async def test_example():
+        # no fixtures needed
+        period = 1.0
+        start = time.time()
+        await example_coroutine(period)
+        elapsed = time.time() - start
+        assert elapsed >= period
+
+
+
+
---
+++ packages/pytest-tornasync/pytest_tornasync-0.4.0-py3-none-any.whl/pytest_tornasync/plugin.py
@@ -0,0 +1,183 @@
+import inspect
+
+import tornado.ioloop
+import tornado.testing
+import tornado.simple_httpclient
+
+import pytest
+
+try:
+    iscoroutinefunction = inspect.iscoroutinefunction
+except AttributeError:
+    def iscoroutinefunction(obj):
+        return False
+
+
+def get_test_timeout(pyfuncitem):
+    timeout = pyfuncitem.config.option.async_test_timeout
+    marker = pyfuncitem.get_marker('timeout')
+    if marker:
+        timeout = marker.kwargs.get('seconds', timeout)
+    return timeout
+
+
+def pytest_addoption(parser):
+    parser.addoption('--async-test-timeout', type=float,
+                     help=('timeout in seconds before failing the test '
+                           '(default is no timeout)'))
+    parser.addoption('--app-fixture', default='app',
+                     help=('fixture name returning a tornado application '
+                           '(default is "app")'))
+
+
+@pytest.mark.tryfirst
+def pytest_pycollect_makeitem(collector, name, obj):
+    if collector.funcnamefilter(name) and iscoroutinefunction(obj):
+        return list(collector._genfunctions(name, obj))
+
+
+@pytest.mark.tryfirst
+def pytest_pyfunc_call(pyfuncitem):
+    funcargs = pyfuncitem.funcargs
+    testargs = {arg: funcargs[arg]
+                for arg in pyfuncitem._fixtureinfo.argnames}
+
+    if not iscoroutinefunction(pyfuncitem.obj):
+        pyfuncitem.obj(**testargs)
+        return True
+
+    try:
+        event_loop = pyfuncitem.funcargs['io_loop']
+    except KeyError:
+        # TODO: figure out best way to get the io_loop fixture, even if not
+        # used by this coroutine, and correctly run it regardless of whether
+        # it's a fixture or yield fixture (including pre-init and post-init)
+        coro = pyfuncitem.obj(**testargs)
+        loop = None
+        while not isinstance(loop, tornado.ioloop.IOLoop):
+            loop = next(io_loop())
+        try:
+            loop.run_sync(lambda: coro, timeout=get_test_timeout(pyfuncitem))
+        finally:
+            _loop_destroy(loop)
+        return True
+
+    if not isinstance(event_loop, tornado.ioloop.IOLoop):
+        raise TypeError("unsupported event loop:  %s" % type(event_loop))
+
+    event_loop.run_sync(lambda: pyfuncitem.obj(**testargs),
+                        timeout=get_test_timeout(pyfuncitem))
+    return True
+
+
+def _loop_create(klass):
+    loop = klass()
+    loop.make_current()
+    return loop
+
+
+def _loop_destroy(loop):
+    loop.clear_current()
+    if not type(loop).initialized() or loop is not type(loop).instance():
+        loop.close(all_fds=True)
+
+
+@pytest.yield_fixture
+def io_loop_tornado():
+    """
+    Create a new `tornado.ioloop.IOLoop` for each test case.
+    """
+    io_loop = _loop_create(tornado.ioloop.IOLoop)
+    yield io_loop
+    _loop_destroy(io_loop)
+
+
+@pytest.yield_fixture
+def io_loop_asyncio():
+    """
+    Create a new `tornado.platform.asyncio.AsyncIOLoop` for each test case.
+    """
+    io_loop = _loop_create(tornado.platform.asyncio.AsyncIOLoop)
+    yield io_loop
+    _loop_destroy(io_loop)
+
+
+# io_loop uses the plain Tornado event loop by default
+io_loop = io_loop_tornado
+
+
+@pytest.fixture
+def http_server_port():
+    """
+    Port used by `http_server`.
+    """
+    return tornado.testing.bind_unused_port()
+
+
+@pytest.yield_fixture
+def http_server(request, io_loop, http_server_port):
+    """Start a tornado HTTP server that listens on all available interfaces.
+
+    You must create an `app` fixture, which returns
+    the `tornado.web.Application` to be tested.
+
+    Raises:
+        FixtureLookupError: tornado application fixture not found
+    """
+    http_app = request.getfuncargvalue(request.config.option.app_fixture)
+    server = tornado.httpserver.HTTPServer(http_app, io_loop=io_loop)
+    server.add_socket(http_server_port[0])
+
+    yield server
+
+    server.stop()
+
+    if hasattr(server, 'close_all_connections'):
+        io_loop.run_sync(server.close_all_connections,
+                         timeout=request.config.option.async_test_timeout)
+
+
+class AsyncHTTPServerClient(tornado.simple_httpclient.SimpleAsyncHTTPClient):
+
+    def initialize(self, io_loop, *, http_server=None):
+        super().initialize(io_loop)
+        self._http_server = http_server
+
+    def fetch(self, path, **kwargs):
+        """
+        Fetch `path` from test server, passing `kwargs` to the `fetch`
+        of the underlying `tornado.simple_httpclient.SimpleAsyncHTTPClient`.
+        """
+        return super().fetch(self.get_url(path), **kwargs)
+
+    def get_protocol(self):
+        return 'http'
+
+    def get_http_port(self):
+        for sock in self._http_server._sockets.values():
+            return sock.getsockname()[1]
+
+    def get_url(self, path):
+        return '%s://127.0.0.1:%s%s' % (self.get_protocol(),
+                                        self.get_http_port(), path)
+
+
+@pytest.yield_fixture
+def http_server_client(http_server):
+    """
+    Create an asynchronous HTTP client that can fetch from `http_server`.
+    """
+    client = AsyncHTTPServerClient(http_server.io_loop,
+                                   http_server=http_server)
+    yield client
+    client.close()
+
+
+@pytest.yield_fixture
+def http_client(http_server):
+    """
+    Create an asynchronous HTTP client that can fetch from anywhere.
+    """
+    client = tornado.httpclient.AsyncHTTPClient(http_server.io_loop)
+    yield client
+    client.close()
