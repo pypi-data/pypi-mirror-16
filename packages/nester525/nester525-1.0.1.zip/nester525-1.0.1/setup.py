46f93a6710bc618511c1d15a732e7375fd3fc9cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:54 +0000

    Add package nester525/1.0.1/nester525-1.0.1.zip

---
+++ packages/nester525/nester525-1.0.1.zip/nester525-1.0.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+            name                   ='nester525',
+            version                 ='1.0.1',
+            py_modules         =['nester525'],
+            author                 ='john',
+            author_email        ='slkfjdlkj@qq.com',
+            url                        ='http://baidu.com',
+            description          ='A simple printer of nested lists',
+        )
