0ba86118d356c8af24859f2b3aeebf8a912e53b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:01 +0000

    Add package minuk_nester/1.3.0/minuk_nester-1.3.0.tar.gz

---
+++ packages/minuk_nester/minuk_nester-1.3.0.tar.gz/minuk_nester-1.3.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name = 'minuk_nester',
+	version = '1.3.0',
+	py_modules = ['minuk_nester'],
+	author = 'Minuk',
+	author_email = 'akalsdnr@kaist.ac.kr',
+	url = '',
+	description = 'A simple printer of nested lists',
+	)
