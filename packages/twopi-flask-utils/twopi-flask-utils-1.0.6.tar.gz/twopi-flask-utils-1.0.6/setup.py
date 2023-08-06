e4c2e3c2ed873b9c5dd90b8d25b57cdb0c68da93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:20 +0000

    Add package twopi-flask-utils/1.0.6/twopi-flask-utils-1.0.6.tar.gz

---
+++ packages/twopi-flask-utils/twopi-flask-utils-1.0.6.tar.gz/twopi-flask-utils-1.0.6/setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup
+import os
+import subprocess
+
+PACKAGE_PATH = './twopi_flask_utils'
+
+packages = []
+
+for item in os.listdir(PACKAGE_PATH):
+    path = os.path.join(PACKAGE_PATH, item)
+    if not item.startswith('_') and os.path.isdir(path):
+        packages.append(('twopi_flask_utils.' + item, path))
+
+extras = {
+    'ldap': [],
+    'restful': ['flask-restful'],
+    'celery': ['celery'],
+    'sentry': ['raven[flask]']
+}
+
+
+tests_require = []
+for x in extras.values():
+    tests_require.extend(x)
+
+def get_version():
+    v = subprocess.check_output(['git', 'describe', '--dirty', '--always'])
+    if type(v) == str:
+        return v
+    return v.decode('UTF-8')
+
+setup(
+    name='twopi-flask-utils',
+    version=get_version().strip(),
+    package_dir=dict(packages),
+    packages=list(map(lambda x: x[0], packages)),
+    extras_require=extras,
+    tests_require=tests_require,
+    test_suite='tests',
+    url='https://github.com/TwoPiCode/twopi-flask-utils',
+    author='Nick Whyte',
+    author_email='nick@twopicode.com',
+    description=('A set of utilities to make working with flask web '
+                 'applications easier.')
+)
