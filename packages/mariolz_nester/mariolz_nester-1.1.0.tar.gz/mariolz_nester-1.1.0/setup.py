42359035f6fe33cd00ec5c58c404d47ce3b6f74f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:19 +0000

    Add package mariolz_nester/1.1.0/mariolz_nester-1.1.0.tar.gz

---
+++ packages/mariolz_nester/mariolz_nester-1.1.0.tar.gz/mariolz_nester-1.1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+name         = "mariolz_nester",
+version      = '1.1.0',
+py_modules   = ['mariolz_nester'],
+author       = 'mariolz',
+author_email = '997388772@qq.com',
+url          = '',
+description  = 'return  the elements of nested lists ' 
+)
