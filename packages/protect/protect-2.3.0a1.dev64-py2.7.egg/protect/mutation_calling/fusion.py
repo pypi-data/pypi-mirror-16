23daa50d330880ec403aacb1e1a1076d8a7a1bd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:25 +0000

    Add package protect/2.3.0a1.dev64/protect-2.3.0a1.dev64-py2.7.egg

---
+++ packages/protect/protect-2.3.0a1.dev64-py2.7.egg/protect/mutation_calling/fusion.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python2.7
+# Copyright 2016 Arjun Arkal Rao
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from __future__ import print_function
+import os
+import csv
+import sys
+import multiprocessing
+from bd2k.util.expando import Expando
+from protect.common import get_files_from_filestore, docker_path, docker_call, export_results, \
+    untargz
+
+
+def run_fusion_caller(job, star_bam, univ_options, fusion_options):
+    """
+    This module will run a fusion caller on DNA bams.  This module will be
+    implemented in the future.
+
+    This module corresponds to node 10 on the tree
+    """
+    job.fileStore.logToMaster('Running FUSION on %s' % univ_options['patient'])
+    job.fileStore.logToMaster('Fusions are currently unsupported.... Skipping.')
+    fusion_file = job.fileStore.getLocalTempFile()
+    output_file = job.fileStore.writeGlobalFile(fusion_file)
+    return output_file
+
+
+def run_ericscript(job, fastqs, univ_options, ericscript_options):
+    """
+    Runs EricScript on tumor Rna-seq reads
+
+    ARGUMENTS
+    1. fastqs: Tuple for paired RNA-seq reads
+        fastqs
+            |- ('tumor_rna.1.fq': <JSid>, 'tumor_rna.2.fq1': <JSid>)
+    2. univ_options: Dict of universal arguments used by almost all tools
+         univ_options
+                +- 'java_Xmx': <java memory usage>
+    3. ericscript_options: Dict of parameters specific to EricScript
+         ericscript_options
+              |- 'database.tar.gz': <JSid for the EricScript database tarball>
+              +- 'n': <number of threads to allocate>
+
+    RETURN VALUES
+    1. EricScript filtered fusion calls <JSid>
+    """
+    job.fileStore.logToMaster('Running EricScript on %s' % univ_options['patient'])
+    work_dir = job.fileStore.getLocalTempDir()
+    input_files = {
+        'rna_1.fq': fastqs[0],
+        'rna_2.fq': fastqs[1],
+        'tool_index.tar.gz': ericscript_options['tool_index']
+    }
+    input_files = get_files_from_filestore(job, input_files, work_dir, docker=False)
+    input_files['tool_index'] = untargz(input_files['tool_index.tar.gz'], work_dir)
+    input_files['tool_index'] = os.path.basename(input_files['tool_index'])
+
+    cores = multiprocessing.cpu_count()
+    parameters = ['-db', input_files['tool_index'],
+		  '-o', '/data/output',
+                  '-name', 'fusions',
+                  '-p', str(cores),
+                  'rna_1.fq', 'rna_2.fq']
+
+    docker_call(tool='ericscript:0.5.5--065b274e9eb4bfb4fbe26a100e9c64ce7c1b735d',
+                tool_parameters=parameters, work_dir=work_dir, dockerhub='jpfeil')
+
+    output_path = os.path.join(work_dir, 'output/fusions.results.filtered.tsv')
+    filtered_fusions = job.fileStore.writeGlobalFile(output_path)
+    export_results(job, output_path, univ_options,
+                   subfolder='mutations/ericscript')
+    return job.addChildJobFn(reformat_ericscript_output, filtered_fusions, univ_options).rv()
+
+
+def parse_ericscript(infile):
+    """
+    Parses EricScript format and returns an Expando object with with basic features
+    """
+    reader = csv.reader(infile, delimiter='\t')
+    header = reader.next()
+    header = {key: index for index, key in enumerate(header)}
+
+    eric_features = ['GeneName1', 'EnsemblGene1', 'strand1', 'chr1',
+                     'GeneName2', 'EnsemblGene2', 'strand2', 'chr2',
+                     'Breakpoint1', 'Breakpoint2', 'spanningreads',
+                     'crossingreads', 'JunctionSequence']
+
+    for line in reader:
+        yield Expando(dict((feature, line[header[feature]]) for feature in eric_features))
+
+
+def reformat_ericscript_output(job, fusion_file, univ_options):
+    """
+    Writes EricScript format in Transgene fusion format and returns a file store ID
+    """
+    job.fileStore.logToMaster('Reformatting EricScript ouput for %s' % univ_options['patient'])
+    work_dir = job.fileStore.getLocalTempDir()
+    input_files = {'results.tsv': fusion_file}
+    input_files = get_files_from_filestore(job, input_files, work_dir, docker=False)
+    output_path = os.path.join(work_dir, 'general_format.tsv')
+    output_file = open(output_path, 'w')
+    writer = csv.writer(output_file, delimiter='\t')
+    header = ['#GeneName1', 'EnsemblGene1', 'strand1', 'chr1', 'Breakpoint1', 'DonorSeq',
+              'GeneName2', 'EnsemblGene2', 'strand2', 'chr2', 'Breakpoint2', 'AcceptorSeq']
+    writer.writerow(header)
+    for record in parse_ericscript(open(input_files['results.tsv'], 'r')):
+        five_prime_seq = ''.join(
+            [base.upper() for base in record.JunctionSequence if base.islower()])
+        three_prime_seq = ''.join([base for base in record.JunctionSequence if base.isupper()])
+        writer.writerow([record.GeneName1, record.EnsemblGene1, record.strand1, record.chr1,
+                         record.Breakpoint1, five_prime_seq,
+                         record.GeneName2, record.EnsemblGene2, record.strand2, record.chr2,
+                         record.Breakpoint2, three_prime_seq])
+    output_file.close()
+    return job.fileStore.writeGlobalFile(output_path)
+
+
