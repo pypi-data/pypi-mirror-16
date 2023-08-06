a20b9bfd7dab953d061bf559c392cabc7e6f1937
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:37 +0000

    Add package rmotr-b7-c1-g1-jobs-detectorr/0.0.4/rmotr-b7-c1-g1-jobs-detectorr-0.0.4.tar.gz

---
+++ packages/rmotr-b7-c1-g1-jobs-detectorr/rmotr-b7-c1-g1-jobs-detectorr-0.0.4.tar.gz/rmotr-b7-c1-g1-jobs-detectorr-0.0.4/setup.py
@@ -0,0 +1,46 @@
+import os
+from setuptools import setup
+from setuptools.command.test import test as TestCommand
+
+
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = ["tests/"]
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        #import here, cause outside the eggs aren't loaded
+        import sys, pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
+
+
+setup(
+    name='rmotr-b7-c1-g1-jobs-detectorr',
+    version='0.0.4',
+    description="rmotr.com Group Project | Jobs Detector",
+    author='Jessica Ngo',
+    author_email='jessica.ngo@live.com',
+    license='CC BY-SA 4.0 License',
+    packages=['jobs_detector'],
+    maintainer='rmotr.com',
+    tests_require=[
+        'pytest==2.9.1'
+    ],
+    install_requires=[
+        'responses',
+        'click'
+        ],
+    entry_points={
+        'console_scripts':['jobs_detector=jobs_detector.main:jobs_detector']
+    },
+    zip_safe=False,
+    cmdclass={'test': PyTest},
+)
