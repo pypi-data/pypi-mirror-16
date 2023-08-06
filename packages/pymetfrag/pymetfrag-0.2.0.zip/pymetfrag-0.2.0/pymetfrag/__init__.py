58ccc861ca77244fbbf88917246daee766cdbda0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:42 +0000

    Add package pymetfrag/0.2.0/pymetfrag-0.2.0.zip

---
+++ packages/pymetfrag/pymetfrag-0.2.0.zip/pymetfrag-0.2.0/pymetfrag/__init__.py
@@ -0,0 +1,6 @@
+# encoding: utf-8
+from __future__ import print_function
+
+from pymetfrag import (default_template, standard_settings, available_adducts,
+                       run_metfrag_on_emzed_spectrum, run_metfrag_on_peaklist,
+                       run_metfrag_on_emzed_spectra, run_metfrag_on_peaklists)
