feb71b3a159c857604e5c05e744a25f18067257a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:03 +0000

    Add package listenginefile2016/1.1.0/listenginefile2016-1.1.0.tar.gz

---
+++ packages/listenginefile2016/listenginefile2016-1.1.0.tar.gz/listenginefile2016-1.1.0/listenginefile2016.py
@@ -0,0 +1,12 @@
+#comment1
+#comment2
+def list_engine(the_list,level):
+        #comment3
+        #comment4
+        for each_item in the_list:
+                if isinstance(each_item,list):
+                        list_engine(each_item,level+1)
+                else:
+                        for tab_stop in range(level):
+                                print("\t",end='')
+                        print(each_item)
