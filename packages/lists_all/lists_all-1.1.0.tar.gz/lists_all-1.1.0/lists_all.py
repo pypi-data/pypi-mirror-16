963ffc45f8ead8fdbd2c5a40d6a10dbec841fbe2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:41 +0000

    Add package lists_all/1.1.0/lists_all-1.1.0.tar.gz

---
+++ packages/lists_all/lists_all-1.1.0.tar.gz/lists_all-1.1.0/lists_all.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# lists_all.py
+# A simple list viewer for nested items
+
+def print_lol(the_list, tab_levels):
+    ''' takes two arguments, "the_list" ,a list-nested or otherwise.
+    second argument "tab_levels" inserts tab-stops if a nested list is encountered.
+
+    returns each item recursively & on it's own line.
+    '''
+    for each_item in the_list:
+        if isinstance(each_item, list):
+            print_lol(each_item, tab_levels + 1)
+        else:
+            for tab_stop in range(tab_levels):
+                print('\t', end='')
+
+            print(each_item)
+
