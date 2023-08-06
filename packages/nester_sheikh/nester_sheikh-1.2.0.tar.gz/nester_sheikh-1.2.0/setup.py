890d38a2b16f84c48871c16e7f994d745d854a1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:57:11 +0000

    Add package nester_sheikh/1.2.0/nester_sheikh-1.2.0.tar.gz

---
+++ packages/nester_sheikh/nester_sheikh-1.2.0.tar.gz/nester_sheikh-1.2.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+	name = 'nester_sheikh',
+	version = '1.2.0',
+	py_modules = ['nester_sheikh'],
+	author = 'sheikh',
+	author_email = 'sadiruddin@gmail.com',
+	url = 'http://www.headfirstlabs.com',
+	description = 'A simple printer of nested lists',
+)
