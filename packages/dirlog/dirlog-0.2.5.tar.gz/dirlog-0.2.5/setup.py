21249a5cd64daedbde8f06e5013ef3386ea9a756
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:55 +0000

    Add package dirlog/0.2.5/dirlog-0.2.5.tar.gz

---
+++ packages/dirlog/dirlog-0.2.5.tar.gz/dirlog-0.2.5/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+try:
+    import fastentrypoints
+except ImportError:
+    from urllib import request
+    fastep = request.urlopen(
+        'https://raw.githubusercontent.com/ninjaaron/fast-entry_points/master/fastentrypoints.py'
+        )
+    namespace = {}
+    exec(fastep.read(), namespace)
+
+setup(
+    name='dirlog',
+    version='0.2.5',
+    description='keep a log of directories you visit to get back fast',
+    long_description=open('README.rst').read(),
+    url='https://github.com/ninjaaron/dirlog',
+    author='Aaron Christianson',
+    author_email='ninjaaron@gmail.com',
+    py_modules = ['dirlog'],
+    classifiers=['Programming Language :: Python :: 3.5'],
+    entry_points={'console_scripts': [
+        'dirlog=dirlog:install',
+        'dlog=dirlog:wrap',
+        'dirlog-cd=dirlog:main']},
+)
