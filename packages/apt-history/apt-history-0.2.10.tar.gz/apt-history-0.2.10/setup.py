cb7b6c509ecc22185209e94560d547b34114256d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:47 +0000

    Add package apt-history/0.2.10/apt-history-0.2.10.tar.gz

---
+++ packages/apt-history/apt-history-0.2.10.tar.gz/apt-history-0.2.10/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+setup(
+  name = 'apt-history',
+  packages = ['apt_history'],
+  version = '0.2.10',
+  description = 'apt history script',
+  author = 'Antti Leppa',
+  author_email = 'antti.leppa@metatavu.fi',
+  url = 'https://github.com/Metatavu/apt-history',
+  download_url = 'https://github.com/Metatavu/apt-history/tarball/0.2.10', 
+  keywords = ['apt'],
+  classifiers = [],
+  entry_points={
+    'console_scripts': [
+      'apt-history=apt_history:main',
+    ],
+  },
+)
