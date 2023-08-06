9751cd4b5f384f4d6100939694bc9633c94c182b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:50 +0000

    Add package pydmm/0.2/pydmm-0.2.zip

---
+++ packages/pydmm/pydmm-0.2.zip/pydmm-0.2/README.txt
@@ -0,0 +1,15 @@
+=====
+PyDMM
+=====
+PyDMM supports reading data from a digital multimeter (DMM).
+Typical usage looks like this::
+
+    from pydmm import read_dmm
+
+    try:
+        number = read_dmm(port=myport, timeout=3)
+        print(number)
+    except:
+        print("Error")
+
+
---
+++ packages/pydmm/pydmm-0.2.zip/pydmm-0.2/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+setup(
+  name = 'pydmm',
+  packages = ['pydmm'], # gleicher Name wie oben
+  version = '0.2',
+  description = 'Read data from a dmm',
+  long_description=open('README.txt').read(),
+  author = 'Michael Weigend',
+  author_email = 'mw@creative-informatics.de',
+  license='Creative Commons Attribution-Noncommercial-Share Alike license',
+  url= 'http://pypi.python.org/pypi/pydmm',
+#  url = 'https://github.com/mweigend/pydmm', # URL zum GitHub 
+#  download_url = 'https://github.com/mweigend/pydmm/tarball/0.1', 
+  keywords = ['dmm', 'digital multimeter'], 
+)
+
