b0873f60bde6fe6fbc8c4a6ff26d15ea93b979d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:35 +0000

    Add package gitfolio/0.8.0.4/gitfolio-0.8.0.4.zip

---
+++ packages/gitfolio/gitfolio-0.8.0.4.zip/gitfolio-0.8.0.4/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+setup(
+  name = 'gitfolio',
+  packages = ["gitfolio","gitfolio/mygitfolio"],
+  version = '0.8.0.4',
+  description = 'Gather all your GitHub repositories, and a small descripton of them into one file, and then send it off.',
+  author = 'Calder White',
+  author_email = 'calderwhite1@gmail.com',
+  url = 'https://github.com/CalderWhite/myGitFolio',
+  keywords = [],
+  classifiers = []
+)
