51e77bf348417025363440774a1e15839362f217
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:15 +0000

    Add package pprinter/0.0.1/pprinter-0.0.1.tar.gz

---
+++ packages/pprinter/pprinter-0.0.1.tar.gz/pprinter-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name = 'pprinter',
+    version = '0.0.1',
+    author = 'Koichi Nakamura',
+    author_email = 'koichi@idein.jp',
+    description = "Philip Wadler's pretty printer for Python",
+    license = 'MIT',
+    url = 'https://github.com/nineties/pprinter',
+    py_modules = ['pprinter']
+    )
