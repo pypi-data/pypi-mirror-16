e4e76d189b5e722bd72f24d4537dcc4684b35f83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:57:46 +0000

    Add package fishbowl/0.1.0/fishbowl-0.1.0-py2.py3-none-any.whl

---
+++ packages/fishbowl/fishbowl-0.1.0-py2.py3-none-any.whl/fishbowl/axes.py
@@ -0,0 +1,44 @@
+from functools import wraps
+from fishbowl.base import loads_from_json, saves_to_json
+
+
+@loads_from_json('fishbowl.axes.json')
+def axes(name):
+    """
+    Return configuration for named axes
+
+    name must be a saved (or default) configuration
+    """
+    # Only handled through the save/load system
+
+
+@saves_to_json('fishbowl.axes.json')
+def save_axes(name, config):
+    """
+    Save a new axes style as name.
+
+    config can be a dictionary of params or a named axes style
+    """
+    if isinstance(config, dict):
+        return config
+    return axes(config)
+
+
+def _despined(init):
+    """
+    Decorator to make the constructor of pyplot.Axes
+    return an axes without left right or top spines.
+    """
+    @wraps(init)
+    def despined_init(self, *args, **kwargs):
+        init(self, *args, **kwargs)
+        for spine in ["left", "right", "top"]:
+            self.spines[spine].set_visible(False)
+        self.xaxis.tick_bottom()
+        self.yaxis.tick_right()
+        for tick in self.xaxis.get_major_ticks():
+            tick.gridline.set_visible(False)
+    return despined_init
+
+
+_initialize_decorators = {'despined': _despined}
