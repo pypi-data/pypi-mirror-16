25cdad61db9baba60c6e859b106dbd9f2416e7eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:32 +0000

    Add package markdownmail/0.3.0/markdownmail-0.3.0.tar.gz

---
+++ packages/markdownmail/markdownmail-0.3.0.tar.gz/markdownmail-0.3.0/setup.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+_NAME = "markdownmail"
+
+setup(
+        name=_NAME,
+        description="E-mail with text and html content provided with markdown",
+        long_description=open("README").read(),
+        version='0.3.0',
+        author="Yaal Team",
+        author_email="contact@yaal.fr",
+        url="http://hg." + _NAME + ".yaal.fr",
+        packages=[
+            _NAME,
+            _NAME + "/styles",
+        ],
+        package_data={
+            '': ['default.css'],
+        },
+        install_requires=[
+            "Markdown==2.6.6",
+            "Envelopes==0.4",
+        ],
+        classifiers=[
+            "Programming Language :: Python",
+            "Development Status :: 4 - Beta",
+            "Intended Audience :: Developers",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+            "Topic :: Software Development :: Libraries :: Python Modules",
+            "Programming Language :: Python :: 2.7",
+            "Programming Language :: Python :: 3",
+        ],      
+)
+
