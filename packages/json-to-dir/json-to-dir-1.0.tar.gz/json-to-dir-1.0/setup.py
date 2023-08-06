1268cb024e2344e7d9ad121df316a3337e5a310f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:03:21 +0000

    Add package json-to-dir/1.0/json-to-dir-1.0.tar.gz

---
+++ packages/json-to-dir/json-to-dir-1.0.tar.gz/json-to-dir-1.0/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+import os
+
+setup(
+    name         = 'json-to-dir',
+    version      = '1.0',
+    author       = 'Giovanni T. Parra',
+    author_email = 'fiatjaf@gmail.com',
+    license      = 'MIT',
+    description  = 'Turns a JSON file into a directory structure.',
+    url          = 'https://github.com/fiatjaf/json-to-dir',
+    packages     = [],
+    package_data = {},
+    scripts          = ['json-to-dir'],
+    install_requires = []
+)
