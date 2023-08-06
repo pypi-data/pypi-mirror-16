d9666e1c39c2353c79a22acb84c1ee5a08c47aef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:36:11 +0000

    Add package apertium_lint/0.11/apertium_lint-0.11.tar.gz

---
+++ packages/apertium_lint/apertium_lint-0.11.tar.gz/apertium_lint-0.11/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='apertium_lint',
+      version='0.11',
+      description='Lint for apertium. Part of GSoC 2016',
+      url='https://gitlab.com/jpsinghgoud/apertium-lint',
+      author='Jaipal Singh Goud',
+      author_email='jpsinghgoud@gmail.com',
+      license='MIT',
+      packages=['apertium_lint'],
+      zip_safe=False)
