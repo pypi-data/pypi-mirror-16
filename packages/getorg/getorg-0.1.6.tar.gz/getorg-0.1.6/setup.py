e9043c3bf78ebf8dc1ce05cf7cab718115b78a55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:23 +0000

    Add package getorg/0.1.6/getorg-0.1.6.tar.gz

---
+++ packages/getorg/getorg-0.1.6.tar.gz/getorg-0.1.6/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='getorg',
+      version='0.1.6',
+      description='For mapping contributors to GitHub organizations',
+      url='http://github.com/getorg/getorg',
+      author='Stuart Geiger',
+      author_email='stuart@stuartgeiger.com',
+      license='MIT',
+      packages=['getorg'],
+      install_requires=[
+          'pygithub', 'ipywidgets', 'geopy'],
+      zip_safe=False)
