66a4cca9e1481fe37205e0a98d8a0cd62a116375
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:39 +0000

    Add package django-activeview/0.1.0/django-activeview-0.1.0.zip

---
+++ packages/django-activeview/django-activeview-0.1.0.zip/django-activeview-0.1.0/activeview/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+from __future__ import (absolute_import, division, print_function, unicode_literals)
+
+__version__ = '0.1.0'
+default_app_config = 'activeview.apps.ActiveViewAppConfig'
