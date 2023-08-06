d99f73608bbee6c67265f3ed0786558032aa5131
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:56 +0000

    Add package nesterasdd/1.0.0/nesterasdd-1.0.0.zip

---
+++ packages/nesterasdd/nesterasdd-1.0.0.zip/nesterasdd-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+		name       ='nesterasdd',
+		version    ='1.0.0',
+		py_modules =['nester'],
+		author     ='adonis',
+		author_email='87223110@qq.com',
+		url        ='http://www.headfirstlabs.com',
+		description='a simple printer os nester lists',
+
+
+	)
