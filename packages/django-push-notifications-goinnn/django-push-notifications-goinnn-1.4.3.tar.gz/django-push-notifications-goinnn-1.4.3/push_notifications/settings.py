d34f1218cf4858a6643da1f475f6de2fab5660d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:02 +0000

    Add package django-push-notifications-goinnn/1.4.3/django-push-notifications-goinnn-1.4.3.tar.gz

---
+++ packages/django-push-notifications-goinnn/django-push-notifications-goinnn-1.4.3.tar.gz/django-push-notifications-goinnn-1.4.3/push_notifications/settings.py
@@ -0,0 +1,28 @@
+from django.conf import settings
+
+PUSH_NOTIFICATIONS_SETTINGS = getattr(settings, "PUSH_NOTIFICATIONS_SETTINGS", {})
+
+PUSH_NOTIFICATIONS_SETTINGS.setdefault("DEBUG", settings.DEBUG)
+
+# GCM
+PUSH_NOTIFICATIONS_SETTINGS.setdefault("GCM_POST_URL", "https://android.googleapis.com/gcm/send")
+PUSH_NOTIFICATIONS_SETTINGS.setdefault("GCM_MAX_RECIPIENTS", 1000)
+PUSH_NOTIFICATIONS_SETTINGS.setdefault("GCM_ERROR_TIMEOUT", None)
+
+
+# APNS
+PUSH_NOTIFICATIONS_SETTINGS.setdefault("APNS_PORT", 2195)
+PUSH_NOTIFICATIONS_SETTINGS.setdefault("APNS_FEEDBACK_PORT", 2196)
+PUSH_NOTIFICATIONS_SETTINGS.setdefault("APNS_ERROR_TIMEOUT", None)
+PUSH_NOTIFICATIONS_SETTINGS.setdefault("APNS_MAX_NOTIFICATION_SIZE", 2048)
+
+if PUSH_NOTIFICATIONS_SETTINGS.get("DEBUG", True):
+	PUSH_NOTIFICATIONS_SETTINGS.setdefault("APNS_HOST", "gateway.sandbox.push.apple.com")
+	PUSH_NOTIFICATIONS_SETTINGS.setdefault("APNS_FEEDBACK_HOST", "feedback.sandbox.push.apple.com")
+else:
+	PUSH_NOTIFICATIONS_SETTINGS.setdefault("APNS_HOST", "gateway.push.apple.com")
+	PUSH_NOTIFICATIONS_SETTINGS.setdefault("APNS_FEEDBACK_HOST", "feedback.push.apple.com")
+
+
+# User model
+PUSH_NOTIFICATIONS_SETTINGS.setdefault("USER_MODEL", settings.AUTH_USER_MODEL)
