6f99b721c7ad5b2e48f1457a2aec551c62999973
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:50 +0000

    Add package ywbde1124/1.3.0/ywbde1124-1.3.0.zip

---
+++ packages/ywbde1124/ywbde1124-1.3.0.zip/ywbde1124-1.3.0/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+setup(name='ywbde1124',
+      version='1.3.0',
+      py_modules=['nested'],
+      author='ywb',
+      author_email='1161376439@qq.com',
+      url='http://service.mail.qq.com',
+      )
