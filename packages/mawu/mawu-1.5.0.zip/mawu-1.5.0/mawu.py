bfce309ae5167d05c61d86616aa232e8fe3f6171
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:59 +0000

    Add package mawu/1.5.0/mawu-1.5.0.zip

---
+++ packages/mawu/mawu-1.5.0.zip/mawu-1.5.0/mawu.py
@@ -0,0 +1,18 @@
+import sys
+"""This is the "mawu.py" module, and it provides one function called
+great_start() which prints lists that may or may not include nested lists"""
+
+def great_start(the_list, indent=False, level=0, fh=sys.stdout):
+    """This function takes one positional argument called "the_list", which
+is any python list (of - possibly - nested lists). Each data item in the
+provided list is (recursively) printed to the screen on it's own line.
+A second argument called "level" is used to insert tab-stops when a nested
+list is encounted."""
+    for each_item in the_list:
+        if isinstance(each_item, list):
+            great_start(each_item, indent, level+1, fh)
+        else:
+            if indent:
+                for tab_stop in range(level):
+                    print("\t", end='', file=fh)
+            print(each_item, file=fh)
