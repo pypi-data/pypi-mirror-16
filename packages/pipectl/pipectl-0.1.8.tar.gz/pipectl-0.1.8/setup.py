a31e08c37f38c1c567520668dfb4d1109c651bf4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:22:14 +0000

    Add package pipectl/0.1.8/pipectl-0.1.8.tar.gz

---
+++ packages/pipectl/pipectl-0.1.8.tar.gz/pipectl-0.1.8/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+
+setup(
+    name="pipectl",
+    version="0.1.8",
+    description="A CI/CD Build pipeline DSL",
+    license="MIT",
+    author="adamar",
+    author_email="none@none.com",
+    url="http://github.com/adamar/pipectl",
+    packages=find_packages(),
+    install_requires=[
+        "bamboo_api_v2",
+        "GitPython>=2.0.0"
+        ],
+    dependency_links=[
+        'git://github.com/adamar/python-bamboo-api.git#egg=bamboo_api_v2'
+        ],
+    scripts=['pipectl/pipectl'],
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+    ]
+)
