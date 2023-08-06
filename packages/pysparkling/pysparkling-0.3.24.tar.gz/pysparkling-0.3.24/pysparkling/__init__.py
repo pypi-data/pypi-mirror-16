9678577056ac168cc001109222b6e057c7ff07d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:28 +0000

    Add package pysparkling/0.3.24/pysparkling-0.3.24.tar.gz

---
+++ packages/pysparkling/pysparkling-0.3.24.tar.gz/pysparkling-0.3.24/pysparkling/__init__.py
@@ -0,0 +1,15 @@
+"""pysparkling module."""
+# flake8: noqa
+
+__version__ = '0.3.24'
+
+from .exceptions import (FileAlreadyExistsException,
+                         ConnectionException)
+
+from .rdd import RDD
+from .context import Context
+from .broadcast import Broadcast
+from .stat_counter import StatCounter
+from .cache_manager import CacheManager
+
+from . import fileio
