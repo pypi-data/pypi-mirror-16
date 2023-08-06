ba7e31c360c0e605552bcbcd4f6cb6ba83d0d51a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:01 +0000

    Add package regional/1.1.2/regional-1.1.2.tar.gz

---
+++ packages/regional/regional-1.1.2.tar.gz/regional-1.1.2/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+version = '1.1.2'
+
+setup(
+    name='regional',
+    version=version,
+    description='simple manipualtion and display of spatial regions',
+    author='freeman-lab',
+    author_email='the.freeman.lab@gmail.com',
+    packages=['regional'],
+    url='https://github.com/freeman-lab/regional',
+    install_requires=open('requirements.txt').read().split(),
+    long_description='See https://github.com/freeman-lab/regional'
+)
