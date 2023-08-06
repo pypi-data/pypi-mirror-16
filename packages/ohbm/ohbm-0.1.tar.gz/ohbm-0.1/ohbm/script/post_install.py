4daa7cbad75c102bf102f01a26da6367a8cea7ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:44 +0000

    Add package ohbm/0.1/ohbm-0.1.tar.gz

---
+++ packages/ohbm/ohbm-0.1.tar.gz/ohbm-0.1/ohbm/script/post_install.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+import os
+
+os.system("pip install rdflib")
+os.system("pip install rdflib-jsonld")
+print "Run server with command 'nidm'"
+
---
+++ packages/ohbm/ohbm-0.1.tar.gz/ohbm-0.1/ohbm/settings.py
@@ -0,0 +1,10 @@
+
+'''
+settings: part of the ohbm-api
+settings for the api
+
+'''
+
+
+base="https://ww5.aievolution.com/hbm1601"
+access_token="084D0DB576B7F03F45BDE95385115D11"
