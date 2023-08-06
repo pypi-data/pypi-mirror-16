e7e2954a8b8cf2a057ccabf543f2287a9432c027
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:05:28 +0000

    Add package django-rlog/1.0.6/django_rlog-1.0.6-py2.py3-none-any.whl

---
+++ packages/django-rlog/django_rlog-1.0.6-py2.py3-none-any.whl/django_rlog/management/commands/rstop.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+from django.conf import settings
+
+from django_rlog.defaults import DEFAULT_CHANNEL
+from django_six import CompatibilityBaseCommand
+
+
+r = settings.REDIS_CACHE
+
+
+class Command(CompatibilityBaseCommand):
+    help = 'Stop rlog.'
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--channel',
+            dest='channel',
+            default=DEFAULT_CHANNEL,
+            help='Pubsub channel RedisHandler usage.',
+        )
+
+    def handle(self, *args, **options):
+        r.publish(options.get('channel', DEFAULT_CHANNEL), 'KILL')
