01c81bf71ebe0009e7c1cfc414db24f48f8caf57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:22:34 +0000

    Add package eapi/0.4.2a/eapi-0.4.2a.tar.gz

---
+++ packages/eapi/eapi-0.4.2a.tar.gz/eapi-0.4.2a/eapi/__init__.py
@@ -0,0 +1,7 @@
+"""Das eapi Modul enthält eine Bibliothek zur Ansteuerung eines
+Eingabe-Ausgabe-Moduls für den Raspberry Pi.
+
+Es besteht aus verschiedenen Untermodulen. Schau in die Dokumentation der
+jeweiligen Untermodule, um mehr über die Funktionsweise zu erfahren. """
+
+VERSION = "0.4.2a"
