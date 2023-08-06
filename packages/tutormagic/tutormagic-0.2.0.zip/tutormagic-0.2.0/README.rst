c52881dc6cdda447ac98bdd5d4d574057b8efb41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:48 +0000

    Add package tutormagic/0.2.0/tutormagic-0.2.0.zip

---
+++ packages/tutormagic/tutormagic-0.2.0.zip/tutormagic-0.2.0/README.rst
@@ -0,0 +1,111 @@
+tutormagic extension for the Jupyter notebook
+=============================================
+
+IPython magics to embed http://www.pythontutor.com within an IFrame in
+the Jupyter notebook or to open a new tab in the browser using the code from an 
+IPython code cell (IPython kernel).
+
+Install
+-------
+
+.. code:: python
+
+    pip install tutormagic
+
+or
+
+.. code:: python
+
+    pip install git+https://github.com/kikocorreoso/tutormagic.git
+
+Tested on Python 2.7.x, 3.4.x and 3.5.x and IPython/Jupyter 3.x/4.x.
+
+Usage
+-----
+
+First, load the extension:
+
+.. code:: python
+
+    %load_ext tutormagic
+
+Once loaded, in a code cell in the notebook type the following:
+
+.. code:: python
+
+    %%tutor --lang python3
+    # some python code
+    # ...
+
+to create an IFrame within the notebook with the http://www.pythontutor.com page
+with the code included in the Jupyter code cell or:
+
+.. code:: python
+
+    %%tutor --lang python3 --tab
+    # some python code
+    # ...
+
+to open http://www.pythontutor.com page in a new browser tab with the code 
+included in the Jupyter code cell.
+
+Options
+-------
+
+The available options are ``--lang`` (or ``-l``), ``--height`` (or ``-h``) and 
+``--tab`` (or ``-t``):
+
+The ``--lang`` or ``-l`` option allows you to
+choose one of the available languages supported by
+`pythontutor <http://www.pythontutor.com>`__. It this option is set then
+it will consider the code in the cell as Python3 code.
+
+-  ``%%tutor --lang python3`` or ``%%tutor -l python3`` or ``%%tutor``
+   to show a pythontutor IFrame with **python3** code.
+-  ``%%tutor --lang python2`` or ``%%tutor -l python2`` to show a
+   pythontutor IFrame with **python2** code.
+-  ``%%tutor --lang java`` or ``%%tutor -l java`` to show a pythontutor
+   IFrame with **java** code.
+-  ``%%tutor --lang javascript`` or ``%%tutor -l javascript`` to show a
+   pythontutor IFrame with **javascript** code.
+-  ``%%tutor --lang typescript`` or ``%%tutor -l typescript`` to show a 
+   pythontutor IFrame with **typescript** code.
+-  ``%%tutor --lang ruby`` or ``%%tutor -l ruby`` to show a 
+   pythontutor IFrame with **ruby** code.
+-  ``%%tutor --lang c`` or ``%%tutor -l c`` to show a 
+   pythontutor IFrame with **c** code.
+-  ``%%tutor --lang c++`` or ``%%tutor -l c++`` to show a 
+   pythontutor IFrame with **c++** code.
+
+The ``--height`` or ``-h`` is used to define the height of the IFrame used to 
+embed http://pythontutor.com within the notebook. If the `--tab` option is used 
+this option will be ignored.
+
+The ``--tab`` or ``-t`` option will open http://pythontutor.com in a new tab 
+instead of within an IFrame within the notebook.
+
+Example (in spanish)
+--------------------
+
+`Example
+notebook <http://nbviewer.ipython.org/github/Pybonacci/notebooks/blob/master/tutormagic.ipynb>`__.
+
+Name of the extension
+---------------------
+
+The name of the extension was suggested by Doug S. Blank 
+(`@dsblank <https://github.com/dsblank>`__).
+
+Changelog
+---------
+Version 0.2.0
+~~~~~~~~~~~~~
+
+-  Added new ``--tab`` option (thanks to Holger Karl (`@hkarl <https://github.com/hkarl>`__)).
+-  Added new ``--height`` option (thanks to Tom Simonart(`@tomsimonart <https://github.com/tomsimonart>`__)).
+-  Added new languages available on http://pythontutor.com (Typescript, Ruby, C and C++).
+
+Version 0.1.0
+~~~~~~~~~~~~~
+
+-  Initial version
---
+++ packages/tutormagic/tutormagic-0.2.0.zip/tutormagic-0.2.0/tutormagic.py
@@ -0,0 +1,150 @@
+"""
+==========
+tutormagic
+==========
+
+Magics to display pythontutor.com in the notebook.
+
+Usage
+=====
+
+To enable the magics below, execute ``%load_ext tutormagic``.
+
+``%%tutormagic``
+
+{tutormagic_DOC}
+
+"""
+
+#-----------------------------------------------------------------------------
+# Copyright (C) 2015 Kiko Correoso and the pythontutor.com developers
+#
+# Distributed under the terms of the MIT License. The full license is in
+# the file LICENSE, distributed as part of this software.
+#
+# Contributors:
+#   kikocorreoso
+#-----------------------------------------------------------------------------
+
+import webbrowser
+import warnings
+warnings.simplefilter("always")
+import sys
+if sys.version_info.major == 2 and sys.version_info.minor == 7:
+    from urllib import quote
+elif sys.version_info.major == 3 and sys.version_info.minor >= 3:
+    from urllib.parse import quote
+else:
+    warnings.warn("This extension has not been tested on this Python version", 
+                  UserWarning)
+
+from IPython.core.magic import Magics, magics_class, cell_magic
+from IPython.testing.skipdoctest import skip_doctest
+from IPython.core.magic_arguments import (argument, magic_arguments,
+                                          parse_argstring)
+from IPython.utils.text import dedent
+from IPython.display import display, IFrame
+
+@magics_class
+class TutorMagics(Magics):
+    """
+    A magic function to show pythontutor.com frame from a code cell.
+
+    """
+    def __init__(self, shell):
+        super(TutorMagics, self).__init__(shell)
+
+    @skip_doctest
+    @magic_arguments()
+    @argument(
+        '-l', '--lang', action='store', nargs = 1,
+        help="Languages to be displayed within the iframe or in a new tab. "
+             "Possible values are: "
+             "python2, python3, java, javascript, typescript, ruby, c, c++"
+        )
+
+    @argument(
+        '-h', '--height', action='store', nargs=1,
+        help="Change the height of the output area display"
+        )
+
+    @argument(
+        '-t', '--tab', action='store_true',
+        help="Open pythontutor in a new tab",
+        )
+
+    #@needs_local_scope
+    @argument(
+        'code',
+        nargs='*',
+        )
+    @cell_magic
+    def tutor(self, line, cell=None, local_ns=None):
+        '''
+        Create an iframe embedding the pythontutor.com page
+        with the code included in the code cell::
+
+        In [1]: %%tutor -l 'python3'
+        ....: a = 1
+        ....: b = 1
+        ....: a + b
+
+        [You will see an iframe with the pythontutor.com page including the 
+        code above]
+        '''
+        args = parse_argstring(self.tutor, line)
+
+        if args.lang:
+            if args.lang[0].lower() in ['python2', 'python3', 
+                                        'java', 
+                                        'javascript',
+                                        'typescript',
+                                        'ruby',
+                                        'c',
+                                        'c++']:
+                lang = args.lang[0].lower()
+            else:
+                raise ValueError(
+                    "{} not supported. Only the following options are allowed: "
+                    "'python2', 'python3', 'java', 'javascript', "
+                    "'typescript', 'ruby', 'c', 'c++'".format(args.lang[0]))
+        else:
+            lang = "python3"
+
+        url = "http://pythontutor.com/iframe-embed.html#code="
+        url += quote(cell)
+        url += "&origin=opt-frontend.js&cumulative=false&heapPrimitives=false"
+        url += "&textReferences=false&"
+        if lang == "python3":
+            url += "py=3&rawInputLstJSON=%5B%5D&curInstr=0&codeDivWidth=50%25&codeDivHeight=100%25"
+        if lang == "python2":
+            url += "py=2&rawInputLstJSON=%5B%5D&curInstr=0&codeDivWidth=50%25&codeDivHeight=100%25"
+        if lang == "java":
+            url += "py=java&rawInputLstJSON=%5B%5D&curInstr=0&codeDivWidth=50%25&codeDivHeight=100%25"
+        if lang == "javascript":
+            url += "py=js&rawInputLstJSON=%5B%5D&curInstr=0&codeDivWidth=50%25&codeDivHeight=100%25"
+        if lang == "typescript":
+            url += "py=ts&rawInputLstJSON=%5B%5D&curInstr=0&codeDivWidth=50%25&codeDivHeight=100%25"
+        if lang == "ruby":
+            url += "py=ruby&rawInputLstJSON=%5B%5D&curInstr=0&codeDivWidth=50%25&codeDivHeight=100%25"
+        if lang == "c":
+            url += "py=c&rawInputLstJSON=%5B%5D&curInstr=0&codeDivWidth=50%25&codeDivHeight=100%25"
+        if lang == "c++":
+            url += "py=cpp&rawInputLstJSON=%5B%5D&curInstr=0&codeDivWidth=50%25&codeDivHeight=100%25"
+
+        # Display in new tab, or in iframe?
+        if args.tab:
+            webbrowser.open_new_tab(url)
+        else:
+            # Display the results in the output area
+            if args.height:
+                display(IFrame(url, height = int(args.height[0]), width = "100%"))
+            else:
+                display(IFrame(url, height = 350, width = "100%"))
+
+__doc__ = __doc__.format(
+    tutormagic_DOC = dedent(TutorMagics.tutor.__doc__))
+
+def load_ipython_extension(ip):
+    """Load the extension in IPython."""
+    ip.register_magics(TutorMagics)
