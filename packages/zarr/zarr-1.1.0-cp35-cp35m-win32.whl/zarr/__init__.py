2c2709c9eaea9fe436d6346058c4293cc62d9d7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:12 +0000

    Add package zarr/1.1.0/zarr-1.1.0-cp35-cp35m-win32.whl

---
+++ packages/zarr/zarr-1.1.0-cp35-cp35m-win32.whl/zarr/__init__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# flake8: noqa
+from __future__ import absolute_import, print_function, division
+import multiprocessing
+import atexit
+
+
+from zarr.creation import create, array, empty, zeros, ones, full, open, \
+    empty_like, zeros_like, ones_like, full_like, open_like
+from zarr.storage import init_store, DirectoryStore
+from zarr.core import Array
+from zarr.sync import ThreadSynchronizer, ProcessSynchronizer, \
+    SynchronizedArray
+from zarr.version import version as __version__
+
+
+try:
+    from zarr import blosc
+except ImportError:  # pragma: no cover
+    pass
+else:
+    ncores = multiprocessing.cpu_count()
+    blosc.init()
+    blosc.set_nthreads(min(8, ncores))
+    atexit.register(blosc.destroy)
