868c045201a83248fddcec167fa08f08d216b378
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:58 +0000

    Add package yjx-nester/1.3.0/yjx-nester-1.3.0.tar.gz

---
+++ packages/yjx-nester/yjx-nester-1.3.0.tar.gz/yjx-nester-1.3.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name            = 'yjx-nester',
+    version         = '1.3.0',
+    py_modules      = ['nester'],
+    author          = ['yjx-python'],
+    author_email    = 'xinpengyu@163.com',
+    url             = 'http://www.headfirstlabs.com',
+    description     = 'A simple printer of nested lists',
+)
