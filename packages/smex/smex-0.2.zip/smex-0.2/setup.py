3fed040f14f43cc25d36f8d5dc75d7a0de17983a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:58:47 +0000

    Add package smex/0.2/smex-0.2.zip

---
+++ packages/smex/smex-0.2.zip/smex-0.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='smex',
+      version='0.2',
+      description='A little state machine',
+      url='https://github.com/enthus1ast/smex',
+      author='David Krause',
+      author_email='david@klangfragment.de',
+      # license='MIT',
+      packages=['smex']
+      # zip_safe=False
+      )
---
+++ packages/smex/smex-0.2.zip/smex-0.2/smex/pissbot.py
@@ -0,0 +1,52 @@
+""" 
+	This is an example of a full finite state machine utilizing smex.
+	It will drink and piss and sleep for you.
+"""
+
+from smex import SM
+import time 
+import sys
+import random
+
+def drinking(what="beer"):
+	print ("DRINKING", what)
+	print(this.pissLevel)
+	this.pissLevel +=  random.randint(1,10)
+	time.sleep(0.2)
+	if this.pissLevel >= 10:
+		SM.go(pissing)
+	else:
+		SM.go(drinking)
+
+def pissing():
+	# raise ValueError("LOOL")
+	if this.pissLevel > 10:
+		sys.stdout.write("PISSING ")
+		sys.stdout.flush()
+		sys.stdout.write("\n")
+		time.sleep(0.5)
+		this.pissLevel = 0 #
+	SM.go(sleeping)
+
+def sleeping():
+	print("zzZZZzzzZZZzzzZZZ")
+	time.sleep(5.2)
+	SM.go(drinking,what="SCHNAPS")
+
+
+def errorState():
+	print("the pissbot is broken, we go sleeping to recover from the shock : ) ")
+	time.sleep(1)
+	SM.go(sleeping)
+
+#def transition():
+
+pissbot = SM()
+pissbot.debug(True)
+pissbot.pissLevel = 10
+pissbot.add(drinking)
+pissbot.add(pissing)
+pissbot.add(sleeping)
+pissbot.add(errorState)
+pissbot.errorState("errorState")
+pissbot.start(drinking,what="beer")
