916d101c4b2b1fa1a4eb81af67e41767218bb11a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:48 +0000

    Add package torPythonInt/1.1/torPythonInt-1.1.tar.gz

---
+++ packages/torPythonInt/torPythonInt-1.1.tar.gz/torPythonInt-1.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'torPythonInt',
+  version = '1.1',
+  packages = ['torPythonInt'],
+  description = 'An easy interface to use tor with python',
+  author = 'Yassir Karroum',
+  author_email = 'ukarroum17@gmail.com',
+  url = 'https://github.com/ukarroum/tor-python-interface',
+  download_url = 'https://github.com/ukarroum/tor-python-interface/tarball/1.2',
+  keywords = ['tor', 'interface'],
+  classifiers = [], requires=['stem']
+)
