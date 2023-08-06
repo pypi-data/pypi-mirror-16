4d0cbc9240507af892d2de1f050d606fa8dcf19b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:54 +0000

    Add package pypdfparse/0.3/pypdfparse-0.3.tar.gz

---
+++ packages/pypdfparse/pypdfparse-0.3.tar.gz/pypdfparse-0.3/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'pypdfparse',
+  packages = ['pypdfparse'],
+  version = '0.3',
+  description = 'A PDF inspector',
+  author = 'Opticaliqlusion',
+  author_email = 'john.laky1@gmail.com',
+  url = 'https://github.com/opticaliqlusion/pypdf',
+  download_url = 'https://github.com/opticaliqlusion/pypdf/tarball/0.3',
+  keywords = ['pdf', 'scanner'],
+  classifiers = [],
+)
