f441ddc949408bff3dd8c9121633f0a293dc4e10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:05:24 +0000

    Add package pcrunner/0.2.5/pcrunner-0.2.5.tar.gz

---
+++ packages/pcrunner/pcrunner-0.2.5.tar.gz/pcrunner-0.2.5/HISTORY.rst
@@ -0,0 +1,38 @@
+=======
+History
+=======
+
+0.2.5 (2016-08-20)
+------------------
+
+* Updated Python installation documentation with new versions.
+
+
+0.2.4 (2016-08-13)
+------------------
+
+* xrange -> range for python3 compatibility.
+
+
+0.2.3 (2016-08-13)
+------------------
+
+* Travis/tox fix
+
+
+0.2.2 (2016-08-13)
+------------------
+
+*  ISC License
+
+
+0.2.1 (2016-08-13)
+------------------
+
+* Documentation RPM build updated.
+
+
+0.2.0 (2016-08-12)
+------------------
+
+* First release on PyPI.
---
+++ packages/pcrunner/pcrunner-0.2.5.tar.gz/pcrunner-0.2.5/setup.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim: ts=4 et sw=4 sts=4 ft=python fenc=UTF-8 ai
+# setup.py
+
+import sys
+from setuptools import setup
+from setuptools.command.test import test as TestCommand
+
+
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def run_tests(self):
+        # Import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
+
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = [
+    'PyYAML==3.11',
+]
+
+test_requirements = [
+    'pytest==2.9.2',
+    'pytest-cov==2.3.1',
+    'tox==2.3.1',
+]
+
+# Add Python 2.6-specific dependencies
+if sys.version_info[:2] < (2, 7):
+    requirements.append('argparse')
+
+# Add Windows-specific dependencies
+if sys.platform == 'win32':
+    requirements.append('pywin32')
+
+setup(
+    name='pcrunner',
+    version='0.2.5',
+    description='Pcrunner (Passive Checks Runner)',
+    long_description=readme + '\n\n' + history,
+    author='Maarten Diemel',
+    author_email='maarten@maartendiemel.nl',
+    url='https://github.com/maartenq/pcrunner',
+    scripts=['scripts/check_dummy.py'],
+    packages=[
+        'pcrunner',
+    ],
+    package_dir={'pcrunner': 'pcrunner'},
+    entry_points={
+        'console_scripts': [
+            'pcrunner = pcrunner.main:main',
+        ]
+    },
+    include_package_data=True,
+    install_requires=requirements,
+    license="ISC license",
+    zip_safe=False,
+    keywords='pcrunner',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: ISC License (ISCL)',
+        'Natural Language :: English',
+        'Operating System :: POSIX',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Documentation :: Sphinx',
+        'Topic :: Utilities',
+    ],
+    test_suite='tests',
+    tests_require=test_requirements,
+    cmdclass={'test': PyTest},
+)
