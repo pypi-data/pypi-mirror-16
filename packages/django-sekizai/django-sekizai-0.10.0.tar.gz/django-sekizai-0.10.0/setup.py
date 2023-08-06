051f1dd29f117588075266814615492bfe02733f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:05 +0000

    Add package django-sekizai/0.10.0/django-sekizai-0.10.0.tar.gz

---
+++ packages/django-sekizai/django-sekizai-0.10.0.tar.gz/django-sekizai-0.10.0/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+version = __import__('sekizai').__version__
+
+
+setup(
+    name='django-sekizai',
+    version=version,
+    description='Django Sekizai',
+    author='Jonas Obrist',
+    author_email='ojiidotch@gmail.com',
+    url='http://github.com/ojii/django-sekizai',
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=[
+        'django-classy-tags>=0.3.1',
+    ],
+    test_suite='runtests.main',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ]
+)
