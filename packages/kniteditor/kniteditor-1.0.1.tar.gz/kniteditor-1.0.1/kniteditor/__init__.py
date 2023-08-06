2cb5be0ce129957774ecc48871e3ebca9352dfc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:39 +0000

    Add package kniteditor/1.0.1/kniteditor-1.0.1.tar.gz

---
+++ packages/kniteditor/kniteditor-1.0.1.tar.gz/kniteditor-1.0.1/kniteditor/__init__.py
@@ -0,0 +1,11 @@
+"""An editor for knitting projects."""
+
+__version__ = "1.0.1"
+
+
+def main(*args, **kw):
+    """Open the kniteditor window."""
+    from .EditorWindow import main
+    main()
+
+__all__ = ["main"]
