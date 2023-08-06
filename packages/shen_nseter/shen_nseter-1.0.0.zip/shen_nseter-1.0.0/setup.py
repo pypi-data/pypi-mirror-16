fb30fae79a99790c8341972bfee4c5e980f2a4de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:45 +0000

    Add package shen_nseter/1.0.0/shen_nseter-1.0.0.zip

---
+++ packages/shen_nseter/shen_nseter-1.0.0.zip/shen_nseter-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	 	name		= 'shen_nseter',
+	 	version		= '1.0.0',
+	 	py_modules	= ['shen_nester'], 
+	 	author		= 'shen_hfpython', 
+	 	author_email= 'shen_hfpython@headfirsttlabs.com',
+	 	url			= 'http://www.headfirstlavs.com',
+	 	description	= 'A simple printer of shen_nested lists',
+	 )
