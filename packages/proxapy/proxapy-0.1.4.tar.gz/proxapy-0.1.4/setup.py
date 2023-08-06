c3342908d6398ec948b3c9a3ab98e70c5b946d89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:53 +0000

    Add package proxapy/0.1.4/proxapy-0.1.4.tar.gz

---
+++ packages/proxapy/proxapy-0.1.4.tar.gz/proxapy-0.1.4/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+with open('README.rst') as README:
+    long_description = README.read()
+
+setup(name='proxapy',
+      version='0.1.4',
+      description='Simple API proxy that uses Flask/requests/gunicorn.',
+      long_description=long_description,
+      install_requires=[
+          'flask',
+          'gunicorn',
+          'requests'
+      ],
+      url='http://github.com/enricobacis/proxapy',
+      author='Enrico Bacis',
+      author_email='enrico.bacis@gmail.com',
+      license='MIT',
+      packages=['proxapy'],
+      zip_safe=False,
+      scripts=['scripts/proxapy'],
+      keywords='api proxy server flask requests gunicorn')
