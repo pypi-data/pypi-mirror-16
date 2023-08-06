6e36f87f218649cd16e39d7d425d715e298c4392
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:16 +0000

    Add package zopyx.plone.hyphenator/0.1.1/zopyx.plone.hyphenator-0.1.1.zip

---
+++ packages/zopyx.plone.hyphenator/zopyx.plone.hyphenator-0.1.1.zip/zopyx.plone.hyphenator-0.1.1/zopyx/plone/hyphenator/browser/resources/local.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+
+    selectors = [];
+
+    var hyphenatorSettings = {
+       selectorfunction: function () {
+            return $(selectors.join(',')).get();
+       }
+    };  
+
+    $.get(PORTAL_URL + '/@@hyphenator-configuration').success(function(data) {
+        data = $.parseJSON(data);
+        selectors = data['selectors'];
+        Hyphenator.config(hyphenatorSettings);
+        Hyphenator.run();
+    });
+});
