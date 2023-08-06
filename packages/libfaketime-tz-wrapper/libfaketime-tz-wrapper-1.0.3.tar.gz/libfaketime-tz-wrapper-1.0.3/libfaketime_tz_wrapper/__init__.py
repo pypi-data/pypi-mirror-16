6e9c984003eb54221c545eb03bbc8f2b338f1547
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:28 +0000

    Add package libfaketime-tz-wrapper/1.0.3/libfaketime-tz-wrapper-1.0.3.tar.gz

---
+++ packages/libfaketime-tz-wrapper/libfaketime-tz-wrapper-1.0.3.tar.gz/libfaketime-tz-wrapper-1.0.3/libfaketime_tz_wrapper/__init__.py
@@ -0,0 +1,26 @@
+from __future__ import print_function
+
+import dateutil.parser
+import libfaketime
+import threading
+
+from datetime import datetime
+from django.utils import timezone
+
+
+
+class fake_time(libfaketime.fake_time):
+    def __init__(self, datetime_spec, *args, **kwargs):
+        datetime_spec = self._prepare(datetime_spec)
+        super(fake_time, self).__init__(datetime_spec, *args, **kwargs)
+
+    def _should_fake(self):
+        return self.only_main_thread and isinstance(threading.current_thread(), threading._MainThread)
+
+    def _prepare(self, spec):
+        dt = spec if isinstance(spec, datetime) else dateutil.parser.parse(spec)
+
+        if timezone.is_aware(dt):
+            return timezone.make_naive(dt)
+        else:
+            return dt
