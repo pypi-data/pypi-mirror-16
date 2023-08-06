6b79e3a4dc61700da665ccbe01bbeb6b9f293d9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:18 +0000

    Add package Khayyam/3.0.9/Khayyam-3.0.9-cp35-cp35m-win_amd64.whl

---
+++ packages/Khayyam/Khayyam-3.0.9-cp35-cp35m-win_amd64.whl/khayyam/__init__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from .constants import *
+from .jalali_date import JalaliDate
+from .jalali_datetime import JalaliDatetime
+from .timezones import TehranTimezone, Timezone
+
+__version__ = '3.0.9'
+
+
+teh_tz = TehranTimezone()
+__author__ = 'vahid'
+
+__all__ = [
+    'MINYEAR',
+    'MAXYEAR',
+    'SATURDAY',
+    'SUNDAY',
+    'MONDAY',
+    'THURSDAY',
+    'WEDNESDAY',
+    'TUESDAY',
+    'FRIDAY',
+    'JalaliDate',
+    'JalaliDatetime',
+    'TehranTimezone',
+    'Timezone',
+    'teh_tz'
+]
