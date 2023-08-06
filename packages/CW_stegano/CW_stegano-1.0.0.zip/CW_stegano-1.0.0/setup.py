c8f163b31275fcacfdb2d17c862215ee26d56e6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:21 +0000

    Add package CW_stegano/1.0.0/CW_stegano-1.0.0.zip

---
+++ packages/CW_stegano/CW_stegano-1.0.0.zip/CW_stegano-1.0.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+files = ["steganography"]
+setup(
+  name = 'CW_stegano',
+  packages = files,
+  version = '1.0.0',
+  description = 'A fun little python script that encodes and decodes strings and images',
+  author = 'Calder White',
+  author_email = 'calderwhite1@gmail.com',
+  url = 'https://github.com/CalderWhite/steganography',
+  keywords = [],
+  classifiers = []
+)
