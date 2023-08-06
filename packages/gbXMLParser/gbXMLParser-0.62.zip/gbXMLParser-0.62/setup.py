dd0176b74cbc6509a8ecba1e21f3243fdcce7940
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:28 +0000

    Add package gbXMLParser/0.62/gbXMLParser-0.62.zip

---
+++ packages/gbXMLParser/gbXMLParser-0.62.zip/gbXMLParser-0.62/setup.py
@@ -0,0 +1,17 @@
+'''
+Created on Jul 25, 2016
+
+@author: t_songr
+'''
+from setuptools import setup
+
+setup(name='gbXMLParser',
+      version='0.62',
+      description='Parser for gbXML file',
+      url='https://git.autodesk.com/t-songr/gbXML_parser',
+      author='Runsheng Song',
+      author_email='runsheng@umail.ucsb.edu',
+      license='MIT',
+      packages=['gbXMLParser'],
+      include_package_data=True,
+      zip_safe=False)
