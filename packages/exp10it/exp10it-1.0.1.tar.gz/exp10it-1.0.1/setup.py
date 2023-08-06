26e08bdeb889f16e4184d6a92ca04282052e1ff0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:10 +0000

    Add package exp10it/1.0.1/exp10it-1.0.1.tar.gz

---
+++ packages/exp10it/exp10it-1.0.1.tar.gz/exp10it-1.0.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='exp10it',
+    version='1.0.1',
+    packages=['.'],
+    url='https://3xp10it.github.io',
+    license='',
+    author='quanyechavshuo',
+    author_email='quanyechavshuo@gmail.com',
+    description='My pipy module'
+)
