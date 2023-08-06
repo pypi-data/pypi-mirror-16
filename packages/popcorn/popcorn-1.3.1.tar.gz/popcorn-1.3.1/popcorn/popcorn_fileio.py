ffb89ca48263ef9152564b43e2070569492df523
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:36 +0000

    Add package popcorn/1.3.1/popcorn-1.3.1.tar.gz

---
+++ packages/popcorn/popcorn-1.3.1.tar.gz/popcorn-1.3.1/popcorn/popcorn_fileio.py
@@ -0,0 +1,10 @@
+'''
+Popcorn 1.3.1
+https://github.com/InitializeSahib/Popcorn
+'''
+def write_string_to_file(filename, string_to_write):
+	with open(filename, "wt") as outstr:
+		outstr.write(string_to_write)
+def read_file(filename):
+	with open(filename, "rt") as instr:
+		return instr.read()
