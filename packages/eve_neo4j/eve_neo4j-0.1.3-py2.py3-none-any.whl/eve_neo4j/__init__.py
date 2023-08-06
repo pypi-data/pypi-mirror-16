51a7441dfae877823c722ed96c2862b92a8ba14b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:48 +0000

    Add package eve_neo4j/0.1.3/eve_neo4j-0.1.3-py2.py3-none-any.whl

---
+++ packages/eve_neo4j/eve_neo4j-0.1.3-py2.py3-none-any.whl/eve_neo4j/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'Rodrigo Rodriguez'
+__email__ = 'rrodriguez@grupoabraxas.com'
+__version__ = '0.1.0'
+
+# flake8: noqa
+from eve_neo4j.neo4j import Neo4j
+from eve_neo4j.validation import ValidatorNeo4j
