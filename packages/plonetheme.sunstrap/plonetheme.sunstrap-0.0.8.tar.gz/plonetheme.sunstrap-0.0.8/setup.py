d5f3320bf1c4bef303b2ea25e2fd231ec7076c24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:30 +0000

    Add package plonetheme.sunstrap/0.0.8/plonetheme.sunstrap-0.0.8.tar.gz

---
+++ packages/plonetheme.sunstrap/plonetheme.sunstrap-0.0.8.tar.gz/plonetheme.sunstrap-0.0.8/setup.py
@@ -0,0 +1,32 @@
+from setuptools import find_packages
+from setuptools import setup
+
+VERSION = '0.0.8'
+
+setup(author='Alex Clark',
+      author_email='aclark@aclark.net',
+      classifiers=[
+          'Framework :: Plone :: 4.3',
+          'Programming Language :: Python :: 2.7',
+      ],
+      description='Plone theme',
+      entry_points={
+          'z3c.autoinclude.plugin': 'target = plone',
+      },
+      keywords='Bootstrap Diazo Modern Plone Sunburst Theme Theming',
+      license='GPL2',
+      include_package_data=True,
+      install_requires=[
+          'setuptools',
+      ],
+      long_description=open('README.rst').read() + '\n' +
+      open('CHANGES.rst').read(),
+      name='plonetheme.sunstrap',
+      namespace_packages=[
+          'plonetheme',
+      ],
+      packages=find_packages(),
+      test_suite='',
+      url='https://github.com/aclark4life/plonetheme.sunstrap',
+      version=VERSION,
+      zip_safe=False, )
