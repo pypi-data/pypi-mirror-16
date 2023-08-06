0f6c047ee7c8f0bae81ac1f6b9434e531028b1ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:57 +0000

    Add package arearec/1.0.0/arearec-1.0.0.tar.gz

---
+++ packages/arearec/arearec-1.0.0.tar.gz/arearec-1.0.0/arearec.py
@@ -0,0 +1,7 @@
+'''Area of a rectangle is demonstrated in this script'''
+
+def zing(l,b):
+ area=l*b
+ print(area)
+
+ zing(10,20)
---
+++ packages/arearec/arearec-1.0.0.tar.gz/arearec-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+        name = 'arearec',
+        version = '1.0.0',
+        py_modules = ['arearec',],
+        author = 'suryapoojary',
+        author_email = 'suryapjr@gmail.com',
+        description = 'A simple formula for calculating area of a rectangle',
+        )
+
