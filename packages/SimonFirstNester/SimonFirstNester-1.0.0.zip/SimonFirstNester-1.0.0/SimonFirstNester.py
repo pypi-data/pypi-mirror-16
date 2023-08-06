345c2c87db20f4c1881eb2024e32e4bfb739b2d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:17 +0000

    Add package SimonFirstNester/1.0.0/SimonFirstNester-1.0.0.zip

---
+++ packages/SimonFirstNester/SimonFirstNester-1.0.0.zip/SimonFirstNester-1.0.0/SimonFirstNester.py
@@ -0,0 +1,13 @@
+"""This is the "SimonFirstNester.py" module and it provides one function called print_list()
+    which prints lists that may or may not include nested lists."""
+
+
+def print_list(the_list):
+    """This function takes one positional argument called "the_list", which is any Python
+        list(of - possibly - nested lists). Each data item in the provided list is
+         (recursively) printed to the screen on it's own line. """
+    for item in the_list:
+        if isinstance(item, list):
+            print_list(item)
+        else:
+            print(item)
