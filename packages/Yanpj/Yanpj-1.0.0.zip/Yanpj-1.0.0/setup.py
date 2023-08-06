348b90dce9f5315ba7e489b55ce3b0a3706bccba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:22:05 +0000

    Add package Yanpj/1.0.0/Yanpj-1.0.0.zip

---
+++ packages/Yanpj/Yanpj-1.0.0.zip/Yanpj-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+          name='Yanpj',
+		  version='1.0.0',
+		  py_modules=['Yanpj'],
+		  author= 'ypj',
+		  authot_email='yanpj@gmail.com',
+		  description= 'sample',
+
+)
