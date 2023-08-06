849ce5358f845a0bcc8250a3913bc42d9724b748
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:13 +0000

    Add package fnv1apc/0.1.2a/fnv1apc-0.1.2a.tar.gz

---
+++ packages/fnv1apc/fnv1apc-0.1.2a.tar.gz/fnv1apc-0.1.2a/fnv1apc.h
@@ -0,0 +1,1 @@
+unsigned short fnv1a(const char *key, int len);
---
+++ packages/fnv1apc/fnv1apc-0.1.2a.tar.gz/fnv1apc-0.1.2a/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from distutils.core import setup, Extension
+
+fnv1apc_module = Extension('_fnv1apc',
+                           sources=['fnv1apc_wrap.c', 'fnv1apc.c', 'fnv1apc.h'],
+                           )
+
+setup (name = 'fnv1apc',
+       version = '0.1.2a',
+       author = 'zhangyet',
+       author_email = 'zhangyet@gmail.com',
+       url = 'https://github.com/ZhangYet/fnv1apc',
+       description = '''pure c fnv1a_32 implement.''',
+       ext_modules = [fnv1apc_module],
+       py_modules = ['fnv1apc'],
+       )
