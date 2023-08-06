aa9ae61f1e08e8ab89ea292cbbed70a0cba450af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:14 +0000

    Add package PhoneCTL/0.0.1/PhoneCTL-0.0.1.tar.gz

---
+++ packages/PhoneCTL/PhoneCTL-0.0.1.tar.gz/PhoneCTL-0.0.1/PhoneCTL/__init__.py
@@ -0,0 +1,1 @@
+print ('Hello {name}.'.format(name='세상'))
---
+++ packages/PhoneCTL/PhoneCTL-0.0.1.tar.gz/PhoneCTL-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='PhoneCTL',
+      version='0.0.1',
+      description='PhoneCTL Test',
+      author='wakira',
+      author_email='jujinesy@gmail.com',
+      url='http://webapp-jj-01.azurewebsites.net',
+      packages=['PhoneCTL'],
+     )
