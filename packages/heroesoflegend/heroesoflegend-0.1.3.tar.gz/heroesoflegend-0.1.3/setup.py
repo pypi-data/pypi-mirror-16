7a945b16ac17965d89a69d6819ab8ef89d7517dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:17:24 +0000

    Add package heroesoflegend/0.1.3/heroesoflegend-0.1.3.tar.gz

---
+++ packages/heroesoflegend/heroesoflegend-0.1.3.tar.gz/heroesoflegend-0.1.3/setup.py
@@ -0,0 +1,15 @@
+#from distutils.core import setup
+from setuptools import setup
+setup(
+  name = 'heroesoflegend',
+  packages = ['heroesoflegend'], # this must be the same as the name above
+  version = '0.1.3',
+  description = 'Converting Heroes of Legend to a python package',
+  author = 'venn177',
+  author_email = 'venn177@gmail.com',
+  license = 'MIT',
+  url = 'https://github.com/venn177/heroesoflegend.py',
+  download_url = '',
+  keywords = ['testing', 'dnd'], # arbitrary keywords
+  classifiers = [],
+)
