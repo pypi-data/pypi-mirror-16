b4d879b9212657e62793e307852fed70b2622d18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:58 +0000

    Add package matplotsave/0.1.2/matplotsave-0.1.2.tar.gz

---
+++ packages/matplotsave/matplotsave-0.1.2.tar.gz/matplotsave-0.1.2/matplotsave/__init__.py
@@ -0,0 +1,17 @@
+import contextlib
+import matplotlib.pyplot as plt
+
+
+@contextlib.contextmanager
+def pltsave(filename, *, title=None, xlabel=None, ylabel=None, shape=(1, 1), size=None, dpi=None):
+    fig = plt.figure(figsize=size)
+    ax = fig.add_subplot(*(shape + (1,)))
+    if not title is None:
+        ax.set_title(title)
+    if not xlabel is None:
+        ax.set_xlabel(xlabel)
+    if not ylabel is None:
+        ax.set_ylabel(ylabel)
+    yield (fig, ax)
+    fig.savefig(filename, dpi=dpi)
+    fig.clear()
