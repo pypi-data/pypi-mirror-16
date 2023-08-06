5f07d258753388041880a64e36e9ab2472b93d91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:47 +0000

    Add package custom-inherit/1.1.0/custom_inherit-1.1.0.zip

---
+++ packages/custom-inherit/custom_inherit-1.1.0.zip/custom_inherit-1.1.0/custom_inherit/__init__.py
@@ -0,0 +1,46 @@
+from __future__ import absolute_import
+from abc import ABCMeta
+from .base import DocInheritorBase
+from .style_store import *
+
+__all__ = ["DocInheritMeta", "styles"]
+__version__ = "1.1.0"
+
+store = {}
+for style_kind in style_store.__all__:
+    style = getattr(style_store, style_kind)
+    try:
+        store[str(style.name)] = style
+    except AttributeError:
+        print("The style metaclass '{}' must have the implement the attribute: 'name'".format(style.__name__))
+        pass
+
+styles = sorted(store.keys())
+
+
+def DocInheritMeta(style="parent", abstract_base_class=False):
+    """ Returns the DocInheritor metaclass of the specified style.
+
+        Parameters
+        ----------
+        style: str, optional (default: 'parent')
+            A valid inheritance-scheme style.
+
+            See custom_inherit.styles for a list of available styles.
+
+        abstract_base_class: bool, optional(default: False)
+            If True, the returned metaclass inherits from abc.ABCMeta.
+
+            Thus a class that derives from DocInheritMeta(style="numpy", abstract_base_class=True)
+            is an abstract base class, whose derived classes will inherit docstrings
+            using the numpy-style inheritance scheme.
+
+
+        Returns
+        -------
+        Union[custom_inherit.DocInheritorBase, custom.ABCDocInheritorBase]"""
+
+    if style not in store:
+        raise NotImplementedError("The available inheritance styles are: " + ", ".join(store))
+    metaclass = store[style]
+    return metaclass if not abstract_base_class else type("abc" + metaclass.__name__, (ABCMeta, metaclass), {})
---
+++ packages/custom-inherit/custom_inherit-1.1.0.zip/custom_inherit-1.1.0/custom_inherit/style_store.py
@@ -0,0 +1,141 @@
+from __future__ import absolute_import
+from .base import DocInheritorBase
+from .doc_parse_tools import merge_numpy_docs
+
+""" Docstring inheritance-style implementations.
+
+    To write your own inheritance style, you must write a (meta)class that derives from DocInheritorBase, and
+    implements the static methods `class_doc_inherit` and `attr_doc_inherit`. Specify the style name as the class
+    attribute `name` (e.g. your_style.name), and simply log your new metaclass in `custom_inherit.style_store.__all__`.
+
+    The built-in styles are:
+        - parent:   Wherever the docstring for a child-class' attribute (or for the class itself) is
+                    `None`, inherit the corresponding docstring from the parent.
+
+                    *NOTE* As of Python 3.5, this is the default behavior of the built-in function
+                    inspect.getdoc, and thus this style is deprecated Python 3.(>=5).
+
+        - numpy:    The numpy-styled docstrings from the parent and child are merged gracefully
+                    with nice formatting.
+
+                    Specifically, any docstring section that appears in the parent's docstring that
+                    is not present in the child's is inherited. Otherwise, the child's docstring
+                    section is utilized. An exception to this is if the parent docstring contains a
+                    "Raises" section, but the child's attribute's docstring contains a "Returns" or
+                    "Yields" section instead. In this instance, the "Raises" section will not appear in the
+                    inherited docstring.
+
+                    Example:
+                        parent-attribute's docstring:
+
+                            ''' Parent's line
+
+                                Parameters
+                                ----------
+                                x: int
+                                    blah-x
+                                y: Union[None, int]
+                                    blah-y
+
+                                Raises
+                                -------
+                                NotImplemented Error'''
+
+                        docstring for corresponding attribute of child:
+
+                            ''' Child's line
+
+                                Returns
+                                -------
+                                int
+
+                                Notes
+                                -----
+                                notes blah blah'''
+
+                        docstring that is ultimately inherited by child's attribute:
+
+                            ''' Child's line
+
+                                Parameters
+                                ----------
+                                x: int
+                                    blah-x
+                                y: Union[None, int]
+                                    blah-y
+
+                                Returns
+                                -------
+                                int
+
+                                Notes
+                                -----
+                                notes blah blah'''
+"""
+
+# All styles must be logged in the __all__ field.
+__all__ = ["InheritParent", "MergeNumpy"]
+
+
+def _f(prnt_doc, child_doc):
+    return child_doc if child_doc is not None else prnt_doc
+
+
+# --------------------------
+# Meta class inheritance styles
+# --------------------------
+
+class InheritParent(DocInheritorBase):
+    name = "parent"
+
+    @staticmethod
+    def class_doc_inherit(prnt_cls_doc, child_cls_doc):
+        """ Parameters
+            ----------
+            prnt_cls_doc: Union[None, str]
+            child_cls_doc: : Union[None, str]
+
+            Returns
+            -------
+            Union[None, str]"""
+        return _f(prnt_cls_doc, child_cls_doc)
+
+    @staticmethod
+    def attr_doc_inherit(prnt_attr_doc, child_attr_doc):
+        """ Parameters
+            ----------
+            prnt_cls_doc: Union[None, str]
+            child_cls_doc: : Union[None, str]
+
+            Returns
+            -------
+            Union[None, str]"""
+        return _f(prnt_attr_doc, child_attr_doc)
+
+
+class MergeNumpy(DocInheritorBase):
+    name = "numpy"
+
+    @staticmethod
+    def class_doc_inherit(prnt_cls_doc, child_cls_doc):
+        """ Parameters
+            ----------
+            prnt_cls_doc: Union[None, str]
+            child_cls_doc: : Union[None, str]
+
+            Returns
+            -------
+            Union[None, str]"""
+        return merge_numpy_docs(prnt_cls_doc, child_cls_doc)
+
+    @staticmethod
+    def attr_doc_inherit(prnt_attr_doc, child_attr_doc):
+        """ Parameters
+            ----------
+            prnt_cls_doc: Union[None, str]
+            child_cls_doc: : Union[None, str]
+
+            Returns
+            -------
+            Union[None, str]"""
+        return merge_numpy_docs(prnt_attr_doc, child_attr_doc)
