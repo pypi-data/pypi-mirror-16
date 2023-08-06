85789860aaf2d9c913ff829a0154f1c2bb8f0f88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:36 +0000

    Add package popcorn/1.4/popcorn-1.4.tar.gz

---
+++ packages/popcorn/popcorn-1.4.tar.gz/popcorn-1.4/popcorn/popcorn_conversion.py
@@ -0,0 +1,18 @@
+
+# Popcorn 1.4
+# https://github.com/InitializeSahib/Popcorn
+
+def kilo_to_pound(kilo):
+    return kilo * 2.2046
+
+
+def pound_to_kilo(pound):
+    return pound / 2.2046
+
+
+def yard_to_foot(yard):
+    return yard * 3
+
+
+def foot_to_yard(foot):
+    return foot / 3
