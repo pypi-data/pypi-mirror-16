9177b1b4e83c8bb3919b0b83b0342d285580d691
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:03 +0000

    Add package netdev/0.4.1/netdev-0.4.1.zip

---
+++ packages/netdev/netdev-0.4.1.zip/netdev-0.4.1/netdev/__init__.py
@@ -0,0 +1,8 @@
+from netdev.dispatcher import create
+from netdev.dispatcher import platforms
+from netdev.exceptions import DisconnectError
+from netdev.logger import logger
+
+__version__ = '0.4.1'
+__all__ = ('create', 'platforms', 'DisconnectError', 'logger')
+__author__ = 'Yakovlev Sergey'
