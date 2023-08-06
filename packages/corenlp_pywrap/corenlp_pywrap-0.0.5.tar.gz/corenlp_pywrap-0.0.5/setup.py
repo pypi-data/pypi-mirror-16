8d96c43d0dc7f8613876493b71e4e2d69b264eed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:05 +0000

    Add package corenlp_pywrap/0.0.5/corenlp_pywrap-0.0.5.tar.gz

---
+++ packages/corenlp_pywrap/corenlp_pywrap-0.0.5.tar.gz/corenlp_pywrap-0.0.5/setup.py
@@ -0,0 +1,29 @@
+import os
+from setuptools import setup
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "corenlp_pywrap",
+    version = "0.0.5",
+    author = "hhsecond",
+    author_email = "sherinct@live.com",
+    description = ("A powerful python wraper for Stanford CoreNLP"),
+    license = "MIT",
+    keywords = "stanford corenlp wrapper",
+    install_requires=['requests'],
+    url = "https://www.github.com/hhsecond/corenlp_pywrap",
+    packages=['corenlp_pywrap'],
+    long_description='this is a long description',
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
