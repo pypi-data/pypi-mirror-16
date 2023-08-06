cb40152b9aedb9dd270f4cca5ec6c13cce8ea757
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:57 +0000

    Add package fengcheng_nester/1.0.0/fengcheng_nester-1.0.0.zip

---
+++ packages/fengcheng_nester/fengcheng_nester-1.0.0.zip/fengcheng_nester-1.0.0/nester.py
@@ -0,0 +1,9 @@
+#coding = "utf-8"
+def print_lol(the_list, level = 0):
+  for each_item in the_list:
+    if isinstance(each_item, list):
+      print_lol(each_item, level)
+    else:
+      for tab in range(level):
+        print("\t", end = '')
+      print(each_item)
---
+++ packages/fengcheng_nester/fengcheng_nester-1.0.0.zip/fengcheng_nester-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name            = 'fengcheng_nester',
+    version         = '1.0.0',
+    py_modules      = ['nester'],
+    author          = 'fengcheang',
+    author_email    = '987380454@qq.com',
+    url             = 'http://www.baidu.com',
+    description     = 'A simple printer of nested lists',
+    )
