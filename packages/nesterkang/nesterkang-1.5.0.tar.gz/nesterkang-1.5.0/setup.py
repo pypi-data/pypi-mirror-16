fb3abc4bc98057984afcc85cf17081e42314d0e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:54 +0000

    Add package nesterkang/1.5.0/nesterkang-1.5.0.tar.gz

---
+++ packages/nesterkang/nesterkang-1.5.0.tar.gz/nesterkang-1.5.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'nesterkang',
+    version = '1.5.0',
+    py_modules = ['nesterkang'],
+    author = 'kangchenyun',
+    author_email = 'kangchenyun@gmail.com',
+    url = 'http://kangchenyun.github.io',
+    description = 'A simple printer of nested lists',
+    )
