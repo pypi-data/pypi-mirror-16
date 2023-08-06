3c71e1f6014d6e5108a4230e11ac191a8c09ec7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:16:46 +0000

    Add package tsuru_dashboard/0.4.0rc6/tsuru_dashboard-0.4.0rc6.tar.gz

---
+++ packages/tsuru_dashboard/tsuru_dashboard-0.4.0rc6.tar.gz/tsuru_dashboard-0.4.0rc6/tsuru_dashboard/events/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+
+import views
+
+
+urlpatterns = [
+    url(r'^$', views.ListEvent.as_view(), name='event-list'),
+    url(r'^kinds/$', views.KindList.as_view(), name='kind-list'),
+    url(r'^(?P<uuid>[\w-]+)/$', views.EventInfo.as_view(), name='event-info'),
+]
