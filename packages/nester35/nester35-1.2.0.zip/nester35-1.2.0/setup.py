1055d6356f89f3c3be8ccaa3d5d2456c3da2d610
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:17:48 +0000

    Add package nester35/1.2.0/nester35-1.2.0.zip

---
+++ packages/nester35/nester35-1.2.0.zip/nester35-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup (
+        name         = 'nester35' ,
+        version      = '1.2.0' ,
+        py_modules   = ['nester35'] ,
+        author       = 'jrmoose33' ,
+        author_email = 'jrmoose33@yahoo.com.tw' ,
+        url          = '',
+        description  = 'A simple printer of nested lists(try upload)' ,
+      )
