bc6c87d884efe621f064915a267b32e77fa7d528
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:18:56 +0000

    Add package git-code-debt/0.7.2/git_code_debt-0.7.2-py2.py3-none-any.whl

---
+++ packages/git-code-debt/git_code_debt-0.7.2-py2.py3-none-any.whl/git_code_debt/util/compat.py
@@ -0,0 +1,13 @@
+# pylint:disable=import-error,no-name-in-module,unused-import
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+import six
+
+
+if six.PY2:  # pragma: no cover (PY2 only)
+    import __builtin__ as builtins  # noqa
+    import urlparse as urllib_parse  # noqa
+else:  # pragma: no cover (PY3 only)
+    import builtins  # noqa
+    import urllib.parse as urllib_parse  # noqa
