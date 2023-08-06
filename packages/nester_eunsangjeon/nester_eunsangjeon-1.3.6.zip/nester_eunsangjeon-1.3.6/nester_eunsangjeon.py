c4f058df5c8ea897f0ad4579bc14a327b0c72c75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:53 +0000

    Add package nester_eunsangjeon/1.3.6/nester_eunsangjeon-1.3.6.zip

---
+++ packages/nester_eunsangjeon/nester_eunsangjeon-1.3.6.zip/nester_eunsangjeon-1.3.6/nester_eunsangjeon.py
@@ -0,0 +1,14 @@
+"""This is the "nester.py" module, and it provides one function called print_lol() which prints lists that may or may not include nested lists."""
+import sys
+#i am a python learner who is following the tutorial of Head First Python
+def print_lol(the_list, indent=False, level=0, fh=sys.stdout):
+    """This function takes a positional arguments called "the_list", which is any Python list. Each data item in the provided list is printed to the screen on its own line."""
+    #version 1.1.0, indentation is added
+    for each_item in the_list:
+        if isinstance(each_item, list):
+            print_lol(each_item, indent, level + 1, fh)
+        else:
+            if indent:
+                for num in range(level):
+                    print("\t", end = '', file = fh)
+            print(each_item, file = fh)
