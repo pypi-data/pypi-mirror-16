9344b014d1c71abbb21b54ecd131d8bfb37cb7ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:41 +0000

    Add package nester_LinD/1.4.0/nester_LinD-1.4.0.tar.gz

---
+++ packages/nester_LinD/nester_LinD-1.4.0.tar.gz/nester_LinD-1.4.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name ='nester_LinD',
+    version = '1.4.0',
+    py_modules = ['nester_LinD'],
+    author = 'LinD',
+    author_email = '1019993150@qq.com',
+    url = 'http://www.headfirstlabs.com',
+    description = 'A edited printer of nested lists',
+    )
