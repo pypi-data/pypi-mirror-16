3a2496da58b27d0ef06db352c3fbae65e7b1ac32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:50 +0000

    Add package nester_paladin/1.3.0/nester_paladin-1.3.0.zip

---
+++ packages/nester_paladin/nester_paladin-1.3.0.zip/nester_paladin-1.3.0/nester.py
@@ -0,0 +1,10 @@
+"""This is nester.py module, V1.3.0"""
+def print_lol(the_list, level=0):
+    """This function print a list"""
+    for each_item in the_list:
+        if isinstance(each_item, list):
+            print_lol(each_item, level+1)
+        else:
+            for tab_stop in range(level):
+                print("\t", end='')
+            print(each_item)
