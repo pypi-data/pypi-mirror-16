6fa598b96726f22489aaa5bf9f287a5b60d07d95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:44 +0000

    Add package shaderdef/0.5.1/shaderdef-0.5.1.tar.gz

---
+++ packages/shaderdef/shaderdef-0.5.1.tar.gz/shaderdef-0.5.1/setup.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python
+
+from setuptools import setup
+
+setup(name='shaderdef',
+      version='0.5.1',
+      description='Transform Python code into GLSL shaders',
+      url='https://github.com/nicholasbishop/shaderdef',
+      author='Nicholas Bishop',
+      author_email='nicholasbishop@gmail.com',
+      license='GNU General Public License v3 or later (GPLv3+)',
+      packages=['shaderdef'],
+      zip_safe=True)
---
+++ packages/shaderdef/shaderdef-0.5.1.tar.gz/shaderdef-0.5.1/shaderdef/glsl_types.py
@@ -0,0 +1,54 @@
+from shaderdef.equality import EqualityMixin
+
+class Attribute(EqualityMixin):
+    def __init__(self, gtype):
+        self.gtype = gtype
+
+    def __repr__(self):
+        return 'Attribute({})'.format(self.gtype)
+
+    def glsl_decl(self, name):
+        return 'in {} {};'.format(self.gtype.__name__, name)
+
+
+class FragOutput(EqualityMixin):
+    def __init__(self, gtype):
+        self.gtype = gtype
+
+    def __repr__(self):
+        return 'FragOutput({})'.format(self.gtype)
+
+    def glsl_decl(self, name):
+        return 'out {} {};'.format(self.gtype.__name__, name)
+
+
+class Uniform(EqualityMixin):
+    def __init__(self, gtype):
+        self.gtype = gtype
+
+    def __repr__(self):
+        return 'Uniform({})'.format(self.gtype)
+
+    def glsl_decl(self, name):
+        return 'uniform {} {};'.format(self.gtype.__name__, name)
+
+
+class BuiltinType(object):
+    pass
+
+
+# pylint: disable=invalid-name
+class mat2(BuiltinType):
+    pass
+class mat3(BuiltinType):
+    pass
+class mat4(BuiltinType):
+    pass
+class vec2(BuiltinType):
+    pass
+class vec3(BuiltinType):
+    pass
+class vec4(BuiltinType):
+    pass
+class void(BuiltinType):
+    pass
