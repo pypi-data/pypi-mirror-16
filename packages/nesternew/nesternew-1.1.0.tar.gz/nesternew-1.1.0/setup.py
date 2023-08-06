713f8f91b245967582af564489b04a1e2b827481
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:44 +0000

    Add package nesternew/1.1.0/nesternew-1.1.0.tar.gz

---
+++ packages/nesternew/nesternew-1.1.0.tar.gz/nesternew-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='nesternew',
+    version='1.1.0',
+    py_modules=['nester'],
+    author='maomao',
+    author_email='maomao@maomao.com',
+    url='http://www.headfirstlabs.com',
+    dsecription='the frist module by maomao'
+    )
