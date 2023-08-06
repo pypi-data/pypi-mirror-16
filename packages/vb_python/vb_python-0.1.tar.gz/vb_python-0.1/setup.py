419ce2151348740edf27b3f3289cb41c2080726d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:33 +0000

    Add package vb_python/0.1/vb_python-0.1.tar.gz

---
+++ packages/vb_python/vb_python-0.1.tar.gz/vb_python-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='vb_python',
+      version='0.1',
+      description='A test python library!',
+      url='http://github.com/kberg721/',
+      author='Kyle Bergman',
+      author_email='kberg721@hotmail.com',
+      license='MIT',
+      packages=['vb_python'],
+      zip_safe=False)
