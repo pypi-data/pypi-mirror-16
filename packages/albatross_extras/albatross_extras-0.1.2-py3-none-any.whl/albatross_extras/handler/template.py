2c3828881341cbfeb3e3d14f090380f25a0afd54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:42 +0000

    Add package albatross_extras/0.1.2/albatross_extras-0.1.2-py3-none-any.whl

---
+++ packages/albatross_extras/albatross_extras-0.1.2-py3-none-any.whl/albatross_extras/handler/template.py
@@ -0,0 +1,25 @@
+
+from jinja2 import Environment, FileSystemLoader
+
+
+class TemplateHandler:
+    """
+
+    server.add_route('/login', TemplateHandler('login.jinja2'))
+    """
+    @classmethod
+    def set_template_directory(cls, path):
+        loader = FileSystemLoader(path)
+        cls.env = Environment(loader=loader)
+
+    env = None
+
+    def __init__(self, template_path, content_type='text/html; charset=utf-8'):
+        self.template = self.env.get_template(template_path)
+        self.content_type = content_type
+
+    async def on_get(self, req, res):
+        res.headers['Content-Type'] = self.content_type
+        res.write(self.template.render(user=getattr(req, 'user', None)))
+
+TemplateHandler.set_template_directory('templates')
