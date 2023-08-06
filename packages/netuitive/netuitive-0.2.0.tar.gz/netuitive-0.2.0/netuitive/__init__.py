7f6ab94c051266ca2a0bfe379604101e1b995d34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:13 +0000

    Add package netuitive/0.2.0/netuitive-0.2.0.tar.gz

---
+++ packages/netuitive/netuitive-0.2.0.tar.gz/netuitive-0.2.0/netuitive/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'Netuitive, Inc'
+__version__ = '0.2.0'
+
+from .client import Client  # nopep8
+from .element import Element  # nopep8
+from .event import Event  # nopep8
