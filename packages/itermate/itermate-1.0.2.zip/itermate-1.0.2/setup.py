4bb301ef71afde7bcfe87f7a7ab069ca1da0ed6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:32 +0000

    Add package itermate/1.0.2/itermate-1.0.2.zip

---
+++ packages/itermate/itermate-1.0.2.zip/itermate-1.0.2/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+
+
+setuptools.setup(
+    name="itermate",
+    packages = ["itermate"],
+    version="1.0.2",
+    description="Iterator-tools for functional programming.",
+    author = 'Henry Weickert',
+    author_email = 'henryweickert@gmail.com',
+    url = 'https://github.com/hweickert/itermate',
+    keywords = ['itermate', 'itertools', 'iapply', 'imapchain']
+)
