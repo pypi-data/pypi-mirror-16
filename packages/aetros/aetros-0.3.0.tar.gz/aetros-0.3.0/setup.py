8d7ca26fbfe1d3e3d85f2ef723676b673a9e0551
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:04:09 +0000

    Add package aetros/0.3.0/aetros-0.3.0.tar.gz

---
+++ packages/aetros/aetros-0.3.0.tar.gz/aetros-0.3.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+from setuptools import find_packages
+import aetros
+
+setup(name='aetros',
+      version=aetros.const.__version__,
+      description='Console application to manage deep neural network trainings in AETROS TRAINER',
+      author='Marc J. Schmidt',
+      author_email='marc@marcjschmidt.de',
+      url='https://github.com/aetros/aetros-cli',
+      download_url='https://github.com/aetros/worker/tarball/0.1.0',
+      license='MIT',
+          entry_points = {
+              'console_scripts': ['aetros = aetros:main'],
+          },
+      install_requires=(
+            'keras>=1.0.6',
+            'requests',
+            'numpy',
+            'scipy',
+            'h5py',
+            'cherrypy>=7.1.0',
+            'py-cpuinfo==0.2.3'
+      ),
+      packages=find_packages())
