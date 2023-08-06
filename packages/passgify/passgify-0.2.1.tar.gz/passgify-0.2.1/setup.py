84ac655b5f6e7f73359e1e43da17f0e671358b05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:19:54 +0000

    Add package passgify/0.2.1/passgify-0.2.1.tar.gz

---
+++ packages/passgify/passgify-0.2.1.tar.gz/passgify-0.2.1/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from setuptools import setup
+from setuptools import find_packages
+__version__ = '0.2.1'
+
+setup(
+    name='passgify',
+    version=__version__,
+    author='Brian Frisbie',
+    author_email='bfrizb@github.com',
+    description='A tool to deterministically generate pseudo-random passwords',
+    url='https://github.com/bfrizb/passgify',
+    scripts=['pg'],
+    packages=find_packages(exclude=['tests']),
+)
