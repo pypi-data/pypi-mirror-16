0740276fb0cdab5ec65ad636a0d492802520f2d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:00 +0000

    Add package coala-bears/0.8.0.dev20160722125150/coala-bears-0.8.0.dev20160722125150.tar.gz

---
+++ packages/coala-bears/coala-bears-0.8.0.dev20160722125150.tar.gz/coala-bears-0.8.0.dev20160722125150/bears/js/JSONFormatBear.py
@@ -0,0 +1,58 @@
+import json
+from collections import OrderedDict
+
+from coalib.bearlib.spacing.SpacingHelper import SpacingHelper
+from coalib.bears.LocalBear import LocalBear
+from coalib.results.Diff import Diff
+from coalib.results.Result import Result
+
+
+class JSONFormatBear(LocalBear):
+    try:
+        DecodeError = json.decoder.JSONDecodeError
+    except AttributeError:
+        DecodeError = ValueError
+
+    LANGUAGES = {"JSON"}
+    AUTHORS = {'The coala developers'}
+    AUTHORS_EMAILS = {'coala-devel@googlegroups.com'}
+    LICENSE = 'AGPL-3.0'
+    CAN_DETECT = {'Formatting'}
+
+    def run(self, filename, file,
+            json_sort: bool=False,
+            tab_width: int=SpacingHelper.DEFAULT_TAB_WIDTH,
+            keep_unicode: bool=False):
+        """
+        Raises issues for any deviations from the pretty-printed JSON.
+
+        :param json_sort:    Whether or not keys should be sorted.
+        :param tab_width:    Number of spaces to indent.
+        :param keep_unicode: Wether or not to escape unicode values using ASCII.
+        """
+        try:
+            json_content = json.loads(''.join(file),
+                                      object_pairs_hook=OrderedDict)
+        except self.DecodeError as err:
+            yield Result.from_values(
+                self,
+                "This file does not contain parsable JSON. " + repr(str(err)),
+                file=filename)
+            return
+
+        corrected = json.dumps(json_content,
+                               sort_keys=json_sort,
+                               indent=tab_width,
+                               ensure_ascii=not keep_unicode).splitlines(True)
+        # Because of a bug in several python versions we have to correct
+        # whitespace here.
+        corrected = tuple(line.rstrip(" \n") + "\n" for line in corrected)
+        diff = Diff.from_string_arrays(file, corrected)
+
+        if len(diff) > 0:
+            yield Result(self,
+                         "This file can be reformatted by sorting keys and "
+                         "following indentation.",
+                         affected_code=tuple(d.range(filename)
+                                             for d in diff.split_diff()),
+                         diffs={filename: diff})
