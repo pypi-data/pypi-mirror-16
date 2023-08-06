9b24eace994ca8d9905c97fdba905c3287a82eb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:49 +0000

    Add package meridies/1.1/meridies-1.1.tar.gz

---
+++ packages/meridies/meridies-1.1.tar.gz/meridies-1.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+import glob
+
+setup(
+    name = 'meridies',
+    version = '1.1',
+    description = '',
+    author = 'Fabian Peter Hammerle',
+    author_email = 'fabian.hammerle@gmail.com',
+    url = 'https://github.com/fphammerle/meridies',
+    download_url = 'https://github.com/fphammerle/meridies/tarball/1.1',
+    keywords = [],
+    classifiers = [],
+    scripts = glob.glob('scripts/*'),
+    install_requires = ['pytz', 'python-dateutil'],
+    tests_require = ['pytest']
+    )
