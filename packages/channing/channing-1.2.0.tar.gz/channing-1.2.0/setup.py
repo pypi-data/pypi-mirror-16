3fb361fc1b7e4dca706489efdf38de923b8a3270
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:22 +0000

    Add package channing/1.2.0/channing-1.2.0.tar.gz

---
+++ packages/channing/channing-1.2.0.tar.gz/channing-1.2.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+	name			=	'channing',
+	version			=	'1.2.0',
+	py_modules		=	['channing'],
+	author			=	'channing',
+	author_email	=	'channig342@gmail.com'
+)
