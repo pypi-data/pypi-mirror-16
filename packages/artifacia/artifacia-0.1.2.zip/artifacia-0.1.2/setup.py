32451c14b82fea497c8b9fc0dda4674ac607b04f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:11 +0000

    Add package artifacia/0.1.2/artifacia-0.1.2.zip

---
+++ packages/artifacia/artifacia-0.1.2.zip/artifacia-0.1.2/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+setup(
+  name = 'artifacia',
+  packages = ['artifacia'],
+  version = '0.1.2',
+  description = "A python client to integrate visual recommendation feature from Artifacia API.",
+  author = 'Ashish Kumar',
+  author_email = 'erashish122@gmail.com',
+  url = 'https://github.com/artifacia/artifacia-client-python',
+  keywords = ['pyartifacia', 'artifaciapy', 'pythonartifacia', 'recommendation', 'artifacia']
+)
