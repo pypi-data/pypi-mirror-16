367287e9759a7f42f9b0ac352f4c8052affe8559
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:35 +0000

    Add package pyglsl_parser/0.5.0/pyglsl_parser-0.5.0.tar.gz

---
+++ packages/pyglsl_parser/pyglsl_parser-0.5.0.tar.gz/pyglsl_parser-0.5.0/setup.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python
+
+# pylint: disable=invalid-name,missing-docstring
+
+from setuptools import setup
+from setuptools.extension import Extension
+from Cython.Build import cythonize
+
+extensions = [
+    Extension(
+        name='pyglsl_parser.parser',
+        sources=['pyglsl_parser/parser.pyx',
+                 'glsl-parser/ast.cpp',
+                 'glsl-parser/lexer.cpp',
+                 'glsl-parser/parser.cpp',
+                 'glsl-parser/util.cpp'],
+        language='c++',
+    )
+]
+
+setup(name='pyglsl_parser',
+      version='0.5.0',
+      test_suite='test',
+      url='https://github.com/nicholasbishop/pyglsl_parser',
+      author='Nicholas Bishop',
+      author_email='nicholasbishop@gmail.com',
+      ext_modules=cythonize(extensions))
