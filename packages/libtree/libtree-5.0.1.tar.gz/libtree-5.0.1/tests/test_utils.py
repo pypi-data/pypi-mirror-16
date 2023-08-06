621f8ffbd54cf7465a58a422a6632bcdf5c69f38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:28 +0000

    Add package libtree/5.0.1/libtree-5.0.1.tar.gz

---
+++ packages/libtree/libtree-5.0.1.tar.gz/libtree-5.0.1/tests/test_utils.py
@@ -0,0 +1,11 @@
+# Copyright (c) 2016 Fabian Kochem
+
+
+from libtree.utils import vectorize_nodes
+
+
+def test_vectorize_nodes(cur, root, nd2, nd2_1, nd2_1_1):
+    nodes = [nd2_1_1, nd2, root, nd2_1]
+    expected = [root, nd2, nd2_1, nd2_1_1]
+    assert vectorize_nodes(nodes) == expected
+    assert vectorize_nodes(*nodes) == expected
