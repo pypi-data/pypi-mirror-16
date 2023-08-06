8411f5de3fd59b9e05fde949bbfaa54526c11e21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:04:32 +0000

    Add package gevent_timer/0.1.1/gevent_timer-0.1.1.tar.gz

---
+++ packages/gevent_timer/gevent_timer-0.1.1.tar.gz/gevent_timer-0.1.1/gevent_timer/__init__.py
@@ -0,0 +1,6 @@
+from gevent_timer.timer import (
+    set_interval,
+    set_timeout,
+    clear_interval,
+    clear_timeout
+)
---
+++ packages/gevent_timer/gevent_timer-0.1.1.tar.gz/gevent_timer-0.1.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+import os
+
+setup(
+    name = 'gevent_timer',
+    version = '0.1.1',
+    packages=[
+        'gevent_timer'
+    ],
+    install_requires=[
+        'gevent'
+    ]
+)
