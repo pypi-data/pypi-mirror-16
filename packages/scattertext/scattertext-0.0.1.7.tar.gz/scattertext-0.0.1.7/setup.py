8ee655ae5ac49342e2610f542100fefc544a0dd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:39 +0000

    Add package scattertext/0.0.1.7/scattertext-0.0.1.7.tar.gz

---
+++ packages/scattertext/scattertext-0.0.1.7.tar.gz/scattertext-0.0.1.7/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+setup(name='scattertext',
+      version='0.0.1.7',
+      description='An NLP libarary to help find interesting terms in small to medium-sized corpora.',
+      url='https://github.com/JasonKessler/scattertext',
+      author='Jason Kessler',
+      author_email='jason.kessler@gmail.com',
+      license='MIT',
+      packages=['scattertext'],
+      install_requires=[
+	      'nose',
+	      'numpy',
+	      'scipy',
+	      'sklearn',
+	      'pandas',
+	      'spacy',
+
+	      #'mpld3',
+	      #'matplotlib',
+	      #'seaborn',
+	      #'jupyter',
+      ],
+      package_data={
+	      'scattertext': ['data/*']
+      },
+			test_suite="nose.collector",
+			tests_require=['nose'],
+			setup_requires=['nose>=1.0'],
+      zip_safe=False)
