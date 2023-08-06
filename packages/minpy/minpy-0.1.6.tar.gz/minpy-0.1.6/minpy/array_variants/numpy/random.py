e91bb05b8db19398e9c0810f5514e32a0c865b05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:42 +0000

    Add package minpy/0.1.6/minpy-0.1.6.tar.gz

---
+++ packages/minpy/minpy-0.1.6.tar.gz/minpy-0.1.6/minpy/array_variants/numpy/random.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Wrapper for NumPy random functions."""
+from __future__ import absolute_import
+
+from . import numpy_wrapper
+import numpy
+
+def register_primitives(reg, prim_wrapper):
+    """ Register primitives """
+    numpy_wrapper.wrap_namespace(numpy.random.__dict__, reg, prim_wrapper)
+
+def def_grads(reg, prims):
+    """ Define gradients of primitives """
+    prims('random').def_grad_zero()
+    prims('randn').def_grad_zero()
