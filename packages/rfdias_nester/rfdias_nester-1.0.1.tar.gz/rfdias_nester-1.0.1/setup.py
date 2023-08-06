0feb5e4db00403be5e14a5d410e05865a44c9774
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:16:34 +0000

    Add package rfdias_nester/1.0.1/rfdias_nester-1.0.1.tar.gz

---
+++ packages/rfdias_nester/rfdias_nester-1.0.1.tar.gz/rfdias_nester-1.0.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup (
+	name		= 'rfdias_nester',
+	version		= '1.0.1',
+	py_modules	= ['nester'], 
+	author		= 'rfdias',
+	author_email	= 'rogerfdias@hotmail.com',
+	url		= 'http://www.maplink.com.br',
+	description	= 'A simple printer of nested lists',
+      )
