1946e3a2b65154c0a17098e43ed783358ba4b7a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:52 +0000

    Add package norwind_nester/1.0.0/norwind_nester-1.0.0.zip

---
+++ packages/norwind_nester/norwind_nester-1.0.0.zip/norwind_nester-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name    = 'norwind_nester',
+    version = '1.0.0',
+    py_modules = ['norwind_nester'],
+    author = 'hf',
+	author_email = 'norwind@163.com',
+    description = 'A simple printer of nested lists',
+    )
