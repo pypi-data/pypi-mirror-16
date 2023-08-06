8567b2a60b35f2c89253a14e7c4d054d31e492ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:04:02 +0000

    Add package hearthstonecarddetector/0.1.2/hearthstonecarddetector-0.1.2.zip

---
+++ packages/hearthstonecarddetector/hearthstonecarddetector-0.1.2.zip/hearthstonecarddetector-0.1.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+
+def readme():
+      with open('README.rst') as f:
+            return f.read()
+
+
+setup(name='hearthstonecarddetector',
+      version='0.1.2',
+      description='match a specific cropped image to a hearthstone card id',
+      long_description=readme(),
+      url='https://www.github.com/russon77/hearthstonecarddetector',
+      author='Tristan Kernan',
+      author_email='russon77@gmail.com',
+      keywords='hearthstone',
+      license='MIT',
+      packages=['hearthstonecarddetector'],
+      zip_safe=False,
+      include_package_data=True,
+      install_requires=[
+            'imagehash',
+            'numpy',
+      ],
+      test_suite='nose.collector',
+      tests_require=['nose', 'PIL'])
+
