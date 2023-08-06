ec901aafcb7712ed1fc2748137ac5a3df0f86b5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:35 +0000

    Add package require.py/1.1.1/require.py-1.1.1.tar.gz

---
+++ packages/require.py/require.py-1.1.1.tar.gz/require.py-1.1.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+  name='require.py',
+  version='1.1.1',
+  description='Path-based imports in Python, inspired by Node.js `require`',
+  url='https://github.com/pcattori/require-py',
+  author='Pedro Cattori',
+  author_email='pcattori@gmail.com',
+  license='MIT',
+  packages=['require']
+)
