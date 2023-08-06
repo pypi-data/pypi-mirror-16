6d762dbbe8b055af15be28993c74af9daf230459
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:22:46 +0000

    Add package micropython-umqtt.simple/1.1/micropython-umqtt.simple-1.1.tar.gz

---
+++ packages/micropython-umqtt.simple/micropython-umqtt.simple-1.1.tar.gz/micropython-umqtt.simple-1.1/setup.py
@@ -0,0 +1,18 @@
+import sys
+# Remove current dir from sys.path, otherwise setuptools will peek up our
+# module instead of system.
+sys.path.pop(0)
+from setuptools import setup
+
+
+setup(name='micropython-umqtt.simple',
+      version='1.1',
+      description='Lightweight MQTT client for MicroPython.',
+      long_description=open('README.rst').read(),
+      url='https://github.com/micropython/micropython/issues/405',
+      author='Paul Sokolovsky',
+      author_email='micro-python@googlegroups.com',
+      maintainer='MicroPython Developers',
+      maintainer_email='micro-python@googlegroups.com',
+      license='MIT',
+      packages=['umqtt'])
