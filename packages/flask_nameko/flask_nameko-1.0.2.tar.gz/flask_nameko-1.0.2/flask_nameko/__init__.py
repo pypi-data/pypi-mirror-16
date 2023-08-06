1ed184f1d4f256d8b65aa0cd94b28944345c0372
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:57:31 +0000

    Add package flask_nameko/1.0.2/flask_nameko-1.0.2.tar.gz

---
+++ packages/flask_nameko/flask_nameko-1.0.2.tar.gz/flask_nameko-1.0.2/flask_nameko/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'Jesse Pollak'
+__email__ = 'jesse@getclef.com'
+__version__ = '1.0.2'
+
+from errors import *
+from proxies import FlaskPooledClusterRpcProxy
