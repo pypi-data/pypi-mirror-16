3581aaec4b056aa4d581d2eac78377d9ce29eee0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:09 +0000

    Add package schemaman/0.1.1/schemaman-0.1.1.tar.gz

---
+++ packages/schemaman/schemaman-0.1.1.tar.gz/schemaman-0.1.1/__init__.py
@@ -0,0 +1,10 @@
+# This is the core of the API, so we will leave it as top level module members
+from datasource import *
+
+# Main()
+import schemaman
+
+# Includes and internal library
+import action
+import utility
+
