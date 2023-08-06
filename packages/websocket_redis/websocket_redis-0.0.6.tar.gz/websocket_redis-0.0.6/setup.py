bb50f44accbf599125479a511ca4e44d0e66f1c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:53 +0000

    Add package websocket_redis/0.0.6/websocket_redis-0.0.6.tar.gz

---
+++ packages/websocket_redis/websocket_redis-0.0.6.tar.gz/websocket_redis-0.0.6/setup.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+import os
+import sys
+
+from setuptools import find_packages, setup
+
+version = ("0", "0", "6")
+
+
+root = os.path.dirname(__file__)
+try:
+    with open(os.path.join(root, 'README.md')) as f:
+        long_description = '\n\n'.join(f.read().split('\n\n')[1:])
+except FileNotFoundError:
+    long_description = ""
+py_version = sys.version_info[:2]
+
+if py_version < (3, 3):
+    install_requires = ["redis"]
+
+    packages = find_packages(exclude=(
+        "websocket_redis.common.aioredis",
+        "websocket_redis.api.async",
+        "websocket_redis.server",
+    ))
+else:
+    install_requires = [
+        "websockets",
+        "aioredis",
+        "redis"
+    ]
+    packages = find_packages(exclude=["tests"])
+
+setup(
+    name="websocket_redis",
+    version=".".join(version),
+    description='Communicate with websocket through redis as messaging broker',
+    long_description=long_description,
+    keywords='python websocket redis asyncio',
+    author='Mohamed abdeljelil',
+    author_email="abdeljelil.mohamed@gmail.com",
+    url='https://github.com/Abdeljelil/websocket_redis',
+    download_url="https://github.com/Abdeljelil/websocket_redis.git",
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: Unix',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    packages=packages,
+    include_package_data=True,
+    extras_require={
+        ':python_version=="3.3"': ['asyncio'],
+    },
+    license='BSD',
+    install_requires=install_requires,
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
