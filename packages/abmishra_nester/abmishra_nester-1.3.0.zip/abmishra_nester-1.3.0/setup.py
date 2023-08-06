b6b49d806eb739563880c1f729e6f9ba7fe99566
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:18:39 +0000

    Add package abmishra_nester/1.3.0/abmishra_nester-1.3.0.zip

---
+++ packages/abmishra_nester/abmishra_nester-1.3.0.zip/abmishra_nester-1.3.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup (
+    name    = 'abmishra_nester',
+    version = '1.3.0',
+    py_modules = ['abmishra_nester'],
+    author  = 'abhi_mishra',
+    author_email    = 'hfpython@headfirstlabs.com',
+    url             = 'http://headfirstlabs.com',
+    description     = 'A simple printer of nested lists',
+    )
