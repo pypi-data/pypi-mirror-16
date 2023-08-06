34859dcb2133da293788fd6d27bae0746b1a1c38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:35 +0000

    Add package pygroker/0.1.1/pygroker-0.1.1.tar.gz

---
+++ packages/pygroker/pygroker-0.1.1.tar.gz/pygroker-0.1.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+import os
+
+long_desc = ''
+
+if os.path.exists('README.rst'):
+    long_desc = open('README.rst').read()
+
+setup(name = 'pygroker',
+    version = '0.1.1',
+    description = 'Temporary package for https://github.com/garyelephant/pygrok/pull/3',
+    long_description= long_desc,
+    url = 'https://github.com/garyelephant/pygrok',
+    author = 'garyelephant',
+    author_email = 'garygaowork@gmail.com',
+    license = 'MIT',
+    packages = ['pygrok'],
+    include_package_data = True,
+    keywords = ['python grok', 'regex'], # arbitrary keywords
+    download_url = 'https://github.com/garyelephant/pygrok/tarball/v0.7.4',
+    install_requires=['regex']
+    )
