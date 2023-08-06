58778892db26280cfe3633188a02c9205a88c508
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:29 +0000

    Add package Js2Py/0.39/Js2Py-0.39.tar.gz

---
+++ packages/Js2Py/Js2Py-0.39.tar.gz/Js2Py-0.39/setup.py
@@ -0,0 +1,36 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+long_desc = '''Translates JavaScript to Python code. Js2Py is able to translate and execute virtually any JavaScript code.
+
+Js2Py is written in pure python and does not have any dependencies. Basically an implementation of JavaScript core in pure python.
+
+
+    import js2py
+
+    f = js2py.eval_js( "function (name) {return name.length}" )
+
+    f("Piotr")
+
+    # returns 5
+
+
+More examples at: https://github.com/PiotrDabkowski/Js2Py
+'''
+
+# python setup.py register -r pypi
+# python setup.py sdist upload -r pypi
+setup(
+    name='Js2Py',
+    version='0.39',
+    packages=['js2py', 'js2py.utils', 'js2py.prototypes', 'js2py.translators', 'js2py.constructors', 'js2py.host'],
+    url='https://github.com/PiotrDabkowski/Js2Py',
+    install_requires = ['tzlocal>=1.2', 'six>=1.10'],
+    license='MIT',
+    author='Piotr Dabkowski',
+    author_email='piotr.dabkowski@balliol.ox.ac.uk',
+    description='JavaScript to Python Translator & JavaScript interpreter written in 100% pure Python.',
+    long_description=long_desc
+)
