7ab89ee419e105aa39e3e5de923e191cd0de0e0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:27 +0000

    Add package simplestatistics/0.1.5/simplestatistics-0.1.5.tar.gz

---
+++ packages/simplestatistics/simplestatistics-0.1.5.tar.gz/simplestatistics-0.1.5/simplestatistics/__init__.py
@@ -0,0 +1,32 @@
+# descriptive statistics
+from .statistics.min import min
+from .statistics.max import max
+from .statistics.sum import sum
+from .statistics.quantile import quantile
+from .statistics.product import product
+
+# measures of central tendency
+from .statistics.mean import mean
+from .statistics.median import median
+from .statistics.mode import mode
+from .statistics.geometric_mean import geometric_mean
+from .statistics.skew import skew
+from .statistics.kurtosis import kurtosis
+
+# measures of dispersion
+from .statistics.variance import variance
+from .statistics.standard_deviation import standard_deviation
+
+# similarity
+from .statistics.correlate import correlate
+from .statistics.z_scores import z_scores
+
+# linear regression
+from .statistics.linear_regression import linear_regression
+
+# distributions
+from .statistics.factorial import factorial
+from .statistics.choose import choose
+from .statistics.normal import normal
+from .statistics.binomial import binomial
+from .statistics.t_test import t_test
