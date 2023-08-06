7a7bb346df6b57606d78e694d954b54d574d2843
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:57:40 +0000

    Add package beamline/1.3.4/beamline-1.3.4-py2.7.egg

---
+++ packages/beamline/beamline-1.3.4-py2.7.egg/beamline/ui/main.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+""" This is a demo:
+    1: UI generated from wxFormBuilder
+    2: event bindings are implemented later
+
+Tong Zhang
+2016-05-27 15:54:59 PM CST
+"""
+
+import myappframe
+import wx
+
+
+def run(debug=True):
+    app = wx.App()
+    frame = myappframe.MyAppFrame(None, 
+            u'Lattice Viewer \u2014 Accelerator Online Modeling Tool')
+    frame.SetSize((1024, 768))
+    frame.Show()
+    app.MainLoop()
+
+
+if __name__ == '__main__':
+    run()
