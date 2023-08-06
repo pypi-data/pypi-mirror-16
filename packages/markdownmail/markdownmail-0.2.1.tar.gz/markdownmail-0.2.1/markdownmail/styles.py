2428a77861901e90e53e8b40ae93bfcc252528f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:41 +0000

    Add package markdownmail/0.2.1/markdownmail-0.2.1.tar.gz

---
+++ packages/markdownmail/markdownmail-0.2.1.tar.gz/markdownmail-0.2.1/markdownmail/styles.py
@@ -0,0 +1,23 @@
+DEFAULT_STYLE = """
+body {
+    font-family: Helvetica, Arial, sans-serif;
+    text-align: left;
+    background-color: #ebebeb;
+    padding-top: 15px;
+    padding-bottom: 15px;
+}
+
+#content {
+    background-color: #ffffff;
+    max-width: 650px;
+    margin-left: auto;
+    margin-right: auto;
+    padding-left: 10px;
+}
+
+@media only screen and (max-width:650px) {
+#content {
+    width: 100%;
+}
+"""
+
