a97f7d2c316c0b41a421ad0ccc9eaeabca4dc331
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:33 +0000

    Add package ohad1234/0.1/ohad1234-0.1.tar.gz

---
+++ packages/ohad1234/ohad1234-0.1.tar.gz/ohad1234-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ohad1234',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='http://github.com/storborg/funniest1234',
+      author='Flying Circus',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['ohad1234'],
+      zip_safe=False)
