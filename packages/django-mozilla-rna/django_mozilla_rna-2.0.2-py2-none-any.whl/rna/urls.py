c78ada1e736e1eb22d6e723a5f0160f284bd3a37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:23:24 +0000

    Add package django-mozilla-rna/2.0.2/django_mozilla_rna-2.0.2-py2-none-any.whl

---
+++ packages/django-mozilla-rna/django_mozilla_rna-2.0.2-py2-none-any.whl/rna/urls.py
@@ -0,0 +1,20 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+from django.conf.urls import url, include
+
+from rest_framework import routers
+
+from . import views
+
+
+router = routers.DefaultRouter()
+router.register('notes', views.NoteViewSet)
+router.register('releases', views.ReleaseViewSet)
+
+urlpatterns = [
+    url(r'^', include(router.urls)),
+    url(r'^releases/(?P<pk>\d+)/notes/$', views.NestedNoteView.as_view()),
+    url(r'^auth_token/$', views.auth_token),
+    url(r'^sync/?$', views.rnasync),
+]
