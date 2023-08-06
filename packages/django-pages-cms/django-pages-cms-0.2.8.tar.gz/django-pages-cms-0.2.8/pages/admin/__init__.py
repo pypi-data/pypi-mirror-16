4318c5a03e07e0605438648e9f9efa4ca9d48394
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:31 +0000

    Add package django-pages-cms/0.2.8/django-pages-cms-0.2.8.tar.gz

---
+++ packages/django-pages-cms/django-pages-cms-0.2.8.tar.gz/django-pages-cms-0.2.8/pages/admin/__init__.py
@@ -0,0 +1,7 @@
+from pages.admin.page import PageAdmin
+from pages.admin.pagecontent import PageContentAdmin
+from pages.admin.pagecontenttype import PageContentTypeAdmin
+from pages.admin.pagecontenttypes import PageSlugContentAdmin
+from pages.admin.pagecontenttypes import PageRedirectContentAdmin
+from pages.admin.pagecontenttypes import PageMetaContentAdmin
+from pages.admin.pagecontenttypes import PageTextContentAdmin
