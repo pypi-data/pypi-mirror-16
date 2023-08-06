f1db936ddddabf8de05155afc4cce584d51e15ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:05 +0000

    Add package ghostlog/0.2.1/ghostlog-0.2.1.tar.gz

---
+++ packages/ghostlog/ghostlog-0.2.1.tar.gz/ghostlog-0.2.1/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+setup(
+    name = 'ghostlog',
+    packages = [
+        'ghostlog',
+    ],
+    version = '0.2.1',
+    description = 'Easily log to syslog or stdout.',
+
+    install_requires=[],
+    tests_require=['testfixtures'],
+
+    test_suite = 'tests.unit',
+
+    author = 'Ezequiel Pochiero',
+    author_email = 'ezequiel@spect.ro',
+
+    url = 'https://bitbucket.org/spctr/spectro-ghostlog',
+    download_url = 'https://bitbucket.org/spctr/spectro-ghostlog/get/v0.2.1.zip',
+
+    keywords = ['log', 'logging', 'syslog'],
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        'Topic :: Utilities',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: System :: Logging',
+        'Topic :: System :: Systems Administration'
+    ]
+)
