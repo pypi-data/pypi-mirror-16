bfab1da7f19a117b593a169280c7f305ed14e5f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:06 +0000

    Add package glu/0.0.4/glu-0.0.4.tar.gz

---
+++ packages/glu/glu-0.0.4.tar.gz/glu-0.0.4/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='glu',
+    packages=find_packages(),
+    version='0.0.4',
+    description='Glue for DRY configurations',
+    author='Park Jong Bin',
+    author_email='chongkong94@gmail.com',
+    keywords=['glue', 'glu', 'dry', 'config', 'dry-config', 'dry-configurable'],
+    install_requires=[
+        'requests==2.10.0'
+    ],
+    entry_points={
+        'console_scripts': [
+            'glu = glu.__main__:main'
+        ]
+    },
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License'
+    ]
+)
