a26b10b33e44a65e3b98d0dd4b42b58d14f06eef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:05:11 +0000

    Add package sphinxmark/0.1.2/sphinxmark-0.1.2-py2.py3-none-any.whl

---
+++ packages/sphinxmark/sphinxmark-0.1.2-py2.py3-none-any.whl/sphinxmark-0.1.2.dist-info/DESCRIPTION.rst
@@ -0,0 +1,82 @@
+==========
+sphinxmark
+==========
+
+.. image:: https://travis-ci.org/kallimachos/sphinxmark.svg?branch=master
+   :target: https://travis-ci.org/kallimachos/sphinxmark
+
+.. image:: https://img.shields.io/pypi/status/sphinxmark.svg?style=flat
+   :target: https://pypi.python.org/pypi/sphinxmark
+
+.. image:: https://img.shields.io/pypi/v/sphinxmark.svg?style=flat
+   :target: https://pypi.python.org/pypi/sphinxmark
+
+.. image:: https://img.shields.io/badge/Python-2.7-brightgreen.svg?style=flat
+   :target: http://python.org
+
+.. image:: https://img.shields.io/badge/Python-3.4-brightgreen.svg?style=flat
+   :target: http://python.org
+
+.. image:: http://img.shields.io/badge/license-GPL-blue.svg?style=flat
+   :target: http://opensource.org/licenses/GPL-3.0
+
+**sphinxmark** is an extension for Sphinx that enables watermarks for
+HTML output.
+
+Full documentation: https://kallimachos.github.io/sphinxmark/
+
+
+Usage
+~~~~~
+
+#. Install sphinxmark:
+
+   .. code::
+
+      $ pip install sphinxmark
+
+#. Add sphinxmark to the list of extensions in ``conf.py``:
+
+   .. code::
+
+      extensions = ['sphinxmark']
+
+#. Specify a static directory in ``conf.py`` for your image files. If the
+   path does not exist, the sphinxmark extension creates the directory and
+   populates it with ``watermark.css`` and ``watermark-draft.png``:
+
+   .. code::
+
+      html_static_path = ['_static']
+
+   .. note::
+
+      ``watermark.css`` is recreated on each Sphinx run. Edits to this file
+      are not retained.
+
+#. Configure sphinxmark in ``conf.py`` as required:
+
+   .. code::
+
+      watermark_enable = True
+      watermark_image = 'default'
+      watermark_div = 'default'
+      watermark_debug = False
+
+#. Put images in your static directory and use the ``watermark_image`` option
+   to specify a custom watermark image.
+
+.. note::
+
+   The extension provides a template css file that uses the specified image
+   as the watermark for any area defined as ``div.body`` by default. To use
+   sphinxmark with themes that have the document body in a different div,
+   specify the div using the ``watermark_div`` option.
+
+   For example, for the ``sphinx_rtd_theme``,
+   use ``watermark_div = 'document'``.
+
+   For ``openstackdocstheme``,
+   use ``watermark_div = 'docs-body'``.
+
+
---
+++ packages/sphinxmark/sphinxmark-0.1.2-py2.py3-none-any.whl/sphinxmark/__init__.py
@@ -0,0 +1,103 @@
+#!/bin/python3
+# coding: utf-8
+"""
+A Sphinx extension that enables watermarks for HTML output.
+
+Options:
+    watermark_enable = True|False (default=False)
+
+    watermark_image = 'default' (image file; default=watermark-draft.png)
+
+    watermark_div = 'default' (CSS div; default=body)
+
+    watermark_debug = True|False (default=False)
+"""
+
+import logging
+import os
+import shutil
+
+from bottle import TEMPLATE_PATH, template
+
+
+def setstatic(app):
+    """Set the static path, and create static directory if required."""
+    staticpath = app.config.html_static_path
+    logging.debug('html_static_path: ' + str(app.config.html_static_path))
+
+    if not staticpath:
+        logging.error('Config error: set `html_static_path` in conf.py')
+        return 1
+    else:
+        staticpath = app.config.html_static_path[0]
+        logging.debug("Using '" + staticpath + "' as static path.")
+
+    if not os.path.exists(staticpath):
+        logging.debug('Watermark: Creating ' + staticpath)
+        os.makedirs(staticpath)
+
+    return(staticpath)
+
+
+def watermark(app, env):
+    """Add watermark."""
+    if app.config.watermark_debug is True:
+        logging.basicConfig(level=logging.DEBUG)
+
+    if app.config.watermark_enable is True:
+        staticpath = setstatic(app)
+
+        if staticpath is 1:
+            logging.error('Failed to add watermark.')
+            return
+
+        # append source directory to TEMPLATE_PATH so template is found
+        srcdir = os.path.abspath(os.path.dirname(__file__))
+        TEMPLATE_PATH.append(srcdir)
+
+        if app.config.watermark_image == 'default':
+            image = os.path.join(srcdir, 'watermark-draft.png')
+            logging.debug('Using default image: ' + image)
+            shutil.copy(image, staticpath)
+            logging.debug("Copying '" + image + "' to '" + staticpath + "'")
+        else:
+            image = app.config.watermark_image
+            logging.debug('Image: ' + image)
+
+        image = os.path.basename(image)
+        if os.path.exists(os.path.join(staticpath, image)) is False:
+            logging.error("Cannot find '%s'. Place watermark images in '%s'",
+                          image, staticpath)
+
+        if app.config.watermark_div == 'default':
+            div = 'body'
+        else:
+            div = app.config.watermark_div
+
+        cssfile = 'watermark.css'
+        css = template('watermark', div=div, image=image)
+        logging.debug("Template: " + css)
+
+        with open(os.path.join(staticpath, cssfile), 'w') as f:
+            f.write(css)
+        app.info('adding watermark...', nonl=True)
+        app.add_stylesheet(cssfile)
+        app.info(' done')
+
+
+def setup(app):
+    """Setup for Sphinx ext."""
+    logging.basicConfig(format='%(levelname)s:Watermark: %(message)s')
+    try:
+        app.add_config_value('watermark_enable', False, 'html')
+        app.add_config_value('watermark_image', 'default', 'html')
+        app.add_config_value('watermark_div', 'default', 'html')
+        app.add_config_value('watermark_debug', False, 'html')
+        app.connect('env-updated', watermark)
+    except:
+        logging.error('Failed to add watermark.')
+    return {'version': '0.1'}
+
+
+if __name__ == '__main__':
+    pass
