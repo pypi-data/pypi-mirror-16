5df2b89c488fc3df040c2aeaa697923e2b9a8ecd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:50 +0000

    Add package FaBoProximity_VCNL4010/1.0.0/FaBoProximity_VCNL4010-1.0.0.tar.gz

---
+++ packages/FaBoProximity_VCNL4010/FaBoProximity_VCNL4010-1.0.0.tar.gz/FaBoProximity_VCNL4010-1.0.0/FaBoProximity_VCNL4010/__init__.py
@@ -0,0 +1,1 @@
+from .VCNL4010 import *
---
+++ packages/FaBoProximity_VCNL4010/FaBoProximity_VCNL4010-1.0.0.tar.gz/FaBoProximity_VCNL4010-1.0.0/setup.py
@@ -0,0 +1,29 @@
+import os
+from setuptools import setup, find_packages
+
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except:
+    long_description = ''
+
+classifiers = ['Development Status :: 4 - Beta',
+               'Operating System :: POSIX :: Linux',
+               'License :: OSI Approved :: Apache Software License',
+               'Intended Audience :: Developers',
+               'Programming Language :: Python :: 2.7',
+               'Topic :: Software Development',
+               'Topic :: System :: Hardware']
+
+setup(
+    name         = 'FaBoProximity_VCNL4010',
+    version      = '1.0.0',
+    author       = 'FaBo',
+    author_email = 'info@fabo.io',
+    description  = "This is a library for the FaBo Humidity I2C Brick.",
+    long_description=long_description,
+    url          = 'https://github.com/FaBoPlatform/FaBoProximity-VCNL4010-Python',
+    license      = 'Apache License 2.0',
+    classifiers  = classifiers,
+    packages     = find_packages()
+)
