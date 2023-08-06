3d84c96adb8e0fbdc8163856747553c677fb565c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:57 +0000

    Add package aloktest/1.0.0/aloktest-1.0.0.tar.gz

---
+++ packages/aloktest/aloktest-1.0.0.tar.gz/aloktest-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+	name		= 'aloktest',
+	version		= '1.0.0',
+	py_modules	= ['aloktest'],
+	author		= 'alok',
+	author_email 	= 'alok_nayak75@yahoo.com',
+	description	= 'A simple printer of nested lists'
+	)
