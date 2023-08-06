bcc74ca453f16a0af7f74efe988a20aac4bbba4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:17 +0000

    Add package ikalman/0.1.7/ikalman-0.1.7.tar.gz

---
+++ packages/ikalman/ikalman-0.1.7.tar.gz/ikalman-0.1.7/setup.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup, Extension
+
+setup(
+        name="ikalman",
+        ext_modules=[Extension("ikalman",
+                               sources=["./python_bindings.c",
+                                        "./gps.c",
+                                        "./kalman.c",
+                                        "./matrix.c"],
+                               extra_compile_args=['-std=c99'])],
+        headers=['./gps.h', './matrix.h', 'kalman.h', 'gpx.h'],
+        version="0.1.7",
+        author="Joshua Semar",
+        author_email="joshua.semar@mapmyfitness.com",
+        download_url='https://github.com/ruipgil/ikalman/archive/master.zip',
+        url="https://github.com/ruipgil/ikalman",
+        description = 'Python bindings for the ikalman c library'
+      )
+
