9a2e97113149263d7bc1c2273c3d8156a4b319c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:48 +0000

    Add package pyactiverecord/0.4.2/pyactiverecord-0.4.2.tar.gz

---
+++ packages/pyactiverecord/pyactiverecord-0.4.2.tar.gz/pyactiverecord-0.4.2/model/column.py
@@ -0,0 +1,4 @@
+class Column:
+    def __init__(self, type, length=255):
+        self.type = type
+        self.length = length
---
+++ packages/pyactiverecord/pyactiverecord-0.4.2.tar.gz/pyactiverecord-0.4.2/model/type.py
@@ -0,0 +1,14 @@
+class Type:
+    tinyint = "tinyint"
+    int = "int"
+    bigint = "bigint"
+    float = "float"
+    double = "double"
+    varchar = "varchar"
+    text = "text"
+    date = "date"
+    datetime = "datetime"
+    timestamp = "timestamp"
+    time = "time"
+
+
