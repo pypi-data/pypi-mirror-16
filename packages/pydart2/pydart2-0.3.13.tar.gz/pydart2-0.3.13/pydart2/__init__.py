4e122f2b88a8a0590822fba74e331523ae19f0d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:33:47 +0000

    Add package pydart2/0.3.13/pydart2-0.3.13.tar.gz

---
+++ packages/pydart2/pydart2-0.3.13.tar.gz/pydart2-0.3.13/pydart2/__init__.py
@@ -0,0 +1,20 @@
+import pydart2_api as papi
+import world
+import constraints
+import os.path
+import sys
+try:
+    import gui
+except Exception:
+    e = sys.exc_info()[1]
+    print("-" * 40)
+    print("Error while importing pydart2.gui")
+    print(e)
+    print("-" * 40)
+
+import utils
+from world import World
+
+
+def init(verbose=True):
+    papi.init(verbose)
