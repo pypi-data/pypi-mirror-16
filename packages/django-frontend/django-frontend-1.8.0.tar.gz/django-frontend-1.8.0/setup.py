cd14054c5d7315708b4f014b7727787f720f36e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:19 +0000

    Add package django-frontend/1.8.0/django-frontend-1.8.0.tar.gz

---
+++ packages/django-frontend/django-frontend-1.8.0.tar.gz/django-frontend-1.8.0/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+version = '1.8.0'
+
+setup(name='django-frontend',
+      version=version,
+      description="Django Frontend is a collection of static files and templates to jumpstart Django front-end development.",
+      long_description=open("README.rst", "r").read(),
+      classifiers=[
+          "Development Status :: 5 - Production/Stable",
+          "Environment :: Web Environment",
+          "Intended Audience :: End Users/Desktop",
+          "Natural Language :: English",
+          "Operating System :: OS Independent",
+          "Framework :: Django",
+          "Framework :: Django :: 1.6",
+          "Framework :: Django :: 1.7",
+          "Framework :: Django :: 1.8",
+          "Framework :: Django :: 1.9",
+          "Programming Language :: Python :: 2",
+          "Programming Language :: Python :: 3",
+          "Programming Language :: Python",
+          "Topic :: Internet :: WWW/HTTP :: Dynamic Content :: CGI Tools/Libraries",
+          "Topic :: Utilities",
+          "License :: OSI Approved :: MIT License",
+          ],
+      keywords='',
+      author='Jon Faustman',
+      author_email='jon@faustman.org',
+      url='http://github.com/jonfaustman/django-frontend',
+      license='MIT',
+      packages=find_packages(),
+      install_requires = [],
+      include_package_data=True,
+      zip_safe=False,
+    )
