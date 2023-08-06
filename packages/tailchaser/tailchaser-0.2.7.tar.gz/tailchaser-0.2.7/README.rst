18e5c18c7bbdcacedf9b5ad377a01020c3035fd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:15 +0000

    Add package tailchaser/0.2.7/tailchaser-0.2.7.tar.gz

---
+++ packages/tailchaser/tailchaser-0.2.7.tar.gz/tailchaser-0.2.7/README.rst
@@ -0,0 +1,306 @@
+========
+Overview
+========
+
+.. start-badges
+
+.. list-table::
+    :stub-columns: 1
+
+    * - docs
+      - |docs|
+    * - tests
+      - | |travis|  |requires|
+        | |codecov| |landscape| |codeclimate|
+    * - package
+      - |version| |downloads| |wheel| |supported-versions| |supported-implementations|
+
+.. |docs| image:: https://readthedocs.org/projects/tailchaser/badge/?style=flat
+    :target: https://readthedocs.org/projects/tailchaser
+    :alt: Documentation Status
+
+.. |travis| image:: https://travis-ci.org/thanos/tailchaser.svg?branch=master
+    :alt: Travis-CI Build Status
+    :target: https://travis-ci.org/thanos/tailchaser
+
+.. |appveyor| image:: https://ci.appveyor.com/api/projects/status/github/thanos/tailchaser?branch=master&svg=true
+    :alt: AppVeyor Build Status
+    :target: https://ci.appveyor.com/project/thanos/tailchaser
+
+.. |requires| image:: https://requires.io/github/thanos/tailchaser/requirements.svg?branch=master
+    :alt: Requirements Status
+    :target: https://requires.io/github/thanos/tailchaser/requirements/?branch=master
+
+.. |codecov| image:: https://codecov.io/github/thanos/tailchaser/coverage.svg?branch=master
+    :alt: Coverage Status
+    :target: https://codecov.io/github/thanos/tailchaser
+    
+
+.. |landscape| image:: https://landscape.io/github/thanos/tailchaser/master/landscape.svg?style=flat
+    :target: https://landscape.io/github/thanos/tailchaser/master
+    :alt: Code Quality Status
+
+.. |codeclimate| image:: https://codeclimate.com/github/thanos/tailchaser/badges/gpa.svg
+   :target: https://codeclimate.com/github/thanos/tailchaser
+   :alt: CodeClimate Quality Status
+
+.. |version| image:: https://img.shields.io/pypi/v/tailchaser.svg?style=flat
+    :alt: PyPI Package latest release
+    :target: https://pypi.python.org/pypi/tailchaser
+
+.. |downloads| image:: https://img.shields.io/pypi/dm/tailchaser.svg?style=flat
+    :alt: PyPI Package monthly downloads
+    :target: https://pypi.python.org/pypi/tailchaser
+
+.. |wheel| image:: https://img.shields.io/pypi/wheel/tailchaser.svg?style=flat
+    :alt: PyPI Wheel
+    :target: https://pypi.python.org/pypi/tailchaser
+
+.. |supported-versions| image:: https://img.shields.io/pypi/pyversions/tailchaser.svg?style=flat
+    :alt: Supported versions
+    :target: https://pypi.python.org/pypi/tailchaser
+
+.. |supported-implementations| image:: https://img.shields.io/pypi/implementation/tailchaser.svg?style=flat
+    :alt: Supported implementations
+    :target: https://pypi.python.org/pypi/tailchaser
+
+
+.. end-badges
+
+The ultimate rotation and windows friendly log tailer plus a lot more...
+
+- backfills rotated files
+- handles gz and bz2 files
+- handles multi line records
+- doesn't break windows rotating log handlers
+- easy to extend
+- Free software: BSD license
+- other than six pure Python stdlib.
+
+Installation
+============ 
+
+::
+    
+    pip install tailchaser
+    
+Thsi will install the tailchaser library in your site-packages and it will also add the script tailchase to your Scripts directory. 
+
+
+Usage
+===== 
+   
+::
+
+    $ tailchase /where/my/logs/*.log
+
+    $ tailchase -h
+    
+    usage: tailchase [-h] [--only-backfill] [--dont-backfill]
+                   [--clear-checkpoint] [--read-period READ_PERIOD]
+                   [--read-pause READ_PAUSE] [--reading-from {unix,win}]
+                   [--temp-dir TEMP_DIR]
+                   [--logging {DEBUG,INFO,WARN,ERROR,CRITICAL}]
+                   file-pattern
+
+
+    positional arguments:
+      file-pattern          The file pattern to tail, such as /var/log/access.*
+
+    optional arguments:
+      -h, --help            show this help message and exit
+      --only-backfill       dont't tail, default: False
+      --dont-backfill       basically only tail, default: False
+      --clear-checkpoint    start form the begining, default: False
+      --read-period READ_PERIOD
+                        how long you read before you pause. If zero you don't
+                        pause, default: 1
+      --read-pause READ_PAUSE
+                        how long you pause between reads, default: 0
+      --reading-from PLATFORM
+                        sets how long you read and then pause can be one of {unix,win}, default: win
+      --temp-dir TEMP_DIR   on back fill files are copied to a temp directory.Use
+                        this to set this directory, default: None
+      --logging LEVEL
+                        logging level it can be one of  DEBUG,INFO,WARN,ERROR,CRITICAL, default: ERROR
+
+
+
+In its simplest form
+--------------------
+
+In its simplest form tailchase works like a combination of cat and tail -f except it will start with the oldest rotated file. So if you were to have ::
+
+    /var/log/opendirectoryd.log
+    /var/log/opendirectoryd.log.0
+    /var/log/opendirectoryd.log.1
+    /var/log/opendirectoryd.log.2
+    /var/log/opendirectoryd.log.3
+    /var/log/opendirectoryd.log.4
+
+it would first output  the text of /var/log/opendirectoryd.log.4, then /var/log/opendirectoryd.log.3, etc. until it reached the newest file then when it  has reached the end of /var/log/opendirectoryd.log revert to a "tail -f" behaviour.
+
+So if you were to do ::
+    
+    tailchase 'tests/logs/opendirectoryd.*'
+    
+"*note: In bash you need to quote the wildcard otherwise it will be expanded into the scripts argv array.*"
+
+you would get something like ::
+
+    2015-12-24 15:39:56.733754 EST - AID: 0x0000000000000000 - Registered node with name '/Contacts'
+    2015-12-24 15:39:56.733933 EST - AID: 0x0000000000000000 - Registered node with name '/LDAPv3' as hidden
+    2015-12-24 15:39:56.736154 EST - AID: 0x0000000000000000 - Registered node with name '/Local' as hidden
+    2015-12-24 15:39:56.736868 EST - AID: 0x0000000000000000 - Registered node with name '/NIS' as hidden
+    2015-12-24 15:39:56.737134 EST - AID: 0x0000000000000000 - Discovered configuration for node name '/Search' at path '       2015-12-24 15:39:56.737151 EST - AID: 0x0000000000000000 - Registered node with name '/Search'
+    2015-12-24 15:39:56.738794 EST - AID: 0x0000000000000000 - Loaded bundle at path '/System/Library/OpenDirectory/Modules     2015-12-24 15:39:56.740509 EST - AID: 0x0000000000000000 - Loaded bundle at path '/System/Library/OpenDirectory/Modules/
+
+
+Coding with tailchaser
+======================
+
+
+Using the tailchaser library in a project is probably best done by example.
+
+
+Example 1 - Tailchase to a REST service.
+----------------------------------------
+
+::
+
+    #
+    # Example 1 - Tail to Elastic
+    #
+
+    import requests
+
+    import tailchaser
+
+    class TailToElastic(tailchaser.Tailer):
+        def handoff(self, file_tailed, checkpoint, record):
+            """ Expect a record like:
+
+            20160204 10:28:15,525 INFO PropertiesLoaderSupport - Loading properties file from URL [file:C:/WaterWorks/Broken/BSE//config/lme-market.properties]
+            20160204 10:28:15,541 INFO PropertiesLoaderSupport - Loading properties file from URL [file:C:/WaterWorks/Broken/BSE//config/default-database.properties]
+            20160204 10:28:15,541 INFO PropertiesLoaderSupport - Loading properties file from URL [file:C:/WaterWorks/Broken/BSE//config/default-hibernate.properties]
+            """
+
+            date, time, level, source, _, message = record.split(5)
+            result = requests.json("http://someelacticserver.com:9200/myindex/log", json={
+                            'timestamp': '{}T{}'.format(date, time)
+                            'level': level,
+                            'source': source,
+                            'message': message
+                            })
+            return result.status_code == requests.codes.ok
+
+
+Example 2 - Tailchase to  Kafka
+-------------------------------
+
+::
+    
+    #
+    # Example 2 - Tail to Kafka - shows how to add your own arguments and then send messages to kafka.
+    #
+
+    from kafka import KafkaProducer
+    rom tailchaser.tailer import Tailer
+    
+    
+    class TailToKafka(Tailer):
+        def add_arguments(cls, parser=None):
+            parser = super(TailToKafka, cls).add_arguments(parser)
+
+        HOSTS = 'localhost:1234'
+        TOPIC = b'log'
+        def startup(self):
+            self.kafka_producer = KafkaProducer(bootstrap_servers=self.HOSTS,value_serializer=msgpack.dumps)
+
+
+        def handoff(self, file_tailed, checkpoint, record):
+            """ Expect a record like:
+
+            20160204 10:28:15,525 INFO PropertiesLoaderSupport - Loading properties file from URL [file:C:/WaterWorks/Broken/BSE//config/lme-market.properties]
+            20160204 10:28:15,541 INFO PropertiesLoaderSupport - Loading properties file from URL [file:C:/WaterWorks/Broken/BSE//config/default-database.properties]
+            20160204 10:28:15,541 INFO PropertiesLoaderSupport - Loading properties file from URL [file:C:/WaterWorks/Broken/BSE//config/default-hibernate.properties]
+            """
+            self.kafka_producer.send(self.TOPIC, record).get(timeout=10)
+            return True
+             
+
+
+Example 3 - Saving and Loading last Offset from Zookeeper 
+---------------------------------------------------------
+
+::
+
+    #
+    # Example 3 - Saving and Loading last Offset from Zookeeper 
+    #
+    import platform
+    import pickle
+    import six
+    from kazoo.client import KazooClient
+    from tailchaser.tailer import Tailer
+
+    class TailToElastic(Tailer):
+        def start(self):
+            self.zk = KazooClient(hosts=self.config.zookeeper_host)
+            self.zk.start()
+            
+            
+        def stop():
+            self.zk.stop()
+            
+        def load_checkpoint(self):
+            zk_path = self.config.checkpoint_filename
+            try:
+                checkpoint = pickle.loads(self.zk.get(zk_path))
+                log.debug('loaded: %s %s', zk_path, checkpoint)
+                return checkpoint
+            except (IOError, EOFError):
+                log.debug('failed to load: %s', zk_path)
+                return self.INIT_CHECKPOINT
+
+        def save_checkpoint(self, checkpoint):
+            log.debug('dumping %s %s', self.config.checkpoint_filename, checkpoint)
+            return self.zk.set(self.config.checkpoint_filename, six.b(pickle.dumps(checkpoint)))
+
+
+        @staticmethod
+        def make_checkpoint_filename(source_pattern, path=None):
+            zk_path =  '/'.join(['tailchase', platform.node(), slugify(source_pattern)]
+            self.zk.ensure_path(zk_path)
+            return zk_path
+
+
+Documentation
+=============
+
+https://tailchaser.readthedocs.org/
+
+Development
+===========
+
+To run the all tests run::
+
+    tox
+
+Note, to combine the coverage data from all the tox environments run:
+
+.. list-table::
+    :widths: 10 90
+    :stub-columns: 1
+
+    - - Windows
+      - ::
+
+            set PYTEST_ADDOPTS=--cov-append
+            tox
+
+    - - Other
+      - ::
+
+            PYTEST_ADDOPTS=--cov-append tox
+
---
+++ packages/tailchaser/tailchaser-0.2.7.tar.gz/tailchaser-0.2.7/tests/test_tailchaser.py
@@ -0,0 +1,488 @@
+import glob
+import gzip
+import logging
+import logging.handlers
+import os
+import tempfile
+import threading
+import time
+
+import six
+
+from tailchaser.cli import main
+from tailchaser.tailer import Tailer
+
+
+def cmp_file(src_file, dst_file):
+    six.print_('testing: ', src_file, dst_file)
+    assert (Tailer.file_opener(src_file, 'rb').read() == Tailer.file_opener(dst_file, 'rb').read())
+
+
+def cmp_files(src_path, dst_path, make_name=os.path.basename):
+    src_files = glob.glob(src_path)
+    for src_file_path in src_files:
+        dst_file_path = os.path.join(dst_path, make_name(src_file_path))
+        cmp_file(src_file_path, dst_file_path)
+
+
+class Logger(threading.Thread):
+    def __init__(self):
+        self.RUNNING = False
+        super(Logger, self).__init__()
+
+    def run(self):
+        count = 0
+        self.RUNNING = True
+        while self.RUNNING:
+            count += 1
+            self.emit(count)
+
+    def emit(self, count):
+        six.print_(count)
+
+
+class RotatingWithDelayFileHandler(logging.handlers.RotatingFileHandler):
+    ROLL_DELAY = 2
+    EMIT_DELAY = 1
+    rolls = 0
+    ENCODING = None
+
+    def doRollover(self):
+        time.sleep(self.ROLL_DELAY)
+        self.rolls += 1
+        six.print_('rolls', self.rolls)
+        return super(RotatingWithDelayFileHandler, self).doRollover()
+
+    def emit(self, record):
+        time.sleep(self.EMIT_DELAY)
+        return super(RotatingWithDelayFileHandler, self).emit(record)
+
+    @classmethod
+    def generate(cls, log_file_path, emits, max_bytes=1024, backup_count=100):
+        count = 0
+        logger = logging.getLogger(__file__)
+        handler = cls(log_file_path, maxBytes=max_bytes, backupCount=backup_count, encoding=cls.ENCODING)
+        formatter = logging.Formatter('%(asctime)s %(levelname)-8s  %(name)-12s %(message)s')
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+        while count < emits:
+            count += 1
+            logger.error("%08d. %s", count, 'x' * 64)
+
+
+class RotatingGzipFileHandler(RotatingWithDelayFileHandler):
+    def doRollover(self):
+        """
+        Do a rollover, as described in __init__().
+        """
+        time.sleep(self.ROLL_DELAY)
+        if self.stream:
+            self.stream.close()
+            self.stream = None
+
+        if self.backupCount > 0:
+            for i in range(self.backupCount - 1, 0, -1):
+                sfn = "%s.%d.gz" % (self.baseFilename, i)
+                dfn = "%s.%d.gz" % (self.baseFilename, i + 1)
+                if os.path.exists(sfn):
+                    # print "%s -> %s" % (sfn, dfn)
+                    if os.path.exists(dfn):
+                        os.remove(dfn)
+                    os.rename(sfn, dfn)
+            dfn = self.baseFilename + ".1.gz"
+            if os.path.exists(dfn):
+                os.remove(dfn)
+            with open(self.baseFilename, "rb") as sf:
+                with gzip.open(dfn, "wb") as df:
+                    df.writelines(sf)
+            os.remove(self.baseFilename)
+            # print "%s -> %s" % (self.baseFilename, dfn)
+        self.mode = 'wb'
+        self.stream = self._open()
+
+
+class MultiLineLogHandler(logging.FileHandler):
+    ROLL_DELAY = 10
+    EMIT_DELAY = .01
+    ENCODING = None
+
+    @classmethod
+    def generate(cls, log_file_path, emits):
+        count = 0
+        logger = logging.getLogger(__file__)
+        handler = cls(log_file_path, encoding=cls.ENCODING)
+        formatter = logging.Formatter('%(asctime)s %(levelname)-8s  %(name)-12s %(message)s')
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+        while count < emits:
+            try:
+                count += 1
+                if count % 2 == 0:
+                    raise NameError("error %08d" % count)
+                logger.error("No Exception Thrown: %08d", count)
+            except NameError:
+                logger.exception("Exception Thrown: %08d", count)
+
+
+TEST_PATH = os.path.dirname(os.path.abspath(__file__))
+
+filter_count = 0
+
+
+def test_filter():
+    work_dir = tempfile.mkdtemp(prefix='tail-test_filter-tail_from_dir')
+    six.print_('generating log files', work_dir)
+    test_file = os.path.join(work_dir, 'test.log')
+    with open(test_file, 'wb') as file_to_tail:
+        file_name = file_to_tail.name
+        six.print_('file to tail with filter', file_to_tail)
+        for i in range(1000):
+            if i % 2 == 0:
+                line = "odd: %d\n" % i
+            else:
+                line = "even: %d\n" % i
+            file_to_tail.write(line)
+
+    def consumer_gen():
+        global filter_count
+        while True:
+            record = yield ()
+            filter_count += 1
+
+    consumer = consumer_gen()
+    consumer.send(None)
+    vargs = [__name__, '--only-backfill', '--clear-checkpoint', '--filter-re=odd:\\s*\\d+']
+    vargs.append(test_file)
+    main(vargs, consumer)
+    assert (500 == filter_count)
+
+
+BACKFILL_EMITS = 50
+
+
+def test_backfill(log_handler=RotatingWithDelayFileHandler, consumer=None, tail_to_dir=None, vargs=None):
+    tail_from_dir = tempfile.mkdtemp(prefix='tail-test_backfill-tail_from_dir')
+    six.print_('generating log files', tail_from_dir)
+    log_handler.generate(os.path.join(tail_from_dir, 'test.log'), BACKFILL_EMITS)
+    if not tail_to_dir:
+        tail_to_dir = tempfile.mkdtemp(prefix='tail-test_backfill-tail_to_dir')
+
+    if not consumer:
+        def consumer_gen():
+            while True:
+                record = yield ()
+                open(os.path.join(tail_to_dir, record[1][0]), 'ab').write(record[2])
+
+        consumer = consumer_gen()
+        consumer.send(None)
+    six.print_('start tailer', tail_to_dir)
+    source_pattern = os.path.join(tail_from_dir, '*')
+    if not vargs:
+        vargs = [__name__, '--only-backfill', '--clear-checkpoint']
+    vargs.append(source_pattern)
+    main(vargs, consumer)
+    cmp_files(source_pattern, tail_to_dir, lambda x: Tailer.make_sig(x))
+    six.print_('all done', tail_to_dir)
+    # for src_file_path in glob.glob(source_pattern):
+    #     dst_file_path = os.path.join(tail_to_dir, Tailer.make_sig(src_file_path))
+    #     six.print_("testing:", src_file_path, dst_file_path)
+    #     assert (Tailer.file_opener(src_file_path).read() == Tailer.file_opener(dst_file_path).read())
+
+
+def test_gzip_backfill():
+    test_backfill(RotatingGzipFileHandler)
+
+
+tailed_records = 0
+
+
+def test_multiline_records():
+    tail_to_dir = tempfile.mkdtemp(prefix='ml')
+
+    def consumer_gen(path):
+        global tailed_records
+        while True:
+            record = yield ()
+            tailed_records += 1
+            open(os.path.join(path, record[1][0]), 'ab').write(record[2])
+
+    consumer = consumer_gen(tail_to_dir)
+    consumer.send(None)
+    start_of_record_re = '\d{4}-\d{2}-\d{2}'
+    vargs = [__name__, '--only-backfill', '--clear-checkpoint', "--start-of-record-re=%s" % start_of_record_re]
+    test_backfill(MultiLineLogHandler, consumer, tail_to_dir, vargs=vargs)
+    six.print_('emimitted %d and  tailed %s' % (BACKFILL_EMITS, tailed_records))
+    assert (BACKFILL_EMITS == tailed_records)
+
+
+#
+#
+#   test against simple tail
+#
+
+def test_tail():
+    tmp_dir = tempfile.mkdtemp(prefix='tail-test')
+    log_file_path = os.path.join(tmp_dir, 'file.log')
+    six.print_(log_file_path)
+
+    class SimpleLogger(Logger):
+        def __init__(self):
+            self.RUNNING = False
+            super(SimpleLogger, self).__init__()
+
+        def emit(self, count):
+            open(log_file_path, 'ab').write("%08d. %s\n" % (count, 'x' * 64))
+            time.sleep(0.1)
+
+    loggen_thread = SimpleLogger()
+    loggen_thread.start()
+    six.print_('logger started')
+    copy_dir = tempfile.mkdtemp(prefix='tail-test')
+    six.print_(copy_dir)
+
+    def gx():
+        while True:
+            record = yield ()
+            open(os.path.join(copy_dir, record[1][0]), 'ab').write(record[2])
+
+    consumer = gx()
+    consumer.send(None)
+    tailer = Tailer(only_backfill=False)
+    tailer_thread = threading.Thread(target=tailer.run, args=(log_file_path, consumer))
+
+    tailer_lag = 30
+    logger_lag = 60
+    loggen_thread.join(tailer_lag)
+    six.print_('logger run more than %d secs, start tailer' % tailer_lag)
+    tailer_thread.start()
+    six.print_('tail started')
+    six.print_('run logger %d secs more' % (logger_lag - tailer_lag))
+    loggen_thread.join(logger_lag - tailer_lag)
+    six.print_('stop logger')
+    loggen_thread.RUNNING = False
+    if loggen_thread.is_alive():
+        loggen_thread.join()
+    log_files = glob.glob(os.path.join(tmp_dir, '*'))
+    six.print_('logger stopped')
+    tailer_thread.join(5)
+    six.print_('wait for tailer to idle')
+    copy_pattern = os.path.join(copy_dir, '*')
+    while True:
+        six.print_('log files %d == files processed %d' % (len(log_files), len(glob.glob(copy_pattern))))
+        if tailer.state == tailer.WAITING and len(log_files) == len(glob.glob(copy_pattern)):
+            break
+        time.sleep(1)
+    six.print_('stop tailer')
+    tailer.state = tailer.STOPPED
+    tailer_thread.join()
+    six.print_('tailer stopped')
+    cmp_files(log_file_path, copy_dir, lambda x: str(Tailer.make_sig(x)))
+
+
+def test_tail_with_break():
+    tmp_dir = tempfile.mkdtemp(prefix='tail-test')
+    log_file_path = os.path.join(tmp_dir, 'file.log')
+    six.print_(log_file_path)
+
+    class SimpleLogger(Logger):
+        def __init__(self):
+            self.RUNNING = False
+            super(SimpleLogger, self).__init__()
+
+        def emit(self, count):
+            open(log_file_path, 'ab').write("%08d. %s\n" % (count, 'x' * 64))
+            time.sleep(0.1)
+
+    class BreakingTailer(Tailer):
+        def __init__(self):
+            super(BreakingTailer, self).__init__(only_backfill=False)
+            self.interupt = threading.Event()
+
+        def handoff(self, file_tailed, checkpoint, record, receiver=None):
+            if self.interupt.is_set():
+                raise SystemExit()
+            super(BreakingTailer, self).handoff(file_tailed, checkpoint, record, receiver)
+
+    loggen_thread = SimpleLogger()
+    loggen_thread.start()
+    six.print_('logger started')
+    copy_dir = tempfile.mkdtemp(prefix='tail-test')
+    six.print_(copy_dir)
+
+    def gx():
+        while True:
+            record = yield ()
+            open(os.path.join(copy_dir, record[1][0]), 'ab').write(record[2])
+
+    consumer = gx()
+    consumer.send(None)
+    tailer = BreakingTailer()
+    tailer_thread = threading.Thread(target=tailer.run, args=(log_file_path, consumer))
+    tailer_lag = 40
+    logger_lag = 80
+    loggen_thread.join(tailer_lag)
+    six.print_('logger run more than %d secs, start tailer' % tailer_lag)
+    tailer_thread.start()
+    six.print_('tail started')
+    six.print_('run logger %d secs more' % (logger_lag - tailer_lag))
+    loggen_thread.join(logger_lag - tailer_lag - 20)
+    six.print_('stopping tailer')
+    tailer.interupt.set()
+    six.print_('waiting for tailer to stop')
+    loggen_thread.join(5)
+    six.print_('tailer stoped - staring again')
+    tailer = BreakingTailer()
+    tailer_thread = threading.Thread(target=tailer.run, args=(log_file_path, consumer))
+    tailer_thread.start()
+    loggen_thread.join(logger_lag - tailer_lag)
+    loggen_thread.RUNNING = False
+    if loggen_thread.is_alive():
+        loggen_thread.join()
+    six.print_('logger stopped')
+    tailer_thread.join(10)
+    six.print_('wait for tailer to idle')
+    log_files = glob.glob(os.path.join(tmp_dir, '*'))
+    copy_pattern = os.path.join(copy_dir, '*')
+    while True:
+        six.print_('log files %d == files processed %d' % (len(log_files), len(glob.glob(copy_pattern))))
+        if tailer.state == tailer.WAITING and len(log_files) == len(glob.glob(copy_pattern)):
+            break
+        time.sleep(1)
+    six.print_('stop tailer')
+    tailer.state = tailer.STOPPED
+    tailer_thread.join()
+    six.print_('tailer stopped')
+    cmp_files(log_file_path, copy_dir, lambda x: str(Tailer.make_sig(x)))
+
+
+#
+#
+#   test file drop senario
+#
+
+
+def test_file_drop():
+    drop_file_name = 'drop_file.txt'
+    src_file = os.path.join(tempfile.mkdtemp(prefix='test_file_drop-src'), drop_file_name)
+    dst_file = os.path.join(tempfile.mkdtemp(prefix='test_file_drop-dst'), drop_file_name)
+
+    def tail_writer():
+        while True:
+            file_name, checkpoint, record = yield ()
+            open(dst_file, 'ab').write(record)
+
+    consumer = tail_writer()
+    consumer.send(None)
+    tailer = Tailer()
+    six.print_("start watch for drop file:", src_file)
+    six.print_("will save to:", dst_file)
+    tailer_thread = threading.Thread(target=tailer.run, args=(src_file, consumer))
+    tailer_thread.start()
+    tailer_thread.join(10)
+
+    def file_drop(num_lines=250):
+        with open(src_file, 'wb') as dump:
+            for count in range(num_lines):
+                dump.write("%08d. %s\n" % (count + 1, 'x' * 64))
+
+    dump_thread = threading.Thread(target=file_drop)
+    dump_thread.start()
+    dump_thread.join()
+    six.print_('file dropped')
+    tailer_thread.join(10)
+
+    while True:
+        six.print_('checking for drop file ingest')
+        if tailer.state == tailer.WAITING and os.path.exists(dst_file):
+            break
+        time.sleep(1)
+
+    six.print_('pickup complete')
+    six.print_('stop tailer')
+    tailer.state = tailer.STOPPED
+    tailer_thread.join()
+    six.print_('tailer stopped')
+    assert (open(src_file, 'rb').read() == open(dst_file, 'rb').read())
+    cmp_files(src_file, dst_file, lambda x: x)
+
+
+#
+#
+#   test against rotating log
+#
+
+def test_rotating_log():
+    tmp_dir = tempfile.mkdtemp(prefix='tail-test')
+    log_file_path = os.path.join(tmp_dir, 'file.log')
+    six.print_(log_file_path)
+
+    class FastRotate(RotatingWithDelayFileHandler):
+        EMIT_DELAY = 0.3
+        ROLL_DELAY = 4
+
+    class RotatingLogger(Logger):
+
+        def __init__(self):
+            self.RUNNING = False
+            self.logger = logging.getLogger(__file__)
+            handler = FastRotate(log_file_path, maxBytes=2048,
+                                 backupCount=100)
+            self.logger.addHandler(handler)
+            super(RotatingLogger, self).__init__()
+
+        def emit(self, count):
+            self.logger.error("%08d. %s", count, 'x' * 256)
+
+    loggen_thread = RotatingLogger()
+    loggen_thread.start()
+    six.print_('logger started')
+    copy_dir = tempfile.mkdtemp(prefix='tail-test')
+    six.print_(copy_dir)
+
+    def gx():
+        while True:
+            record = yield ()
+            open(os.path.join(copy_dir, record[1][0]), 'ab').write(record[2])
+
+    consumer = gx()
+    consumer.send(None)
+    tailer = Tailer(only_backfill=False, read_pause=2)
+    tailer_thread = threading.Thread(target=tailer.run, args=(os.path.join(tmp_dir, '*'), consumer))
+
+    tailer_lag = 20
+    logger_run = 40
+    loggen_thread.join(tailer_lag)
+    six.print_('logger run more than %d secs, start tailer' % tailer_lag)
+    tailer_thread.start()
+    six.print_('tail started')
+    six.print_('run logger %d secs more' % (logger_run - tailer_lag))
+    loggen_thread.join(logger_run - tailer_lag)
+    six.print_('stop logger')
+    loggen_thread.RUNNING = False
+    if loggen_thread.is_alive():
+        loggen_thread.join()
+    log_files = glob.glob(os.path.join(tmp_dir, '*'))
+    six.print_('logger stopped')
+    tailer_thread.join(5)
+    six.print_('wait for tailer to idle')
+    copy_pattern = os.path.join(copy_dir, '*')
+    while True:
+        six.print_('log files %d == files processed %d --> %s' % (len(log_files), len(glob.glob(copy_pattern)),
+                                                                  tailer.state))
+        if tailer.state == tailer.WAITING and len(log_files) <= len(glob.glob(copy_pattern)):
+            break
+        time.sleep(1)
+    six.print_('stop tailer')
+    tailer.state = tailer.STOPPED
+    tailer_thread.join()
+    six.print_('tailer stopped')
+
+    for src_file_path in log_files:
+        dst_file_path = os.path.join(copy_dir, str(Tailer.make_sig(src_file_path)))
+        assert (open(src_file_path).read() == open(dst_file_path).read())
+
+        # cmp_files(tmp_dir, copy_dir, lambda x: str(Tailer.make_sig(x)))
+
+
+if __name__ == '__main__':
+    test_filter()
