da8fd5df786e25189e57f559d9efb9dc3fd1d568
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:38 +0000

    Add package exchangelib/1.5.2/exchangelib-1.5.2.tar.gz

---
+++ packages/exchangelib/exchangelib-1.5.2.tar.gz/exchangelib-1.5.2/exchangelib/__init__.py
@@ -0,0 +1,14 @@
+from .account import Account
+from .autodiscover import discover
+from .configuration import Configuration
+from .credentials import DELEGATE, IMPERSONATION, Credentials
+from .ewsdatetime import EWSDateTime, EWSTimeZone
+from .services import IdOnly, AllProperties, SHALLOW, DEEP
+from .transport import NTLM, DIGEST, BASIC
+
+
+def close_connections():
+    from .autodiscover import close_connections as close_autodiscover_connections
+    from .protocol import close_connections as close_protocol_connections
+    close_autodiscover_connections()
+    close_protocol_connections()
