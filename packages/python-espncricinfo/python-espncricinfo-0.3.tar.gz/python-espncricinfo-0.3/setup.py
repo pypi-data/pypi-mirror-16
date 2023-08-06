cdfab7fa5dcb379aef03711e737467b9bcc0f9ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:16 +0000

    Add package python-espncricinfo/0.3/python-espncricinfo-0.3.tar.gz

---
+++ packages/python-espncricinfo/python-espncricinfo-0.3.tar.gz/python-espncricinfo-0.3/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(name="python-espncricinfo",
+      version="0.3",
+      description="ESPNCricInfo API client",
+      license="MIT",
+      install_requires=["requests", "bs4", "dateparser"],
+      author="Derek Willis",
+      author_email="dwillis@gmail.com",
+      url="http://github.com/dwillis/python-espncricinfo",
+      packages = find_packages(),
+      keywords= "espncricinfo cricket t20 odi",
+      classifiers=['Development Status :: 4 - Beta'],
+      zip_safe = True)
