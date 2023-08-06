9062d608f13834cf8744ecec347c25781c740822
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:02 +0000

    Add package everett/0.1/everett-0.1-py2.py3-none-any.whl

---
+++ packages/everett/everett-0.1-py2.py3-none-any.whl/everett/__init__.py
@@ -0,0 +1,20 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+# Singleton indicating a non-value
+NO_VALUE = object()
+
+
+# Configuration error indicator
+class ConfigurationError(Exception):
+    pass
+
+
+__author__ = 'Will Kahn-Greene'
+__email__ = 'willkg@mozilla.com'
+
+# yyyymmdd
+__releasedate__ = '20160801'
+# x.y or x.y.dev
+__version__ = '0.1'
