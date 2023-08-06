cce4fd0eb8f4d1e8d2a50a3b9b2bae1b8eb3cdbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:33 +0000

    Add package wllvm/1.0.1/wllvm-1.0.1.tar.gz

---
+++ packages/wllvm/wllvm-1.0.1.tar.gz/wllvm-1.0.1/wllvm/version.py
@@ -0,0 +1,19 @@
+# Freeping Creaturism:
+#
+# this is the all important version number used by pip.
+#
+#
+"""
+Version History:
+
+1.0.0   - 8/2/2016 initial birth as a pip package.
+1.0.1   - 8/2/2016 the rst gets a make over, and doc strings 
+          became more pervasive.
+
+
+
+
+
+"""
+wllvm_version='1.0.1'
+
