b9bc17fc400c66322ad69b27e6d6f5b92708d85d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:06:58 +0000

    Add package fluffy-server/1.6.3/fluffy-server-1.6.3.tar.gz

---
+++ packages/fluffy-server/fluffy-server-1.6.3.tar.gz/fluffy-server-1.6.3/setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name='fluffy-server',
+    version='1.6.3',
+    author='Chris Kuehl',
+    author_email='ckuehl@ocf.berkeley.edu',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires={
+        'boto3',
+        'cached_property',
+        'flask',
+        'pygments',
+    },
+    classifiers={
+        'Programming Language :: Python :: 3',
+    },
+    entry_points={
+        'console_scripts': [
+            'fluffy-upload-assets = fluffy.assets:upload_assets',
+        ],
+    },
+)
