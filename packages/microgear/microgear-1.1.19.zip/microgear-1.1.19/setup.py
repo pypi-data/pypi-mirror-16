3ea9d3322d6296032a14b1dde1945470a874b6a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:57:39 +0000

    Add package microgear/1.1.19/microgear-1.1.19.zip

---
+++ packages/microgear/microgear-1.1.19.zip/microgear-1.1.19/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'microgear',
+  packages = ['microgear'], # this must be the same as the name above
+  version = '1.1.19',
+  description = 'Client library of Python, connect application code or hardware to netpie platform.',
+  author = 'Ridnarong Promya',
+  author_email = 'developer@netpie.io',
+  url = 'https://github.com/netpieio/microgear-python', # use the URL to the github repo
+  keywords = ['netpie','iot','mqtt'], # arbitrary keywords
+  classifiers = [],
+  install_requires=['paho-mqtt','oauth2'],
+)
