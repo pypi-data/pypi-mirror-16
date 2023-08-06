1522b01839d36dcfd3f43651536850b4f0c0baf0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:35 +0000

    Add package msg/0.7.8/msg-0.7.8.tar.gz

---
+++ packages/msg/msg-0.7.8.tar.gz/msg-0.7.8/msg/__init__.py
@@ -0,0 +1,6 @@
+if __name__ != "__main__":
+    from . import msg
+    from . import models
+    from . import config
+
+__version__ = "0.7.8"
---
+++ packages/msg/msg-0.7.8.tar.gz/msg-0.7.8/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+"""msg package installer.
+
+"""
+
+import sys
+from setuptools import setup
+
+
+# demon magic to get version without activating
+# imports
+exec(open('msg/__init__.py').read())
+setup(name='msg',
+      packages=['msg'],
+      version=__version__,
+      description='msg server backend',
+      setup_requires=['setuptools-markdown',],
+      install_requires=['flask', 'flask_sqlalchemy',
+                        'flask_sqlalchemy', 'flask_limiter', 'flask-restful',
+                        'flask-httpauth', 'requests', 'flask_sse',
+                        'gunicorn', 'jsonschema', 'gevent'],
+      long_description_markdown_filename='README.md',
+      author='Lily Seabreeze',
+      author_email='lillian.gardenia.seabreeze@gmail.com',
+      license='MIT',
+      classifiers=['Development Status :: 3 - Alpha',
+                   'Intended Audience :: Developers',
+                   'Natural Language :: English',
+                   'Programming Language :: Python :: 2.7',
+                   'Programming Language :: Python :: 3.5',
+                  ],
+    )
