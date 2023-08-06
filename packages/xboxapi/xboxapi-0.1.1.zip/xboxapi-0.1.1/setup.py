d95fb63caf9d925f72680450c1da9de19883e313
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:37 +0000

    Add package xboxapi/0.1.1/xboxapi-0.1.1.zip

---
+++ packages/xboxapi/xboxapi-0.1.1.zip/xboxapi-0.1.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name='xboxapi',
+  packages=['xboxapi'],
+  version='0.1.1',
+  description='A wrapper library for XboxAPI.com',
+  author='XboxAPI.com',
+  author_email='support@xboxapi.com',
+  url='https://github.com/mKeRix/Python-Wrapper',
+  download_url='https://github.com/mKeRix/Python-Wrapper/tarball/0.1.1',
+  keywords=['xbox live', 'xboxapi', 'xbox one', 'gaming', 'wrapper', 'library'],
+  classifiers=[],
+)
