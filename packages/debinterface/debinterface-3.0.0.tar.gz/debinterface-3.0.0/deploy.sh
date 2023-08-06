45e9a481bd4acafd23ae92ab2fa6cdce303c5e88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:23:39 +0000

    Add package debinterface/3.0.0/debinterface-3.0.0.tar.gz

---
+++ packages/debinterface/debinterface-3.0.0.tar.gz/debinterface-3.0.0/deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Only needed once
+# python setup.py register -r https://testpypi.python.org/pypi
+# python setup.py register -r https://pypi.python.org/pypi
+
+# Test
+python setup.py test
+
+# check
+check-manifest -u -v
+
+# Build packages
+python setup.py sdist
+python setup.py bdist_wheel  # Not universal
+
+# Deploy with gpg
+twine upload dist/* -r pypitest --sign --skip-existing
+twine upload dist/* --sign
+
+# Deploy legacy version
+# Upload to test PyPi
+# python setup.py sdist upload -r https://testpypi.python.org/pypi
+# Upload to prod PyPi
+# python setup.py sdist upload
