66574c5be7fd7b997e83eb964c036799be64c481
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:02 +0000

    Add package Lumpy/0.3.1/Lumpy-0.3.1-py2.7.egg

---
+++ packages/Lumpy/Lumpy-0.3.1-py2.7.egg/lumpy/__init__.py
@@ -0,0 +1,10 @@
+"""
+Sends an email to a single recipient straight to his MTA
+"""
+from .cli import run  # noqa
+from .mail import Mail  # noqa
+
+__author__ = "Jorge Barata"
+__author_email__ = "jorge.barata.gonzalez@gmail.com"
+__version__ = "0.3.1"
+__license__ = "MIT"
