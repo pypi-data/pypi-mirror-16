95f0ae685d342533f0ea928743edcaad0339316f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:58:34 +0000

    Add package nester_volnet/1.0.1/nester_volnet-1.0.1.tar.gz

---
+++ packages/nester_volnet/nester_volnet-1.0.1.tar.gz/nester_volnet-1.0.1/nester.py
@@ -0,0 +1,11 @@
+"""This is the "standard" way to include a multiple-line comment in your code."""
+
+# This is a single line comment
+
+def print_list(the_list):
+	for item in the_list:
+		if isinstance(item, list):
+			print("    ")
+			print_list(item)
+		else:
+			print(item)
