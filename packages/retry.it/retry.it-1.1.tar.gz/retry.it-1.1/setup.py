6c1dcac8f2023c6f680582f3cacf69e1bc190dc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:03:09 +0000

    Add package retry.it/1.1/retry.it-1.1.tar.gz

---
+++ packages/retry.it/retry.it-1.1.tar.gz/retry.it-1.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+import retry
+
+retry_classifiers = [
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 3',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Topic :: Software Development :: Libraries',
+    'Topic :: Utilities',
+]
+
+description = 'A small and simple library to retry functions'
+
+setup(
+    name='retry.it',
+    version=retry.__version__,
+    author=retry.__author__,
+    author_email='seemethere101@gmail.com',
+    url='https://github.com/seemethere/retry',
+    py_modules=['retry'],
+    description=description,
+    long_description=description,
+    license='MIT',
+    classifiers=retry_classifiers,
+    setup_requires=['pytest-runner'],
+    tests_requires=['pytest'],
+)
