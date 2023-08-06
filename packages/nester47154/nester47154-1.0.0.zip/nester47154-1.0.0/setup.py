b4c5b9ef16557b10e9bb551984c7a3cc3c0893fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:50 +0000

    Add package nester47154/1.0.0/nester47154-1.0.0.zip

---
+++ packages/nester47154/nester47154-1.0.0.zip/nester47154-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name ='nester47154',
+    version = '1.0.0',
+    py_modules = ['nester47154'],
+    author = '4a1g0118',
+    author_email = '4a1g0118@stust.edu.tw',
+    url = 'http://www.headfirstlabs.com',
+    description = 'A simple printer of nested lists',
+    )
