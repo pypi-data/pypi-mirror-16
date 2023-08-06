8cd4bb969cd236112fc4fac945e36e81b1e1fc2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:17:17 +0000

    Add package placeholders/1.1/placeholders-1.1.tar.gz

---
+++ packages/placeholders/placeholders-1.1.tar.gz/placeholders-1.1/placeholder_tools.py
@@ -0,0 +1,25 @@
+import os
+import sys
+import piexif
+from PIL import Image as IMG
+
+
+def set_image_tag(filename, tag):
+    exif_ifd = {
+        piexif.ExifIFD.UserComment: unicode(tag)
+    }
+    exif_dict = {"Exif": exif_ifd}
+    exif_bytes = piexif.dump(exif_dict)
+    im = IMG.open(filename)
+    im.save(filename, exif=exif_bytes)
+
+def get_image_tag(filename):
+    try:
+        exif_dict = piexif.load(filename)
+        if piexif.ExifIFD.UserComment in exif_dict['Exif']:
+            message = exif_dict['Exif'][piexif.ExifIFD.UserComment].strip(' \t\r\n\0')
+        else:
+            message = ''
+    except Exception as e:
+        message = "no tag or issue finding tag"
+    return message
