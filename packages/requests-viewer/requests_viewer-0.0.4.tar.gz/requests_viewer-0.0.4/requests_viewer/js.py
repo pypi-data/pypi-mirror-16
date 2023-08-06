bb1bb4b74b75957436fbdfcb613df1c4616a93e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:55 +0000

    Add package requests-viewer/0.0.4/requests_viewer-0.0.4.tar.gz

---
+++ packages/requests-viewer/requests_viewer-0.0.4.tar.gz/requests_viewer-0.0.4/requests_viewer/js.py
@@ -0,0 +1,16 @@
+import json
+import tempfile
+import time
+import webbrowser
+
+
+def wrap_json_into_html(x):
+    return "<html><body><code style='white-space: pre-wrap;'>{}</code></body></html>".format(x)
+
+
+def view_request(r):
+    js = json.dumps(r.json(), indent=4)
+    with tempfile.NamedTemporaryFile("w") as f:
+        f.write(wrap_json_into_html(js))
+        webbrowser.open('file://' + f.name)
+        time.sleep(1)
