b7e7a7d19bda1d4948bc81073b2a597e3de8fbd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:52 +0000

    Add package JobsGe/0.2/JobsGe-0.2.zip

---
+++ packages/JobsGe/JobsGe-0.2.zip/JobsGe-0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='JobsGe',
+      version='0.2',
+      description='Rest api for jobs.ge',
+      url='',
+      author='Goga Patarkatsishvili',
+      author_email='ent1c3d@gmail.com',
+      license='MIT',
+      packages=['jobsge'],
+      zip_safe=False)
