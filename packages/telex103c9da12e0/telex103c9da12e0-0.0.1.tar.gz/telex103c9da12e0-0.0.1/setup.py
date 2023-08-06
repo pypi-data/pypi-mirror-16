04c09bcd1aec3602de2d417762fff8431a46b179
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:31 +0000

    Add package telex103c9da12e0/0.0.1/telex103c9da12e0-0.0.1.tar.gz

---
+++ packages/telex103c9da12e0/telex103c9da12e0-0.0.1.tar.gz/telex103c9da12e0-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='telex103c9da12e0',
+      version='0.0.1',
+      description='DO NOT DOWNLOAD! Test respository to track statistics',
+      url='http://github.com/staeiou/telex103c9da12e0',
+      author='Stuart Geiger',
+      author_email='stuart@stuartgeiger.com',
+      license='MIT',
+      packages=['telex103c9da12e0'],
+      zip_safe=False)
