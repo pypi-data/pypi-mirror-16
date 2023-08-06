d009ef0a7fbd0ce1099d76f6a98e63423d1f6733
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:14:46 +0000

    Add package nester_zizu/1.1.0/nester_zizu-1.1.0.zip

---
+++ packages/nester_zizu/nester_zizu-1.1.0.zip/nester_zizu-1.1.0/setup.py
@@ -0,0 +1,14 @@
+
+
+from distutils.core import setup
+
+setup (
+        name = 'nester_zizu',
+        version = '1.1.0',
+        py_modules = ['nester_zizu'],
+        author = 'zizu9',
+        author_email = 'gudaz90@gmail.com',
+        url = 'http://www.headfirstlab.com',
+        description = 'A simple printer of nested list'
+      )
+      
