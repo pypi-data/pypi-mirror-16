eb1d19430b7811ed66c715037ecb71dcbcc5c09b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:33:52 +0000

    Add package avinit/1.0.1/avinit-1.0.1.tar.gz

---
+++ packages/avinit/avinit-1.0.1.tar.gz/avinit-1.0.1/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='avinit',
+    version='1.0.1',
+    description='Generate avatars using name initials',
+    author='Sergio Oliveira',
+    author_email='seocam@seocam.com',
+    url='https://github.com/CraveFood/avinit',
+    packages=['avinit'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python',
+    ],
+)
