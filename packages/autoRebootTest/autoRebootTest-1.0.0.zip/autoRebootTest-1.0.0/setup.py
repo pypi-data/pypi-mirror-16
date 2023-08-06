8abe437dae470b6848a977deaf66e26bfba54ccc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:37 +0000

    Add package autoRebootTest/1.0.0/autoRebootTest-1.0.0.zip

---
+++ packages/autoRebootTest/autoRebootTest-1.0.0.zip/autoRebootTest-1.0.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'autoRebootTest',
+    version = '1.0.0',
+    description = 'autoReboot test tool for android/yunos',
+    license = 'MIT License',
+    author = 'panluhai',
+    author_email = '19730953@qq.com',
+    
+    packages = find_packages(),
+    platforms = 'any',
+)
