aa6591f6a3c78ea7d1c1d8e8ccf2290401799e74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:33:47 +0000

    Add package pydeeptoy/1.0.0.5/pydeeptoy-1.0.0.5.tar.gz

---
+++ packages/pydeeptoy/pydeeptoy-1.0.0.5.tar.gz/pydeeptoy-1.0.0.5/setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+setup(
+  name = 'pydeeptoy',
+  packages = ['pydeeptoy'], # this must be the same as the name above
+  version = '1.0.0.5',
+  description = 'Python deep learning library for learning purposes',
+  author = 'Kirill Panarin',
+  author_email = 'kirill.panarin@gmail.com',
+  url = 'https://github.com/stormy-ua/DeepLearningToy/tree/master/src/pydeeptoy', # use the URL to the github repo
+  #download_url = 'https://github.com/peterldowns/mypackage/tarball/0.1', # I'll explain this in a second
+  keywords = ['machine learning', 'deep learning', 'neural network'], # arbitrary keywords
+  classifiers = []#, requires=['sklearn']
+)
+
+# Execute the follwoing two commands in order to push the package into the PyPI:
+#
+# python setup.py register -r pypi
+# python setup.py sdist upload -r pypi
