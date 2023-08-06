698606e1e88d0a44f1099e46a56a905523be71b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:51 +0000

    Add package python-myna/1.2.0/python-myna-1.2.0.tar.gz

---
+++ packages/python-myna/python-myna-1.2.0.tar.gz/python-myna-1.2.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "python-myna",
+    version = "1.2.0",
+    packages = find_packages(),
+    author = "SpectoLabs",
+    author_email = "admin@specto.io",
+    url = "https://github.com/SpectoLabs/myna",
+    description = "Using the Myna process virtualisation testing tool from Python unit tests",
+    classifiers = [
+	    'Programming Language :: Python',
+    ]
+ )
