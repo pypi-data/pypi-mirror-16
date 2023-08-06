f024548e417b545ed4fe0c963fa1bfe501a6d1f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:17:24 +0000

    Add package graphql-core/1.0.dev20160823054952/graphql-core-1.0.dev20160823054952.tar.gz

---
+++ packages/graphql-core/graphql-core-1.0.dev20160823054952.tar.gz/graphql-core-1.0.dev20160823054952/graphql/type/directives.py
@@ -0,0 +1,86 @@
+import collections
+
+from ..pyutils.ordereddict import OrderedDict
+from ..utils.assert_valid_name import assert_valid_name
+from .definition import GraphQLArgument, GraphQLNonNull, is_input_type
+from .scalars import GraphQLBoolean
+
+
+class DirectiveLocation(object):
+    QUERY = 'QUERY'
+    MUTATION = 'MUTATION'
+    SUBSCRIPTION = 'SUBSCRIPTION'
+    FIELD = 'FIELD'
+    FRAGMENT_DEFINITION = 'FRAGMENT_DEFINITION'
+    FRAGMENT_SPREAD = 'FRAGMENT_SPREAD'
+    INLINE_FRAGMENT = 'INLINE_FRAGMENT'
+
+    OPERATION_LOCATIONS = [
+        QUERY,
+        MUTATION,
+        SUBSCRIPTION
+    ]
+
+    FRAGMENT_LOCATIONS = [
+        FRAGMENT_DEFINITION,
+        FRAGMENT_SPREAD,
+        INLINE_FRAGMENT
+    ]
+
+    FIELD_LOCATIONS = [
+        FIELD
+    ]
+
+
+class GraphQLDirective(object):
+    __slots__ = 'name', 'args', 'description', 'locations'
+
+    def __init__(self, name, description=None, args=None, locations=None):
+        assert name, 'Directive must be named.'
+        assert_valid_name(name)
+        assert isinstance(locations, collections.Iterable), 'Must provide locations for directive.'
+
+        self.name = name
+        self.description = description
+        self.locations = locations
+
+        if args:
+            assert isinstance(args, collections.Mapping), '{} args must be a dict with argument names as keys.'.format(name)
+            for arg_name, _arg in args.items():
+                assert_valid_name(arg_name)
+                assert is_input_type(_arg.type), '{}({}) argument type must be Input Type but got {}.'.format(
+                    name,
+                    arg_name,
+                    _arg.type)
+        self.args = args or OrderedDict()
+
+
+GraphQLIncludeDirective = GraphQLDirective(
+    name='include',
+    args={
+        'if': GraphQLArgument(
+            type=GraphQLNonNull(GraphQLBoolean),
+            description='Included when true.',
+        ),
+    },
+    locations=[
+        DirectiveLocation.FIELD,
+        DirectiveLocation.FRAGMENT_SPREAD,
+        DirectiveLocation.INLINE_FRAGMENT,
+    ]
+)
+
+GraphQLSkipDirective = GraphQLDirective(
+    name='skip',
+    args={
+        'if': GraphQLArgument(
+            type=GraphQLNonNull(GraphQLBoolean),
+            description='Skipped when true.',
+        ),
+    },
+    locations=[
+        DirectiveLocation.FIELD,
+        DirectiveLocation.FRAGMENT_SPREAD,
+        DirectiveLocation.INLINE_FRAGMENT,
+    ]
+)
