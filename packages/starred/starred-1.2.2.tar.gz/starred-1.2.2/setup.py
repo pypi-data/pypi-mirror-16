70c33fb10ed0c3c2bc3b6060686baf54df6a0fb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:46 +0000

    Add package starred/1.2.2/starred-1.2.2.tar.gz

---
+++ packages/starred/starred-1.2.2.tar.gz/starred-1.2.2/setup.py
@@ -0,0 +1,56 @@
+"""
+Install
+```````
+
+.. code:: bash
+
+  pip install starred
+
+  starred --username maguowei --sort > README.md
+
+Links
+`````
+
+* `website <https://github.com/maguowei/starred>`_
+
+"""
+
+
+from setuptools import setup
+
+setup(
+    name='starred',
+    version='1.2.2',
+    url='https://github.com/maguowei/starred',
+    license='MIT',
+    author='maguowei',
+    author_email='imaguowei@gmail.com',
+    keywords='GitHub starred',
+    description='creating your own Awesome List used GitHub stars!',
+    long_description=__doc__,
+    py_modules=['starred'],
+    platforms='any',
+    install_requires=[
+        'click==6.6',
+        'github3.py==1.0.0a4',
+    ],
+    entry_points={
+        'console_scripts': [
+            'starred=starred:starred'
+        ]
+    },
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
