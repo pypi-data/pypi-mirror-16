e3181f420d566a80886b8bbaddaa35dea8288582
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:00 +0000

    Add package ai_nester/1.4.0/ai_nester-1.4.0.zip

---
+++ packages/ai_nester/ai_nester-1.4.0.zip/ai_nester-1.4.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name='ai_nester',
+	version='1.4.0',
+	py_modules=['ai_nester'],
+	author='1821562166',
+	author_email='453692941@qq.com',
+	url='http://www.headfirstlabs.com',
+	description='A simple printer of nested lists',
+	)
