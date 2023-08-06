a2a2b96a7b4f2f914ad7c7b8addaa24296534fc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:34 +0000

    Add package wallace-platform/0.12.3/wallace-platform-0.12.3.tar.gz

---
+++ packages/wallace-platform/wallace-platform-0.12.3.tar.gz/wallace-platform-0.12.3/setup.py
@@ -0,0 +1,38 @@
+"""Install Wallace as a command line utility."""
+
+from setuptools import setup
+
+setup_args = dict(
+    name='wallace-platform',
+    packages=['wallace'],
+    version="0.12.3",
+    description='Wallace, a platform for experimental cultural evolution',
+    url='http://github.com/berkeley-cocosci/Wallace',
+    author='Berkeley CoCoSci',
+    author_email='wallace@cocosci.berkeley.edu',
+    license='MIT',
+    keywords=['science', 'cultural evolution', 'experiments', 'psychology'],
+    classifiers=[],
+    include_package_data=True,
+    zip_safe=False,
+    entry_points={
+        'console_scripts': [
+            'wallace = wallace.command_line:wallace',
+        ],
+    }
+)
+
+# Read in requirements.txt for dependencies.
+setup_args['install_requires'] = install_requires = []
+setup_args['dependency_links'] = dependency_links = []
+with open('requirements.txt') as f:
+    for line in f.readlines():
+        req = line.strip()
+        if not req or req.startswith('#'):
+            continue
+        if req.startswith('-e '):
+            dependency_links.append(req[3:])
+        else:
+            install_requires.append(req)
+
+setup(**setup_args)
---
+++ packages/wallace-platform/wallace-platform-0.12.3.tar.gz/wallace-platform-0.12.3/wallace/version.py
@@ -0,0 +1,3 @@
+"""Wallace version number."""
+
+__version__ = "0.12.3"
