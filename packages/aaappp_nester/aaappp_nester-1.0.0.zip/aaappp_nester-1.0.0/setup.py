a9c66d04dd59aa41ad12e2fafcb84c5edd09042c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:22:59 +0000

    Add package aaappp_nester/1.0.0/aaappp_nester-1.0.0.zip

---
+++ packages/aaappp_nester/aaappp_nester-1.0.0.zip/aaappp_nester-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name         = 'aaappp_nester',
+    version      = '1.0.0',
+    py_modules   = ['nester'],
+    author       = 'pvdiepen',
+    author_email = 'pvdiepen@gmail.com',
+    url          = 'http://www.aaappp.nl',
+    desciption   = 'A simple printer of nested list',
+    )
