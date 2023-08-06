c475991e968232a0687fe678da333f7cd9498bf1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:50 +0000

    Add package libtree/5.1.0/libtree-5.1.0.tar.gz

---
+++ packages/libtree/libtree-5.1.0.tar.gz/libtree-5.1.0/libtree/utils.py
@@ -0,0 +1,20 @@
+# Copyright (c) 2016 Fabian Kochem
+
+
+import collections
+
+
+def vectorize_nodes(*nodes):
+    if len(nodes) == 1 and isinstance(nodes[0], collections.Iterable):
+        nodes = nodes[0]
+
+    ret = []
+    parents = {node.parent: node for node in nodes}
+
+    last_parent = None
+    for _ in range(len(parents)):
+        node = parents[last_parent]
+        ret.append(node)
+        last_parent = node.id
+
+    return ret
