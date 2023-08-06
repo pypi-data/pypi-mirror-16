f356cb110294d73b65eb05e1d382bc2994d0b002
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:22:07 +0000

    Add package pyhooked/0.8.0/pyhooked-0.8.0.zip

---
+++ packages/pyhooked/pyhooked-0.8.0.zip/pyhooked-0.8.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+long_desc = None
+with open('readme.md') as f:
+    long_desc = f.read()
+
+setup(
+    name='pyhooked',
+    packages=['pyhooked'],
+    version='0.8.0',
+    description='Pure Python hotkey hook, with thanks to pyHook and pyhk',
+    long_description=long_desc,
+    author='Ethan Smith',
+    author_email='mr.smittye@gmail.com',
+    url='https://github.com/ethanhs/hooked',
+    download_url="https://github.com/ethanhs/pyhooked/archive/master.zip",
+    keywords=['hotkey', 'shortcut', 'windows', 'keyboard', 'hooks', 'hook'],
+    classifiers=[],
+)
