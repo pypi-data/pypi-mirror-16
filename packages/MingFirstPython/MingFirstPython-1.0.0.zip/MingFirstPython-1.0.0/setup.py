c87a7383649af5a852041aa2895634948914ccdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:58:09 +0000

    Add package MingFirstPython/1.0.0/MingFirstPython-1.0.0.zip

---
+++ packages/MingFirstPython/MingFirstPython-1.0.0.zip/MingFirstPython-1.0.0/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+           name = "MingFirstPython",
+
+           version = "1.0.0",
+
+           py_modules = ["MingFirstPython"],
+
+           author = "mingming",
+
+           author_email = "67285279@qq.com",
+
+           url = "http://www.67285279@qq.com",
+
+           description = "A simple printer of nested lists",
+           )
