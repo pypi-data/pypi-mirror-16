fbabff843e4e8bfd1cddb2d38a1606bb6c304504
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:19:59 +0000

    Add package osmo/0.2.5/osmo-0.2.5.tar.gz

---
+++ packages/osmo/osmo-0.2.5.tar.gz/osmo-0.2.5/osmo/app/application.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright @ 2016 OPS, YY Inc.
+#
+# Author: Jinlong Yang
+#
+
+import sys
+
+from oslo.config import cfg
+
+from osmo.app.singleton import Singleton
+
+
+class QApplication(Singleton):
+
+    def init_app(self):
+        """before the application is running,
+           make the oslo.config module take effect,
+           otherwise other modules such as log module will invalid.
+
+           Notice: every subclass must be define two static variable:
+                   name and version.
+        """
+        cfg.CONF(sys.argv[1:],
+                 project=self.name,
+                 version=self.version,
+                 default_config_files=None)
+
+    def run(self):
+        raise NotImplementedError('app.run is not implemented!')
+
+    def main(self):
+        """calling process:
+            1. initialize application
+            2. application run
+        """
+        self.init_app()
+        self.run()
+
+    def make_entry_point(self):
+        def wrapper():
+            self.main()
+        return wrapper
