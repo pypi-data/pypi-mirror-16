711e2717a96125c43603effc0a3c572a8d0a8b34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:32 +0000

    Add package funniestcsjoke/0.1/funniestcsjoke-0.1.tar.gz

---
+++ packages/funniestcsjoke/funniestcsjoke-0.1.tar.gz/funniestcsjoke-0.1/funniestcsjoke/__init__.py
@@ -0,0 +1,2 @@
+def joke():
+    return (u'There are 10 types of people: those who understand binary, and those who do not understand it.')
---
+++ packages/funniestcsjoke/funniestcsjoke-0.1.tar.gz/funniestcsjoke-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='funniestcsjoke',
+      version='0.1',
+      description='The funniest CS joke I found',
+      url='http://github.com/fierobert/funniestcsjoke',
+      author='RF',
+      author_email='rf@itsoftwareengineering.com',
+      license='MIT',
+      packages=['funniestcsjoke'],
+      zip_safe=False)
