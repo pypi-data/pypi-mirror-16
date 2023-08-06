155e93db1b982bf8b71c43805c122f3633161fdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:50 +0000

    Add package aasemble/0.1/aasemble-0.1.tar.gz

---
+++ packages/aasemble/aasemble-0.1.tar.gz/aasemble-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='aasemble',
+    version='0.1',
+    author='Soren Hansen',
+    author_email='soren@aasemble.com',
+    install_requires=['aasemble.deployment'],
+)
