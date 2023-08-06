48a82fbc25c2b7f82d7cc0b6ed19832278f7debc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:09 +0000

    Add package bottle-swagger/1.2.0/bottle-swagger-1.2.0.tar.gz

---
+++ packages/bottle-swagger/bottle-swagger-1.2.0.tar.gz/bottle-swagger-1.2.0/README.rst
@@ -0,0 +1,77 @@
+=====================
+Bottle Swagger Plugin
+=====================
+
+About
+-----
+This project is a Bottle plugin for working with Swagger.
+`Bottle <http://bottlepy.org/>`_ is a Python web framework.
+`Swagger (OpenAPI) <http://swagger.io/>`_ is a standard for defining REST APIs.
+
+So if you are serving a REST API with Bottle,
+and you have a defined a Swagger schema for that API,
+this plugin can:
+
+* Validate incoming requests and outgoing responses against the swagger schema
+* Return appropriate error responses on validation failures
+* Serve your swagger schema via Bottle (for use in `Swagger UI <http://swagger.io/swagger-ui/>`_ for example)
+
+Requirements
+------------
+
+* Python >= 2.7
+* Bottle >= 0.12
+* Swagger specification >= 2.0
+
+This project relies on `bravado-core <https://github.com/Yelp/bravado-core>`_ to perform the swagger schema validation,
+so any version of the Swagger spec supported by that project is also supported by this plugin.
+
+Installation
+------------
+::
+
+  $ pip install bottle-swagger
+
+Usage
+-----
+See the "example" directory for a working example of using this plugin.
+
+The simplest usage is::
+
+  import bottle
+
+  swagger_def = _load_swagger_def()
+  bottle.install(SwaggerPlugin(swagger_def))
+
+Where "_load_swagger_def" returns a dict representing your swagger specification
+(loaded from a yaml file, for example).
+
+There are a number of arguments that you can pass to the plugin constructor:
+
+* ``validate_requests`` - Boolean (default ``True``) indicating if incoming requests should be validated or not
+* ``validate_responses`` - Boolean (default ``True``) indicating if outgoing responses should be validated or not
+* ``ignore_undefined_routes`` - Boolean (default ``False``) indicating if undefined routes
+  (that is, routes not defined in the swagger spec) should be passed on ("True") or return a 404 ("False")
+* ``invalid_request_handler`` - Callback called when request validation has failed.
+  Default behaviour is to return a "400 Bad Request" response.
+* ``invalid_response_handler`` - Callback called when response validation has failed.
+  Default behaviour is to return a "500 Server Error" response.
+* ``swagger_op_not_found_handler`` - Callback called when no swagger operation matching the request was found in the swagger schema.
+  Default behaviour is to return a "404 Not Found" response.
+* ``exception_handler=_server_error_handler`` - Callback called when an exception is thrown by downstream handlers (including exceptions thrown by your code).
+  Default behaviour is to return a "500 Server Error" response.
+* ``serve_swagger_schema`` - Boolean (default ``True``) indicating if the Swagger schema JSON should be served
+* ``swagger_schema_url`` - URL (default ``/swagger.json``) on which to serve the Swagger schema JSON
+
+All the callbacks above receive a single parameter representing the ``Exception`` that was raised,
+or in the case of ``swagger_op_not_found_handler`` the ``Route`` that was not found.
+They should all return a Bottle ``Response`` object.
+
+Contributing
+------------
+Development happens in the `bottle-swagger GitHub respository <https://github.com/ampedandwired/bottle-swagger>`_.
+Pull requests (with accompanying unit tests), feature suggestions and bug reports are welcome.
+
+Use "tox" to run the unit tests::
+
+  $ tox
---
+++ packages/bottle-swagger/bottle-swagger-1.2.0.tar.gz/bottle-swagger-1.2.0/bottle_swagger.py
@@ -0,0 +1,144 @@
+import re
+from bottle import request, response, HTTPResponse
+from bravado_core.exception import MatchingResponseNotFound
+from bravado_core.request import IncomingRequest, unmarshal_request
+from bravado_core.response import OutgoingResponse, validate_response, get_response_spec
+from bravado_core.spec import Spec
+from jsonschema import ValidationError
+
+
+def _error_response(status, e):
+    response.status = status
+    return {"code": status, "message": str(e)}
+
+
+def _server_error_handler(e):
+    return _error_response(500, e)
+
+
+def _bad_request_handler(e):
+    return _error_response(400, e)
+
+
+def _not_found_handler(e):
+    return _error_response(404, e)
+
+
+class SwaggerPlugin:
+    DEFAULT_SWAGGER_SCHEMA_URL = '/swagger.json'
+
+    name = 'swagger'
+    api = 2
+
+    def __init__(self, swagger_def,
+                 validate_requests=True,
+                 validate_responses=True,
+                 ignore_undefined_routes=False,
+                 invalid_request_handler=_bad_request_handler,
+                 invalid_response_handler=_server_error_handler,
+                 swagger_op_not_found_handler=_not_found_handler,
+                 exception_handler=_server_error_handler,
+                 serve_swagger_schema=True,
+                 swagger_schema_url=DEFAULT_SWAGGER_SCHEMA_URL):
+        self.swagger = Spec.from_dict(swagger_def)
+        self.validate_requests = validate_requests
+        self.validate_responses = validate_responses
+        self.ignore_undefined_routes = ignore_undefined_routes
+        self.invalid_request_handler = invalid_request_handler
+        self.invalid_response_handler = invalid_response_handler
+        self.swagger_op_not_found_handler = swagger_op_not_found_handler
+        self.exception_handler = exception_handler
+        self.serve_swagger_schema = serve_swagger_schema
+        self.swagger_schema_url = swagger_schema_url
+
+    def apply(self, callback, route):
+        def wrapper(*args, **kwargs):
+            return self._swagger_validate(callback, route, *args, **kwargs)
+
+        return wrapper
+
+    def setup(self, app):
+        if self.serve_swagger_schema:
+            @app.get(self.swagger_schema_url)
+            def swagger_schema():
+                return self.swagger.spec_dict
+
+    def _swagger_validate(self, callback, route, *args, **kwargs):
+        swagger_op = self._swagger_op(route)
+        if not swagger_op:
+            if self.ignore_undefined_routes or self._is_swagger_schema_route(route):
+                return callback(*args, **kwargs)
+            else:
+                return self.swagger_op_not_found_handler(route)
+
+        try:
+            if self.validate_requests:
+                try:
+                    self._validate_request(swagger_op)
+                except ValidationError as e:
+                    return self.invalid_request_handler(e)
+
+            result = callback(*args, **kwargs)
+
+            if self.validate_responses:
+                try:
+                    self._validate_response(swagger_op, result)
+                except (ValidationError, MatchingResponseNotFound) as e:
+                    return self.invalid_response_handler(e)
+
+        except Exception as e:
+            # Bottle handles redirects by raising an HTTPResponse instance
+            if isinstance(e, HTTPResponse):
+                raise e
+
+            return self.exception_handler(e)
+
+        return result
+
+    @staticmethod
+    def _validate_request(swagger_op):
+        unmarshal_request(BottleIncomingRequest(request), swagger_op)
+
+    @staticmethod
+    def _validate_response(swagger_op, result):
+        response_spec = get_response_spec(int(response.status_code), swagger_op)
+        outgoing_response = BottleOutgoingResponse(response, result)
+        validate_response(response_spec, swagger_op, outgoing_response)
+
+    def _swagger_op(self, route):
+        # Convert bottle "<param>" style path params to swagger "{param}" style
+        path = re.sub(r'/<(.+?)>', r'/{\1}', route.rule)
+        return self.swagger.get_op_for_request(request.method, path)
+
+    def _is_swagger_schema_route(self, route):
+        return self.serve_swagger_schema and route.rule == self.swagger_schema_url
+
+
+class BottleIncomingRequest(IncomingRequest):
+    def __init__(self, bottle_request):
+        self.request = bottle_request
+        self.path = bottle_request.url_args
+
+    def json(self):
+        return self.request.json
+
+    @property
+    def query(self):
+        return self.request.query
+
+    @property
+    def headers(self):
+        return self.request.headers
+
+    @property
+    def form(self):
+        return self.request.forms
+
+class BottleOutgoingResponse(OutgoingResponse):
+    def __init__(self, bottle_response, response_json):
+        self.response = bottle_response
+        self.response_json = response_json
+        self.content_type = bottle_response.content_type if bottle_response.content_type else 'application/json'
+
+    def json(self):
+        return self.response_json
