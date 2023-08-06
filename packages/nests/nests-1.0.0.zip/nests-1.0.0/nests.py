6e7de13ccc2d22ca960d645a180a26bdf75ca678
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:13 +0000

    Add package nests/1.0.0/nests-1.0.0.zip

---
+++ packages/nests/nests-1.0.0.zip/nests-1.0.0/nests.py
@@ -0,0 +1,21 @@
+"""This is the standard way to
+   include a multiple-line comment in
+   your code."""
+
+#movies = [ "The Holy Grail", 1975, "Terry Jones & Terry Gilliam", 91, ["Graham Chapman",     ["Michael Palin",[1,2,3], "John Cleese", "Terry Gilliam", "Eric Idle", "Terry Jones"]]]
+
+def print_lol(the_list):
+   """This function takes a positional argument called “the_list",
+   which is any Python list (of, possibly, nested lists).
+   Each data item in the provided list is (recursively)
+   printed to the screen on its own line."""
+ 
+   for each_item in the_list:
+       if isinstance(each_item, list):
+           print_lol(each_item)
+       else:
+           print(each_item)
+
+#print_lol(movies)
+
+
---
+++ packages/nests/nests-1.0.0.zip/nests-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name           = 'nests',
+        version        = '1.0.0',
+        py_modules     = ['nests'],
+        author         = 'SS',
+        author_email  = 'sunnysvet7@gmail.com',
+        url            = 'https://github.com/lanasem',
+        description    = 'A simple printer of nested lists',
+      )  
