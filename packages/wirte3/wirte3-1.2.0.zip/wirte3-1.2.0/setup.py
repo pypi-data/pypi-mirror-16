0358f968fae408160d61fda22e165cf2b22535e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:46 +0000

    Add package wirte3/1.2.0/wirte3-1.2.0.zip

---
+++ packages/wirte3/wirte3-1.2.0.zip/wirte3-1.2.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+		name = 'wirte3',
+		version = '1.2.0',
+		py_modules = ['wirte3'],
+		author = 'Wjq_python',
+		author_email = '614754894@qq.com',
+		url =   'http://user.qzone.qq.com/614754894/main',
+		description =  'A simple printer of nested lists ，model3'
+	 )
