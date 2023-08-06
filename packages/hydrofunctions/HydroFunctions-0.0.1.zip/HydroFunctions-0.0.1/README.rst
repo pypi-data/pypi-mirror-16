175f56b21d0174fb1ed74f34e0546632f0f12563
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:24 +0000

    Add package hydrofunctions/0.0.1/HydroFunctions-0.0.1.zip

---
+++ packages/hydrofunctions/HydroFunctions-0.0.1.zip/HydroFunctions-0.0.1/README.rst
@@ -0,0 +1,55 @@
+==============
+HydroFunctions
+==============
+
+*A suite of convenience functions for working with hydrology data in an
+interactive Python session.*
+
+.. image:: https://travis-ci.org/mroberge/hf.png
+    :target: https://travis-ci.org/mroberge/hf
+    :alt: Build status
+
+.. image:: https://img.shields.io/pypi/dm/hydrofunctions.svg
+    :target: https://pypi.python.org/pypi/hydrofunctions
+    :alt: PyPI downloads
+
+.. image:: https://img.shields.io/pypi/v/hydrofunctions.svg
+    :target: https://pypi.python.org/pypi/hydrofunctions
+    :alt: Version
+
+.. image:: https://img.shields.io/pypi/l/hydrofunctions.svg
+    :target: https://pypi.python.org/pypi/hydrofunctions
+    :alt: License
+
+.. image:: https://img.shields.io/pypi/pyversions/hydrofunctions.svg
+    :target: https://pypi.python.org/pypi/hydrofunctions
+    :alt: Support Python versions
+
+.. image:: https://img.shields.io/pypi/status/hydrofunctions.svg
+    :target: https://pypi.python.org/pypi/hydrofunctions
+    :alt: project status
+
+----
+
+Install
+-------
+
+install hydrofunctions using pip from the command line:
+
+  code-block:: python
+
+  pip install hydrofunctions
+
+
+Usage
+-----
+
+then, import it into your own code:
+
+.. code-block:: python
+
+  import numpy as np
+  import pandas as pd
+  import hydrofunctions as hf
+
+this makes it easy to
---
+++ packages/hydrofunctions/HydroFunctions-0.0.1.zip/HydroFunctions-0.0.1/setup.py
@@ -0,0 +1,46 @@
+"""
+    a simple version of the setup() command from:
+    https://setuptools.readthedocs.io/en/latest/setuptools.html
+"""
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+# Get the long description from the README file
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    # name is the handle you use to import this package.
+    name="HydroFunctions",
+    version="0.0.1",
+    description='convenience functions for hydrology',
+    long_description=long_description,
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.4'
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Topic :: Scientific/Engineering :: Visualization',
+        'Topic :: Utilities'
+    ],
+    keywords='hydrology USGS NWIS stream gage water',
+    url='https://github.com/mroberge/hf',
+    author='Martin Roberge',
+    author_email='mroberge.whois@gmail.com',
+    license='MIT',
+    # packages=['hydrofunctions'],
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'pandas'
+    ],
+    test_suite='nose2.collector.collector',
+    # I guess you don't need to install unittest since it is part of the main
+    # tests_require=['unittest'],
+    include_package_data=True,
+    zip_safe=False,
+)
