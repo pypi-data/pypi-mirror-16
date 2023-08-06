10b7e6e1ee4cf88dd9c105d13668be6657d94db6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:36 +0000

    Add package Jane2016/1.0.0/Jane2016-1.0.0.zip

---
+++ packages/Jane2016/Jane2016-1.0.0.zip/Jane2016-1.0.0/setup.py
@@ -0,0 +1,11 @@
+﻿from distutils.core import setup
+
+setup(
+	name = 'Jane2016',
+	version ='1.0.0',
+	py_modules = ['nester'],
+	author = 'hfpython',
+	author_email = '273638301@qq.com',
+	url = 'http://www.headfirstlabs.com',
+	description = 'A simple printer of nested lists',
+)
