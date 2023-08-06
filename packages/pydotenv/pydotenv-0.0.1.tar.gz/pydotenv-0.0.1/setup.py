49a1672d402bc78b5a7940674c5fb6fed5a37064
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:31 +0000

    Add package pydotenv/0.0.1/pydotenv-0.0.1.tar.gz

---
+++ packages/pydotenv/pydotenv-0.0.1.tar.gz/pydotenv-0.0.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name="pydotenv",
+    version=__import__('pydotenv').__version__,
+    author="Alexandre Varas",
+    author_email="alej0varas@gmail.com",
+    py_modules=['pydotenv', ],
+    include_package_data=True,
+    license='GNU Library or Lesser General Public License (LGPL)',
+    description="A libray to write .env files",
+    url='https://github.com/alej0varas/pydotenv',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development :: Libraries',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+    ],
+)
