7509065f1622944c29413736a0a544effaf13381
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:23 +0000

    Add package scalr-ctl/1.0.52/scalr-ctl-1.0.52.tar.gz

---
+++ packages/scalr-ctl/scalr-ctl-1.0.52.tar.gz/scalr-ctl-1.0.52/scalrctl/commands/image.py
@@ -0,0 +1,24 @@
+__author__ = 'Dmitriy Korsakov'
+__doc__ = 'Image management'
+
+from scalrctl import commands
+from scalrctl import click
+
+
+class ChangeImageAttrs(commands.Action):
+    mutable_body_parts = ["name"]
+    prompt_for = ["imageId"]
+
+
+class RegisterImage(commands.Action):
+
+    def pre(self, *args, **kwargs):
+        #XXX: this code does not work yet!
+        if 'imageId' not in kwargs:
+            kwargs["image"] = click.termui.prompt("Image object JSON")
+
+        return super(RegisterImage, self).pre(*args, **kwargs)
+
+
+class RetrieveImage(commands.Action):
+    prompt_for = ["imageId"]
