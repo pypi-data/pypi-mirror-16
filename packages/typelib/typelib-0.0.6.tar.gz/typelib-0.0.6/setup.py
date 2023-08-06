f3b87d623e50771f2a32445b611c03a67d3efb4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:25 +0000

    Add package typelib/0.0.6/typelib-0.0.6.tar.gz

---
+++ packages/typelib/typelib-0.0.6.tar.gz/typelib-0.0.6/setup.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+
+import os
+import sys
+from distutils.sysconfig import get_python_lib
+
+from setuptools import find_packages, setup
+
+# Warn if we are installing over top of an existing installation. 
+overlay_warning = False
+if "install" in sys.argv:
+    lib_paths = [get_python_lib()]
+    if lib_paths[0].startswith("/usr/lib/"):
+        # We have to try also with an explicit prefix of /usr/local in order to
+        # catch Debian's custom user site-packages directory.
+        lib_paths.append(get_python_lib(prefix="/usr/local"))
+    for lib_path in lib_paths:
+        existing_path = os.path.abspath(os.path.join(lib_path, "typelib"))
+        if os.path.exists(existing_path):
+            # We note the need for the warning here, but present it after the
+            # command is run, so it's more likely to be seen.
+            overlay_warning = True
+            break
+
+
+# Any packages inside the typelib source folder we dont want in the packages
+EXCLUDE_FROM_PACKAGES = [ ]
+
+# Dynamically calculate the version based on typelib.VERSION.
+version = __import__('typelib').get_version()
+
+setup(
+    name='typelib',
+    version=version,
+    url='https://github.com/panyam/typelib',
+    author='Sriram Panyam',
+    author_email='sri.panyam@gmail.com',
+    description=("Utilities and library to model types and type systems"),
+    license='BSD',
+    packages=find_packages(exclude=EXCLUDE_FROM_PACKAGES),
+    include_package_data=True,
+    scripts=['bin/clean_install.sh', 'bin/package.sh'],
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Internet :: WWW/HTTP :: WSGI',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Software Development :: Code Generators',
+        'Topic :: Software Development :: Compilers',
+    ],
+)
+
+
+if overlay_warning:
+    sys.stderr.write("""
+
+========
+WARNING!
+========
+
+You have just installed Typelang over top of an existing
+installation, without removing it first. Because of this,
+your install may now include extraneous files from a
+previous version that have since been removed from
+Typelang . This is known to cause a variety of problems. You
+should manually remove the
+
+%(existing_path)s
+
+directory and re-install Typelang.
+
+""" % {"existing_path": existing_path})
