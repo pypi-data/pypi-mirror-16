98406f6a4e16d379b4443de788628810bc6e2fda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:57:21 +0000

    Add package go_vncdriver/0.0.6/go_vncdriver-0.0.6.tar.gz

---
+++ packages/go_vncdriver/go_vncdriver-0.0.6.tar.gz/go_vncdriver-0.0.6/build.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# Builds against your current Python version. You must have numpy installed.
+
+set -ex
+
+cd "$(dirname "$0")"
+
+mkdir -p .build/src/github.com/openai/gym-vnc
+ln -s ../../../../.. .build/src/github.com/openai/gym-vnc/go-vncdriver || :
+# trap 'rm -rf .build' EXIT
+
+export GOPATH="$(pwd)/.build"
+export CGO_CFLAGS=$(
+    python -c "import numpy, sysconfig
+print('-I{} -I{}\n'.format(numpy.get_include(), sysconfig.get_config_var('INCLUDEPY')))"
+)
+export CGO_LDFLAGS=$(python -c "import re, sysconfig;
+library = sysconfig.get_config_var('LIBRARY')
+match = re.search('^lib(.*)\.a', library)
+if match is None:
+  raise RuntimeError('Could not parse LIBRARY: {}'.format(library))
+print('-L{} -l{}\n'.format(sysconfig.get_config_var('LIBDIR'), match.group(1)))
+")
+
+cd "$(pwd)/.build/src/github.com/openai/gym-vnc/go-vncdriver"
+godep go build -buildmode=c-shared -o go_vncdriver.so
---
+++ packages/go_vncdriver/go_vncdriver-0.0.6.tar.gz/go_vncdriver-0.0.6/setup.py
@@ -0,0 +1,27 @@
+import os
+import subprocess
+import sys
+
+from distutils.command.build import build as DistutilsBuild
+from setuptools import setup
+
+class Build(DistutilsBuild):
+    def run(self):
+        print('testing')
+        cmd = ['make', 'build', '-C', 'go_vncdriver']
+        try:
+            subprocess.check_call(cmd, cwd=os.path.join(os.path.dirname(__file__)))
+        except subprocess.CalledProcessError as e:
+            sys.stderr.write("Could not build go_vncdriver: %s\n" % e)
+            raise
+        except OSError as e:
+            sys.stderr.write("Unable to execute '{}'. HINT: are you sure `make` is installed?\n".format(' '.join(cmd)))
+            raise
+        DistutilsBuild.run(self)
+
+setup(name='go_vncdriver',
+      version='0.0.6',
+      cmdclass={'build': Build},
+      packages=['go_vncdriver'],
+      package_dir={'go_vncdriver': '.'},
+)
