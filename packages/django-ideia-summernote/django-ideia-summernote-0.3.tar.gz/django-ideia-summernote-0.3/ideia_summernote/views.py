ff5122d69a9aa75e790fe7602a2a34f0e7dc4188
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:04 +0000

    Add package django-ideia-summernote/0.3/django-ideia-summernote-0.3.tar.gz

---
+++ packages/django-ideia-summernote/django-ideia-summernote-0.3.tar.gz/django-ideia-summernote-0.3/ideia_summernote/views.py
@@ -0,0 +1,54 @@
+from django.http import JsonResponse
+from django.views.generic import View
+from django.core.files.storage import default_storage
+from django.conf import settings
+import os
+from default import SUMMERNOTE_DEFAULT_CONFIG
+from ideia_summernote.local_exceptions import FileSizeNotSupported
+from django.utils.translation import ugettext as _
+
+SUMMERNOTE_SETTINGS =  getattr(settings, 'SUMMERNOTE_CONFIG', SUMMERNOTE_DEFAULT_CONFIG)
+MAXIMUM_SIZE = getattr(SUMMERNOTE_SETTINGS, 'maximum_image_upload', SUMMERNOTE_DEFAULT_CONFIG['maximum_image_upload'])
+
+
+def check_sizes(files):
+
+    if files:
+        for file in files.values():
+            if file.size > MAXIMUM_SIZE:
+                raise FileSizeNotSupported()
+
+
+class Upload(View):
+
+    def post(self, request):
+
+        files = request.FILES
+        urls = []
+
+        is_user_authenticated = request.user.is_authenticated()
+        if SUMMERNOTE_SETTINGS['restrict_to_user']:
+            if not is_user_authenticated:
+                return JsonResponse(data={'urls': urls})
+
+
+
+        if request.FILES:
+
+            try:
+                check_sizes(request.FILES)
+            except FileSizeNotSupported:
+                return JsonResponse(data={'message': _('File size not supported!')}, status=400)
+
+            for file in files.values():
+                user_path=''
+
+                if SUMMERNOTE_SETTINGS['use_path_user'] and is_user_authenticated:
+                    user_path = request.user.username
+
+                path = os.path.join(settings.MEDIA_ROOT, user_path, file.name)
+                path = default_storage.save(path, file)
+                if path:
+                    urls.append(os.path.join(settings.MEDIA_URL, user_path, file.name))
+
+        return JsonResponse(data={'urls': urls})
