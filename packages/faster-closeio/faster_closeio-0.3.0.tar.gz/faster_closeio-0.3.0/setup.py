c77603c02360a7abff7169ed28dd84f31866931f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:00 +0000

    Add package faster-closeio/0.3.0/faster_closeio-0.3.0.tar.gz

---
+++ packages/faster-closeio/faster_closeio-0.3.0.tar.gz/faster_closeio-0.3.0/setup.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+readme = open('README.rst').read()
+history = open('HISTORY.rst').read().replace('.. :changelog:', '')
+
+
+setup(
+    name='faster_closeio',
+    version='0.3.0',
+    description='Slim API wrapper to access close.io CRM',
+    long_description=readme + '\n\n' + history,
+    author='Denis Cornehl',
+    author_email='denis.cornehl@gmail.com',
+    download_url='https://github.com/Thermondo/closeio',
+    url='https://github.com/fasterweb/closeio',
+    packages=[
+        'closeio',
+        'closeio.contrib',
+        'closeio.contrib.django',
+    ],
+    package_dir={'closeio': 'closeio'},
+    include_package_data=True,
+    install_requires=[
+        'requests',
+        'slumber',
+        'python-dateutil',
+        'six',
+    ],
+    license="BSD",
+    zip_safe=False,
+    keywords='closeio',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    entry_points={'pytest11': ['closeio = closeio.contrib.pytest_plugin']},
+)
