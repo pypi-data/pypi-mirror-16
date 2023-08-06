ff0808b7edb3cbaf83bad5c8448649cff147280d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:23:24 +0000

    Add package shellish/2.3/shellish-2.3.tar.gz

---
+++ packages/shellish/shellish-2.3.tar.gz/shellish-2.3/shellish/__init__.py
@@ -0,0 +1,7 @@
+from .completer import *
+from .command import *
+from .layout import *
+from .data import *
+from .eventing import *
+from .session import *
+from .paging import *
---
+++ packages/shellish/shellish-2.3.tar.gz/shellish-2.3/shellish/layout/__init__.py
@@ -0,0 +1,6 @@
+from .vtml import *
+from .html import *
+from .column import *
+from .tree import *
+from .table import *
+from .progress import *
