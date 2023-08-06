f05b6158278c388571b9d53c55a671cef5a2c4bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:12 +0000

    Add package torsession/0.2.0/torsession-0.2.0.tar.gz

---
+++ packages/torsession/torsession-0.2.0.tar.gz/torsession-0.2.0/setup.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+import torsession
+
+classifiers = """
+Intended Audience :: Developers
+License :: OSI Approved :: Apache Software License
+Development Status :: 5 - Production/Stable
+Natural Language :: English
+Programming Language :: Python :: 2
+Programming Language :: Python :: 2.6
+Programming Language :: Python :: 2.7
+Programming Language :: Python :: 3
+Programming Language :: Python :: 3.0
+Programming Language :: Python :: 3.3
+Operating System :: MacOS :: MacOS X
+Operating System :: Unix
+Programming Language :: Python
+Programming Language :: Python :: Implementation :: CPython
+"""
+
+version = torsession.__version__
+description = 'An asynchronous session backend with mongodb for tornado'
+long_description = open("README.rst").read()
+packages = ['torsession']
+
+setup(
+    name='torsession',
+    version=version,
+    packages=packages,
+    description=description,
+    long_description=long_description,
+    author='Lime YH.Shi',
+    author_email='shiyanhui66@gmail.com',
+    url='https://github.com/shiyanhui/torsession',
+    license='http://www.apache.org/licenses/LICENSE-2.0',
+    classifiers=filter(None, classifiers.split('\n')),
+    keywords=[
+        'torsession', 'mongo', 'mongodb', 'motor', 'session', 'backend',
+        'tornado', 'asynchronous'
+    ],
+    install_requires=[
+        "motor >= 0.6.3.dev0",
+        "tornado >= 3.0"
+    ],
+    zip_safe=False,
+)
