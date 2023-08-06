3ac44901983b879d561c95b7a8e0a0c4d4de64ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:58 +0000

    Add package lwj_nester/1.0.0/lwj_nester-1.0.0.zip

---
+++ packages/lwj_nester/lwj_nester-1.0.0.zip/lwj_nester-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'lwj_nester',
+    version = '1.0.0',
+    py_modules = ['lwj_nester'],
+    author = 'li',
+    author_email = 'li@qq.com',
+    url = 'http://www.headfirstlabs.com',
+    description = 'A simple printer of nested lists',
+    )
