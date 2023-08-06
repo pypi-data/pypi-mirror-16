df66ea8b2f1d2faebaee7d6f76bd9bb6edc0f3e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:17:23 +0000

    Add package vk-requests/0.9.6/vk-requests-0.9.6.tar.gz

---
+++ packages/vk-requests/vk-requests-0.9.6.tar.gz/vk-requests-0.9.6/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name='vk-requests',
+    version='0.9.6',
+    packages=['vk_requests'],
+    url='https://github.com/prawn-cake/vk-requests',
+    license='MIT',
+    author='Maksim Ekimovskii',
+    author_email='ekimovsky.maksim@gmail.com',
+    description='vk.com requests for humans. API library for vk.com',
+    install_requires=[
+        'six',
+        'requests>=2.8.1',
+        'beautifulsoup4>=4.4.1'
+    ],
+    classifiers=[
+        'Intended Audience :: Developers',
+
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+    ]
+)
