a867ab7e8ba4a6598ef2df6ef35ac199f997f2e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:51 +0000

    Add package invenio-migrator/1.0.0a7/invenio-migrator-1.0.0a7.tar.gz

---
+++ packages/invenio-migrator/invenio-migrator-1.0.0a7.tar.gz/invenio-migrator-1.0.0a7/RELEASE-NOTES.rst
@@ -0,0 +1,36 @@
+===========================
+ Invenio-Migrator v1.0.0a7
+===========================
+
+Invenio-Migrator v1.0.0a7 was released on August 5, 2016.
+
+About
+-----
+
+Utilities for migrating past Invenio versions to Invenio 3.0.
+
+*This is an experimental developer preview release.*
+
+What's new
+----------
+
+- Initial public release.
+
+Installation
+------------
+
+   $ pip install invenio-migrator==1.0.0a7
+
+Documentation
+-------------
+
+   http://pythonhosted.org/invenio-migrator/
+
+Happy hacking and thanks for flying Invenio-Migrator.
+
+| Invenio Development Team
+|   Email: info@inveniosoftware.org
+|   IRC: #invenio on irc.freenode.net
+|   Twitter: http://twitter.com/inveniosoftware
+|   GitHub: https://github.com/inveniosoftware/invenio-migrator
+|   URL: http://inveniosoftware.org
---
+++ packages/invenio-migrator/invenio-migrator-1.0.0a7.tar.gz/invenio-migrator-1.0.0a7/invenio_migrator/tasks/users.py
@@ -0,0 +1,101 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of Invenio.
+# Copyright (C) 2016 CERN.
+#
+# Invenio is free software; you can redistribute it
+# and/or modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of the
+# License, or (at your option) any later version.
+#
+# Invenio is distributed in the hope that it will be
+# useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Invenio; if not, write to the
+# Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+# MA 02111-1307, USA.
+#
+# In applying this license, CERN does not
+# waive the privileges and immunities granted to it by virtue of its status
+# as an Intergovernmental Organization or submit itself to any jurisdiction.
+
+"""Celery task for records migration."""
+
+from __future__ import absolute_import, print_function
+
+import uuid
+from datetime import datetime
+
+import arrow
+from celery import shared_task
+from flask import current_app
+from invenio_db import db
+
+
+@shared_task()
+def load_user(data):
+    """Load user from data dump.
+
+    :param data: Dictionary containing user data.
+    :type data: dict
+    """
+    from invenio_accounts.models import User
+    from invenio_userprofiles.api import UserProfile
+    email = data['email']
+    if User.query.filter_by(email=data['email']).count() > 0:
+        email = 'DUPLICATE_{}'.format(data['email'])
+
+    last_login = None
+    if data['last_login']:
+        last_login = arrow.get(data['last_login']).datetime
+
+    confirmed_at = None
+    if data['note'] == '1':
+        confirmed_at = datetime.utcnow()
+
+    salt = data['password_salt']
+    checksum = data['password']
+    if not checksum:
+        new_password = None
+    # Test if password hash is in Modular Crypt Format
+    elif checksum.startswith('$'):
+        new_password = checksum
+    else:
+        new_password = str.join('$', ['', u'invenio-aes', salt, checksum])
+    with db.session.begin_nested():
+        obj = User(
+            id=data['id'],
+            password=new_password,
+            email=email,
+            confirmed_at=confirmed_at,
+            last_login_at=last_login,
+            active=(data['note'] != '0'),
+        )
+        db.session.add(obj)
+
+    nickname = data['nickname'].strip()
+    if nickname:
+        full_name = (data.get('given_names', '') + u' ' +
+                     data.get('family_name', '')).strip()
+
+        if UserProfile.query.filter(
+                UserProfile._username == nickname.lower()).count() > 0:
+            nickname = 'DUPLICATE_{0}_{1}'.format(str(uuid.uuid4()), nickname)
+
+        p = UserProfile(user=obj)
+        p.full_name = full_name or ''
+        try:
+            p.username = nickname
+        except ValueError:
+            current_app.logger.warn(
+                u'Invalid username {0} for user_id {1}'.format(
+                    nickname, data['id']))
+            p._username = nickname.lower()
+            p._displayname = nickname
+
+        db.session.add(p)
+
+    db.session.commit()
