5c797ff8fee4cb396c465ffd865125ef4fdf4695
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:45 +0000

    Add package numcube/0.1.6/numcube-0.1.6.zip

---
+++ packages/numcube/numcube-0.1.6.zip/numcube-0.1.6/numcube/__init__.py
@@ -0,0 +1,7 @@
+from numcube.version import __version__
+from numcube.axis import Axis
+from numcube.cube import Cube, concatenate, stack, apply_op
+from numcube.index import Index
+from numcube.functions import apply, reduce
+from numcube.experimental.table import Table
+from numcube.exceptions import *
