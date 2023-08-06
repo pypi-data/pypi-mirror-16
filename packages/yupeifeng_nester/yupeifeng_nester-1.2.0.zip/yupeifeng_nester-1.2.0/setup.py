b59480ae14e88af613c806ebc9a1e03568f58613
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:49 +0000

    Add package yupeifeng_nester/1.2.0/yupeifeng_nester-1.2.0.zip

---
+++ packages/yupeifeng_nester/yupeifeng_nester-1.2.0.zip/yupeifeng_nester-1.2.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+		name			="yupeifeng_nester",
+		version			="1.2.0",
+		py_modules		=["yupeifeng_nester"],
+		author			="hfpython",
+		author_email	="2417187213@qq.com",
+		url				="http://www.headfirstlabs.com",
+		description		="A simple printer of nested lists",	
+	)
