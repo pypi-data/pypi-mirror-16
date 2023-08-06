d63c0d4b1125aa260dafcfe9b5c19c2cabe1c4bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:53 +0000

    Add package RanCat/1.0.0/RanCat-1.0.0.tar.gz

---
+++ packages/RanCat/RanCat-1.0.0.tar.gz/RanCat-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='RanCat',
+    version='1.0.0',
+    description='A random string generator',
+    author='Matthew Egan',
+    url='https://github.com/mattjegan/rancat',
+    packages=['rancat']
+)
