206f78cb495088eb04d587b04bd99c7cb81403a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:05 +0000

    Add package named_decorator/0.1.4/named_decorator-0.1.4.tar.gz

---
+++ packages/named_decorator/named_decorator-0.1.4.tar.gz/named_decorator-0.1.4/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+setup(
+    name='named_decorator',
+    description="""
+    Utility to name wrappers based on their callees, dynamically. This makes it
+    easy to trace calls in large codebases with heavily used decorators.
+    """,
+    url='https://github.com/Yelp/named_decorator',
+    version='0.1.4',
+    author='Yelp Performance Team',
+    author_email='no-reply+use_github_issues@yelp.com',
+    platforms='all',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+    install_requires=['six'],
+    py_modules=['named_decorator', 'five'],
+    options={
+        'bdist_wheel': {
+            'universal': 1,
+        }
+    },
+)
