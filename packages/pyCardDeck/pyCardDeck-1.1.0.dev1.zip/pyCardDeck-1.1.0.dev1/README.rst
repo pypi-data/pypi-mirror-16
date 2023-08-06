decb658f5deee71e8d79fc06fa73dbba9f3be11f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:33:38 +0000

    Add package pyCardDeck/1.1.0.dev1/pyCardDeck-1.1.0.dev1.zip

---
+++ packages/pyCardDeck/pyCardDeck-1.1.0.dev1.zip/pyCardDeck-1.1.0.dev1/README.rst
@@ -0,0 +1,26 @@
+pyCardDeck
+==========
+
+.. image:: https://readthedocs.org/projects/pycarddeck/badge/?version=latest
+    :target: http://pycarddeck.readthedocs.io/en/latest/?badge=latest
+    :alt: Documentation Status
+
+.. image:: https://img.shields.io/github/license/mashape/apistatus.svg?maxAge=2592000
+    :target: https://opensource.org/licenses/MIT
+    :alt: MIT License
+
+Library aimed at anyone who wants to do any kind of deck manipulation in python.
+So probably game developers. The goal is to have the ultimate library for all of this,
+supporting all kinds of game types with clean and beautiful API - kind of like requests :)
+
+For developers
+--------------
+
+The library will support only Python 3 and up, with py.test as the only required library.
+Always aim to write clean and readable code, make sure your tests are passing, document in docstrings (rst format)
+and when writing new modules, classes or functions, add them to docs (we are using Shpinx autodocs)
+
+TODO
+~~~~
+
+* Write examples
---
+++ packages/pyCardDeck/pyCardDeck-1.1.0.dev1.zip/pyCardDeck-1.1.0.dev1/setup.py
@@ -0,0 +1,63 @@
+from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+from codecs import open
+import sys
+
+class PyTest(TestCommand):
+    user_options = [('pytest-args', 'a', 'Arguments to pass into py.test')]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+
+
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
+
+
+test_requirements = ['pytest>=2.8.0', 'pytest-cov']
+
+
+with open('README.rst', encoding='utf-8') as f:
+    long_description = f.read()
+
+
+setup(name='pyCardDeck',
+      version='1.1.0.dev1',
+      description='Logic for decks with cards',
+      long_description=long_description,
+      url='https://www.djetelina.cz/project/pycarddeck/',
+      author='David Jetelina',
+      author_email='david@djetelina.cz',
+      license='MIT',
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Intended Audience :: Developers',
+          'Topic :: Software Development :: Libraries',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+          'Topic :: Utilities',
+          'License :: OSI Approved :: MIT License',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 3.0',
+          'Programming Language :: Python :: 3.1',
+          'Programming Language :: Python :: 3.2',
+          'Programming Language :: Python :: 3.3',
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5'
+      ],
+      keywords='cards deck card game shuffle draw discard',
+      packages=find_packages(exclude=['tests', 'docs']),
+      install_requires=[],
+      cmdclass={'test': PyTest},
+      tests_require=test_requirements
+      )
