42b689ad59241a347669276556edb54c607994f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:36 +0000

    Add package pymatgen/4.2.1/pymatgen-4.2.1-cp35-cp35m-macosx_10_11_x86_64.whl

---
+++ packages/pymatgen/pymatgen-4.2.1-cp35-cp35m-macosx_10_11_x86_64.whl/pymatgen/phasediagram/pdmaker.py
@@ -0,0 +1,4 @@
+import warnings
+warnings.warn("pymatgen.phasediagram.pdmaker has been moved pymatgen.phasediagram.maker. This stub "
+              "will be removed in pymatgen 4.0.", DeprecationWarning)
+from .maker import *
