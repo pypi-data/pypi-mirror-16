5c0e011d01497b637e58af9ca10bf9800fdd5c9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:03:37 +0000

    Add package pr_module/1.1.3/pr_module-1.1.3.tar.gz

---
+++ packages/pr_module/pr_module-1.1.3.tar.gz/pr_module-1.1.3/pr_module.py
@@ -0,0 +1,11 @@
+def printsomething(the_list, level=0): 
+	"""
+	prints the input list for you
+	"""
+	for each_item in the_list:
+		if isinstance(each_item, list):
+			printsomething(each_item, level+1) 
+		else:
+			for tab_stop in range(level): 
+				print("\t", end='')
+			print(each_item)
---
+++ packages/pr_module/pr_module-1.1.3.tar.gz/pr_module-1.1.3/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+	name		= 'pr_module' ,
+	version		= '1.1.3' ,
+	py_modules	= ['pr_module'],
+	author		= 'fatemeh_s' ,
+	author_email= 'fatemeh.s9594@gmail.com',
+	url 		= 'http://www.headfirstlabs.com', 
+	description = 'A simple printer of a list',
+)
