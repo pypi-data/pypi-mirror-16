bec64afe55c13eddbf7f3e3523f5597207ff43df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:04 +0000

    Add package mistool/1.0.0b0/mistool-1.0.0b0-py3-none-any.whl

---
+++ packages/mistool/mistool-1.0.0b0-py3-none-any.whl/mistool/config/frame.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+# Note: the following ugly dictionary was automatically built.
+
+ALL_FRAMES = {}
+ALL_FRAMES['ascii_star'] = {'down': '*', 'upleft': '**', 'downright': '**', 'downleft': '**', 'right': ' *', 'left': '* ', 'up': '*', 'upright': '**'}
+ALL_FRAMES['c_basic'] = {'down': '*', 'upleft': '/**', 'downright': '**/', 'downleft': ' **', 'right': ' *', 'left': ' * ', 'up': '*', 'upright': '**'}
+ALL_FRAMES['c_pretty'] = {'down': '-', 'upleft': '/* ---', 'downright': '--- */', 'downleft': ' * ---', 'right': ' -- *', 'left': ' * -- ', 'up': '-', 'upright': '--- *'}
+ALL_FRAMES['pyba_title_1'] = {'down': '=', 'upleft': '', 'downright': '', 'downleft': '', 'right': '', 'left': '', 'up': '=', 'upright': ''}
+ALL_FRAMES['pyba_title_2'] = {'down': '-', 'upleft': '', 'downright': '', 'downleft': '', 'right': '', 'left': '', 'up': '-', 'upright': ''}
+ALL_FRAMES['pyba_title_3'] = {'down': '*', 'upleft': '', 'downright': '', 'downleft': '', 'right': '', 'left': '', 'up': '*', 'upright': ''}
+ALL_FRAMES['pyba_title_4'] = {'down': ':', 'upleft': '', 'downright': '', 'downleft': '', 'right': '', 'left': '', 'up': ':', 'upright': ''}
+ALL_FRAMES['python_basic'] = {'down': '#', 'upleft': '##', 'downright': '##', 'downleft': '##', 'right': ' #', 'left': '# ', 'up': '#', 'upright': '##'}
+ALL_FRAMES['python_pretty'] = {'down': '-', 'upleft': '# ---', 'downright': '--- #', 'downleft': '# ---', 'right': ' -- #', 'left': '# -- ', 'up': '-', 'upright': '--- #'}
