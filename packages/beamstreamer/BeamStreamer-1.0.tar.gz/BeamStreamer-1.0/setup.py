a3733ca920d91c0e341e16dbe9243e10dbd56dbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:36 +0000

    Add package beamstreamer/1.0/BeamStreamer-1.0.tar.gz

---
+++ packages/beamstreamer/BeamStreamer-1.0.tar.gz/BeamStreamer-1.0/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(name="BeamStreamer",
+        version="1.0",
+        description="Watch Beam streams in VLC!",
+        author="Innectic",
+        author_email="no@notforyou.whatever",
+        url="https://www.github.com/innectic/beam-streamer",
+        packages=["beamstreamer"],
+        scripts=["bstream"])
