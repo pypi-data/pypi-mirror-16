2bb03d4636d21d01a887a8f2ea7f94615c14afde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:53 +0000

    Add package pydipankar/1.0/pydipankar-1.0.tar.gz

---
+++ packages/pydipankar/pydipankar-1.0.tar.gz/pydipankar-1.0/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '1.0'
+
+setup(name='pydipankar',
+      version=version,
+      description="Common lib used by Dipoankar",
+      long_description="""\
+Common lib used by dipankar""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='dipankar spider common lib experimenet',
+      author='dipankar',
+      author_email='ddutta@microsoft.com',
+      url='',
+      license='',
+      #version='1.0',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-i
+          'requests',
+          'fake_useragent',
+          'beautifulsoup4'
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
