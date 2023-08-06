f2d38c0ea45c50c0eacbd37373d17b0dabcf2307
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:56 +0000

    Add package blind_magic/0.1.2/blind_magic-0.1.2.zip

---
+++ packages/blind_magic/blind_magic-0.1.2.zip/blind_magic-0.1.2/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'blind_magic',
+  packages = ['blind_magic'], # this must be the same as the name above
+  version = '0.1.2',
+  description = 'blind sql injection manually',
+  author = 'TRAN NHAT TRUONG',
+  author_email = 'truongtn0806@gmail.com',
+  url = 'https://github.com/truongtn/blind_magic', # use the URL to the github repo
+  download_url = '', # I'll explain this in a second
+  keywords = ['sql injection', 'blind sql injection', 'manual'], # arbitrary keywords
+  classifiers = [],
+)
