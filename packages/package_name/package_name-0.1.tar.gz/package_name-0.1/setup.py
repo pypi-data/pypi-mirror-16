429c279ae99ee29b97728838829bc0aec67162fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:15 +0000

    Add package package_name/0.1/package_name-0.1.tar.gz

---
+++ packages/package_name/package_name-0.1.tar.gz/package_name-0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+
+setup(
+    name='package_name',
+    version='0.1',
+    description='Package Description',
+    url='https://github.com/yourself/package_name',
+    # dependencies
+    install_requires=[
+    ],
+    # python modules
+    packages=[
+        'package_name',
+    ],
+)
+
