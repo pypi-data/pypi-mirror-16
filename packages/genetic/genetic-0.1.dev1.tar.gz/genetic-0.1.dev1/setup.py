4626764747a8c7edc65d0d4f45138ea4267a0955
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:28 +0000

    Add package genetic/0.1.dev1/genetic-0.1.dev1.tar.gz

---
+++ packages/genetic/genetic-0.1.dev1.tar.gz/genetic-0.1.dev1/setup.py
@@ -0,0 +1,44 @@
+"""
+Setup module
+"""
+
+
+from setuptools import setup
+
+
+setup(
+    name='genetic',
+    version='0.1.dev1',
+
+    description=("A versatile distributable genetic algorithm build with"
+                 "flexibility and ease of use in mind"),
+
+    url="https://github.com/grayfall/genetic.git",
+
+    # Author details
+    author="Ilia Korvigo",
+    author_email="ilia.korvigo@gmail.com",
+
+    license="MIT",
+
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Scientific/Engineering ",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+    ],
+
+    # What does your project relate to?
+    keywords=("genetic algorithm, multiprocessing, numerical optimisation,"
+              "stochastic optimisation"),
+
+    packages=["genetic"],
+
+    install_requires=["numpy>=1.11.0",
+                      "scipy>=0.17.0",
+                      "multiprocess>=0.70.4"],
+)
