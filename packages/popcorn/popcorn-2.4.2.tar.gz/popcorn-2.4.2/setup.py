be1121acffd29d94e9f0dd302d77d363ff417e29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:21 +0000

    Add package popcorn/2.4.2/popcorn-2.4.2.tar.gz

---
+++ packages/popcorn/popcorn-2.4.2.tar.gz/popcorn-2.4.2/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+	name = 'popcorn',
+	packages = ['popcorn'],
+	version = '2.4.2',
+	description = 'The Python implementation of Inverse',
+	author = 'Sahibdeep Nann',
+	author_email = 'nann_sahibdeep@outlook.com',
+	url = 'https://github.com/InitializeSahib/Popcorn',
+	download_url = 'https://github.com/InitializeSahib/Popcorn/tarball/2.4.2',
+	keywords = ['inverse', 'module', 'library'],
+	classifiers = [],
+)
