a5c572a86717ec9892a7439a40681d6cdb8dda51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:31 +0000

    Add package jargparse/0.0.2/jargparse-0.0.2.tar.gz

---
+++ packages/jargparse/jargparse-0.0.2.tar.gz/jargparse-0.0.2/jargparse/__init__.py
@@ -0,0 +1,1 @@
+Add the __init__.py necessary to turn jargparse into a module
---
+++ packages/jargparse/jargparse-0.0.2.tar.gz/jargparse-0.0.2/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+setup(
+  name = 'jargparse',
+  packages = ['jargparse'], # this must be the same as the name above
+  version = '0.0.2',
+  description = 'A tiny super-dumb python module just because I like to see the usage info on stdout on an error',
+  author = 'Justin Clark-Casey',
+  author_email = 'justincc@justincc.org',
+  url = 'https://github.com/justinccdev/jargparse', # use the URL to the github repo
+  keywords = ['logging'], # arbitrary keywords
+)
