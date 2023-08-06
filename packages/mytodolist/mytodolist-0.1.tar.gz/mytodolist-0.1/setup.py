e4941802dfc74d13a67d3e6d297d6908a5c1eca3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:13 +0000

    Add package mytodolist/0.1/mytodolist-0.1.tar.gz

---
+++ packages/mytodolist/mytodolist-0.1.tar.gz/mytodolist-0.1/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+  name = 'mytodolist',
+  packages = ['mytodolist'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A random test lib',
+  author = 'kevin kiarie',
+  author_email = 'kevikiarie@yahoo.com',
+  url = 'https://github.com/kevinkiarie', # use the URL to the github repo
+  download_url = 'https://github.com/kevinkiarie/todo/tarball/0.1',
+  keywords = ['todo list', 'django', 'example'], # arbitrary keywords
+  classifiers = [],
+)
+
