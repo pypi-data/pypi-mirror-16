bf3771be5d955ec34fe644a4f8b3026012606d78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:07 +0000

    Add package ha-ffmpeg/0.3/ha-ffmpeg-0.3.zip

---
+++ packages/ha-ffmpeg/ha-ffmpeg-0.3.zip/ha-ffmpeg-0.3/haffmpeg/camera.py
@@ -0,0 +1,24 @@
+"""For HA camera components."""
+from .core import HAFFmpeg
+
+
+class CameraMjpeg(HAFFmpeg):
+    """Implement a camera they convert video stream to MJPEG."""
+
+    def __init__(self, ffmpeg_bin):
+        """Init CameraMjpeg."""
+        HAFFmpeg.__init__(self, ffmpeg_bin=ffmpeg_bin)
+
+    def open_camera(self, input_source, extra_cmd=None):
+        """Open FFmpeg process as mjpeg video stream."""
+        command = [
+            "-i",
+            input_source,
+            "-an",
+            "-c:v",
+            "mjpeg",
+            "-f",
+            "mpjpeg"
+        ]
+
+        self.open(cmd=command, extra_cmd=extra_cmd)
