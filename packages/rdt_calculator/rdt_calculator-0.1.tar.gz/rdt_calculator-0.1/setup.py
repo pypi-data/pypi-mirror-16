8c550250aaad0bfc25a8502e2c407a1b5fb61e30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:11 +0000

    Add package rdt_calculator/0.1/rdt_calculator-0.1.tar.gz

---
+++ packages/rdt_calculator/rdt_calculator-0.1.tar.gz/rdt_calculator-0.1/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+
+setup(
+  name = 'rdt_calculator',
+  packages = ['rdt_calculator'],
+  version = '0.1',
+  description = 'A simple calculator',
+  author = 'Rawdata Technologies PVT Limited',
+  author_email = 'logging@rawdatatech.com',
+  url = 'https://github.com/abdulshukoorpk/rdt_calculator',
+  download_url = 'https://github.com/abdulshukoorpk/rdt_calculator/1.0',
+  keywords = ['testing', 'calculator', 'addition'],
+  classifiers = [],
+)
