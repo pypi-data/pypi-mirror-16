8227d12cfa4f01bcd3e362a14d119275b3408abc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:54 +0000

    Add package pdf417gen/0.1.0/pdf417gen-0.1.0-py2.py3-none-any.whl

---
+++ packages/pdf417gen/pdf417gen-0.1.0-py2.py3-none-any.whl/pdf417/__init__.py
@@ -0,0 +1,4 @@
+from pdf417.encoding import encode
+from pdf417.rendering import render_image
+
+__all__ = [encode, render_image]
---
+++ packages/pdf417gen/pdf417gen-0.1.0-py2.py3-none-any.whl/pdf417gen-0.1.0.dist-info/DESCRIPTION.rst
@@ -0,0 +1,4 @@
+
+Easily encode your data into a 2D barcode following the PDF417 format.
+
+
