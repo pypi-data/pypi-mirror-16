94755749adc8197d237b90f9ce11fd814920afd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:40 +0000

    Add package PigLatinTranslation/0.1.3/PigLatinTranslation-0.1.3.tar.gz

---
+++ packages/PigLatinTranslation/PigLatinTranslation-0.1.3.tar.gz/PigLatinTranslation-0.1.3/setup.py
@@ -0,0 +1,24 @@
+import os
+from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name='PigLatinTranslation',
+    version='0.1.3',
+    include_package_data=True,
+    zip_safe=False,
+    url='https://github.com/Jai-Chaudhary/pig-latin-translation-microservice',
+    license='MIT',
+    author='Jai Chaudhary',
+    author_email='jai.chaudhary.ittd@gmail.com',
+    packages=['piglatintranslation',],
+    install_requires=['Flask'],
+    long_description=read('README.md'),
+    description='A Pig Latin Translation Microservice',
+    classifiers=[
+      "Development Status :: 3 - Alpha",
+    ]
+)
