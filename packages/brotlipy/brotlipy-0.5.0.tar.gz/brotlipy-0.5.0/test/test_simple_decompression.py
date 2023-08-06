8c31e306b796d83c606cd99d4075cf7332a0c5ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:22:30 +0000

    Add package brotlipy/0.5.0/brotlipy-0.5.0.tar.gz

---
+++ packages/brotlipy/brotlipy-0.5.0.tar.gz/brotlipy-0.5.0/test/test_simple_decompression.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+"""
+test_simple_decompression
+~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Tests for decompression of single chunks.
+"""
+import brotli
+
+import pytest
+
+
+def test_decompression(simple_compressed_file):
+    """
+    Decompressing files returns their original form using decompress.
+    """
+    with open(simple_compressed_file[0], 'rb') as f:
+        uncompressed_data = f.read()
+
+    with open(simple_compressed_file[1], 'rb') as f:
+        compressed_data = f.read()
+
+    assert brotli.decompress(compressed_data) == uncompressed_data
+
+
+def test_decompressobj(simple_compressed_file):
+    with open(simple_compressed_file[0], 'rb') as f:
+        uncompressed_data = f.read()
+
+    with open(simple_compressed_file[1], 'rb') as f:
+        compressed_data = f.read()
+
+    o = brotli.Decompressor()
+    data = o.decompress(compressed_data)
+    data += o.flush()
+
+    assert data == uncompressed_data
+
+
+def test_drip_feed(simple_compressed_file):
+    """
+    Sending in the data one byte at a time still works.
+    """
+    with open(simple_compressed_file[0], 'rb') as f:
+        uncompressed_data = f.read()
+
+    with open(simple_compressed_file[1], 'rb') as f:
+        compressed_data = f.read()
+
+    outdata = []
+    o = brotli.Decompressor()
+    for i in range(0, len(compressed_data)):
+        outdata.append(o.decompress(compressed_data[i:i+1]))
+
+    outdata.append(o.flush())
+
+    assert b''.join(outdata) == uncompressed_data
+
+
+def test_streaming_decompression_fails_properly_on_garbage():
+    """
+    Garbage data properly fails decompression.
+    """
+    o = brotli.Decompressor()
+    with pytest.raises(ValueError):
+        o.decompress(b'some random garbage')
+
+
+def test_decompression_fails_properly_on_garbage():
+    """
+    Garbage data properly fails decompression.
+    """
+    with pytest.raises(ValueError):
+        brotli.decompress(b'some random garbage')
