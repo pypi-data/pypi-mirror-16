6926a61b2d3a352ff80da4edc753b50c2ea5661f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:29 +0000

    Add package wladmin/1.6/wladmin-1.6-py2.7.egg

---
+++ packages/wladmin/wladmin-1.6-py2.7.egg/wladmin/static/wladmin/tinymce/jscripts/tiny_mce/plugins/searchreplace/langs/cn_dlg.js
@@ -0,0 +1,16 @@
+tinyMCE.addI18n('cn.searchreplace_dlg', {
+    findwhat: "查找内容",
+    replacewith: "替换为",
+    direction: "方向",
+    up: "向上",
+    down: "向下",
+    mcase: "区分大小写",
+    findnext: "查找下一个",
+    allreplaced: "搜索字符串的所有事件都被替换。",
+    "searchnext_desc": "再次查找",
+    notfound: "搜索已经完成。无法找到搜索字符串。",
+    "search_title": "查找",
+    "replace_title": "查找/替换",
+    replaceall: "全部替换",
+    replace: "替换"
+});
---
+++ packages/wladmin/wladmin-1.6-py2.7.egg/wladmin/static/wladmin/tinymce/jscripts/tiny_mce/themes/simple/langs/cn.js
@@ -0,0 +1,11 @@
+tinyMCE.addI18n('cn.simple', {
+    "cleanup_desc": "清理乱码",
+    "redo_desc": "重做 (Ctrl+Y)",
+    "undo_desc": "复原 (Ctrl+Z)",
+    "numlist_desc": "插入/删除编号列表",
+    "bullist_desc": "插入/删除项目符号列表",
+    "striketrough_desc": "删除线",
+    "underline_desc": "下划线 (Ctrl+U)",
+    "italic_desc": "斜体 (Ctrl+I)",
+    "bold_desc": "加粗 (Ctrl+B)"
+});
