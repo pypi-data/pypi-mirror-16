95cbb44082b80f5c31a8c4ec5f16b6409806c348
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:19:45 +0000

    Add package lazynester/1.0.0/lazynester-1.0.0.zip

---
+++ packages/lazynester/lazynester-1.0.0.zip/lazynester-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name    = 'lazynester',
+    version = '1.0.0',
+    py_modules = ['lazynester'],
+    author  = 'lazyfennec',
+    author_email  = 'lazyfenenc@163.com',
+    url     = 'http://pypi.python.org/pypi',
+    description = 'a simple printer of nested list',
+    )
