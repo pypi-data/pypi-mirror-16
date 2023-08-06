da3e785a600152fa25e0ab9c7e1c83134fe806a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:57:14 +0000

    Add package PyDrive/1.2.1/PyDrive-1.2.1.zip

---
+++ packages/PyDrive/PyDrive-1.2.1.zip/PyDrive-1.2.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='PyDrive',
+    version='1.2.1',
+    author='JunYoung Gwak',
+    author_email='jgwak@dreamylab.com',
+    maintainer='Robin Nabel',
+    maintainer_email='rnabel@ucdavis.edu',
+    packages=['pydrive', 'pydrive.test'],
+    url='https://github.com/googledrive/PyDrive',
+    license='LICENSE',
+    description='Google Drive API made easy.',
+    long_description=open('README.rst').read(),
+    install_requires=[
+        "google-api-python-client >= 1.2",
+        "PyYAML >= 3.0",
+    ],
+)
