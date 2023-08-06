5c55e37234e302a12dc99257acd90319498a2d45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:41 +0000

    Add package nolds/0.1.0/nolds-0.1.0.zip

---
+++ packages/nolds/nolds-0.1.0.zip/nolds-0.1.0/setup.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+setup(
+  name = 'nolds',
+  packages = ['nolds'],
+  version = '0.1.0',
+  description = 'Nonlinear measures for dynamical systems (based on one-dimensional time series)',
+  author = 'Christopher Schölzel',
+  author_email = 'christopher.schoelzel@gmx.net',
+  url = 'https://github.com/CSchoel/nolds',
+  download_url = 'https://github.com/CSchoel/nolds/tarball/0.1.0',
+  keywords = [
+    'nonlinear', 'dynamical system', 'chaos', 
+    'lyapunov', 'hurst', 'hurst exponent', 'rescaled range', 'DFA', 
+    'detrended fluctuation analysis', 'sample entropy', 'correlation dimension'],
+  classifiers = [],
+)
