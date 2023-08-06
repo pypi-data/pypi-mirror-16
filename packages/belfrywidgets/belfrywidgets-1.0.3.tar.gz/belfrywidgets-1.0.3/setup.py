9ae9b56e81f4452f27db829d8ff0d9e10c760f98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:05:04 +0000

    Add package belfrywidgets/1.0.3/belfrywidgets-1.0.3.tar.gz

---
+++ packages/belfrywidgets/belfrywidgets-1.0.3.tar.gz/belfrywidgets-1.0.3/setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+VERSION = "1.0.3"
+
+
+with open('README.rst') as f:
+    LONG_DESCR = f.read()
+
+data_files = []
+
+setup(
+    name='belfrywidgets',
+    version=VERSION,
+    description='A collection of useful tkinter widgets.',
+    long_description=LONG_DESCR,
+    author='Revar Desmera',
+    author_email='revarbat@gmail.com',
+    url='https://github.com/revarbat/belfrywidgets',
+    download_url='https://github.com/revarbat/belfrywidgets/archive/master.zip',
+    packages=['belfrywidgets'],
+    license='MIT License',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: MacOS X',
+        'Environment :: MacOS X :: Aqua',
+        'Environment :: MacOS X :: Carbon',
+        'Environment :: MacOS X :: Cocoa',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Software Development :: Widget Sets',
+    ],
+    keywords='tkinter widgets',
+    install_requires=['setuptools'],
+    data_files=data_files,
+)
