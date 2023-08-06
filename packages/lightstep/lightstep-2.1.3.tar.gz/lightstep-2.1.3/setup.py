69131d1c3749b84f4be8f618944b1ab4b0e3a5b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:46 +0000

    Add package lightstep/2.1.3/lightstep-2.1.3.tar.gz

---
+++ packages/lightstep/lightstep-2.1.3.tar.gz/lightstep-2.1.3/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='lightstep',
+    version='2.1.3',
+    description='LightStep Python OpenTracing Implementation',
+    long_description='',
+    author='LightStep',
+    license='',
+    install_requires=['thrift==0.9.2',
+                      'jsonpickle',
+                      'basictracer==2.0.1.dev1',
+                      'opentracing==2.0.0.dev3'],
+    tests_require=['sphinx',
+                   'sphinx-epytext'],
+
+    classifiers=[
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        # 'Programming Language :: Python :: 3',
+    ],
+
+    keywords=[ 'opentracing', 'lightstep', 'traceguide', 'tracing', 'microservices', 'distributed' ],
+    packages=find_packages(exclude=['docs*', 'tests*', 'sample*']),
+)
