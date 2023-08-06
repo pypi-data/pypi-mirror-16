bb553c01a1aa12e57ba26b9954721b97509f084c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:08 +0000

    Add package middleware/1.2/middleware-1.2.tar.gz

---
+++ packages/middleware/middleware-1.2.tar.gz/middleware-1.2/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'middleware',
+  packages = ['middleware'],
+  version = '1.2',
+  description = 'Python module to add general middleware',
+  author = 'David Betz',
+  author_email = 'dfb@davidbetz.net',
+  url = 'https://github.com/davidbetz/middleware',
+  download_url = 'https://github.com/davidbetz/middleware/tarball/1.2',
+  keywords = ['middleware'],
+  classifiers = [],
+)
