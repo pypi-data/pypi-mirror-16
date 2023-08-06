ca81e7fdd387d0a717489b3d9fbc2e8159eac817
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:16 +0000

    Add package msb_client/0.1/msb_client-0.1.tar.gz

---
+++ packages/msb_client/msb_client-0.1.tar.gz/msb_client-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'msb_client',
+  packages = ['msb_client'],
+  version = '0.1',
+  description = 'MSB client implementation for python 2 and 3',
+  author = 'Andy Grabow',
+  author_email = 'andy@freilandkiwis.de',
+  url = 'https://bitbucket.org/kakulukia/msb-client-py',
+  download_url = 'https://bitbucket.org/kakulukia/msb-client-py/get/0.1.0.tar.gz',
+  keywords = ['VFK', 'MSB', 'Client'],
+  classifiers = [],
+)
