5091acaf33369cc621c8afb433bec64c0e4b1757
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:32 +0000

    Add package pyretry/1.0.0/pyretry-1.0.0.tar.gz

---
+++ packages/pyretry/pyretry-1.0.0.tar.gz/pyretry-1.0.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+
+setup(
+    name='pyretry',
+    version="1.0.0",
+    description='Separate your retry logic from your business logic',
+    author='Bob Renwick',
+    author_email='bob.renwick@gmail.com',
+    url='https://github.com/bobbyrenwick/pyretry',
+    packages=['pyretry'],
+    classifiers=(
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+    ),
+)
