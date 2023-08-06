3d62fa8486ac967c8a1126de13b6a5e58ee4501f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:33:29 +0000

    Add package homely/0.8.1/homely-0.8.1.tar.gz

---
+++ packages/homely/homely-0.8.1.tar.gz/homely-0.8.1/test/system/cmdline.py
@@ -0,0 +1,5 @@
+from pytest import HOMELY, contents, TestRepo, checkrepolist
+
+
+def test_homely_update_check(tmpdir):
+    raise Exception("TODO: test all aspects of 'homely updatecheck'")  # noqa
