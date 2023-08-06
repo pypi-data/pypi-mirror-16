24f9aeea13fe1983c9bc1c041692f63c300e0f77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:19 +0000

    Add package CherryMusic/0.37.1/CherryMusic-0.37.1.tar.gz

---
+++ packages/CherryMusic/CherryMusic-0.37.1.tar.gz/CherryMusic-0.37.1/res/react-client/tests/components/MessageOfTheDay.spec.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import MessageOfTheDay from 'components/MessageOfTheDay/MessageOfTheDay'
+
+describe('(Component) MessageOfTheDay', () => {
+  it('should exist', () => {
+
+  })
+})
