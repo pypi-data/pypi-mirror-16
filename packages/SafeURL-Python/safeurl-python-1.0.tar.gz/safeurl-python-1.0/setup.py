677356c34935c80b7310cbcd00bbb7da9d6a8451
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:36:25 +0000

    Add package SafeURL-Python/1.0/safeurl-python-1.0.tar.gz

---
+++ packages/SafeURL-Python/safeurl-python-1.0.tar.gz/safeurl-python-1.0/setup.py
@@ -0,0 +1,3 @@
+from distutils.core import setup
+
+setup(name="safeurl-python",url="https://github.com/IncludeSecurity/safeurl-python",version="1.0",license="MIT License", long_description=open("README").read(), author="IncludeSecurity", author_email="safeurl@includesecurity.com")
