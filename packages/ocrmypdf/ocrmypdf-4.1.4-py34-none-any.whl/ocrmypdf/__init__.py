0a4d5554280f219d94a1515cbfdecadc91b35c11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:05 +0000

    Add package ocrmypdf/4.1.4/ocrmypdf-4.1.4-py34-none-any.whl

---
+++ packages/ocrmypdf/ocrmypdf-4.1.4-py34-none-any.whl/ocrmypdf/__init__.py
@@ -0,0 +1,24 @@
+from enum import IntEnum
+import os
+
+
+class ExitCode(IntEnum):
+    ok = 0
+    bad_args = 1
+    input_file = 2
+    missing_dependency = 3
+    invalid_output_pdfa = 4
+    file_access_error = 5
+    already_done_ocr = 6
+    child_process_error = 7
+    encrypted_pdf = 8
+    other_error = 15
+
+
+def get_program(name):
+    envvar = 'OCRMYPDF_' + name.upper()
+    return os.environ.get(envvar, name)
+
+
+def page_number(input_file):
+    return int(os.path.basename(input_file)[0:6])
