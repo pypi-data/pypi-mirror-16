62c6bb9963cb4e0cf1047083391fd900a205c38b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:58 +0000

    Add package hgijson/1.4.0/hgijson-1.4.0.tar.gz

---
+++ packages/hgijson/hgijson-1.4.0.tar.gz/hgijson-1.4.0/CHANGELOG.md
@@ -0,0 +1,47 @@
+# Change Log
+## 1.4.0 - 2016-08-04
+### Changed
+- Encoders now serialise `None` to `null` and visa-versa for decoders 
+([#17](https://github.com/wtsi-hgi/python-json/issues/17)). (Thanks to 
+[YuriIvanov](https://github.com/YuriIvanov) for bringing this improvement to light in 
+[#16](https://github.com/wtsi-hgi/python-json/issues/16)).
+- Corrected self-referential type hinting. 
+
+## 1.3.1 - 2016-05-19
+### Changed
+- Corrections to `setup.py`.
+
+## 1.3.0 - 2016-05-19
+### Added
+- Documentation on how to serialize to/from a dictionary opposed to a string.
+- Documentation on how to install the library, import methods/classes and develop.
+- imports via `__init__.py`.
+
+### Changed
+- Removed useless equality and hash calculation methods on `PropertyMapping`.
+- Improvements to documentation.
+
+### Removed
+- Helper test runner scripts.
+
+## 1.2.2 - 2016-05-18
+### Changed
+- Hotfix for dependency installation when installed via PyPI.
+
+## 1.2.1 - 2016-05-18
+### Changed
+- Hotfix for package installation via PyPI.
+
+## 1.2.0 - 2016-05-18
+### Added
+- Added documentation on how the order of mappings is determined when using mappers and inheritance.
+
+### Changed
+- Sorted packing to allow upload as package to PyPI.
+- Changed license from GPL to MIT.
+
+### Removed
+- Dependency on `hgicommon` library (still required for testing though).
+
+## 1.0.0 - 2016-04-14
+- First stable release.
---
+++ packages/hgijson/hgijson-1.4.0.tar.gz/hgijson-1.4.0/hgijson/json/_converters.py
@@ -0,0 +1,117 @@
+import json
+from abc import ABCMeta, abstractproperty
+from json import JSONDecoder, JSONEncoder
+from typing import Any, Dict, Optional
+
+from hgijson.json.interfaces import ParsedJSONDecoder
+from hgijson.serialization import Deserializer, Serializer
+from hgijson.types import PrimitiveJsonSerializableType, PrimitiveUnionType, SerializableType
+
+_serializer_cache = dict()  # type: Dict[type, Serializer]
+_deserializer_cache = dict()    # type: Dict[type, Deserializer]
+
+_PLACEHOLDER = object
+
+
+class _JSONEncoderAsSerializer(Serializer, metaclass=ABCMeta):
+    """
+    JSON encoder wrapped to work as a `Serializer`.
+    """
+    @abstractproperty
+    def _get_encoder_type(self, *args) -> type:
+        """
+        Gets the `JSONEncoder` type that is used by this serialiser.
+        :return: the encoder type (must be a subclass of `JSONEncoder`)
+        """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__([])
+        json_encoder_cls = self._get_encoder_type()
+        self._encoder = json_encoder_cls(*args, **kwargs)  # type: JSONEncoder
+
+    def serialize(self, serializable: Optional[SerializableType]) -> PrimitiveUnionType:
+        if type(self._encoder) == JSONEncoder:
+            # Have to load from string to more rich representation - not possible to stop `encode` going to string :/
+            return json.loads(self._encoder.encode(serializable))
+        else:
+            return self._encoder.default(serializable)
+
+    def _create_serializer_of_type(self, serializer_type: type) -> None:
+        """
+        Unused - implemented to satisfy the interface only.
+        """
+
+    def _create_serialized_container(self) -> None:
+        """
+        Unused - implemented to satisfy the interface only.
+        """
+
+
+class _JSONDecoderAsDeserializer(Deserializer, metaclass=ABCMeta):
+    """
+    JSON decoder wrapped to work as a `Deserializer`.
+    """
+    @abstractproperty
+    def _get_decoder_type(self, *args) -> type:
+        """
+        Gets the `JSONDecoder` type that is used by this deserialiser.
+        :return: the decoder type (must be a subclass of `JSONDecoder`)
+        """
+
+    def __init__(self, *args, **kwargs):
+        json_decoder_cls = self._get_decoder_type()
+        super().__init__([], json_decoder_cls)
+        self._decoder = json_decoder_cls(*args, **kwargs)  # type: JSONDecoder
+
+    def deserialize(self, json_as_dict: PrimitiveJsonSerializableType) -> Optional[SerializableType]:
+        if not isinstance(self._decoder, ParsedJSONDecoder):
+            # Decode must take a string (even though we have a richer representation) :/
+            json_as_string = json.dumps(json_as_dict)
+            return self._decoder.decode(json_as_string)
+        else:
+            # Optimisation - no need to convert our relatively rich representation into a string (just to turn it back
+            # again!)
+            return self._decoder.decode_parsed(json_as_dict)
+
+    def _create_deserializer_of_type(self, deserializer_type: type) -> None:
+        """
+        Unused - implemented to satisfy the interface only.
+        """
+
+
+def json_encoder_to_serializer(encoder_cls: type) -> type:
+    """
+    Converts a `JSONEncoder` class into an equivalent `Serializer` class.
+    :param encoder_cls: the encoder class
+    :return: the equivalent `Serializer` class
+    """
+    if encoder_cls not in _serializer_cache:
+        encoder_as_serializer_cls = type(
+            "%sAsSerializer" % encoder_cls.__class__.__name__,
+            (_JSONEncoderAsSerializer,),
+            {
+                "_get_encoder_type": lambda self: encoder_cls
+            }
+        )
+        _serializer_cache[encoder_cls] = encoder_as_serializer_cls
+
+    return _serializer_cache[encoder_cls]
+
+
+def json_decoder_to_deserializer(decoder_cls: type) -> type:
+    """
+    Converts a `JSONDecoder` class into an equivalent `Deserializer` class.
+    :param decoder_cls: the decoder class
+    :return: the equivalent `Deserializer` class
+    """
+    if decoder_cls not in _deserializer_cache:
+        encoder_as_deserializer_cls = type(
+            "%sAsDeserializer" % decoder_cls.__class__.__name__,
+            (_JSONDecoderAsDeserializer,),
+            {
+                "_get_decoder_type": lambda self: decoder_cls
+            }
+        )
+        _deserializer_cache[decoder_cls] = encoder_as_deserializer_cls
+
+    return _deserializer_cache[decoder_cls]
