46d2735ec5c64e4cc076e3c439957c2081c6f7eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:37 +0000

    Add package yurual_test1/1.1.0/yurual_test1-1.1.0.tar.gz

---
+++ packages/yurual_test1/yurual_test1-1.1.0.tar.gz/yurual_test1-1.1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+        name         = 'yurual_test1',
+        version      = '1.1.0',
+        py_modules   = ['nester'],
+        author       = 'rogeryu',
+        author_email ='xiangyu.power@gmail.com',
+       url           = 'http://www.headfirstlabs.com',
+       description   = 'A simple printer of nested  lists',
+       )
