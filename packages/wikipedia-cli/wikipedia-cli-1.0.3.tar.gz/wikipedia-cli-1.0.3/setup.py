028cb7ad62ab22e573a9199f30cc8b5239e03010
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:46 +0000

    Add package wikipedia-cli/1.0.3/wikipedia-cli-1.0.3.tar.gz

---
+++ packages/wikipedia-cli/wikipedia-cli-1.0.3.tar.gz/wikipedia-cli-1.0.3/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+
+DESCRIPTION = "A command-line interface for fetching summaries of wikipedia pages."
+
+with open('README.rst', 'r') as f:
+    LONG_DESCRIPTION = f.read()
+
+with open('requirements.txt', 'r') as f:
+    requirements = [line.rstrip() for line in f]
+
+setup(
+        author="Kalle Saari",
+        author_email="kasaar2@gmail.com",
+        url="https://github.com/kalleroska/wikipedia-cli",
+        name="wikipedia-cli",
+        version="1.0.3",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        license="MIT License",
+        keywords='wikipedia cli summary',
+        classifiers=['Programming Language :: Python',
+                     'Programming Language :: Python :: 3.5',
+                     'License :: OSI Approved :: MIT License',
+                     'Development Status :: 4 - Beta',
+                     'Topic :: Utilities',
+                     'Intended Audience :: Developers',
+                     'Environment :: Console'],
+        py_modules = ['wikipedia_cli'],
+        install_requires=requirements,
+        entry_points={
+            'console_scripts': [
+                'wikipedia=wikipedia_cli:main'
+                ]
+            }
+        )
