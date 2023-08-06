2a2d86fc78bbbd6e72996180a9239f925832368c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:31 +0000

    Add package felapps/1.5.7/felapps-1.5.7-py2.py3-none-any.whl

---
+++ packages/felapps/felapps-1.5.7-py2.py3-none-any.whl/felapps/__init__.py
@@ -0,0 +1,25 @@
+from .utils import felutils
+from .utils import funutils
+from .utils import resutils
+from .utils import scanutils
+from .utils import datautils
+from .utils import matchutils
+from .utils import imageutils
+from .utils import uiutils
+from .utils import myui
+from .utils import analysisframe
+from .physics import felcalc
+from .physics import felbase
+from .facilities import dcls
+from .apps.imageviewer  import imageviewer
+from .apps.cornalyzer   import cornalyzer
+from .apps.felformula   import felformula
+from .apps.dataworkshop import dataworkshop
+from .apps.matchwizard  import matchwizard
+from .apps.appdrawer    import appdrawer
+from .apps.wxmpv        import wxmpv
+
+__version__ = appdrawer.__version__
+
+#__all__ = [imageviewer]
+
