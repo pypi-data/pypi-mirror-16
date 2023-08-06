fc9cc08a80239f8109ea14fc7d360947e28cee55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:23 +0000

    Add package neosdong_nester/1.3.1/neosdong_nester-1.3.1.tar.gz

---
+++ packages/neosdong_nester/neosdong_nester-1.3.1.tar.gz/neosdong_nester-1.3.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='neosdong_nester',
+    version='1.3.1',
+    py_modules=['nester'],
+    author='neosdong',
+    author_email='neosdong@gmail.com',
+    url='http://google.com',
+    description='A simple printer of nested lists'
+
+)
