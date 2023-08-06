a25e025b265a008a83e4270de43a6eb1056e37be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:56 +0000

    Add package ndic/0.6/ndic-0.6.tar.gz

---
+++ packages/ndic/ndic-0.6.tar.gz/ndic-0.6/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+
+setup(
+  name = 'ndic',
+  packages = ['ndic'],
+  # py_modules=['ndic'],
+  install_requires = [
+    'requests',
+    'beautifulsoup4',
+    'click',
+  ],
+  version = '0.6',
+  description = 'Python module for NAVER English-Korean and Korean-English dictionaries',
+  author = 'jupiny',
+  author_email = 'tmdghks584@gmail.com',
+  url = 'https://github.com/jupiny/ndic',
+  download_url = 'https://github.com/jupiny/ndic/tarball/0.6',
+  # keywords = ['dictionary', 'translate', 'English', 'Korean', 'Naver'],
+  classifiers = [],
+  entry_points='''
+    [console_scripts]
+    ndic=ndic.scripts.ndic:ndic
+  ''',
+)
