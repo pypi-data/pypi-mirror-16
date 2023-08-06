12619b6e0897a6df8a15274570316c92908f4b41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:01 +0000

    Add package jupyter_nim_kernel/1.1/jupyter_nim_kernel-1.1.zip

---
+++ packages/jupyter_nim_kernel/jupyter_nim_kernel-1.1.zip/jupyter_nim_kernel-1.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='jupyter_nim_kernel',
+      version='1.1',
+      description='Simple Nim kernel for Jupyter',
+      author='stisa',
+      author_email='silvio@stisa.space',
+      url='https://github.com/stisa/jupyter-nim-kernel.git',
+      download_url='https://github.com/stisa/jupyter-nim-kernel/archive/master.zip',
+      packages=['jupyter_nim_kernel'],
+      keywords=['jupyter', 'kernel', 'nim']
+      )
