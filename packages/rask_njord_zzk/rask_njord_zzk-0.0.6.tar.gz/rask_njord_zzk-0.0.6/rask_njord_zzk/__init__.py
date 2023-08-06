c870a41becb0f5be382f8daba49418cc1aa12aea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:05:08 +0000

    Add package rask_njord_zzk/0.0.6/rask_njord_zzk-0.0.6.tar.gz

---
+++ packages/rask_njord_zzk/rask_njord_zzk-0.0.6.tar.gz/rask_njord_zzk-0.0.6/rask_njord_zzk/__init__.py
@@ -0,0 +1,54 @@
+from rask.base import Base
+from rask.parser.json import jsonify
+
+__all__ = ['ZZK']
+
+class ZZK(Base):
+    options = {
+        'url':'http://api.00k.srv.br'
+    }
+    
+    def __init__(self,token,http):
+        self.http = http
+        self.token = token
+
+    @property
+    def request(self):
+        return {'headers':{'Authorization':self.token,'Content-Type':'application/json;charset=UTF-8'},'request_timeout':60.0}
+
+    def get_category(self,uid,future):
+        def on_fetch(_):
+            future.set_result(_.result())
+            return True
+        
+        request = self.request
+        request['method'] = 'GET'
+        request['url'] = '%s/catalog/categories?externalid=%s' % (self.options['url'],uid)
+        
+        self.http.fetch(request,future=self.ioengine.future(on_fetch))
+        return True
+    
+    def get_sku(self,sku,future):
+        def on_fetch(_):
+            future.set_result(_.result())
+            return True
+        
+        request = self.request
+        request['method'] = 'GET',
+        request['url'] = '%s/catalog/products/all?sku=%s' % (self.options['url'],sku)
+        
+        self.http.fetch(request,future=self.ioengine.future(on_fetch))
+        return True
+
+    def set_stock_bulk(self,payload,future):
+        def on_fetch(_):
+            future.set_result(_.result())
+            return True
+        
+        request = self.request
+        request['body'] = jsonify(payload)
+        request['method'] = 'PUT'
+        request['url'] = '%s/catalog/products/stock' % self.options['url']
+
+        self.http.fetch(request,future=self.ioengine.future(on_fetch))
+        return True
