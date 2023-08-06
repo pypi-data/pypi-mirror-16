1d45d6401f0d2f6ea9a37f69f070a61a199a9f2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:02 +0000

    Add package py-cassandra-journal-forwarder/0.4/py-cassandra-journal-forwarder-0.4.tar.gz

---
+++ packages/py-cassandra-journal-forwarder/py-cassandra-journal-forwarder-0.4.tar.gz/py-cassandra-journal-forwarder-0.4/setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python2
+
+from setuptools import setup, find_packages
+setup(name='py-cassandra-journal-forwarder',
+      version='0.4',
+      author='Max Chesterfield',
+      license='MIT',
+      description='Forwards systemd journal to a cassandra cluster',
+      packages=find_packages())
