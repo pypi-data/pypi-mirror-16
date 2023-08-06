b0d1f394d2811822eb3c9a95ebc28afb860cf516
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:04 +0000

    Add package fio_sale_channel/3.4.34.0/fio_sale_channel-3.4.34.0.tar.gz

---
+++ packages/fio_sale_channel/fio_sale_channel-3.4.34.0.tar.gz/fio_sale_channel-3.4.34.0/tests/__init__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+    tests/__init__.py
+
+"""
+import unittest
+
+import trytond.tests.test_tryton
+
+from tests.test_views_depends import TestViewsDepends
+from tests.test_sale_channel import TestSaleChannel
+
+
+def suite():
+    """
+    Define suite
+    """
+    test_suite = trytond.tests.test_tryton.suite()
+    test_suite.addTests([
+        unittest.TestLoader().loadTestsFromTestCase(TestViewsDepends),
+        unittest.TestLoader().loadTestsFromTestCase(TestSaleChannel),
+    ])
+    return test_suite
+
+if __name__ == '__main__':
+    unittest.TextTestRunner(verbosity=2).run(suite())
