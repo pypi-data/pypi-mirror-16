13689931343eebb55064905597196b5b1704b58d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:38 +0000

    Add package pydmm/0.1/pydmm-0.1.zip

---
+++ packages/pydmm/pydmm-0.1.zip/pydmm-0.1/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+setup(
+  name = 'pydmm',
+  packages = ['pydmm'], # gleicher Name wie oben
+  version = '0.1',
+  description = 'Read data from a dmm',
+  author = 'Michael Weigend',
+  author_email = 'mw@creative-informatics.de',
+  license='Creative Commons Attribution-Noncommercial-Share Alike license',
+  url = 'https://github.com/mweigend/pydmm', # URL zum GitHub 
+  download_url = 'https://github.com/mweigend/pydmm/tarball/0.1', # I'll explain this in a second
+  keywords = ['dmm', 'digital multimeter'], # arbitrary keywords
+  classifiers = [],
+)
+
