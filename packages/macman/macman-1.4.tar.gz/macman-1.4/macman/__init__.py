565153ff40ba865decb81d1116fcf1d3ba75c89f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:07 +0000

    Add package macman/1.4/macman-1.4.tar.gz

---
+++ packages/macman/macman-1.4.tar.gz/macman-1.4/macman/__init__.py
@@ -0,0 +1,17 @@
+
+from macman import cocoadialog
+from macman import files
+from macman import logs
+from macman import misc
+from macman import munki
+from macman import plists
+from macman import remoteaccess
+from macman import systime
+from macman import users
+
+__version__ = 1.4
+
+# if no log file specified, don't use log
+if '__builtin__log' not in locals():
+    import __builtin__
+    __builtin__.log = None
