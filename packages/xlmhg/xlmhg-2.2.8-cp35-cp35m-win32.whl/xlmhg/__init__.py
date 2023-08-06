51492c50e94eab422e1e8948ddcb08c37cdd2e86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:15 +0000

    Add package xlmhg/2.2.8/xlmhg-2.2.8-cp35-cp35m-win32.whl

---
+++ packages/xlmhg/xlmhg-2.2.8-cp35-cp35m-win32.whl/xlmhg/__init__.py
@@ -0,0 +1,8 @@
+from __future__ import (absolute_import, division,
+                        print_function, unicode_literals)
+from builtins import *
+
+from .result import mHGResult
+from .test import xlmhg_test, get_xlmhg_test_result
+
+__all__ = ['mHGResult', 'xlmhg_test', 'get_xlmhg_test_result']
