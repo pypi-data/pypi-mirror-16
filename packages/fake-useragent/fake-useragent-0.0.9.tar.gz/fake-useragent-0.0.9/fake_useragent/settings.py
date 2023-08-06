3b82b6ca70d56a9d6858f5dff83291fbc69f1b6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:42 +0000

    Add package fake-useragent/0.0.9/fake-useragent-0.0.9.tar.gz

---
+++ packages/fake-useragent/fake-useragent-0.0.9.tar.gz/fake-useragent-0.0.9/fake_useragent/settings.py
@@ -0,0 +1,28 @@
+import os
+import tempfile
+
+DB = os.path.join(
+    tempfile.gettempdir(), 'fake_useragent.json'
+)
+
+BROWSERS_STATS_PAGE = 'http://www.w3schools.com/browsers/browsers_stats.asp'
+
+BROWSER_BASE_PAGE = 'http://useragentstring.com/pages/useragentstring.php?name=%s'  # noqa
+
+
+BROWSERS_COUNT_LIMIT = 30
+
+REPLACEMENTS = (' ', '_')
+
+SHORTCUTS = (
+    ('internet explorer', 'internetexplorer'),
+    ('ie', 'internetexplorer'),
+    ('msie', 'internetexplorer'),
+    ('google', 'chrome'),
+    ('googlechrome', 'chrome'),
+    ('ff', 'firefox')
+)
+
+OVERRIDES = (
+    ('Internet Explorer', 'IE'),
+)
