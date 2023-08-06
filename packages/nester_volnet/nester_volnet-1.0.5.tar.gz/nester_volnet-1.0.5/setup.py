a3302408e93092aeae422e5867aa24553dc0ba5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:58:48 +0000

    Add package nester_volnet/1.0.5/nester_volnet-1.0.5.tar.gz

---
+++ packages/nester_volnet/nester_volnet-1.0.5.tar.gz/nester_volnet-1.0.5/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'nester_volnet',
+    version = '1.0.5',
+    py_modules = ['nester'],
+    author = 'volnet',
+    author_email = 'volnet@tom.com',
+    url = 'http://volnet.github.io',
+    description='A simple printer of nested lists, this version add the testnester.py',
+)
