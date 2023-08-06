15b19fa00a9afc3da62c317d20c6f14af8d2e79e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:06:38 +0000

    Add package gotchatwitter/0.0.7/gotchatwitter-0.0.7.tar.gz

---
+++ packages/gotchatwitter/gotchatwitter-0.0.7.tar.gz/gotchatwitter-0.0.7/gotchatwitter/reader_utils.py
@@ -0,0 +1,25 @@
+import warnings
+
+
+def read_job(job):
+    jobs = ['timeline', 'threads']
+    if job in set(jobs):
+        return job
+    else:
+        raise LookupError('Job options:\ttimeline threads')
+
+
+def read_header(job, **kwargs):
+    readers = {'timeline': _read_header_timeline(**kwargs)}
+    return readers[job]
+
+
+def _read_header_timeline(**kwargs):
+    default = ['status', 'uid', 'screen_name', 'tid', 'timestamp', 'text', 'media',
+               'language', 'n_retweets', 'n_likes',
+               'location_id', 'location_name']
+    header = kwargs.get('output_header', default)
+    other_ = set(header) - set(default)
+    if other_:
+        warnings.warn('header %s not found' % other_)
+    return header
