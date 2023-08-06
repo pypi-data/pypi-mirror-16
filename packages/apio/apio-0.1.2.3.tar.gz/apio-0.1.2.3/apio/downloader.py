7279271378e80e19ff0b161cf1711875214ad656
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:04 +0000

    Add package apio/0.1.2.3/apio-0.1.2.3.tar.gz

---
+++ packages/apio/apio-0.1.2.3.tar.gz/apio-0.1.2.3/apio/downloader.py
@@ -0,0 +1,105 @@
+# -*- coding: utf-8 -*-
+# -- This file is part of the Apio project
+# -- (C) 2016 FPGAwars
+# -- Author Jesús Arroyo
+# -- Licence GPLv2
+# -- Derived from:
+# ---- Platformio project
+# ---- (C) 2014-2016 Ivan Kravets <me@ikravets.com>
+# ---- Licence Apache v2
+
+from email.utils import parsedate_tz
+from math import ceil
+from os.path import getsize, join
+from time import mktime
+
+from . import util
+from .exception import FDSHASumMismatch, FDSizeMismatch, \
+                       FDUnrecognizedStatusCode
+
+import click
+import requests
+requests.packages.urllib3.disable_warnings()
+
+
+class FileDownloader(object):
+
+    CHUNK_SIZE = 1024
+
+    def __init__(self, url, dest_dir=None):
+        self._url = url
+        self._fname = url.split("/")[-1]
+
+        self._destination = self._fname
+        if dest_dir:
+            self.set_destination(join(dest_dir, self._fname))
+
+        self._progressbar = None
+        self._request = None
+
+        # make connection
+        self._request = requests.get(url, stream=True,
+                                     headers=util.get_request_defheaders())
+        if self._request.status_code != 200:
+            raise FDUnrecognizedStatusCode(self._request.status_code, url)
+
+    def set_destination(self, destination):
+        self._destination = destination
+
+    def get_filepath(self):
+        return self._destination
+
+    def get_lmtime(self):
+        if 'last-modified' in self._request.headers:
+            return self._request.headers['last-modified']
+
+    def get_size(self):
+        return int(self._request.headers['content-length'])
+
+    def start(self):
+        itercontent = self._request.iter_content(chunk_size=self.CHUNK_SIZE)
+        f = open(self._destination, "wb")
+        chunks = int(ceil(self.get_size() / float(self.CHUNK_SIZE)))
+
+        with click.progressbar(length=chunks, label="Downloading") as pb:
+            for _ in pb:
+                f.write(next(itercontent))
+        f.close()
+        self._request.close()
+
+        self._preserve_filemtime(self.get_lmtime())
+
+    def verify(self, sha1=None):
+        _dlsize = getsize(self._destination)
+        if _dlsize != self.get_size():
+            raise FDSizeMismatch(_dlsize, self._fname, self.get_size())
+
+        if not sha1:
+            return
+
+        dlsha1 = None
+        try:
+            result = util.exec_command(["sha1sum", self._destination])
+            dlsha1 = result['out']
+        except (OSError, ValueError):
+            try:
+                result = util.exec_command(
+                    ["shasum", "-a", "1", self._destination])
+                dlsha1 = result['out']
+            except (OSError, ValueError):
+                pass
+
+        if dlsha1:
+            dlsha1 = dlsha1[1:41] if dlsha1.startswith("\\") else dlsha1[:40]
+            if sha1 != dlsha1:
+                raise FDSHASumMismatch(dlsha1, self._fname, sha1)
+
+    def _preserve_filemtime(self, lmdate):
+        if lmdate is not None:
+            timedata = parsedate_tz(lmdate)
+            lmtime = mktime(timedata[:9])
+            util.change_filemtime(self._destination, lmtime)
+
+    def __del__(self):
+        if self._request:
+            self._request.close()
---
+++ packages/apio/apio-0.1.2.3.tar.gz/apio-0.1.2.3/setup.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name='apio',
+    version='0.1.2.3',
+    author='Jesús Arroyo Torrens',
+    author_email='jesus.jkhlg@gmail.com',
+    url='https://github.com/FPGAwars/apio',
+    license='GPLv2',
+    packages=['apio'],
+    package_data={
+        'apio': ['SConstruct',
+                 'resources/*.rules',
+                 'resources/*.json']
+    },
+    install_requires=[
+        'click',
+        'requests'
+    ],
+    entry_points={
+        'console_scripts': ['apio=apio:cli']
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Programming Language :: Python']
+)
