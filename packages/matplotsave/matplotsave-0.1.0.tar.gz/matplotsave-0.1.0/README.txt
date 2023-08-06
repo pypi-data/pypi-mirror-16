9baf00c43e4676245563041bc5ba0ec787ef054a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:57 +0000

    Add package matplotsave/0.1.0/matplotsave-0.1.0.tar.gz

---
+++ packages/matplotsave/matplotsave-0.1.0.tar.gz/matplotsave-0.1.0/README.txt
@@ -0,0 +1,9 @@
+Intro
+=====
+
+A library of functions (just one at the moment) to streamline the saving
+and exporting of matplotlib plots as images and whatnot.
+
+Usage
+=====
+
---
+++ packages/matplotsave/matplotsave-0.1.0.tar.gz/matplotsave-0.1.0/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+import os
+long_description = 'Shortcuts for saving and exporting matplotlib plots.'
+if os.path.exists('README.txt'):
+    long_description = open('README.txt').read()
+
+setup(
+    name='matplotsave',
+    author='Christopher Chen',
+    author_email='christopher.chen1995@gmail.com',
+    version='0.1.0',
+    description="""Helper functions and context manager to make it easier to save matplotlib plots as images.""",
+    long_description=long_description,
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Operating System :: OS Independent",
+    ],
+    keywords="matplotlib save export images context managers functions",
+    url='https://bitbucket.org/TheCDC/matplotsave',
+    license='Public Domain',
+    packages=['matplotsave'],
+    download_url="https://bitbucket.org/TheCDC/matplotsave/get/HEAD.zip",
+    install_requires=[],
+    zip_safe=True
+)
