03be62d0ca24b6a3b7ad65fcacdbaf424c891cef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:24 +0000

    Add package cloud_pipe/0.0.3/cloud_pipe-0.0.3.zip

---
+++ packages/cloud_pipe/cloud_pipe-0.0.3.zip/cloud_pipe-0.0.3/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+
+setup(name='cloud_pipe',
+      version='0.0.3',
+      description='An Automatic tool for setting up your image processing work flow on the cloud',
+      url='https://github.com/wangyx2005/cloud_pipe.git',
+      author='Yuxing Wang',
+      author_email='wangyx2005@gmail.com',
+      packages=find_packages(),
+      install_requires=['boto3', 'Haikunator'],
+      # scripts=['bin/wrap', 'bin/setup_pipe'],
+      entry_points={
+          'console_scripts': [
+              'wrap = cloud_pipe.scripts.wrap:main',
+              'clean-up = cloud_pipe.pipeline.cleanup:main',
+              'setup-pipe = cloud_pipe.scripts.setup_pipe:main',
+              'create-lambda-exec-role = cloud_pipe.scripts.setup_pipe: create_lambda_exec_role']
+      },
+      include_package_data=True,
+      zip_safe=False)
