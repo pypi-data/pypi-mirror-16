864ea95670df30050a608e59e82907d438162644
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:34 +0000

    Add package umls_request_manager/0.5/umls_request_manager-0.5.tar.gz

---
+++ packages/umls_request_manager/umls_request_manager-0.5.tar.gz/umls_request_manager-0.5/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(name='umls_request_manager',
+	version='0.5',
+	description='Authenticates, caches, and pipelines requests to the UMLS REST metathesaurus API',
+	url='http://github.com/aduriseti/umls_request_manager',
+	author='Amal Duriseti',
+	author_email='aduriseti@gmail.com',
+	license='MIT',
+	packages=['umls_request_manager'],
+	install_requires=[
+		'requests',
+		'grequests',
+		'pyquery',
+		'lxml',
+	],
+	zip_safe=False)
---
+++ packages/umls_request_manager/umls_request_manager-0.5.tar.gz/umls_request_manager-0.5/umls_request_manager/__init__.py
@@ -0,0 +1,3 @@
+from .umls_request_manager import *
+from .umls_auth import *
+
