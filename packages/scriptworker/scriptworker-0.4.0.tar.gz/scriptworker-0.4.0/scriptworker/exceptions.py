5bbb92fe51bd15a432dc2907db90b6ba87eb1cf1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:24 +0000

    Add package scriptworker/0.4.0/scriptworker-0.4.0.tar.gz

---
+++ packages/scriptworker/scriptworker-0.4.0.tar.gz/scriptworker-0.4.0/scriptworker/exceptions.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+"""Exceptions
+"""
+
+
+class ScriptWorkerException(Exception):
+    pass
+
+
+class ScriptWorkerRetryException(ScriptWorkerException):
+    pass
+
+
+class ScriptWorkerTaskException(ScriptWorkerException):
+    """
+    To use::
+
+        import sys
+        import traceback
+        try:
+            ...
+        except ScriptWorkerTaskException as exc:
+            traceback.print_exc()
+            sys.exit(exc.exit_code)
+
+    """
+    def __init__(self, *args, exit_code=1, **kwargs):
+        self.exit_code = exit_code
+        super(ScriptWorkerTaskException, self).__init__(*args, **kwargs)
