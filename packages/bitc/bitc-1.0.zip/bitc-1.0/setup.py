f6976a6547d5285e62cbb3beacaf46da4833eca3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:22:58 +0000

    Add package bitc/1.0/bitc-1.0.zip

---
+++ packages/bitc/bitc-1.0.zip/bitc-1.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='bitc',
+      version='1.0',
+      author="Kieran O'Leary",
+      author_email="kieran.o.leary@gmail.com",
+      url='https://github.com/kieranjol/ifiscripts/',
+      download_url='https://github.com/kieranjol/IFIscripts/archive/0.2.zip',
+      py_modules=['bitc'],
+      )
