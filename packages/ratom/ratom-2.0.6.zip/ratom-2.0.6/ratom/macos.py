993bfe2e8b41f0c8478fb0bb84be08deaa724189
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:02 +0000

    Add package ratom/2.0.6/ratom-2.0.6.zip

---
+++ packages/ratom/ratom-2.0.6.zip/ratom-2.0.6/ratom/macos.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+"""update macOS"""
+
+# File: ratom/macos.py
+# Version: 2.0.6
+# Date: 2016-08-05
+# Author: qtfkwk <qtfkwk+ratom@gmail.com>
+# Copyright: (C) 2016 by qtfkwk
+# License: BSD 2-Clause License (https://opensource.org/licenses/BSD-2-Clause)
+
+from common import *
+
+def check():
+    """check if can update macOS"""
+    return has('softwareupdate')
+
+def main(argv=None, cfg=None):
+    """update macOS"""
+    cfg = init(argv, cfg)
+    info('macos: started')
+    if not check():
+        info('macos: failed check')
+        return
+    section('macOS', 'sudo softwareupdate -iav', dryrun=cfg['dryrun'])
+    info('macos: finished')
+
+if __name__ == '__main__':
+    main()
+
