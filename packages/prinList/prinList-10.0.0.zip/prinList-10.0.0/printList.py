bf1c78d9d1192286ffd24bb9800f4a943be5a6ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:23:25 +0000

    Add package prinList/10.0.0/prinList-10.0.0.zip

---
+++ packages/prinList/prinList-10.0.0.zip/prinList-10.0.0/printList.py
@@ -0,0 +1,6 @@
+def printList (the_list) :
+	for each_item in the_list:
+		if isinstance(each_item,list):
+			printList(each_item)
+		else :
+			print (each_item)
---
+++ packages/prinList/prinList-10.0.0.zip/prinList-10.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup (
+		name			='prinList',
+		version			='10.0.0',
+		py_modules		=['printList'],
+		author			='lanlinxiapython',
+		author_email	='18618102357@163.com',
+		url				='http://www.headfirstLables.com',
+		description		='test'
+	   )
