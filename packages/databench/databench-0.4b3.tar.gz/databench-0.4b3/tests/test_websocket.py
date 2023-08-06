bd9601e3d92a9552327df1c5548072efc4528e1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:01 +0000

    Add package databench/0.4b3/databench-0.4b3.tar.gz

---
+++ packages/databench/databench-0.4b3.tar.gz/databench-0.4b3/tests/test_websocket.py
@@ -0,0 +1,67 @@
+"""Websocket test."""
+
+import databench
+import tornado.testing
+
+
+class Basics(object):
+    ANALYSIS = None
+
+    def test_index(self):
+        response = self.fetch('/')
+        self.assertEqual(response.code, 200)
+        self.assertIn(b'Dummy', response.body)
+
+    @tornado.testing.gen_test
+    def test_connect(self):
+        c = yield self.ws_connect(self.analysis)
+        yield c.close()
+        self.assertEqual(len(c.analysis_id), 8)
+
+
+class BasicsDummypi(Basics, databench.AnalysisTestCase):
+    analysis = 'dummypi'
+
+
+class BasicsDummypiPy(Basics, databench.AnalysisTestCase):
+    analysis = 'dummypi_py'
+
+
+class BasicsDummypiSSL(Basics, databench.AnalysisTestCaseSSL):
+    analysis = 'dummypi'
+
+
+class BasicsDummypiPySSL(Basics, databench.AnalysisTestCaseSSL):
+    analysis = 'dummypi_py'
+
+
+class BasicsTestAnalyses(databench.AnalysisTestCase):
+    analyses_path = 'tests.analyses'
+
+    def test_index(self):
+        response = self.fetch('/')
+        self.assertEqual(response.code, 200)
+        self.assertIn(b'simple1', response.body)
+
+    def test_static_file(self):
+        response = self.fetch('/static/test_file.txt')
+        self.assertEqual(response.code, 200)
+        self.assertIn(b'placeholder', response.body)
+
+    def test_node_modules_file(self):
+        response = self.fetch('/node_modules/test_file.txt')
+        self.assertEqual(response.code, 200)
+        self.assertIn(b'placeholder', response.body)
+
+    @tornado.testing.gen_test
+    def test_connection_interruption(self):
+        connection1 = yield self.ws_connect('connection_interruption')
+        yield connection1.close()
+        analysis_id1 = connection1.analysis_id
+        self.assertEqual(len(analysis_id1), 8)
+
+        connection2 = yield self.ws_connect('connection_interruption',
+                                            analysis_id1)
+        yield connection2.close()
+        analysis_id2 = connection2.analysis_id
+        self.assertEqual(analysis_id1, analysis_id2)
