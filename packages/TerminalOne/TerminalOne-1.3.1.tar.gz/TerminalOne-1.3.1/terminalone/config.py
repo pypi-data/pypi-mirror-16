6555b967b3867b0a35b547a6672f242353783430
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:22:51 +0000

    Add package TerminalOne/1.3.1/TerminalOne-1.3.1.tar.gz

---
+++ packages/TerminalOne/TerminalOne-1.3.1.tar.gz/TerminalOne-1.3.1/terminalone/config.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""Provides configuration data"""
+
+VALID_ENVS = frozenset(['production', 'qa', 'sandbox'])
+
+API_BASES = {
+    'production': 'api.mediamath.com',
+    'sandbox': 't1sandbox.mediamath.com',
+}
+
+ACCEPT_HEADERS = {
+    'json': 'application/vnd.mediamath.v1+json',
+}
+
+PATHS = {
+    'mgmt': 'api/v2.0',
+    'reports': 'reporting/v1/std',
+    'uniques': 'uniques/v1',
+    'oauth2': 'oauth2/v1.0',
+}
