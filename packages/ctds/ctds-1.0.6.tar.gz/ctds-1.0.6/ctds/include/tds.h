1c3c3ac29919b912a85a5c26b9de7b96e74ef851
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:23:42 +0000

    Add package ctds/1.0.6/ctds-1.0.6.tar.gz

---
+++ packages/ctds/ctds-1.0.6.tar.gz/ctds-1.0.6/ctds/include/tds.h
@@ -0,0 +1,26 @@
+#ifndef __TDS_H__
+#define __TDS_H__
+
+#include "push_warnings.h"
+#include <Python.h>
+#include "pop_warnings.h"
+
+#include <stdlib.h>
+
+extern PyObject* PyExc_tds_Warning;
+extern PyObject* PyExc_tds_Error;
+extern PyObject* PyExc_tds_InterfaceError;
+extern PyObject* PyExc_tds_DatabaseError;
+extern PyObject* PyExc_tds_DataError;
+extern PyObject* PyExc_tds_OperationalError;
+extern PyObject* PyExc_tds_IntegrityError;
+extern PyObject* PyExc_tds_InternalError;
+extern PyObject* PyExc_tds_ProgrammingError;
+extern PyObject* PyExc_tds_NotSupportedError;
+
+#define tds_mem_malloc malloc
+#define tds_mem_realloc realloc
+#define tds_mem_calloc calloc
+#define tds_mem_free free
+
+#endif /* ifndef __TDS_H__ */
