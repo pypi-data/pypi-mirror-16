63cb3f442ab10696bb0e2af982913ebea9bff648
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:22:22 +0000

    Add package islist16/1.1.6/islist16-1.1.6.zip

---
+++ packages/islist16/islist16-1.1.6.zip/islist16-1.1.6/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name    =   'islist16',
+    version =   '1.1.6',
+    py_modules = ['islist16'],
+    author = 'hf',
+    author_email ='',
+    url ='',
+    description ='is list by yyx',
+    )
