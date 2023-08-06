2713701e963c329677980ef534ddc138ed348720
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:18:43 +0000

    Add package whatever-forever/0.0.13/whatever_forever-0.0.13-py2.py3-none-any.whl

---
+++ packages/whatever-forever/whatever_forever-0.0.13-py2.py3-none-any.whl/whatever/__init__.py
@@ -0,0 +1,22 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+__version_info__ = (0, 0, 13)
+__version__ = '.'.join(map(str, __version_info__))
+
+from .chain import Chain, this, _X
+from .magic import magical
+from .dispatch import Dispatch
+from toolz.curried import *
+import toolz.curried
+
+__all__ = [
+    'magical', 'Chain', 'this', 'Dispatch', '_X', *pipe(
+        toolz.curried, dir, filter(
+            complement(lambda s: s.startswith('_'))
+        ), list
+    )
+]
+
