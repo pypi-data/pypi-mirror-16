55ab9746e976339f1f5bf0a033c58dd3e54c3209
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:05 +0000

    Add package pinax-news/1.0.0/pinax-news-1.0.0.tar.gz

---
+++ packages/pinax-news/pinax-news-1.0.0.tar.gz/pinax-news-1.0.0/pinax/news/apps.py
@@ -0,0 +1,9 @@
+from django.apps import AppConfig as BaseAppConfig
+from django.utils.translation import ugettext_lazy as _
+
+
+class AppConfig(BaseAppConfig):
+
+    name = "pinax.news"
+    label = "pinax_news"
+    verbose_name = _("Pinax News")
