1807f6c3b891460b21eeeba656cfd0842c54de0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:04 +0000

    Add package pyflux/0.3.5/pyflux-0.3.5.tar.gz

---
+++ packages/pyflux/pyflux-0.3.5.tar.gz/pyflux-0.3.5/pyflux/gas/__init__.py
@@ -0,0 +1,8 @@
+from .gas import GAS
+from .gasx import GASX
+from .gasmodels import GASPoisson, GASNormal, GASLaplace, GASt, GASSkewt, GASExponential
+from .gasllm import GASLLEV
+from .gasllt import GASLLT
+from .gasreg import GASReg
+
+from .scores import BetatScore, SkewtScore
---
+++ packages/pyflux/pyflux-0.3.5.tar.gz/pyflux-0.3.5/pyflux/ssm/__init__.py
@@ -0,0 +1,6 @@
+from .llm import LLEV
+from .llt import LLT
+from .nllm import NLLEV
+from .nllt import NLLT
+from .dynlin import DynReg
+from .ndynlin import NDynReg
