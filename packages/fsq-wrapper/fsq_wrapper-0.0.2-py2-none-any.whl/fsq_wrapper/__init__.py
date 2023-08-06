194ffb57cf7f3031dec7d3d473e01514350a5f67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:01 +0000

    Add package fsq-wrapper/0.0.2/fsq_wrapper-0.0.2-py2-none-any.whl

---
+++ packages/fsq-wrapper/fsq_wrapper-0.0.2-py2-none-any.whl/fsq_wrapper/__init__.py
@@ -0,0 +1,29 @@
+import argparse
+import webbrowser
+import requests
+from api_functions import *
+
+def main():
+    parser = argparse.ArgumentParser(description="Wrapper around the foursquare API. Use it to retrieve information about\
+                                     specific venues or groups of venues.")
+    parser.add_argument("endpoint", help="Name of the resource you want to access", choices=["categories", "trending",\
+                                                                                             "explore", "search"])
+    parser.add_argument("client_id", help="Your Foursquare Client ID")
+    parser.add_argument("client_secret", help="Your Foursquare Client Secret")
+    args = parser.parse_args()
+
+    payload = {
+        "v": 20160612,
+        "m": "foursquare",
+        "client_id": args.client_id,
+        "client_secret": args.client_secret
+    }
+
+    if args.endpoint == "categories":
+        get_categories(payload)
+    elif args.endpoint == "search":
+        search_venues(payload)
+    elif args.endpoint == "trending":
+        get_trending_venues(payload)
+    elif args.endpoint == "explore":
+        explore_venues(payload)
