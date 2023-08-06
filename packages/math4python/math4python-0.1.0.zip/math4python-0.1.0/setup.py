63daa41be7ca10845b8307ed6dc99f477916f624
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:57:33 +0000

    Add package math4python/0.1.0/math4python-0.1.0.zip

---
+++ packages/math4python/math4python-0.1.0.zip/math4python-0.1.0/setup.py
@@ -0,0 +1,30 @@
+from distutils.core import setup
+
+setup(
+    # Application name:
+    name="math4python",
+
+    # Version number (initial):
+    version="0.1.0",
+
+    # Application author details:
+    author="khanh ngo",
+    author_email="ngoduykhanh.fami@gmail.com",
+
+    # Packages
+    packages=["math4python"],
+
+    # Include additional files into the package
+    include_package_data=True,
+
+    # Details
+    url="http://pypi.python.org/pypi/math4python/0.1.0",
+
+    # license="LICENSE.txt",
+    description="Capy Math Library for Python",
+
+    # long_description=open("README.txt").read(),
+
+    # Dependent packages (distributions)
+
+)
