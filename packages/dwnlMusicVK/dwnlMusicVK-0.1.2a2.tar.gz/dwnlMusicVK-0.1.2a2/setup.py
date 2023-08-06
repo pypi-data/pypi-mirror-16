a7744dbcac0cd1622aac9e0c1db1d76bcc4b42c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:45 +0000

    Add package dwnlMusicVK/0.1.2a2/dwnlMusicVK-0.1.2a2.tar.gz

---
+++ packages/dwnlMusicVK/dwnlMusicVK-0.1.2a2.tar.gz/dwnlMusicVK-0.1.2a2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+from os.path import join, dirname
+import downloadMusicVK
+
+setup(
+    name='dwnlMusicVK',
+    version=downloadMusicVK.__version__,
+    packages=find_packages(),
+    license='GNU',
+    author='Akanoi',
+    url=r'https://github.com/akanoi/DownloadMusicVK',
+    description='Download all your music VK.',
+    entry_points={
+        'console_scripts':
+            ['musicvk = downloadMusicVK.downloadMusic:main']
+    }
+)
