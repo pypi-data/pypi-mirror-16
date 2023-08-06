6a91092856d439bd65f19f01dc6fe84ae64072c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:52 +0000

    Add package nester_roy/1.3.0/nester_roy-1.3.0.zip

---
+++ packages/nester_roy/nester_roy-1.3.0.zip/nester_roy-1.3.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name    = 'nester_roy',
+        version  = '1.3.0',
+        py_modules = ['nester_roy'],
+        author = 'RoyMeng',
+        author_email = 'melodyless@126.com',
+        url = 'http://www.126.com',
+        description = 'A simple printer of nested lists',
+    )
