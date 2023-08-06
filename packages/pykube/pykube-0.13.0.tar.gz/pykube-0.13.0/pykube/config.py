0ab7bdc6eac272d3b9dbcacafd100f077e22612c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:16:25 +0000

    Add package pykube/0.13.0/pykube-0.13.0.tar.gz

---
+++ packages/pykube/pykube-0.13.0.tar.gz/pykube-0.13.0/pykube/config.py
@@ -0,0 +1,235 @@
+"""
+Configuration code.
+"""
+
+import base64
+import copy
+import tempfile
+import os
+
+import yaml
+
+from pykube import exceptions
+
+
+class KubeConfig(object):
+    """
+    Main configuration class.
+    """
+
+    @classmethod
+    def from_service_account(cls, path="/var/run/secrets/kubernetes.io/serviceaccount"):
+        with open(os.path.join(path, "token")) as fp:
+            token = fp.read()
+        host = os.environ.get("PYKUBE_KUBERNETES_SERVICE_HOST")
+        if host is None:
+            host = os.environ["KUBERNETES_SERVICE_HOST"]
+        port = os.environ.get("PYKUBE_KUBERNETES_SERVICE_PORT")
+        if port is None:
+            port = os.environ["KUBERNETES_SERVICE_PORT"]
+        doc = {
+            "clusters": [
+                {
+                    "name": "self",
+                    "cluster": {
+                        "server": "https://{}:{}".format(host, port),
+                        "certificate-authority": os.path.join(path, "ca.crt"),
+                    },
+                },
+            ],
+            "users": [
+                {
+                    "name": "self",
+                    "user": {
+                        "token": token,
+                    },
+                },
+            ],
+            "contexts": [
+                {
+                    "name": "self",
+                    "context": {
+                        "cluster": "self",
+                        "user": "self",
+                    },
+                }
+            ],
+            "current-context": "self",
+        }
+        self = cls(doc)
+        return self
+
+    @classmethod
+    def from_file(cls, filename):
+        """
+        Creates an instance of the KubeConfig class from a kubeconfig file.
+
+        :Parameters:
+           - `filename`: The full path to the configuration file
+        """
+        filename = os.path.expanduser(filename)
+        if not os.path.isfile(filename):
+            raise exceptions.PyKubeError("Configuration file {} not found".format(filename))
+        with open(filename) as f:
+            doc = yaml.safe_load(f.read())
+        self = cls(doc)
+        self.filename = filename
+        return self
+
+    @classmethod
+    def from_url(cls, url):
+        """
+        Creates an instance of the KubeConfig class from a single URL (useful
+        for interacting with kubectl proxy).
+        """
+        doc = {
+            "clusters": [
+                {
+                    "name": "self",
+                    "cluster": {
+                        "server": url,
+                    },
+                },
+            ],
+            "contexts": [
+                {
+                    "name": "self",
+                    "context": {
+                        "cluster": "self",
+                    },
+                }
+            ],
+            "current-context": "self",
+        }
+        self = cls(doc)
+        return self
+
+    def __init__(self, doc):
+        """
+        Creates an instance of the KubeConfig class.
+        """
+        self.doc = doc
+        self.current_context = None
+        if "current-context" in doc and doc["current-context"]:
+            self.set_current_context(doc["current-context"])
+
+    def set_current_context(self, value):
+        """
+        Sets the context to the provided value.
+
+        :Parameters:
+           - `value`: The value for the current context
+        """
+        self.current_context = value
+
+    @property
+    def clusters(self):
+        """
+        Returns known clusters by exposing as a read-only property.
+        """
+        if not hasattr(self, "_clusters"):
+            cs = {}
+            for cr in self.doc["clusters"]:
+                cs[cr["name"]] = c = copy.deepcopy(cr["cluster"])
+                if "server" not in c:
+                    c["server"] = "http://localhost"
+                BytesOrFile.maybe_set(c, "certificate-authority")
+            self._clusters = cs
+        return self._clusters
+
+    @property
+    def users(self):
+        """
+        Returns known users by exposing as a read-only property.
+        """
+        if not hasattr(self, "_users"):
+            us = {}
+            if "users" in self.doc:
+                for ur in self.doc["users"]:
+                    us[ur["name"]] = u = copy.deepcopy(ur["user"])
+                    BytesOrFile.maybe_set(u, "client-certificate")
+                    BytesOrFile.maybe_set(u, "client-key")
+            self._users = us
+        return self._users
+
+    @property
+    def contexts(self):
+        """
+        Returns known contexts by exposing as a read-only property.
+        """
+        if not hasattr(self, "_contexts"):
+            cs = {}
+            for cr in self.doc["contexts"]:
+                cs[cr["name"]] = copy.deepcopy(cr["context"])
+            self._contexts = cs
+        return self._contexts
+
+    @property
+    def cluster(self):
+        """
+        Returns the current selected cluster by exposing as a
+        read-only property.
+        """
+        if self.current_context is None:
+            raise exceptions.PyKubeError("current context not set; call set_current_context")
+        return self.clusters[self.contexts[self.current_context]["cluster"]]
+
+    @property
+    def user(self):
+        """
+        Returns the current user by exposing as a read-only property.
+        """
+        if self.current_context is None:
+            raise exceptions.PyKubeError("current context not set; call set_current_context")
+        return self.users.get(self.contexts[self.current_context].get("user", ""), {})
+
+
+class BytesOrFile(object):
+    """
+    Implements the same interface for files and byte input.
+    """
+
+    @classmethod
+    def maybe_set(cls, d, key):
+        file_key = key
+        data_key = "{}-data".format(key)
+        if data_key in d:
+            d[file_key] = cls(d[data_key])
+            del d[data_key]
+        elif file_key in d:
+            d[file_key] = cls(d[file_key])
+
+    def __init__(self, data):
+        """
+        Creates a new instance of BytesOrFile.
+
+        :Parameters:
+           - `data`: A full path to a file or base64 encoded bytes
+        """
+        self._filename = None
+        self._bytes = None
+        if os.path.isfile(data):
+            self._filename = data
+        else:
+            self._bytes = base64.b64decode(data)
+
+    def bytes(self):
+        """
+        Returns the provided data as bytes.
+        """
+        if self._filename:
+            with open(self._filename, "rb") as f:
+                return f.read()
+        else:
+            return self._bytes
+
+    def filename(self):
+        """
+        Returns the provided data as a file location.
+        """
+        if self._filename:
+            return self._filename
+        else:
+            with tempfile.NamedTemporaryFile(delete=False) as f:
+                f.write(self._bytes)
+            return f.name
---
+++ packages/pykube/pykube-0.13.0.tar.gz/pykube-0.13.0/pykube/http.py
@@ -0,0 +1,187 @@
+"""
+HTTP request related code.
+"""
+
+import posixpath
+import re
+import sys
+import warnings
+import requests
+
+from six.moves.urllib.parse import urlparse
+
+from .exceptions import HTTPError
+
+
+_ipv4_re = re.compile(r"^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?).){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$")
+
+
+class HTTPClient(object):
+    """
+    Client for interfacing with the Kubernetes API.
+    """
+
+    def __init__(self, config):
+        """
+        Creates a new instance of the HTTPClient.
+
+        :Parameters:
+           - `config`: The configuration instance
+        """
+        self.config = config
+        self.url = self.config.cluster["server"]
+        self.session = self.build_session()
+
+    @property
+    def url(self):
+        return self._url
+
+    @url.setter
+    def url(self, value):
+        pr = urlparse(value)
+        if sys.version_info < (3, 5) and ("::" in pr.hostname or _ipv4_re.match(pr.hostname)):
+            warnings.warn("IP address hostnames are not supported with Python < 3.5. Please see https://github.com/kelproject/pykube/issues/29 for more info.", RuntimeWarning)
+        self._url = pr.geturl()
+
+    def build_session(self):
+        """
+        Creates a new session for the client.
+        """
+        s = requests.Session()
+        if "certificate-authority" in self.config.cluster:
+            s.verify = self.config.cluster["certificate-authority"].filename()
+        elif "insecure-skip-tls-verify" in self.config.cluster:
+            s.verify = not self.config.cluster["insecure-skip-tls-verify"]
+        if "token" in self.config.user and self.config.user["token"]:
+            s.headers["Authorization"] = "Bearer {}".format(self.config.user["token"])
+        elif "client-certificate" in self.config.user:
+            s.cert = (
+                self.config.user["client-certificate"].filename(),
+                self.config.user["client-key"].filename(),
+            )
+        else:  # no user present; don't configure anything
+            pass
+        return s
+
+    def get_kwargs(self, **kwargs):
+        """
+        Creates a full URL to request based on arguments.
+
+        :Parametes:
+           - `kwargs`: All keyword arguments to build a kubernetes API endpoint
+        """
+        version = kwargs.pop("version", "v1")
+        if version == "v1":
+            base = kwargs.pop("base", "/api")
+        elif "/" in version:
+            base = kwargs.pop("base", "/apis")
+        else:
+            if "base" not in kwargs:
+                raise TypeError("unknown API version; base kwarg must be specified.")
+            base = kwargs.pop("base")
+        bits = [base, version]
+        if "namespace" in kwargs:
+            namespace = kwargs.pop("namespace")
+            if namespace:
+                bits.extend([
+                    "namespaces",
+                    namespace,
+                ])
+        url = kwargs.get("url", "")
+        if url.startswith("/"):
+            url = url[1:]
+        bits.append(url)
+        kwargs["url"] = self.url + posixpath.join(*bits)
+        return kwargs
+
+    def raise_for_status(self, resp):
+        try:
+            resp.raise_for_status()
+        except Exception:
+            # attempt to provide a more specific exception based around what
+            # Kubernetes returned as the error.
+            if resp.headers["content-type"] == "application/json":
+                payload = resp.json()
+                if payload["kind"] == "Status":
+                    raise HTTPError(payload["message"])
+            raise
+
+    def request(self, *args, **kwargs):
+        """
+        Makes an API request based on arguments.
+
+        :Parameters:
+           - `args`: Non-keyword arguments
+           - `kwargs`: Keyword arguments
+        """
+        return self.session.request(*args, **self.get_kwargs(**kwargs))
+
+    def get(self, *args, **kwargs):
+        """
+        Executes an HTTP GET.
+
+        :Parameters:
+           - `args`: Non-keyword arguments
+           - `kwargs`: Keyword arguments
+        """
+        return self.session.get(*args, **self.get_kwargs(**kwargs))
+
+    def options(self, *args, **kwargs):
+        """
+        Executes an HTTP OPTIONS.
+
+        :Parameters:
+           - `args`: Non-keyword arguments
+           - `kwargs`: Keyword arguments
+        """
+        return self.session.options(*args, **self.get_kwargs(**kwargs))
+
+    def head(self, *args, **kwargs):
+        """
+        Executes an HTTP HEAD.
+
+        :Parameters:
+           - `args`: Non-keyword arguments
+           - `kwargs`: Keyword arguments
+        """
+        return self.session.head(*args, **self.get_kwargs(**kwargs))
+
+    def post(self, *args, **kwargs):
+        """
+        Executes an HTTP POST.
+
+        :Parameters:
+           - `args`: Non-keyword arguments
+           - `kwargs`: Keyword arguments
+        """
+        return self.session.post(*args, **self.get_kwargs(**kwargs))
+
+    def put(self, *args, **kwargs):
+        """
+        Executes an HTTP PUT.
+
+        :Parameters:
+           - `args`: Non-keyword arguments
+           - `kwargs`: Keyword arguments
+        """
+        return self.session.put(*args, **self.get_kwargs(**kwargs))
+
+    def patch(self, *args, **kwargs):
+        """
+        Executes an HTTP PATCH.
+
+        :Parameters:
+           - `args`: Non-keyword arguments
+           - `kwargs`: Keyword arguments
+        """
+        return self.session.patch(*args, **self.get_kwargs(**kwargs))
+
+    def delete(self, *args, **kwargs):
+        """
+        Executes an HTTP DELETE.
+
+        :Parameters:
+           - `args`: Non-keyword arguments
+           - `kwargs`: Keyword arguments
+        """
+        return self.session.delete(*args, **self.get_kwargs(**kwargs))
