77cd09d5dedec616413bd99b7ffa57d5b9634e1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:22 +0000

    Add package inspektor/0.2.2/inspektor-0.2.2.tar.gz

---
+++ packages/inspektor/inspektor-0.2.2.tar.gz/inspektor-0.2.2/inspektor/cli/app.py
@@ -0,0 +1,64 @@
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+# See LICENSE for more details.
+#
+# Copyright: Red Hat 2013-2014
+# Author: Lucas Meneghel Rodrigues <lmr@redhat.com>
+
+"""
+Implements the base inspektor application.
+"""
+import logging
+from argparse import ArgumentParser
+
+from inspektor import lint
+from inspektor import reindent
+from inspektor import style
+from inspektor import check
+from inspektor import license
+
+log = logging.getLogger("inspektor.app")
+
+ERROR_INTERRUPTED = 3
+
+
+class InspektorApp(object):
+
+    """
+    Basic inspektor application.
+    """
+
+    def __init__(self):
+        self.arg_parser = ArgumentParser(description='Inspektor code check')
+        self.arg_parser.add_argument('-v', '--verbose', action='store_true',
+                                     help=('print extra debug messages '
+                                           '(alters behavior of the lint '
+                                           'subcommand)'),
+                                     dest='verbose')
+        self.arg_parser.add_argument('--exclude', type=str,
+                                     help='Quoted string containing paths or '
+                                          'patterns to be excluded from '
+                                          'checking, comma separated')
+        subparsers = self.arg_parser.add_subparsers(title='subcommands',
+                                                    description='valid subcommands',
+                                                    help='subcommand help')
+        lint.set_arguments(subparsers)
+        reindent.set_arguments(subparsers)
+        style.set_arguments(subparsers)
+        check.set_arguments(subparsers)
+        license.set_arguments(subparsers)
+        self.args = self.arg_parser.parse_args()
+
+    def run(self):
+        try:
+            return self.args.func(self.args)
+        except KeyboardInterrupt:
+            log.error('User pressed Ctrl+C, exiting...')
+            return ERROR_INTERRUPTED
