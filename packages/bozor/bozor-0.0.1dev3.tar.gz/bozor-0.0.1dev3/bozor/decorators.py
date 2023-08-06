b8625821be240ccfd2b15203b64252db595c5dff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:10 +0000

    Add package bozor/0.0.1dev3/bozor-0.0.1dev3.tar.gz

---
+++ packages/bozor/bozor-0.0.1dev3.tar.gz/bozor-0.0.1dev3/bozor/decorators.py
@@ -0,0 +1,10 @@
+"""
+.. module: bozor.decorators
+    :platform: Unix
+    :copyright: (c) 2015 by Netflix Inc., see AUTHORS for more
+    :license: Apache, see LICENSE for more details.
+.. moduleauthor:: Patrick Kelley <patrick@netflix.com>
+"""
+import functools
+
+# Decorators go here
