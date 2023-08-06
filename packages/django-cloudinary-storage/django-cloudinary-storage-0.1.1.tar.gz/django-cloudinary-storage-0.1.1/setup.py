a45e938a07b4446a6a1e06a630e265acf58d9680
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:23:26 +0000

    Add package django-cloudinary-storage/0.1.1/django-cloudinary-storage-0.1.1.tar.gz

---
+++ packages/django-cloudinary-storage/django-cloudinary-storage-0.1.1.tar.gz/django-cloudinary-storage-0.1.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+description = ('Django package that provides Cloudinary storages for both media and static files '
+               'as well as management commands for removing unnecessary files.')
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(
+    name='django-cloudinary-storage',
+    version='0.1.1',
+    author='Konrad Lisiczynski',
+    author_email='klisiczynski@gmail.com',
+    description=description,
+    long_description=long_description,
+    licence='MIT',
+    url='https://github.com/klis87/django-cloudinary-storage',
+    keywords='django cloudinary storage',
+    packages=['cloudinary_storage'],
+    include_package_data=True,
+    install_requires=[
+        'requests>=2.10.0',
+        'cloudinary>=1.4.0'
+    ],
+    extras_require={
+        'video': ['python-magic>=0.4.12']
+    }
+)
