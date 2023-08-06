a8bfcfeb721404bfcd6a0c5885239c93da24cee6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:22:33 +0000

    Add package pyconsole/0.5/pyconsole-0.5.tar.gz

---
+++ packages/pyconsole/pyconsole-0.5.tar.gz/pyconsole-0.5/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name = "pyconsole",
+    packages = ["pyconsole", "tests"],
+    version = "0.5",
+    description = "An interactive customizeable shell",
+    license = 'MIT',
+    author = "Atmaram Shetye",
+    author_email = "atmaram.shetye@gmail.com",
+    url = "https://github.com/Atmaram/pyshell",
+    download_url = "https://github.com/Atmaram/pyshell/tarball/0.5",
+    keywords = ['terminal', 'console', 'shell', 'interactive'],
+    classifiers = [],
+)
