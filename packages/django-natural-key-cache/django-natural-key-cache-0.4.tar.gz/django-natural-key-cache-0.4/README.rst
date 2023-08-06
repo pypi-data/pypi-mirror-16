2aca0fdb858c1e88ccf62b45669ba42f5da217bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:13 +0000

    Add package django-natural-key-cache/0.4/django-natural-key-cache-0.4.tar.gz

---
+++ packages/django-natural-key-cache/django-natural-key-cache-0.4.tar.gz/django-natural-key-cache-0.4/README.rst
@@ -0,0 +1,61 @@
+Django Natural Key Cache
+========================
+
+|build-status| |coverage| |codecov| |deps| |docs| |pypi|
+
+Cache models via natural keys.
+
+.. |build-status| image:: http://img.shields.io/travis/shadowfax-chc/django-natural-key-cache/master.svg?style=flat
+    :alt: Build Status
+    :scale: 100%
+    :target: https://travis-ci.org/shadowfax-chc/django-natural-key-cache
+
+.. |coverage| image:: http://img.shields.io/coveralls/shadowfax-chc/django-natural-key-cache.svg?style=flat
+    :alt: Coveralls Status
+    :scale: 100%
+    :target: https://coveralls.io/r/shadowfax-chc/django-natural-key-cache?branch=master
+
+.. |codecov| image:: https://img.shields.io/codecov/c/github/shadowfax-chc/django-natural-key-cache/master.svg
+    :alt: Codecov Status
+    :scale: 100%
+    :target: https://codecov.io/gh/shadowfax-chc/django-natural-key-cache
+
+.. |deps| image:: http://img.shields.io/gemnasium/shadowfax-chc/django-natural-key-cache.svg?style=flat
+    :alt: Dependency Status
+    :scale: 100%
+    :target: https://gemnasium.com/shadowfax-chc/django-natural-key-cache
+
+.. |docs| image:: https://readthedocs.org/projects/django-natural-key-cache/badge/
+    :alt: Documentation Status
+    :scale: 100%
+    :target: http://django-natural-key-cache.readthedocs.org/en/latest/
+
+.. |pypi| image:: http://img.shields.io/pypi/v/django-natural-key-cache.svg?style=flat
+    :alt: PyPi version
+    :scale: 100%
+    :target: https://pypi.python.org/pypi/django-natural-key-cache
+
+
+Quick Start
+-----------
+
+Add manager to model.
+
+.. code-block:: python
+
+    from django.db import models
+    from natural_key_cache.cache_manager import NaturalKeyCacheManager
+
+    class MyModel(models.Model):
+        # Define natural keys, this can be ommited if id/pk is the natural key
+        natural_keys = (
+            'nat_key',
+        )
+        cache = NaturalKeyCacheManager(natural_keys)
+        nat_key = models.CharField(max_length=32, unique=True)
+
+Then access the cached model via the new manager.
+
+.. code-block:: python
+
+    MyModel.cache.get(nat_key='unique_instance')
---
+++ packages/django-natural-key-cache/django-natural-key-cache-0.4.tar.gz/django-natural-key-cache-0.4/setup.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+# vim: set et ts=4 sw=4 fileencoding=utf-8:
+'''
+setup script for natural_key_cache
+'''
+import os
+from setuptools import setup, find_packages, Command
+
+SETUP_DIRNAME = os.path.dirname(__file__)
+if SETUP_DIRNAME != '':
+    os.chdir(SETUP_DIRNAME)
+
+REQS_FILE = os.path.join(os.path.abspath(SETUP_DIRNAME), 'requirements.txt')
+REQUIREMENTS = []
+with open(REQS_FILE) as rfh:
+    for line in rfh.readlines():
+        if not line or line.startswith('#'):
+            continue
+        REQUIREMENTS.append(line.strip())
+
+
+class TestCommand(Command):
+    '''
+    Custom Test command.
+
+    Runs test suite for test django project
+    '''
+    user_options = []
+
+    def initialize_options(self):
+        ''' no options '''
+        pass
+
+    def finalize_options(self):
+        ''' no options '''
+        pass
+
+    def run(self):
+        ''' Run django project test suite '''
+        import sys, subprocess
+        raise SystemExit(
+            subprocess.call([
+                'coverage',
+                'run',
+                '--source=natural_key_cache',
+                'tests/manage.py',
+                'test',
+                '--verbosity=2',
+            ]))
+
+
+class CleanCommand(Command):
+    ''' Clean command '''
+    description = 'Clean dist/build directories'
+    user_options = []
+
+    def initialize_options(self):
+        ''' no options '''
+        pass
+
+    def finalize_options(self):
+        ''' no options '''
+
+    def run(self):
+        ''' clean things up '''
+        os.chdir(SETUP_DIRNAME)
+        os.system('rm -rf ./build ./dist *.egg-info')
+
+
+setup(
+    name='django-natural-key-cache',
+    version='0.4',
+    license='ISC License',
+    description='Cache manager for caching django models by natural keys',
+    long_description='Cache manager for caching django models by natural keys',
+    author='Timothy Messier',
+    author_email='tim.messier@gmail.com',
+    url='https://github.com/shadowfax-chc/django-natural-key-cache',
+    packages=find_packages(exclude=('tests', 'doc')),
+    include_package_data=True,
+    install_requires=REQUIREMENTS,
+    zip_safe=False,
+    keywords='django-natural-key-cache',
+    cmdclass={
+        'test': TestCommand,
+        'clean': CleanCommand,
+    },
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.7',
+    ],
+)
