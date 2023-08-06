6e744e7257fc51c921d57741dcb79ac62101bcfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:05:59 +0000

    Add package tdigest/0.4.0.2/tdigest-0.4.0.2.tar.gz

---
+++ packages/tdigest/tdigest-0.4.0.2.tar.gz/tdigest-0.4.0.2/setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+import os
+from distutils.core import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(name='tdigest',
+      version='0.4.0.2',
+      description='T-Digest data structure',
+      author='Cam Davidson-pilon',
+      author_email='cam.davidson.pilon@gmail.com',
+      url='https://github.com/CamDavidsonPilon/tdigest',
+      packages=['tdigest'],
+      long_description=read('README.md'),
+      install_requires=[
+          "bintrees",
+          "pyudorandom"
+      ],
+      classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Topic :: Scientific/Engineering",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        ],
+      license="MIT",
+      keywords='percentile, median, probabilistic data structure, quantitle, distributed, qdigest, tdigest, streaming, pyspark',
+      package_data={
+        "tdigest": [
+            "../README.md",
+            "../LICENSE.txt",
+            "../MANIFEST",
+        ]
+      },
+      extras_require = { 'tests': [ 'pytest', 'pytest-timeout', 'pytest-cov', 'numpy' ] },
+)
