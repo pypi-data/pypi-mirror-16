0db425737a49923fd2e7a227c3e437c53cce7096
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:07 +0000

    Add package releases/1.2.1/releases-1.2.1.tar.gz

---
+++ packages/releases/releases-1.2.1.tar.gz/releases-1.2.1/tasks.py
@@ -0,0 +1,17 @@
+from invocations import docs
+from invocations.testing import test, integration, watch_tests
+from invocations.packaging import release
+
+from invoke import Collection
+
+
+ns = Collection(test, integration, watch_tests, release, docs)
+ns.configure({
+    'tests': {
+        'package': 'releases',
+    },
+    'packaging': {
+        'sign': True,
+        'wheel': True,
+    },
+})
