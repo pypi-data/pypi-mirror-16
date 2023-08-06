8e711fb6bb759f14287d8ca2239ff975f7c5a66c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:19:46 +0000

    Add package s3bp/0.post.dev24/s3bp-0.post.dev24.tar.gz

---
+++ packages/s3bp/s3bp-0.post.dev24.tar.gz/s3bp-0.post.dev24/setup.py
@@ -0,0 +1,18 @@
+"""Setup file for the s3bp package."""
+
+from distutils.core import setup
+import versioneer
+
+setup(
+    name='s3bp',
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    description='Read and write Python objects from/to S3.',
+    author='Shay Palachy',
+    author_email='shaypal5@gmail.com',
+    url='https://github.com/shaypal5/s3bp',
+    # download_url='https://github.com/shaypal5/s3po/tarball/0.1.1',
+    packages=['s3bp'],
+    keywords=['pandas', 'dataframe', 's3'],
+    classifiers=[],
+)
