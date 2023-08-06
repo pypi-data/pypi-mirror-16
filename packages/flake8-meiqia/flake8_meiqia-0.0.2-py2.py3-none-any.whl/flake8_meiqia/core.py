723ddc7c7bd1fed5bcf4cb4b06d52a19d4b082c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:45 +0000

    Add package flake8-meiqia/0.0.2/flake8_meiqia-0.0.2-py2.py3-none-any.whl

---
+++ packages/flake8-meiqia/flake8_meiqia-0.0.2-py2.py3-none-any.whl/flake8_meiqia/core.py
@@ -0,0 +1,10 @@
+__version__ = '0.0.2'
+
+
+def flake8ext(f):
+    f.name = 'flake8_meiqia'
+    f.version = __version__
+    f.skip_on_py3 = False
+    if not hasattr(f, 'off_by_default'):
+        f.off_by_default = False
+    return f
