47515696d40f5485c32c5c391f7938858c64c55c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:49 +0000

    Add package databench/0.4b4/databench-0.4b4.tar.gz

---
+++ packages/databench/databench-0.4b4.tar.gz/databench-0.4b4/tests/analyses/parameters/analysis.py
@@ -0,0 +1,16 @@
+import databench
+
+
+class Parameters(databench.Analysis):
+
+    def on_test_fn(self, first_param, second_param=100):
+        """Echo params."""
+        self.emit('test_fn', (first_param, second_param))
+
+    def on_test_action(self):
+        """process an action without a message"""
+        self.emit('test_action_ack')
+
+    def on_test_data(self, key, value):
+        """process an action without a message"""
+        self.data[key] = value
---
+++ packages/databench/databench-0.4b4.tar.gz/databench-0.4b4/tests/test_multiple_connections.py
@@ -0,0 +1,28 @@
+from databench.testing import AnalysisTestCase
+import tornado.testing
+
+
+class MultipleConnections(object):
+
+    @tornado.testing.gen_test
+    def test_run(self):
+        connections = []
+
+        for _ in range(4):
+            connection = self.connection(self.analysis)
+            yield connection.connect()
+            connections.append(connection)
+
+        for connection in connections:
+            self.assertEqual(len(connection.analysis_id), 8)
+
+        for connection in connections:
+            yield connection.close()
+
+
+class MultipleDummypi(MultipleConnections, AnalysisTestCase):
+    analysis = 'dummypi'
+
+
+class MultipleDummypiPy(MultipleConnections, AnalysisTestCase):
+    analysis = 'dummypi_py'
