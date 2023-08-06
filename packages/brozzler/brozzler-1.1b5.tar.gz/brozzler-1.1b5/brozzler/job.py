fb79f2210650dfe50867a23bd8955c181a98de78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:57 +0000

    Add package brozzler/1.1b5/brozzler-1.1b5.tar.gz

---
+++ packages/brozzler/brozzler-1.1b5.tar.gz/brozzler-1.1b5/brozzler/job.py
@@ -0,0 +1,111 @@
+#
+# brozzler/job.py - Job class representing a brozzler crawl job, and functions
+# for setting up a job with supplied configuration
+#
+# Copyright (C) 2014-2016 Internet Archive
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import logging
+import brozzler
+import yaml
+import json
+import datetime
+import uuid
+import rethinkstuff
+
+def merge(a, b):
+    if isinstance(a, dict) and isinstance(b, dict):
+        merged = dict(a)
+        b_tmp = dict(b)
+        for k in a:
+            merged[k] = merge(a[k], b_tmp.pop(k, None))
+        merged.update(b_tmp)
+        return merged
+    elif isinstance(a, list) and isinstance(b, list):
+        return a + b
+    else:
+        return a
+
+def new_job_file(frontier, job_conf_file):
+    logging.info("loading %s", job_conf_file)
+    with open(job_conf_file) as f:
+        job_conf = yaml.load(f)
+        new_job(frontier, job_conf)
+
+def new_job(frontier, job_conf):
+    job = Job(
+            id=job_conf.get("id"), conf=job_conf, status="ACTIVE",
+            started=rethinkstuff.utcnow())
+
+    sites = []
+    for seed_conf in job_conf["seeds"]:
+        merged_conf = merge(seed_conf, job_conf)
+        # XXX check for unknown settings, invalid url, etc
+
+        site = brozzler.Site(
+                job_id=job.id, seed=merged_conf["url"],
+                scope=merged_conf.get("scope"),
+                time_limit=merged_conf.get("time_limit"),
+                proxy=merged_conf.get("proxy"),
+                ignore_robots=merged_conf.get("ignore_robots"),
+                enable_warcprox_features=merged_conf.get(
+                    "enable_warcprox_features"),
+                warcprox_meta=merged_conf.get("warcprox_meta"),
+                metadata=merged_conf.get("metadata"),
+                remember_outlinks=merged_conf.get("remember_outlinks"))
+        sites.append(site)
+
+    # insert all the sites into database before the job
+    for site in sites:
+        new_site(frontier, site)
+
+    frontier.new_job(job)
+
+def new_site(frontier, site):
+    site.id = str(uuid.uuid4())
+    logging.info("new site {}".format(site))
+    try:
+        # insert the Page into the database before the Site, to avoid situation
+        # where a brozzler worker immediately claims the site, finds no pages
+        # to crawl, and decides the site is finished
+        try:
+            if brozzler.is_permitted_by_robots(site, site.seed):
+                page = brozzler.Page(site.seed, site_id=site.id,
+                    job_id=site.job_id, hops_from_seed=0, priority=1000)
+                frontier.new_page(page)
+                logging.info("queued page %s", page)
+            else:
+                logging.warn("seed url {} is blocked by robots.txt".format(site.seed))
+        finally:
+            # finally block because we want to insert the Site no matter what
+            frontier.new_site(site)
+    except brozzler.ReachedLimit as e:
+        frontier.reached_limit(site, e)
+
+class Job(brozzler.BaseDictable):
+    logger = logging.getLogger(__module__ + "." + __qualname__)
+
+    def __init__(self, id=None, conf=None, status="ACTIVE", started=None,
+                 finished=None, stop_requested=None):
+        self.id = id
+        self.conf = conf
+        self.status = status
+        self.started = started
+        self.finished = finished
+        self.stop_requested = stop_requested
+
+    def __str__(self):
+        return 'Job(id=%s)' % self.id
+
---
+++ packages/brozzler/brozzler-1.1b5.tar.gz/brozzler-1.1b5/setup.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+'''
+setup.py - brozzler setup script
+
+Copyright (C) 2014-2016 Internet Archive
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+'''
+
+import setuptools
+import os
+
+def find_package_data(package):
+    pkg_data = []
+    depth = len(package.split('.'))
+    path = os.path.join(*package.split('.'))
+    for dirpath, dirnames, filenames in os.walk(path):
+        if not os.path.exists(os.path.join(dirpath, '__init__.py')):
+            relpath = os.path.join(*dirpath.split(os.sep)[depth:])
+            pkg_data.extend(os.path.join(relpath, f) for f in filenames)
+    return pkg_data
+
+setuptools.setup(
+        name='brozzler',
+        version='1.1b5',
+        description='Distributed web crawling with browsers',
+        url='https://github.com/internetarchive/brozzler',
+        author='Noah Levitt',
+        author_email='nlevitt@archive.org',
+        long_description=open('README.rst', encoding='UTF-8').read(),
+        license='Apache License 2.0',
+        packages=['brozzler', 'brozzler.webconsole'],
+        package_data={
+            'brozzler': ['behaviors.d/*.js*', 'behaviors.yaml'],
+            'brozzler.webconsole': find_package_data('brozzler.webconsole'),
+        },
+        entry_points={
+            'console_scripts': [
+                'brozzle-page=brozzler.cli:brozzle_page',
+                'brozzler-new-job=brozzler.cli:brozzler_new_job',
+                'brozzler-new-site=brozzler.cli:brozzler_new_site',
+                'brozzler-worker=brozzler.cli:brozzler_worker',
+                'brozzler-ensure-tables=brozzler.cli:brozzler_ensure_tables',
+                'brozzler-webconsole=brozzler.webconsole:run',
+                'brozzler-easy=brozzler.easy:main',
+            ],
+        },
+        install_requires=[
+            'PyYAML',
+            'youtube-dl',
+            'reppy',
+            'requests',
+            'websocket-client',
+            'pillow',
+            'surt>=0.3.0',
+            'rethinkstuff>=0.1.5',
+            'rethinkdb>=2.3,<2.4',
+            'psutil',
+        ],
+        extras_require={
+            'webconsole': ['flask>=0.11', 'gunicorn'],
+            'easy': ['warcprox>=2.0b1', 'pywb'],
+        },
+        zip_safe=False,
+        classifiers=[
+            'Development Status :: 4 - Beta',
+            'Environment :: Console',
+            'License :: OSI Approved :: Apache Software License',
+            'Programming Language :: Python :: 3.4',
+            'Topic :: Internet :: WWW/HTTP',
+            'Topic :: System :: Archiving',
+        ])
