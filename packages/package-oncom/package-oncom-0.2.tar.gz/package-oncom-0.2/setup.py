80348fde9d33ad594e80f80a0e063f64e71c124d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:19:55 +0000

    Add package package-oncom/0.2/package-oncom-0.2.tar.gz

---
+++ packages/package-oncom/package-oncom-0.2.tar.gz/package-oncom-0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='package-oncom',
+      version='0.2',
+      description='Cuma latihan upload package 1',
+      url='http://github.com/blahblah/package-oncom',
+      author='Yanzen',
+      author_email='yanzen@example.com',
+      license='MIT',
+      packages=['package_oncom'],
+      zip_safe=False)
