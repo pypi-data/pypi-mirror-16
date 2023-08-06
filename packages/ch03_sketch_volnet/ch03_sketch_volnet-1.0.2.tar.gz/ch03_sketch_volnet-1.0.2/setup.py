664434ae6e52be2c736106ea30b447246707f09e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:17 +0000

    Add package ch03_sketch_volnet/1.0.2/ch03_sketch_volnet-1.0.2.tar.gz

---
+++ packages/ch03_sketch_volnet/ch03_sketch_volnet-1.0.2.tar.gz/ch03_sketch_volnet-1.0.2/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'ch03_sketch_volnet',
+    version = '1.0.2',
+    py_modules = ['sketch'],
+    author = 'volnet',
+    author_email = 'volnet@tom.com',
+    url = 'http://volnet.github.io',
+    description='learn how to open file',
+)
