389ef90692de116048e7a0cd104daf2b4fe9c8ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:59 +0000

    Add package chargehound/1.2.0/chargehound-1.2.0.tar.gz

---
+++ packages/chargehound/chargehound-1.2.0.tar.gz/chargehound-1.2.0/README.md
@@ -0,0 +1,57 @@
+# Chargehound python bindings 
+
+[![Build Status](https://travis-ci.org/chargehound/chargehound-python.svg?branch=master)](https://travis-ci.org/chargehound/chargehound-python) [![PyPI version](https://badge.fury.io/py/chargehound.svg)](https://badge.fury.io/py/chargehound)
+
+## Installation
+
+`pip install chargehound`
+
+## Usage
+
+Import chargehound and set your API key.
+
+```python
+import chargehound
+chargehound.api_key = '{ YOUR_API_KEY }'
+```
+
+## Documentation
+
+[Disputes](https://www.chargehound.com/docs/api/index.html?python#disputes)
+
+[Errors](https://www.chargehound.com/docs/api/index.html?python#errors)
+
+## Development
+
+To build and install from the latest source:
+
+```bash
+$ git clone git@github.com:chargehound/chargehound-python.git
+$ pip install -r dev_requirements.txt
+```
+
+Run tests:
+
+```bash
+$ python setup.py test
+```
+
+## Deployment
+
+To deploy a new version of the SDK, you will need Pandoc (http://pandoc.org/) installed.
+Pandoc will convert the README.md into the .rst format required for the Python repository.
+Instructions are here: [http://pandoc.org/installing.html](http://pandoc.org/installing.html).
+
+Next, install PyPandoc, the Python wrapper for Pandoc, following [these instructions](https://pypi.python.org/pypi/pypandoc).
+ 
+The last pre-requisite is [Twine](https://pypi.python.org/pypi/twine), a utility for interacting with PyPi.
+
+Once Pandoc, PyPandoc and Twine are installed, you can build and deploy a new module to PyPi with the following steps:
+
+ 1. Update the CHANGELOG to describe what feature have been added.
+ 2. Bump the version number in `chargehound/version.py`
+ 3. Rebuild and deploy the package with:
+   ```python setup.py sdist```
+ 4. Upload the distributable to PyPi using:   
+   ```twine upload dist/{name_of_generated_package}```
+ 4. Confirm the new package version is available at [https://pypi.python.org/pypi/chargehound](https://pypi.python.org/pypi/chargehound)  
---
+++ packages/chargehound/chargehound-1.2.0.tar.gz/chargehound-1.2.0/test/test_disputes.py
@@ -0,0 +1,148 @@
+import chargehound
+import requests_mock
+import unittest2
+import json
+
+from chargehound.version import VERSION
+
+
+get_headers = {
+    'accept': 'application/json',
+    'authorization': 'Basic QVBJX0tFWTo=',
+    'user-agent': 'Chargehound/v1 PythonBindings/' + VERSION
+}
+
+post_headers = {
+    'accept': 'application/json',
+    'authorization': 'Basic QVBJX0tFWTo=',
+    'content-type': 'application/json',
+    'user-agent': 'Chargehound/v1 PythonBindings/' + VERSION
+}
+
+product_info = [{'name': 'Saxophone',
+                 'description': 'Alto saxophone, with carrying case',
+                 'image': 'http://s3.amazonaws.com/chargehound/saxophone.png',
+                 'sku': '17283001272',
+                 'quantity': 1,
+                 'amount': 20000,
+                 'url': 'http://www.example.com', },
+                {'name': 'Milk',
+                 'description': 'Semi-skimmed Organic',
+                 'image': 'http://s3.amazonaws.com/chargehound/milk.png',
+                 'sku': '26377382910',
+                 'quantity': '64oz',
+                 'amount': 400,
+                 'url': 'http://www.example.com'}]
+
+
+def is_json(response_body):
+    """Can this HTTP response be parsed as JSON?"""
+    try:
+        json.loads(response_body)
+        return True
+    except:
+        return False
+
+
+def json_has_structure(response_body, expected_json):
+    """Does the JSON this HTTP response match the expected structure?"""
+    actual_json = json.loads(response_body)
+
+    return actual_json == expected_json
+
+
+class DisputeTest(unittest2.TestCase):
+
+    def setUp(self):
+        super(DisputeTest, self).setUp()
+        chargehound.api_key = 'API_KEY'
+
+    @requests_mock.mock()
+    def test_retrieve_dispute(self, mock):
+        mock.get('https://api.chargehound.com/v1/disputes/dp_123',
+                 status_code=200,
+                 request_headers=get_headers,
+                 json={'id': 'dp_123'})
+        chargehound.Disputes.retrieve('dp_123')
+        assert mock.called
+
+    @requests_mock.mock()
+    def test_list_all_disputes(self, mock):
+        mock.get('https://api.chargehound.com/v1/disputes',
+                 status_code=200,
+                 request_headers=get_headers,
+                 json={'data': [{'id': 'dp_123'}]})
+        chargehound.Disputes.list()
+        assert mock.called
+
+    @requests_mock.mock()
+    def test_submit_dispute(self, mock):
+        mock.post('https://api.chargehound.com/v1/disputes/dp_123/submit',
+                  status_code=201,
+                  json={'id': 'dp_123'})
+        chargehound.Disputes.submit('dp_123',
+                                    fields={'customer_name': 'Susie'})
+
+        assert mock.called
+
+        response = mock.request_history[0].body
+
+        assert response
+        assert is_json(response)
+        assert json_has_structure(response,
+                                  {'fields': {'customer_name': 'Susie'}})
+
+    @requests_mock.mock()
+    def test_submit_dispute_with_product_info(self, mock):
+        mock.post('https://api.chargehound.com/v1/disputes/dp_123/submit',
+                  status_code=201,
+                  json={'id': 'dp_123'})
+        chargehound.Disputes.submit('dp_123',
+                                    fields={'customer_name': 'Susie'},
+                                    products=product_info)
+
+        assert mock.called
+
+        response = mock.request_history[0].body
+
+        assert response
+        assert is_json(response)
+        assert json_has_structure(response,
+                                  {'fields': {'customer_name': 'Susie'},
+                                   'products': product_info})
+
+    @requests_mock.mock()
+    def test_update_dispute(self, mock):
+        mock.post('https://api.chargehound.com/v1/disputes/dp_123',
+                  status_code=200,
+                  json={'id': 'dp_123'})
+        chargehound.Disputes.update('dp_123',
+                                    fields={'customer_name': 'Susie'})
+
+        assert mock.called
+
+        response = mock.request_history[0].body
+
+        assert response
+        assert is_json(response)
+        assert json_has_structure(response,
+                                  {'fields': {'customer_name': 'Susie'}})
+
+    @requests_mock.mock()
+    def test_update_dispute_with_product_info(self, mock):
+        mock.post('https://api.chargehound.com/v1/disputes/dp_123',
+                  status_code=200,
+                  json={'id': 'dp_123'})
+        chargehound.Disputes.update('dp_123',
+                                    fields={'customer_name': 'Susie'},
+                                    products=product_info)
+
+        assert mock.called
+
+        response = mock.request_history[0].body
+
+        assert response
+        assert is_json(response)
+        assert json_has_structure(response,
+                                  {'fields': {'customer_name': 'Susie'},
+                                   'products': product_info})
