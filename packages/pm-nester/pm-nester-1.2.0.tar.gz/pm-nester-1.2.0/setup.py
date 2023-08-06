49781bc16cb29d0b41bc900d4608740d7435ca2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:05:17 +0000

    Add package pm-nester/1.2.0/pm-nester-1.2.0.tar.gz

---
+++ packages/pm-nester/pm-nester-1.2.0.tar.gz/pm-nester-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'pm-nester',
+    version = '1.2.0',
+    py_modules = ['pm-nester'],
+    author = 'pmodali',
+    author_email = 'prasad.modali@yahoo.com',
+    url = '',
+    description = 'Simple printer of nested lists',
+    )
