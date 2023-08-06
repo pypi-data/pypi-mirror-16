db16af123fdd7c40f4c43ed9a598127778f7d111
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:18:43 +0000

    Add package django-iprestrict/0.4.4/django-iprestrict-0.4.4.tar.gz

---
+++ packages/django-iprestrict/django-iprestrict-0.4.4.tar.gz/django-iprestrict-0.4.4/docs/installation.rst
@@ -0,0 +1,31 @@
+Requirements and Installation
+=============================
+
+Requirements
+------------
+
+Runtime
+^^^^^^^
+
+* Django 1.7+
+* django-templatetag-handlebars 1.3.1+
+
+Development
+^^^^^^^^^^^
+
+In addition to Runtime requirements above:
+
+* sqlite3 (default) or any other RDBMS supported by Django
+* Sphinx (only to generate docs)
+
+Installation
+------------
+
+You can pip install from PyPI::
+
+    pip install django-iprestrict
+
+Alternately, you can select a release from Github and install that
+with pip::
+
+    pip install https://github.com/muccg/django-iprestrict/archive/0.4.1.zip
---
+++ packages/django-iprestrict/django-iprestrict-0.4.4.tar.gz/django-iprestrict-0.4.4/setup.py
@@ -0,0 +1,43 @@
+import os
+import re
+from setuptools import setup
+
+def get_package_version(package):
+    version = re.compile(r"(?:__)?version(?:__)?\s*=\s\"(.*)\"", re.I)
+    initfile = os.path.join(os.path.dirname(__file__), package, "__init__.py")
+    for line in open(initfile):
+        m = version.match(line)
+        if m:
+            return m.group(1)
+    return "UNKNOWN"
+
+setup(
+    name='django-iprestrict',
+    version=get_package_version("iprestrict"),
+    description='Django app + middleware to restrict access to all or sections of a Django project by client IP ranges',
+    long_description='Django app + middleware to restrict access to all or sections of a Django project by client IP ranges',
+    author='Tamas Szabo, CCG, Murdoch University',
+    author_email='devops@ccg.murdoch.edu.au',
+    url='https://github.com/muccg/django-iprestrict',
+    download_url='https://github.com/muccg/django-iprestrict/releases',
+    classifiers=[
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development"
+    ],
+    packages=[
+        'iprestrict',
+        'iprestrict.management',
+        'iprestrict.management.commands',
+        'iprestrict.migrations',
+    ],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'Django>=1.7',
+        'django-templatetag-handlebars',
+    ],
+    test_suite='tests.runtests.main',
+)
