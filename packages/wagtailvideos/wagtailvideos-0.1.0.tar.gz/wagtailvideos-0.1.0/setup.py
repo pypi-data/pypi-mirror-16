683084d5d95b9f95156584072f2e7f4a45ea720b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:06:52 +0000

    Add package wagtailvideos/0.1.0/wagtailvideos-0.1.0.tar.gz

---
+++ packages/wagtailvideos/wagtailvideos-0.1.0.tar.gz/wagtailvideos-0.1.0/setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+"""
+Install wagtailvideos using setuptools
+"""
+
+with open('README.md', 'r') as f:
+    readme = f.read()
+
+from setuptools import find_packages, setup
+
+setup(
+    name='wagtailvideos',
+    version='0.1.0',
+    description="A wagtail module for uploading and displaying videos in various codecs.",
+    long_description=readme,
+    author='Takeflight',
+    author_email='developers@takeflight.com.au',
+    url='https://github.com/takeflight/wagtailvideos',
+
+    install_requires=[
+        'wagtail>=1.4',
+        'django-enumchoicefield==0.6.0',
+    ],
+    zip_safe=False,
+    license='BSD License',
+
+    packages=find_packages(),
+
+    include_package_data=True,
+    package_data={},
+
+
+
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+        'License :: OSI Approved :: BSD License',
+    ],
+)
