493fb11ad443431330a15190e1a0a468258ff5ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:54 +0000

    Add package thorn/1.4.2/thorn-1.4.2-py2.py3-none-any.whl

---
+++ packages/thorn/thorn-1.4.2-py2.py3-none-any.whl/thorn/exceptions.py
@@ -0,0 +1,14 @@
+"""Thorn-related exceptions."""
+from __future__ import absolute_import, unicode_literals
+
+
+class ThornError(Exception):
+    pass
+
+
+class ImproperlyConfigured(ThornError):
+    pass
+
+
+class SecurityError(ThornError):
+    pass
