bff619b0eda4e96cac5695cf5d8a36883b2e0113
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:17 +0000

    Add package XStatic-angular-ui-router/0.3.1.2/XStatic-angular-ui-router-0.3.1.2.tar.gz

---
+++ packages/XStatic-angular-ui-router/XStatic-angular-ui-router-0.3.1.2.tar.gz/XStatic-angular-ui-router-0.3.1.2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+# The README.txt file should be written in reST so that PyPI can use
+# it to generate your project's PyPI page.
+long_description = open('README.txt').read()
+
+setup(
+    name='XStatic-angular-ui-router',
+    summary="""angular-ui-router 0.3.1 (XStatic packaging standard)""",
+    description=long_description,
+    maintainer="Richard Jones",
+    maintainer_email='r1chardj0n3s@gmail.com',
+    use_scm_version=True,
+    setup_requires=['setuptools_scm', 'wheel'],
+    packages=find_packages(),
+    include_package_data=True
+)
