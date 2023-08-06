87469e0d8f0707988420010288ff5d0455bac690
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:11 +0000

    Add package hanshu1/1.2.0/hanshu1-1.2.0.zip

---
+++ packages/hanshu1/hanshu1-1.2.0.zip/hanshu1-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'hanshu1',
+    version = '1.2.0',
+    py_modules = ['hanshu1'],
+    author = 'ndjdkb',
+    author_email = 'ndjdkb@outlook.com',
+    url = 'http://www.sina.com.cn',
+    description = 'a simple',
+     )
