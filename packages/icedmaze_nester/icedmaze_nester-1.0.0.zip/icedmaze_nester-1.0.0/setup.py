51dd4dbb934bd41815280e8226ef5570bcfa6587
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:31 +0000

    Add package icedmaze_nester/1.0.0/icedmaze_nester-1.0.0.zip

---
+++ packages/icedmaze_nester/icedmaze_nester-1.0.0.zip/icedmaze_nester-1.0.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(
+    name ='icedmaze_nester',
+    version='1.0.0',
+    py_modules=['icedmaze_nester'],
+    author='icedmaze',
+    author_email='1846011956@qq.com',
+    description='my head first test project'
+    )
