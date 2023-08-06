c9573edf06af5cf736cbb7000d419b7dc2d56148
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:40 +0000

    Add package python-pygaze/0.6.0a17/python-pygaze-0.6.0a17.tar.gz

---
+++ packages/python-pygaze/python-pygaze-0.6.0a17.tar.gz/python-pygaze-0.6.0a17/opensesame_plugins/pygaze_init/resources/ts/it_IT.ts
@@ -0,0 +1,144 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="2.0">
+<context>
+    <name>core_item_category</name>
+    <message>
+        <location filename="../../translatables-tmp.py" line="31"/>
+        <source>PyGaze</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context encoding="UTF-8">
+    <name>plugin_pygaze_init</name>
+    <message>
+        <location filename="../../translatables-tmp.py" line="3"/>
+        <source>Select tracker type</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../translatables-tmp.py" line="4"/>
+        <source>A list of supported eye trackers</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../translatables-tmp.py" line="5"/>
+        <source>Calibrate tracker</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../translatables-tmp.py" line="6"/>
+        <source>Indicates whether calibration should be started</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../translatables-tmp.py" line="7"/>
+        <source>Calibration beep</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../translatables-tmp.py" line="8"/>
+        <source>Play a beep when the calibration target jumps</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../translatables-tmp.py" line="9"/>
+        <source>Log file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../translatables-tmp.py" line="10"/>
+        <source>The name for the log file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../translatables-tmp.py" line="11"/>
+        <source>Saccade velocity threshold</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../translatables-tmp.py" line="12"/>
+        <source>Velocity threshold for saccade detection algorithm</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="../../translatables-tmp.py" line="13"/>
+        <source> °/s</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../translatables-tmp.py" line="14"/>
+        <source>Saccade acceleration threshold</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../translatables-tmp.py" line="15"/>
+        <source>Acceleration threshold for saccade detection algorithm</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="../../translatables-tmp.py" line="16"/>
+        <source> °/s/s</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../translatables-tmp.py" line="17"/>
+        <source>&lt;b&gt;Warning:&lt;/b&gt; PyLink is required for EyeLink functionality and is not installed. Visit http://osdoc.cogsci.nl/devices/eyelink for more information.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../translatables-tmp.py" line="18"/>
+        <source>DUMMY</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../translatables-tmp.py" line="19"/>
+        <source>Force drift correction (for EyeLink 1000)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../translatables-tmp.py" line="20"/>
+        <source>Turn on active drift correction</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../translatables-tmp.py" line="21"/>
+        <source>Pupil-size mode</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../translatables-tmp.py" line="22"/>
+        <source>Determines whether pupil size is recorded as area or diameter</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../translatables-tmp.py" line="23"/>
+        <source>SMI IP address</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../translatables-tmp.py" line="24"/>
+        <source>The tracker&apos;s IP address</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../translatables-tmp.py" line="25"/>
+        <source>SMI send-port number</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../translatables-tmp.py" line="26"/>
+        <source>Port number for sending messages to the SMI tracker</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../translatables-tmp.py" line="27"/>
+        <source>SMI receive-port number</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../translatables-tmp.py" line="28"/>
+        <source>Port number for receiving messages from the SMI tracker</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
---
+++ packages/python-pygaze/python-pygaze-0.6.0a17.tar.gz/python-pygaze-0.6.0a17/opensesame_plugins/pygaze_init/translate.pro
@@ -0,0 +1,10 @@
+CODECFORTR = UTF-8
+FORMS = 
+SOURCES = translatables-tmp.py
+TRANSLATIONS = resources/ts/fr_FR.ts \
+	resources/ts/de_DE.ts \
+	resources/ts/it_IT.ts \
+	resources/ts/zh_CN.ts \
+	resources/ts/ru_RU.ts \
+	resources/ts/es_ES.ts \
+	resources/ts/translatables.ts
