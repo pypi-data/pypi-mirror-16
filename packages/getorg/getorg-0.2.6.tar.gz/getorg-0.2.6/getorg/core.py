6300b6b34c6797cf5391e90f1fbd898f0ec54164
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:56 +0000

    Add package getorg/0.2.6/getorg-0.2.6.tar.gz

---
+++ packages/getorg/getorg-0.2.6.tar.gz/getorg-0.2.6/getorg/core.py
@@ -0,0 +1,21 @@
+getorg_version = "0.2.6"
+
+def handle_org_name_or_object(github_obj, org_name_or_object):
+    """
+    Takes in either an org name or Github Organization object, and returns
+    a Github Organization object.
+    """
+    import github
+    if isinstance(org_name_or_object, github.Organization.Organization):
+        org = org_name_or_object
+    elif isinstance(org_name_or_object, str):
+        org = github_obj.get_organization(org_name_or_object)
+    else:
+        error_str = "Must pass a github object or string. Passed a(n) " + str(type(org_name_or_object))
+        assert False, error_str
+
+    return org    
+
+def get_org_repos(github_obj,org_name_or_object):
+    org_obj = handle_org_name_or_object(github_obj, org_name_or_object)
+
