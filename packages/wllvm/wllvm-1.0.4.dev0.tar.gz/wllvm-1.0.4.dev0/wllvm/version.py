012d6f4f22f075d05ae70fb488456fa87059075d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:05 +0000

    Add package wllvm/1.0.4.dev0/wllvm-1.0.4.dev0.tar.gz

---
+++ packages/wllvm/wllvm-1.0.4.dev0.tar.gz/wllvm-1.0.4.dev0/wllvm/version.py
@@ -0,0 +1,24 @@
+# Freeping Creaturism:
+#
+# this is the all important version number used by pip.
+#
+#
+"""
+Version History:
+
+1.0.0   - 8/2/2016 initial birth as a pip package.
+
+1.0.1   - 8/2/2016 the rst gets a make over, and doc strings 
+          became more pervasive.
+
+1.0.2   - 8/2/2016 dragonegg issues. trying to include a polite 'as' wrapper
+          (i.e. not a  console_script called as).
+
+1.0.3   - 8/2/2016 travis build fixes.
+
+
+
+
+"""
+wllvm_version='1.0.4.dev0'
+
