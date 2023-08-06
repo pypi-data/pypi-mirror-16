687bd09d95ce2fb5787e379443459dd7f45a80fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:05 +0000

    Add package johansen/0.0.2/johansen-0.0.2.tar.gz

---
+++ packages/johansen/johansen-0.0.2.tar.gz/johansen-0.0.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(name='johansen',
+      version='0.0.2',
+      description='Python implementation of the Johansen test for cointegration',
+      author='Isaiah Yoo',
+      author_email='yoo.isaiah@gmail.com',
+      maintainer='Isaiah Yoo',
+      maintainer_email='yoo.isaiah@gmail.com',
+      license='MIT License',
+      url='https://github.com/https://github.com/iisayoo/johansen',
+      data_files=[("", ["README.md"])],
+      packages=find_packages(),
+      install_requires=[
+          'scipy>=0.18.0',
+          'statsmodels>=0.6.1',
+          'numpy>=1.11.1',
+          'pandas>=0.18.1',
+      ],
+      include_package_data=True,
+)
