ce642af1a79ba0e2ddcba3502abc01ef437cc0d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:28 +0000

    Add package zymbit-connect/0.4.0/zymbit-connect-0.4.0.tar.gz

---
+++ packages/zymbit-connect/zymbit-connect-0.4.0.tar.gz/zymbit-connect-0.4.0/src/zymbit/util/buffer.py
@@ -0,0 +1,29 @@
+import collections
+
+
+class BufferIterator(collections.Iterable):
+    def __init__(self, buf='', split_at='\n'):
+        self.buf = buf
+        self.split_at = split_at
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        try:
+            idx = self.buf.index(self.split_at)
+        except ValueError:
+            raise StopIteration()
+
+        buf = self.buf[:idx+1]
+        self.buf = self.buf[idx+1:]
+
+        return buf
+
+    def write(self, buf):
+        try:
+            buf.decode('utf8')
+        except AttributeError:
+            pass
+
+        self.buf += buf
