a5ab7ae94eac7817fdb92287aee54fc96bed20ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:44 +0000

    Add package LiStatham_nester/1.0.0/LiStatham_nester-1.0.0.tar.gz

---
+++ packages/LiStatham_nester/LiStatham_nester-1.0.0.tar.gz/LiStatham_nester-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name          = 'LiStatham_nester',
+    version       = '1.0.0',
+    py_modules    = ['LiStatham_nester'],
+    author        = 'Pythonlearner Li',
+    author_email  = 'lizhaoning1994@icloud.com',
+    url           = 'http://www.headfirstlabs.com',
+    description   = 'A simple printer of nested lists',
+    )
