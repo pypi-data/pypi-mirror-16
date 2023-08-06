45c8cc8af1ca55d2250cb55a4be4cf2235dbe63f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:50 +0000

    Add package nester_paladin/1.5.0/nester_paladin-1.5.0.zip

---
+++ packages/nester_paladin/nester_paladin-1.5.0.zip/nester_paladin-1.5.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'nester_paladin',
+    version = '1.5.0',
+    py_modules = ['nester'],
+    author = 'hfpython',
+    author_email = 'aa@bb.com',
+    url = 'http://www.bb.com',
+    description = 'A simple printer of nested lists',
+    )
