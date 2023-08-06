56977d9a211defab48f3d714ad6eb2b809e4d6ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:19:45 +0000

    Add package gviewer/2.1.1/gviewer-2.1.1.tar.gz

---
+++ packages/gviewer/gviewer-2.1.1.tar.gz/gviewer-2.1.1/gviewer/__init__.py
@@ -0,0 +1,5 @@
+from gviewer import GViewer
+from store import BaseDataStore, StaticDataStore, AsyncDataStore
+from displayer import BaseDisplayer
+from config import Config
+from view import Line, Text, Prop, Group, PropsGroup, Groups, View
