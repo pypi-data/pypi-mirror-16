e7e7b18d7d0bd2cedc88155177e0a17a85a047ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:47 +0000

    Add package nester1990xing/1.1.4/nester1990xing-1.1.4.zip

---
+++ packages/nester1990xing/nester1990xing-1.1.4.zip/nester1990xing-1.1.4/nester1990xing.py
@@ -0,0 +1,12 @@
+Python 3.5.2 (v3.5.2:4def2a2901a5, Jun 25 2016, 22:18:55) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> def print_lol(the_list):
+	for each_item in the_list:
+		if isinstance(each_item,list):
+			print_lol(each_item)
+		else:
+			print(each_item)
+
+			
+>>> movies=['zhenxing','lixia','yaoyao','liujing','dongdong',778,['weige','heiniu']]
+>>> 
