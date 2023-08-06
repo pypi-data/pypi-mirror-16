41d91aebfda5b1cd50ec6d7b5a75e882cc1f9130
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:01 +0000

    Add package loompy/0.2/loompy-0.2.tar.gz

---
+++ packages/loompy/loompy-0.2.tar.gz/loompy-0.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "loompy",
+    version = "0.2",
+    packages = find_packages(),
+    install_requires = [
+        'scikit-learn>=0.17.1',
+        'h5py>=2.6.0', 
+        'pandas>=0.18.1', 
+        'scipy>=0.18.0',
+        'numpy>=1.11.1', 
+    ],
+
+    # metadata for upload to PyPI
+    author = "Sten Linnarsson",
+    author_email = "sten.linnarsson@ki.se",
+    description = "Create and use .loom files from Python",
+    license = "BSD",
+    keywords = "loom omics transcriptomics bioinformatics",
+    url = "https://github.com/linnarsson-lab/loompy",
+)
