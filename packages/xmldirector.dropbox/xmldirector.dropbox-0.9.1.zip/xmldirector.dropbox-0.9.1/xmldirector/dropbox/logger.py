f2fd9a13e9af03d756336d56701e6e807ee4e721
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:53 +0000

    Add package xmldirector.dropbox/0.9.1/xmldirector.dropbox-0.9.1.zip

---
+++ packages/xmldirector.dropbox/xmldirector.dropbox-0.9.1.zip/xmldirector.dropbox-0.9.1/xmldirector/dropbox/logger.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+################################################################
+# xmldirector.dropbox
+# (C) 2016,  Andreas Jung, www.zopyx.com, Tuebingen, Germany
+################################################################
+
+import logging
+
+LOG = logging.getLogger('xmldirector.dropbox')
