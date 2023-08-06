67548fa7abade25bfcb371e411801cffc4e866dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:25 +0000

    Add package requests_logger/0.0.2/requests_logger-0.0.2.tar.gz

---
+++ packages/requests_logger/requests_logger-0.0.2.tar.gz/requests_logger-0.0.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='requests_logger',
+    version='0.0.2',
+    description='Wrapper around Requests to add logging',
+    author='Marcus Medley',
+    author_email='mdmeds@gmail.com',
+    packages=find_packages(exclude=('tests*')),
+    tests_require=['tox'],
+    keywords=['requests', 'logging', 'request'],
+    url='https://github.com/mdmedley/requests_logger',
+    classifiers=(
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: Other/Proprietary License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5'
+    ),
+)
