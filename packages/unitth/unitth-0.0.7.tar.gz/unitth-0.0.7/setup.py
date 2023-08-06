eb4ce59b7caacb62d1cb126c2773dc71397ef5df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:43 +0000

    Add package unitth/0.0.7/unitth-0.0.7.tar.gz

---
+++ packages/unitth/unitth-0.0.7.tar.gz/unitth-0.0.7/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+import os
+
+# version
+version = '0.0.7'
+
+# parse requirements.txt
+install_requires = [line.rstrip() for line in open('requirements.txt')]
+
+setup(
+    name="unitth",
+    version=version,
+    description="Python interface for UnitTH unit test history report generator",
+    url="https://github.com/KarrLab/unitth",
+    download_url='https://github.com/KarrLab/unitth/tarball/{}'.format(version),
+    author="Jonathan Karr",
+    author_email="jonrkarr@gmail.com",
+    license="MIT",
+    keywords='unit test xunit junit unitth HTML history',
+    packages=find_packages(),
+    package_data={
+        'unitth': ['lib/unitth/unitth.jar'],
+    },
+    install_requires=install_requires,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+    ],
+    entry_points={
+        'console_scripts': [
+            'unitth = unitth.__main__:main',
+        ],
+    },
+)
