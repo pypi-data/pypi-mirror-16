fb0d357b5167c15d887519587224181b89ec36f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:56:44 +0000

    Add package tuturu2/0.5/tuturu2-0.5.tar.gz

---
+++ packages/tuturu2/tuturu2-0.5.tar.gz/tuturu2-0.5/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+  name = 'tuturu2',
+  packages = ['tuturu2'], # this must be the same as the name above
+  version = '0.5',
+  description = 'A module to simplify the well know module named pickle.',
+  author = 'Pickle',
+  author_email = 'pickle@gmail.com',
+  url = 'http://marco213.com', # use the URL to the github repo
+  keywords = ['pickle', 'tuturu'], # arbitrary keywords
+  classifiers = [],
+)
