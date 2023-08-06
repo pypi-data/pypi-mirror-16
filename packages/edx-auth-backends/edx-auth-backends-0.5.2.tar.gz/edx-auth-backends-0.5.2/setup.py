9f31f66e798dde5849aebcd7763791cc6c5bc222
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:22:52 +0000

    Add package edx-auth-backends/0.5.2/edx-auth-backends-0.5.2.tar.gz

---
+++ packages/edx-auth-backends/edx-auth-backends-0.5.2.tar.gz/edx-auth-backends-0.5.2/setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+from auth_backends import __version__
+
+
+with open('README.rst') as a, open('HISTORY.rst') as b, open('AUTHORS') as c:
+    long_description = '{}\n\n{}\n\n{}'.format(a.read(), b.read(), c.read())
+
+setup(
+    name='edx-auth-backends',
+    version=__version__,
+    description='Custom edX authentication backends and pipeline steps',
+    long_description=long_description,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Affero General Public License v3',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Framework :: Django',
+        'Framework :: Django :: 1.8',
+        'Framework :: Django :: 1.9',
+        'Topic :: Internet',
+    ],
+    keywords='authentication edx',
+    url='https://github.com/edx/auth-backends',
+    author='edX',
+    author_email='oscm@edx.org',
+    license='AGPL',
+    packages=find_packages(),
+    install_requires=[
+        'Django>=1.8,<1.10',
+        'python-social-auth>=0.2.21,<1.0.0',
+        'six>=1.10.0,<2.0.0'
+    ],
+)
