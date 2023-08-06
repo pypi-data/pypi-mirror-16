865d380b340bb115b90509e1f516261ced984a42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:53 +0000

    Add package Smyte/0.1.2/Smyte-0.1.2.tar.gz

---
+++ packages/Smyte/Smyte-0.1.2.tar.gz/Smyte-0.1.2/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+  name='Smyte',
+  version='0.1.2',
+  description='Smyte client library for Python',
+  author='Pete Hunt',
+  py_modules=['smyte'],
+  install_requires=['requests'],
+)
