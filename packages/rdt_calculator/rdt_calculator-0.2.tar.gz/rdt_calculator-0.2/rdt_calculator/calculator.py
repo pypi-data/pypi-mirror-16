8c7739ee66ec5a2e2777569175dbb8603d828de5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:20 +0000

    Add package rdt_calculator/0.2/rdt_calculator-0.2.tar.gz

---
+++ packages/rdt_calculator/rdt_calculator-0.2.tar.gz/rdt_calculator-0.2/rdt_calculator/calculator.py
@@ -0,0 +1,8 @@
+class Calculator(object):
+
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def add(self):
+        return a + b
---
+++ packages/rdt_calculator/rdt_calculator-0.2.tar.gz/rdt_calculator-0.2/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+
+setup(
+  name = 'rdt_calculator',
+  packages = ['rdt_calculator'],
+  version = '0.2',
+  description = 'A simple calculator',
+  author = 'Rawdata Technologies PVT Limited',
+  author_email = 'logging@rawdatatech.com',
+  url = 'https://github.com/abdulshukoorpk/rdt_calculator',
+  download_url = 'https://github.com/abdulshukoorpk/rdt_calculator',
+  keywords = ['testing', 'calculator', 'addition'],
+  classifiers = [],
+)
