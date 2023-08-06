8c1b1ae31d7e366434929b0c297edcdb2f426b78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:32 +0000

    Add package mlb_nester/1.1.0/mlb_nester-1.1.0.tar.gz

---
+++ packages/mlb_nester/mlb_nester-1.1.0.tar.gz/mlb_nester-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name			= 'mlb_nester',
+	version			= '1.1.0',
+	py_modules		= ['nester'],
+	author			= 'markbradley',
+	author_email	= 'mlbradleyii@gmail.com',
+    url             = 'http://www.mlbradleyii.com',
+	description		= 'A simple printer of nested lists',
+)
