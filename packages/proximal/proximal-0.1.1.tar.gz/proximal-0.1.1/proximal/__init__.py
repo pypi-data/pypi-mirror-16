88e07185d9025376b98d95a020f91ed9196388b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:27 +0000

    Add package proximal/0.1.1/proximal-0.1.1.tar.gz

---
+++ packages/proximal/proximal-0.1.1.tar.gz/proximal-0.1.1/proximal/__init__.py
@@ -0,0 +1,6 @@
+__version__ = "0.1.1"
+
+from .prox_fns import *
+from .lin_ops import *
+from .algorithms import *
+
---
+++ packages/proximal/proximal-0.1.1.tar.gz/proximal-0.1.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name='proximal',
+    version='0.1.1', 
+    packages=['proximal',
+              'proximal.prox_fns',
+              'proximal.lin_ops',
+              'proximal.algorithms',
+              'proximal.utils',
+              'proximal.halide',
+              'proximal.tests',
+              'proximal.tests.data'],
+    package_dir = {'proximal':'proximal'},
+    package_data = {'proximal.tests.data': ['angela.jpg'],
+                    'proximal.halide': ['src/*.cpp', 'src/core/*', 'src/external/*', 'src/fft/*']},
+    install_requires=["numpy >= 1.9",
+                      "scipy >= 0.15",
+                      "cv2",
+                      "PIL"],
+    use_2to3=True,
+)
