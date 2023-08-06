c90e207c88b8adf2b13feb6882952541b85bdfc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:09 +0000

    Add package pytsite/0.80.24/PytSite-0.80.24.tar.gz

---
+++ packages/pytsite/PytSite-0.80.24.tar.gz/PytSite-0.80.24/pytsite/comments/_error.py
@@ -0,0 +1,17 @@
+"""PytSite Comments Package Errors.
+"""
+__author__ = 'Alexander Shepetko'
+__email__ = 'a@shepetko.com'
+__license__ = 'MIT'
+
+
+class DriverAlreadyRegistered(Exception):
+    pass
+
+
+class DriverNotRegistered(Exception):
+    pass
+
+
+class InvalidCommentStatus(Exception):
+    pass
