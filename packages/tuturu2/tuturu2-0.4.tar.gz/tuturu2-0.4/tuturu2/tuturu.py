5217638d206863456ab58413117489da824bcb6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:51 +0000

    Add package tuturu2/0.4/tuturu2-0.4.tar.gz

---
+++ packages/tuturu2/tuturu2-0.4.tar.gz/tuturu2-0.4/tuturu2/tuturu.py
@@ -0,0 +1,18 @@
+import pickle 
+def load(path):
+    try:
+        file = open(path, "rb")
+        tuturu = pickle.load(file)
+        file.close()
+        return tuturu
+    except Exception:
+        return None
+        
+def dump(var, path):
+    try:
+        file = open(path, "wb")
+        pickle.dump(var, file)
+        file.close()
+        return True
+    except Exception:
+        return False
