672623bc9edc9248110ef2a87754b35ef4da28bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:42 +0000

    Add package temser/0.1.0/temser-0.1.0.tar.gz

---
+++ packages/temser/temser-0.1.0.tar.gz/temser-0.1.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='temser',
+    version='0.1.0',
+    description='A templating engine and web server for data driven pages.',
+    url='https://github.com/dagnelies/temser',
+    author='Arnaud Dagnelies',
+    author_email='arnaud.dagnelies@gmail.com',
+    license='MIT',
+    keywords='template server',
+    py_modules=['temser'],
+    scripts=['temser.py'],
+    install_requires=[
+        'bottle',
+        'pystache'
+    ]
+)
