c30fab7a7938214e04a4b98535660c138e299cd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:15:00 +0000

    Add package nester_wanglei/1.0.0/nester_wanglei-1.0.0.zip

---
+++ packages/nester_wanglei/nester_wanglei-1.0.0.zip/nester_wanglei-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name			= 'nester_wanglei',
+	version			= '1.0.0',
+	py_modules		= ['nester'],
+	author			= 'hfpython',
+	author_email	= 'lei@126.com',
+	url				= 'http://www.baidu.com',
+	description		= 'A simple printer of nested lists',
+	)
