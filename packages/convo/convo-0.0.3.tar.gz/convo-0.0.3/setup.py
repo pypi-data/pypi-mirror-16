ebf830ddfe622539497fad8527992297d5381632
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:14:46 +0000

    Add package convo/0.0.3/convo-0.0.3.tar.gz

---
+++ packages/convo/convo-0.0.3.tar.gz/convo-0.0.3/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(name='convo',
+      version='0.0.3',
+      description='Medium-agnostic conversation wrangler',
+      long_description=open('README.rst').read(),
+      author='Benno Rice',
+      author_email='benno@jeamland.net',
+      url='https://github.com/jeamland/convo',
+      package_data={'': ['LICENSE']},
+      py_modules=['convo'],
+      classifiers=(
+          'Development Status :: 2 - Pre-Alpha',
+          'Intended Audience :: Developers',
+          'Natural Language :: English',
+          'License :: OSI Approved :: BSD License',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: Implementation :: CPython',
+      ),
+)
