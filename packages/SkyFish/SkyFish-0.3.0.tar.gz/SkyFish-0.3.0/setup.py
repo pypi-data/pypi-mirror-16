92c803d09cba1c19473891d51fc0f5724d57e377
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:22:32 +0000

    Add package SkyFish/0.3.0/SkyFish-0.3.0.tar.gz

---
+++ packages/SkyFish/SkyFish-0.3.0.tar.gz/SkyFish-0.3.0/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='SkyFish',
+    version='0.3.0',
+    description='Official Skyfish Inventory Integration SDK for Merchants',
+    long_description="Skyfish merchants can use this as the simplest way to integrate and sync their inventory to skyfish merchants",
+    author='Fauzan Emmerling',
+    author_email='erich@emfeld.com',
+    url='https://github.com/coralhq/skyfish-py-sdk',
+    license='MIT',
+    packages=['skyfish'],
+    install_requires=['requests'],
+    zip_safe=False)
