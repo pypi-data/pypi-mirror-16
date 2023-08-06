40d705ff80ca1d670cd34f366c98529e69f9cbff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:49 +0000

    Add package hanoolchoi_nester/1.2.0/hanoolchoi_nester-1.2.0.zip

---
+++ packages/hanoolchoi_nester/hanoolchoi_nester-1.2.0.zip/hanoolchoi_nester-1.2.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+
+    name='hanoolchoi_nester',
+    version='1.2.0',
+    py_modules = ['hanoolchoi_nester'],
+    author = 'hanoolchoi',
+    author_email = 'choihanool12@gmail.com',
+    url = 'http://hanoolchoi.blogspot.kr/',
+    description = 'A simple printer of nested lists',
+    )
