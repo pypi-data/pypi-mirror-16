9810e06f51c84373771fc1056927fcd1336ef5f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:57 +0000

    Add package chapter1_2/1.2.0/chapter1_2-1.2.0.zip

---
+++ packages/chapter1_2/chapter1_2-1.2.0.zip/chapter1_2-1.2.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+	name = 'chapter1_2',
+	version = '1.2.0',
+	py_modules = ['chapter1_2'],
+	author = 'TTT',
+	author_email = 'TzuTeng.Tzeng@ctci.com.tw',
+	url = '',
+	description = 'A simple printer of nested lists'
+	)
