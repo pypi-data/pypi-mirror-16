ff8e6d1c33ef9ca5f8f1cb9c1c494c22bdd93a13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:25 +0000

    Add package django-bulbs/3.9.4/django-bulbs-3.9.4.tar.gz

---
+++ packages/django-bulbs/django-bulbs-3.9.4.tar.gz/django-bulbs-3.9.4/bulbs/contributions/renderers.py
@@ -0,0 +1,50 @@
+from rest_framework_csv.renderers import CSVRenderer
+
+
+class HeaderCSVRenderer(CSVRenderer):
+
+    results_field = 'results'
+
+    def render(self, data, media_type=None, renderer_context=None):
+        if not isinstance(data, list):
+            data = data.get(self.results_field, [])
+        return super(HeaderCSVRenderer, self).render(
+            data, media_type=media_type, renderer_context=renderer_context
+        )
+
+    def tablize(self, data):
+        """
+        rest_framework_csv doesn't let us easily set headers.
+        So this will by defining the header_fields attribute.
+        """
+        table = super(HeaderCSVRenderer, self).tablize(data)
+        if self.header_fields and table:
+            header_row = []
+            for key in table[0]:
+                header_row.append(self.header_fields.get(key, key))
+            table[0] = header_row
+        return table
+
+
+class ContributionReportingRenderer(HeaderCSVRenderer):
+
+    header_fields = {
+        'id': 'Content ID',
+        'first_name': 'First Name',
+        'last_name': 'Last Name',
+        'title': 'Title',
+        'feature_type': 'Feature Type',
+        'published': 'Publish Date',
+        'rate': 'Rate',
+        'payroll_name': 'Payroll Name'
+    }
+
+
+class LineItemRenderer(HeaderCSVRenderer):
+
+    header_fields = {
+        "note": "Note",
+        "amount": "Amount",
+        "payment_date": "Payment Date",
+        "payroll_name": "Payroll Name"
+    }
