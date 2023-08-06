069f7f7a2f13f256112966635aefc2f7c333dc2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:01 +0000

    Add package bbcondeparser/0.0.2/bbcondeparser-0.0.2.tar.gz

---
+++ packages/bbcondeparser/bbcondeparser-0.0.2.tar.gz/bbcondeparser-0.0.2/bbcondeparser/__init__.py
@@ -0,0 +1,9 @@
+from bbcondeparser.tags import (
+    BaseTag, RawText, ErrorText, SimpleTag, TagCategory,
+)
+from bbcondeparser.tree_parser import BaseTreeParser
+
+__all__ = [
+    'BaseTag', 'RawText', 'ErrorText', 'SimpleTag', 'TagCategory',
+    'BaseTreeParser',
+]
