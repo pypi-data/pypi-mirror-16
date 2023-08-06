75ebef04dc4fa24d932fb7e605fbf192ff1a977b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:30 +0000

    Add package fandango/12.0/fandango-12.0.1.tar.gz

---
+++ packages/fandango/fandango-12.0.1.tar.gz/fandango-12.0.1/fandango/device/__init__.py
@@ -0,0 +1,13 @@
+
+try:
+  from DDebug import *
+except:
+  print('Unable to import DDebug')
+try:
+  from Dev4Tango import *
+except:
+  print('Unable to import Dev4Tango')
+try:
+  from FolderDS import FolderDS, FolderDSClass, FolderAPI
+except:
+  print('Unable to import FolderDS')
