a788557ce405f08ce676f310a9d947b0313375a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:24 +0000

    Add package wagtailerrorpages/1.1.3/wagtailerrorpages-1.1.3-py2.py3-none-any.whl

---
+++ packages/wagtailerrorpages/wagtailerrorpages-1.1.3-py2.py3-none-any.whl/wagtailerrorpages/templatetags/wagtailerrorpages_tags.py
@@ -0,0 +1,22 @@
+from django import template
+from wagtail.wagtailcore.models import Page
+try:
+    # Python 3
+    from urllib.parse import unquote_plus
+except ImportError:
+    # Python 2
+    from urllib import unquote_plus
+
+register = template.Library()
+
+
+@register.inclusion_tag('wagtailerrorpages/fragments/404message.html', takes_context=True)
+def message404(context):
+    url_path = context['request'].path_info
+    search_query = unquote_plus(url_path).replace('/', ' ')
+    search_results = Page.objects.live().public().search(search_query)
+
+    return {
+        'search_query': search_query,
+        'search_results': search_results,
+    }
