a0af989e0c03f8ae0d8089c1a6786647a4600385
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:03:52 +0000

    Add package planemo/0.28.0/planemo-0.28.0-py2.7.egg

---
+++ packages/planemo/planemo-0.28.0-py2.7.egg/planemo/commands/cmd_shed_build.py
@@ -0,0 +1,30 @@
+"""Module describing the planemo ``shed_build`` command."""
+import sys
+
+import click
+import shutil
+
+from planemo.cli import command_function
+from planemo import options
+from planemo import shed
+
+
+@click.command("shed_build")
+@options.optional_tools_arg(multiple=False)
+@command_function
+def cli(ctx, path, **kwds):
+    """Create a Galaxy tool tarball.
+
+    This will use the .shed.yml file to prepare a tarball
+    (which you could upload to the Tool Shed manually).
+    """
+
+    def build(realized_repository):
+        tarpath = shed.build_tarball(realized_repository.real_path)
+        outpath = realized_repository.real_path + ".tar.gz"
+        shutil.move(tarpath, outpath)
+        print("Created: %s" % (outpath))
+        return 0
+
+    exit_code = shed.for_each_repository(ctx, build, [path], **kwds)
+    sys.exit(exit_code)
