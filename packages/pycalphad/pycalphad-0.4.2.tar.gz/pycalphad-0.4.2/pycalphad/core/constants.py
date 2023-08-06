19cc3e4e7854917e9796a7daea55dedaaa8b4e1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:15:32 +0000

    Add package pycalphad/0.4.2/pycalphad-0.4.2.tar.gz

---
+++ packages/pycalphad/pycalphad-0.4.2.tar.gz/pycalphad-0.4.2/pycalphad/core/constants.py
@@ -0,0 +1,12 @@
+"""
+The constants module contains some numerical constants for use
+in the module.
+Note that modifying these may yield unpredictable results.
+"""
+# Force zero values to this amount, for numerical stability
+MIN_SITE_FRACTION = 1e-12
+# For each phase pair with composition difference below tolerance, eliminate phase with largest index
+COMP_DIFFERENCE_TOL = 1e-9
+
+# 'infinity' for numerical purposes
+BIGNUM = 1e60
