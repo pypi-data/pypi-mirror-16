6ce8a53293aa78ad7d0e6efe2057e1ff714d62a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:00 +0000

    Add package Vinod_python/1.0.0/Vinod_python-1.0.0.tar.gz

---
+++ packages/Vinod_python/Vinod_python-1.0.0.tar.gz/Vinod_python-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup (
+	name 		= 'Vinod_python',
+	version		= '1.0.0',
+	py_modules 	= ['Vinod_python'],
+	author	 	= 'Vinod7788',
+	author_email 	= 'vinod_h_chavan@rediffmail.com',
+	url		= 'http://www.headfirstlabs.com',
+	description	= 'Wordcount LOgic',
+      )   	
