b07f2b0fa30bd7bdf033b8ec17b8a59a4265e06d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:11 +0000

    Add package pytsite/0.80.3/PytSite-0.80.3.tar.gz

---
+++ packages/pytsite/PytSite-0.80.3.tar.gz/PytSite-0.80.3/pytsite/flag/_model.py
@@ -0,0 +1,24 @@
+"""Flag Package Models.
+"""
+from pytsite import odm as _odm
+
+__author__ = 'Alexander Shepetko'
+__email__ = 'a@shepetko.com'
+__license__ = 'MIT'
+
+
+class Flag(_odm.model.Entity):
+    """Flag ODM Model.
+    """
+    def _setup_fields(self):
+        """Hook.
+        """
+        self.define_field(_odm.field.String('type', default='default'))
+        self.define_field(_odm.field.Ref('entity', nonempty=True))
+        self.define_field(_odm.field.Ref('author', model='user', nonempty=True))
+        self.define_field(_odm.field.Decimal('score', default=1))
+
+    def _setup_indexes(self):
+        """Hook.
+        """
+        self.define_index([('entity', _odm.I_ASC), ('author', _odm.I_ASC)], unique=True)
