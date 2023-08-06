aa0c5c7b19ec3419651cffe22cd39097ca7db519
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:17:44 +0000

    Add package pixelsorter/1.1/pixelsorter-1.1.tar.gz

---
+++ packages/pixelsorter/pixelsorter-1.1.tar.gz/pixelsorter-1.1/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='pixelsorter',
+    packages=['pixelsorter'],
+    version='1.1',
+    description='A powerful and extensible library for pixel-sorting.',
+    author='Raphael Kargon',
+    author_email='raphael.kargon@gmail.com',
+    url='https://github.com/rkargon/pixelsorter',
+    download_url='https://github.com/rkargon/pixelsorter/tarball/1.1',
+    keywords=['pixelsorting', 'glitch art', 'pixel', 'sorting'],
+    classifiers=[],
+)
