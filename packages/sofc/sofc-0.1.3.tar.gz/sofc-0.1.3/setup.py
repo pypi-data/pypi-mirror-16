b0cb9a94a04082bbbfdf6c47cf2d2fc59dfee6d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:39 +0000

    Add package sofc/0.1.3/sofc-0.1.3.tar.gz

---
+++ packages/sofc/sofc-0.1.3.tar.gz/sofc-0.1.3/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding=utf-8
+#
+# Author:
+# Created Time: 2016年07月26日 星期二 11时02分41秒
+
+from setuptools import setup, find_packages
+
+setup(
+    name='sofc',
+    version='0.1.3',
+    keywords=('stackoverflow'),
+    description='stackoverflow client for quick search',
+    license='MIT License',
+
+    url='https://cyy0523xc.github.io/',
+    author='Alex Cai',
+    author_email='cyy0523xc@gmail.com',
+
+    packages=find_packages(),
+    include_package_data=True,
+    platforms='any',
+    install_requires=["click", "pyquery", "urllib", "requests"],
+    scripts=["sofc/sofc.py"],
+    entry_points={
+        "console_scripts": [
+            'sofc = sofc.sofc:main'
+        ]
+    }
+)
