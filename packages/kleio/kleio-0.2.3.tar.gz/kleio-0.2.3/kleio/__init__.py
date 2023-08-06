1dfd9a864d79b49b18b0da4098d0a788ae40d575
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:39 +0000

    Add package kleio/0.2.3/kleio-0.2.3.tar.gz

---
+++ packages/kleio/kleio-0.2.3.tar.gz/kleio-0.2.3/kleio/__init__.py
@@ -0,0 +1,14 @@
+__author__ = 'szednik'
+__version__ = "0.2.3"
+__date__ = "2016/07/27"
+__docformat__ = "restructuredtext en"
+
+__all__ = [
+    'prov'
+]
+
+NORMALIZE_LITERALS = True
+
+DAWG_LITERAL_COLLATION = False
+
+from kleio import prov
