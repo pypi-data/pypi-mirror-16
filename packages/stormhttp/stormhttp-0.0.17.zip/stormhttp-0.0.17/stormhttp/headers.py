74637a54f1d8a9cd75d5e6d6586cbc90b24108ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:34 +0000

    Add package stormhttp/0.0.17/stormhttp-0.0.17.zip

---
+++ packages/stormhttp/stormhttp-0.0.17.zip/stormhttp-0.0.17/stormhttp/headers.py
@@ -0,0 +1,34 @@
+# Global Variables
+__all__ = [
+    "HttpHeaders"
+]
+
+
+class HttpHeaders(dict):
+    def __init__(self, *args, **kwargs):
+        self.update(*args, **kwargs)
+
+    def __getitem__(self, key: bytes) -> bytes:
+        return dict.__getitem__(self, key.upper())
+
+    def __setitem__(self, key: bytes, val: bytes) -> None:
+        dict.__setitem__(self, key.upper(), val)
+
+    def __delitem__(self, key: bytes) -> None:
+        dict.__delitem__(self, key.upper())
+
+    def __contains__(self, key: bytes) -> bool:
+        return dict.__contains__(self, key.upper())
+
+    def __repr__(self):
+        return dict.__repr__(self)
+
+    def get(self, key, default=None):
+        return dict.get(self, key.upper(), default)
+
+    def update(self, *args, **kwargs):
+        for key, val in dict(*args, **kwargs).items():
+            self[key] = val
+
+    def to_bytes(self) -> bytes:
+        return b'\r\n'.join(b'%b: %b' % (key, val) for key, val in self.items())
