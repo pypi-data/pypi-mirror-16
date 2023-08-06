e05725b8b9a7930afc24b0cf942ecc22fe486618
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:58 +0000

    Add package simplejsonrpc/1.2.1/simplejsonrpc-1.2.1.zip

---
+++ packages/simplejsonrpc/simplejsonrpc-1.2.1.zip/simplejsonrpc-1.2.1/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+  name = 'simplejsonrpc',
+  packages = ['simplejsonrpc'],
+  version = '1.2.1',
+  description = 'Simple JSON-RPC 2.0 compilant middleware for python using decorators. With just a few lines of code you get a JSONRPC 2.0 compilant web API running and ready for your needs!',
+  license = 'MIT',
+  author = 'Moritz Wundke',
+  author_email = 'b.thax.dcg@gmail.com',
+  url = 'https://github.com/moritz-wundke/simplejsonrpc',
+  download_url = 'https://github.com/moritz-wundke/simplejsonrpc/releases/tag/Release_1.2.1',
+  keywords = ['jsonrpc', 'rpc', 'json'],
+  classifiers = [],
+)
