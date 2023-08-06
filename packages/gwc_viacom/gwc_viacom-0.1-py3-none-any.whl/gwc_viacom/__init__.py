518616bfb1f79b33b8f70c4021c567b76288f818
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:16:35 +0000

    Add package gwc_viacom/0.1/gwc_viacom-0.1-py3-none-any.whl

---
+++ packages/gwc_viacom/gwc_viacom-0.1-py3-none-any.whl/gwc_viacom/__init__.py
@@ -0,0 +1,50 @@
+def gwc_viacom():
+    print(u'1. Write new code, but keep the old (with version control).' + '\n' +
+    	'2. Floor 11. It’s all you need to know.' + '\n' +
+    	'3. Comment your code so that others can read it! ' + '\n' + 
+    	'4. Space it out.' + '\n' + 
+    	'5. Good style keeps the bugs away.' + '\n' +
+    	'6. When it gets too confusing google it.' + '\n' +
+    	'7. Classes are blueprints for objects and objects in a given class have specific attributes and methods, which can be inherited.' + '\n' +
+    	'8. Dab it out with some dank memes.' + '\n' +
+    	'9. Find new ways to do the same thing -- don’t get stuck in one way.' + '\n' +
+    	'10. Avoid the meanies.' + '\n' +
+    	'11. KPOP, BTS + coding == <3; TRUE' + '\n' +
+    	'12. Take breaks, have patience, and ask Rachel for grapes.' + '\n' +
+    	'13. Them semicolons can be your worse nightmare.' + '\n' +
+    	'14. Emily once got free dessert at an Italian restaurant.' + '\n' +
+    	'15. Without requirements or design, programming is the art of adding bugs to an empty file.' + '\n' +
+    	'16. You\'re not you when you\'re hungry.' + '\n' +
+    	'17. Resist the urge to copy code.' + '\n' +
+    	'18. If you struggle with a code for hours you\'ll feel inclined to throw your laptop out the window. Don\'t, you’ll be glad you did not.' + '\n' +
+    	'19. 20 girls can fit into one elevator.' + '\n' +
+    	'20. Rainbow.' + '\n' + 
+    	 '---Rules by GWC Vicaom \'16')
+
+gwc_viacom()
+
+#if __name__ == "__main__":
+#	gwc_viacom()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+ 
+
+
+
