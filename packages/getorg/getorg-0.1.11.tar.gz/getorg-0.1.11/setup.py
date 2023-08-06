2857b6267b9dc28d0ec8486561121fa3c0a17cc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:08 +0000

    Add package getorg/0.1.11/getorg-0.1.11.tar.gz

---
+++ packages/getorg/getorg-0.1.11.tar.gz/getorg-0.1.11/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='getorg',
+      version='0.1.11',
+      description='Information and analytics about GitHub organizations',
+      url='http://github.com/getorg/getorg',
+      author='Stuart Geiger',
+      author_email='stuart@stuartgeiger.com',
+      license='MIT',
+      packages=['getorg'],
+      install_requires=[
+          'pygithub', 'ipywidgets', 'ipyleaflet', 'geopy', 'datetime'],
+      zip_safe=False)
