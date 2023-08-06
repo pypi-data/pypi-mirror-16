8a355797033fb94bb5cf3dc931183a038e13b7b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:27 +0000

    Add package ascii/2.6/ascii-2.6.tar.gz

---
+++ packages/ascii/ascii-2.6.tar.gz/ascii-2.6/ascii/colorize.py
@@ -0,0 +1,18 @@
+from ascii import convert
+
+def colorize(rawChar, px):
+	r = px[0]
+	g = px[1]
+	b = px[2]
+	rgb = [r,g,b]
+	code = convert.convert["rgb"]["ansi256"](rgb)
+	color = wrapAnsi256(code, 0)
+	reset = '\x1b[0m'
+	
+	return color + rawChar + reset
+
+def wrapAnsi16(code, offset):
+	return '\x1b[' + str(code) + 'm'
+
+def wrapAnsi256(code, offset):
+	return '\x1b[38;5;' + str(offset) + 'm'
