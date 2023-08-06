0ae8ba695e4aac20e4e61ce0995477788fc137b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:27 +0000

    Add package django-perms/1.3.0/django-perms-1.3.0.tar.gz

---
+++ packages/django-perms/django-perms-1.3.0.tar.gz/django-perms-1.3.0/permissions/tests/base.py
@@ -0,0 +1,44 @@
+from django.test import TestCase as BaseTestCase
+
+from django.test import RequestFactory
+
+from permissions import PermissionsRegistry as BasePermissionsRegistry
+
+
+class PermissionsRegistry(BasePermissionsRegistry):
+
+    def _get_user_model(self):
+        return User
+
+    def _get_model_instance(self, model, **kwargs):
+        return model(**kwargs)
+
+
+class Model(object):
+
+    def __init__(self, **kwargs):
+        for k, v in kwargs.items():
+            setattr(self, k, v)
+
+
+class User(Model):
+
+    def __init__(self, **kwargs):
+        kwargs.setdefault('permissions', [])
+        super(User, self).__init__(**kwargs)
+
+    def is_anonymous(self):
+        return False
+
+
+class AnonymousUser(User):
+
+    def is_anonymous(self):
+        return True
+
+
+class TestCase(BaseTestCase):
+
+    def setUp(self):
+        self.registry = PermissionsRegistry()
+        self.request_factory = RequestFactory()
