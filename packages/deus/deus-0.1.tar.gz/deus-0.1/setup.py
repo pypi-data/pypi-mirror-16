affb41e986e65e4953feace3191b4da182dcc0df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:34 +0000

    Add package deus/0.1/deus-0.1.tar.gz

---
+++ packages/deus/deus-0.1.tar.gz/deus-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='deus',
+      version='0.1',
+      description='Package scaffolder inspired by Bundler and Mix',
+      url='http://github.com/ohsabry/deus',
+      author='Omar Sabry',
+      author_email='ohsabry@gmail.com',
+      license='MIT',
+      packages=['deus'],
+      zip_safe=False)
